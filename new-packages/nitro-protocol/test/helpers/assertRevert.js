d24824d97cef9ccb16aa099d497c0f34dbffff8d
Tom Close <tom.close@cantab.net>
2018-02-28 22:35:41 -0800

    RockPaperScissors working

---
+++ new-packages/nitro-protocol/test/helpers/assertRevert.js
@@ -0,0 +1,10 @@
+// https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/test/helpers/assertRevert.js
+export default async promise => {
+  try {
+    await promise;
+    assert.fail('Expected revert not received');
+  } catch (error) {
+    const revertFound = error.message.search('revert') >= 0;
+    assert(revertFound, `Expected "revert", got ${error} instead`);
+  }
+};
