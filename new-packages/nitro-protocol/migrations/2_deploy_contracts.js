8b3d6c0dfb68bf4eac34e3fd8e6d8c15ba5b6ccb
Tom Close <tom.close@cantab.net>
2018-03-02 17:54:11 -0800

    Get gameState extraction and delegation working

--- new-packages/nitro-protocol/migrations/2_deploy_contracts.js
+++ new-packages/nitro-protocol/migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 var RockPaperScissors = artifacts.require("./RockPaperScissors.sol");
 var StartFinishGame = artifacts.require("./StartFinishGame.sol");
+var SimpleAdjudicator = artifacts.require("./SimpleAdjudicator.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(RockPaperScissors);
   deployer.deploy(StartFinishGame);
+  deployer.deploy(SimpleAdjudicator);
   // deployer.link(ConvertLib, MetaCoin);
   // deployer.deploy(MetaCoin);
 };
