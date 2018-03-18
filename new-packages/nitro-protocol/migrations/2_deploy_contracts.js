75a2b3529968e0c171c4dd19d8da68892493bf82
Tom Close <tom.close@cantab.net>
2018-03-18 09:13:09 +0000

    Simple Adjudicator converted

--- new-packages/nitro-protocol/migrations/2_deploy_contracts.js
+++ new-packages/nitro-protocol/migrations/2_deploy_contracts.js
@@ -1,13 +1,14 @@
 var RockPaperScissorsGame = artifacts.require("./RockPaperScissorsGame.sol");
 var RockPaperScissorsState = artifacts.require("./RockPaperScissorsState.sol");
-// var SimpleAdjudicator = artifacts.require("./SimpleAdjudicator.sol");
+var SimpleAdjudicator = artifacts.require("./SimpleAdjudicator.sol");
 var IncrementationState = artifacts.require("./IncrementationState.sol");
 var IncrementationGame = artifacts.require("./IncrementationGame.sol");
 var CommonState = artifacts.require("./CommonState.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(SimpleAdjudicator);
   deployer.deploy(CommonState);
+  deployer.link(CommonState, SimpleAdjudicator);
+  deployer.deploy(SimpleAdjudicator);
 
   deployer.link(CommonState, IncrementationState);
   deployer.deploy(IncrementationState);
