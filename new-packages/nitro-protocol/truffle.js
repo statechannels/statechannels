1a0f3e0a48ef699e96e0c4a10695575c46d3e91d
Tom Close <tom.close@cantab.net>
2018-03-19 08:13:36 +0000

    Ability to calculate gas usage in testing

--- new-packages/nitro-protocol/truffle.js
+++ new-packages/nitro-protocol/truffle.js
@@ -4,4 +4,10 @@ require("babel-polyfill");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  mocha: {
+    // reporter: 'eth-gas-reporter',
+    reporterOptions : {
+      currency: 'USD',
+    }
+  }
 };
