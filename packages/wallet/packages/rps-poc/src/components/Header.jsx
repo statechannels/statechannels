59aba33497d12f9bf7944ecc8c0cb18cb274bba6
Andrew Hunt <andrew@hunt.codes>
2018-05-10 22:26:20 -0700

    [first commit]

---
+++ packages/wallet/packages/rps-poc/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<div
+			style={{
+				width: "100%",
+				height: 58,
+				borderBottomStyle: "solid",
+				borderBottomWidth: 1,
+				borderBottomColor: "#bbb"
+			}}
+		>
+			<div style={{ position: 'absolute', left: 20 }}>
+				<a href="/">
+					<h3>RPS</h3>
+				</a>
+			</div>
+			<div style={{ position: 'absolute', right: 20, top: 0 }}>
+				<a style={{ display: 'inline-block', marginRight: 16 }} href="/">
+					<h3>Home</h3>
+				</a>
+				<a style={{ display: 'inline-block' }} href="/about">
+					<h3>About</h3>
+				</a>
+			</div>
+		</div>
+	);
+}
