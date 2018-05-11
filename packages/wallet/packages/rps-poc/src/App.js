8d496ad900e431e58f79bb8297bf03dc0e8c9a92
Andrew Hunt <andrew@hunt.codes>
2018-05-11 14:50:48 -0700

    switch home page back in

--- packages/wallet/packages/rps-poc/src/App.js
+++ packages/wallet/packages/rps-poc/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import HomePage from './components/HomePage';
-import HowItWorksPage from './components/HowItWorksPage';
+// import HowItWorksPage from './components/HowItWorksPage';
 import './App.css';
 
 // TODO(ah): Add react-router
@@ -14,7 +14,7 @@ class App extends React.Component {
         <header>
           <Header />
         </header>
-        <HowItWorksPage />
+        <HomePage />
       </div>
     );
   }
