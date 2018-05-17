9805d158ae0c24334fd9756aca21782490716515
Andrew Hunt <andrew@hunt.codes>
2018-05-16 23:01:16 -0700

    add routing, more styling, and about page

--- packages/rps/packages/rps-poc/src/App.js
+++ packages/rps/packages/rps-poc/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import './App.css';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
-// import HowItWorksPage from './components/HowItWorksPage';
-import './App.css';
-
-// TODO(ah): Add react-router
+import HowItWorksPage from './components/HowItWorksPage';
+import AboutPage from './components/AboutPage';
+import { ROUTE_PATHS } from './constants';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="font">
-        <header>
-          <Header />
-        </header>
-        <HomePage />
-      </div>
+      <BrowserRouter>
+        <div className="font">
+          <header>
+            <Header />
+          </header>
+          <Route exact path="/" component={HomePage} />
+          <Route path={`/${ROUTE_PATHS.HOW_IT_WORKS}`} component={HowItWorksPage} />
+          <Route path={`/${ROUTE_PATHS.ABOUT}`} component={AboutPage} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
