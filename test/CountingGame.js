ac03f85e7e2b1e120068cb2856a4470902e5f8a9
Tom Close <tom.close@cantab.net>
2018-05-23 14:06:25 -0700

    Update CountingGame

--- test/CountingGame.js
+++ test/CountingGame.js
@@ -1,62 +1,51 @@
 import { pack as packState } from '../src/CountingGame';
 import assertRevert from './helpers/assertRevert';
 
-var IG = artifacts.require("./CountingGame.sol");
+import { Position as CountingPosition } from '../src/CountingGame';
+import { Channel, State } from '../src/CommonState';
+
+var CountingGame = artifacts.require("./CountingGame.sol");
 // enum names aren't supported in ABI, so have to use integers for time being
 const START = 0;
 const CONCLUDED = 1;
 
 contract('CountingGame', (accounts) => {
-  let game;
-  let start;
-  let firstMove;
-  let allowedConcluded;
-  let disallowedConcluded;
+  let game, state0, state1, stateBalChange;
 
   before(async () => {
-    game = await IG.deployed();
+    game = await CountingGame.deployed();
+    let channel = new Channel(game.address, 0, [accounts[0], accounts[1]]);
+    let startPosition = CountingPosition.initialPosition(5, 4);
 
-    let pack = (turnNum, stateType, aBal, bBal, count) => {
-      return packState(
-        game.address, 0, accounts[0], accounts[1],
-        turnNum, stateType, aBal, bBal, count
-      );
-    };
+    state0 = new State(channel, State.StateTypes.GAME, 0, startPosition);
+    state1 = state0.next(state0.position.next());
 
-    start = pack(0, START, 5, 4, 3);
-    firstMove = pack(1, START, 5, 4, 4);
-    allowedConcluded = pack(1, CONCLUDED, 5, 4, 4);
-    disallowedConcluded = pack(1, CONCLUDED, 5, 4, 2);
+    let posBalChange = new CountingPosition(4, 5, 1);
+    stateBalChange = new State(channel, State.StateTypes.GAME, 1, posBalChange);
   });
 
   // Transition fuction tests
   // ========================
 
   it("allows a move where the count increment", async () => {
-    var output = await game.validTransition.call(start, firstMove);
+    var output = await game.validTransition.call(state0.toHex(), state1.toHex());
     assert.equal(output, true);
   });
 
-  it("allows START -> CONCLUDED if totals match", async () => {
-    var output = await game.validTransition.call(start, allowedConcluded);
-    assert.equal(output, true);
-  });
+  // it("allows START -> CONCLUDED if totals match", async () => {
+  //   var output = await game.validTransition.call(start, allowedConcluded);
+  //   assert.equal(output, true);
+  // });
 
-  it("doesn't allow START -> CONCLUDED if totals don't match", async () => {
-    await assertRevert(game.validTransition.call(start, disallowedConcluded));
+  it("doesn't allow transitions if totals don't match", async () => {
+    await assertRevert(game.validTransition.call(state0.toHex(), stateBalChange.toHex()));
   });
 
   // Resolution function tests
   // =========================
 
-  it("resolves the START correctly", async () => {
-    let [aBal, bBal] = await game.resolve.call(start);
-    assert.equal(aBal, 5);
-    assert.equal(bBal, 4);
-  });
-
-  it("resolves the CONCLUDED correctly", async () => {
-    let [aBal, bBal] = await game.resolve.call(allowedConcluded);
+  it("resolves states correctly", async () => {
+    let [aBal, bBal] = await game.resolve.call(state0.toHex());
     assert.equal(aBal, 5);
     assert.equal(bBal, 4);
   });
