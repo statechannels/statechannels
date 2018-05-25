1542df0bdbf94f2b7de701c1f69f197b331ede14
andrew_hunt <andrew.hunt@airbnb.com>
2018-05-24 21:42:45 -0700

    update readme adjust 2

--- packages/rps/packages/rps-poc/src/index.js
+++ packages/rps/packages/rps-poc/src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
