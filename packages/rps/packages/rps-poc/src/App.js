c77b780552f51d32949bba22c5382867c6a0bced
Andrew Hunt <andrew@hunt.codes>
2018-05-24 21:13:29 -0700

    Merge pull request #1 from magmo/andrew-responsive
    add opponent selection page, some styling

--- packages/rps/packages/rps-poc/src/App.js
+++ packages/rps/packages/rps-poc/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import HowItWorksPage from './components/HowItWorksPage';
+import OpponentSelectionPage from './components/OpponentSelectionPage';
 import AboutPage from './components/AboutPage';
 import { ROUTE_PATHS } from './constants';
 
@@ -18,6 +19,7 @@ class App extends React.Component {
           </header>
           <Route exact path="/" component={HomePage} />
           <Route path={`/${ROUTE_PATHS.HOW_IT_WORKS}`} component={HowItWorksPage} />
+          <Route path={`/${ROUTE_PATHS.OPPONENT_SELECTION}`} component={OpponentSelectionPage} />
           <Route path={`/${ROUTE_PATHS.ABOUT}`} component={AboutPage} />
         </div>
       </BrowserRouter>
--- packages/rps/packages/rps-poc/src/components/HomePage.jsx
+++ packages/rps/packages/rps-poc/src/components/HomePage.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 
 import Button from './Button';
+import { ROUTE_PATHS } from '../constants';
 
 export default function HomePage() {
-	return (
-		<div style={{ maxWidth: '90%', margin: 'auto' }}>
-			<div style={{ textAlign: 'center' }}>
-				<h1>Rock, Paper, Scissors</h1>
-			</div>
-			<div>
-				<div style={{ display: 'inline-block', maxWidth: '33%' }}>
-					<img alt="rock" width="100%" src="https://i.imgur.com/4JnbeAm.png" />
-				</div>
-				<div style={{ display: 'inline-block', maxWidth: '33%' }}>
-					<img
-						alt="paper"
-						width="100%"
-						src="https://images.vexels.com/media/users/3/128601/isolated/preview/adea4e2cdcac05cab39a471f7cd7178d-red-rectangular-origami-banner-by-vexels.png"
-					/>
-				</div>
-				<div style={{ display: 'inline-block', maxWidth: '33%' }}>
-					<img
-						alt="scissors"
-						width="100%"
-						src="http://www.pngmart.com/files/1/Scissors-PNG-Free-Download.png"
-					/>
-				</div>
-			</div>
-			<div style={{ textAlign: 'center', paddingTop: 40 }}>
-				<Button href="/how">Begin</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div style={{ maxWidth: '90%', margin: 'auto' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Rock, Paper, Scissors</h1>
+      </div>
+      <div>
+        <div style={{ display: 'inline-block', maxWidth: '33%' }}>
+          <img alt="rock" width="100%" src="https://i.imgur.com/4JnbeAm.png" />
+        </div>
+        <div style={{ display: 'inline-block', maxWidth: '33%' }}>
+          <img
+            alt="paper"
+            width="100%"
+            src="https://images.vexels.com/media/users/3/128601/isolated/preview/adea4e2cdcac05cab39a471f7cd7178d-red-rectangular-origami-banner-by-vexels.png"
+          />
+        </div>
+        <div style={{ display: 'inline-block', maxWidth: '33%' }}>
+          <img
+            alt="scissors"
+            width="100%"
+            src="http://www.pngmart.com/files/1/Scissors-PNG-Free-Download.png"
+          />
+        </div>
+      </div>
+      <div style={{ textAlign: 'center', paddingTop: 40 }}>
+        <Button href={ROUTE_PATHS.HOW_IT_WORKS}>Begin</Button>
+      </div>
+    </div>
+  );
 }
