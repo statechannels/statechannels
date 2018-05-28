b2d1f9cf792cb0b3e9d4ae8a00770f7ccb58aebb
Tom Close <tom.close@cantab.net>
2018-05-27 22:19:46 -0700

    Four spaces good, two spaces bad.

--- contracts/CountingGame.sol
+++ contracts/CountingGame.sol
@@ -1,35 +1,35 @@
 pragma solidity ^0.4.18;
 
-import './CommonState.sol';
-import './CountingState.sol';
+import "./CommonState.sol";
+import "./CountingState.sol";
 
 contract CountingGame {
-  using CommonState for bytes;
-  using CountingState for bytes;
+    using CommonState for bytes;
+    using CountingState for bytes;
 
-  // The following transitions are allowed:
-  //
-  // Start -> Concluded
-  //
-  function validTransition(bytes _old, bytes _new) public pure returns (bool) {
-    // regardless of whether we move to a Start or Concluded state, we must have:
-    // 1. balances remain the same
-    // 2. count must increase
-    require(_new.aBal() == _old.aBal());
-    require(_new.bBal() == _old.bBal());
-    require(_new.count() == _old.count() + 1);
+    // The following transitions are allowed:
+    //
+    // Start -> Concluded
+    //
+    function validTransition(bytes _old, bytes _new) public pure returns (bool) {
+        // regardless of whether we move to a Start or Concluded state, we must have:
+        // 1. balances remain the same
+        // 2. count must increase
+        require(_new.aBal() == _old.aBal());
+        require(_new.bBal() == _old.bBal());
+        require(_new.count() == _old.count() + 1);
 
-    return true;
+        return true;
   }
 
-  // in this case the resolution function is pure, but it doesn't have to be in general
-  function resolve(bytes _state) public pure returns (uint aBal, uint bBal) {
-    aBal = _state.aBal();
-    bBal = _state.bBal();
-  }
+    // in this case the resolution function is pure, but it doesn't have to be in general
+    function resolve(bytes _state) public pure returns (uint aBal, uint bBal) {
+        aBal = _state.aBal();
+        bBal = _state.bBal();
+    }
 
-  // TODO: remove
-  function isConcluded(bytes _state) pure public returns(bool) {
-    return false;
-  }
+    // TODO: remove
+    function isConcluded(bytes _state) pure public returns(bool) {
+        return false;
+    }
 }
