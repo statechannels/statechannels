a9088f78a21202f4d172efb7ac3c84876cda490c
Tom Close <tom.close@cantab.net>
2018-06-06 16:26:41 -0700

    Tidy up rps-state test

--- packages/fmg-rock-paper-scissors/test/rps-state.js
+++ packages/fmg-rock-paper-scissors/test/rps-state.js
@@ -27,15 +27,15 @@ contract('RockPaperScissorsPositions', (accounts) => {
     bPlay,
     salt,
   });
-  // let recovered = Position.fromHex(original.toHex());
+  
   let stateContract;
 
   before( async () => {
     stateContract = await RpsStateContract.deployed();
   });
 
-  xit("can parse positionType", async () => {
-    // assert.equal(recovered.positionType, original.positionType);
+  // skipped because web3 can't cope with the positionType object that is returned
+  it.skip("can parse positionType", async () => {
     assert.equal(await stateContract.positionType(original.toHex()), original.positionType);
   });
 
@@ -44,66 +44,29 @@ contract('RockPaperScissorsPositions', (accounts) => {
   });
 
   it("can parse bBal", async () => {
-    // assert.equal(recovered.bBal, original.bBal);
     assert.equal(await stateContract.bResolution(original.toHex()), resolution[1]);
   });
 
   it("can parse stake", async () => {
-    // assert.equal(recovered.stake, original.stake);
     assert.equal(await stateContract.stake(original.toHex()), stake);
   });
 
   it("can parse preCommit", async () => {
-    // assert.equal(recovered.preCommit, original.preCommit);
     assert.equal(await stateContract.preCommit(original.toHex()), preCommit);
   });
 
-  xit("can parse bPlay", async () => {
-    // assert.equal(recovered.bPlay, original.bPlay);
+  // skipped because web3 can't cope with the Play object that is returned
+  it.skip("can parse bPlay", async () => {
     assert.equal(await stateContract.bPlay.call(original.toHex()), bPlay);
   });
 
-  xit("can parse aPlay", async () => {
-    // assert.equal(recovered.aPlay, original.aPlay);
+  // skipped because web3 can't cope with the Play object that is returned
+  it.skip("can parse aPlay", async () => {
     assert.equal(await stateContract.aPlay.call(original.toHex()), aPlay);
   });
 
   it("can parse salt", async () => {
-    // assert.equal(recovered.salt, original.salt);
     assert.equal(await stateContract.salt(original.toHex()), salt);
   });
-
-
-  // State transitions
-
-  it.skip("can do state transitions", () => {
-    let resting = Position.initialPosition(4, 5);
-
-    // 1. a proposes a round
-    let propose = resting.propose(2, Position.Plays.ROCK, "0xabcdef");
-
-    // 2. b accepts
-    let accept = propose.accept(Position.Plays.PAPER);
-
-    // 3. a reveals
-    let reveal = accept.reveal(Position.Plays.ROCK, "0xabcdef");
-
-    // 4. b confirms
-    let resting2 = reveal.confirm();
-
-    // outcome
-    assert.equal(resting2.aBal, 2); // a lost their stake
-    assert.equal(resting2.bBal, 7); // b won it
-
-    // alternative history
-    // 2(b) b rejects
-    let resting2b = propose.reject();
-
-    // outcome
-    assert.equal(resting2b.aBal, 4); // same as in the beginning
-    assert.equal(resting2b.bBal, 5); // same as in the beginning
-  });
-
-
 
 });
