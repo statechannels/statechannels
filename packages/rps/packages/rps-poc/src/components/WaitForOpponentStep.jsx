5ae95a2e57f93b484f0c69a854dfe8358a73ecf7
Andrew Hunt <andrew@hunt.codes>
2018-06-07 18:13:23 -0700

    Merge pull request #3 from magmo/andrew-firebase
    add firebase sdk and two game steps

---
+++ packages/rps/packages/rps-poc/src/components/WaitForOpponentStep.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { MOVE_OPTIONS } from '../constants';
+
+export default class WaitForOpponent extends React.PureComponent {
+  render() {
+    const { moveId } = this.props;
+
+    return (
+      <div style={{ maxWidth: '90%', margin: 'auto' }}>
+        <div>
+          <h1>Waiting for opponent...</h1>
+        </div>
+        <div style={{ width: '100%' }}>
+          You've chosen {MOVE_OPTIONS.find(option => option.id === moveId).name}
+        </div>
+      </div>
+    );
+  }
+}
