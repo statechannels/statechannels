c7bcae5e9235b8a9a723ce0dd59dd452c1e60805
Andrew Hunt <andrew@hunt.codes>
2018-06-07 18:13:23 -0700

    Merge pull request #3 from magmo/andrew-firebase
    add firebase sdk and two game steps

--- packages/rps/packages/rps-poc/src/components/PlayPage.jsx
+++ packages/rps/packages/rps-poc/src/components/PlayPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import OpponentSelectionStep from './OpponentSelectionStep';
+import SelectMoveStep from './SelectMoveStep';
+import WaitForOpponentStep from './WaitForOpponentStep';
 import { GAME_STAGES } from '../constants';
 
 // TODO: Get from Firebase
@@ -36,34 +38,54 @@ export default class PlayPage extends React.PureComponent {
     super(props);
 
     this.state = {
-      stage: GAME_STAGES.SELECT_CHALLENGER,
+      stage: GAME_STAGES.SELECT_MOVE,
+      selectedMove: null,
     };
 
     this.selectChallenge = this.selectChallenge.bind(this);
     this.createChallenge = this.createChallenge.bind(this);
+    this.selectMove = this.selectMove.bind(this);
   }
 
   selectChallenge() {
     console.log('TODO (select): DO SOMETHING HERE');
+    this.setState({ stage: GAME_STAGES.SELECT_MOVE });
   }
 
   createChallenge() {
     console.log('TODO (create): DO SOMETHING HERE');
   }
 
+  selectMove(id) {
+    this.setState({ stage: GAME_STAGES.WAIT_FOR_OPPONENT_MOVE, moveId: id });
+  }
+
   render() {
-    const { stage } = this.state;
+    const { stage, moveId } = this.state;
 
-    if (stage === GAME_STAGES.SELECT_CHALLENGER) {
-      return (
-        <OpponentSelectionStep
-          handleSelectChallenge={this.selectChallenge}
-          handleCreateChallenge={this.createChallenge}
-          opponents={opponents}
-        />
-      );
+    switch (stage) {
+      case GAME_STAGES.SELECT_CHALLENGER:
+        return (
+          <OpponentSelectionStep
+            handleSelectChallenge={this.selectChallenge}
+            handleCreateChallenge={this.createChallenge}
+            opponents={opponents}
+          />
+        );
+      case GAME_STAGES.SELECT_MOVE:
+        return (
+          <SelectMoveStep
+            handleSelectMove={this.selectMove}
+          />
+        );
+      case GAME_STAGES.WAIT_FOR_OPPONENT_MOVE:
+        return (
+          <WaitForOpponentStep
+            moveId={moveId}
+          />
+        );
+      default:
+        return null;
     }
-
-    return null;
   }
 }
