581bdfa2dc60fc40f25fa03aa4d6bafb4eda7a09
Martin Muñoz <im.mmun@gmail.com>
2018-06-22 16:29:05 -0400

    Remove babel

--- new-packages/nitro-protocol/packages/fmg-rock-paper-scissors/truffle.js
+++ new-packages/nitro-protocol/packages/fmg-rock-paper-scissors/truffle.js
@@ -1,6 +1,3 @@
-require('babel-register');
-require("babel-polyfill");
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
