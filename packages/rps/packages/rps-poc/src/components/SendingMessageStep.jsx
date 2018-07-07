9ac57f80620f1ea0481437e0c474014e49ac2919
tomclose <tom.close@cantab.net>
2018-07-07 13:59:50 -0700

    Merge pull request #17 from magmo/amh-add-game-engine
    add game engine and a few other updates

---
+++ packages/rps/packages/rps-poc/src/components/SendingMessageStep.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class WaitForOpponent extends React.PureComponent {
+  render() {
+    return (
+      <div style={{ maxWidth: '90%', margin: 'auto' }}>
+        <div>
+          <h1>Sending message to opponent...</h1>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ packages/rps/packages/rps-poc/src/game-engine/GameEngine.js
@@ -0,0 +1,68 @@
+import { GE_STAGES, GE_COMMANDS } from '../constants';
+
+export default class GameEngine {
+  constructor(props) {
+    this.state = {
+      selectedPlayId: null,
+      opponentMoveId: null,
+      opponentId: null,
+      stage: null,
+      stake: null,
+    };
+  }
+
+  init() {
+    this.state.stage = GE_STAGES.SELECT_CHALLENGER;
+
+    const updateObj = {
+      stage: this.state.stage,
+    };
+
+    return {
+      updateObj,
+    };
+  }
+
+  selectChallenge({ stake, opponentId }) {
+    this.state.stage = GE_STAGES.READY_TO_SEND_PREFUND;
+    this.state.stake = stake;
+    this.state.opponentId = opponentId;
+
+    const updateObj = {
+      stage: this.state.stage,
+    };
+
+    return {
+      updateObj,
+      command: GE_COMMANDS.SEND_PRE_FUND_MESSAGE,
+    };
+  }
+
+  preFundProposalSent() {
+    this.state.stage = GE_STAGES.PREFUND_SENT;
+
+    const updateObj = {
+      stage: this.state.stage,
+    };
+
+    return {
+      updateObj,
+    };
+  }
+
+  returnToOpponentSelection() {
+    this.state.stage = GE_STAGES.SELECT_CHALLENGER;
+
+    const updateObj = {
+      stage: this.state.stage,
+    };
+
+    // TODO: should we clear all other competitor info so that the state isn't dirty with the
+    // previous opponent's data?
+    return {
+      updateObj,
+    };
+  }
+}
+
+
