b63938e25609ae7ae726dd90c3f91e2a9e667220
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-07-13 15:47:49 -0600

    Channel wallet signs messages

--- packages/wallet/packages/rps-poc/src/game-engine/ChannelWallet.js
+++ packages/wallet/packages/rps-poc/src/game-engine/ChannelWallet.js
@@ -1,5 +1,18 @@
+import { Message } from './Message';
+import Web3 from 'web3';
+
 export default class ChannelWallet {
-    constructor({ address }) {
-        this.address = address;
+    constructor() {
+        let web3 = new Web3('');
+        this.account = web3.eth.accounts.create();
+    }
+
+    get address() {
+        return this.account.address;
+    }
+
+    sign(state) {
+        let signature = this.account.sign(state).signature;
+        return new Message({ state, signature });
     }
 }
