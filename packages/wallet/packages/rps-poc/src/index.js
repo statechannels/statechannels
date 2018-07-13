7ff4ddc24147d230ef45e46ee8a2686e8781ffed
Tom Close <tom.close@cantab.net>
2018-07-13 12:13:49 -0700

    Create store and Provider

--- packages/wallet/packages/rps-poc/src/index.js
+++ packages/wallet/packages/rps-poc/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
