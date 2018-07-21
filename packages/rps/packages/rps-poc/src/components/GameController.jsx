9710ecf9e1499ac563d11208d88bc03393b74c03
Tom Close <tom.close@cantab.net>
2018-07-21 16:25:31 +0200

    Add in the ReadyToSend* states. Fixes #45.

--- packages/rps/packages/rps-poc/src/components/GameController.jsx
+++ packages/rps/packages/rps-poc/src/components/GameController.jsx
@@ -15,8 +15,14 @@ export default class GameController extends PureComponent {
     } = this.props;
 
     switch (applicationState && applicationState.type) {
+      case playerAStates.ReadyToSendPreFundSetup0:
+        return <WaitingStep message="ready to propose game" />;
+
       case playerAStates.WaitForPreFundSetup1:
         return <WaitingStep message="opponent to accept game" />;
+
+      case playerAStates.ReadyToDeploy:
+        return <WaitingStep message="ready to deploy adjudicator" />;
 
       case playerAStates.WaitForBlockchainDeploy:
         return <WaitingStep message="confirmation of adjudicator deployment" />;
@@ -24,14 +30,23 @@ export default class GameController extends PureComponent {
       case playerAStates.WaitForBToDeposit:
         return <WaitingStep message="confirmation of opponent's deposit" />;
 
+      case playerAStates.ReadyToSendPostFundSetup0:
+        return <WaitingStep message="ready to send deposit confirmation" />;
+
       case playerAStates.WaitForPostFundSetup1:
         return <WaitingStep message="opponent to confirm deposits" />;
 
       case playerAStates.ReadyToChooseAPlay:
         return <SelectPlayStep handleSelectPlay={chooseAPlay} />;
 
+      case playerAStates.ReadyToSendPropose:
+        return <WaitingStep message="ready to send round proposal" />;
+
       case playerAStates.WaitForAccept:
         return <WaitingStep message="opponent to choose their move" />;
+
+      case playerAStates.ReadyToSendReveal:
+        return <WaitingStep message="ready to send reveal" />;
 
       case playerAStates.WaitForResting:
         return <WaitingStep message="opponent to accept the outcome" />;
