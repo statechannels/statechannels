14ce49c100411f5c49b68b08de5d9a3b6f6b0166
Tom Close <tom.close@cantab.net>
2018-07-27 14:12:54 -0700

    Refactor login/wallet so that logged in => has wallet

--- packages/rps/packages/rps-poc/src/redux/reducers/index.js
+++ packages/rps/packages/rps-poc/src/redux/reducers/index.js
@@ -3,11 +3,9 @@ import { combineReducers } from 'redux';
 import game from './game';
 import opponents from './opponents';
 import login from './login';
-import wallet from './wallet';
 
 export default combineReducers({
   game,
   opponents,
   login,
-  wallet,
 });
