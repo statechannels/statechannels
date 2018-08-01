fdb21d95653fcfc35637b385669c345a3ef852c4
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-08-01 14:12:20 -0230

    build.sh copies contracts over

--- packages/fmg-core/bin/build.sh
+++ packages/fmg-core/bin/build.sh
@@ -2,4 +2,11 @@ rm -rf built
 
 tsc
 
-find contracts test -name '*.sol' -exec cp "{}" "built/{}" \;
+cp_with_dir () {
+    mkdir -p "built/$(dirname $1)"
+    cp "$1" "built/$1";
+}
+
+export -f cp_with_dir
+
+find contracts test -name '*.sol' -exec bash -c "cp_with_dir {}" \;
