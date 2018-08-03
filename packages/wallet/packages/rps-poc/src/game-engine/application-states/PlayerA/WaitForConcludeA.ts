fdcffa06956bbd69752686c5228a404bbdb63306
Tom Close <tom.close@cantab.net>
2018-08-03 15:05:50 -0700

    PlayerA application state tests passing

--- packages/wallet/packages/rps-poc/src/game-engine/application-states/PlayerA/WaitForConcludeA.ts
+++ packages/wallet/packages/rps-poc/src/game-engine/application-states/PlayerA/WaitForConcludeA.ts
@@ -1,11 +1,13 @@
 import BasePlayerA from './Base';
+import Message from '../../Message';
 
 export default class WaitForConcludeA extends BasePlayerA {
   adjudicator: any;
   message: Message;
 
-  constructor(channel, balances, adjudicator, message) {
-    super(channel, balances, adjudicator);
+  constructor({ channel, balances, adjudicator, message }) {
+    const stake = 0;
+    super({ channel, balances, stake });
     this.adjudicator = adjudicator;
     this.message = message;
   }
--- packages/wallet/packages/rps-poc/src/game-engine/application-states/PlayerA/WaitForResting.ts
+++ packages/wallet/packages/rps-poc/src/game-engine/application-states/PlayerA/WaitForResting.ts
@@ -10,8 +10,8 @@ export default class WaitForResting extends BasePlayerA {
   salt: string;
   adjudicator: string;
 
-  constructor(channel, stake, balances, adjudicator, aPlay, bPlay, result, salt, message) {
-    super(channel, stake, balances);
+  constructor({ channel, stake, balances, adjudicator, aPlay, bPlay, result, salt, message }) {
+    super({ channel, stake, balances });
     this.message = message;
     this.aPlay = aPlay;
     this.bPlay = bPlay;
