fe7ff1cc0b174889784be08196b6699011ef43ea
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-08-08 13:36:16 -0600

    ActionCreatorsMapObject is an interface

--- packages/wallet/packages/rps-poc/src/redux/actions/type-helpers.ts
+++ packages/wallet/packages/rps-poc/src/redux/actions/type-helpers.ts
@@ -1,6 +1,7 @@
 // ref: https://medium.com/@martin_hotell/improved-redux-type-safety-with-typescript-2-8-2c11a8062575
 
 type FunctionType = (...args: any[]) => any;
-type ActionCreatorsMapObject = { [actionCreator: string]: FunctionType };
+interface ActionCreatorsMapObject { [actionCreator: string]: FunctionType };
+
 
 export type ActionsUnion<A extends ActionCreatorsMapObject> = ReturnType<A[keyof A]>;
