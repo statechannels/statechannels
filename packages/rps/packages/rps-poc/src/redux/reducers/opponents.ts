5076c7de3bc1f3588c12bb5eb852b79843273349
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-08-08 13:33:37 -0600

    Reducers work again

--- packages/rps/packages/rps-poc/src/redux/reducers/opponents.ts
+++ packages/rps/packages/rps-poc/src/redux/reducers/opponents.ts
@@ -1,11 +1,15 @@
+import { Reducer } from 'redux';
 import { OpponentActionType, OpponentAction } from '../actions/opponents';
 
-const initialState = [];
+export interface OpponentState {
+  opponents: string[];
+}
+const initialState = {opponents: []};
 
-export default function reducer (state = initialState, action: OpponentAction) {
+export const opponentReducer: Reducer<OpponentState> = (state = initialState, action: OpponentAction) => {
   switch (action.type) {
     case OpponentActionType.SYNC:
-      return action.opponents;
+      return { opponents: action.opponents };
     default:
       return state
   }
