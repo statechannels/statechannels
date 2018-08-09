ae74cccd23516bc820016435fab212736becc548
Tom Close <tom.close@cantab.net>
2018-08-08 21:50:57 -0700

    Remove lib dir

--- new-packages/nitro-protocol/packages/fmg-core/lib/contracts/ForceMoveGame.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.23;
-
-interface ForceMoveGame {
-  function validTransition(bytes oldState, bytes newState) external pure returns (bool);
-}
--- new-packages/nitro-protocol/packages/fmg-core/lib/test/helpers/assert-revert.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (promise: any) => Promise<void>;
-export default _default;
