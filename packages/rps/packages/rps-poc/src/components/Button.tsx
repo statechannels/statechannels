31bf7762c1bbdaedaf597aa94ecb1a9c0a161961
Andrew Hunt <andrew@hunt.codes>
2018-08-09 22:00:55 -0700

    Merge pull request #72 from magmo/amh-aphrodite
    [aphrodite] convert a single component to aphrodite

--- packages/rps/packages/rps-poc/src/components/Button.tsx
+++ packages/rps/packages/rps-poc/src/components/Button.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+import { BRAND_COLOR } from '../constants';
 
 interface IProps {
   children: any;
@@ -7,10 +10,30 @@ interface IProps {
 
 const Button: React.SFC<IProps> = ({ children, onClick }) => {
   return (
-    <button onClick={onClick} type="button" className="button">
+    <button onClick={onClick} type="button" className={css(styles.button)}>
       {children}
     </button>
   );
 };
 
 export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 3,
+    padding: '3px 16px 4px',
+    backgroundColor: BRAND_COLOR,
+    borderColor: '#666',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    color: '#fff',
+    transition: 'background-color 0.5s ease',
+    boxShadow: '0px 0px 1px',
+    textTransform: 'uppercase',
+  },
+
+  ':hover': {
+    textDecorationLine: 'none',
+    backgroundColor: '#aaa',
+  },
+});
