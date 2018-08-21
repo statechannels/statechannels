f4b656e1babd01211bb40ef7840bc5e956fb3638
tomclose <tom.close@cantab.net>
2018-08-21 14:01:14 -0700

    Merge pull request #90 from magmo/exclude-current-user-from-opponents
    Exclude current user from list of opponents

--- packages/rps/packages/rps-poc/src/containers/GameContainer.ts
+++ packages/rps/packages/rps-poc/src/containers/GameContainer.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { connect } from 'react-redux';
 
 import { GameAction } from '../redux/actions/game';
@@ -5,10 +6,17 @@ import { OpponentAction } from '../redux/actions/opponents';
 import GameController from '../components/GameController';
 import { ApplicationState } from '../redux/reducers';
 
-const mapStateToProps = (state: ApplicationState) => ({
-  applicationState: state.game,
-  opponents: state.opponents,
-});
+const mapStateToProps = (state: ApplicationState) => {
+	return {
+	  applicationState: state.game,
+	  opponents: getOpponents(),
+	}
+
+	function getOpponents() {
+		const currentUserAddress = _.get(state, 'login.wallet.account.address');
+		return _.filter(state.opponents, o => o.address !== currentUserAddress);
+	}
+}
 
 const mapDispatchToProps = {
   choosePlay: GameAction.choosePlay,
