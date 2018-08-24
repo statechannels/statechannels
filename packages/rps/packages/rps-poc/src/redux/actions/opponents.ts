5fa999f46ed2d3074fa153c69a433809e20045cc
Neil Whitehead <neil@gladly.com>
2018-08-24 14:04:01 -0600

    Users can create challenges (#102)

--- packages/rps/packages/rps-poc/src/redux/actions/opponents.ts
+++ packages/rps/packages/rps-poc/src/redux/actions/opponents.ts
@@ -1,6 +1,7 @@
 import { Opponent } from '../reducers/opponents';
 
 export enum OpponentActionType {
+  CREATE_CHALLENGE = 'OPPONENTS.CREATE_CHALLENGE',
   SYNC = 'OPPONENTS.SYNC',
   SUBSCRIBE = 'OPPONENTS.SUBSCRIBE',
   UNSUBSCRIBE = 'OPPONENTS.UNSUBSCRIBE',
@@ -19,11 +20,17 @@ export const OpponentAction = {
   unsubscribeOpponents: () => ({
     type: OpponentActionType.UNSUBSCRIBE as typeof OpponentActionType.UNSUBSCRIBE,
   }),
+
+  createChallenge: (challenge: any) => ({
+    type: OpponentActionType.CREATE_CHALLENGE as typeof OpponentActionType.CREATE_CHALLENGE,
+    challenge,
+  }),
 }
 
 export type SyncOpponentsAction = ReturnType<typeof OpponentAction.syncOpponents>;
 export type SubscribeOpponentsAction = ReturnType<typeof OpponentAction.subscribeOpponents>;
 export type UnsubscribeOpponentsAction = ReturnType<typeof OpponentAction.unsubscribeOpponents>;
+export type CreateChallenge = ReturnType<typeof OpponentAction.createChallenge>;
 
-export type OpponentAction = SyncOpponentsAction | SubscribeOpponentsAction | UnsubscribeOpponentsAction;
+export type OpponentAction = SyncOpponentsAction | SubscribeOpponentsAction | UnsubscribeOpponentsAction | CreateChallenge;
 
