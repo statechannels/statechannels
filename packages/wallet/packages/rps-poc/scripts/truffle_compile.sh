7f8be30eec3f7de0193259cee435c7e25eca0f71
Alex Gap <alex.gap@gmail.com>
2018-08-29 16:18:48 -0700

    Merge pull request #111 from magmo/ag-fix-truffle
    Update src/contract JSON files after migrate is run

--- packages/wallet/packages/rps-poc/scripts/truffle_compile.sh
+++ packages/wallet/packages/rps-poc/scripts/truffle_compile.sh
@@ -1,10 +1,43 @@
+#!/usr/bin/env bash
+
+# TODO: This would probably be more maintanable in a node script instead.
+while getopts "cm" opt; do
+  case $opt in
+    c)
+      clean=true
+      ;;
+    m)
+      migrate=true
+      ;;
+  esac
+done
+
 # Create our working directory
-mkdir -p tmp
+mkdir -p tmp;
 # Copy over truffle files we'll need
-copyfiles './contracts/**/*' './migrations/**/*' '.env' ./tmp
+copyfiles './contracts/**/*' './migrations/**/*' '.env' ./tmp;
 # Build truffle.js
-tsc -p tsconfig.truffle.json
+tsc -p tsconfig.truffle.json;
+
+cd tmp;
+
 # Compile truffle JSON files
-cd tmp && ../node_modules/.bin/truffle compile
+../node_modules/.bin/truffle compile;
+
+if [ "$migrate" = true ];
+then 
+    echo "Calling truffle migrate";
+    ../node_modules/.bin/truffle migrate --network development;
+fi
+
+echo "Updating JSON artifacts in src folder";
 # Copy the JSON files to the src folder
-copyfiles -f build/contracts/*.json ../src/contracts
+copyfiles -f build/contracts/*.json ../src/contracts;
+
+cd ../
+
+if [ "$clean" = true ] 
+then 
+    echo "Cleaning tmp directory"
+    rm -rf tmp
+fi
