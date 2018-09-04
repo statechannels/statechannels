431946fb716f57f5ad3165aa262c4307e52ad1e9
Alex Gap <alex.gap@gmail.com>
2018-09-04 14:01:09 -0700

    Merge pull request #124 from magmo/ag-deposit-adjudicator
    Clean up blockchain actions and add deposit action

--- packages/rps/packages/rps-poc/src/wallet/redux/sagas/blockchain.ts
+++ packages/rps/packages/rps-poc/src/wallet/redux/sagas/blockchain.ts
@@ -6,29 +6,43 @@ import contract from 'truffle-contract';
 import detectNetwork from 'web3-detect-network';
 // The blockchain saga will be responsible for dealing with the blockchain using truffle
 export function* blockchainSaga() {
-  const channel = yield actionChannel(blockchainActions.BLOCKCHAIN_DEPLOYADJUDICATOR);
+  const channel = yield actionChannel(
+    a =>
+      a.type === blockchainActions.DEPLOY_REQUEST ||
+      a.type === blockchainActions.DEPOSIT_REQUEST,
+  );
   while (true) {
     const action = yield take(channel);
-    switch (action.type) {
-      case blockchainActions.BLOCKCHAIN_DEPLOYADJUDICATOR:
-        const simpleAdjudicatorContract = contract(simpleAdjudicatorArtifact);
+    const network = yield call(detectNetwork, web3.currentProvider);
+    const simpleAdjudicatorContract = contract(simpleAdjudicatorArtifact);
+    yield call (simpleAdjudicatorContract.defaults,{from:web3.eth.defaultAccount})
+    if (!Object.keys(simpleAdjudicatorContract.networks).find(id => id === network.id)) {
+      yield put(blockchainActions.wrongNetwork(network.id));
+      continue;
+    }
 
-        const network = yield call(detectNetwork, web3.currentProvider);
-        if (!Object.keys(simpleAdjudicatorContract.networks).find(id => id === network.id)) {
-          yield put(blockchainActions.deploymentWrongNetworkFailure(network.id));
-          break;
-        }
-        simpleAdjudicatorContract.setProvider(web3.currentProvider);
-        simpleAdjudicatorContract.setNetwork(network.id);
+    simpleAdjudicatorContract.setProvider(web3.currentProvider);
+    simpleAdjudicatorContract.setNetwork(network.id);
+
+    switch (action.type) {
+      case blockchainActions.DEPLOY_REQUEST:
         try {
-          const deployedContract = yield simpleAdjudicatorContract.new([action.channelId], {
-            from: web3.eth.defaultAccount,
-          });
+          const deployedContract = yield call(simpleAdjudicatorContract.new, [action.channelId]);
           yield put(blockchainActions.deploymentSuccess(deployedContract.address));
         } catch (err) {
-          yield put(blockchainActions.deploymentMetamaskFailure(err));
+          yield put(blockchainActions.deploymentFailure(err));
         }
         break;
+      case blockchainActions.DEPOSIT_REQUEST:
+        try {
+          const existingContract = yield call(simpleAdjudicatorContract.at, action.address);
+          // TODO: Use correct amount
+          const transaction = yield call(existingContract.send, web3.toWei(action.amount, "ether"));
+          yield put(blockchainActions.depositSuccess(transaction));
+          break;
+        } catch (err) {
+          yield put(blockchainActions.depositFailure(err));
+        }
     }
   }
 }
