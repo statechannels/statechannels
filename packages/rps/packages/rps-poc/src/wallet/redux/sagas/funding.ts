9969e88c0dc1328eacdd8ffdd99238437ebaa911
Alex Gap <alex.gap@gmail.com>
2018-09-04 09:02:06 -0700

    Merge pull request #123 from magmo/ag-deploy-adjudicator
    Add simple adjudicator deployment

--- packages/rps/packages/rps-poc/src/wallet/redux/sagas/funding.ts
+++ packages/rps/packages/rps-poc/src/wallet/redux/sagas/funding.ts
@@ -1,4 +1,8 @@
+import * as blockchainActions from '../actions/blockchain';
+import { put } from 'redux-saga/effects';
+
 export function* fundingSaga(channelId: string) {
+  
   // todo:
   // - check that we already have both prefundsetup states for that channel
   // - determine the player and the amounts from the prefund setup states
@@ -6,6 +10,7 @@ export function* fundingSaga(channelId: string) {
   // - wait for user's response
   // - if player a
   //   - send transaction to blockchain
+  yield put(blockchainActions.deploymentRequest(channelId));
   //   - update state to display "waiting for deploy"
   //   - wait for confirmation
   //   - send adjudicator address to opponent
@@ -13,8 +18,7 @@ export function* fundingSaga(channelId: string) {
   //   - wait for opponent to deposit / blockchain confirmation
   //   - update state to display the success screen
   //   - wait for user to click "return-to-app"
-
-
+  
 
   return true;
 }
