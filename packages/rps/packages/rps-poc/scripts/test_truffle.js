b3b0e68c8da17c54762c7b48126fcf343555c9af
Alex Gap <alex.gap@gmail.com>
2018-09-05 10:29:05 -0700

    Merge pull request #128 from magmo/ag-fix-truffle-test
    Update test script to fail on error and remove obsolete .gitingore entry

--- packages/rps/packages/rps-poc/scripts/test_truffle.js
+++ packages/rps/packages/rps-poc/scripts/test_truffle.js
@@ -2,7 +2,8 @@
 
 //TODO: Add more functionality
 var ganache = require('ganache-cli');
-const { execFile, exec } = require('child_process');
+var path = require('path');
+const { exec } = require('child_process');
 
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
@@ -11,20 +12,26 @@ process.env.PUBLIC_URL = '';
 if (!process.env.DEV_GANACHE_PORT) {
   process.env.DEV_GANACHE_PORT = 5732;
 }
-console.log(`Using port ${process.env.DEV_GANACHE_PORT} for Ganache.`)
-
+console.log(`Using port ${process.env.DEV_GANACHE_PORT} for Ganache.`);
 try {
-  var ganache = require('ganache-cli');
   var server = ganache.server({ port: process.env.DEV_GANACHE_PORT });
   server.listen(process.env.DEV_GANACHE_PORT, function(err, blockchain) {
     if (err) {
       return console.log(err);
     }
   });
-
-  exec('truffle.cmd test --network ganache', (err, stdout, stderr) => {
-    // Errors seem to be piped to stdout so we just output that always
+  const trufflePath = path.resolve(__dirname, '../node_modules/.bin/truffle');
+  const truffleCommand = `${trufflePath} test --network ganache`;
+  exec(truffleCommand, (err, stdout, stderr) => {
     console.log(stdout);
+    if (stderr) {
+      console.log(stderr);
+      process.exit(1);
+    }
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
   });
 
   server.close();
