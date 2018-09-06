6a99f21576dca9c09be186743e3ace8bd4a0e52b
tomclose <tom.close@cantab.net>
2018-09-06 13:30:48 -0700

    Merge pull request #130 from magmo/ag-remove-drizzle
    Remove drizzle

--- packages/rps/packages/rps-poc/src/redux/reducers/index.ts
+++ packages/rps/packages/rps-poc/src/redux/reducers/index.ts
@@ -4,14 +4,12 @@ import { gameReducer, GameState } from './game';
 import { opponentReducer, OpponentState } from './opponents';
 import { loginReducer, LoginState } from './login';
 import { WalletState, walletStateReducer } from '../../wallet/redux/reducers/wallet-state';
-import { drizzleReducers } from 'drizzle';
 
 export interface ApplicationState {
   game: GameState;
   opponents: OpponentState;
   login: LoginState;
   wallet: WalletState;
-  contracts: any[];
 }
 
 export default combineReducers<ApplicationState>({
@@ -19,5 +17,4 @@ export default combineReducers<ApplicationState>({
   opponents: opponentReducer,
   login: loginReducer,
   wallet: walletStateReducer,
-  ...drizzleReducers,
 });
