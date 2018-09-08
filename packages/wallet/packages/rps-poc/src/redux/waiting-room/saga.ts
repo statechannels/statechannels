c68a9dd897594ae45c35ad8db869a8fb2732e366
tomclose <tom.close@cantab.net>
2018-09-08 12:04:21 -0700

    Merge pull request #147 from magmo/as-challenge-heartbeat
    Challenges expire after 5 seconds.

--- packages/wallet/packages/rps-poc/src/redux/waiting-room/saga.ts
+++ packages/wallet/packages/rps-poc/src/redux/waiting-room/saga.ts
@@ -1,16 +1,20 @@
-import { take, actionChannel, put, call } from 'redux-saga/effects'; 
-import { reduxSagaFirebase } from '../../gateways/firebase';
+import { take, actionChannel, put, call, fork } from 'redux-saga/effects'; 
+import { default as firebase, reduxSagaFirebase } from '../../gateways/firebase';
  
 import * as waitingRoomActions from '../waiting-room/actions';
 import * as messageActions from '../message-service/actions';
 import * as applicationActions from '../application/actions';
 import GameEngineB from '../../game-engine/GameEngineB';
 import decode from '../../game-engine/positions/decode';
+import { delay } from 'redux-saga';
 
 type ActionType = (
   | waitingRoomActions.CancelChallenge
   | messageActions.MessageReceived
 );
+
+const REFRESH_INTERVAL = 4000; // milliseconds
+const EXPIRATION_INTERVAL = 5000; // milliseconds
 
 export default function * waitingRoomSaga(address: string, name: string, stake: number, isPublic: boolean) {
 
@@ -25,11 +29,14 @@ export default function * waitingRoomSaga(address: string, name: string, stake: 
     stake,
     isPublic,
     lastSeen: new Date().getTime(),
+    expiresAt: new Date().getTime() + EXPIRATION_INTERVAL,
   }
 
   yield put(applicationActions.waitingRoomSuccess(challenge));
   // use update to allow us to pick our own key
   yield call(reduxSagaFirebase.database.update, `/challenges/${address}`, challenge);
+
+  yield fork(challengeHeartbeatSaga, challenge);
 
   while (true) {
     const action: ActionType = yield take(channel);
@@ -51,4 +58,21 @@ export default function * waitingRoomSaga(address: string, name: string, stake: 
   }
 }
 
+function * challengeHeartbeatSaga(challenge) {
+  while(true) {
+    yield call(delay, REFRESH_INTERVAL);
+    yield refreshChallenge(challenge);
+  }
+}
 
+const challengeRef = (challenge) => {
+  return firebase.database().ref(`challenges/${challenge.address}`);
+}
+
+function * refreshChallenge(challenge) {
+  const challengeParams = {
+    expiresAt: new Date().getTime() + EXPIRATION_INTERVAL,
+  }
+
+  return yield call(reduxSagaFirebase.database.patch, challengeRef(challenge), challengeParams);
+}
