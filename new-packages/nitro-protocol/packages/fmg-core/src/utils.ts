ca603f91f10232feea36e47b9b029cf8c39a5e62
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-10-03 10:21:11 -0700

    Add sign and recover to utils

--- new-packages/nitro-protocol/packages/fmg-core/src/utils.ts
+++ new-packages/nitro-protocol/packages/fmg-core/src/utils.ts
@@ -1,3 +1,7 @@
+import Web3 from 'web3';
+import { sha3 } from 'web3-utils';
+import { Signature } from 'web3/eth/accounts';
+
 export function toHex32(num) {
   return "0x" + toPaddedHexString(num, 64);
 }
@@ -16,4 +20,18 @@ export function padBytes32(data){
 export function toPaddedHexString(num, len) {
     let str = num.toString(16);
     return "0".repeat(len - str.length) + str;
+}
+
+export function sign(state: string, privateKey)
+  : {v: string, r: string, s: string} {
+  const localWeb3 = new Web3('');
+  const account:any = localWeb3.eth.accounts.privateKeyToAccount(privateKey);
+  const hash = sha3(state);
+  return localWeb3.eth.accounts.sign(hash, account.privateKey, true) as Signature;
+}
+
+export function recover(data: string, v: string, r: string, s: string): string {
+  const web3 = new Web3('');
+  const hash = sha3(data);
+  return web3.eth.accounts.recover(hash, v, r, s);
 }
