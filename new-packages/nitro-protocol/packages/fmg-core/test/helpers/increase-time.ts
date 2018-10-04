23272c0dd0acfad3a11f4b0caf04524ad666fa15
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-10-03 22:07:51 -0700

    currentProvider.sendAsync is broken in web3 1.0.0@beta

--- new-packages/nitro-protocol/packages/fmg-core/test/helpers/increase-time.ts
+++ new-packages/nitro-protocol/packages/fmg-core/test/helpers/increase-time.ts
@@ -7,7 +7,7 @@ export function increaseTime (duration) {
   const id = Date.now();
 
   return new Promise((resolve, reject) => {
-    web3.currentProvider.sendAsync({
+    web3.currentProvider.send({
       jsonrpc: '2.0',
       method: 'evm_increaseTime',
       params: [duration],
@@ -15,7 +15,7 @@ export function increaseTime (duration) {
     }, err1 => {
       if (err1) { return reject(err1); }
 
-      web3.currentProvider.sendAsync({
+      web3.currentProvider.send({
         jsonrpc: '2.0',
         method: 'evm_mine',
         id: id + 1,
