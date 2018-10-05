2a8dc9818e7ecfadb906eccd1900922b7b4c468b
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-10-04 17:21:22 -0700

    Export SolidityParameter class

--- packages/fmg-core/src/index.ts
+++ packages/fmg-core/src/index.ts
@@ -1,6 +1,7 @@
 export { State } from './state';
 export { Channel } from './channel';
-export { toHex32, padBytes32, sign, recover } from './utils';
+export { toHex32, padBytes32, sign, recover, SolidityParameter } from './utils';
+
 
 // TODO: these should probably be in their own package
 export { default as assertRevert } from '../test/helpers/assert-revert';
