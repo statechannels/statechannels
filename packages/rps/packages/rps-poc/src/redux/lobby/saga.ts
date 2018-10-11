0afb7d810538c83c5aa469d15f0c5269e9199d20
kerzhner <1062379+kerzhner@users.noreply.github.com>
2018-10-10 21:33:15 -0700

    Merge pull request #203 from magmo/challenge-presence
    Removing challenge heartbeats

--- packages/rps/packages/rps-poc/src/redux/lobby/saga.ts
+++ packages/rps/packages/rps-poc/src/redux/lobby/saga.ts
@@ -1,4 +1,4 @@
-import { fork, put, take, actionChannel, call, takeLatest, } from 'redux-saga/effects';
+import { fork, put, take, actionChannel} from 'redux-saga/effects';
 
 import { reduxSagaFirebase } from '../../gateways/firebase';
 
@@ -7,15 +7,14 @@ import * as applicationActions from '../application/actions';
 
 import GameEngineA from '../../game-engine/GameEngineA';
 import BN from 'bn.js';
-import { delay } from 'redux-saga';
-import { CHALLENGE_EXPIRATION_INTERVAL } from '../../constants';
+
 
 const DEFAULT_BALANCES = 50;
 
 export default function* lobbySaga(address: string) {
   yield put(applicationActions.lobbySuccess());
   // subscribe to challenges
-  yield fork(challengeSyncer);
+  yield* challengeSyncer();
 
   const channel = yield actionChannel([
     lobbyActions.ACCEPT_CHALLENGE,
@@ -59,14 +58,10 @@ const challengeTransformer = (dict) => {
     // Convert the stake from a string to a BN
     dict.value[key].stake = new BN(dict.value[key].stake);
     return dict.value[key];
-  }).filter((challenge) => {
-    // TODO: filter self challenges
-    return Date.now() < challenge.updatedAt + CHALLENGE_EXPIRATION_INTERVAL;
   });
 };
 
 function* challengeSyncer() {
-  // TODO: figure out how to use a Firebase reference here to limit the data
   yield fork(
     reduxSagaFirebase.database.sync,
     'challenges',
@@ -76,22 +71,4 @@ function* challengeSyncer() {
     },
     'value',
   );
-
-  yield takeLatest(lobbyActions.SYNC_CHALLENGES, expireChallenges);
-}
-
-function* expireChallenges() {
-  // This needs to be debounced at least as long as `CHALLENGE_EXPIRATION_INTERVAL`,
-  // in case we've just received a challenge that was just refreshed (In fact, this
-  // is the typical scenario.)
-  yield call(delay, CHALLENGE_EXPIRATION_INTERVAL);
-  const challenges = yield call(reduxSagaFirebase.database.read, '/challenges');
-  if (challenges) {
-    const activeChallenges = Object.keys(challenges).map(
-      (addr) => challenges[addr]
-    ).filter(
-      c => Date.now() < c.updatedAt + CHALLENGE_EXPIRATION_INTERVAL
-    );
-    yield put(lobbyActions.expireChallenges(activeChallenges));
-  }
 }
--- packages/rps/packages/rps-poc/src/redux/waiting-room/saga.ts
+++ packages/rps/packages/rps-poc/src/redux/waiting-room/saga.ts
@@ -1,4 +1,4 @@
-import { take, actionChannel, put, call, fork } from 'redux-saga/effects';
+import { take, actionChannel, put, call, apply } from 'redux-saga/effects';
 import { default as firebase, reduxSagaFirebase } from '../../gateways/firebase';
 
 import * as waitingRoomActions from '../waiting-room/actions';
@@ -6,9 +6,7 @@ import * as messageActions from '../message-service/actions';
 import * as applicationActions from '../application/actions';
 import GameEngineB from '../../game-engine/GameEngineB';
 import decode from '../../game-engine/positions/decode';
-import { delay } from 'redux-saga';
 import BN from 'bn.js';
-import { CHALLENGE_REFRESH_INTERVAL } from '../../constants';
 
 type ActionType = waitingRoomActions.CancelChallenge | messageActions.MessageReceived;
 
@@ -31,25 +29,25 @@ export default function* waitingRoomSaga(
     updatedAt: new Date().getTime(),
   };
 
+  const challengeKey = `/challenges/${address}`;
+
   yield put(applicationActions.waitingRoomSuccess({ ...commonChallengeProps, stake }));
   const serializedChallenge = {
     ...commonChallengeProps,
     stake: stake.toString(),
   };
+  
+  const disconnect = firebase.database().ref(challengeKey).onDisconnect();
+  yield apply(disconnect, disconnect.remove);
   // use update to allow us to pick our own key
-  yield call(reduxSagaFirebase.database.update, `/challenges/${address}`, serializedChallenge);
-
-  yield fork(challengeHeartbeatSaga, {
-    ...commonChallengeProps,
-    stake,
-  });
+  yield call(reduxSagaFirebase.database.update, challengeKey, serializedChallenge);
 
   while (true) {
     const action: ActionType = yield take(channel);
 
     switch (action.type) {
       case waitingRoomActions.CANCEL_CHALLENGE:
-        yield call(reduxSagaFirebase.database.delete, `/challenges/${address}`);
+        yield call(reduxSagaFirebase.database.delete, challengeKey);
         yield put(applicationActions.lobbyRequest());
         break;
 
@@ -57,29 +55,9 @@ export default function* waitingRoomSaga(
         const position = decode(action.message);
         const gameEngine = GameEngineB.fromProposal(position);
         // todo: handle error if it isn't a propose state with the right properties
-        yield call(reduxSagaFirebase.database.delete, `/challenges/${address}`);
+        yield call(reduxSagaFirebase.database.delete, challengeKey);
         yield put(applicationActions.gameRequest(gameEngine));
         break;
     }
   }
-}
-
-function* challengeHeartbeatSaga(challenge) {
-  while (true) {
-    yield call(delay, CHALLENGE_REFRESH_INTERVAL);
-    yield updateChallenge(challenge);
-  }
-}
-
-const challengeRef = challenge => {
-  return firebase.database().ref(`challenges/${challenge.address}`);
-};
-
-function* updateChallenge(challenge, updateParams?) {
-  const challengeParams = {
-    ...updateParams,
-    updatedAt: new Date().getTime(),
-  };
-
-  return yield call(reduxSagaFirebase.database.patch, challengeRef(challenge), challengeParams);
 }
