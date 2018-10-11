14a2a3fc34c0dabc9ff1ca1b8420af2ef7aabb59
kerzhner <1062379+kerzhner@users.noreply.github.com>
2018-10-10 21:33:15 -0700

    Merge pull request #203 from magmo/challenge-presence
    Removing challenge heartbeats

--- packages/wallet/packages/rps-poc/src/redux/lobby/actions.ts
+++ packages/wallet/packages/rps-poc/src/redux/lobby/actions.ts
@@ -3,7 +3,6 @@ import BN from 'bn.js';
 export const ACCEPT_CHALLENGE = 'LOBBY.ACCEPT_CHALLENGE';
 export const CREATE_CHALLENGE = 'LOBBY.CREATE_CHALLENGE';
 export const SYNC_CHALLENGES = 'LOBBY.SYNC_CHALLENGES';
-export const EXPIRE_CHALLENGES = 'LOBBY.EXPIRE_CHALLENGES';
 
 export const acceptChallenge = (address: string, stake: BN) => ({
   type: ACCEPT_CHALLENGE as typeof ACCEPT_CHALLENGE,
@@ -22,14 +21,8 @@ export const syncChallenges = (challenges: any[]) => ({
   challenges,
 });
 
-export const expireChallenges = (challenges: any[]) => ({
-  type: EXPIRE_CHALLENGES as typeof EXPIRE_CHALLENGES,
-  challenges,
-});
-
 export type AcceptChallenge = ReturnType<typeof acceptChallenge>;
 export type CreateChallenge = ReturnType<typeof createChallenge>;
 export type SyncChallenge = ReturnType<typeof syncChallenges>;
-export type ExpireChallenges = ReturnType<typeof expireChallenges>;
 
-export type AnyAction = AcceptChallenge | CreateChallenge | SyncChallenge | ExpireChallenges;
+export type AnyAction = AcceptChallenge | CreateChallenge | SyncChallenge;
