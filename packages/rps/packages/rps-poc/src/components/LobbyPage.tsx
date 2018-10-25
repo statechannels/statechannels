597224b1e15bdf48a9a26eeb95682a8baa963f48
tomclose <tom.close@cantab.net>
2018-10-25 18:06:44 +0100

    Merge pull request #230 from magmo/navigation-bar-container
    Navigation bar container

--- packages/rps/packages/rps-poc/src/components/LobbyPage.tsx
+++ packages/rps/packages/rps-poc/src/components/LobbyPage.tsx
@@ -7,13 +7,9 @@ import { OpenGame } from '../redux/open-games/state';
 
 import { Button, ButtonGroup, Table } from 'reactstrap';
 import BN from 'bn.js';
-import NavigationBar from './NavigationBar';
-import { LoginState } from 'src/redux/login/reducer';
-import { RulesState } from 'src/redux/global/state';
+import { ApplicationLayout } from './ApplicationLayout';
 
 interface Props {
-  loginState: LoginState;
-  rulesState: RulesState;
   openGames: OpenGame[];
   joinOpenGame: (
     myName: string,
@@ -25,8 +21,6 @@ interface Props {
     roundBuyIn: BN,
   ) => void;
   newOpenGame: () => void;
-  logoutRequest: () => void;
-  rulesRequest: () => void;
 }
 
 const initialState = { showChallenges: true };
@@ -36,61 +30,58 @@ export default class LobbyPage extends React.PureComponent<Props, State> {
   readonly state: State = initialState;
 
   render() {
-    const { loginState, rulesState, newOpenGame, joinOpenGame, logoutRequest, rulesRequest } = this.props;
+    const { newOpenGame, joinOpenGame } = this.props;
     const openGames = this.props.openGames || [];
 
     return (
-      <div className="w-100">
-        <NavigationBar login={loginState} rules={rulesState} logoutRequest={logoutRequest} rulesRequest={rulesRequest}/>
-        <div className="container centered-container w-100">
-          <div className="w-100">
-            <ButtonGroup className="d-flex w-100 mb-3">
-              <Button
-                className="w-50"
-                outline={false}
-              >
-                Select an opponent
+      <ApplicationLayout>
+        <div className="w-100">
+          <ButtonGroup className="d-flex w-100 mb-3">
+            <Button
+              className="w-50"
+              outline={false}
+            >
+              Select an opponent
               </Button>
-              <Button
-                className="w-50"
-                outline={true}
-                onClick={newOpenGame}
-              >
-                Create a game
+            <Button
+              className="w-50"
+              outline={true}
+              onClick={newOpenGame}
+            >
+              Create a game
               </Button>
-            </ButtonGroup>
+          </ButtonGroup>
 
-            <div className="mb-5">
-              <Table hover={true}>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Wager (Finney)</th>
+          <div className="mb-5">
+            <Table hover={true}>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Wager (Finney)</th>
+                </tr>
+              </thead>
+              <tbody>
+                {openGames.map(openGame => (
+                  <tr
+                    key={openGame.address}
+                    onClick={() => joinOpenGame(
+                      'TODO myName',
+                      'TODO myAddress',
+                      openGame.name,
+                      openGame.address,
+                      'TODO libraryAddress',
+                      5,
+                      openGame.stake)}
+                  >
+                    <td>{openGame.name}</td>
+                    <td>{web3Utils.fromWei(openGame.stake.toString(), 'finney')}</td>
                   </tr>
-                </thead>
-                <tbody>
-                  {openGames.map(openGame => (
-                    <tr
-                      key={openGame.address}
-                      onClick={() => joinOpenGame(
-                        'TODO myName',
-                        'TODO myAddress',
-                        openGame.name,
-                        openGame.address,
-                        'TODO libraryAddress',
-                        5,
-                        openGame.stake)}
-                    >
-                      <td>{openGame.name}</td>
-                      <td>{web3Utils.fromWei(openGame.stake.toString(), 'finney')}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </div>
+                ))}
+              </tbody>
+            </Table>
           </div>
         </div>
-      </div>
+      </ApplicationLayout >
     );
   }
 }
