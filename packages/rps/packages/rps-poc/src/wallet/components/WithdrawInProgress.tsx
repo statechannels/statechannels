0ec3f107902c417ee99b4915f1ee54b05fab2b0f
tomclose <tom.close@cantab.net>
2018-10-28 17:51:03 +0000

    Merge pull request #247 from magmo/ag-wallet-logo
    Add stash icon to wallet screens

--- packages/rps/packages/rps-poc/src/wallet/components/WithdrawInProgress.tsx
+++ packages/rps/packages/rps-poc/src/wallet/components/WithdrawInProgress.tsx
@@ -4,7 +4,7 @@ import progressIcon from '../../images/icon_arrow.svg';
 import completedIcon from '../../images/icon_success.svg';
 import waitingIcon from '../../images/icon_loading.svg';
 
-import ethIcon from '../../images/ethereum_icon.svg';
+import walletHeaderIcon from '../../images/wallet_header_icon.svg';
 
 import web3Utils from 'web3-utils';
 import Button from 'reactstrap/lib/Button';
@@ -14,43 +14,43 @@ interface Props {
   loginDisplayName: string;
   amount: BN;
   withdrawStatus: BlockchainStatus;
-  exitGame?:()=>void;
+  exitGame?: () => void;
 }
 
 
 export default class WithdrawInProgress extends React.PureComponent<Props> {
   render() {
-  let title;
-  const {withdrawStatus, amount, exitGame } = this.props;
-  switch (withdrawStatus){
-    case BlockchainStatus.NotStarted:
-    title = "Your Transfer";
-    break;
-    case BlockchainStatus.InProgress:
-    title =" Transfer in Progress";
-    break;
-    case BlockchainStatus.Completed:
-    title="Transfer Complete";
-  }
+    let title;
+    const { withdrawStatus, amount, exitGame } = this.props;
+    switch (withdrawStatus) {
+      case BlockchainStatus.NotStarted:
+        title = "Your Transfer";
+        break;
+      case BlockchainStatus.InProgress:
+        title = " Transfer in Progress";
+        break;
+      case BlockchainStatus.Completed:
+        title = "Transfer Complete";
+    }
 
 
     return (
       <div className="withdraw-container" >
-      <div className="withdraw-header">
-      <img src ={ethIcon} />
-       <div className="withdraw-circle">
-          <div className="withdraw-user">{this.getInitials(this.props.loginDisplayName)}</div>
+        <div className="withdraw-header">
+          <img src={walletHeaderIcon} className="withdraw-header-icon" />
+          <div className="withdraw-circle">
+            <div className="withdraw-user">{this.getInitials(this.props.loginDisplayName)}</div>
+          </div>
         </div>
-      </div>
-      <div className="withdraw-transfer-container">
-        <div className="withdraw-container">
-          <img className="withdraw-icon" src={this.getIcon(withdrawStatus)} />
-          <div className="withdraw-title">{title}</div>
-          <div className="withdraw-amount">Amount {web3Utils.fromWei(amount, 'ether')} ETH</div>
-          Check your wallet to the right to confirm the amount is correct.
+        <div className="withdraw-transfer-container">
+          <div className="withdraw-container">
+            <img className="withdraw-icon" src={this.getIcon(withdrawStatus)} />
+            <div className="withdraw-title">{title}</div>
+            <div className="withdraw-amount">Amount {web3Utils.fromWei(amount, 'ether')} ETH</div>
+            Check your wallet to the right to confirm the amount is correct.
         </div>
-      </div>
-      {withdrawStatus === BlockchainStatus.Completed && exitGame &&  <Button className="exit-game-button" onClick={exitGame} >Exit Game</Button> }
+        </div>
+        {withdrawStatus === BlockchainStatus.Completed && exitGame && <Button className="exit-game-button" onClick={exitGame} >Exit Game</Button>}
       </div>
     );
   }
@@ -58,18 +58,18 @@ export default class WithdrawInProgress extends React.PureComponent<Props> {
     const userDisplayName = loginDisplayName.split(" ");
     return userDisplayName.map(name => name.charAt(0)).join("");
   }
-  
+
 
   getIcon(blockchainStatus: BlockchainStatus) {
     switch (blockchainStatus) {
       case BlockchainStatus.NotStarted:
-        return  waitingIcon;
+        return waitingIcon;
         break;
       case BlockchainStatus.InProgress:
-        return  progressIcon;
+        return progressIcon;
         break;
       case BlockchainStatus.Completed:
-        return  completedIcon;
+        return completedIcon;
         break;
     }
   }
