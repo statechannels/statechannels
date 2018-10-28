4828a14dee2fc53466d3afd8868b6292889fe504
Alex Gap <alex.gap@gmail.com>
2018-10-28 17:55:48 +0100

    Merge pull request #246 from magmo/ag-happy-path
    Styling Touch Ups & Game Over Modal

--- packages/rps/packages/rps-poc/src/redux/metamask/saga.ts
+++ packages/rps/packages/rps-poc/src/redux/metamask/saga.ts
@@ -2,6 +2,7 @@ import * as metamaskActions from './actions';
 import { put, cps } from 'redux-saga/effects';
 import truffle from 'truffle.js';
 import { MetamaskErrorType } from './actions';
+import { delay } from 'redux-saga';
 
 export default function* checkMetamask() {
   if (typeof web3 !== 'object' || web3 === null) {
@@ -13,15 +14,14 @@ export default function* checkMetamask() {
     return false;
   }
 
-  // TODO: Check if account is locked
 
   const targetNetworkName = process.env.TARGET_NETWORK;
   const selectedNetworkId = parseInt(yield cps(web3.version.getNetwork), 10);
   // Find the network name that matches the currently selected network id
-  const selectedNetworkName = Object.keys(truffle.networks).find(networkName => 
-    truffle.networks[networkName].network_id === selectedNetworkId)|| "development";
-  
-    if (targetNetworkName !== selectedNetworkName) {
+  const selectedNetworkName = Object.keys(truffle.networks).find(networkName =>
+    truffle.networks[networkName].network_id === selectedNetworkId) || "development";
+
+  if (targetNetworkName !== selectedNetworkName) {
     yield put(
       metamaskActions.metamaskErrorOccurred({
         errorType: MetamaskErrorType.WrongNetwork,
@@ -30,7 +30,19 @@ export default function* checkMetamask() {
     );
     return false;
   }
-
+  let accountUnlocked = false;
+  while (!accountUnlocked) {
+    const accounts = yield cps(web3.eth.getAccounts);
+    accountUnlocked = accounts && accounts.length > 0;
+    if (!accountUnlocked) {
+      yield put(
+        metamaskActions.metamaskErrorOccurred({
+          errorType: MetamaskErrorType.MetamaskLocked,
+        }));
+    } else {
+      delay(1000);
+    }
+  }
   yield put(metamaskActions.metamaskSuccess());
   return true;
 }
