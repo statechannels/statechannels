71ddb775b9dfd061c2807076516944bb6f388544
Alex Gap <alex.gap@gmail.com>
2018-10-29 12:35:42 +0100

    Merge pull request #264 from magmo/create-game-copy2
    Removing references to wager

--- packages/rps/packages/rps-poc/src/components/RulesModal.tsx
+++ packages/rps/packages/rps-poc/src/components/RulesModal.tsx
@@ -15,11 +15,11 @@ export const RulesModal = (props: Props) => {
       <ModalBody>
         <ul>
           <li>Each player must contribute their buy-in amount to enter a game.</li>
-          <li>Each player must have enough funds to cover the wager for each round.</li>
+          <li>Each player must have enough funds to cover the buy-in for each round.</li>
           <li>Each player takes turns picking a selection.</li>
           <li>At the end of each round a winner is determined.</li>
-          <li>The winner of each round collects their wager + their opponents wager.</li>
-          <li>The game ends when one player is out of wager funds or leaves the game.</li>
+          <li>The winner of each round collects their buy-in + their opponent's buy-in.</li>
+          <li>The game ends when one player does not have enough funds for a round buy-in or leaves the game.</li>
         </ul>
       </ModalBody>
     </Modal>
