e0d60391b717001f8265a7888f124596f8e92f43
andrewgordstewart <andrew.gord.stewart@gmail.com>
2018-10-30 12:22:26 +0100

    Merge pull request #285 from magmo/as-split-result-page
    Split ResultPage into PlayAgain and WaitForRestingA

---
+++ packages/rps/packages/rps-poc/src/components/PlayAgain.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+
+import { Button } from 'reactstrap';
+
+import { Move, Result } from '../core';
+import { MoveBadge } from './MoveBadge';
+import { GameLayout } from './GameLayout';
+
+interface Props {
+  yourMove: Move;
+  theirMove: Move;
+  result: Result;
+  playAgain: () => void;
+}
+
+export default class Playagain extends React.PureComponent<Props> {
+  renderResultText() {
+    const { result } = this.props;
+
+    switch (result) {
+      case Result.YouWin:
+        return 'You won! 🎉';
+
+      case Result.YouLose:
+        return 'You lost 😭';
+
+      default:
+        return "It's a tie! 🙄";
+    }
+  }
+
+  render() {
+    const { yourMove, theirMove, playAgain } = this.props;
+
+    return (
+      <GameLayout>
+        <div className="w-100 text-center">
+          <h1 className="mb-5">{this.renderResultText()}</h1>
+          <div className="row">
+            <div className="col-sm-6">
+              <p className="lead">
+                You chose <strong>{Move[yourMove]}</strong>
+              </p>
+              <div>
+                <MoveBadge move={yourMove} />
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <p className="lead">
+                Your opponent chose <strong>{Move[theirMove]}</strong>
+              </p>
+              <div>
+                <MoveBadge move={theirMove} />
+              </div>
+            </div>
+          </div>
+
+          <Button className="cog-button" onClick={playAgain}>
+            Play again
+          </Button>
+          <div> Waiting for opponent to suggest a new game </div>
+        </div>
+      </GameLayout>
+    );
+  }
+}
