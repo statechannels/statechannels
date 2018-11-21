cad7c8343d003df4b7e8da4f19dbe507f088e5c0
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-21 09:23:37 -0700

    WIP: migrate test contracts
    We'll need to determine a way to only migrate these test contracts
    in a test environment

--- new-packages/nitro-protocol/packages/fmg-core/migrations/2_deploy_contracts.js
+++ new-packages/nitro-protocol/packages/fmg-core/migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 // var RockPaperScissorsGame = artifacts.require("./RockPaperScissorsGame.sol");
 // var RockPaperScissorsState = artifacts.require("./RockPaperScissorsState.sol");
 // var SimpleAdjudicator = artifacts.require("./SimpleAdjudicator.sol");
-// var CountingState = artifacts.require("./CountingState.sol");
-// var CountingGame = artifacts.require("./CountingGame.sol");
+var CountingState = artifacts.require("./CountingState.sol");
+var CountingGame = artifacts.require("./CountingGame.sol");
 var State = artifacts.require("./State.sol");
 var Rules = artifacts.require("./Rules.sol");
 
@@ -16,11 +16,11 @@ module.exports = function(deployer) {
   // deployer.link(Rules, SimpleAdjudicator);
   // deployer.deploy(SimpleAdjudicator);
 
-  // deployer.link(State, CountingState);
-  // deployer.deploy(CountingState);
-  // deployer.link(CountingState, CountingGame);
-  // deployer.link(State, CountingGame);
-  // deployer.deploy(CountingGame);
+  deployer.link(State, CountingState);
+  deployer.deploy(CountingState);
+  deployer.link(CountingState, CountingGame);
+  deployer.link(State, CountingGame);
+  deployer.deploy(CountingGame);
 
   // deployer.link(State, RockPaperScissorsState);
   // deployer.deploy(RockPaperScissorsState);
