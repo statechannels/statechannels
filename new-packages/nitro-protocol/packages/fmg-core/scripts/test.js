35a1531ae7526ef0cac310b9932bb7bf6b43db8f
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-26 10:00:20 -0700

    WIP: continue past start-ganache

--- new-packages/nitro-protocol/packages/fmg-core/scripts/test.js
+++ new-packages/nitro-protocol/packages/fmg-core/scripts/test.js
@@ -1,15 +1,35 @@
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
+//TODO: This will be obsolete when we move to jest tests
+var path = require('path');
+const {
+  exec
+} = require('child_process');
+
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+if (!process.env.DEV_GANACHE_PORT) {
+  process.env.DEV_GANACHE_PORT = 8545;
+}
+
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-require('magmo-devtools').runJest();
+let {
+  startGanache,
+  deployContracts,
+  runJest
+} = require('magmo-devtools');
+
+console.log(`Using port ${process.env.DEV_GANACHE_PORT} for Ganache.`);
+startGanache().then(() => {
+
+  console.log('deploying contracts');
+  deployContracts().then(() => {
+    console.log('running jest');
+    runJest()
+  });
+});
