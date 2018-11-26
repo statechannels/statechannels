14b0c178204dd636e03d00a258fa33a41cb3c8e3
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-26 12:20:12 -0700

    Use private-key funded by magmo-devtools

--- new-packages/nitro-protocol/packages/fmg-simple-adjudicator/test/simple-adjudicator.test.ts
+++ new-packages/nitro-protocol/packages/fmg-simple-adjudicator/test/simple-adjudicator.test.ts
@@ -47,7 +47,7 @@ const ACCOUNTS = [
 describe('SimpleAdjudicator', () => {
   let networkId;
   const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-  const privateKey = '0x' + '1'.repeat(64);
+  const privateKey = "0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d";
   const wallet = new Wallet(privateKey, provider);
 
   let simpleAdj;
