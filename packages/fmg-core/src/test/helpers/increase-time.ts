0f8443f79b7e2b255b9d09cfe67d06329b216ec7
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-26 23:47:35 -0700

    increaseTime uses ethers

--- packages/fmg-core/src/test/helpers/increase-time.ts
+++ packages/fmg-core/src/test/helpers/increase-time.ts
@@ -1,43 +1,14 @@
 // From https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/test/helpers/increaseTime.js
 
-import latestTime from "./latest-time";
-
 // Increases testrpc time by the passed duration in seconds
-export function increaseTime(duration) {
-  const id = Date.now();
-
+export function increaseTime(duration, provider) {
   return new Promise((resolve, reject) => {
-    web3.currentProvider.send({
-      jsonrpc: "2.0",
-      method: "evm_increaseTime",
-      params: [duration],
-      id,
-    }, (err1) => {
-      if (err1) { return reject(err1); }
-
-      web3.currentProvider.send({
-        jsonrpc: "2.0",
-        method: "evm_mine",
-        id: id + 1,
-      }, (err2, res) => {
+    provider.send( "evm_increaseTime", [duration]).then(() => {
+      provider.send("evm_mine").then((res, err2) => {
         return err2 ? reject(err2) : resolve(res);
       });
     });
   });
-}
-
-/**
- * Beware that due to the need of calling two separate testrpc methods and rpc calls overhead
- * it's hard to increase time precisely to a target point so design your test to tolerate
- * small fluctuations from time to time.
- *
- * @param target time in seconds
- */
-export function increaseTimeTo(target) {
-  const now = latestTime();
-  if (target < now) { throw Error(`Cannot increase current time(${now}) to a moment in the past(${target})`); }
-  const diff = target - now;
-  return increaseTime(diff);
 }
 
 export const DURATION = {
