86d3573c9fc0d270a712d1ed706c18adc4baa745
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-27 15:18:06 -0700

    Rules and State are libraries again

--- packages/fmg-core/contracts/ForceMoveGame.sol
+++ packages/fmg-core/contracts/ForceMoveGame.sol
@@ -1,8 +1,5 @@
 pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
-
-import './State.sol';
 
 interface ForceMoveGame {
-  function validTransition(State.StateStruct calldata oldState, State.StateStruct calldata newState) external pure returns (bool);
+  function validTransition(bytes calldata oldState, bytes calldata newState) external pure returns (bool);
 }
