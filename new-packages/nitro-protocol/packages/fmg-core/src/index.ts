a9860e0935338a2cc5518b743bc207a9881001d2
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-27 12:41:14 -0700

    Configure and run prettier

--- new-packages/nitro-protocol/packages/fmg-core/src/index.ts
+++ new-packages/nitro-protocol/packages/fmg-core/src/index.ts
@@ -1,9 +1,17 @@
-export { State } from "./state";
-export { Channel } from "./channel";
-export { toHex32, padBytes32, sign, recover, decodeSignature, SolidityType, SolidityParameter } from "./utils";
+export { State } from './state';
+export { Channel } from './channel';
+export {
+  toHex32,
+  padBytes32,
+  sign,
+  recover,
+  decodeSignature,
+  SolidityType,
+  SolidityParameter,
+} from './utils';
 
 // TODO: these should probably be in their own package
-export { default as expectRevert } from "./test/helpers/expect-revert";
-export { increaseTime, DURATION } from "./test/helpers/increase-time";
+export { default as expectRevert } from './test/helpers/expect-revert';
+export { increaseTime, DURATION } from './test/helpers/increase-time';
 
-export { CountingGame } from "./test-game/counting-game";
+export { CountingGame } from './test-game/counting-game';
--- new-packages/nitro-protocol/packages/fmg-core/src/test/helpers/expect-revert.ts
+++ new-packages/nitro-protocol/packages/fmg-core/src/test/helpers/expect-revert.ts
@@ -1,10 +1,10 @@
 // https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/test/helpers/assertRevert.js
-export default async (promise) => {
+export default async promise => {
   try {
     await promise;
-    throw new Error("Expected revert not received");
+    throw new Error('Expected revert not received');
   } catch (error) {
-    const revertFound = error.message.search("revert") >= 0;
+    const revertFound = error.message.search('revert') >= 0;
     expect(revertFound).toBe(true);
   }
 };
