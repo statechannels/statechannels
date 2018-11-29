9221ed2c5d92651f78842428c0ebe3d9797a08ae
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-29 14:23:57 -0700

    Deploy contracts

--- packages/fmg-core/migrations/2_deploy_contracts.js
+++ packages/fmg-core/migrations/2_deploy_contracts.js
@@ -1,11 +1,20 @@
 var State = artifacts.require('./State.sol');
-// var Rules = artifacts.require('./Rules.sol');
-var SampleState = artifacts.require('./SampleState.sol');
+var Rules = artifacts.require('./Rules.sol');
+
+var CountingState = artifacts.require('./test-game/CountingState.sol');
+var CountingGame = artifacts.require('./test-game/CountingGame.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(State);
-  deployer.deploy(SampleState);
 
-  // deployer.link(State, Rules);
-  // deployer.deploy(Rules);
+  deployer.link(State, Rules);
+  deployer.deploy(Rules);
+
+  deployer.link(State, CountingState);
+  deployer.deploy(CountingState);
+
+  deployer.link(State, CountingGame);
+  deployer.link(CountingState, CountingGame);
+  deployer.link(Rules, CountingGame);
+  deployer.deploy(CountingGame);
 };
