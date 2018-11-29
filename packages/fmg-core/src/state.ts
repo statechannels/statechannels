ee863c6db827a4f0331a3576ce5627f343ee5501
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-28 22:59:42 -0700

    Update state.ts

--- packages/fmg-core/src/state.ts
+++ packages/fmg-core/src/state.ts
@@ -1,27 +1,37 @@
-import { toHex32 } from './utils';
 import { Channel } from './channel';
 import BN from 'bn.js';
+import abi from 'web3-eth-abi';
+
+const SolidityStateType = {
+  "StateStruct": {
+    "channelType": "address",
+    "channelNonce": "uint256",
+    "numberOfParticipants": "uint256",
+    "participants": "address[]",
+    "stateType": "uint8",
+    "turnNum": "uint256",
+    "stateCount": "uint256",
+    "resolution": "uint256[]",
+    "gameAttributes": "bytes",
+  },
+};
 
 class State {
-  channel: any;
+  channel: Channel;
   stateType: State.StateType;
   turnNum: number;
   resolution: BN[];
   stateCount: number;
 
-  constructor({
-    channel,
-    stateType,
-    turnNum,
-    resolution,
-    stateCount = 0,
-  }: {
-    channel: Channel;
-    stateType: State.StateType;
-    turnNum: number;
-    resolution: BN[];
-    stateCount?: number;
-  }) {
+  constructor({channel, stateType, turnNum, resolution, stateCount=0}: 
+    {
+      channel: Channel, 
+      stateType: State.StateType,
+      turnNum: number,
+      resolution: BN[],
+      stateCount?: number,
+    }
+  ) {
     this.channel = channel;
     this.stateType = stateType;
     this.turnNum = turnNum;
@@ -34,31 +44,43 @@ class State {
   }
 
   toHex() {
-    return (
-      this.channel.toHex() +
-      toHex32(this.stateType).substr(2) +
-      toHex32(this.turnNum).substr(2) +
-      toHex32(this.stateCount).substr(2) +
-      this.resolution.map(x => toHex32(x).substr(2)).join('')
-    );
+    return abi.encodeParameter(SolidityStateType, this.args);
   }
 
   get numberOfParticipants() {
     return this.channel.numberOfParticipants;
   }
 
+  get gameAttributes() {
+    return "0x";
+  }
+
   get mover() {
     return this.channel.participants[this.turnNum % this.numberOfParticipants];
+  }
+
+  get args() {
+    return [
+      this.channel.channelType,
+      this.channel.channelNonce,
+      this.numberOfParticipants,
+      this.channel.participants,
+      this.stateType,
+      this.turnNum,
+      this.stateCount,
+      this.resolution.map(String),
+      '0x',
+    ];
   }
 }
 
 // tslint:disable-next-line:no-namespace
 namespace State {
   export enum StateType {
-    PreFundSetup,
-    PostFundSetup,
-    Game,
-    Conclude,
+    PreFundSetup = '0',
+    PostFundSetup = '1',
+    Game = '2',
+    Conclude = '3',
   }
 }
 
