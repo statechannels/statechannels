31f693a07b9a15e4a4b64677917910d04992686d
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-30 13:22:48 -0700

    Add tests for CountingGame

---
+++ new-packages/nitro-protocol/packages/fmg-core/src/test/test-game/counting-game.test.ts
@@ -0,0 +1,79 @@
+import { CountingGame } from '../../test-game/counting-game';
+import expectRevert from '../helpers/expect-revert';
+import linker from 'solc/linker';
+
+import { Channel } from '../../';
+import BN from 'bn.js';
+import { ethers, Wallet, ContractFactory } from 'ethers';
+
+import StateArtifact from '../../../build/contracts/State.json';
+
+import CountingStateArtifact from '../../../build/contracts/CountingState.json';
+import CountingGameArtifact from '../../../build/contracts/CountingGame.json';
+
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+const privateKey = '0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d';
+const wallet = new Wallet(privateKey, provider);
+
+describe('CountingGame', () => {
+  let game;
+  let state0;
+  let state1;
+  let stateBalChange;
+
+  beforeAll(async () => {
+    // Contract setup --------------------------------------------------------------------------
+    const networkId = (await provider.getNetwork()).chainId;
+    CountingStateArtifact.bytecode = linker.linkBytecode(CountingStateArtifact.bytecode, {
+      State: StateArtifact.networks[networkId].address,
+    });
+
+    CountingGameArtifact.bytecode = linker.linkBytecode(CountingGameArtifact.bytecode, {
+      CountingState: CountingStateArtifact.networks[networkId].address,
+    });
+    game = await ContractFactory.fromSolidity(CountingGameArtifact, wallet).attach(
+      CountingGameArtifact.networks[networkId].address,
+    );
+    // Contract setup --------------------------------------------------------------------------
+
+    const participantA = new ethers.Wallet(
+      '6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1',
+    );
+    const participantB = new ethers.Wallet(
+      '6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c',
+    );
+    const participants = [participantA.address, participantB.address];
+
+
+    const channel = new Channel(game.address, 0, participants);
+    
+    const defaults = { channel, resolution: [new BN(5), new BN(4)] };
+
+    state0 = CountingGame.gameState({ ...defaults, turnNum: 6, gameCounter: 1 });
+    state1 = CountingGame.gameState({ ...defaults, turnNum: 7, gameCounter: 2 });
+
+    stateBalChange = CountingGame.gameState({
+      ...defaults,
+      resolution: [new BN(6), new BN(3)],
+      turnNum: 7,
+      gameCounter: 2,
+    });
+  });
+
+  // Transition fuction tests
+  // ========================
+
+  it('allows a move where the count increment', async () => {
+    const output = await game.validTransition(state0.args, state1.args);
+    expect(output).toBe(true);
+  });
+
+  // it("allows START -> CONCLUDED if totals match", async () => {
+  //   var output = await game.validTransition.call(start, allowedConcluded);
+  //   assert.equal(output, true);
+  // });
+
+  it("doesn't allow transitions if totals don't match", async () => {
+    await expectRevert(game.validTransition(state0.args, stateBalChange.args), "CountingGame: resolutions must be equal");
+  });
+});
