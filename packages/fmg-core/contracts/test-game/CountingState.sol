0c1f311cd8e4f2bce589264c71f6fb96e2ba8cce
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-30 13:22:24 -0700

    CountingState tests pass

--- packages/fmg-core/contracts/test-game/CountingState.sol
+++ packages/fmg-core/contracts/test-game/CountingState.sol
@@ -6,8 +6,18 @@ import "../State.sol";
 library CountingState {
     using State for State.StateStruct;
 
+    struct GameAttributes {
+        uint256 gameCounter;
+    }
+
     struct CountingStateStruct {
         uint256 gameCounter;
-        State.StateStruct frameworkState;
+        uint256[] resolution;
+    }
+
+    function fromFrameworkState(State.StateStruct memory frameworkState) public pure returns (CountingStateStruct memory) {
+        GameAttributes memory gameAttributes = abi.decode(frameworkState.gameAttributes, (GameAttributes));
+
+        return CountingStateStruct(gameAttributes.gameCounter, frameworkState.resolution);
     }
 }
