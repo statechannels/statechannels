5883bdee8a605b3640e35cf5c4833e97dbcf4036
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-11-30 21:06:13 -0700

    Add linkedByteCode helper

--- packages/devtools/index.js
+++ packages/devtools/index.js
@@ -2,10 +2,12 @@ const { startGanache } = require('./utils/startGanache');
 const { deployContracts } = require('./utils/deployContracts');
 const { runJest } = require('./utils/runJest');
 const { expectEvent } = require('./utils/expectEvent');
+const { linkedByteCode } = require('./utils/linkedByteCode');
 
 module.exports = {
     startGanache,
     deployContracts,
     runJest,
-    expectEvent
+    expectEvent,
+    linkedByteCode
 };
