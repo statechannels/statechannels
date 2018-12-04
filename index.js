30c2f4b4a93a7e6425e82570eae3ad1df256efe7
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-12-04 09:51:59 -0700

    Add assertRevert
    chai assertions are a cleaner way to display a failure message than jest
    expectations. (Throwing an error inside of a promise resolution doesn't
    play well with jest test cases.)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { deployContracts } = require('./utils/deployContracts');
 const { runJest } = require('./utils/runJest');
 const { expectEvent } = require('./utils/expectEvent');
 const { linkedByteCode } = require('./utils/linkedByteCode');
+const { assertRevert } = require('./utils/assertRevert');
 const { ganacheProvider, privateKeyWithEth, walletWithEthAndProvider, getNetworkId } = require("./utils/networkSetup");
 
 module.exports = {
@@ -11,6 +12,7 @@ module.exports = {
     runJest,
     expectEvent,
     linkedByteCode,
+    assertRevert,
     ganacheProvider,
     privateKeyWithEth,
     walletWithEthAndProvider,
