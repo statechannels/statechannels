cb31086fe2bf2ae2ebcd3eef844caafaf70308b1
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-12-04 09:51:34 -0700

    Add networkSetup

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const { deployContracts } = require('./utils/deployContracts');
 const { runJest } = require('./utils/runJest');
 const { expectEvent } = require('./utils/expectEvent');
 const { linkedByteCode } = require('./utils/linkedByteCode');
+const { ganacheProvider, privateKeyWithEth, walletWithEthAndProvider, getNetworkId } = require("./utils/networkSetup");
 
 module.exports = {
     startGanache,
     deployContracts,
     runJest,
     expectEvent,
-    linkedByteCode
+    linkedByteCode,
+    ganacheProvider,
+    privateKeyWithEth,
+    walletWithEthAndProvider,
+    getNetworkId,
 };
