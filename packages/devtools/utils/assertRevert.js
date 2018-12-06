9bd6b16d1d17965dfbc5caba044e66c86a983f72
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-12-06 12:35:30 -0700

    Assert revert uses indexOf with a string pattern

--- packages/devtools/utils/assertRevert.js
+++ packages/devtools/utils/assertRevert.js
@@ -1,15 +1,19 @@
 const chai = require('chai');
 module.exports = {
-    assertRevert: (promise, pattern) => {
+    assertRevert: async (promise, pattern) => {
         let revertFound, messageMatch;
-        promise.then(() => {
+        promise.then((val) => {
             chai.assert.fail('Expected revert not received');
         }).catch((error) => {
             revertFound = error.message.search('revert') >= 0;
             chai.assert(revertFound, `Revert not found. ${error}`);
 
-            messageMatch = error.message.search(pattern) >= 0;
+            if (typeof pattern === "string") {
+                messageMatch = error.message.indexOf(pattern) >= 0;
+            } else {
+                messageMatch = error.message.search(pattern) >= 0;
+            }
             chai.assert(messageMatch,`Pattern not found in revert reason. Expected Pattern = ${pattern}; Reason Given = ${error.message}`);
-        });
+        })
     }
 }
