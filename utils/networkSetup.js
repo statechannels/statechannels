9c16f9a43db255ec02e3b19c9b658a949fd35e5a
Alex Gap <alex.gap@gmail.com>
2018-12-05 17:17:08 -0800

    changed networkSetup to get functions to prevent ethers issue on load

--- utils/networkSetup.js
+++ utils/networkSetup.js
@@ -1,15 +1,25 @@
-const ethers = require('ethers');
 require("dotenv").config();
 
 const privateKeyWithEth = "0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d";
-const ganacheProvider = new ethers.providers.JsonRpcProvider(`http://${process.env.DEV_GANACHE_HOST}:${process.env.DEV_GANACHE_PORT}`);
+
 
 module.exports = {
     // the following private key is funded with 1 million eth in the startGanache function
-    ganacheProvider: ganacheProvider,
-    privateKeyWithEth: privateKeyWithEth,
-    walletWithEthAndProvider: new ethers.Wallet(privateKeyWithEth, ganacheProvider),
+    getGanacheProvider: function () {
+        const ethers = require('ethers');
+        return new ethers.providers.JsonRpcProvider(`http://${process.env.DEV_GANACHE_HOST}:${process.env.DEV_GANACHE_PORT}`);
+    },
+    getPrivateKeyWithEth: function () {
+        return privateKeyWithEth;
+    },
+    getWalletWithEthAndProvider: function () {
+        const ethers = require('ethers');
+        const ganacheProvider = new ethers.providers.JsonRpcProvider(`http://${process.env.DEV_GANACHE_HOST}:${process.env.DEV_GANACHE_PORT}`);
+        return new ethers.Wallet(privateKeyWithEth, ganacheProvider);
+    },
     getNetworkId: async () => {
+        const ethers = require('ethers');
+        const ganacheProvider = new ethers.providers.JsonRpcProvider(`http://${process.env.DEV_GANACHE_HOST}:${process.env.DEV_GANACHE_PORT}`);
         return (await ganacheProvider.getNetwork()).chainId;
     }
 }
