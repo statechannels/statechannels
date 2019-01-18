60b3f1ee38a1ecb575dc58e48ace63a390282f22
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-01-18 13:39:39 -0700

    Fix test

--- new-packages/nitro-protocol/packages/fmg-core/src/test/state.test.ts
+++ new-packages/nitro-protocol/packages/fmg-core/src/test/state.test.ts
@@ -77,7 +77,7 @@ describe('State', () => {
     const chainId: string = await testStateLib.channelId(state.asEthersObject);
     const localId: string = channel.id;
 
-    expect(chainId.toLowerCase()).toEqual(localId);
+    expect(chainId).toEqual(localId);
   });
 
   it('can check if a state is signed', async () => {
