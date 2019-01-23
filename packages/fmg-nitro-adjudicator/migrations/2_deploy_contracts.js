cb10d2340e1aabd92a15e4cdb412333ebfb34574
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-01-23 16:03:58 -0700

    Add consensus game

--- packages/fmg-nitro-adjudicator/migrations/2_deploy_contracts.js
+++ packages/fmg-nitro-adjudicator/migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var State = artifacts.require('./State.sol');
 var Rules = artifacts.require('./Rules.sol');
-var CountingState = artifacts.require('./CountingState.sol');
-var CountingGame = artifacts.require('./CountingGame.sol');
+var ConsensusState = artifacts.require('./ConsensusState.sol');
+var ConsensusGame = artifacts.require('./ConsensusGame.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(State);
@@ -9,11 +9,11 @@ module.exports = function(deployer) {
   deployer.link(State, Rules);
   deployer.deploy(Rules);
 
-  deployer.link(State, CountingState);
-  deployer.deploy(CountingState);
+  deployer.link(State, ConsensusState);
+  deployer.deploy(ConsensusState);
 
-  deployer.link(State, CountingGame);
-  deployer.link(Rules, CountingGame);
-  deployer.link(CountingState, CountingGame);
-  deployer.deploy(CountingGame);
+  deployer.link(State, ConsensusGame);
+  deployer.link(Rules, ConsensusGame);
+  deployer.link(ConsensusState, ConsensusGame);
+  deployer.deploy(ConsensusGame);
 };
