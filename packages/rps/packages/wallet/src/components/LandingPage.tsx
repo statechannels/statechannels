fe49d963c227afa269e1fddfa8d7261795958fcb
kerzhner <1062379+kerzhner@users.noreply.github.com>
2019-01-24 07:57:44 -0700

    Merge pull request #52 from magmo/ag-wallet-landing-page
    Added simple landing page for the wallet.

---
+++ packages/rps/packages/wallet/src/components/LandingPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import walletIcon from '../images/wallet_icon.svg';
+
+export default class LandingPage extends React.PureComponent {
+
+  render() {
+
+    return (
+      <div className="pt-5 container-fluid pagination-centered text-center">
+        <h1>Welcome to the Magmo Wallet!</h1>
+        <img className="p-3" src={walletIcon} />
+        <p>The Magmo wallet cannot be directly accessed from here.</p>
+        <p>Check out some of the applications that use the Magmo wallet:</p>
+        <p><a href="https://demo.magmo.com">Rock Paper Scissors (by Magmo)</a></p>
+      </div>
+    );
+  }
+}
