47293aefd3c301d41baebdc5d790c6ce66493901
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-01-24 09:58:22 -0700

    Add counting game back to migrations

--- new-packages/nitro-protocol/packages/fmg-nitro-adjudicator/migrations/2_deploy_contracts.js
+++ new-packages/nitro-protocol/packages/fmg-nitro-adjudicator/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var State = artifacts.require('./State.sol');
 var Rules = artifacts.require('./Rules.sol');
+var CountingState = artifacts.require('./CountingState.sol');
+var CountingGame = artifacts.require('./CountingGame.sol');
 var ConsensusState = artifacts.require('./ConsensusState.sol');
 var ConsensusGame = artifacts.require('./ConsensusGame.sol');
 
@@ -8,6 +10,14 @@ module.exports = function(deployer) {
 
   deployer.link(State, Rules);
   deployer.deploy(Rules);
+
+  deployer.link(State, CountingState);
+  deployer.deploy(CountingState);
+
+  deployer.link(State, CountingGame);
+  deployer.link(Rules, CountingGame);
+  deployer.link(CountingState, CountingGame);
+  deployer.deploy(CountingGame);
 
   deployer.link(State, ConsensusState);
   deployer.deploy(ConsensusState);
