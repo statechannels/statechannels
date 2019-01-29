7eec65aa69f99b5c2fc3ce4c098a55ba9c145ac6
Mike Kerzhner <mikerzhner@gmail.com>
2019-01-28 17:31:10 -0700

    Adding ropsten config

--- packages/wallet/packages/tictactoe/truffle.js
+++ packages/wallet/packages/tictactoe/truffle.js
@@ -1,6 +1,8 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config()
+
 // We only want this loaded in a test environment
 // so we can load and parse this file from the app
-require('dotenv').config()
 
 if (process.env.NODE_ENV === 'test') {
   require('ts-node/register');
@@ -16,7 +18,31 @@ module.exports = {
       network_id: "*",
       gas: 6721975,
       gasPrice: 20000000,
-    }
+    },
+    main: {
+      provider: () => new HDWalletProvider(process.env.ETH_ACCOUNT_MNENOMIC, "https://infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: process.env.DEFAULT_GAS,
+      gasPrice: process.env.DEFAULT_GAS_PRICE,
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.ETH_ACCOUNT_MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: process.env.DEFAULT_GAS,
+      gasPrice: process.env.DEFAULT_GAS_PRICE,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.ETH_ACCOUNT_MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: process.env.DEFAULT_GAS,
+      gasPrice: process.env.DEFAULT_GAS_PRICE,
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.ETH_ACCOUNT_MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 42,
+      gas: process.env.DEFAULT_GAS,
+      gasPrice: process.env.DEFAULT_GAS_PRICE,
+    },
   },
   solc: {
     optimizer: {
