d8246bed44cf4a574acef98380909620d36e0c3f
George C. Knee <georgeknee@googlemail.com>
2019-01-30 16:35:23 +0000

    Merge pull request #115 from magmo/gk-ttt-building-with-predeployed-artifacts
    A bunch of work getting `yarn build` to work in ttt

--- packages/rps/packages/tictactoe/migrations/2_deploy_contracts.js
+++ packages/rps/packages/tictactoe/migrations/2_deploy_contracts.js
@@ -2,8 +2,6 @@ var TicTacToeState = artifacts.require("TicTacToeState");
 var TicTacToeHelpers = artifacts.require("TicTacToeHelpers");
 var TicTacToeGame = artifacts.require("TicTacToeGame");
 var State = artifacts.require("fmg-core/contracts/State.sol");
-var Rules = artifacts.require("fmg-core/contracts/Rules.sol");
-var SimpleAdjudicator = artifacts.require("fmg-simple-adjudicator/contracts/SimpleAdjudicator.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(State);
@@ -18,10 +16,4 @@ module.exports = function(deployer) {
   deployer.link(TicTacToeHelpers,TicTacToeGame)
   deployer.deploy(TicTacToeGame);
 
-  deployer.link(State, Rules);
-  deployer.deploy(Rules);
-  // Linking these without deploying doesn't seem to update the bytecode
-  // so we probably don't have to bother doing it.
-  deployer.link(State, SimpleAdjudicator);
-  deployer.link(Rules, SimpleAdjudicator);
 };
