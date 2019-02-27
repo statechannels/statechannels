44335d6b3b2811f4f47e643580098aa62893e88b
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-02-27 07:58:25 -0700

    Clean up utils

--- new-packages/nitro-protocol/packages/fmg-core/src/utils.ts
+++ new-packages/nitro-protocol/packages/fmg-core/src/utils.ts
@@ -1,31 +1,9 @@
 import Web3 from 'web3';
-import { MessageSignature, Signature, Uint256 } from './types';
+import { Signature, Uint256 } from './types';
 import { bigNumberify } from 'ethers/utils';
-
-// TODO: write some jest tests for utils.
-
-export function toHex32(num) {
-  return '0x' + toPaddedHexString(num, 64);
-}
-
-export function padBytes32(data) {
-  const l = 66 - data.length;
-  let x = data.substr(2, data.length);
-
-  for (let i = 0; i < l; i++) {
-    x = 0 + x;
-  }
-  return '0x' + x;
-}
 
 export function toUint256(num: number): Uint256 {
   return bigNumberify(num).toHexString();
-}
-
-// https://stackoverflow.com/a/42203200
-export function toPaddedHexString(num, len) {
-  const str = num.toString(16);
-  return '0'.repeat(len - str.length) + str;
 }
 
 export enum SolidityType {
@@ -50,10 +28,9 @@ export class SolidityParameter {
 
 export type SignableData = string | SolidityParameter | SolidityParameter[];
 
-
-export function sign(data: SignableData, privateKey): MessageSignature {
+export function sign(data: SignableData, privateKey): Signature {
   const localWeb3 = new Web3('');
-  return localWeb3.eth.accounts.sign(hash(data), privateKey) as MessageSignature;
+  return localWeb3.eth.accounts.sign(hash(data), privateKey);
 }
 
 export function recover(data: SignableData, signature: Signature): string {
