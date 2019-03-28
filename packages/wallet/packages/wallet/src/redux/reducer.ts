4bd4da68237528fb180c3698daa866c74d14a9fe
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-03-28 09:57:15 -0600

    Merge pull request #264 from magmo/as-clear-outbox
    Extract outbox clearing logic

--- packages/wallet/packages/wallet/src/redux/reducer.ts
+++ packages/wallet/packages/wallet/src/redux/reducer.ts
@@ -4,6 +4,7 @@ import { initializingReducer } from './initializing/reducer';
 import * as actions from './actions';
 import { unreachable } from '../utils/reducer-utils';
 import { initializedReducer } from './initialized/reducer';
+import { clearOutbox } from './outbox/reducer';
 
 const initialState = waitForLogin();
 
@@ -11,25 +12,8 @@ export const walletReducer = (
   state: WalletState = initialState,
   action: actions.WalletAction,
 ): WalletState => {
-  const nextOutbox = { ...state.outboxState };
-  if (action.type === actions.MESSAGE_SENT) {
-    nextOutbox.messageOutbox = nextOutbox.messageOutbox.slice(1);
-  }
-  if (action.type === actions.DISPLAY_MESSAGE_SENT) {
-    nextOutbox.displayOutbox = nextOutbox.displayOutbox.slice(1);
-  }
-  if (action.type === actions.TRANSACTION_SENT_TO_METAMASK) {
-    // TODO: Should this be a channel message?
-    nextOutbox.transactionOutbox = nextOutbox.transactionOutbox.slice(1);
-  }
+  const nextState = { ...state, outboxState: clearOutbox(state.outboxState, action) };
 
-  if (action.type.match('WALLET.INTERNAL')) {
-    // For the moment, only one action should ever be put in the actionOutbox,
-    // so it's always safe to clear it.
-    nextOutbox.actionOutbox = nextOutbox.actionOutbox.slice(1);
-  }
-
-  const nextState = { ...state, outboxState: nextOutbox };
   switch (nextState.stage) {
     case INITIALIZING:
       return initializingReducer(nextState, action);
