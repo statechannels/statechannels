a4cfbbaa5726a9444ee3a491f3cc5d88f4d629cc
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-04-02 16:34:35 -0600

    Merge pull request #290 from magmo/ag-player-a-states
    Indirect Funding Player A states

---
+++ packages/wallet/packages/wallet/src/redux/indirectFunding/playerA/state.ts
@@ -0,0 +1,76 @@
+import { Properties } from '../../utils';
+import { PlayerIndex } from '../../types';
+
+export const WAIT_FOR_APPROVAL = 'WAIT_FOR_APPROVAL';
+export const WAIT_FOR_PRE_FUND_SETUP_1 = 'WAIT_FOR_PRE_FUND_SETUP_1';
+export const WAIT_FOR_DIRECT_FUNDING = 'WAIT_FOR_DIRECT_FUNDING';
+export const WAIT_FOR_POST_FUND_SETUP_1 = 'WAIT_FOR_POST_FUND_SETUP_1';
+export const WAIT_FOR_LEDGER_UPDATE_1 = 'WAIT_FOR_LEDGER_UPDATE_1';
+
+interface BasePlayerAState {
+  channelId: string;
+  player: PlayerIndex.A;
+}
+
+interface LedgerChannelExists extends BasePlayerAState {
+  ledgerId: string;
+}
+
+export interface WaitForApproval extends BasePlayerAState {
+  type: typeof WAIT_FOR_APPROVAL;
+}
+
+export interface WaitForPreFundSetup1 extends LedgerChannelExists {
+  type: typeof WAIT_FOR_PRE_FUND_SETUP_1;
+}
+
+export interface WaitForDirectFunding extends LedgerChannelExists {
+  type: typeof WAIT_FOR_DIRECT_FUNDING;
+}
+export interface WaitForPostFundSetup1 extends LedgerChannelExists {
+  type: typeof WAIT_FOR_POST_FUND_SETUP_1;
+}
+export interface WaitForLedgerUpdate1 extends LedgerChannelExists {
+  type: typeof WAIT_FOR_LEDGER_UPDATE_1;
+}
+
+export type PlayerAState =
+  | WaitForApproval
+  | WaitForPreFundSetup1
+  | WaitForDirectFunding
+  | WaitForPostFundSetup1
+  | WaitForLedgerUpdate1;
+
+export function waitForApproval(params: Properties<WaitForApproval>): WaitForApproval {
+  const { channelId } = params;
+
+  return { type: WAIT_FOR_APPROVAL, player: PlayerIndex.A, channelId };
+}
+
+export function waitForPreFundSetup1(
+  params: Properties<WaitForPreFundSetup1>,
+): WaitForPreFundSetup1 {
+  const { channelId, ledgerId } = params;
+  return { type: WAIT_FOR_PRE_FUND_SETUP_1, player: PlayerIndex.A, channelId, ledgerId };
+}
+
+export function waitForDirectFunding(
+  params: Properties<WaitForDirectFunding>,
+): WaitForDirectFunding {
+  const { channelId, ledgerId } = params;
+  return { type: WAIT_FOR_DIRECT_FUNDING, player: PlayerIndex.A, channelId, ledgerId };
+}
+
+export function waitForPostFundSetup1(
+  params: Properties<WaitForPostFundSetup1>,
+): WaitForPostFundSetup1 {
+  const { channelId, ledgerId } = params;
+  return { type: WAIT_FOR_POST_FUND_SETUP_1, player: PlayerIndex.A, channelId, ledgerId };
+}
+
+export function waitForLedgerUpdate1(
+  params: Properties<WaitForLedgerUpdate1>,
+): WaitForLedgerUpdate1 {
+  const { channelId, ledgerId } = params;
+  return { type: WAIT_FOR_LEDGER_UPDATE_1, player: PlayerIndex.A, channelId, ledgerId };
+}
