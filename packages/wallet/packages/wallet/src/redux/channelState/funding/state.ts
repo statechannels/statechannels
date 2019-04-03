09c2f698ba4b1b3534999574a0f976aa6d9fed5a
tomclose <tom.close@cantab.net>
2019-04-03 01:13:16 -0700

    Merge pull request #287 from magmo/ledger-consensus
    channelState funding reducer for both ledger and application channels

--- packages/wallet/packages/wallet/src/redux/channelState/funding/state.ts
+++ packages/wallet/packages/wallet/src/redux/channelState/funding/state.ts
@@ -11,7 +11,6 @@ export const FUNDING = 'FUNDING';
 // STATE TYPES
 // Funding setup
 export const WAIT_FOR_FUNDING_REQUEST = 'CHANNEL.WAIT_FOR_FUNDING_REQUEST';
-export const WAIT_FOR_FUNDING_APPROVAL = 'CHANNEL.WAIT_FOR_FUNDING_APPROVAL';
 
 // Funding ongoing
 export const WAIT_FOR_FUNDING_AND_POST_FUND_SETUP = 'WAIT_FOR_FUNDING_AND_POST_FUND_SETUP';
@@ -19,17 +18,8 @@ export const WAIT_FOR_FUNDING_CONFIRMATION = 'WAIT_FOR_FUNDING_CONFIRMATION';
 export const A_WAIT_FOR_POST_FUND_SETUP = 'A_WAIT_FOR_POST_FUND_SETUP';
 export const B_WAIT_FOR_POST_FUND_SETUP = 'B_WAIT_FOR_POST_FUND_SETUP';
 
-// Possible end states
-export const ACKNOWLEDGE_FUNDING_SUCCESS = 'ACKNOWLEDGE_FUNDING_SUCCESS';
-export const ACKNOWLEDGE_FUNDING_DECLINED = 'ACKNOWLEDGE_FUNDING_DECLINED';
-
 export interface WaitForFundingRequest extends ChannelOpen {
   type: typeof WAIT_FOR_FUNDING_REQUEST;
-  stage: typeof FUNDING;
-}
-
-export interface WaitForFundingApproval extends ChannelOpen {
-  type: typeof WAIT_FOR_FUNDING_APPROVAL;
   stage: typeof FUNDING;
 }
 
@@ -53,27 +43,9 @@ export interface BWaitForPostFundSetup extends ChannelOpen {
   stage: typeof FUNDING;
 }
 
-export interface AcknowledgeFundingSuccess extends ChannelOpen {
-  type: typeof ACKNOWLEDGE_FUNDING_SUCCESS;
-  stage: typeof FUNDING;
-}
-
-export interface AcknowledgeFundingDeclined extends ChannelOpen {
-  type: typeof ACKNOWLEDGE_FUNDING_DECLINED;
-  stage: typeof FUNDING;
-}
-
 export function waitForFundingRequest<T extends ChannelOpen>(params: T): WaitForFundingRequest {
   return {
     type: WAIT_FOR_FUNDING_REQUEST,
-    stage: FUNDING,
-    ...channelOpen(params),
-  };
-}
-
-export function approveFunding<T extends ChannelOpen>(params: T): WaitForFundingApproval {
-  return {
-    type: WAIT_FOR_FUNDING_APPROVAL,
     stage: FUNDING,
     ...channelOpen(params),
   };
@@ -103,26 +75,11 @@ export function bWaitForPostFundSetup<T extends ChannelOpen>(params: T): BWaitFo
   return { type: B_WAIT_FOR_POST_FUND_SETUP, stage: FUNDING, ...channelOpen(params) };
 }
 
-export function acknowledgeFundingSuccess<T extends ChannelOpen>(
-  params: T,
-): AcknowledgeFundingSuccess {
-  return { type: ACKNOWLEDGE_FUNDING_SUCCESS, stage: FUNDING, ...channelOpen(params) };
-}
-
-export function acknowledgeFundingDeclined<T extends ChannelOpen>(
-  params: T,
-): AcknowledgeFundingDeclined {
-  return { type: ACKNOWLEDGE_FUNDING_DECLINED, stage: FUNDING, ...channelOpen(params) };
-}
-
 export type FundingState =
   | WaitForFundingRequest
-  | WaitForFundingApproval
   | WaitForFundingAndPostFundSetup
   | WaitForFundingConfirmation
   // ^^^ PlayerA should never let PlayerB get into this state, as it lets PlayerB move to the application phase
   // with no real value at stake, giving them a chance to take the real value that's backing PlayerA's stake.
   | AWaitForPostFundSetup
-  | BWaitForPostFundSetup
-  | AcknowledgeFundingSuccess // <-- at this point, a message might need to be sent
-  | AcknowledgeFundingDeclined;
+  | BWaitForPostFundSetup;
