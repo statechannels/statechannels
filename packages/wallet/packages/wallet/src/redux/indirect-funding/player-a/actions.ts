5bc7e59112a97388d97457d3ebaddc3f38dbb8f3
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-04-03 09:44:39 -0600

    Merge pull request #302 from magmo/clean-up-action-signature
    Don't pass procedure in, as it is constant

--- packages/wallet/packages/wallet/src/redux/indirect-funding/player-a/actions.ts
+++ packages/wallet/packages/wallet/src/redux/indirect-funding/player-a/actions.ts
@@ -2,25 +2,18 @@ import { WalletProcedure } from '../../types';
 import { Commitment } from 'fmg-core/lib/commitment';
 
 export const STRATEGY_APPROVED = 'WALLET.INDIRECT_FUNDING.STRATEGY_APPROVED';
-export const strategyApproved = (
-  channelId: string,
-  procedure: WalletProcedure.IndirectFunding,
-) => ({
+export const strategyApproved = (channelId: string) => ({
   type: STRATEGY_APPROVED as typeof STRATEGY_APPROVED,
   channelId,
-  procedure,
+  procedure: WalletProcedure.IndirectFunding as WalletProcedure.IndirectFunding,
 });
 export type StrategyApproved = ReturnType<typeof strategyApproved>;
 
 export const ALLOCATION_CHANGED = 'WALLET.INDIRECT_FUNDING.ALLOCATION_CHANGED';
-export const allocationChanged = (
-  channelId: string,
-  procedure: WalletProcedure.IndirectFunding,
-  commitment: Commitment,
-) => ({
+export const allocationChanged = (channelId: string, commitment: Commitment) => ({
   type: ALLOCATION_CHANGED as typeof ALLOCATION_CHANGED,
   channelId,
-  procedure,
+  procedure: WalletProcedure.IndirectFunding as WalletProcedure.IndirectFunding,
   commitment,
 });
 export type AllocationChanged = ReturnType<typeof allocationChanged>;
--- packages/wallet/packages/wallet/src/redux/indirect-funding/player-b/actions.ts
+++ packages/wallet/packages/wallet/src/redux/indirect-funding/player-b/actions.ts
@@ -2,25 +2,18 @@ import { WalletProcedure } from '../../types';
 import { Commitment } from 'fmg-core/lib/commitment';
 
 export const STRATEGY_PROPOSED = 'WALLET.INDIRECT_FUNDING.STRATEGY_PROPOSED';
-export const strategyProposed = (
-  channelId: string,
-  procedure: WalletProcedure.IndirectFunding,
-) => ({
+export const strategyProposed = (channelId: string) => ({
   type: STRATEGY_PROPOSED as typeof STRATEGY_PROPOSED,
   channelId,
-  procedure,
+  procedure: WalletProcedure.IndirectFunding as WalletProcedure.IndirectFunding,
 });
 export type StrategyProposed = ReturnType<typeof strategyProposed>;
 
 export const ALLOCATION_CHANGE_REQUESTED = 'WALLET.INDIRECT_FUNDING.ALLOCATION_CHANGE_REQUESTED';
-export const allocationChangeRequested = (
-  channelId: string,
-  procedure: WalletProcedure.IndirectFunding,
-  commitment: Commitment,
-) => ({
+export const allocationChangeRequested = (channelId: string, commitment: Commitment) => ({
   type: ALLOCATION_CHANGE_REQUESTED as typeof ALLOCATION_CHANGE_REQUESTED,
   channelId,
-  procedure,
+  procedure: WalletProcedure.IndirectFunding as WalletProcedure.IndirectFunding,
   commitment,
 });
 export type AllocationChangeRequested = ReturnType<typeof allocationChangeRequested>;
