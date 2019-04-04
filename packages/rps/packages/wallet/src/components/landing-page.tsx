e57c94ee2d470c299c6b79705cea579ca150a3ea
tomclose <tom.close@cantab.net>
2019-04-04 09:18:57 +0100

    Merge pull request #307 from magmo/image-renames
    Kebab case for non typescript files

--- packages/rps/packages/wallet/src/components/landing-page.tsx
+++ packages/rps/packages/wallet/src/components/landing-page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import walletIcon from '../images/orange_fireball.svg';
+import walletIcon from '../images/orange-fireball.svg';
 
 export default class LandingPage extends React.PureComponent {
   render() {
