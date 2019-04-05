cff88b062bcce57b3aa5c00606c7416a5ca6e7ae
tomclose <tom.close@cantab.net>
2019-04-05 11:47:18 +0100

    Merge pull request #297 from magmo/ag-player-a-reducer
    Add Player A reducer logic.

--- packages/rps/packages/wallet/src/redux/utils.ts
+++ packages/rps/packages/wallet/src/redux/utils.ts
@@ -1,7 +1,9 @@
 import { SideEffects } from './outbox/state';
 
-// Constructs a type that must include all properties of T apart from 'type' and 'stage'
-export type Properties<T> = Pick<T, Exclude<keyof T, 'type' | 'stage'>> & { [x: string]: any };
+// Constructs a type that must include all properties of T apart from 'type', 'stage', and 'player'
+export type Properties<T> = Pick<T, Exclude<keyof T, 'type' | 'stage' | 'player'>> & {
+  [x: string]: any;
+};
 
 export interface StateWithSideEffects<T> {
   state: T;
