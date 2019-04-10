0866ab82f68c97a1a27b840b79771b35bfc67c6b
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-04-10 11:34:33 -0600

    Merge pull request #329 from magmo/ag-protocol-reducer-definition
    Add a Protocol Reducer Interface

---
+++ packages/wallet/packages/wallet/src/redux/protocols/index.ts
@@ -0,0 +1,20 @@
+import { OutboxState } from '../outbox/state';
+import { ChannelState } from '../channel-state/state';
+import { IndirectFundingState } from '../indirect-funding/state';
+
+export interface SharedData {
+  channelState: ChannelState;
+  outboxState: OutboxState;
+}
+
+export type ProtocolState = IndirectFundingState;
+
+export type ProtocolReducer<T extends ProtocolState> = (
+  state: ProtocolStateWithSharedData<T>,
+  action,
+) => ProtocolStateWithSharedData<T>;
+
+export interface ProtocolStateWithSharedData<T extends ProtocolState> {
+  protocolState: T;
+  sharedData: SharedData;
+}
