8fb9272b6efe696c5354011abbbd35c9342900cd
tomclose <tom.close@cantab.net>
2019-04-19 09:48:22 +0100

    Merge pull request #380 from magmo/ag-responding-protocol
    Responding Protocol

---
+++ packages/rps/packages/wallet/src/redux/protocols/responding/__tests__/stories.tsx
@@ -0,0 +1,70 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import Modal from 'react-modal';
+import { Provider } from 'react-redux';
+import { fakeStore } from '../../../../__stories__/index';
+import StatusBarLayout from '../../../../components/status-bar-layout';
+import * as scenarios from './scenarios';
+import { Responding } from '../container';
+
+const render = container => () => {
+  // todo: rework this modal stuff
+  return (
+    <Provider store={fakeStore({})}>
+      <Modal
+        isOpen={true}
+        className={'wallet-content-center'}
+        overlayClassName={'wallet-overlay-center'}
+        ariaHideApp={false}
+      >
+        <StatusBarLayout>{container}</StatusBarLayout>
+      </Modal>
+    </Provider>
+  );
+};
+
+const refuteHappyPathStories = {
+  WaitForApproval: scenarios.refuteHappyPath.waitForApproval,
+  WaitForTransaction: scenarios.refuteHappyPath.waitForTransaction,
+  WaitForAcknowledgement: scenarios.refuteHappyPath.waitForAcknowledgement,
+  Success: scenarios.refuteHappyPath.success,
+};
+
+const respondWithExistingMoveHappyPathStories = {
+  WaitForApproval: scenarios.respondWithExistingCommitmentHappyPath.waitForApproval,
+  WaitForTransaction: scenarios.respondWithExistingCommitmentHappyPath.waitForTransaction,
+  WaitForAcknowledgement: scenarios.respondWithExistingCommitmentHappyPath.waitForAcknowledgement,
+  Success: scenarios.respondWithExistingCommitmentHappyPath.success,
+};
+
+const requireResponseHappyPathStories = {
+  WaitForApproval: scenarios.respondWithExistingCommitmentHappyPath.waitForApproval,
+  WaitForResponse: scenarios.requireResponseHappyPath.waitForResponse,
+  WaitForTransaction: scenarios.respondWithExistingCommitmentHappyPath.waitForTransaction,
+  WaitForAcknowledgement: scenarios.respondWithExistingCommitmentHappyPath.waitForAcknowledgement,
+  Success: scenarios.requireResponseHappyPath.success,
+};
+
+const userDeclinesStories = {
+  WaitForApproval: scenarios.userDeclines.waitForApproval,
+  Failure: scenarios.userDeclines.failure,
+};
+
+const transactionFailureStories = {
+  WaitForApproval: scenarios.transactionFails.waitForApproval,
+  WaitForTransaction: scenarios.transactionFails.waitForTransaction,
+  Failure: scenarios.transactionFails.failure,
+};
+
+addStories(respondWithExistingMoveHappyPathStories, 'Responding / Respond with Existing Move');
+addStories(requireResponseHappyPathStories, 'Responding / Requires new Response');
+addStories(refuteHappyPathStories, 'Responding / Refute challenge');
+addStories(transactionFailureStories, 'Responding / Transaction fails');
+addStories(userDeclinesStories, 'Responding / User rejects responding ');
+
+function addStories(collection, chapter) {
+  Object.keys(collection).map(storyName => {
+    const state = collection[storyName];
+    storiesOf(chapter, module).add(storyName, render(<Responding state={state} />));
+  });
+}
---
+++ packages/rps/packages/wallet/src/redux/protocols/responding/container.tsx
@@ -0,0 +1,70 @@
+import * as states from './state';
+import * as actions from './actions';
+import { PureComponent } from 'react';
+import { Commitment } from 'fmg-core/lib/commitment';
+import React from 'react';
+import Success from '../shared-components/success';
+import Failure from '../shared-components/failure';
+import { unreachable } from '../../../utils/reducer-utils';
+import Acknowledge from '../shared-components/acknowledge';
+import WaitForApproval from './components/wait-for-approval';
+import { TransactionSubmission } from '../transaction-submission';
+import { connect } from 'react-redux';
+
+interface Props {
+  state: states.RespondingState;
+  respondApproved: (processId: string) => void;
+  respondRejected: (processId: string) => void;
+  respondSuccessAcknowledged: (processId: string) => void;
+  responseProvided: (processId: string, commitment: Commitment) => void;
+}
+class RespondingContainer extends PureComponent<Props> {
+  render() {
+    const { state, respondSuccessAcknowledged, respondApproved, respondRejected } = this.props;
+    switch (state.type) {
+      case states.WAIT_FOR_ACKNOWLEDGEMENT:
+        return (
+          <Acknowledge
+            title="Response Complete"
+            description="You have successfully responded to the challenge."
+            acknowledge={() => respondSuccessAcknowledged(state.processId)}
+          />
+        );
+      case states.WAIT_FOR_APPROVAL:
+        return (
+          <WaitForApproval
+            approve={() => respondApproved(state.processId)}
+            deny={() => respondRejected(state.processId)}
+          />
+        );
+      case states.WAIT_FOR_RESPONSE:
+        // TODO: Should this ever been seen? We expect protocol above this to figure out getting the response
+        return <div>Waiting for response</div>;
+      case states.WAIT_FOR_TRANSACTION:
+        return (
+          <TransactionSubmission
+            state={state.transactionSubmissionState}
+            transactionName="Respond"
+          />
+        );
+      case states.FAILURE:
+        return <Failure name="respond" reason={state.reason} />;
+      case states.SUCCESS:
+        return <Success name="respond" />;
+      default:
+        return unreachable(state);
+    }
+  }
+}
+
+const mapDispatchToProps = {
+  respondApproved: actions.respondApproved,
+  respondRejected: actions.respondRejected,
+  responseProvided: actions.responseProvided,
+  respondSuccessAcknowledged: actions.respondSuccessAcknowledged,
+};
+
+export const Responding = connect(
+  () => ({}),
+  mapDispatchToProps,
+)(RespondingContainer);
