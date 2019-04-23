f1afee12c1b1c6ea01e65f9b463bcbfe45a7be45
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-04-23 13:06:38 -0600

    Merge pull request #401 from magmo/resolve-issue-358
    player A funding protocol reducer

--- packages/wallet/packages/wallet/src/redux/protocols/funding/player-b/__tests__/scenarios.ts
+++ packages/wallet/packages/wallet/src/redux/protocols/funding/player-b/__tests__/scenarios.ts
@@ -3,15 +3,12 @@ import * as actions from '../actions';
 import { PlayerIndex } from '../../../../types';
 
 import { EMPTY_SHARED_DATA } from '../../../../state';
-import * as walletActions from '../../../../actions';
-import * as walletScenarios from '../../../../__tests__/test-scenarios';
 
 // To test all paths through the state machine we will use 4 different scenarios:
 //
 // 1. Happy path: WaitForStrategyProposal
 //             -> WaitForStrategyApproval
 //             -> WaitForFunding
-//             -> WaitForPostFundSetup
 //             -> WaitForSuccessConfirmation
 //             -> Success
 //
@@ -25,7 +22,6 @@ import * as walletScenarios from '../../../../__tests__/test-scenarios';
 // ---------
 const processId = 'process-id.123';
 const sharedData = EMPTY_SHARED_DATA;
-const { ledgerCommitments } = walletScenarios;
 
 const props = { processId, sharedData, fundingState: 'funding state' as 'funding state' };
 
@@ -35,7 +31,6 @@ const props = { processId, sharedData, fundingState: 'funding state' as 'funding
 const waitForStrategyProposal = states.waitForStrategyProposal(props);
 const waitForStrategyApproval = states.waitForStrategyApproval(props);
 const waitForFunding = states.waitForFunding(props);
-const waitForPostFundSetup = states.waitForPostFundSetup(props);
 const waitForSuccessConfirmation = states.waitForSuccessConfirmation(props);
 const success = states.success();
 const failure = states.failure('User refused');
@@ -46,11 +41,6 @@ const failure2 = states.failure('Opponent refused');
 // -------
 const strategyProposed = actions.strategyProposed(processId);
 const strategyApproved = actions.strategyApproved(processId);
-const postFundSetupArrived = walletActions.commitmentReceived(
-  processId,
-  ledgerCommitments.postFundCommitment0,
-  'Signature',
-);
 const successConfirmed = actions.fundingSuccessAcknowledged(processId);
 const strategyRejected = actions.strategyRejected(processId);
 const cancelledByB = actions.cancelled(processId, PlayerIndex.B);
@@ -65,13 +55,11 @@ export const happyPath = {
   waitForStrategyProposal,
   waitForStrategyApproval,
   waitForFunding,
-  waitForPostFundSetup,
   waitForSuccessConfirmation,
   success,
   // Actions
   strategyProposed,
   strategyApproved,
-  postFundSetupArrived,
   successConfirmed,
 };
 
