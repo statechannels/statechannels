207ce8fef7eff0a19d7834c6f8a64a89ea9f6ac1
Alex Gap <alex@magmo.com>
2019-05-05 14:05:34 -0400

    Merge pull request #459 from magmo/ag-hook-up-funding-protocol
    Add Indirect Funding Sub-protocol to funding protocol

---
+++ packages/rps/packages/wallet/src/redux/protocols/indirect-funding/player-a/__tests__/index.ts
@@ -0,0 +1,9 @@
+import { happyPath, ledgerFundingFails } from './scenarios';
+
+export const initialState = happyPath.waitForPreFundL1.state;
+
+export const preSuccessState = happyPath.waitForPostFund1.state;
+export const successTrigger = happyPath.waitForPostFund1.action;
+
+export const preFailureState = ledgerFundingFails.waitForDirectFunding.state;
+export const failureTrigger = ledgerFundingFails.waitForDirectFunding.action;
---
+++ packages/rps/packages/wallet/src/redux/protocols/indirect-funding/player-b/__tests__/index.ts
@@ -0,0 +1,9 @@
+import { happyPath, ledgerFundingFails } from './scenarios';
+
+export const initialState = happyPath.waitForPreFundSetup0.state;
+
+export const preSuccessState = happyPath.waitForPostFund0.state;
+export const successTrigger = happyPath.waitForPostFund0.action;
+
+export const preFailureState = ledgerFundingFails.waitForDirectFunding.state;
+export const failureTrigger = ledgerFundingFails.waitForDirectFunding.action;
