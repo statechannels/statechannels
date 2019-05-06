7cc389e32ef9c130ea7ae6b5322c85657ef2a0f4
Alex Gap <alex.gap@gmail.com>
2019-05-06 09:15:20 -0700

    Remove Simple Adjudicator

--- new-packages/nitro-protocol/packages/fmg-simple-adjudicator/contracts/loadContractWorkaround.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.2;
-
-// truffle will only create artifacts for contracts from external modules if they
-// are referenced by a contract in the `contracts` or `test` directory. The purpose
-// of this contract is to work around this limitation, by mentioning the contracts
-// that are used in the tests.
-
-import "fmg-core/contracts/test-game/CountingState.sol";
-import "fmg-core/contracts/test-game/CountingGame.sol";
