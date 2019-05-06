a579797a66203942f736e6ca58bb54e1d07957b9
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-06 11:57:06 -0600

    Update CHANNEL_EXISTS error message

--- src/wallet/errors.ts
+++ src/wallet/errors.ts
@@ -1,4 +1,6 @@
-const CHANNEL_EXISTS = new Error('Attempting to open a channel that already exists');
+const CHANNEL_EXISTS = new Error(
+  'Attempting to open a channel that already exists -- use a different nonce',
+);
 const CHANNEL_MISSING = new Error('Channel does not exist');
 const COMMITMENT_NOT_SIGNED = new Error('Commitment not signed by mover');
 const INVALID_TRANSITION = new Error('Invalid transition');
