95f62a998e598104c899c3d4c0f5d1b631c6f679
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-05-07 10:04:54 -0600

    Merge pull request #460 from magmo/clean-up-funding-actions
    Clean up funding actions

--- packages/wallet/packages/wallet/src/redux/protocols/funding/actions.ts
+++ packages/wallet/packages/wallet/src/redux/protocols/funding/actions.ts
@@ -1,37 +1,32 @@
-import { FundingAction as PlayerAFundingAction } from './player-a/actions';
-import { FundingAction as PlayerBFundingAction } from './player-b/actions';
-import * as playerAFundingActions from './player-a/actions';
-import * as playerBFundingActions from './player-b/actions';
-import * as playerA from './player-a/states';
-import * as playerB from './player-b/states';
+import * as playerA from './player-a/actions';
+import * as playerB from './player-b/actions';
 import {
   isIndirectFundingAction,
   Action as IndirectFundingAction,
 } from '../indirect-funding/actions';
 
-export type FundingAction = PlayerAFundingAction | PlayerBFundingAction | IndirectFundingAction;
+type EmbeddedAction = IndirectFundingAction;
+const isEmbeddedAction = isIndirectFundingAction;
 
-export { playerA, playerB };
+export type FundingAction = playerA.FundingAction | playerB.FundingAction | EmbeddedAction;
 
-export function isPlayerAFundingAction(action: FundingAction): action is PlayerAFundingAction {
+export function isPlayerAFundingAction(action: FundingAction): action is playerA.FundingAction {
   return (
-    action.type === playerAFundingActions.CANCELLED ||
-    // TODO: Add this to the check when the action exists action.type === playerAFundingActions.CANCELLED_BY_OPPONENT ||
-    action.type === playerAFundingActions.FUNDING_SUCCESS_ACKNOWLEDGED ||
-    action.type === playerAFundingActions.STRATEGY_APPROVED ||
-    action.type === playerAFundingActions.STRATEGY_CHOSEN ||
-    action.type === playerAFundingActions.STRATEGY_REJECTED ||
-    isIndirectFundingAction(action)
+    action.type === playerA.CANCELLED ||
+    action.type === playerA.FUNDING_SUCCESS_ACKNOWLEDGED ||
+    action.type === playerA.STRATEGY_APPROVED ||
+    action.type === playerA.STRATEGY_CHOSEN ||
+    action.type === playerA.STRATEGY_REJECTED ||
+    isEmbeddedAction(action)
   );
 }
-export function isPlayerBFundingAction(action: FundingAction): action is PlayerBFundingAction {
+export function isPlayerBFundingAction(action: FundingAction): action is playerB.FundingAction {
   return (
-    action.type === playerBFundingActions.CANCELLED ||
-    // TODO: Add this to the check when the action exists action.type === playerAFundingActions.CANCELLED_BY_OPPONENT ||
-    action.type === playerBFundingActions.FUNDING_SUCCESS_ACKNOWLEDGED ||
-    action.type === playerBFundingActions.STRATEGY_APPROVED ||
-    action.type === playerBFundingActions.STRATEGY_PROPOSED ||
-    action.type === playerBFundingActions.STRATEGY_REJECTED ||
-    isIndirectFundingAction(action)
+    action.type === playerB.CANCELLED ||
+    action.type === playerB.FUNDING_SUCCESS_ACKNOWLEDGED ||
+    action.type === playerB.STRATEGY_APPROVED ||
+    action.type === playerB.STRATEGY_PROPOSED ||
+    action.type === playerB.STRATEGY_REJECTED ||
+    isEmbeddedAction(action)
   );
 }
