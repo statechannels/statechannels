f42189b1fe4151a66c143a959fede26cfd27206f
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-07 12:32:31 -0600

    Sketch out channels route

--- src/app/routes/v2/channels.ts
+++ src/app/routes/v2/channels.ts
@@ -2,6 +2,7 @@ import * as koaBody from 'koa-body';
 import * as Router from 'koa-router';
 
 import { errors } from '../../../wallet';
+import { getProcess } from '../../../wallet/db/queries/walletProcess';
 export const BASE_URL = `/api/v2/channels`;
 
 const router = new Router();
@@ -50,15 +51,46 @@ router.post(`${BASE_URL}`, koaBody(), async ctx => {
 
 export const rpsChannelRoutes = router.routes();
 
-async function isNewProcessAction(action) {
-  return true;
+async function isNewProcessAction(action): Promise<boolean> {
+  if (action.type === 'WALLET.FUNDING.STRATEGY_PROPOSED' || opensAppChannel(action)) {
+    const { processId } = action;
+    const process = await getProcess(processId);
+    if (process) {
+      throw new Error(`Process ${processId} is already running.`);
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
-async function routeToNewProcessInitializer(action) {
-  return true;
+
+async function isProtocolAction(action): Promise<boolean> {
+  if (
+    action.type === 'WALLET.CONCLUDING.CONCLUDE_CHANNEL' ||
+    (action.type === 'WALLET.COMMON.COMMITMENT_RECEIVED' && !opensAppChannel(action))
+  ) {
+    const { processId } = action;
+    const process = await getProcess(processId);
+    if (!process) {
+      throw new Error(`Process ${processId} is not running.`);
+    }
+    return true;
+  }
+
+  return false;
 }
-async function isProtocolAction(action) {
-  return true;
-}
+
 async function routeToProtocolReducer(action) {
+  return true;
+}
+
+async function routeToNewProcessInitializer(action) {
+  switch (action.type) {
+    case 'WALLET.COMMON.COMMITMENT_RECEIVED':
+    case 'WALLET.CONCLUDING.CONCLUDE_CHANNEL':
+  }
+}
+
+function opensAppChannel(action): boolean {
   return true;
 }
