0192f78c1220b67d21257aeae2d3d12d3ccc23d2
tomclose <tom.close@cantab.net>
2019-05-09 08:58:02 +0100

    Merge pull request #472 from magmo/ag-ledger-defunding-update
    Update Ledger De-funding to work with Consensus App

--- packages/rps/packages/wallet/src/redux/protocols/indirect-defunding/__tests__/reducer.test.ts
+++ packages/rps/packages/wallet/src/redux/protocols/indirect-defunding/__tests__/reducer.test.ts
@@ -1,9 +1,139 @@
-// TODO: Update for new consensus game
+import * as scenarios from './scenarios';
+import { IndirectDefundingState } from '../state';
+import { ProtocolStateWithSharedData } from '../..';
+import { getLastMessage } from '../../../state';
+import { SignedCommitment } from '../../../../domain';
+import * as states from '../state';
+import { initialize, indirectDefundingReducer } from '../reducer';
 
-it.skip('dummy', () => {
-  expect(true).toBe(true);
+describe('player A happy path', () => {
+  const scenario = scenarios.playerAHappyPath;
+  const {
+    processId,
+    channelId,
+    ledgerId,
+    store,
+    proposedAllocation,
+    proposedDestination,
+  } = scenario.initialParams;
+
+  describe('when initializing', () => {
+    const result = initialize(
+      processId,
+      channelId,
+      ledgerId,
+      proposedAllocation,
+      proposedDestination,
+      store,
+    );
+    itTransitionsTo(result, states.WAIT_FOR_LEDGER_UPDATE);
+    itSendsMessage(result, scenario.initialParams.reply);
+  });
+
+  describe('when in WaitForLedgerUpdate', () => {
+    const { state, action } = scenario.waitForLedgerUpdate;
+    const updatedState = indirectDefundingReducer(state.state, state.store, action);
+    itTransitionsTo(updatedState, states.SUCCESS);
+  });
 });
 
+describe('player A invalid commitment', () => {
+  const scenario = scenarios.playerAInvalidCommitment;
+
+  describe('when in WaitForLedgerUpdate', () => {
+    const { state, action } = scenario.waitForLedgerUpdate;
+    const updatedState = indirectDefundingReducer(state.state, state.store, action);
+    itTransitionsTo(updatedState, states.FAILURE);
+  });
+});
+
+describe('player B happy path', () => {
+  const scenario = scenarios.playerBHappyPath;
+  const {
+    processId,
+    channelId,
+    ledgerId,
+    store,
+    proposedAllocation,
+    proposedDestination,
+  } = scenario.initialParams;
+
+  describe('when initializing', () => {
+    const result = initialize(
+      processId,
+      channelId,
+      ledgerId,
+      proposedAllocation,
+      proposedDestination,
+      store,
+    );
+    itTransitionsTo(result, states.WAIT_FOR_LEDGER_UPDATE);
+  });
+
+  describe('when in WaitForLedgerUpdate', () => {
+    const { state, action, reply } = scenario.waitForLedgerUpdate;
+    const updatedState = indirectDefundingReducer(state.state, state.store, action);
+    itTransitionsTo(updatedState, states.SUCCESS);
+    itSendsMessage(updatedState, reply);
+  });
+});
+
+describe('player B invalid commitment', () => {
+  const scenario = scenarios.playerBInvalidCommitment;
+
+  describe('when in WaitForLedgerUpdate', () => {
+    const { state, action } = scenario.waitForLedgerUpdate;
+    const updatedState = indirectDefundingReducer(state.state, state.store, action);
+    itTransitionsTo(updatedState, states.FAILURE);
+  });
+});
+
+describe('not defundable', () => {
+  const scenario = scenarios.notDefundable;
+  const {
+    processId,
+    channelId,
+    ledgerId,
+    store,
+    proposedAllocation,
+    proposedDestination,
+  } = scenario.initialParams;
+  describe('when initializing', () => {
+    const result = initialize(
+      processId,
+      channelId,
+      ledgerId,
+      proposedAllocation,
+      proposedDestination,
+      store,
+    );
+    itTransitionsTo(result, states.FAILURE);
+  });
+});
+
+type ReturnVal = ProtocolStateWithSharedData<IndirectDefundingState>;
+function itTransitionsTo(state: ReturnVal, type: IndirectDefundingState['type']) {
+  it(`transitions protocol state to ${type}`, () => {
+    expect(state.protocolState.type).toEqual(type);
+  });
+}
+
+function itSendsMessage(state: ReturnVal, message: SignedCommitment) {
+  it('sends a message', () => {
+    const lastMessage = getLastMessage(state.sharedData);
+    if (lastMessage && 'messagePayload' in lastMessage) {
+      const dataPayload = lastMessage.messagePayload.data;
+      // This is yuk. The data in a message is currently of 'any' type..
+      if (!('signedCommitment' in dataPayload)) {
+        fail('No signedCommitment in the last message.');
+      }
+      const { commitment, signature } = dataPayload.signedCommitment;
+      expect({ commitment, signature }).toEqual(message);
+    } else {
+      fail('No messages in the outbox.');
+    }
+  });
+}
 // import * as states from '../state';
 // import { initialize, indirectDefundingReducer } from '../reducer';
 // import * as scenarios from './scenarios';
