0a353e5654b42ea4003c19e91fd7cb8d24754575
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-14 10:29:13 -0600

    handle STRATEGY_PROPOSED

--- src/app/handlers/handle-ongoing-process-action.ts
+++ src/app/handlers/handle-ongoing-process-action.ts
@@ -1,3 +1,25 @@
+import { communication } from 'magmo-wallet';
+import { errors } from '../../wallet';
+import { getProcess } from '../../wallet/db/queries/walletProcess';
+
 export async function handleOngoingProcessAction(ctx) {
-  return ctx;
+  const { data: action } = ctx.request.body;
+  switch (action.type) {
+    case 'WALLET.COMMON.COMMITMENT_RECEIVED':
+    case 'WALLET.CONCLUDING.CONCLUDE_CHANNEL':
+    case 'WALLET.FUNDING.STRATEGY_APPROVED':
+      return ctx;
+    case 'WALLET.FUNDING.STRATEGY_PROPOSED':
+      const { processId } = action;
+      const process = await getProcess(processId);
+      if (!process) {
+        throw errors.processMissing(processId);
+      }
+
+      const { their_address: theirAddress } = process;
+      ctx.body = communication.sendStrategyApproved(theirAddress, processId);
+      ctx.status = 200;
+
+      return ctx;
+  }
 }
