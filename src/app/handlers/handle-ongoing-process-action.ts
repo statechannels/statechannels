eb108ddae359d11d55cf32db382de5625cc4e70b
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-14 15:20:35 -0600

    Sketch out handling of WALLET.COMMON.COMMITMENT_RECEIVED

--- src/app/handlers/handle-ongoing-process-action.ts
+++ src/app/handlers/handle-ongoing-process-action.ts
@@ -5,11 +5,18 @@ import { getProcess } from '../../wallet/db/queries/walletProcess';
 export async function handleOngoingProcessAction(ctx) {
   const action = ctx.request.body;
   switch (action.type) {
-    case 'WALLET.COMMON.COMMITMENT_RECEIVED':
     case 'WALLET.CONCLUDING.CONCLUDE_CHANNEL':
     case 'WALLET.FUNDING.STRATEGY_APPROVED':
       return ctx;
-    case 'WALLET.FUNDING.STRATEGY_PROPOSED':
+    case 'WALLET.COMMON.COMMITMENT_RECEIVED': {
+      const { processId } = action;
+      const process = await getProcess(processId);
+      if (!process) {
+        throw errors.processMissing(processId);
+      }
+      return ctx;
+    }
+    case 'WALLET.FUNDING.STRATEGY_PROPOSED': {
       const { processId } = action;
       const process = await getProcess(processId);
       if (!process) {
@@ -21,5 +28,6 @@ export async function handleOngoingProcessAction(ctx) {
       ctx.status = 200;
 
       return ctx;
+    }
   }
 }
