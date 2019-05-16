6a1b8fb1555123f3bdf22d64f1f3e4918c6f4913
Mike Kerzhner <mikerzhner@gmail.com>
2019-05-16 15:47:18 -0700

    Run prettier

--- packages/devtools/utils/linkedByteCode.js
+++ packages/devtools/utils/linkedByteCode.js
@@ -1,14 +1,19 @@
 const linker = require("solc/linker");
 
 module.exports = {
-    linkedByteCode: function linkedByteCode(artifact, linkedLibrary, networkId) {
-        const lookup = {};
-        try {
-            lookup[linkedLibrary.contractName] = linkedLibrary.networks[networkId].address;
-        } catch (err) {
-            // tslint:disable-next-line:no-console
-            console.error(linkedLibrary.networks, linkedLibrary.contractName, networkId);
-        }
-        return linker.linkBytecode(artifact.bytecode, lookup);
+  linkedByteCode: function linkedByteCode(artifact, linkedLibrary, networkId) {
+    const lookup = {};
+    try {
+      lookup[linkedLibrary.contractName] =
+        linkedLibrary.networks[networkId].address;
+    } catch (err) {
+      // tslint:disable-next-line:no-console
+      console.error(
+        linkedLibrary.networks,
+        linkedLibrary.contractName,
+        networkId
+      );
     }
-}
+    return linker.linkBytecode(artifact.bytecode, lookup);
+  }
+};
--- packages/devtools/utils/logger.js
+++ packages/devtools/utils/logger.js
@@ -1,22 +1,22 @@
-var fs = require('fs');
+var fs = require("fs");
 
-var Logger = exports.Logger = {};
-var infoStream = fs.createWriteStream('info.log');
-var errorStream = fs.createWriteStream('error.log');
-var debugStream = fs.createWriteStream('debug.log');
+var Logger = (exports.Logger = {});
+var infoStream = fs.createWriteStream("info.log");
+var errorStream = fs.createWriteStream("error.log");
+var debugStream = fs.createWriteStream("debug.log");
 
 Logger.info = function(msg) {
-    infoStream.write(msg + "\n");
+  infoStream.write(msg + "\n");
 };
-  
+
 Logger.log = function(msg) {
-    infoStream.write(msg + "\n");
+  infoStream.write(msg + "\n");
 };
 
 Logger.debug = function(msg) {
-    debugStream.write(msg + "\n");
+  debugStream.write(msg + "\n");
 };
 
 Logger.error = function(msg) {
-    errorStream.write(msg + "\n");
-}
+  errorStream.write(msg + "\n");
+};
