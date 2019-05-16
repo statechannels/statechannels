925b5a50494430f1772812b69a73381ff7d4ba3b
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-16 13:46:48 -0600

    Route ConcludeInstigated to handler

--- src/app/handlers/handle-new-process-action.ts
+++ src/app/handlers/handle-new-process-action.ts
@@ -1,19 +1,24 @@
-import { communication } from 'magmo-wallet';
+import { communication, ConcludeInstigated } from 'magmo-wallet';
 import { unreachable } from 'magmo-wallet';
 
 export async function handleNewProcessAction(ctx) {
-  const { data: action } = ctx.request.body;
+  const action = ctx.request.body;
   if (!communication.isRelayableAction(action)) {
     throw new Error(`Action ${action.type} is not valid`);
   }
 
   switch (action.type) {
     case 'WALLET.COMMON.COMMITMENT_RECEIVED':
-    case 'WALLET.NEW_PROCESS.CONCLUDE_INSTIGATED':
     case 'WALLET.FUNDING.STRATEGY_PROPOSED':
     case 'WALLET.FUNDING.STRATEGY_APPROVED':
       return ctx;
+    case 'WALLET.NEW_PROCESS.CONCLUDE_INSTIGATED':
+      return handleConcludeInstigated(ctx, action);
     default:
       return unreachable(action);
   }
+}
+
+function handleConcludeInstigated(ctx, action: ConcludeInstigated) {
+  return ctx;
 }
