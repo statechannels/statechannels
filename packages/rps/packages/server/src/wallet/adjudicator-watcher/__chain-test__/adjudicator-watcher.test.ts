1c76685d98f433f0ca1710f7ef228a3d718be9c2
Mike Kerzhner <mikerzhner@gmail.com>
2019-05-16 11:57:02 -0700

    Fork adjudicator watcher

--- packages/rps/packages/server/src/wallet/adjudicator-watcher/__chain-test__/adjudicator-watcher.test.ts
+++ packages/rps/packages/server/src/wallet/adjudicator-watcher/__chain-test__/adjudicator-watcher.test.ts
@@ -1,13 +1,13 @@
+import { fork } from 'child_process';
 import { ethers } from 'ethers';
 import { bigNumberify } from 'ethers/utils';
 import { channelID } from 'fmg-core/lib/channel';
-import { listen } from '..';
 import AllocatorChannel from '../../models/allocatorChannel';
 import { channel, depositIntoContract } from './utils';
 
 jest.setTimeout(60000);
 const channelId = channelID(channel);
-let removeListeners = null;
+let killSubprocess = null;
 
 async function getHoldings() {
   return (await AllocatorChannel.query()
@@ -23,20 +23,24 @@ describe('adjudicator listener', () => {
 
   it('should handle a funds received event when channel is in the database', async done => {
     const preEventHoldings = await getHoldings();
+    const adjudicatorWatcher = fork('lib/wallet/adjudicator-watcher');
 
-    const eventCallback = async eventType => {
+    killSubprocess = () => {
+      adjudicatorWatcher.kill();
+    };
+
+    adjudicatorWatcher.on('message', async message => {
+      console.log(`Parent received message: ${message}`);
       const postEventHoldings = await getHoldings();
-
       const depositedAmount = bigNumberify(postEventHoldings).sub(bigNumberify(preEventHoldings));
       expect(depositedAmount.toNumber()).toBeGreaterThanOrEqual(5);
       done();
-    };
+    });
 
-    removeListeners = await listen(eventCallback);
     await depositIntoContract(provider, channelId);
   });
 });
 
 afterEach(() => {
-  removeListeners();
+  killSubprocess();
 });
