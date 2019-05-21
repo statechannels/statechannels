c9a261663abfdac04901092b0b6eaec098a9206e
Mike Kerzhner <mikerzhner@gmail.com>
2019-05-21 14:34:28 -0700

    Starting on server deposit manager

---
+++ src/wallet/services/depositManager.ts
@@ -0,0 +1,56 @@
+import { BigNumber, bigNumberify } from 'ethers/utils';
+import { Address } from 'fmg-core';
+import { HUB_ADDRESS } from '../../constants';
+import AllocatorChannel from '../models/allocatorChannel';
+import { Blockchain } from './blockchain';
+
+export async function onDepositEvent(
+  channelId: Address,
+  amountDeposited: BigNumber,
+  destinationHoldings: BigNumber,
+) {
+  // TODO: holdings for the channel should be updated.
+
+  // todo: to avoid manual case conversions, we can switch to knexSnakeCaseMappers.
+  // https://vincit.github.io/objection.js/recipes/snake-case-to-camel-case-conversion.html#snake-case-to-camel-case-conversion
+  const channel_id = channelId;
+  const allocatorChannel = await AllocatorChannel.query()
+    .findOne({
+      channel_id,
+    })
+    .eager('[commitments, commitments.allocations, participants]');
+  const commitments = allocatorChannel.commitments;
+  const latestCommitment = commitments.reduce((prevCommitment, currentCommitment) => {
+    return prevCommitment.turnNumber > currentCommitment.turnNumber
+      ? prevCommitment
+      : currentCommitment;
+  });
+
+  const hubParticipatIndex = allocatorChannel.participants
+    .map(participant => participant.address)
+    .indexOf(HUB_ADDRESS);
+
+  const totalNeededInAdjudicator = latestCommitment.allocations
+    .map(allocation => allocation.amount)
+    .reduce((accumulator, currentAmount) => {
+      const sumSoFar = bigNumberify(accumulator);
+      const toAdd = bigNumberify(currentAmount);
+      const sum = sumSoFar.add(toAdd);
+      return sum.toHexString();
+    });
+
+  const channelNeedsMoreFunds = bigNumberify(totalNeededInAdjudicator).gt(
+    bigNumberify(destinationHoldings),
+  );
+
+  if (channelNeedsMoreFunds) {
+    const allocationNeededFromHub = latestCommitment.allocations[hubParticipatIndex];
+    await Blockchain.fund(
+      channelId,
+      destinationHoldings.toHexString(),
+      allocationNeededFromHub.amount,
+    );
+  } else {
+    console.log('Channel is fully funded');
+  }
+}
