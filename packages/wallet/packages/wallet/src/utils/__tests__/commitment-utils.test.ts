3e9838efc44cb3633dfb58f599b2ae750b208fed
Alex Gap <alex@magmo.com>
2019-06-10 14:25:18 -0700

    Merge pull request #521 from magmo/ag-fix-compose-conclude
    Fix composeConcludeCommitment

---
+++ packages/wallet/packages/wallet/src/utils/__tests__/commitment-utils.test.ts
@@ -0,0 +1,32 @@
+import { appCommitment } from '../../domain/commitments/__tests__';
+import { channelFromCommitments } from '../../redux/channel-store/channel-state/__tests__';
+import * as channelScenarios from '../../redux/__tests__/test-scenarios';
+import { composeConcludeCommitment } from '../commitment-utils';
+import { CommitmentType } from 'fmg-core';
+describe('composeConcludeCommitment', () => {
+  const { asPrivateKey, asAddress } = channelScenarios;
+  const app10 = appCommitment({ turnNum: 10, balances: [] });
+  const app11 = appCommitment({ turnNum: 11, balances: [] });
+  const app12 = appCommitment({ turnNum: 11, balances: [], isFinal: true });
+
+  it('should create a correct conclude commitment from an app commitment', () => {
+    const channelState = channelFromCommitments(app10, app11, asAddress, asPrivateKey);
+    const concludeCommitment = composeConcludeCommitment(channelState);
+    expect(concludeCommitment).toMatchObject({
+      ...app11.commitment,
+      commitmentType: CommitmentType.Conclude,
+      turnNum: app11.commitment.turnNum + 1,
+      commitmentCount: 0,
+    });
+  });
+  it('should create a correct conclude commitment from a conclude commitment', () => {
+    const channelState = channelFromCommitments(app11, app12, asAddress, asPrivateKey);
+    const concludeCommitment = composeConcludeCommitment(channelState);
+    expect(concludeCommitment).toMatchObject({
+      ...app11.commitment,
+      commitmentType: CommitmentType.Conclude,
+      turnNum: app12.commitment.turnNum + 1,
+      commitmentCount: 1,
+    });
+  });
+});
