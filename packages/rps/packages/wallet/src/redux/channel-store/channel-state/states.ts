e5a32492c40ae4d54940cca0278423b28f4302f5
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-06-20 16:28:26 -0600

    Merge pull request #533 from magmo/advance-channel-reducer
    WIP: Advance channel reducer

--- packages/rps/packages/wallet/src/redux/channel-store/channel-state/states.ts
+++ packages/rps/packages/wallet/src/redux/channel-store/channel-state/states.ts
@@ -1,4 +1,5 @@
 import { SignedCommitment, getChannelId, Commitment } from '../../../domain';
+import { ethers } from 'ethers';
 
 export type Commitments = SignedCommitment[];
 
@@ -31,11 +32,11 @@ export function getPenultimateCommitment(state: ChannelState): Commitment {
 
 export function initializeChannel(
   signedCommitment: SignedCommitment,
-  address: string,
   privateKey: string,
 ): ChannelState {
   const { commitment } = signedCommitment;
   const { turnNum, channel } = commitment;
+  const address = new ethers.Wallet(privateKey).address;
   const ourIndex = commitment.destination.indexOf(address);
   const channelId = getChannelId(commitment);
   return {
@@ -58,7 +59,11 @@ export function pushCommitment(
   signedCommitment: SignedCommitment,
 ): ChannelState {
   const commitments = [...state.commitments];
-  commitments.shift();
+  const numParticipants = state.participants.length;
+  if (commitments.length === numParticipants) {
+    // We've got a full round of commitments, and should therefore drop the first one
+    commitments.shift();
+  }
   commitments.push(signedCommitment);
   const turnNum = signedCommitment.commitment.turnNum;
   return { ...state, commitments, turnNum };
