8523b1cb8687a24e0c00526421790ede189cb604
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-06-21 09:30:50 -0600

    Merge pull request #538 from magmo/advance-channel-reducer-post-fund
    Advance channel reducer when channel exists

--- packages/rps/packages/wallet/src/redux/protocols/advance-channel/__tests__/reducer.test.ts
+++ packages/rps/packages/wallet/src/redux/protocols/advance-channel/__tests__/reducer.test.ts
@@ -111,6 +111,15 @@ describe('sending preFundSetup as Hub', () => {
     itSendsNoMessage(result);
   });
 
+  describe('when receiving prefund commitments from A', () => {
+    const { state, sharedData, action, commitments } = scenario.receiveFromA;
+    const { protocolState, sharedData: result } = reducer(state, sharedData, action);
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.ChannelUnknown');
+    itStoresThisCommitment(result, commitments[0]);
+    itSendsNoMessage(result);
+  });
+
   describe('when receiving prefund commitments from B', () => {
     const { state, sharedData, action, commitments } = scenario.receiveFromB;
     const { protocolState, sharedData: result } = reducer(state, sharedData, action);
@@ -119,5 +128,109 @@ describe('sending preFundSetup as Hub', () => {
     itStoresThisCommitment(result, commitments[2]);
     expectTheseCommitmentsSent(result, commitments);
     itRegistersThisChannel(result, channelId, processId);
+  });
+});
+
+describe('sending postFundSetup as A', () => {
+  const scenario = scenarios.existingChannelAsA;
+  const { processId, commitmentType } = scenario;
+
+  describe('when initializing', () => {
+    const { sharedData, commitments, args } = scenario.initialize;
+    const { protocolState, sharedData: result } = initialize(
+      processId,
+      sharedData,
+      commitmentType,
+      args,
+    );
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.CommitmentSent');
+    expectTheseCommitmentsSent(result, commitments);
+    itStoresThisCommitment(result, commitments[2]);
+  });
+
+  describe('when receiving postFund commitments from b', () => {
+    const { commitments, state, sharedData, action } = scenario.receiveFromB;
+    const { protocolState, sharedData: result } = reducer(state, sharedData, action);
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.CommitmentSent');
+    itSendsNoMessage(result);
+    itStoresThisCommitment(result, commitments[2]);
+  });
+
+  describe('when receiving postfund commitments from the hub', () => {
+    const { state, sharedData, action, commitments } = scenario.receiveFromHub;
+    const { protocolState, sharedData: result } = reducer(state, sharedData, action);
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.Success');
+    itStoresThisCommitment(result, commitments[2]);
+    itSendsNoMessage(result);
+  });
+});
+
+describe('sending postFundSetup as B', () => {
+  const scenario = scenarios.existingChannelAsB;
+  const { processId, commitmentType } = scenario;
+
+  describe('when initializing', () => {
+    const { sharedData, commitments, args } = scenario.initialize;
+    const { protocolState, sharedData: result } = initialize(
+      processId,
+      sharedData,
+      commitmentType,
+      args,
+    );
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.NotSafeToSend');
+    itSendsNoMessage(result);
+    itStoresThisCommitment(result, commitments[2]);
+  });
+
+  describe('when receiving a PostFund commitment from A', () => {
+    const { commitments, state, sharedData, action } = scenario.receiveFromA;
+    const { protocolState, sharedData: result } = reducer(state, sharedData, action);
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.CommitmentSent');
+    expectTheseCommitmentsSent(result, commitments);
+    itStoresThisCommitment(result, commitments[2]);
+  });
+
+  describe('when receiving postfund commitments from the hub', () => {
+    const { state, sharedData, action, commitments } = scenario.receiveFromHub;
+
+    const { protocolState, sharedData: result } = reducer(state, sharedData, action);
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.Success');
+    itStoresThisCommitment(result, commitments[2]);
+    itSendsNoMessage(result);
+  });
+});
+
+describe('sending postFundSetup as Hub', () => {
+  const scenario = scenarios.existingChannelAsHub;
+  const { processId, commitmentType } = scenario;
+
+  describe('when initializing', () => {
+    const { sharedData, commitments, args } = scenario.initialize;
+    const { protocolState, sharedData: result } = initialize(
+      processId,
+      sharedData,
+      commitmentType,
+      args,
+    );
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.NotSafeToSend');
+    itSendsNoMessage(result);
+    itStoresThisCommitment(result, commitments[2]);
+  });
+
+  describe('when receiving postfund commitments from the hub', () => {
+    const { state, sharedData, action, commitments } = scenario.receiveFromB;
+
+    const { protocolState, sharedData: result } = reducer(state, sharedData, action);
+
+    itTransitionsTo(protocolState, 'AdvanceChannel.Success');
+    itStoresThisCommitment(result, commitments[2]);
+    expectTheseCommitmentsSent(result, commitments);
   });
 });
