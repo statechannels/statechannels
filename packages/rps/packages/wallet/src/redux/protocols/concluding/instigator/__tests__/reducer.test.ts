be2bcb3dbd0eff4fb5fbddb494653e367b92a82a
andrewgordstewart <andrew.gord.stewart@gmail.com>
2019-06-26 12:36:56 -0600

    Merge pull request #553 from magmo/improve-commitment-test-helpers
    Improve commitment test helpers

--- packages/rps/packages/wallet/src/redux/protocols/concluding/instigator/__tests__/reducer.test.ts
+++ packages/rps/packages/wallet/src/redux/protocols/concluding/instigator/__tests__/reducer.test.ts
@@ -5,7 +5,7 @@ import { FailureReason } from '../../states';
 import { SharedData } from '../../../../state';
 import { Commitment } from '../../../../../domain';
 import {
-  expectThisMessageAndCommitmentSent,
+  expectThisCommitmentSent,
   itSendsThisMessage,
   itSendsThisDisplayEventType,
   describeScenarioStep,
@@ -195,11 +195,7 @@ describe('[ Defund failed ]', () => {
 
 function itSendsConcludeInstigated(sharedData: SharedData, commitment: Commitment) {
   it('sends a conclude instigated message with the correct commitment', () => {
-    expectThisMessageAndCommitmentSent(
-      sharedData,
-      commitment,
-      'WALLET.NEW_PROCESS.CONCLUDE_INSTIGATED',
-    );
+    expectThisCommitmentSent(sharedData, commitment, 'WALLET.NEW_PROCESS.CONCLUDE_INSTIGATED');
   });
 }
 
