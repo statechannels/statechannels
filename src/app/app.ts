b29e7b3c65e3d43b0388d664282a781bcbec529a
Mike Kerzhner <mikerzhner@gmail.com>
2019-06-27 15:33:56 -0700

    Add message sender to firebase-relay

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import * as cors from '@koa/cors';
 import * as Koa from 'koa';
+import { sendToOpponent } from '../message/firebase-relay';
 import { logger } from './logging';
 import { indexRoutes } from './routes/index';
 import { ledgerChannelRoutes } from './routes/v1/ledger_channels';
@@ -9,6 +10,17 @@ import { channelRoutes } from './routes/v2/channels';
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  await next();
+  const status = ctx.status;
+  if (status === 200 || status === 201) {
+    const to = ctx.body.to;
+    const messagePayload = ctx.body.messagePayload;
+    if (to && messagePayload) {
+      sendToOpponent(to, messagePayload);
+    }
+  }
+});
 app.use(logger);
 app.use(cors());
 app.use(indexRoutes);
