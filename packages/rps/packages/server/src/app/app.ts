3411d0b2f63f5104d5f3662be8e42f5bca35e4f3
Mike Kerzhner <mikerzhner@gmail.com>
2019-06-27 19:53:59 -0700

    Remvove rps_games route

--- packages/rps/packages/server/src/app/app.ts
+++ packages/rps/packages/server/src/app/app.ts
@@ -1,23 +1,23 @@
 import * as cors from '@koa/cors';
 import * as Koa from 'koa';
-import { sendToOpponent } from '../message/firebase-relay';
+import { send } from '../message/firebase-relay';
 import { logger } from './logging';
 import { indexRoutes } from './routes/index';
 import { ledgerChannelRoutes } from './routes/v1/ledger_channels';
 import { rpsChannelRoutes } from './routes/v1/rps_channels';
-import { rpsGamesRoutes } from './routes/v1/rps_games';
 import { channelRoutes } from './routes/v2/channels';
 
 const app = new Koa();
 
 app.use(async (ctx, next) => {
   await next();
+  // Executed after all other middleware
   const status = ctx.status;
   if (status === 200 || status === 201) {
     const to = ctx.body.to;
     const messagePayload = ctx.body.messagePayload;
     if (to && messagePayload) {
-      sendToOpponent(to, messagePayload);
+      send(to, messagePayload);
     }
   }
 });
@@ -26,7 +26,6 @@ app.use(cors());
 app.use(indexRoutes);
 app.use(ledgerChannelRoutes);
 app.use(rpsChannelRoutes);
-app.use(rpsGamesRoutes);
 app.use(channelRoutes);
 
 export default app;
