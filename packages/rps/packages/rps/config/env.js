11e868f2ba14fdf7432d03ce6982657a4bf7db95
Mike Kerzhner <mikerzhner@gmail.com>
2019-07-09 13:40:38 -0700

    Do not use bespoke dotenv loading in rps package.

--- packages/rps/packages/rps/config/env.js
+++ packages/rps/packages/rps/config/env.js
@@ -3,42 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 const paths = require('./paths');
+const { configureEnvVariables } = require('magmo-devtools');
 
 // Make sure that including paths.js after env.js will read .env variables.
 delete require.cache[require.resolve('./paths')];
 
-const NODE_ENV = process.env.NODE_ENV;
-if (!NODE_ENV) {
-  throw new Error(
-    'The NODE_ENV environment variable is required but was not specified.'
-  );
-}
-
-// https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-var dotenvFiles = [
-  `${paths.dotenv}.${NODE_ENV}.local`,
-  `${paths.dotenv}.${NODE_ENV}`,
-  // Don't include `.env.local` for `test` environment
-  // since normally you expect tests to produce the same
-  // results for everyone
-  NODE_ENV !== 'test' && `${paths.dotenv}.local`,
-  paths.dotenv,
-].filter(Boolean);
-
-// Load environment variables from .env* files. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.  Variable expansion is supported in .env files.
-// https://github.com/motdotla/dotenv
-// https://github.com/motdotla/dotenv-expand
-dotenvFiles.forEach(dotenvFile => {
-  if (fs.existsSync(dotenvFile)) {
-    require('dotenv-expand')(
-      require('dotenv').config({
-        path: dotenvFile,
-      })
-    );
-  }
-});
+configureEnvVariables();
 
 // We support resolving modules according to `NODE_PATH`.
 // This lets you use absolute paths in imports inside large monorepos:
@@ -60,18 +30,26 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 // injected into the application via DefinePlugin in Webpack configuration.
 const REACT_APP = /^REACT_APP_/i;
 // These are Magmo specific values that should be injected into the application
-const MAGMO_VALUES = ['TARGET_NETWORK', 'FIREBASE_PROJECT', 'FIREBASE_API_KEY', 'WALLET_URL', 'BOT_URL', 'SERVER_WALLET_ADDRESS'];
+const MAGMO_VALUES = [
+  'TARGET_NETWORK',
+  'FIREBASE_PROJECT',
+  'FIREBASE_API_KEY',
+  'WALLET_URL',
+  'BOT_URL',
+  'SERVER_WALLET_ADDRESS',
+];
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
     .filter(key => {
-      return REACT_APP.test(key) || MAGMO_VALUES.indexOf(key) > -1
+      return REACT_APP.test(key) || MAGMO_VALUES.indexOf(key) > -1;
     })
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
         return env;
-      }, {
+      },
+      {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
         NODE_ENV: process.env.NODE_ENV || 'development',
@@ -82,20 +60,18 @@ function getClientEnvironment(publicUrl) {
         PUBLIC_URL: publicUrl,
 
         BUILD_VERSION: new Date(Date.now()).toISOString(),
-      }
+      },
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
-    'process.env': Object.keys(raw).reduce(
-      (env, key) => {
-        env[key] = JSON.stringify(raw[key]);
-        return env;
-      }, {}
-    ),
+    'process.env': Object.keys(raw).reduce((env, key) => {
+      env[key] = JSON.stringify(raw[key]);
+      return env;
+    }, {}),
   };
   return {
     raw,
-    stringified
+    stringified,
   };
 }
 
