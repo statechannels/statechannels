a64eb455c7faaabadd4ca9d19e5552b12a7a76be
Mike Kerzhner <mikerzhner@gmail.com>
2019-07-09 14:54:45 -0700

    Add back NODE_ENV to env.js in rps

--- packages/rps/packages/rps/config/env.js
+++ packages/rps/packages/rps/config/env.js
@@ -2,12 +2,12 @@
 
 const fs = require('fs');
 const path = require('path');
-const paths = require('./paths');
 const { configureEnvVariables } = require('magmo-devtools');
 
-// Make sure that including paths.js after env.js will read .env variables.
-delete require.cache[require.resolve('./paths')];
-
+const NODE_ENV = process.env.NODE_ENV;
+if (!NODE_ENV) {
+  throw new Error('The NODE_ENV environment variable is required but was not specified.');
+}
 configureEnvVariables();
 
 // We support resolving modules according to `NODE_PATH`.
@@ -36,7 +36,6 @@ const MAGMO_VALUES = [
   'FIREBASE_API_KEY',
   'WALLET_URL',
   'BOT_URL',
-  'SERVER_WALLET_ADDRESS',
 ];
 
 function getClientEnvironment(publicUrl) {
