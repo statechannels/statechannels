cf36b4bdbae06b26c4139df23a017044df71e52e
George Knee <georgeknee@googlemail.com>
2019-07-09 16:22:14 +0100

    Write new test that passes

---
+++ new-packages/nitro-protocol/packages/fmg-nitro-adjudicator/test/nitro.test.ts
@@ -0,0 +1,34 @@
+import * as ethers from 'ethers';
+import NitroArtifact from '../build/contracts/NitroAdjudicator.json';
+import { AddressZero } from 'ethers/constants';
+jest.setTimeout(20000);
+
+const DEPOSIT_AMOUNT = ethers.utils.parseEther('0'); //
+
+describe('Nitro', () => {
+  let networkId;
+  const provider = new ethers.providers.JsonRpcProvider(
+    `http://localhost:${process.env.DEV_GANACHE_PORT}`,
+  );
+  const signer1 = provider.getSigner(1);
+  let nitro;
+
+  beforeAll(async () => {
+    networkId = (await provider.getNetwork()).chainId;
+    const libraryAddress = NitroArtifact.networks[networkId].address;
+    nitro = new ethers.Contract(libraryAddress, NitroArtifact.abi, signer1);
+  });
+
+  // Transition function tests
+  // ========================
+
+  it('deposits', async () => {
+    // Create a new instance of the Contract with a Signer, which allows
+    // update methods
+    const tx = await nitro.deposit(AddressZero, 0, DEPOSIT_AMOUNT, AddressZero);
+    console.log(tx.hash);
+    await tx.wait();
+    const allocatedAmount = await nitro.holdings(AddressZero, AddressZero);
+    expect(allocatedAmount).toEqual(DEPOSIT_AMOUNT);
+  });
+});
