db306dfe16346778148daf97aa018e49cb1e9161
Mike Kerzhner <mikerzhner@gmail.com>
2019-07-09 12:48:54 -0700

    Look for env variable files in the root of the monoreop

--- config/env.js
+++ config/env.js
@@ -1,26 +1,35 @@
 "use strict";
 
 module.exports = {
-  configureEnvVariables: function() {
+  configureEnvVariables: function(monorepo = true) {
     const fs = require("fs");
+    const path = require("path");
 
     const NODE_ENV = process.env.NODE_ENV;
     if (!NODE_ENV) {
       throw new Error(
-        "The NODE_ENV environment variable is required but was not specified."
+        "The NODE_ENV environment variable is required but was not specified.",
       );
     }
 
     // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-    var dotenvFiles = [
+    let dotenvFiles = [
       `.env.${NODE_ENV}.local`,
       // Don't include `.env.local` for `test` environment
       // since normally you expect tests to produce the same
       // results for everyone
       NODE_ENV !== "test" && `.env.local`,
       `.env.${NODE_ENV}`,
-      ".env"
+      ".env",
     ].filter(Boolean);
+
+    if (monorepo) {
+      const monorepoDotenvFiles = dotenvFiles.slice(0);
+      dotenvFiles.forEach(dotenvFile => {
+        monorepoDotenvFiles.push(path.join("../..", dotenvFile));
+      });
+      dotenvFiles = monorepoDotenvFiles;
+    }
 
     // Load environment variables from .env* files. Suppress warnings using silent
     // if this file is missing. dotenv will never modify any environment variables
@@ -28,13 +37,14 @@ module.exports = {
     // https://github.com/motdotla/dotenv
     // https://github.com/motdotla/dotenv-expand
     dotenvFiles.forEach(dotenvFile => {
+      console.log(`Processing ${dotenvFile}`);
       if (fs.existsSync(dotenvFile)) {
         require("dotenv-expand")(
           require("dotenv").config({
-            path: dotenvFile
-          })
+            path: dotenvFile,
+          }),
         );
       }
     });
-  }
+  },
 };
