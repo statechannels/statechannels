e5f3731c206d60237ab9cca7fca30b5aa7d69c39
George Knee <georgeknee@googlemail.com>
2019-07-10 20:16:55 +0100

    Use _mint a la zeppelin v2

--- new-packages/nitro-protocol/packages/fmg-nitro-adjudicator/contracts/test-contracts/testERC20.sol
+++ new-packages/nitro-protocol/packages/fmg-nitro-adjudicator/contracts/test-contracts/testERC20.sol
@@ -2,11 +2,8 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract testERC20 is ERC20 {
-    mapping (address => uint256) private _balances;
-    uint256 private _totalSupply;
     address winner = 0x57153E563526c1ce131E6af71ffFDA2C3A50b980;
-constructor() public {
-    _totalSupply = 1000000;
-    _balances[winner] = 100;
+        constructor() public {
+        _mint(winner, 1000);
     }
 }
