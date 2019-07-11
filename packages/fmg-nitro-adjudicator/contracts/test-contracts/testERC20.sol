f7b362a5de910a41b66bd0b36f577fbba403d615
George Knee <georgeknee@googlemail.com>
2019-07-11 14:01:14 +0100

    Add some additional expectations to help debug

--- packages/fmg-nitro-adjudicator/contracts/test-contracts/testERC20.sol
+++ packages/fmg-nitro-adjudicator/contracts/test-contracts/testERC20.sol
@@ -4,6 +4,6 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 contract testERC20 is ERC20 {
     address winner = 0x57153E563526c1ce131E6af71ffFDA2C3A50b980;
         constructor() public {
-        _mint(winner, 1000);
+        _mint(winner, 10000);
     }
 }
