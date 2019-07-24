bdc0b02b76fb6c42383fad0c324f81a63faaa216
George Knee <georgeknee@googlemail.com>
2019-07-24 09:23:49 +0100

    Add links to help page

--- website/pages/en/help.js
+++ website/pages/en/help.js
@@ -13,26 +13,21 @@ const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
 function Help(props) {
-  const {config: siteConfig, language = ''} = props;
-  const {baseUrl, docsUrl} = siteConfig;
+  const { config: siteConfig, language = '' } = props;
+  const { baseUrl, docsUrl } = siteConfig;
   const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
   const langPart = `${language ? `${language}/` : ''}`;
   const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
 
   const supportLinks = [
     {
-      content: `Learn more using the [documentation on this site.](${docUrl(
-        'doc1.html',
-      )})`,
+      content: `Learn more using the [documentation on this site.](${docUrl('introduction.html')})`,
       title: 'Browse Docs',
     },
     {
-      content: 'Ask questions about the documentation and project',
+      content:
+        'Contribute to the code and to the documentation [on github](https://github.com/magmo/force-move-protocol)',
       title: 'Join the community',
-    },
-    {
-      content: "Find out what's new with this project",
-      title: 'Stay up to date',
     },
   ];
 
