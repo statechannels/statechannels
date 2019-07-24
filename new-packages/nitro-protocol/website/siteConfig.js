fd4366954736bc75d5f751f3749c15c3371bfe87
George Knee <georgeknee@googlemail.com>
2019-07-24 09:30:59 +0100

    Strip out most of the links in the footer

--- new-packages/nitro-protocol/website/siteConfig.js
+++ new-packages/nitro-protocol/website/siteConfig.js
@@ -90,14 +90,14 @@ const siteConfig = {
   twitterImage: 'img/undraw_tweetstorm.svg',
 
   // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+  enableUpdateBy: true,
 
   // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  repoUrl: 'https://github.com/magmo/force-move-protocol',
 };
 
 module.exports = siteConfig;
