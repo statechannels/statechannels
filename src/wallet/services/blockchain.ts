394eb512cceba97407512bff3ceb1d51255e4374
Alex Gap <alex.gap@gmail.com>
2019-07-30 11:20:34 -0700

    Lock on depositing

--- src/wallet/services/blockchain.ts
+++ src/wallet/services/blockchain.ts
@@ -1,16 +1,20 @@
 import { Address, Uint256 } from 'fmg-core';
 
+import * as AsyncLock from 'async-lock';
 import { nitroAdjudicator } from '../utilities/blockchain';
-
+const lock = new AsyncLock();
 export class Blockchain {
   static nitro: any;
   static async fund(channelID: Address, expectedHeld: Uint256, value: Uint256): Promise<Uint256> {
-    await Blockchain.attachNitro();
+    // We lock to prevent issues with the transaction nonce getting out of sync in ethers.js
+    return lock.acquire('depositing', async () => {
+      await Blockchain.attachNitro();
 
-    const tx = await Blockchain.nitro.deposit(channelID, expectedHeld, { value });
-    await tx.wait();
+      const tx = await Blockchain.nitro.deposit(channelID, expectedHeld, { value });
+      await tx.wait();
 
-    return (await Blockchain.nitro.holdings(channelID)).toString();
+      return (await Blockchain.nitro.holdings(channelID)).toString();
+    });
   }
 
   static async holdings(channelID: Address): Promise<Uint256> {
