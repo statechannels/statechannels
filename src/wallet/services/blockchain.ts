a45dc72f09b70e564b587ae9fcfa10acf0c57106
Alex Gap <alex.gap@gmail.com>
2019-08-01 10:07:54 -0700

    Add clearer comment (hopefully)

--- src/wallet/services/blockchain.ts
+++ src/wallet/services/blockchain.ts
@@ -6,11 +6,16 @@ const lock = new AsyncLock();
 export class Blockchain {
   static nitro: any;
   static async fund(channelID: Address, expectedHeld: Uint256, value: Uint256): Promise<Uint256> {
-    // We lock to prevent issues with the transaction nonce getting out of sync in ethers.js
+    // We lock to avoid this issue: https://github.com/ethers-io/ethers.js/issues/363
+    // When ethers.js attempts to run multiple transactions around the same time it results in an error
+    // due to the nonce getting out of sync.
+    // To avoid this we only allow deposit transactions to happen serially.
     return lock.acquire('depositing', async () => {
       await Blockchain.attachNitro();
 
-      const tx = await Blockchain.nitro.deposit(channelID, expectedHeld, { value });
+      const tx = await Blockchain.nitro.deposit(channelID, expectedHeld, {
+        value,
+      });
       await tx.wait();
 
       return (await Blockchain.nitro.holdings(channelID)).toString();
