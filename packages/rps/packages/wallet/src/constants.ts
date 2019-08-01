6d2afd773a3d72c1c61ba9cff85ce3ab45ad49b1
Alex Gap <alex@magmo.com>
2019-08-01 08:57:28 -0700

    Merge pull request #652 from magmo/ag-storage-flag
    Add flag to control storage

--- packages/rps/packages/wallet/src/constants.ts
+++ packages/rps/packages/wallet/src/constants.ts
@@ -7,3 +7,4 @@ import {
 export const ADJUDICATOR_ADDRESS = getAdjudicatorContractAddress();
 export const CONSENSUS_LIBRARY_ADDRESS = getConsensusContractAddress();
 export const NETWORK_ID = getNetworkId();
+export const USE_STORAGE = process.env.USE_STORAGE === 'TRUE';
