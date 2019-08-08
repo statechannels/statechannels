ad1f5ec081a96828ad7b74fb7147e4c87451c344
George Knee <georgeknee@googlemail.com>
2019-08-08 10:50:40 +0100

    Remove old contracts and tests
    - move new WIP contract to contracts folder
    - prettier no longer targets notes folder

--- old-contracts/ForceMoveApp.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.2;
-pragma experimental ABIEncoderV2;
-
-import './Commitment.sol';
-interface ForceMoveApp {
-  function validTransition(Commitment.CommitmentStruct calldata oldCommitment, Commitment.CommitmentStruct calldata newCommitment) external pure returns (bool);
-}
--- old-contracts/test-contracts/TestERC20.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.2;
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-
-contract TestERC20 is ERC20 {
-            constructor() public {
-        _mint(msg.sender, 10000);
-    }
-}
