bcdf39cd74a5fc7da70355d5c1779519fcb8a451
George Knee <georgeknee@googlemail.com>
2019-08-09 15:04:29 +0100

    Add Migrations contract, modify deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,49 +1,5 @@
-const Commitment = artifacts.require('Commitment');
-const Outcome = artifacts.require('Outcome');
-const Rules = artifacts.require('Rules');
-const CountingCommitment = artifacts.require('CountingCommitment');
-const CountingApp = artifacts.require('CountingApp');
-var PaymentApp = artifacts.require('PaymentApp');
-var PaymentCommitment = artifacts.require('PaymentCommitment');
-const ConsensusCommitment = artifacts.require('ConsensusCommitment');
-const ConsensusApp = artifacts.require('ConsensusApp');
-const NitroLibrary = artifacts.require('NitroLibrary');
-const NitroAdjudicator = artifacts.require('NitroAdjudicator');
+const OptimizedForceMove = artifacts.require('OptimizedForceMove');
 
 module.exports = function(deployer) {
-  deployer.deploy(Commitment);
-  deployer.deploy(Outcome);
-
-  deployer.link(Commitment, Rules);
-  deployer.deploy(Rules);
-
-  deployer.link(Commitment, CountingCommitment);
-  deployer.deploy(CountingCommitment);
-
-  deployer.link(Commitment, CountingApp);
-  deployer.link(Rules, CountingApp);
-  deployer.link(CountingCommitment, CountingApp);
-  deployer.deploy(CountingApp);
-
-  deployer.link(Commitment, PaymentCommitment);
-  deployer.deploy(PaymentCommitment);
-  deployer.link(PaymentCommitment, PaymentApp);
-  deployer.link(Commitment, PaymentApp);
-  deployer.deploy(PaymentApp);
-
-  deployer.link(Commitment, ConsensusCommitment);
-  deployer.deploy(ConsensusCommitment);
-
-  deployer.link(Commitment, ConsensusApp);
-  deployer.link(Rules, ConsensusApp);
-  deployer.link(ConsensusCommitment, ConsensusApp);
-  deployer.deploy(ConsensusApp);
-
-  deployer.link(Commitment, NitroLibrary);
-  deployer.link(Rules, NitroLibrary);
-  deployer.link(Commitment, NitroAdjudicator);
-  deployer.link(Rules, NitroAdjudicator);
-  deployer.deploy(NitroLibrary).then(function() {
-    return deployer.deploy(NitroAdjudicator, NitroLibrary.address);
-  });
+  deployer.deploy(OptimizedForceMove);
 };
