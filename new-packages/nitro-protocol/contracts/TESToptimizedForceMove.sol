5a42f24d85e9fdbb464c46ee3770f1896253bd93
George Knee <georgeknee@googlemail.com>
2019-08-09 15:04:56 +0100

    Add test contract that wraps optimizedForceMove

---
+++ new-packages/nitro-protocol/contracts/TESToptimizedForceMove.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.2;
+pragma experimental ABIEncoderV2;
+import './optimizedForceMove.sol';
+
+contract TESTOptimizedForceMove is OptimizedForceMove {
+    // Public wrappers for internal methods:
+
+    function isAddressInArray(address suspect, address[] memory addresses)
+        public
+        pure
+        returns (bool)
+    {
+        return _isAddressInArray(suspect, addresses);
+    }
+
+    function validSignatures(
+        uint256 largestTurnNum,
+        address[] memory participants,
+        bytes32[] memory stateHashes,
+        Signature[] memory sigs,
+        uint8[] memory whoSignedWhat // whoSignedWhat[i] is the index of the state in stateHashes that was signed by participants[i]
+    ) public pure returns (bool) {
+        return _validSignatures(largestTurnNum, participants, stateHashes, sigs, whoSignedWhat);
+    }
+
+    function acceptableWhoSignedWhat(
+        uint8[] memory whoSignedWhat,
+        uint256 largestTurnNum,
+        uint256 nParticipants,
+        uint256 nStates
+    ) public pure returns (bool) {
+        return _acceptableWhoSignedWhat(whoSignedWhat, largestTurnNum, nParticipants, nStates);
+    }
+
+}
--- new-packages/nitro-protocol/migrations/3_deploy_test_contracts.js
+++ new-packages/nitro-protocol/migrations/3_deploy_test_contracts.js
@@ -1,31 +1,5 @@
-const Commitment = artifacts.require('./Commitment.sol');
-const Rules = artifacts.require('Rules');
-const ConsensusCommitment = artifacts.require('./ConsensusCommitment.sol');
-const TestConsensusCommitment = artifacts.require('./test-contracts/TestConsensusCommitment.sol');
-const TestERC20 = artifacts.require('TestERC20');
-const NitroAdjudicator = artifacts.require('NitroAdjudicator');
-const TestNitroAdjudicator = artifacts.require('TestNitroAdjudicator');
-const NitroLibrary = artifacts.require('NitroLibrary');
-const CountingApp = artifacts.require('CountingApp');
-const CountingCommitment = artifacts.require('CountingCommitment');
+const OptimizedForceMove = artifacts.require('TESTOptimizedForceMove');
 
 module.exports = function(deployer) {
-  deployer.link(Commitment, TestConsensusCommitment);
-  deployer.link(ConsensusCommitment, TestConsensusCommitment);
-  deployer.deploy(TestConsensusCommitment);
-
-  deployer.deploy(TestERC20);
-
-  deployer.link(Commitment, TestNitroAdjudicator);
-  deployer.link(Rules, TestNitroAdjudicator);
-  deployer.link(NitroAdjudicator, TestNitroAdjudicator);
-
-  deployer.deploy(NitroLibrary).then(function() {
-    return deployer.deploy(TestNitroAdjudicator, NitroLibrary.address);
-  });
-
-  deployer.link(Commitment, CountingApp);
-  deployer.link(Rules, CountingApp);
-  deployer.link(CountingCommitment, CountingApp);
-  deployer.deploy(CountingApp);
+  deployer.deploy(OptimizedForceMove);
 };
