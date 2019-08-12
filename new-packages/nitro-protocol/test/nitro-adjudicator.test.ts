5280624e49a7ea5430e58020ec4f1f0c36a2c0d9
George Knee <georgeknee@googlemail.com>
2019-08-12 11:50:02 +0100

    Add more tests for _acceptableWhoSignedWhat

--- new-packages/nitro-protocol/test/nitro-adjudicator.test.ts
+++ new-packages/nitro-protocol/test/nitro-adjudicator.test.ts
@@ -1,4 +1,5 @@
 import {ethers} from 'ethers';
+import {expectRevert} from 'magmo-devtools';
 // @ts-ignore
 import optimizedForceMoveArtifact from '../build/contracts/TESTOptimizedForceMove.json';
 
@@ -44,7 +45,7 @@ describe('_isAddressInArray', () => {
 describe('_acceptableWhoSignedWhat', () => {
   let whoSignedWhat;
   let largestTurnNum;
-  let nParticipants;
+  let nParticipants = 3;
   let nStates;
   it('verifies correct array of who signed what (n states)', async () => {
     whoSignedWhat = [0, 1, 2];
@@ -61,10 +62,62 @@ describe('_acceptableWhoSignedWhat', () => {
       ).toBe(true);
     }
   });
-  // it('verifies correct array of who signed what (fewer than n states)',
-  // });
-  // it('verifies correct array of who signed what (1 state)', async () => {
-  // });
-  // it('reverts when the array is not the required length', async () => {
-  // });
+  it('verifies correct array of who signed what (fewer than n states)', async () => {
+    whoSignedWhat = [0, 0, 1];
+    nStates = 2;
+    for (largestTurnNum = 2; largestTurnNum < 14; largestTurnNum += nParticipants) {
+      expect(
+        await optimizedForceMove.acceptableWhoSignedWhat(
+          whoSignedWhat,
+          largestTurnNum,
+          nParticipants,
+          nStates,
+        ),
+      ).toBe(true);
+    }
+  });
+  it('verifies correct array of who signed what (1 state)', async () => {
+    whoSignedWhat = [0, 0, 0];
+    nStates = 1;
+    for (largestTurnNum = 2; largestTurnNum < 14; largestTurnNum += nParticipants) {
+      expect(
+        await optimizedForceMove.acceptableWhoSignedWhat(
+          whoSignedWhat,
+          largestTurnNum,
+          nParticipants,
+          nStates,
+        ),
+      ).toBe(true);
+    }
+  });
+  it('reverts when the array is not the required length', async () => {
+    whoSignedWhat = [0, 0];
+    nStates = 1;
+    for (largestTurnNum = 2; largestTurnNum < 14; largestTurnNum += nParticipants) {
+      await expectRevert(
+        () =>
+          optimizedForceMove.acceptableWhoSignedWhat(
+            whoSignedWhat,
+            largestTurnNum,
+            nParticipants,
+            nStates,
+          ),
+        '_validSignatures: whoSignedWhat must be the same length as participants',
+      );
+    }
+  });
+  it('returns false when a participant signs a state with an insufficiently large turnNum', async () => {
+    whoSignedWhat = [0, 0, 2];
+    nStates = 3;
+    for (largestTurnNum = 2; largestTurnNum < 14; largestTurnNum += nParticipants) {
+      expect(
+        await optimizedForceMove.acceptableWhoSignedWhat(
+          whoSignedWhat,
+          largestTurnNum,
+          nParticipants,
+          nStates,
+        ),
+      ).toBe(false);
+    }
+  });
 });
