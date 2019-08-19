ebb597da098420cbc28ef8b0086a2b53507754c5
George Knee <georgeknee@googlemail.com>
2019-08-19 11:03:48 +0100

    Add CountingApp implementation and use in test
    - breaks the current test due to nontrivial check on appData in validTransition

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const OptimizedForceMove = artifacts.require('OptimizedForceMove');
 const TrivialApp = artifacts.require('TrivialApp');
+const CountingApp = artifacts.require('CountingApp');
 
 module.exports = function(deployer) {
   deployer.deploy(OptimizedForceMove);
   deployer.deploy(TrivialApp);
+  deployer.deploy(CountingApp);
 };
