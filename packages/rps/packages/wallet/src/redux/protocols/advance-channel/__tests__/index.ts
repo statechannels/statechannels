9d1713c3e7fb4e4b573b04dd47d1d5512654cdc9
Alex Gap <alex@magmo.com>
2019-08-19 15:05:36 -0700

    Merge pull request #665 from magmo/ag-add-concluding-tests
    Add Concluding Tests

--- packages/rps/packages/wallet/src/redux/protocols/advance-channel/__tests__/index.ts
+++ packages/rps/packages/wallet/src/redux/protocols/advance-channel/__tests__/index.ts
@@ -2,4 +2,4 @@ import { preFund } from './scenarios';
 const { preSuccess, success } = preFund;
 
 export { preSuccess, success };
-export { preFund, postFund } from './scenarios';
+export { preFund, postFund, conclude } from './scenarios';
