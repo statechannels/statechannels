db41c637e334a1c16113beccaf09ca20d35905b3
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-08-28 11:44:12 -0300

    refactored WebTorrentCustom

--- src/webtorrent-ilp/webtorrent-custom.js
+++ src/webtorrent-ilp/webtorrent-custom.js
@@ -1,5 +1,77 @@
 import WebTorrent from "webtorrent";
-import paidStreamingExtension from "./wire-extension";
+import paidStreamingExtension, {
+  PaidStreamingExtensionEvents,
+  PaidStreamingExtensionNotices
+} from "./wire-extension";
+
+export const WireEvents = {
+  DOWNLOAD: "download"
+};
+
+export const TorrentEvents = {
+  WIRE: "wire",
+  NOTICE: "notice",
+  STOP: "stop",
+  DONE: "done",
+  ERROR: "error"
+};
+
+function setupWire(torrent, wire, pseAccount) {
+  wire.use(paidStreamingExtension({ pseAccount }));
+  wire.setKeepAlive(true);
+
+  wire.on(WireEvents.DOWNLOAD, bytes => {
+    console.log(`>> downloaded ${bytes} bytes`);
+  });
+
+  wire.paidStreamingExtension.on(
+    PaidStreamingExtensionEvents.FIRST_REQUEST,
+    () => {
+      console.log(
+        `> first_request of ${wire.peerExtendedHandshake.pseAccount}`
+      );
+      wire.emit(
+        PaidStreamingExtensionEvents.FIRST_REQUEST,
+        wire.peerExtendedHandshake.pseAccount
+      );
+    }
+  );
+
+  wire.paidStreamingExtension.on(
+    PaidStreamingExtensionEvents.NOTICE,
+    notice => {
+      torrent.emit(PaidStreamingExtensionEvents.NOTICE, wire, notice);
+    }
+  );
+}
+
+function setupTorrent(torrent, pseAccount) {
+  if (torrent.usingPaidStreaming) {
+    return torrent;
+  }
+
+  torrent.on(TorrentEvents.WIRE, wire => setupWire(torrent, wire, pseAccount));
+
+  torrent.on(TorrentEvents.NOTICE, (wire, notice) => {
+    console.log(
+      `> notice recieved from ${wire.peerExtendedHandshake.pseAccount}: ${notice}`
+    );
+    if (notice === PaidStreamingExtensionNotices.STOP) {
+      wire.paidStreamingExtension.ack();
+      console.log("< stop acknowledged");
+    }
+  });
+
+  torrent.on(TorrentEvents.DONE, () => {
+    console.log(">DONE");
+  });
+
+  torrent.on(TorrentEvents.ERROR, err => {
+    console.log(">torrent error:", err);
+  });
+
+  torrent.usingPaidStreaming = true;
+}
 
 export default class WebTorrentPaidStreaming extends WebTorrent {
   constructor(opts) {
@@ -10,13 +82,13 @@ export default class WebTorrentPaidStreaming extends WebTorrent {
 
   seed() {
     const torrent = WebTorrent.prototype.seed.apply(this, arguments);
-    this._setupTorrent(torrent);
+    setupTorrent(torrent, this.pseAccount);
     return torrent;
   }
 
   add() {
     const torrent = WebTorrent.prototype.add.apply(this, arguments);
-    this._setupTorrent(torrent);
+    setupTorrent(torrent, this.pseAccount);
     return torrent;
   }
 
@@ -28,46 +100,5 @@ export default class WebTorrentPaidStreaming extends WebTorrent {
   retractNotice(wire, peerAccount) {
     wire.paidStreamingExtension.start();
     console.log("> retractNotice", peerAccount);
-  }
-
-  _setupWire(torrent, wire) {
-    wire.use(paidStreamingExtension({ pseAccount: this.pseAccount }));
-    wire.setKeepAlive(true);
-    wire.on("download", bytes => {
-      console.log(`>> downloaded ${bytes} bytes`);
-    });
-    wire.paidStreamingExtension.on("first_request", () => {
-      console.log(
-        `> first_request of ${wire.peerExtendedHandshake.pseAccount}`
-      );
-      wire.emit("first_request", wire.peerExtendedHandshake.pseAccount);
-    });
-    wire.paidStreamingExtension.on("notice", notice => {
-      torrent.emit("notice", wire, notice);
-    });
-  }
-
-  _setupTorrent(torrent) {
-    if (torrent.usingPaidStreaming) {
-      return torrent;
-    }
-    torrent.on("wire", this._setupWire.bind(this, torrent));
-    torrent.on("notice", (wire, notice) => {
-      console.log(
-        `> notice recieved from ${wire.peerExtendedHandshake.pseAccount}: ${notice}`
-      );
-      if (notice === "stop") {
-        wire.paidStreamingExtension.ack();
-        console.log("< stop acknowledged");
-      }
-    });
-    torrent.on("done", () => {
-      console.log(">DONE");
-    });
-    torrent.on("error", err => {
-      console.log(">torrent error:", err);
-    });
-
-    torrent.usingPaidStreaming = true;
   }
 }
