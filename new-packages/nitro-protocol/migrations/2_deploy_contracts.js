764bb982df1cdb962f9a4640d8d23ce026abfbc9
George Knee <georgeknee@googlemail.com>
2019-08-28 15:26:00 +0100

    Add ETHAssetHolder and ERC20AssetHolder contracts
    Also add a Token contract which mints ERC20s to deployer

--- new-packages/nitro-protocol/migrations/2_deploy_contracts.js
+++ new-packages/nitro-protocol/migrations/2_deploy_contracts.js
@@ -1,11 +1,22 @@
 const ForceMove = artifacts.require('ForceMove');
 const TrivialApp = artifacts.require('TrivialApp');
 const CountingApp = artifacts.require('CountingApp');
-const AssetHolder = artifacts.require('AssetHolder');
+const Token = artifacts.require('Token');
+const ETHAssetHolder = artifacts.require('ETHAssetHolder');
+const ERC20AssetHolder = artifacts.require('ERC20AssetHolder');
 
-module.exports = function(deployer) {
-  deployer.deploy(ForceMove);
-  deployer.deploy(TrivialApp);
-  deployer.deploy(CountingApp);
-  deployer.deploy(AssetHolder);
+module.exports = async function(deployer) {
+  deployer.then(async () => {
+    await Promise.all([deployer.deploy(ForceMove), deployer.deploy(Token)]);
+
+    const [ForceMoveInstance, TokenInstance] = await Promise.all([
+      ForceMove.deployed(),
+      Token.deployed(),
+    ]);
+
+    await deployer.deploy(ETHAssetHolder, ForceMoveInstance.address);
+    await deployer.deploy(ERC20AssetHolder, ForceMoveInstance.address, TokenInstance.address);
+    await deployer.deploy(TrivialApp);
+    await deployer.deploy(CountingApp);
+  });
 };
