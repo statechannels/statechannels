89865b7dfcecdfde416d28ac2a1994318e1e1456
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-08-28 12:57:07 -0300

    moved torrent logic into client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import webtorrent from "./webtorrent-ilp/webtorrent-custom";
+import WebTorrentPaidStreamingClient, {
+  ClientEvents
+} from "./webtorrent-ilp/webtorrent-custom";
 
 const initialState = {
   working: "Done/Idle",
@@ -42,6 +44,15 @@ const progressLogger = (status, setStatus) => (
   }
 };
 
+/**
+ *
+ * @param {WebTorrentPaidStreamingClient} client
+ * @param {*} files
+ * @param {*} setMagnet
+ * @param {*} progressLogger
+ * @param {*} allowedPeers
+ * @param {*} updateAllowedPeers
+ */
 const upload = (
   client,
   files,
@@ -50,42 +61,65 @@ const upload = (
   allowedPeers,
   updateAllowedPeers
 ) => {
-  client.seed(files, function(torrent) {
+  client.seed(files, torrent => {
     setMagnet(torrent.magnetURI);
-    torrent.on("wire", wire => {
-      wire.on("first_request", peerAccount => {
-        console.log("UI - NEW WIRE FROM " + peerAccount, allowedPeers);
-        if (peerAccount in allowedPeers && !allowedPeers[peerAccount].allowed) {
-          console.log(
-            "UI - ALREADY BLOCKED WIRE FROM " + peerAccount,
-            allowedPeers
-          );
-          client.sendNotice(wire, peerAccount);
-        } else {
-          console.log("UI - SEEMS LEGIT WIRE " + peerAccount, allowedPeers);
-          updateAllowedPeers({ id: peerAccount, allowed: true, wire });
-        }
-      });
-    });
+
+    // torrent.on("wire", wire => {
+    //   wire.on("first_request", peerAccount => {
+    //     console.log("UI - NEW WIRE FROM " + peerAccount, allowedPeers);
+    //     if (peerAccount in allowedPeers && !allowedPeers[peerAccount].allowed) {
+    //       console.log(
+    //         "UI - ALREADY BLOCKED WIRE FROM " + peerAccount,
+    //         allowedPeers
+    //       );
+    //       client.sendNotice(wire, peerAccount);
+    //     } else {
+    //       console.log("UI - SEEMS LEGIT WIRE " + peerAccount, allowedPeers);
+    //       updateAllowedPeers({ id: peerAccount, allowed: true, wire });
+    //     }
+    //   });
+    // });
+
     progressLogger(torrent, "Seeding", false);
   });
 };
 
+/**
+ *
+ * @param {WebTorrentPaidStreamingClient} client
+ * @param {*} torrentOrigin
+ * @param {*} progressLogger
+ */
 const download = (client, torrentOrigin, progressLogger) => {
   var torrentId =
     torrentOrigin || "https://webtorrent.io/torrents/sintel.torrent";
+
+  client.once(ClientEvents.CLIENT_RESET, newClient =>
+    download(newClient, torrentOrigin, progressLogger)
+  );
+
+  client.once(ClientEvents.CLIENT_RESET, newClient =>
+    download(newClient, torrentOrigin, progressLogger)
+  );
+
   client.add(torrentId, torrent => {
     console.log("UI - Download Started - MY account id is", client, torrent);
     const logger = progressLogger(torrent, "Leeching", false);
+
     torrent.on("notice", (wire, notice) => {
       if (notice === "start") {
         clearInterval(logger);
-        client.destroy();
-        client = new webtorrent({ pseAccount: client.pseAccount });
-        download(client, torrentOrigin, progressLogger);
+        // if (!client.destroyed) {
+        //   client.destroy();
+        // }
+        // client = new WebTorrentPaidStreamingClient({
+        //   pseAccount: client.pseAccount
+        // });
+        // download(client, torrentOrigin, progressLogger);
       }
     });
-    torrent.on("done", () => {
+
+    client.once(ClientEvents.TORRENT_DONE, () => {
       console.log("Done!", "File downloaded: " + torrent.files[0].name);
       clearInterval(logger);
       progressLogger(initialState, "Done/Idle", true, torrent.files[0]);
@@ -93,18 +127,26 @@ const download = (client, torrentOrigin, progressLogger) => {
   });
 };
 
+/**
+ *
+ * @param {WebTorrentPaidStreamingClient} client
+ * @param {*} peerAccount
+ * @param {*} allowedPeers
+ * @param {*} updateAllowedPeers
+ */
 const togglePeer = (client, peerAccount, allowedPeers, updateAllowedPeers) => {
-  if (!allowedPeers.hasOwnProperty(peerAccount)) {
-    console.error("That peer account doesnt exist in the list");
-    return;
-  }
-  const { wire, allowed } = allowedPeers[peerAccount];
-  if (allowed) {
-    client.sendNotice(wire, peerAccount);
-  } else {
-    client.retractNotice(wire, peerAccount);
-  }
-  updateAllowedPeers({ id: peerAccount, allowed: !allowed, wire });
+  // if (!allowedPeers.hasOwnProperty(peerAccount)) {
+  //   console.error("That peer account doesnt exist in the list");
+  //   return;
+  // }
+  // const { wire, allowed } = allowedPeers[peerAccount];
+  // if (allowed) {
+  //   client.sendNotice(wire, peerAccount);
+  // } else {
+  //   client.retractNotice(wire, peerAccount);
+  // }
+  // updateAllowedPeers({ id: peerAccount, allowed: !allowed, wire });
+  client.togglePeer(peerAccount);
 };
 
 const updatePeers = (allowedPeers, setAllowedPeers) => peer => {
@@ -129,6 +171,11 @@ function App() {
   const [leechMagnet, setLeechMagnet] = useState("");
   const { downloadSpeed, uploadSpeed, working, numPeers } = status;
   const updateAllowedPeers = updatePeers(allowedPeers, setAllowedPeers);
+
+  client.once(ClientEvents.PEER_STATUS_CHANGED, peer =>
+    updateAllowedPeers(peer)
+  );
+
   return (
     <div className="App">
       <div className="hero" id="hero">
@@ -223,11 +270,16 @@ function App() {
     </div>
   );
 }
-const wt = new webtorrent({
+
+let wt = new WebTorrentPaidStreamingClient({
   pseAccount: Math.floor(Math.random() * 99999999999999999)
 });
+
+wt.on(ClientEvents.CLIENT_RESET, newClient => (wt = newClient));
+
 const WebTorrentContext = React.createContext({});
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(
   <WebTorrentContext.Provider value={wt}>
     <App />
--- src/webtorrent-ilp/webtorrent-custom.js
+++ src/webtorrent-ilp/webtorrent-custom.js
@@ -5,7 +5,8 @@ import paidStreamingExtension, {
 } from "./wire-extension";
 
 export const WireEvents = {
-  DOWNLOAD: "download"
+  DOWNLOAD: "download",
+  FIRST_REQUEST: "first_request"
 };
 
 export const TorrentEvents = {
@@ -16,12 +17,39 @@ export const TorrentEvents = {
   ERROR: "error"
 };
 
-function setupWire(torrent, wire, pseAccount) {
-  wire.use(paidStreamingExtension({ pseAccount }));
+export const ClientEvents = {
+  PEER_STATUS_CHANGED: "peer_status_changed",
+  CLIENT_RESET: "client_reset",
+  TORRENT_DONE: "torrent_done",
+  TORRENT_ERROR: "torrent_error"
+};
+
+/**
+ * @this {WebTorrentPaidStreamingClient}
+ */
+function setupWire(torrent, wire) {
+  wire.use(paidStreamingExtension({ pseAccount: this.pseAccount }));
   wire.setKeepAlive(true);
 
   wire.on(WireEvents.DOWNLOAD, bytes => {
     console.log(`>> downloaded ${bytes} bytes`);
+  });
+
+  wire.on(WireEvents.FIRST_REQUEST, peerAccount => {
+    if (
+      peerAccount in this.allowedPeers &&
+      !this.allowedPeers[peerAccount].allowed
+    ) {
+      this.sendNotice(wire, peerAccount);
+    } else {
+      const newAllowedPeer = {
+        id: peerAccount,
+        allowed: true,
+        wire
+      };
+      this.allowedPeers[peerAccount] = newAllowedPeer;
+      this.emit(ClientEvents.PEER_STATUS_CHANGED, newAllowedPeer);
+    }
   });
 
   wire.paidStreamingExtension.on(
@@ -45,50 +73,67 @@ function setupWire(torrent, wire, pseAccount) {
   );
 }
 
-function setupTorrent(torrent, pseAccount) {
+/**
+ * @this {WebTorrentPaidStreamingClient}
+ */
+function setupTorrent(torrent) {
   if (torrent.usingPaidStreaming) {
     return torrent;
   }
 
-  torrent.on(TorrentEvents.WIRE, wire => setupWire(torrent, wire, pseAccount));
+  torrent.on(TorrentEvents.WIRE, wire => setupWire.call(this, torrent, wire));
 
   torrent.on(TorrentEvents.NOTICE, (wire, notice) => {
     console.log(
       `> notice recieved from ${wire.peerExtendedHandshake.pseAccount}: ${notice}`
     );
+
     if (notice === PaidStreamingExtensionNotices.STOP) {
       wire.paidStreamingExtension.ack();
       console.log("< stop acknowledged");
+    }
+
+    if (notice === PaidStreamingExtensionNotices.START) {
+      /**
+       * @todo This should use choking/unchoking.
+       */
+      this.destroy();
+      const newClient = new WebTorrentPaidStreamingClient({
+        pseAccount: this.pseAccount
+      });
+      this.emit(ClientEvents.CLIENT_RESET, newClient);
     }
   });
 
   torrent.on(TorrentEvents.DONE, () => {
     console.log(">DONE");
+    this.emit(ClientEvents.TORRENT_DONE, torrent);
   });
 
   torrent.on(TorrentEvents.ERROR, err => {
     console.log(">torrent error:", err);
+    this.emit(ClientEvents.TORRENT_ERROR, torrent, err);
   });
 
   torrent.usingPaidStreaming = true;
 }
 
-export default class WebTorrentPaidStreaming extends WebTorrent {
+export default class WebTorrentPaidStreamingClient extends WebTorrent {
   constructor(opts) {
     super(opts);
-    this.peerWires = [];
+    this.allowedPeers = {};
     this.pseAccount = opts && opts.pseAccount;
   }
 
   seed() {
-    const torrent = WebTorrent.prototype.seed.apply(this, arguments);
-    setupTorrent(torrent, this.pseAccount);
+    const torrent = super.seed.apply(this, arguments);
+    setupTorrent.call(this, torrent);
     return torrent;
   }
 
   add() {
-    const torrent = WebTorrent.prototype.add.apply(this, arguments);
-    setupTorrent(torrent, this.pseAccount);
+    const torrent = super.add.apply(this, arguments);
+    setupTorrent.call(this, torrent);
     return torrent;
   }
 
@@ -100,5 +145,16 @@ export default class WebTorrentPaidStreaming extends WebTorrent {
   retractNotice(wire, peerAccount) {
     wire.paidStreamingExtension.start();
     console.log("> retractNotice", peerAccount);
+  }
+
+  togglePeer(peerAccount) {
+    const { wire, allowed } = this.allowedPeers[peerAccount];
+    if (allowed) {
+      this.sendNotice(wire, peerAccount);
+    } else {
+      this.retractNotice(wire, peerAccount);
+    }
+    this.allowedPeers[peerAccount].allowed = !allowed;
+    this.emit(ClientEvents.PEER_STATUS_CHANGED, this.allowedPeers[peerAccount]);
   }
 }
