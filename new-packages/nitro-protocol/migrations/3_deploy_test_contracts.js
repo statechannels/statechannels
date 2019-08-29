2590bead3134144a9a8e5cb294ee86cbf32c76f1
George Knee <georgeknee@googlemail.com>
2019-08-29 16:16:17 +0100

    Deploy NitroAdjudicator, not ForceMove

--- new-packages/nitro-protocol/migrations/3_deploy_test_contracts.js
+++ new-packages/nitro-protocol/migrations/3_deploy_test_contracts.js
@@ -1,10 +1,28 @@
-const ForceMove = artifacts.require('TESTForceMove');
+const Token = artifacts.require('Token');
+const NitroAdjudicator = artifacts.require('TESTNitroAdjudicator');
 const AssetHolder = artifacts.require('TESTAssetHolder');
 const TrivialApp = artifacts.require('TrivialApp');
 const CountingApp = artifacts.require('CountingApp');
+const ETHAssetHolder = artifacts.require('ETHAssetHolder');
+const ERC20AssetHolder = artifacts.require('ERC20AssetHolder');
 
 module.exports = function(deployer) {
-  deployer.deploy(ForceMove);
+  deployer.then(async () => {
+    await Promise.all([deployer.deploy(NitroAdjudicator), deployer.deploy(Token)]);
+
+    const [NitroAdjudicatorInstance, TokenInstance] = await Promise.all([
+      NitroAdjudicator.deployed(),
+      Token.deployed(),
+    ]);
+
+    await deployer.deploy(ETHAssetHolder, NitroAdjudicatorInstance.address);
+    await deployer.deploy(
+      ERC20AssetHolder,
+      NitroAdjudicatorInstance.address,
+      TokenInstance.address,
+    );
+  });
+
   deployer.deploy(AssetHolder);
   deployer.deploy(TrivialApp);
   deployer.deploy(CountingApp);
