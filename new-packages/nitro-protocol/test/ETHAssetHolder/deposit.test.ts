489d8498c98404711cf2f7ae40f03ef8a0690276
George Knee <georgeknee@googlemail.com>
2019-09-02 14:44:48 +0100

    Switch order of tests

--- new-packages/nitro-protocol/test/ETHAssetHolder/deposit.test.ts
+++ new-packages/nitro-protocol/test/ETHAssetHolder/deposit.test.ts
@@ -10,39 +10,39 @@ const provider = new ethers.providers.JsonRpcProvider(
 const signer = provider.getSigner(0); // convention matches setupContracts function
 let ETHAssetHolder: ethers.Contract;
 let depositedEvent;
+const destinations = [];
+
+// populate destinations array
+for (let i = 0; i < 4; i++) {
+  destinations[i] = ethers.Wallet.createRandom().address.padEnd(66, '0');
+}
 
 beforeAll(async () => {
   ETHAssetHolder = await setupContracts(provider, ETHAssetHolderArtifact);
 });
 
-const description1 = 'Deposits ETH (msg.value = amount , expectedHeld = 0)';
-const description2 = 'Reverts deposit of ETH (msg.value = amount, expectedHeld > holdings)';
-const description3 = 'Deposits ETH (msg.value = amount, expectedHeld + amount < holdings)';
-const description4 =
+const description0 = 'Deposits ETH (msg.value = amount , expectedHeld = 0)';
+const description1 = 'Reverts deposit of ETH (msg.value = amount, expectedHeld > holdings)';
+const description2 = 'Deposits ETH (msg.value = amount, expectedHeld + amount < holdings)';
+const description3 =
   'Deposits ETH (msg.value = amount,  amount < holdings < amount + expectedHeld)';
 
 // amounts are valueString represenationa of wei
 describe('deposit', () => {
   it.each`
-    description     | destinationType       | held   | expectedHeld | amount | msgValue | heldAfter | reasonString
-    ${description1} | ${'randomEOABytes32'} | ${'0'} | ${'0'}       | ${'1'} | ${'1'}   | ${'1'}    | ${undefined}
-    ${description2} | ${'randomEOABytes32'} | ${'0'} | ${'1'}       | ${'2'} | ${'2'}   | ${'0'}    | ${'Deposit | holdings[destination] is less than expected'}
-    ${description3} | ${'randomEOABytes32'} | ${'3'} | ${'1'}       | ${'1'} | ${'1'}   | ${'3'}    | ${'Deposit | holdings[destination] already meets or exceeds expectedHeld + amount'}
-    ${description4} | ${'randomEOABytes32'} | ${'3'} | ${'2'}       | ${'2'} | ${'2'}   | ${'4'}    | ${undefined}
+    description     | destination        | held   | expectedHeld | amount | msgValue | heldAfter | reasonString
+    ${description1} | ${destinations[1]} | ${'0'} | ${'1'}       | ${'2'} | ${'2'}   | ${'0'}    | ${'Deposit | holdings[destination] is less than expected'}
+    ${description2} | ${destinations[2]} | ${'3'} | ${'1'}       | ${'1'} | ${'1'}   | ${'3'}    | ${'Deposit | holdings[destination] already meets or exceeds expectedHeld + amount'}
+    ${description3} | ${destinations[3]} | ${'3'} | ${'2'}       | ${'2'} | ${'2'}   | ${'4'}    | ${undefined}
+    ${description0} | ${destinations[0]} | ${'0'} | ${'0'}       | ${'1'} | ${'1'}   | ${'1'}    | ${undefined}
   `(
     '$description',
-    async ({destinationType, held, expectedHeld, amount, msgValue, reasonString, heldAfter}) => {
+    async ({destination, held, expectedHeld, amount, msgValue, reasonString, heldAfter}) => {
       held = ethers.utils.parseUnits(held, 'wei');
       expectedHeld = ethers.utils.parseUnits(expectedHeld, 'wei');
       amount = ethers.utils.parseUnits(amount, 'wei');
       msgValue = ethers.utils.parseUnits(msgValue, 'wei');
       heldAfter = ethers.utils.parseUnits(heldAfter, 'wei');
-
-      let destination;
-      if (destinationType === 'randomEOABytes32') {
-        const randomAddress = ethers.Wallet.createRandom().address;
-        destination = randomAddress.padEnd(66, '0');
-      }
 
       // set holdings by depositing in the 'safest' way
       if (held > 0) {
