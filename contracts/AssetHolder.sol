901fab45b01d414d6f996eede80bc5222d23b215
George Knee <georgeknee@googlemail.com>
2019-09-03 13:26:25 +0100

    Implement transferAll

--- contracts/AssetHolder.sol
+++ contracts/AssetHolder.sol
@@ -28,6 +28,72 @@ contract AssetHolder {
     // Public methods
     // **************
 
+    function transferAll(bytes32 channelId, Outcome.AllocationItem[] memory allocation) public {
+        // requirements
+        require(
+            outcomeHashes[channelId] ==
+                keccak256(
+                    abi.encode(
+                        Outcome.LabelledAllocationOrGuarantee(
+                            uint8(Outcome.OutcomeType.Allocation),
+                            abi.encode(allocation)
+                        )
+                    )
+                ),
+            'transferAll | submitted data does not match stored outcomeHash'
+        );
+
+        uint256 balance = holdings[channelId];
+        Outcome.AllocationItem[] memory payouts;
+        Outcome.AllocationItem[] memory newAllocation;
+        uint256 j = 0;
+        for (uint256 i = 0; i < allocation.length; i++) {
+            if (balance == 0) {
+                newAllocation[i] = Outcome.AllocationItem(
+                    allocation[i].destination,
+                    allocation[i].amount
+                );
+                j++;
+            } else {
+                payouts[i] = Outcome.AllocationItem(
+                    allocation[i].destination,
+                    allocation[i].amount
+                );
+                if (balance <= allocation[i].amount) {
+                    newAllocation[j] = Outcome.AllocationItem(
+                        allocation[i].destination,
+                        allocation[i].amount - balance
+                    );
+                    balance = 0;
+                } else {
+                    payouts[i] = Outcome.AllocationItem(
+                        allocation[i].destination,
+                        allocation[i].amount
+                    );
+                    balance -= allocation[i].amount;
+                }
+            }
+        }
+
+        // effects
+        holdings[channelId] = balance;
+        if (newAllocation.length > 0) {
+            outcomeHashes[channelId] = keccak256(abi.encode(newAllocation));
+        } else {
+            delete outcomeHashes[channelId];
+        }
+
+        for (uint256 i = 0; i < payouts.length; i++) {
+            if (_isExternalAddress(payouts[i].destination)) {
+                _transferAsset(_bytes32ToAddress(payouts[i].destination), payouts[i].amount);
+            } else {
+                holdings[payouts[i].destination] =
+                    holdings[payouts[i].destination] +
+                    payouts[i].amount;
+            }
+        }
+    }
+
     // **************
     // Permissioned methods
     // **************
@@ -55,12 +121,18 @@ contract AssetHolder {
     // Internal methods
     // **************
 
+    function _transferAsset(address payable destination, uint256 amount) internal {}
+
     function _isExternalAddress(bytes32 destination) internal pure returns (bool) {
         return (destination == bytes32(bytes20(destination)));
     }
 
     function _addressToBytes32(address participant) internal pure returns (bytes32) {
         return bytes32(bytes20(participant));
+    }
+
+    function _bytes32ToAddress(bytes32 destination) internal pure returns (address payable) {
+        return address(bytes20(destination));
     }
 
     function recoverSigner(bytes memory _d, uint8 _v, bytes32 _r, bytes32 _s)
