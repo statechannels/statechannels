e6b23a93efb03553d592f08fb395a2270b4b30e1
George Knee <georgeknee@googlemail.com>
2019-09-05 11:19:05 +0100

    Add tests with two beneficiaries for transferAll
    - test script has scope to be DRY-ed out more

--- new-packages/nitro-protocol/test/AssetHolder/transferAll.test.ts
+++ new-packages/nitro-protocol/test/AssetHolder/transferAll.test.ts
@@ -9,11 +9,14 @@ import {HashZero} from 'ethers/constants';
 const provider = new ethers.providers.JsonRpcProvider(
   `http://localhost:${process.env.DEV_GANACHE_PORT}`,
 );
-const signer0 = provider.getSigner(0); // convention matches setupContracts function
+const signer0 = provider.getSigner(0);
+const signer1 = provider.getSigner(1);
 let signer0Address;
+let signer1Address;
 let AssetHolder: ethers.Contract;
 let assetTransferredEvent;
-const chainId = 1234;
+let assetTransferredEvent0;
+let assetTransferredEvent1;
 const participants = ['', '', ''];
 
 function allocationToParams(allocation) {
@@ -33,13 +36,14 @@ function allocationToParams(allocation) {
 beforeAll(async () => {
   AssetHolder = await setupContracts(provider, AssetHolderArtifact);
   signer0Address = await signer0.getAddress();
+  signer1Address = await signer1.getAddress();
 });
 
 const description0 = 'Reverts transferAll tx when outcomeHash does not match';
 const description1 =
   'Pays out all holdings from directly-funded channel allocating to a single external address';
 const description2 =
-  'Pays out some of the holdings when directly-overfunded channel allocates  assets to a single external address';
+  'Pays out some of the holdings when directly-overfunded channel allocates assets to a single external address';
 const description3 =
   'Pays out all of the holdings when directly-underfunded channel allocates assets to a single external address';
 const description4 =
@@ -50,7 +54,7 @@ const description6 =
   'Transfers all holdings from directly-underfunded channel allocating to a single channel';
 
 // amounts are valueString represenationa of wei
-describe('transferAll', () => {
+describe('transferAll (single beneficiary)', () => {
   it.each`
     description     | channelNonce | held   | allocated | beneficiaryExternal | amount | outcomeSet | reasonString
     ${description0} | ${0}         | ${'1'} | ${'0'}    | ${true}             | ${'1'} | ${false}   | ${'transferAll | submitted data does not match stored outcomeHash'}
@@ -141,6 +145,135 @@ describe('transferAll', () => {
           expectedOutcomeHash = HashZero;
         } else {
           const newAllocation = [{destination, amount: allocated.sub(amount)}]; // sufficient
+          [_, expectedOutcomeHash] = allocationToParams(newAllocation);
+        }
+
+        expect(await AssetHolder.outcomeHashes(channelId)).toEqual(expectedOutcomeHash);
+      }
+    },
+  );
+});
+
+const description9 =
+  'Transfers all holdings from directly-funded channel allocating to two external addresses (full payout, full payout)';
+const description10 =
+  'Transfers all holdings from directly-funded channel allocating to two external addresses (full payout, no payout)';
+const description11 =
+  'Transfers all holdings from directly-funded channel allocating to two external addresses (full payout, partial payout)';
+
+describe('transferAll (two beneficiaries)', () => {
+  it.each`
+    description      | channelNonce | held   | allocated     | beneficiaryExternal | amount        | outcomeSet | reasonString
+    ${description9}  | ${9}         | ${'2'} | ${['1', '1']} | ${[true, true]}     | ${['1', '1']} | ${true}    | ${undefined}
+    ${description10} | ${10}        | ${'1'} | ${['1', '1']} | ${[true, true]}     | ${['1', '0']} | ${true}    | ${undefined}
+    ${description11} | ${11}        | ${'3'} | ${['2', '2']} | ${[true, true]}     | ${['2', '1']} | ${true}    | ${undefined}
+  `(
+    '$description',
+    async ({
+      channelNonce,
+      held,
+      allocated,
+      beneficiaryExternal,
+      amount,
+      outcomeSet,
+      reasonString,
+    }) => {
+      held = ethers.utils.parseUnits(held, 'wei');
+      amount[0] = ethers.utils.parseUnits(amount[0], 'wei');
+      amount[1] = ethers.utils.parseUnits(amount[1], 'wei');
+      allocated[0] = ethers.utils.parseUnits(allocated[0], 'wei');
+      allocated[1] = ethers.utils.parseUnits(allocated[1], 'wei');
+
+      const destination0 = beneficiaryExternal[0]
+        ? signer0Address.padEnd(66, '0')
+        : randomChannelId(channelNonce * 111);
+      const destination1 = beneficiaryExternal[1]
+        ? signer1Address.padEnd(66, '0')
+        : randomChannelId(channelNonce * 222);
+
+      // compute channelId
+      const channelId = randomChannelId(channelNonce);
+
+      // set holdings (only works on test contract)
+      if (held > 0) {
+        await (await AssetHolder.setHoldings(channelId, held)).wait();
+        expect(await AssetHolder.holdings(channelId)).toEqual(held);
+      }
+
+      // compute an appropriate allocation
+      const allocation = [
+        {destination: destination0, amount: allocated[0]},
+        {destination: destination1, amount: allocated[1]},
+      ];
+      const [allocationBytes, outcomeHash] = allocationToParams(allocation);
+
+      // set outcomeHash
+      if (outcomeSet) {
+        await (await AssetHolder.setOutcomePermissionless(channelId, outcomeHash)).wait();
+        expect(await AssetHolder.outcomeHashes(channelId)).toBe(outcomeHash);
+      }
+
+      // call method in a slightly different way if expecting a revert
+      if (reasonString) {
+        const regex = new RegExp(
+          '^' + 'VM Exception while processing transaction: revert ' + reasonString + '$',
+        );
+        await expectRevert(() => AssetHolder.transferAll(channelId, allocationBytes), regex);
+      } else {
+        if (beneficiaryExternal[0] && amount[0].gt(0)) {
+          // register for events
+          assetTransferredEvent0 = newAssetTransferredEvent(AssetHolder, destination0);
+        } else {
+          assetTransferredEvent0 = undefined;
+        }
+        if (beneficiaryExternal[1] && amount[1].gt(0)) {
+          // register for events
+          assetTransferredEvent1 = newAssetTransferredEvent(AssetHolder, destination1);
+        } else {
+          assetTransferredEvent1 = undefined;
+        }
+
+        // submit tx
+        const tx = await AssetHolder.transferAll(channelId, allocationBytes);
+
+        // wait for tx to be mined
+        await tx.wait();
+
+        // wait for all events to be caught
+        [assetTransferredEvent0, assetTransferredEvent1] = await Promise.all([
+          assetTransferredEvent0,
+          assetTransferredEvent1,
+        ]);
+
+        if (beneficiaryExternal[0] && amount[0].gt(0)) {
+          // catch event
+          expect(assetTransferredEvent0).toEqual(amount[0]);
+        } else {
+          expect(AssetHolder.holdings(destination0)).toEqual(amount[0]);
+        }
+        if (beneficiaryExternal[1] && amount[1].gt(0)) {
+          // catch event
+          expect(assetTransferredEvent1).toEqual(amount[1]);
+        } else {
+          expect(await AssetHolder.holdings(destination1)).toEqual(amount[1]);
+        }
+
+        // check new holdings
+        expect(await AssetHolder.holdings(channelId)).toEqual(held.sub(amount[0].add(amount[1])));
+
+        // check new outcomeHash
+        let expectedOutcomeHash;
+        let _;
+        if (allocated[0].eq(amount[0]) && allocated[1].eq(amount[1])) {
+          expectedOutcomeHash = HashZero;
+        } else {
+          const newAllocation = [];
+          if (allocated[0].sub(amount[0]).gt(0)) {
+            newAllocation.push({destination: destination0, amount: allocated[0].sub(amount[0])});
+          }
+          if (allocated[1].sub(amount[1]).gt(0)) {
+            newAllocation.push({destination: destination1, amount: allocated[1].sub(amount[1])});
+          }
           [_, expectedOutcomeHash] = allocationToParams(newAllocation);
         }
 
