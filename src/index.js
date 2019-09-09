17c0a5bd1f8f71cbfae1daf2fece463cd401f40a
Santiago Persico <santiagopersico@gmail.com>
2019-09-09 18:48:40 -0300

    ui refactoring

--- src/index.js
+++ src/index.js
@@ -1,65 +1,48 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import WebTorrent, { ClientEvents } from "./webtorrent-custom";
+import WebTorrent, { ClientEvents, InitialState } from "./webtorrent-custom";
 import prettierBytes from "prettier-bytes";
+const client = new WebTorrent()
 
-const initialState = {
-  working: "Done/Idle",
-  verb: "from",
-  numPeers: 0,
-  downloadSpeed: 0,
-  uploadSpeed: 0,
-  torrent: null,
-  files: []
-};
-const initialClient = new WebTorrent({ pseAccount: Math.floor(Math.random() * 99999999999999999) })
-
-const progressLogger = (logger, status, setStatus) => (torrent = initialState, action = "Idle", torrentIsDone, torrrentFile) => {
+const progressLogger = (logger, status, setStatus, setMagnet) => (torrent = InitialState) => {
   clearInterval(logger);
+  let torrentIsDone, isSeeder
   logger = setInterval(() => {
-    torrentIsDone = torrent && torrent.done && !torrent.downloadSpeed && !torrent.uploadSpeed;
-    torrrentFile = torrent && torrent.files && torrent.files[0];
+    torrentIsDone = torrent.done && !torrent.downloadSpeed && !torrent.uploadSpeed;
+    isSeeder = torrent.created;
 
     setStatus({
       ...status,
-      working: torrentIsDone ? "Done/Idle" : action,
+      working: torrentIsDone ? "Done/Idle" : isSeeder ? "Seeding" : "Leeching",
       downloadSpeed: prettierBytes(torrent.downloadSpeed),
       uploadSpeed: prettierBytes(torrent.uploadSpeed),
       numPeers: torrent.numPeers
     })
 
-    if (torrentIsDone && !torrent.created && torrrentFile && torrrentFile.done) {
-      torrrentFile.getBlobURL((err, url) => {
-        setStatus(Object.assign(initialState, { url, filename: torrrentFile.name }));
-        clearInterval(logger);
-      });
+    if (torrentIsDone) {
+      isSeeder ?
+        setMagnet(torrent.magnetURI) :
+        torrent.files[0].getBlobURL((err, url) => {
+          setStatus(Object.assign(InitialState, { url, filename: torrent.files[0].name }));
+          clearInterval(logger);
+        });
     }
   }, 500);
 };
 
-const upload = (client, files, setMagnet, progressLogger, setAllowedPeers) => {
+const upload = (files, log, setAllowedPeers) => {
   client.on(ClientEvents.PEER_STATUS_CHANGED, ({ allowedPeers }) => setAllowedPeers(allowedPeers))
-  client.seed(files, (torrent) => {
-    setMagnet(torrent.magnetURI);
-    progressLogger(torrent, "Seeding");
-  });
+  client.seed(files, (torrent) => log(torrent));
 };
-
-const download = (client, torrentOrigin, progressLogger, setClient) => {
-  var torrentId = torrentOrigin || "https://webtorrent.io/torrents/sintel.torrent";
-  client.on(ClientEvents.CLIENT_RESET, torrent => progressLogger(torrent, "Leeching"));
-  client.add(torrentId, torrent => progressLogger(torrent, "Leeching"));
-};
-
 
 function App () {
-  let logger;
-  const [client, setClient] = useState(initialClient);
-  const [status, setStatus] = useState(initialState);
+  let loggerId;
+  const [status, setStatus] = useState(InitialState);
   const [seedMagnet, setSeedMagnet] = useState("");
   const [allowedPeers, setAllowedPeers] = useState({});
   const [leechMagnet, setLeechMagnet] = useState("");
+  const log = progressLogger(loggerId, status, setStatus, setSeedMagnet);
 
   return (
     <div className="App">
@@ -89,67 +72,30 @@ function App () {
         <input
           type="file"
           name="upload"
-          onChange={event =>
-            upload(
-              client,
-              event.target.files,
-              setSeedMagnet,
-              progressLogger(logger, status, setStatus),
-              setAllowedPeers
-            )
-          }
+          onChange={event => upload(event.target.files, log, setAllowedPeers)}
         />
-        {
-          Object.values(allowedPeers).map(({ id, allowed }) => {
-            return (
-              <React.Fragment key={id}>
-                <br />
-                <button onClick={() => client.togglePeer(id)}>
-                  {id} - {allowed ? "" : "NOT"} Allowed
-                </button>
-              </React.Fragment>
-            );
-          })
+        {Object.values(allowedPeers).map(({ id, allowed }) =>
+          <button className={"peerStatus-" + allowed} key={id} onClick={() => client.togglePeer(id)}>{id}</button>
+        )}
+        {!seedMagnet ?
+          false :
+          <div className="magnetLink" title="Copy and share this so that others can download the file" >{seedMagnet}</div>
         }
-        {seedMagnet ? (
-          <p>
-            <h3>Share this to share the file</h3>
-            <code>{seedMagnet}</code>
-          </p>
-        ) : (
-            false
-          )}
       </div>
 
       <div className="hero" id="hero-leecher">
         <h2>Leecher</h2>
         <h5>Insert a magnet URI to download</h5>
+        <input type="text" name="download" onChange={event => setLeechMagnet(event.target.value)} />
         <br />
-        <input
-          type="text"
-          name="download"
-          onChange={event => setLeechMagnet(event.target.value)}
-        />
-        <br />
-
         {!!status.url ?
-          <>
-            <br />
-            <a href={status.url} download={status.filename}>Download {status.filename}</a>
-          </>
-          : <button
-            onClick={() =>
-              download(
-                client,
-                leechMagnet,
-                progressLogger(logger, status, setStatus),
-                setClient
-              )
-            }
-          >
-            START DOWNLOAD
-        </button>}
-
+          <a href={status.url} download={status.filename}> Download {status.filename}</a> :
+          <div>
+            <button onClick={() => client.add(leechMagnet || "https://webtorrent.io/torrents/sintel.torrent", torrent => log(torrent))}>
+              START DOWNLOAD
+          </button>
+          </div>
+        }
       </div>
     </div>
   );
