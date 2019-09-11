50277f136f4bcc53784611a15e4e6083399a3913
Mike Kerzhner <mikerzhner@gmail.com>
2019-09-11 12:29:33 -0700

    Apply prettier to ignored contracts

--- new-packages/nitro-protocol/contracts/ConsensusApp.sol
+++ new-packages/nitro-protocol/contracts/ConsensusApp.sol
@@ -19,19 +19,39 @@ contract ConsensusApp is ForceMoveApp {
         uint256 turnNumB, // unused
         uint256 numParticipants
     ) public pure returns (bool) {
-
         ConsensusAppData memory appDataA = appData(a.appData);
         ConsensusAppData memory appDataB = appData(b.appData);
 
-        if(appDataB.furtherVotesRequired == numParticipants - 1) { // propose/veto/pass
-            require(identical(a.outcome, b.outcome), 'ConsensusApp: when proposing/vetoing/passing outcome must not change');
-        } else if (appDataB.furtherVotesRequired == 0 ) { // final vote
-            require(appDataA.furtherVotesRequired == 1,'ConsensusApp: invalid final vote, furtherVotesRequired must transition from 1');
-            require(identical(appDataA.proposedOutcome, b.outcome), 'ConsensusApp: invalid final vote, outcome must equal previous proposedOutcome');
-        } else { // vote
-            require(appDataB.furtherVotesRequired == appDataA.furtherVotesRequired - 1, 'ConsensusApp: invalid vote, furtherVotesRequired should decrement');
-            require(identical(a.outcome, b.outcome), 'ConsensusApp: when voting, outcome must not change');
-            require(identical(appDataA.proposedOutcome, appDataB.proposedOutcome), 'ConsensusApp: invalid vote, proposedOutcome must not change');
+        if (appDataB.furtherVotesRequired == numParticipants - 1) {
+            // propose/veto/pass
+            require(
+                identical(a.outcome, b.outcome),
+                'ConsensusApp: when proposing/vetoing/passing outcome must not change'
+            );
+        } else if (appDataB.furtherVotesRequired == 0) {
+            // final vote
+            require(
+                appDataA.furtherVotesRequired == 1,
+                'ConsensusApp: invalid final vote, furtherVotesRequired must transition from 1'
+            );
+            require(
+                identical(appDataA.proposedOutcome, b.outcome),
+                'ConsensusApp: invalid final vote, outcome must equal previous proposedOutcome'
+            );
+        } else {
+            // vote
+            require(
+                appDataB.furtherVotesRequired == appDataA.furtherVotesRequired - 1,
+                'ConsensusApp: invalid vote, furtherVotesRequired should decrement'
+            );
+            require(
+                identical(a.outcome, b.outcome),
+                'ConsensusApp: when voting, outcome must not change'
+            );
+            require(
+                identical(appDataA.proposedOutcome, appDataB.proposedOutcome),
+                'ConsensusApp: invalid vote, proposedOutcome must not change'
+            );
         }
         return true;
     }
