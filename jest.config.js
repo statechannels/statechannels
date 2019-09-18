9a298bd94043978dd632b1bb7d66d01c9e4d0081
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-18 17:01:12 -0300

    dropped jest config in favour of package.json config

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reporters: ['default', 'jest-junit'],
-  testMatch: ['<rootDir>/tests/?(*.)+(test).js'],
-  globalSetup: './tests/setup.js',
-  globalTeardown: './tests/teardown.js'
-};
