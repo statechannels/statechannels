a3e58ffc8cc4c173607cfb4600f9fd99ccf31300
Santiago Persico <santiagopersico@gmail.com>
2019-09-18 16:41:45 -0300

    refactors tests, adds testing of time related leeching

--- tests/basic-functionality.test.js
+++ tests/basic-functionality.test.js
@@ -1,37 +1,29 @@
-import fixtures from 'webtorrent-fixtures';
 import WebTorrent from './../src/web3torrent-lib';
+import { defaultFile, defaultSeedingOptions, defaultTorrentHash, defaultFileMagnetURI } from './utils';
 
 describe('Base Seeding', () => {
   it('Seed and remove a Torrent', done => {
-    if (typeof Blob === 'undefined') return t.end();
     expect.assertions(7);
     const client = new WebTorrent({ dht: false });
     expect(client.pseAccount).not.toBeNaN();
-    client.on('error', err => {
-      fail(err);
-    });
-    client.on('warning', err => {
-      fail(err);
-    });
-    client.seed(
-      new Blob([fixtures.leaves.content]),
-      { name: 'Leaves of Grass by Walt Whitman.epub', announce: [process.env.TRACKER_URL] },
-      torrent => {
-        expect(client.torrents.length).toEqual(1);
-        expect(torrent.infoHash).toEqual(fixtures.leaves.parsedTorrent.infoHash);
-        expect(torrent.magnetURI).toEqual(
-          `${fixtures.leaves.magnetURI}&tr=${encodeURIComponent(process.env.TRACKER_URL)}`
-        );
+    
+    client.on('error', err => fail(err));
+    client.on('warning', err => fail(err));
+    
+    client.seed(defaultFile, defaultSeedingOptions(false), torrent => {
+      expect(client.torrents.length).toEqual(1);
+      expect(torrent.infoHash).toEqual(defaultTorrentHash);
+      expect(torrent.magnetURI).toEqual(defaultFileMagnetURI);
 
-        client.remove(torrent, err => {
+      client.remove(torrent, err => {
+        expect(err).toBeFalsy();
+        expect(client.torrents.length).toEqual(0);
+        client.destroy(err => {
           expect(err).toBeFalsy();
-          expect(client.torrents.length).toEqual(0);
-          client.destroy(err => {
-            expect(err).toBeFalsy();
-            done();
-          });
+          done();
         });
-      }
+      });
+    }
     );
   });
 
