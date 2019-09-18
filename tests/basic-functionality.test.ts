cc7c79e595c0a35cb5dd8f1794b9291c3efd53e3
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-18 17:51:09 -0300

    typescripted all the tests!

--- tests/basic-functionality.test.ts
+++ tests/basic-functionality.test.ts
@@ -1,16 +1,16 @@
-import WebTorrent from './../src/web3torrent-lib';
-import { defaultFile, defaultSeedingOptions, defaultTorrentHash, defaultFileMagnetURI } from './utils';
+import WebTorrentPaidStreamingClient from './../src/web3torrent-lib';
+import { defaultFile, defaultFileMagnetURI, defaultSeedingOptions, defaultTorrentHash } from './utils';
 
 describe('Base Seeding', () => {
   it('Seed and remove a Torrent', done => {
     expect.assertions(7);
-    const client = new WebTorrent({ dht: false });
+    const client = new WebTorrentPaidStreamingClient({ dht: false });
     expect(client.pseAccount).not.toBeNaN();
-    
+
     client.on('error', err => fail(err));
     client.on('warning', err => fail(err));
-    
-    client.seed(defaultFile, defaultSeedingOptions(false), torrent => {
+
+    client.seed(defaultFile as File, defaultSeedingOptions(false), torrent => {
       expect(client.torrents.length).toEqual(1);
       expect(torrent.infoHash).toEqual(defaultTorrentHash);
       expect(torrent.magnetURI).toEqual(defaultFileMagnetURI);
@@ -23,9 +23,6 @@ describe('Base Seeding', () => {
           done();
         });
       });
-    }
-    );
+    });
   });
-
-  it;
 });
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,9 +1,10 @@
-import fixtures from 'webtorrent-fixtures';
 import MemoryChunkStore from 'memory-chunk-store';
+import fixtures from 'webtorrent-fixtures';
 
 export const defaultFile = new Blob([fixtures.leaves.content]);
 export const defaultTorrentHash = fixtures.leaves.parsedTorrent.infoHash;
-export const defaultFileMagnetURI = `${fixtures.leaves.magnetURI}&tr=${encodeURIComponent(process.env.TRACKER_URL)}`;
+export const defaultFileMagnetURI = `${fixtures.leaves.magnetURI}&tr=${encodeURIComponent(process.env
+  .TRACKER_URL as string)}`;
 export const defaultSeedingOptions = (randomizeName = true) => ({
   name: (randomizeName ? Math.random() * 1000 : '') + 'Leaves of Grass by Walt Whitman.epub',
   announce: [process.env.TRACKER_URL],
