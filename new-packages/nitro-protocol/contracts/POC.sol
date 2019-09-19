0c8a7b1102a769fd7deba0354d74ed9bb5df0110
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-09-19 15:44:00 -0600

    POC.sol uses bytes32 not uint256

--- new-packages/nitro-protocol/contracts/POC.sol
+++ new-packages/nitro-protocol/contracts/POC.sol
@@ -10,11 +10,24 @@ contract POC {
         bytes32 outcomeHash;
     }
 
-    function getHash(ChannelStorage memory channelStorage) public pure returns (uint256 newHash) {
-        newHash = uint256(keccak256(abi.encode(channelStorage))) >> 160;
-        newHash |= channelStorage.turnNumRecord << 160;
-        newHash |= channelStorage.finalizesAt << 208;
-        return newHash;
+    function getHashedStorage(ChannelStorage memory channelStorage)
+        public
+        pure
+        returns (bytes32 newHash)
+    {
+        // The hash is constructed from left to right.
+        uint256 result;
+
+        // Shift finalizesAt 256 - 48 = 208 bits left to fill the first 48 bits
+        result = uint256(channelStorage.finalizesAt) << 208;
+
+        // logical or with turnNumRecord padded with 160 zeros to get the next 48 bits
+        result = result | (channelStorage.turnNumRecord << 160);
+
+        // logical or with the last 160 bits of the hash of the encoded storage
+        result = result | uint256(uint160(uint256(keccak256(abi.encode(channelStorage)))));
+
+        newHash = bytes32(result);
     }
 
     function getData(uint256 storageHash)
@@ -24,10 +37,10 @@ contract POC {
     {
         fingerprint = uint160(storageHash);
         turnNumRecord = uint48(storageHash >> 160);
-        finalizesAt = uint48(storageHash >> 208);
+        finalizesAt = uint48(storageHash >> (160 + 48));
     }
 
-    function matchesHash(ChannelStorage memory cs, uint256 h) public pure returns (bool) {
-        return getHash(cs) == h;
+    function matchesHash(ChannelStorage memory cs, bytes32 h) public pure returns (bool) {
+        return getHashedStorage(cs) == h;
     }
 }
