4e4dde896e798de71ccfd5d7d29aeaec7263d402
Alex Gap <alex.gap@gmail.com>
2019-09-19 09:04:51 -0700

    Only take in previous state

--- new-packages/nitro-protocol/src/validation.ts
+++ new-packages/nitro-protocol/src/validation.ts
@@ -5,12 +5,17 @@ import * as forceMoveApp from './contract/force-move-app';
 export async function validTransition(
   signer: ethers.Signer,
   newState: SignedState,
-  previousStates: SignedState[],
+  previousState?: SignedState,
 ): Promise<boolean> {
-  if (previousStates.length === 0) {
+  if (!previousState) {
     return true; // Allows validTransition to be called without the caller inspecting how many previous states there are
   }
-  const lastState = previousStates[previousStates.length - 1];
-  const appAddress = lastState.state.appDefinition;
-  return await forceMoveApp.validTransition(lastState.state, newState.state, appAddress, signer);
+
+  const appAddress = previousState.state.appDefinition;
+  return await forceMoveApp.validTransition(
+    previousState.state,
+    newState.state,
+    appAddress,
+    signer,
+  );
 }
