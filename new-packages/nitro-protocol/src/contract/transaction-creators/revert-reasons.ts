f15c485d7845cf7d109d96a6721579693018e475
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-09-20 15:12:22 -0600

    Create revert-reasons.ts

---
+++ new-packages/nitro-protocol/src/contract/transaction-creators/revert-reasons.ts
@@ -0,0 +1,15 @@
+// Generic messages
+export const CHANNEL_FINALIZED = 'Channel Finalized.';
+export const NO_ONGOING_CHALLENGE = 'No ongoing challenge.';
+export const TURN_NUM_RECORD_NOT_INCREASED = 'turnNumRecord not increased.';
+export const WRONG_CHANNEL_STORAGE = 'Channel storage does not match stored version.';
+export const CHANNEL_NOT_OPEN = 'Channel not open.';
+export const INVALID_SIGNATURES = 'Invalid signatures';
+export const WHO_SIGNED_WHAT_WRONG_LENGTH =
+  '_validSignatures: whoSignedWhat must be the same length as participants';
+export const UNACCEPTABLE_WHO_SIGNED_WHAT = 'Unacceptable whoSignedWhat array';
+
+// Function-specific messages
+export const CHALLENGER_NON_PARTICIPANT = 'Challenger is not a participant';
+export const RESPONSE_UNAUTHORIZED = 'Response not signed by authorized mover';
+export const WRONG_REFUTATION_SIGNATURE = 'Refutation state not signed by challenger';
