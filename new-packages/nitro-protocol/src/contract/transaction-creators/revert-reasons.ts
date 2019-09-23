964c5266c6b33509e18835fa6b805747c755fee2
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-09-23 09:43:44 -0600

    Sort revert-reasons

--- new-packages/nitro-protocol/src/contract/transaction-creators/revert-reasons.ts
+++ new-packages/nitro-protocol/src/contract/transaction-creators/revert-reasons.ts
@@ -1,15 +1,15 @@
 // Generic messages
 export const CHANNEL_FINALIZED = 'Channel finalized.';
 export const CHANNEL_NOT_FINALIZED = 'Channel not finalized.';
-export const NO_ONGOING_CHALLENGE = 'No ongoing challenge.';
-export const TURN_NUM_RECORD_NOT_INCREASED = 'turnNumRecord not increased.';
-export const TURN_NUM_RECORD_DECREASED = 'turnNumRecord decreased.';
-export const WRONG_CHANNEL_STORAGE = 'Channel storage does not match stored version.';
 export const CHANNEL_NOT_OPEN = 'Channel not open.';
 export const INVALID_SIGNATURES = 'Invalid signatures';
+export const NO_ONGOING_CHALLENGE = 'No ongoing challenge.';
+export const TURN_NUM_RECORD_DECREASED = 'turnNumRecord decreased.';
+export const TURN_NUM_RECORD_NOT_INCREASED = 'turnNumRecord not increased.';
+export const UNACCEPTABLE_WHO_SIGNED_WHAT = 'Unacceptable whoSignedWhat array';
 export const WHO_SIGNED_WHAT_WRONG_LENGTH =
   '_validSignatures: whoSignedWhat must be the same length as participants';
-export const UNACCEPTABLE_WHO_SIGNED_WHAT = 'Unacceptable whoSignedWhat array';
+export const WRONG_CHANNEL_STORAGE = 'Channel storage does not match stored version.';
 
 // Function-specific messages
 export const CHALLENGER_NON_PARTICIPANT = 'Challenger is not a participant';
