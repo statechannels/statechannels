0de820e106437bc1dadf09e4e2626908672870c9
Alex Gap <alex.gap@gmail.com>
2019-09-23 13:39:50 -0700

    Deploy ETHAssetHolder

---
+++ packages/wallet/packages/wallet/contracts/AssetHolder.sol
@@ -0,0 +1,2 @@
+// This is seperate from Nitro.sol to prevent the errror "DeclarationError: Identifier already declared."
+import "nitro-protocol/contracts/ETHAssetHolder.sol";
--- packages/wallet/packages/wallet/migrations/2-deploy-contracts.js
+++ packages/wallet/packages/wallet/migrations/2-deploy-contracts.js
@@ -1,10 +1,12 @@
-
 const NitroAdjudicator = artifacts.require('NitroAdjudicator');
 const ConsensusApp = artifacts.require('ConsensusApp');
+const ETHAssetHolder = artifacts.require('ETHAssetHolder');
 
 module.exports = async function(deployer) {
   deployer.then(async () => {
-    await deployer.deploy(NitroAdjudicator)
+    await deployer.deploy(NitroAdjudicator);
     await deployer.deploy(ConsensusApp);
+    const NitroAdjudicatorInstance = await NitroAdjudicator.deployed();
+    await deployer.deploy(ETHAssetHolder, NitroAdjudicatorInstance.address);
   });
 };
