2ebeaf453a9dd5a74d7cf46d66fc4e7583c34a0e
George Knee <georgeknee@googlemail.com>
2019-09-26 10:02:46 +0100

    NatSpec the IForceMove abstract contract

--- packages/nitro-protocol/contracts/interfaces/IForceMove.sol
+++ packages/nitro-protocol/contracts/interfaces/IForceMove.sol
@@ -3,6 +3,9 @@ pragma experimental ABIEncoderV2;
 
 import './ForceMoveApp.sol';
 
+/**
+  * @dev The ForceMove contract allows state channels to be adjudicated and finalized.
+*/
 contract IForceMove {
     struct Signature {
         uint8 v;
@@ -45,11 +48,17 @@ contract IForceMove {
 
     // Public methods:
 
-    function getData(bytes32 channelId)
-        public
-        view
-        returns (uint48 finalizesAt, uint48 turnNumRecord, uint160 fingerprint);
-
+    /**
+    * @notice Registers a challenge against a state channel. A challenge will either prompt another participant into clearing the challenge (via one of the other methods), or cause the channel to finalize at a specific time.
+    * @dev Registers a challenge against a state channel. A challenge will either prompt another participant into clearing the challenge (via one of the other methods), or cause the channel to finalize at a specific time.
+    * @param fixedPart Data describing properties of the state channel that do not change with state updates.
+    * @param largestTurnNum The largest turn number of the submitted states; will overwrite the stored value of `turnNumRecord`.
+    * @param variableParts An ordered array of structs, each decribing the properties of the state channel that may change with each state update.
+    * @param isFinalCount Describes how many of the submitted states have the `isFinal` property set to `true`. It is implied that the rightmost `isFinalCount` states are final, and the rest are not final.
+    * @param sigs An array of signatures that support the state with the `largestTurnNum`.
+    * @param whoSignedWhat An array denoting which participant has signed which state: `participant[i]` signed the state with index `whoSignedWhat[i]`.
+    * @param challengerSig The signature of a participant on the keccak256 of the abi.encode of (supportedStateHash, 'forceMove').
+    */
     function forceMove(
         FixedPart memory fixedPart,
         uint48 largestTurnNum,
@@ -60,6 +69,15 @@ contract IForceMove {
         Signature memory challengerSig
     ) public;
 
+    /**
+    * @notice Repsonds to an ongoing challenge registered against a state channel.
+    * @dev Repsonds to an ongoing challenge registered against a state channel.
+    * @param challenger The address of the participant whom registered the challenge.
+    * @param isFinalAB An pair of booleans describing if the challenge state and/or the response state have the `isFinal` property set to `true`.
+    * @param fixedPart Data describing properties of the state channel that do not change with state updates.
+    * @param variablePartAB An pair of structs, each decribing the properties of the state channel that may change with each state update (for the challenge state and for the response state).
+    * @param sig The responder's signature on the `responseStateHash`.
+    */
     function respond(
         address challenger,
         bool[2] memory isFinalAB,
@@ -70,6 +88,16 @@ contract IForceMove {
         Signature memory sig
     ) public;
 
+    /**
+    * @notice Overwrites the `turnNumRecord` stored against a channel by providing a state with higher turn number, supported by a signature from each participant.
+    * @dev Overwrites the `turnNumRecord` stored against a channel by providing a state with higher turn number, supported by a signature from each participant.
+    * @param fixedPart Data describing properties of the state channel that do not change with state updates.
+    * @param largestTurnNum The largest turn number of the submitted states; will overwrite the stored value of `turnNumRecord`.
+    * @param variableParts An ordered array of structs, each decribing the properties of the state channel that may change with each state update.
+    * @param isFinalCount Describes how many of the submitted states have the `isFinal` property set to `true`. It is implied that the rightmost `isFinalCount` states are final, and the rest are not final.
+    * @param sigs An array of signatures that support the state with the `largestTurnNum`.
+    * @param whoSignedWhat An array denoting which participant has signed which state: `participant[i]` signed the state with index `whoSignedWhat[i]`.
+    */
     function checkpoint(
         FixedPart memory fixedPart,
         uint48 largestTurnNum,
@@ -77,6 +105,26 @@ contract IForceMove {
         uint8 isFinalCount, // how many of the states are final
         Signature[] memory sigs,
         uint8[] memory whoSignedWhat
+    ) public;
+
+    /**
+    * @notice Finalizes a channel by providing a finalization proof.
+    * @dev Overwrites the `turnNumRecord` stored against a channel by providing a state with higher turn number, supported by a signature from each participant.
+    * @param largestTurnNum The largest turn number of the submitted states; will overwrite the stored value of `turnNumRecord`.
+    * @param fixedPart Data describing properties of the state channel that do not change with state updates.
+    * @param appPartHash The keccak256 of the abi.encode of `(challengeDuration, appDefinition, appData)`. Applies to all states in the finalization proof.
+    * @param outcomeHash The keccak256 of the abi.encode of the `outcome`. Applies to all stats in the finalization proof.
+    * @param whoSignedWhat An array denoting which participant has signed which state: `participant[i]` signed the state with index `whoSignedWhat[i]`.
+    * @param sigs An array of signatures that support the state with the `largestTurnNum`.
+    */
+    function conclude(
+        uint48 largestTurnNum,
+        FixedPart memory fixedPart,
+        bytes32 appPartHash,
+        bytes32 outcomeHash,
+        uint8 numStates,
+        uint8[] memory whoSignedWhat,
+        Signature[] memory sigs
     ) public;
 
     // events
