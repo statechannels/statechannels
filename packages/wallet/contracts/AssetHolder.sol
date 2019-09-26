48f0aa2c2cd5e16ff8cdfda82d0e67d5528d9d61
Liam Horne <liam@lihorne.com>
2019-09-26 18:43:48 +0900

    Update CircleCI to use test:ci command

--- packages/wallet/contracts/AssetHolder.sol
+++ packages/wallet/contracts/AssetHolder.sol
@@ -1,2 +1,5 @@
-// This is seperate from Nitro.sol to prevent the errror "DeclarationError: Identifier already declared."
+pragma solidity ^0.5.11;
+pragma experimental ABIEncoderV2;
+
+// This is seperate from Nitro.sol to prevent the error "DeclarationError: Identifier already declared."
 import "@statechannels/nitro-protocol/contracts/ETHAssetHolder.sol";
