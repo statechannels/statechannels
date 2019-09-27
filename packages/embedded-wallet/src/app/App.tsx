c579cda35299e139989190ff74c6722b4b8b78d0
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-27 15:07:19 -0300

    app: updated routes and minimal tests

--- packages/embedded-wallet/src/app/App.tsx
+++ packages/embedded-wallet/src/app/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { JsonRpcRoute as Route, JsonRpcRouter as Router } from "../json-rpc-router";
-import { TestMessage } from "../message-handlers/TestMessage";
-import { UnicornMessage } from "../message-handlers/UnicornMessage";
+import { BudgetAllocation } from "../message-handlers";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Route method="chan_test" component={TestMessage} />
-      <Route method="chan_unicorn" component={UnicornMessage} />
+      <Route method="chan_allocate" component={BudgetAllocation} />
     </Router>
   );
 };
