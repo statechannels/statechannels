7059ebafe602a35646c6fa32cca60853e60474e0
Liam Horne <liam@lihorne.com>
2019-09-27 13:55:31 +0900

    Add TrivialApp to wallet migrations

--- packages/wallet/contracts/Nitro.sol
+++ packages/wallet/contracts/Nitro.sol
@@ -4,3 +4,4 @@ pragma experimental ABIEncoderV2;
 import { ConsensusApp } from "@statechannels/nitro-protocol/contracts/ConsensusApp.sol";
 import { ETHAssetHolder } from "@statechannels/nitro-protocol/contracts/ETHAssetHolder.sol";
 import { NitroAdjudicator } from "@statechannels/nitro-protocol/contracts/NitroAdjudicator.sol";
+import { TrivialApp } from "@statechannels/nitro-protocol/contracts/TrivialApp.sol";
