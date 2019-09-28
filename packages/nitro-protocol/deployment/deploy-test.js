4d124334c2fc7dfe055cfa38849d25c986372b5e
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-09-27 22:19:07 -0600

    Refactor deploy scripts in nitro-protocol

--- packages/nitro-protocol/deployment/deploy-test.js
+++ packages/nitro-protocol/deployment/deploy-test.js
@@ -1,8 +1,5 @@
-const ethers = require('ethers');
-const etherlime = require('etherlime-lib');
 const path = require('path');
 const writeJsonFile = require('write-json-file');
-const loadJsonFile = require('load-json-file');
 
 const testForceMoveArtifact = require('../build/contracts/TESTForceMove.json');
 const testNitroAdjudicatorArtifact = require('../build/contracts/TESTNitroAdjudicator.json');
@@ -14,61 +11,35 @@ const singleAssetPaymentsArtifact = require('../build/contracts/SingleAssetPayme
 const erc20AssetHolderArtifact = require('../build/contracts/ERC20AssetHolder.json');
 const ethAssetHolderArtifact = require('../build/contracts/ETHAssetHolder.json');
 const tokenArtifact = require('../build/contracts/Token.json');
+const {migrate, migrationFactory, deploy} = require('./deploy');
 
-let contractsToAddresses = {};
+async function deployTest(network, secret, etherscanApiKey) {
+  let {networkMap, deployer, networkId} = await deploy(network, secret, etherscanApiKey);
 
-async function getNetworkMap() {
-  try {
-    return await loadJsonFile(path.join(__dirname, 'network-map.json'));
-  } catch (err) {
-    if (!!err.message.match('ENOENT: no such file or directory')) {
-      return {};
-    } else {
-      throw err;
-    }
-  }
-}
-
-async function deployArtifact(deployer, artifact, constructorArgs = []) {
-  const contract = await deployer.deploy(artifact, false, ...constructorArgs);
-  // todo: contract name as a key does not hold enough information as there can be many version of a contract
-  contractsToAddresses = {
-    ...contractsToAddresses,
-    [artifact.contractName]: contract.contractAddress,
-  };
-}
-
-const deploy = async (network, secret, etherscanApiKey) => {
-  let networkMap = await getNetworkMap();
-  const deployer = new etherlime.EtherlimeGanacheDeployer();
-  const provider = new ethers.providers.JsonRpcProvider(deployer.nodeUrl);
-  const networkId = (await provider.getNetwork()).chainId;
-
-  await deployArtifact(deployer, testNitroAdjudicatorArtifact);
-  console.log(
-    `trying to pull out ${contractsToAddresses[testNitroAdjudicatorArtifact.contractName]}`,
-  );
-  await deployArtifact(deployer, ethAssetHolderArtifact, [
-    contractsToAddresses[testNitroAdjudicatorArtifact.contractName],
+  const startingMap = networkMap[networkId] || {};
+  const contractsToAddresses = await migrate(deployer, startingMap, [
+    migrationFactory(testNitroAdjudicatorArtifact),
+    migrationFactory(ethAssetHolderArtifact, currentMap => [
+      currentMap[testNitroAdjudicatorArtifact.contractName],
+    ]),
+    migrationFactory(erc20AssetHolderArtifact, currentMap => [
+      currentMap[testNitroAdjudicatorArtifact.contractName],
+      currentMap[tokenArtifact.contractName],
+    ]),
+    migrationFactory(testAssetHolderArtifact),
+    migrationFactory(trivialAppArtifact),
+    migrationFactory(countingAppArtifact),
+    migrationFactory(singleAssetPaymentsArtifact),
+    migrationFactory(testForceMoveArtifact),
   ]);
-
-  await deployArtifact(deployer, erc20AssetHolderArtifact, [
-    contractsToAddresses[testNitroAdjudicatorArtifact.contractName],
-    networkMap[networkId][tokenArtifact.contractName],
-  ]);
-  await deployArtifact(deployer, testAssetHolderArtifact);
-  await deployArtifact(deployer, trivialAppArtifact);
-  await deployArtifact(deployer, countingAppArtifact);
-  await deployArtifact(deployer, singleAssetPaymentsArtifact);
-  await deployArtifact(deployer, testForceMoveArtifact);
 
   networkMap = {
     ...networkMap,
     [networkId]: {...networkMap[networkId], ...contractsToAddresses},
   };
   await writeJsonFile(path.join(__dirname, 'network-map.json'), networkMap);
-};
+}
 
 module.exports = {
-  deploy,
+  deploy: deployTest,
 };
