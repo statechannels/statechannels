0a6210a2cd459e390e98ca55ec4bac76c2465cff
Santiago Persico <santiagopersico@gmail.com>
2019-09-27 22:24:05 -0300

    fix build (a11y button)

--- packages/web3torrent/src/components/layout/layout-header/LayoutHeader.tsx
+++ packages/web3torrent/src/components/layout/layout-header/LayoutHeader.tsx
@@ -10,7 +10,9 @@ const LayoutHeader: React.FC<RouteComponentProps> = props => {
   return (
     <header className="header">
       <nav className="header-content">
-        <a className="header-logo" href={RoutePath.Root}></a>
+        <a className="header-logo" href={RoutePath.Root}>
+          <span className="header-logo-hidden">Web3Torrent Logo - Go to Home </span>
+        </a>
         <div className="actions-container">
           <FormButton name="setup" onClick={() => props.history.push(RoutePath.Upload)}>
             Upload
