a63f1b56d225292f7105c14238f699ead7a48250
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-09-27 20:54:50 -0600

    Refactor wallet deploy scripts

--- packages/wallet/deployment/deploy-test.js
+++ packages/wallet/deployment/deploy-test.js
@@ -1,49 +1,26 @@
-const ethers = require('ethers');
-const etherlime = require('etherlime-lib');
 const path = require('path');
 const writeJsonFile = require('write-json-file');
-const loadJsonFile = require('load-json-file');
-
 const trivialAppArtifact = require('../build/contracts/TrivialApp.json');
 
-let contractsToAddresses = {};
+process.env.NODE_ENV = 'test'
+const {configureEnvVariables} = require('@statechannels/devtools');
+configureEnvVariables();
 
-async function getNetworkMap() {
-  try {
-    return await loadJsonFile(path.join(__dirname, 'network-map.json'));
-  } catch (err) {
-    if (!!err.message.match('ENOENT: no such file or directory')) {
-      return {};
-    } else {
-      throw err;
-    }
-  }
-}
+const { migrate, migrationFactory, deploy } = require('./deploy');
 
-async function deployArtifact(deployer, artifact, constructorArgs = []) {
-  const contract = await deployer.deploy(artifact, false, ...constructorArgs);
-  // todo: contract name as a key does not hold enough information as there can be many version of a contract
-  contractsToAddresses = {
-    ...contractsToAddresses,
-    [artifact.contractName]: contract.contractAddress,
-  };
-}
+const deployTest = async (network, secret, etherscanApiKey) => {
+  let { networkMap, deployer, networkId } = await deploy(network, secret, etherscanApiKey);
+  const startingMap = networkMap[networkId] || {};
+  const contractsToAddresses = await migrate(deployer, startingMap, [ migrationFactory(trivialAppArtifact) ])
 
-const deploy = async (network, secret, etherscanApiKey) => {
-  let networkMap = await getNetworkMap();
-  const deployer = new etherlime.EtherlimeGanacheDeployer();
-  const provider = new ethers.providers.JsonRpcProvider(deployer.nodeUrl);
-  const networkId = (await provider.getNetwork()).chainId;
-
-  await deployArtifact(deployer, trivialAppArtifact);
-
-  networkMap = {
+  const updatedNetworkMap = {
     ...networkMap,
     [networkId]: {...networkMap[networkId], ...contractsToAddresses},
   };
-  await writeJsonFile(path.join(__dirname, 'network-map.json'), networkMap);
+  
+  await writeJsonFile(path.join(__dirname, 'network-map.json'), updatedNetworkMap);
 };
 
 module.exports = {
-  deploy,
+  deploy: deployTest,
 };
--- packages/wallet/deployment/deploy.js
+++ packages/wallet/deployment/deploy.js
@@ -8,19 +8,32 @@ const ethAssetHolderArtifact = require('../build/contracts/ETHAssetHolder');
 const nitroAdjudicatorArtifact = require('../build/contracts/NitroAdjudicator');
 const consensusAppArtifact = require('../build/contracts/ConsensusApp');
 
-let contractsToAddresses = {};
+const {configureEnvVariables} = require('@statechannels/devtools');
+configureEnvVariables();
 
-async function deployArtifact(deployer, artifact, constructorArgs = []) {
-  const contract = await deployer.deploy(artifact, false, ...constructorArgs);
-  // todo: contract name as a key does not hold enough information as there can be many version of a contract
-  contractsToAddresses = {
-    ...contractsToAddresses,
-    [artifact.contractName]: contract.contractAddress,
-  };
+const migrationFactory = (artifact, argsConstructor = () => []) => {
+  return async (deployer, contractsToAddresses) => {
+    const args = argsConstructor(contractsToAddresses)
+    const contract = await deployer.deploy(artifact, false, ...args);
+    // todo: contract name as a key does not hold enough information
+    // as there can be many version of a contract
+    return {
+      ...contractsToAddresses,
+      [artifact.contractName]: contract.contractAddress,
+    };
+  }
+}
+
+const migrate = async (deployer, startingMap, migrations) => {
+  return await migrations.reduce(async (currentMap, migration ) => {
+    // TODO: Don't migrate if the contract is already on the given network
+    return migration(deployer, await currentMap)
+  }, startingMap)
 }
 
 const deploy = async (network, secret, etherscanApiKey) => {
   // todo: use network parameter to pick deployer.
+
   let networkMap;
   try {
     networkMap = await loadJsonFile(path.join(__dirname, '/network-map.json'));
@@ -31,20 +44,32 @@ const deploy = async (network, secret, etherscanApiKey) => {
       throw err;
     }
   }
-  const deployer = new etherlime.EtherlimeGanacheDeployer();
-  const provider = new ethers.providers.JsonRpcProvider(deployer.nodeUrl);
+  const deployer = new etherlime.EtherlimeGanacheDeployer(
+    // The privateKey is optional, but we have to provide it in order to provide a port.
+    new etherlime.EtherlimeGanacheDeployer().signer.privateKey, 
+    Number(process.env.GANACHE_PORT) 
+  )
+  if (!process.env.GANACHE_PORT) {
+    throw new Error(`Environment variable GANACHE_PORT undefined`);
+  }
+  const provider = new ethers.providers.JsonRpcProvider(`http://localhost:${process.env.GANACHE_PORT}`);
   const networkId = (await provider.getNetwork()).chainId;
 
-  await deployArtifact(deployer, nitroAdjudicatorArtifact);
-  await deployArtifact(deployer, ethAssetHolderArtifact, [
-    contractsToAddresses[nitroAdjudicatorArtifact.contractName],
-  ]);
-  await deployArtifact(deployer, consensusAppArtifact);
+  const startingMap = networkMap[networkId] || [];
+  const contractsToAddresses = await migrate(deployer, startingMap, [ 
+    migrationFactory(nitroAdjudicatorArtifact),
+    migrationFactory(ethAssetHolderArtifact, networkMap => [ networkMap[nitroAdjudicatorArtifact.contractName], ]),
+    migrationFactory(consensusAppArtifact)
+  ])
 
   updatedNetworkMap = {...networkMap, [networkId]: contractsToAddresses};
   await writeJsonFile(path.join(__dirname, 'network-map.json'), updatedNetworkMap);
+
+  return { networkMap: updatedNetworkMap, deployer, networkId };
 };
 
 module.exports = {
   deploy,
+  migrate,
+  migrationFactory
 };
