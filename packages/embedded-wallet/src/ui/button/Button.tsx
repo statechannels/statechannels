1f71109a46a21f3194c9b502cf277fc3b208ee14
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-28 11:16:24 -0300

    button: added support for icons

--- packages/embedded-wallet/src/ui/button/Button.tsx
+++ packages/embedded-wallet/src/ui/button/Button.tsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { Icon, Icons } from "../icon/Icon";
 import css from "./Button.module.css";
 
 export type ButtonProps = {
-  icon?: string;
+  icon?: Icons;
+  iconPosition?: "left" | "right";
   type: "primary" | "secondary";
   label: string;
   onClick: () => void;
 };
 
-const Button: React.FC<ButtonProps> = ({ type, label, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ type, label, onClick, icon, iconPosition = "right" }) => {
   return (
-    <button autoFocus={type === "primary"} onClick={onClick} className={`${css.button} ${css[type]}`}>
+    <button
+      autoFocus={type === "primary"}
+      onClick={onClick}
+      className={`${css.button} ${css[type]} ${icon ? css[`withIconToThe${iconPosition}`] : ""}`}
+    >
       {label}
+      {icon ? <Icon name={icon} color="inverse" /> : {}}
     </button>
   );
 };
