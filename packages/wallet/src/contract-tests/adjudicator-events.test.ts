0e696a402a656d6c7198d713b54acefd5ff8220a
Liam Horne <liam@lihorne.com>
2019-09-30 16:55:26 +0900

    Add husky precommit hook

--- packages/wallet/src/contract-tests/adjudicator-events.test.ts
+++ packages/wallet/src/contract-tests/adjudicator-events.test.ts
@@ -73,7 +73,6 @@ describe.skip("adjudicator listener", () => {
     const channelIdToIgnore = await getChannelId(provider, getNextNonce(), participantA, participantB);
     const processId = ethers.Wallet.createRandom().address;
     const sagaTester = new SagaTester({initialState: createWatcherState(processId, channelId)});
-
     sagaTester.start(adjudicatorWatcher, provider);
 
     await depositContract(provider, channelIdToIgnore);
