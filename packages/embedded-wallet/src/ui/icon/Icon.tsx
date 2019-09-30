22ddf7eb14002b63b93a09e06cf501d82e8565b2
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-30 11:38:06 -0300

    icon: added icon pack

--- packages/embedded-wallet/src/ui/icon/Icon.tsx
+++ packages/embedded-wallet/src/ui/icon/Icon.tsx
@@ -2,31 +2,30 @@ import React from "react";
 import css from "./Icon.module.css";
 
 export enum Icons {
-  OpenExternalSite = "openExternalSite",
-  Spinner = "spinner",
+  Check = "check",
+  ExternalLink = "externalLink",
   Hourglass = "hourglass",
-  Check = "check"
+  Link = "link"
 }
 
 export type IconProps = {
   name: Icons;
-  color?: "primary" | "inverse";
   decorative?: boolean;
 };
 
 const iconDescriptions: { [key in Icons]: string } = {
-  [Icons.OpenExternalSite]: "Icon of an arrow exiting a browser window, about to open a new page",
-  [Icons.Spinner]: "Icon of a rotating circle, indicating something is going on",
+  [Icons.ExternalLink]: "Icon of an arrow exiting a browser window, about to open a new page",
   [Icons.Hourglass]: "Icon of an hourglass with sand falling top-down, indicating a wait for something to happen",
-  [Icons.Check]: "Icon of a checkmark, indicating all is well"
+  [Icons.Check]: "Icon of a checkmark, indicating all is well",
+  [Icons.Link]: "Icon of a chain binding, indicating a connection between things"
 };
 
-const Icon: React.FC<IconProps> = ({ name, color = "primary", decorative }) => (
+const Icon: React.FC<IconProps> = ({ name, decorative }) => (
   <span
     aria-hidden={decorative}
     aria-label={iconDescriptions[name]}
     role="img"
-    className={`${css.icon} ${name} ${color}`}
+    className={`${css.icon} ${css[name]}`}
   ></span>
 );
 
