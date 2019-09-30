13b5dde3a858b934ecc087be4e5b05dd432d9c8f
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-30 14:21:49 -0300

    connect-to-hub: added screen

---
+++ packages/embedded-wallet/src/message-handlers/connect-to-hub/ConnectToHub.tsx
@@ -0,0 +1,59 @@
+import debug from "debug";
+import React, { useEffect, useState } from "react";
+import { JsonRpcComponentProps } from "../../json-rpc-router";
+import { closeWallet } from "../../message-dispatchers";
+import { Dialog } from "../../ui";
+import { FlowProcess } from "../../ui/flow-process/FlowProcess";
+import { FlowStep, FlowStepProps, FlowStepStatus } from "../../ui/flow-process/FlowStep";
+
+const log = debug("wallet:connect-to-hub");
+
+const ConnectToHub: React.FC<JsonRpcComponentProps> = () => {
+  const [steps, setSteps] = useState<FlowStepProps[]>([
+    {
+      title: "Deposit 5 ETH",
+      status: FlowStepStatus.InProgress
+    },
+    {
+      title: "Wait for TX to mine",
+      status: FlowStepStatus.Pending
+    },
+    {
+      title: "Wait for hub.com",
+      status: FlowStepStatus.Pending
+    },
+    {
+      title: "Done!",
+      status: FlowStepStatus.Pending
+    }
+  ]);
+
+  useEffect(() => {
+    if (steps[steps.length - 1].status !== FlowStepStatus.Done) {
+      setTimeout(() => {
+        const newSteps = [...steps];
+        const finishedStep = newSteps.findIndex(step => step.status === FlowStepStatus.InProgress);
+        newSteps[finishedStep].status = FlowStepStatus.Done;
+        log("step updated: %o", newSteps[finishedStep]);
+        if (newSteps[finishedStep + 1]) {
+          newSteps[finishedStep + 1].status = FlowStepStatus.InProgress;
+        } else {
+          setTimeout(() => closeWallet(), 1000);
+        }
+        setSteps(newSteps);
+      }, 1000);
+    }
+  }, [steps]);
+
+  return (
+    <Dialog title="Connect to Hub" onClose={closeWallet}>
+      <FlowProcess>
+        {steps.map((step, index) => (
+          <FlowStep key={`step${index}`} {...step} />
+        ))}
+      </FlowProcess>
+    </Dialog>
+  );
+};
+
+export { ConnectToHub };
