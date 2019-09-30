d4107a6aac79d865e513265a09798f49a94ff2f2
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-30 11:39:06 -0300

    no-hub: added demo to dummy app

--- packages/embedded-wallet/src/app/App.tsx
+++ packages/embedded-wallet/src/app/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { JsonRpcRoute as Route, JsonRpcRouter as Router } from "../json-rpc-router";
-import { BudgetAllocation } from "../message-handlers";
+import { BudgetAllocation, NoHub } from "../message-handlers";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Route method="chan_allocate" component={BudgetAllocation} />
+      <Route method="chan_noHub" component={NoHub} />
     </Router>
   );
 };
