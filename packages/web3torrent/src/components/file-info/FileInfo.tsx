453e9ba61cc601b366fabdd9069d61a4d583d08a
Santiago Persico <santiagopersico@gmail.com>
2019-09-30 23:29:49 -0300

    improve File Info component

--- packages/web3torrent/src/components/file-info/FileInfo.tsx
+++ packages/web3torrent/src/components/file-info/FileInfo.tsx
@@ -1,15 +1,60 @@
 import React from "react";
 import "./FileInfo.scss";
+import { ProgressBar } from "./progress-bar/ProgressBar";
+import { FormButton } from "../form";
 
 export type FileInfoProps = { file: any };
 
 const FileInfo: React.FC<FileInfoProps> = ({ file }: FileInfoProps) => {
   return (
-    <section className="fileInfo">
-      <span className="fileName">{file.filename}</span>
-      <span className="fileSize">{file.size}Mb</span>
-      <span className="fileCost">Est. cost ${file.cost}</span>
-    </section>
+    <>
+      <section className={`fileInfo ${file.link ? " with-link" : ""}`}>
+        <span className="fileName">{file.filename}</span>
+        <span className="fileSize">{file.size}Mb</span>
+        {file.status ? <span className="fileStatus">{file.status}</span> : false}
+        <span className="fileCost">Est. cost ${file.cost}</span>
+        {file.link ? (
+          <a href={file.link} className="fileLink">
+            Share Link
+          </a>
+        ) : (
+          false
+        )}
+      </section>
+      {!file.downloaded ? (
+        false
+      ) : (
+        <section className="downloadingInfo">
+          <ProgressBar
+            downloaded={file.downloaded}
+            size={file.size}
+            status={file.downloaded !== file.size ? "Downloading" : "Completed"}
+          />
+          <p>
+            ETA 1m30s. 500Kbits/s down, 500Kbits/s up <br />
+            Connected to <strong>10</strong> peers.
+          </p>
+          {file.downloaded !== file.size ? (
+            false
+          ) : (
+            <FormButton name="save-download" onClick={() => null}>
+              Save Download
+            </FormButton>
+          )}
+        </section>
+      )}
+      {!file.peersConnected ? (
+        false
+      ) : (
+        <section className="uploadingInfo">
+          <p>
+            Total Recieved: <strong>$1.34</strong>
+            <br />
+            <strong>{file.peersConnected}</strong> Peers connected
+          </p>
+        </section>
+      )}
+    </>
   );
 };
 
---
+++ packages/web3torrent/src/components/file-info/progress-bar/ProgressBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ProgressBar.scss";
+
+export type ProgressBarProps = {
+  downloaded: number;
+  size: number;
+  status: "Completed" | "Downloading";
+  stopAction?: () => void;
+};
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({
+  downloaded,
+  size,
+  status = "Downloading"
+}: ProgressBarProps) => {
+  return (
+    <div className="progress-bar" data-label={downloaded + "/" + size}>
+      <span className="filler" style={{ width: `${(downloaded / size) * 100}%` }} />
+      <span className="bar-progress">
+        {downloaded}Mb/{size}Mb
+      </span>
+      <span className="bar-status">{status}</span>
+      <button type="button" className="bar-cancelButton">
+        X
+      </button>
+    </div>
+  );
+};
