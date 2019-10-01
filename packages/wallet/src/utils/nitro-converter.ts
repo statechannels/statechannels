6107efd0a02cf21de493390835fdce1367914317
cf19drofxots <nima@l4v.io>
2019-10-01 12:19:38 +0900

    fix: partial revert of code from merge

--- packages/wallet/src/utils/nitro-converter.ts
+++ packages/wallet/src/utils/nitro-converter.ts
@@ -9,13 +9,44 @@ import {
   Channel as NitroChannel,
   Signatures,
   AllocationItem,
-  Outcome
+  Outcome,
+  isAllocationOutcome
 } from "@statechannels/nitro-protocol";
 
 const CHALLENGE_DURATION = 0x12c; // 5 minutes
 // This temporarily handles converting fmg-core entities to nitro-protocol entities
 // Eventually once nitro-protocol is more properly embedded in the wallet this will go away
 
+export function convertStateToCommitment(state: State): Commitment {
+  const { channel: nitroChannel, turnNum } = state;
+  const convertedOutcome = convertOutcomeToAllocation(state.outcome);
+  const fmgChannel: Channel = {
+    participants: nitroChannel.participants,
+    channelType: state.appDefinition,
+    nonce: bigNumberify(nitroChannel.channelNonce).toNumber(),
+    guaranteedChannel: convertedOutcome.guaranteedChannel,
+  };
+
+   let commitmentType = CommitmentType.App;
+  // TODO: If this method is being for prefund/postfund states
+  // we'll need the correct commitmentCount
+  const commitmentCount = 0;
+  if (state.isFinal) {
+    commitmentType = CommitmentType.Conclude;
+  } else if (state.turnNum < nitroChannel.participants.length) {
+    commitmentType = CommitmentType.PreFundSetup;
+  } else if (state.turnNum < 2 * nitroChannel.participants.length) {
+    commitmentType = CommitmentType.PostFundSetup;
+  }
+  return {
+    ...convertedOutcome,
+    channel: fmgChannel,
+    turnNum,
+    commitmentType,
+    commitmentCount,
+    appAttributes: state.appData,
+  };
+}
 
 export function convertCommitmentToSignedState(commitment: Commitment, privateKey: string): SignedState {
   const state = convertCommitmentToState(commitment);
@@ -98,4 +129,27 @@ function convertGuaranteeToOutcome({ guaranteedChannel, destination }: { guarant
     destinations: destination.map(convertAddressToBytes32).map(d => d.toLowerCase()),
   };
   return [{ assetHolderAddress: ETH_ASSET_HOLDER_ADDRESS, guarantee }];
+}
+
+function convertOutcomeToAllocation(outcome: Outcome): { allocation: string[]; destination: string[]; guaranteedChannel?: string } {
+  const allocation: string[] = [];
+  let destination: string[] = [];
+  let guaranteedChannel;
+  // We will only be dealing with one outcome initially, since we're converting from a commitment
+  if (outcome.length !== 1) {
+    throw new Error(`Currently the wallet only supports one outcome, however there were ${outcome.length} outcomes.`);
+  }
+  const assetOutcome = outcome[0];
+  if (isAllocationOutcome(assetOutcome)) {
+    assetOutcome.allocation.forEach(a => {
+      allocation.push(a.amount);
+      destination.push(convertBytes32ToAddress(a.destination).toLowerCase());
+    });
+  } else {
+    const { guarantee } = assetOutcome;
+    destination = destination.concat(guarantee.destinations.map(convertBytes32ToAddress).map(d => d.toLowerCase()));
+    guaranteedChannel = convertBytes32ToAddress(guarantee.targetChannelId);
+  }
+
+   return { allocation, destination, guaranteedChannel };
 }
