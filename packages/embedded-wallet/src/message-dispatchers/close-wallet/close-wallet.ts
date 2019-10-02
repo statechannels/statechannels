2d2bbd200aa5f6c416d360612eeb97350d8752d5
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-10-02 17:17:10 -0300

    [embedded-wallet] Support Request/Response call style (#170)
    * bridge: rewritten as class, added request()
    
    * embedded-wallet: moved to port 1701 🖖
    
    * demo-app: using new request/response call style
    
    * message-dispatchers: added logs, allocate response
    
    * connect-to-hub: added allocate response call

--- packages/embedded-wallet/src/message-dispatchers/close-wallet/close-wallet.ts
+++ packages/embedded-wallet/src/message-dispatchers/close-wallet/close-wallet.ts
@@ -1,4 +1,9 @@
+import debug from 'debug';
+
+const log = debug('wallet:dispatch');
+
 const closeWallet = () => {
+  log('Sending: %o', 'ui:wallet:close');
   window.parent.postMessage('ui:wallet:close', '*');
 };
 
