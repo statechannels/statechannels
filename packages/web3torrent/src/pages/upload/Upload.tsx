e36f7059ecac416d24f93f0eb40fc8ac2277840f
Santiago Persico <spersico@users.noreply.github.com>
2019-10-02 17:09:36 -0300

    [web3torrent] Uploads mock (#169)
    * refactor torrent info, add uploads mock
    
    * fix colors in progress bar
    
    * fix joel's comments

--- packages/web3torrent/src/pages/upload/Upload.tsx
+++ packages/web3torrent/src/pages/upload/Upload.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {RouteComponentProps} from 'react-router-dom';
 import {FormButton} from '../../components/form';
+import {TorrentInfo} from '../../components/torrent-info/TorrentInfo';
+import {Torrent} from '../../types';
 import './Upload.scss';
 
-const Upload: React.FC<RouteComponentProps> = () => {
+const UploadStart = ({
+  setTorrent,
+  torrent
+}: {
+  setTorrent: (torrent: Torrent) => void;
+  torrent: Torrent;
+}) => {
   return (
     <section className="section fill">
       <div className="jumbotron-upload"></div>
       <div className="upload-action-bar">
         <label htmlFor="file">Select file to upload</label>
         <input type="file" name="file" id="file" className="inputfile"></input>
-        <FormButton name="start" onClick={() => null}>
+        <FormButton
+          name="start"
+          onClick={() => setTorrent({...torrent, uploaded: 250, length: 350})}
+        >
           Start
         </FormButton>
       </div>
@@ -21,6 +32,29 @@ const Upload: React.FC<RouteComponentProps> = () => {
         </p>
       </div>
     </section>
+  );
+};
+
+const Upload: React.FC<RouteComponentProps> = () => {
+  const [torrent, setTorrent] = useState({
+    name: 'File_1.dat',
+    length: 0,
+    numSeeds: 27,
+    numPeers: 4,
+    cost: 0.5,
+    downloaded: 0,
+    files: [],
+    status: 'Seeding',
+    magnetURI: '#'
+  } as Torrent);
+  return (
+    <>
+      {!torrent.length ? (
+        <UploadStart torrent={torrent} setTorrent={setTorrent} />
+      ) : (
+        <TorrentInfo torrent={torrent} />
+      )}
+    </>
   );
 };
 
