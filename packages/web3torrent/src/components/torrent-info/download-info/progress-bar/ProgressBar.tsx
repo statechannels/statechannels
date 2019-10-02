ed8d895be3a40e76a203d756cade5067d99b1c51
Santiago Persico <santiagopersico@gmail.com>
2019-10-02 16:57:04 -0300

    fix colors in progress bar

--- packages/web3torrent/src/components/torrent-info/download-info/progress-bar/ProgressBar.tsx
+++ packages/web3torrent/src/components/torrent-info/download-info/progress-bar/ProgressBar.tsx
@@ -14,18 +14,34 @@ export const ProgressBar: React.FC<ProgressBarProps> = ({
   status = 'Downloading'
 }: ProgressBarProps) => {
   return (
-    <div className="progress-bar" data-label={downloaded + '/' + size}>
-      <span className="filler" style={{width: `${(downloaded / size) * 100}%`}} />
-      <span className="bar-progress">
-        {downloaded}Mb/{size}Mb
-      </span>
-      <span className="bar-status">{status}</span>
-      <button type="button" className="bar-cancelButton">
-        <svg>
-          <path d="M0,20,20,0" />
-          <path d="M0,0,20,20" />
-        </svg>
-      </button>
+    <div className="progress-bar">
+      <div
+        className={`positive ${downloaded === size ? ' complete' : ''}`}
+        style={{width: `${(downloaded / size) * 100}%`}}
+      >
+        <span className="bar-progress">
+          {downloaded}Mb/{size}Mb
+        </span>
+        <span className="bar-status">{status}</span>
+        <button type="button" className="bar-cancelButton">
+          <svg>
+            <path d="M0,20,20,0" />
+            <path d="M0,0,20,20" />
+          </svg>
+        </button>
+      </div>
+      <div className="negative">
+        <span className="bar-progress">
+          {downloaded}Mb/{size}Mb
+        </span>
+        <span className="bar-status">{status}</span>
+        <button type="button" className="bar-cancelButton">
+          <svg>
+            <path d="M0,20,20,0" />
+            <path d="M0,0,20,20" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
