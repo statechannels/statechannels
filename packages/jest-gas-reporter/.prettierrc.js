d3446a7fa5ad475b2bc21644dd3106deda44d4ca
Liam Horne <liam@lihorne.com>
2019-10-02 12:53:12 +0900

    Apply yarn lint:write to jest-gas-reporter

--- packages/jest-gas-reporter/.prettierrc.js
+++ packages/jest-gas-reporter/.prettierrc.js
@@ -1,7 +1,4 @@
 module.exports = {
     ...require('../../.prettierrc.js'),
-    "trailingComma": "es5",
-    "singleQuote": false,
-    "bracketSpacing": false,
-    "printWidth": 120
+    "trailingComma": "es5"
 }
