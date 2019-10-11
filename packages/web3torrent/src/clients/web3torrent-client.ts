94604aef4fcb6f86a7b827b341aa53309e52d0b9
Santiago Persico <santiagopersico@gmail.com>
2019-10-11 20:58:31 -0300

    implement functional download screen

--- packages/web3torrent/src/clients/web3torrent-client.ts
+++ packages/web3torrent/src/clients/web3torrent-client.ts
@@ -1,11 +1,138 @@
 import React from 'react';
 import WebTorrentPaidStreamingClient from '../library/web3torrent-lib';
+import {Torrent} from '../types';
 
 export const web3torrent = new WebTorrentPaidStreamingClient();
 export const WebTorrentContext = React.createContext(web3torrent);
 
-export const download = torrentData =>
-  new Promise(resolve => web3torrent.add(torrentData, torrent => resolve(torrent)));
+export const download: (torrent: any) => Promise<Torrent> = torrentData =>
+  new Promise(resolve =>
+    web3torrent.add(torrentData, torrent => resolve({...torrent, status: 'Connecting'}))
+  );
 
 export const upload = files =>
   new Promise(resolve => web3torrent.seed(files as FileList, torrent => resolve(torrent)));
+
+export const remove = (id: string) => {
+  return new Promise((resolve, reject) =>
+    web3torrent.remove(id, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(id);
+      }
+    })
+  );
+};
+
+const getStatus = (torrent: Torrent) => {
+  const {uploadSpeed, downloadSpeed} = torrent;
+  if (torrent.downloaded && torrent.downloaded === torrent.length && torrent.done) {
+    return 'Completed';
+  } else if (torrent.destroyed) {
+    return 'Stopped';
+  }
+
+  if (uploadSpeed - downloadSpeed === 0) {
+    return 'Connecting';
+  } else if (uploadSpeed - downloadSpeed > 0) {
+    return 'Seeding';
+  } else {
+    return 'Downloading';
+  }
+};
+
+const getFormattedETA = (torrent: Torrent) => {
+  const {done, timeRemaining} = torrent;
+  if (done) {
+    return 'Done';
+  } else {
+    const remaining = timeRemaining || 0;
+    const days = Math.floor(remaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+
+    return timeRemaining === Infinity
+      ? 'ETA Unknown'
+      : `ETA ${(days && days + 'd ') || ''}${(hours && hours + 'h ') || ''}${(minutes &&
+          minutes + 'm ') ||
+          ''}${seconds && seconds + 's'}`;
+  }
+};
+
+export const getLiveTorrentData = (previousData: Torrent, infoHash): Torrent => {
+  const baseData: Torrent = {
+    ...previousData,
+    name: previousData.name || 'unknown',
+    status: 'Connecting',
+    done: false,
+    ready: false,
+    downloaded: 0
+  };
+
+  if (!infoHash) {
+    // torrent in magnet form
+    return {...baseData, status: 'Idle'};
+  }
+
+  const live = web3torrent.get(infoHash) as Torrent;
+  if (!live) {
+    // torrent after being destroyed
+    return {...baseData, destroyed: true, status: 'Idle'};
+  }
+
+  return {
+    // active torrent
+    ...baseData,
+    ...live,
+    ...{
+      name: live.name || baseData.name,
+      length: live.length || baseData.length,
+      downloaded: (live && live.downloaded) || 0,
+      status: getStatus(live),
+      uploadSpeed: live.uploadSpeed,
+      downloadSpeed: live.downloadSpeed,
+      numPeers: live.numPeers,
+      parsedTimeRemaining: getFormattedETA(live),
+      ready: true
+    }
+  };
+};
+
+export const parseMagnetURL: (rawMagnetURL: string) => Torrent = (rawMagnetURL = '') => {
+  const emptyTorrentData = {
+    name: 'unknown',
+    magnetURI: rawMagnetURL || '',
+    infoHash: '',
+    length: 0,
+    downloadSpeed: 0,
+    uploadSpeed: 0,
+    cost: 0,
+    status: 'Idle',
+    downloaded: 0,
+    files: []
+  } as Torrent;
+  if (!rawMagnetURL.trim()) {
+    return emptyTorrentData;
+  }
+  const magnetURI = rawMagnetURL.trim().substring(1);
+  const magnetAsArray = magnetURI.split('&');
+
+  const magnetName = magnetAsArray.find(h => h.includes('dn=') || h.includes('name='));
+  const name = magnetName && magnetName.replace(/(dn=)|(name=)/g, '').replace(/\+/g, ' ');
+
+  const magnetLength = magnetAsArray.find(h => h.includes('xl='));
+  const length: number = Number(magnetLength && magnetLength.replace(/(xl=)/g, ''));
+
+  const magnetCost = magnetAsArray.find(h => h.includes('cost='));
+  const cost: number = Number(magnetCost && magnetCost.replace(/(cost=)/g, ''));
+
+  return {
+    ...emptyTorrentData,
+    name,
+    magnetURI,
+    length: isNaN(length) ? 0 : length,
+    cost: isNaN(cost) ? 0 : cost
+  };
+};
--- packages/web3torrent/src/components/torrent-info/TorrentInfo.tsx
+++ packages/web3torrent/src/components/torrent-info/TorrentInfo.tsx
@@ -1,30 +1,48 @@
 import prettier from 'prettier-bytes';
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
 import {Torrent} from '../../types';
+import {clipboardCopy} from '../../utils/copy-to-clipboard';
 import {DownloadInfo} from './download-info/DownloadInfo';
 import './TorrentInfo.scss';
 import {UploadInfo} from './upload-info/UploadInfo';
 
-export type TorrentInfoProps = {torrent: Torrent};
+const MagnetLinkButton: React.FC<{magnetURI: string}> = ({magnetURI}) => {
+  const [magnetInfo, setMagnetInfo] = useState({copied: false, magnet: magnetURI});
+  return (
+    <button
+      className="fileLink"
+      type="button"
+      onClick={() => {
+        clipboardCopy(magnetInfo.magnet);
+        setMagnetInfo({...magnetInfo, copied: true});
+        setTimeout(() => setMagnetInfo({...magnetInfo, copied: false}), 3000);
+      }}
+    >
+      <span className={'tooltiptext ' + magnetInfo.copied} id="myTooltip">
+        {magnetInfo.copied ? 'Great! Copied to your clipboard' : 'Copy to clipboard'}
+      </span>
+      Share Link
+    </button>
+  );
+};
 
-const TorrentInfo: React.FC<TorrentInfoProps> = ({torrent}: TorrentInfoProps) => {
+const TorrentInfo: React.FC<{torrent: Torrent}> = ({torrent}) => {
   return (
     <>
       <section className={`torrentInfo ${torrent.magnetURI ? ' with-link' : ''}`}>
         <span className="fileName">{torrent.name}</span>
-        <span className="fileSize">{prettier(torrent.length)}</span>
+        <span className="fileSize">{!torrent.length ? '? Mb' : prettier(torrent.length)}</span>
         {torrent.status ? <span className="fileStatus">{torrent.status}</span> : false}
-        <span className="fileCost">Est. cost ${Number(torrent.cost).toFixed(2)}</span>
-        {torrent.magnetURI ? (
-          <Link to={torrent.magnetURI} className="fileLink">
-            Share Link
-          </Link>
-        ) : (
-          false
-        )}
+        <span className="fileCost">
+          Est. cost ${!torrent.cost ? '?' : Number(torrent.cost).toFixed(2)}
+        </span>
+        {torrent.magnetURI ? <MagnetLinkButton magnetURI={torrent.magnetURI} /> : false}
       </section>
-      {torrent.downloaded ? <DownloadInfo torrent={torrent} /> : false}
+      {(torrent.downloaded || torrent.status === 'Connecting') && torrent.ready ? (
+        <DownloadInfo torrent={torrent} />
+      ) : (
+        false
+      )}
       {torrent.uploaded ? <UploadInfo torrent={torrent} /> : false}
     </>
   );
