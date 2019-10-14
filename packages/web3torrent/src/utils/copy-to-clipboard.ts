4bd742f8f7039b6b8f335f66cacd1c5432527f6c
Santiago Persico <santiagopersico@gmail.com>
2019-10-14 13:27:44 -0300

    simplifies the copy to clipboard function

--- packages/web3torrent/src/utils/copy-to-clipboard.ts
+++ packages/web3torrent/src/utils/copy-to-clipboard.ts
@@ -1,28 +1,11 @@
 export const clipboardCopy = (text: string) => {
-  const span = document.createElement('span');
-  span.textContent = text;
-
-  document.body.appendChild(span);
-
-  const selection = window.getSelection();
-  const range = window.document.createRange();
-  if (selection) {
-    selection.removeAllRanges();
-    range.selectNode(span);
-    selection.addRange(range);
-  }
-  let success = false;
-  try {
-    success = window.document.execCommand('copy');
-  } catch (err) {
-    console.error('error', err);
-  }
-  if (selection) {
-    selection.removeAllRanges();
-  }
-  window.document.body.removeChild(span);
-
-  return success
-    ? Promise.resolve()
-    : Promise.reject(new DOMException('The request is not allowed', 'NotAllowedError'));
+  const el = document.createElement('textarea');
+  el.value = text;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
 };
