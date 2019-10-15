3acebf1aad1c0386e7039f915c4ff221fd020c1f
George Knee <georgeknee@googlemail.com>
2019-10-15 11:26:09 +0100

    Update config.repoUrl

--- packages/nitro-protocol/website/siteConfig.js
+++ packages/nitro-protocol/website/siteConfig.js
@@ -108,7 +108,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/statechannels/nitro-protocol',
+  repoUrl: 'https://github.com/statechannels/monorepo',
 };
 
 module.exports = siteConfig;
