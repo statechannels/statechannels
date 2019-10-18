86b9f0953da0a3951d4aafaddd4d40472ef45c6b
Liam Horne <liam@lihorne.com>
2019-10-18 10:13:41 +0900

    Use --native option on etherlime

--- packages/nitro-protocol/bin/compile.sh
+++ packages/nitro-protocol/bin/compile.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+export $(egrep -v '^#' .env | xargs)
+
 if [ -z "$USE_NATIVE_SOLC" ]; then
   yarn run etherlime compile --buildDirectory=./build/contracts
 else
   yarn run etherlime compile --solcVersion=native --buildDirectory=./build/contracts
 fi
-
---
+++ packages/wallet/bin/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$USE_NATIVE_SOLC" ]; then
+  yarn run etherlime compile --buildDirectory=./build/contracts
+else
+  yarn run etherlime compile --solcVersion=native --buildDirectory=./build/contracts
+fi
