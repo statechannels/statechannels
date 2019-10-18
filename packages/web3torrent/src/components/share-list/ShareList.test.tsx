b5a45ed22e07a523b2d94f37932132f5a2bb5800
Santiago Persico <santiagopersico@gmail.com>
2019-10-18 15:29:20 -0300

    move mockTorrents to avoid repetition

--- packages/web3torrent/src/components/share-list/ShareList.test.tsx
+++ packages/web3torrent/src/components/share-list/ShareList.test.tsx
@@ -5,19 +5,14 @@ import React from 'react';
 import {MemoryRouter as Router} from 'react-router-dom';
 import {ShareList, ShareListProps} from './ShareList';
 import {RoutePath} from '../../routes';
+import {mockTorrents} from '../../constants';
 
 function setup(withNoTorrents = false) {
   Enzyme.configure({adapter: new Adapter()});
   const history = createMemoryHistory();
   const props: ShareListProps = {
     history,
-    torrents: withNoTorrents
-      ? []
-      : [
-          {name: 'Sample_1.dat', length: 350, numSeeds: 27, numPeers: 350, cost: '0.5', files: []},
-          {name: 'Sample_2.dat', length: 250, numSeeds: 35, numPeers: 400, cost: '0.5', files: []},
-          {name: 'Sample_3.dat', length: 50, numSeeds: 2, numPeers: 360, cost: '0.5', files: []}
-        ]
+    torrents: withNoTorrents ? [] : mockTorrents
   };
   const component = mount(
     <Router>
--- packages/web3torrent/src/pages/welcome/Welcome.tsx
+++ packages/web3torrent/src/pages/welcome/Welcome.tsx
@@ -3,14 +3,10 @@ import {RouteComponentProps} from 'react-router-dom';
 import {FormButton} from '../../components/form';
 import {ShareList} from '../../components/share-list/ShareList';
 import {RoutePath} from '../../routes';
-import {Torrent} from '../../types';
 import './Welcome.scss';
+import { mockTorrents } from '../../constants';
 
-const mockTorrents: Array<Partial<Torrent>> = [
-  {name: 'Sample_1.dat', length: 350, numSeeds: 27, numPeers: 350, cost: '0.5', files: []},
-  {name: 'Sample_2.dat', length: 250, numSeeds: 35, numPeers: 400, cost: '0.5', files: []},
-  {name: 'Sample_3.dat', length: 50, numSeeds: 2, numPeers: 360, cost: '0.5', files: []}
-];
+
 const Welcome: React.FC<RouteComponentProps> = ({history}) => {
   return (
     <section className="section fill">
