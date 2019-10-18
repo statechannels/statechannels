87076af59f8921db094bc957c8d26e3e2cea3179
Santiago Persico <santiagopersico@gmail.com>
2019-10-18 11:10:57 -0300

    add linting to test/**

--- packages/web3torrent/tests/utils.ts
+++ packages/web3torrent/tests/utils.ts
@@ -1,5 +1,5 @@
-import MemoryChunkStore from "memory-chunk-store";
-import fixtures from "webtorrent-fixtures";
+import MemoryChunkStore from 'memory-chunk-store';
+import fixtures from 'webtorrent-fixtures';
 
 export function testSelector(name: string): string {
   return `[data-test-selector='${name}']`;
@@ -7,15 +7,15 @@ export function testSelector(name: string): string {
 
 export const defaultFile = new Blob([fixtures.leaves.content]);
 export const defaultTorrentHash = fixtures.leaves.parsedTorrent.infoHash;
-export const defaultFileMagnetURI = `${fixtures.leaves.magnetURI}&tr=${encodeURIComponent(process.env
-  .TRACKER_URL as string)}`;
+export const defaultFileMagnetURI = `${fixtures.leaves.magnetURI}&tr=${encodeURIComponent(process
+  .env.TRACKER_URL as string)}`;
 export const defaultSeedingOptions = (randomizeName = true) => ({
-  name: (randomizeName ? Math.random() * 1000 : "") + "Leaves of Grass by Walt Whitman.epub",
+  name: (randomizeName ? Math.random() * 1000 : '') + 'Leaves of Grass by Walt Whitman.epub',
   announce: [process.env.TRACKER_URL],
   store: MemoryChunkStore
 });
 
 export const defaultLeechingOptions = {
-  name: "Leaves of Grass by Walt Whitman.epub",
+  name: 'Leaves of Grass by Walt Whitman.epub',
   store: MemoryChunkStore
 };
