4731e161dbbb48d18084f9d6ecf675bc1f3fbdff
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-10-21 13:06:41 -0300

    web3torrent: added mock utils for Torrent, TorrentPeer

--- packages/web3torrent/src/utils/test-utils.ts
+++ packages/web3torrent/src/utils/test-utils.ts
@@ -1,3 +1,37 @@
+import {TorrentPeers} from '../library/types';
+import {Torrent} from '../types';
+
 export function testSelector(name: string): string {
   return `[data-test-selector='${name}']`;
+}
+export function createMockTorrent(props?: Partial<Torrent>): Partial<Torrent> {
+  return {
+    magnetURI:
+      'magnet:?xt=urn%3Abtih%3A0303b8f867f4377ef4a25eba8836cc5f7fdd992b&dn=on-the-shortness-of-life-1.jpg&xl=128864&cost=0',
+    name: 'on-the-shortness-of-life-1.jpg',
+    downloaded: 0,
+    length: 128913,
+    cost: '1.34',
+    ready: true,
+    ...props
+  };
+}
+
+export function createMockTorrentPeers(): TorrentPeers {
+  return {
+    '7595267661936611': {
+      id: '7595267661936611',
+      allowed: true,
+      wire: {
+        uploaded: 4225
+      }
+    },
+    '5589113806923374': {
+      id: '5589113806923374',
+      allowed: true,
+      wire: {
+        uploaded: 52923
+      }
+    }
+  };
 }
