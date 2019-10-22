8fac183dbe989fc3dfecb4043fe339b1e0b1e1bf
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-10-22 12:22:44 -0300

    [web3torrent] Add tests for EmbeddedWalletClient (#237)
    * web3torrent: minor fixed to wallet client
    
    * web3torrent: added tests for EmbeddedWalletClient

--- packages/web3torrent/src/clients/embedded-wallet-client.ts
+++ packages/web3torrent/src/clients/embedded-wallet-client.ts
@@ -1,24 +1,32 @@
-const wallet = window.EmbeddedWallet;
+import {JsonRPCRequest} from 'web3/providers';
 
 export const connectToWallet = () => {
+  const wallet = window.EmbeddedWallet;
+
   try {
     wallet.enable(process.env.REACT_APP_EMBEDDED_WALLET_URL);
   } catch (error) {
-    console.log('Error while connecting to wallet', error.name);
+    console.log('Error while connecting to wallet');
+    console.log(error.stack);
   }
 };
 
-export async function makeWalletRequest() {
+export async function makeWalletRequest(
+  request: JsonRPCRequest = {
+    jsonrpc: '2.0',
+    method: 'chan_allocate',
+    id: 123,
+    params: ['foo', 'bar', 3, false]
+  }
+) {
+  const wallet = window.EmbeddedWallet;
+
   try {
-    return wallet.request({
-      jsonrpc: '2.0',
-      method: 'chan_allocate',
-      id: 123,
-      params: ['foo', 'bar', 3, false]
-    });
+    return wallet.request(request);
   } catch (error) {
-    console.log('Error while making request to wallet', error.name);
-    return Promise.reject();
+    console.log('Error while making request to wallet');
+    console.log(error.stack);
+    return Promise.reject(error);
   }
 }
 
