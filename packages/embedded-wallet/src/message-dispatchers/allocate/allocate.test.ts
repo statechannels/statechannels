7ab0c4c8881c29a24cdd6cf6ec9dfc41fd843589
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-10-30 14:20:48 -0300

    embedded-wallet: implemented rejectAllocation

--- packages/embedded-wallet/src/message-dispatchers/allocate/allocate.test.ts
+++ packages/embedded-wallet/src/message-dispatchers/allocate/allocate.test.ts
@@ -1,15 +1,15 @@
-import {JsonRPCResponse} from 'web3/providers';
+import {JsonRpcResponse} from '../../types';
 import {allocate} from './allocate';
 
 describe('MessageDispatchers - Allocate', () => {
   it('should dispatch a JSONRPC call with the `allocate` method', async done => {
     window.onmessage = (event: MessageEvent) => {
-      const response = event.data as JsonRPCResponse;
+      const response = event.data as JsonRpcResponse;
       if (!response.jsonrpc) {
         fail('Expected a JSONRPC response');
       } else {
         expect(response.jsonrpc).toEqual('2.0');
-        expect(response.error).toBeUndefined();
+        expect('error' in response).toEqual(false);
         expect(response.result).toEqual({allocated: true});
         expect(response.id).toEqual(123);
       }
--- packages/embedded-wallet/src/message-dispatchers/allocate/allocate.ts
+++ packages/embedded-wallet/src/message-dispatchers/allocate/allocate.ts
@@ -1,12 +1,26 @@
 import debug from 'debug';
-import {JsonRPCResponse} from 'web3/providers';
+import {JsonRpcErrorResponse, JsonRpcResponse} from '../../types';
+import {JsonRpcErrorCodes} from '../error-codes';
 
 const log = debug('wallet:dispatch');
 
 const allocate = (requestId: number, result: {[key: string]: string | number | boolean}) => {
-  const message: JsonRPCResponse = {jsonrpc: '2.0', id: requestId, result};
+  const message: JsonRpcResponse = {jsonrpc: '2.0', id: requestId, result};
   log('Sending: %o', message);
   window.parent.postMessage(message, '*');
 };
 
-export {allocate};
+const rejectAllocation = (requestId: number) => {
+  const message: JsonRpcErrorResponse = {
+    jsonrpc: '2.0',
+    id: requestId,
+    error: {
+      code: JsonRpcErrorCodes.BudgetAllocationRejected,
+      message: 'User has rejected budget allocation'
+    }
+  };
+  log('Sending: %o', message);
+  window.parent.postMessage(message, '*');
+};
+
+export {allocate, rejectAllocation};
