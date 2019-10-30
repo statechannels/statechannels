bdb7d374cd8325910f0e1d14fa62718759e4cc42
Alon Bukai <alonzorz@gmail.com>
2019-10-30 18:41:45 +0200

    tests: Add random input tests for TrivialApp

--- packages/nitro-protocol/test/contracts/TrivalApp/validTransition.test.ts
+++ packages/nitro-protocol/test/contracts/TrivalApp/validTransition.test.ts
@@ -1,7 +1,8 @@
 // @ts-ignore
 import {expectRevert} from '@statechannels/devtools';
 import {Contract} from 'ethers';
-import {HashZero} from 'ethers/constants';
+import {MaxUint256} from 'ethers/constants';
+import {solidityKeccak256} from 'ethers/utils';
 import TrivialAppArtifact from '../../../build/contracts/TrivialApp.json';
 import {VariablePart} from '../../../src/contract/state.js';
 import {getTestProvider, setupContracts} from '../../test-helpers';
@@ -9,24 +10,42 @@ import {getTestProvider, setupContracts} from '../../test-helpers';
 const provider = getTestProvider();
 let trivialApp: Contract;
 
+function computeRandomHash(salt: string, num: number) {
+  return solidityKeccak256(['bytes32', 'uint256'], [salt, num]);
+}
+
 beforeAll(async () => {
   trivialApp = await setupContracts(provider, TrivialAppArtifact);
 });
 
 describe('validTransition', () => {
   it.each`
-    isValid  | description
-    ${true}  | ${'IsValid True = True'}
-    ${false} | ${'IsValid False = True'}
-  `('$description', async ({isValid}: {isValid: boolean}) => {
+    randomNum                          | description
+    ${Math.floor(Math.random() * 100)} | ${'True'}
+  `('$description', async ({randomNum}: {randomNum: number}) => {
+    const salt = MaxUint256.toHexString();
+    const hash = computeRandomHash(salt, randomNum);
+
     const variablePart: VariablePart = {
-      outcome: HashZero,
-      appData: HashZero,
+      outcome: hash,
+      appData: hash,
     };
     const isValidFromCall = await trivialApp.validTransition(variablePart, variablePart, 0, 0);
-    if (isValid) {
-      expect(isValidFromCall).toBe(true);
-    } else {
+    expect(isValidFromCall).toBe(true);
+  });
+
+  it('Random inputs always equal true', async () => {
+    expect.assertions(5);
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * 100);
+      const salt = MaxUint256.toHexString();
+      const hash = computeRandomHash(salt, randomNum);
+
+      const variablePart: VariablePart = {
+        outcome: hash,
+        appData: hash,
+      };
+      const isValidFromCall = await trivialApp.validTransition(variablePart, variablePart, 0, 0);
       expect(isValidFromCall).toBe(true);
     }
   });
