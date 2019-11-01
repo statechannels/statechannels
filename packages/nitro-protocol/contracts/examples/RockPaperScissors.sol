beb9ad2d202d4611175bcefe7d35def3f9b6fd36
George Knee <georgeknee@googlemail.com>
2019-11-01 15:14:04 +0000

    Add action deduction logic

--- packages/nitro-protocol/contracts/examples/RockPaperScissors.sol
+++ packages/nitro-protocol/contracts/examples/RockPaperScissors.sol
@@ -6,8 +6,38 @@ import '../Outcome.sol';
 
 /**
   * @dev The RockPaperScissors contract complies with the ForceMoveApp interface and implements a commit-reveal game of Rock Paper Scissors (henceforth RPS).
+  * The following transitions are allowed:
+  *
+  * Start -> RoundProposed  [ PROPOSE ]
+  * RoundProposed -> Start  [ REJECT ]
+  * RoundProposed -> RoundAccepted [ ACCEPT ]
+  * RoundAccepted -> Reveal [ REVEAL ]
+  * Reveal -> Start [ FINISH ]
+  *
 */
 contract RockPaperScissors is ForceMoveApp {
+
+    enum PositionType { Start, RoundProposed, RoundAccepted, Reveal }
+    enum Weapon { Rock, Paper, Scissors }
+
+    struct RPSData{
+        PositionType positionType;
+        uint256 stake;
+        bytes32 preCommit;
+        Weapon bWeapon;
+        Weapon aWeapon;
+        bytes32 salt;
+    }
+
+    /**
+    * @notice Deocdes the appData.
+    * @dev Deocdes the appData.
+    * @param appDataBytes The abi.encode of a RPSData struct describing the application-specific data.
+    * @return An RPSData struct containing the application-specific data.
+    */
+    function appData(bytes memory appDataBytes) internal pure returns (RPSData memory) {
+        return abi.decode(appDataBytes, (RPSData));
+    }
 
     /**
     * @notice Encodes the RPS update rules.
@@ -24,6 +54,38 @@ contract RockPaperScissors is ForceMoveApp {
         uint256 turnNumB,
         uint256 nParticipants
     ) public pure returns (bool) {
-        return true;
+
+        // decode application-specific data
+        RPSData memory appDataA = appData(a.appData);
+        RPSData memory appDataB = appData(b.appData);
+
+        // deduce action
+        if (appDataA.positionType == PositionType.Start) {
+            require(appDataB.positionType == PositionType.RoundProposed, 'Start may only transition to RoundProposed');
+            validPROPOSE();
+            return true;
+        }
+        if (appDataA.positionType == PositionType.RoundProposed) {
+            if (appDataB.positionType == PositionType.Start) {
+                validREJECT();
+                return true;
+            }
+            if (appDataB.positionType == PositionType.RoundAccepted) {
+                validACCEPT();
+                return true;
+            }
+        revert('Proposed may only transition to Start or RoundAccepted');
+        }
+        if (appDataA.positionType == PositionType.Reveal) {
+            require(appDataB.positionType == PositionType.Start, 'Reveal may only transition to Start');
+            validFINISH();
+            return true;
+        }
+        revert('No valid transition found');
     }
+
+    function validPROPOSE() private pure returns (bool) {return true;}
+    function validREJECT() private pure returns (bool) {return true;}
+    function validACCEPT() private pure returns (bool) {return true;}
+    function validFINISH() private pure returns (bool) {return true;}
 }
