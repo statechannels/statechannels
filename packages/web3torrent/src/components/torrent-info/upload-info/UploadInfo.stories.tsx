84e40c0e26b7f319ff968dd3297d527dfe3026f8
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-11-06 12:54:53 -0300

    web3torrent: added stories for UploadInfo

---
+++ packages/web3torrent/src/components/torrent-info/upload-info/UploadInfo.stories.tsx
@@ -0,0 +1,47 @@
+import {withActions} from '@storybook/addon-actions';
+import {number, withKnobs} from '@storybook/addon-knobs';
+import {storiesOf} from '@storybook/react';
+import React from 'react';
+import '../../../App.scss';
+import {TorrentPeers} from '../../../library/types';
+import {Torrent} from '../../../types';
+import {UploadInfo} from './UploadInfo';
+import './UploadInfo.scss';
+
+storiesOf('Web3Torrent', module)
+  .addDecorator(withActions('click'))
+  .addDecorator(withKnobs())
+  .add('UploadInfo', () => (
+    <UploadInfo
+      torrent={
+        {
+          numPeers: 3
+        } as Torrent
+      }
+      peers={
+        {
+          '9720534187': {
+            id: '9720534187',
+            wire: {
+              uploaded: number('Uploaded bytes for Peer 9720534187', 3533366, {step: 1000}, 'Peers')
+            },
+            allowed: true
+          },
+          '9202959009': {
+            id: '9202959009',
+            wire: {
+              uploaded: number('Uploaded bytes for Peer 9202959009', 5611625, {step: 1000}, 'Peers')
+            },
+            allowed: true
+          },
+          '2190352424': {
+            id: '2190352424',
+            wire: {
+              uploaded: number('Uploaded bytes for Peer 2190352424', 5051532, {step: 1000}, 'Peers')
+            },
+            allowed: true
+          }
+        } as TorrentPeers
+      }
+    ></UploadInfo>
+  ));
