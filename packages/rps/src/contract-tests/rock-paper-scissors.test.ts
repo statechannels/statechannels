345b13e11ee74f0cb79b8f7a0e294221daefd7cc
George Knee <georgeknee@googlemail.com>
2019-11-11 13:19:29 +0000

    Add test cases with expectation = false
    - NB destination swap is only setup correctly if JS object keys have an order https://www.stefanjudis.com/today-i-learned/property-order-is-predictable-in-javascript-objects-since-es2015/

--- packages/rps/src/contract-tests/rock-paper-scissors.test.ts
+++ packages/rps/src/contract-tests/rock-paper-scissors.test.ts
@@ -44,139 +44,6 @@ interface RPSData {
   salt: string; // bytes32
 }
 
-// describe.skip('Rock Paper Scissors', () => {
-//   let networkId;
-
-//   let rpsContract;
-
-//   let postFundSetupB: RPSCommitment;
-//   let postFundSetupB2: RPSCommitment;
-//   let propose: RPSCommitment;
-//   let propose2: RPSCommitment;
-//   let propose3: RPSCommitment;
-
-//   let accept: RPSCommitment;
-//   let reveal: RPSCommitment;
-//   let resting: RPSCommitment;
-
-//   beforeAll(async () => {
-//     networkId = (await provider.getNetwork()).chainId;
-//     const libraryAddress = RockPaperScissorsArtifact.networks[networkId].address;
-
-//     rpsContract = new ethers.Contract(libraryAddress, RockPaperScissorsArtifact.abi, provider);
-
-//     const account1 = ethers.Wallet.createRandom();
-//     const account2 = ethers.Wallet.createRandom();
-//     const scenario = scenarios.build(libraryAddress, account1.address, account2.address);
-//     postFundSetupB = scenario.postFundSetupB;
-//     postFundSetupB2 = scenario.postFundSetupB2;
-//     propose = scenario.propose;
-//     propose2 = scenario.propose2;
-//     propose3 = scenario.propose3;
-//     accept = scenario.accept;
-//     reveal = scenario.reveal;
-//     resting = scenario.resting;
-//   });
-
-//   const validTransition = async (commitment1: RPSCommitment, commitment2: RPSCommitment) => {
-//     const coreCommitment1 = asCoreCommitment(commitment1);
-//     const coreCommitment2 = asCoreCommitment(commitment2);
-
-//     return await rpsContract.validTransition(
-//       asEthersObject(coreCommitment1),
-//       asEthersObject(coreCommitment2)
-//     );
-//   };
-
-//   // Transition function tests
-//   // ========================
-
-//   it('disallows transitions where the stake changes', async () => {
-//     reveal.stake = bigNumberify(88).toHexString();
-//     const coreCommitment1 = asCoreCommitment(reveal);
-//     const coreCommitment2 = asCoreCommitment(resting);
-//     expect.assertions(1);
-//     await expectRevert(
-//       () =>
-//         rpsContract.validTransition(
-//           asEthersObject(coreCommitment1),
-//           asEthersObject(coreCommitment2)
-//         ),
-//       'The stake should be the same between commitments'
-//     );
-//   });
-
-//   it('disallows transitions where the allocations change incorrectly', async () => {
-//     const coreCommitment1 = asCoreCommitment(postFundSetupB);
-//     const coreCommitment2 = asCoreCommitment(propose2);
-//     expect.assertions(1);
-//     await expectRevert(
-//       () =>
-//         rpsContract.validTransition(
-//           asEthersObject(coreCommitment1),
-//           asEthersObject(coreCommitment2)
-//         ),
-//       'The allocation for player A must be the same between commitments'
-//     );
-//   });
-
-//   it('disallows transitions where destination changes', async () => {
-//     const coreCommitment1 = asCoreCommitment({...propose});
-//     const coreCommitment2 = asCoreCommitment({
-//       ...accept,
-//       destination: ['0x6Ecbe1DB9EF729CBe972C83Fb886247691Fb6beb']
-//     });
-//     expect.assertions(1);
-//     await expectRevert(
-//       () =>
-//         rpsContract.validTransition(
-//           asEthersObject(coreCommitment1),
-//           asEthersObject(coreCommitment2)
-//         ),
-//       'The destination field must not change'
-//     );
-//   });
-
-//   it('disallows a stake that the players cannot afford', async () => {
-//     const coreCommitment1 = asCoreCommitment(postFundSetupB2);
-//     const coreCommitment2 = asCoreCommitment(propose3);
-//     expect.assertions(1);
-//     await expectRevert(
-//       () =>
-//         rpsContract.validTransition(
-//           asEthersObject(coreCommitment1),
-//           asEthersObject(coreCommitment2)
-//         ),
-//       'The allocation for player A must not fall below the stake.'
-//     );
-//   });
-
-//   // The following scenario is highly notional, since the situation should (and is) prevented from occuring at the Propose stage
-//   it('Reverts an underflow via SafeMath', async () => {
-//     reveal.stake = accept.stake = bigNumberify(20).toHexString();
-//     accept.allocation = [bigNumberify(0).toHexString(), bigNumberify(0).toHexString()] as [
-//       string,
-//       string
-//     ];
-//     reveal.allocation = [bigNumberify(40).toHexString(), bigNumberify(0).toHexString()] as [
-//       string,
-//       string
-//     ];
-
-//     const coreCommitment1 = asCoreCommitment(accept);
-//     const coreCommitment2 = asCoreCommitment(reveal);
-//     expect.assertions(1);
-//     await expectRevert(
-//       () =>
-//         rpsContract.validTransition(
-//           asEthersObject(coreCommitment1),
-//           asEthersObject(coreCommitment2)
-//         ),
-//       'revert'
-//     ); // Note that SafeMath does not have revert messages
-//   });
-// });
-
 let RockPaperScissors: Contract;
 
 const numParticipants = 3;
@@ -192,13 +59,17 @@ beforeAll(async () => {
 
 describe('validTransition', () => {
   it.each`
-    isValid | positionType                                                | stake               | AWeapon                       | BWeapon                       | fromBalances    | toBalances      | description
-    ${true} | ${[PositionType.Start, PositionType.RoundProposed]}         | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows Start -> RoundProposed'}
-    ${true} | ${[PositionType.RoundProposed, PositionType.RoundAccepted]} | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows RoundProposed -> RoundAccepted'}
-    ${true} | ${[PositionType.RoundAccepted, PositionType.Reveal]}        | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows RoundAccepted -> Reveal'}
-    ${true} | ${[PositionType.Reveal, PositionType.Start]}                | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows RoundAccepted -> Reveal'}
+    isValid  | positionType                                                | stake               | AWeapon                       | BWeapon                       | fromBalances    | toBalances      | description
+    ${true}  | ${[PositionType.Start, PositionType.RoundProposed]}         | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows Start -> Proposed'}
+    ${true}  | ${[PositionType.RoundProposed, PositionType.RoundAccepted]} | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows Proposed -> Accepted'}
+    ${true}  | ${[PositionType.RoundAccepted, PositionType.Reveal]}        | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows Accepted -> Reveal'}
+    ${true}  | ${[PositionType.Reveal, PositionType.Start]}                | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Allows Accepted -> Reveal'}
+    ${false} | ${[PositionType.Reveal, PositionType.Start]}                | ${{from: 1, to: 2}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Disallows stake change on Reveal -> Start'}
+    ${false} | ${[PositionType.Start, PositionType.RoundProposed]}         | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 6, B: 4}} | ${'Disallows allocations change on Start -> Propose'}
+    ${false} | ${[PositionType.RoundProposed, PositionType.RoundAccepted]} | ${{from: 1, to: 1}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 6, B: 4}} | ${{B: 6, A: 4}} | ${'Disallows destination swap on Propose -> Accept'}
+    ${false} | ${[PositionType.Start, PositionType.RoundProposed]}         | ${{from: 1, to: 6}} | ${[Weapon.Rock, Weapon.Rock]} | ${[Weapon.Rock, Weapon.Rock]} | ${{A: 5, B: 5}} | ${{A: 5, B: 5}} | ${'Disallows a stake that is too large'}
   `(
-    '$description',
+    '$description', // TODO print positionTypes automatically, avoid writing out manually
     async ({
       isValid,
       positionType,
