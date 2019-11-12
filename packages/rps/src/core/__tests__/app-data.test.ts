561227d80ee108525b21bb584b21fd0cfffa9e4b
George Knee <georgeknee@googlemail.com>
2019-11-12 17:00:21 +0000

    Changes in response to PR comments
    -  types that Pick-ed are now interfaces with explicit props
    - some missing fields added
    - encoding and decoding accepts and returns appData
    - switches accordingly to discriminate the union
    - tests updated

--- packages/rps/src/core/__tests__/app-data.test.ts
+++ packages/rps/src/core/__tests__/app-data.test.ts
@@ -1,40 +1,24 @@
-import {bigNumberify} from 'ethers/utils';
-import {randomHex} from '../../utils/randomHex';
-import {decodeAppData, encodeAppData, RPSData, PositionType, Reveal, toRPSData} from '../app-data';
+import {decodeAppData, encodeAppData, Reveal, RoundProposed} from '../app-data';
 import {HashZero} from 'ethers/constants';
 import {Weapon} from '../weapons';
-
-const testAppData: RPSData = {
-  positionType: PositionType.RoundProposed,
-  stake: bigNumberify(2),
-  preCommit: HashZero,
-  playerAWeapon: Weapon.Paper,
-  playerBWeapon: Weapon.Rock,
-  salt: randomHex(64),
-};
+import {bigNumberify} from 'ethers/utils';
 
 const testReveal: Reveal = {
   type: 'reveal',
   playerAWeapon: Weapon.Paper,
   playerBWeapon: Weapon.Rock,
+  salt: HashZero,
 };
 
-const testRevealWithDefaults: RPSData = {
-  playerAWeapon: Weapon.Paper,
-  playerBWeapon: Weapon.Rock,
-  positionType: PositionType.Start,
-  stake: bigNumberify(0),
+const testRoundProposed: RoundProposed = {
+  type: 'roundProposed',
+  stake: bigNumberify(99),
   preCommit: HashZero,
-  salt: randomHex(64),
 };
 
 describe('app-data.ts', () => {
   test('decode is the inverse of encode', () => {
-    expect(decodeAppData(encodeAppData(testAppData))).toStrictEqual(testAppData);
-  });
-  test('can encode a Reveal by padding with defaults', () => {
-    expect(decodeAppData(encodeAppData(toRPSData(testReveal)))).toStrictEqual(
-      testRevealWithDefaults
-    );
+    expect(decodeAppData(encodeAppData(testRoundProposed))).toStrictEqual(testRoundProposed);
+    expect(decodeAppData(encodeAppData(testReveal))).toStrictEqual(testReveal);
   });
 });
