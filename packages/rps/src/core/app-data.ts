f094a3e76f407aea3b7bf4ce203bdebe6680a44d
George Knee <georgeknee@googlemail.com>
2019-11-12 17:00:21 +0000

    Changes in response to PR comments
    -  types that Pick-ed are now interfaces with explicit props
    - some missing fields added
    - encoding and decoding accepts and returns appData
    - switches accordingly to discriminate the union
    - tests updated

--- packages/rps/src/core/app-data.ts
+++ packages/rps/src/core/app-data.ts
@@ -20,18 +20,45 @@ export interface RPSData {
 export interface Start {
   type: 'start';
 }
+export interface RoundProposed {
+  type: 'roundProposed';
+  stake: BigNumber;
+  preCommit: string;
+}
 
-export type RoundProposed = Pick<RPSData, 'stake' | 'preCommit'> & {type: 'roundProposed'};
+export interface RoundAccepted {
+  type: 'roundAccepted';
+  stake: BigNumber;
+  preCommit: string;
+  playerBWeapon: Weapon;
+}
 
-export type RoundAccepted = Pick<RPSData, 'stake' | 'preCommit'> & {type: 'roundAccepted'};
-
-export type Reveal = Pick<RPSData, 'playerAWeapon' | 'playerBWeapon'> & {type: 'reveal'};
-
+export interface Reveal {
+  type: 'reveal';
+  playerAWeapon: Weapon;
+  playerBWeapon: Weapon;
+  salt;
+}
 export type AppData = Start | RoundProposed | RoundAccepted | Reveal;
 
-export function toRPSData(appData: AppData): RPSData {
+function toRPSData(appData: AppData): RPSData {
+  let positionType;
+  switch (appData.type) {
+    case 'start':
+      positionType = PositionType.Reveal;
+      break;
+    case 'roundProposed':
+      positionType = PositionType.RoundProposed;
+      break;
+    case 'roundAccepted':
+      positionType = PositionType.RoundAccepted;
+      break;
+    case 'reveal':
+      positionType = PositionType.Reveal;
+      break;
+  }
   const defaults: RPSData = {
-    positionType: PositionType.Start,
+    positionType,
     stake: bigNumberify(0),
     preCommit: HashZero,
     playerAWeapon: Weapon.Rock,
@@ -42,30 +69,60 @@ export function toRPSData(appData: AppData): RPSData {
   return {...defaults, ...appData};
 }
 
-export function encodeAppData(appData: RPSData): string {
+export function encodeAppData(appData: AppData): string {
   return defaultAbiCoder.encode(
     [
       'tuple(uint8 positionType, uint256 stake, bytes32 preCommit, uint8 playerAWeapon, uint8 playerBWeapon, bytes32 salt)',
     ],
-    [appData]
+    [toRPSData(appData)]
   );
 }
 
-export function decodeAppData(appDataBytes: string): RPSData {
+export function decodeAppData(appDataBytes: string): AppData {
   const parameters = defaultAbiCoder.decode(
     [
       'tuple(uint8 positionType, uint256 stake, bytes32 preCommit, uint8 playerAWeapon, uint8 playerBWeapon, bytes32 salt)',
     ],
     appDataBytes
   )[0];
-  return {
-    positionType: parameters[0],
-    stake: parameters[1],
-    preCommit: parameters[2],
-    playerAWeapon: parameters[3],
-    playerBWeapon: parameters[4],
-    salt: parameters[5],
-  };
+
+  const positionType = parameters[0];
+  const stake = parameters[1];
+  const preCommit = parameters[2];
+  const playerAWeapon = parameters[3];
+  const playerBWeapon = parameters[4];
+  const salt = parameters[5];
+
+  switch (positionType as PositionType) {
+    case PositionType.Start: // TODO replace these with functions that project out the desired fields
+      const start: Start = {
+        type: 'start',
+      };
+      return start;
+    case PositionType.RoundProposed:
+      const roundProposed: RoundProposed = {
+        type: 'roundProposed',
+        stake,
+        preCommit,
+      };
+      return roundProposed;
+    case PositionType.RoundAccepted:
+      const roundAccepted: RoundAccepted = {
+        type: 'roundAccepted',
+        stake,
+        preCommit,
+        playerBWeapon,
+      };
+      return roundAccepted;
+    case PositionType.Reveal:
+      const reveal: Reveal = {
+        type: 'reveal',
+        playerAWeapon,
+        playerBWeapon,
+        salt,
+      };
+      return reveal;
+  }
 }
 
 export function hashPreCommit(weapon: Weapon, salt: string) {
