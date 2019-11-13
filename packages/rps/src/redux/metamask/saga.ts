6e4e2b350cee4d33da3e72476d0560688f60bb04
George Knee <georgeknee@googlemail.com>
2019-11-13 15:47:46 +0000

    Metamask saga gets name-> id mapping from itself

--- packages/rps/src/redux/metamask/saga.ts
+++ packages/rps/src/redux/metamask/saga.ts
@@ -13,11 +13,35 @@ export default function* checkMetamask() {
     return false;
   }
 
+  const networks = {
+    development: {
+      network_id: '*', // match any network
+    },
+    main: {
+      network_id: 1,
+    },
+    ropsten: {
+      network_id: 3,
+    },
+    rinkeby: {
+      network_id: 4,
+    },
+    kovan: {
+      network_id: 42,
+    },
+  };
+
   try {
     const targetNetworkName = process.env.TARGET_NETWORK;
     ethereum.enable();
+
     // Find the network name that matches the currently selected network id
-    const selectedNetworkName = 'development';
+    const selectedNetworkId = parseInt(yield cps(web3.version.getNetwork), 10);
+    // Find the network name that matches the currently selected network id
+    const selectedNetworkName =
+      Object.keys(networks).find(
+        networkName => networks[networkName].network_id === selectedNetworkId
+      ) || 'development';
 
     if (targetNetworkName !== selectedNetworkName) {
       yield put(
