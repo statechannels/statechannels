05f07ba7f9ae53b974d3f0d2c02eb649b1767c0c
cf19drofxots <nima@l4v.io>
2019-11-14 14:43:11 -0800

    chore: add loglevel to wallet; remove stale jest global setup config

--- packages/wallet/config/jest/global-setup-jest-contracts.js
+++ packages/wallet/config/jest/global-setup-jest-contracts.js
@@ -1,27 +1,3 @@
-const net = require("net");
-const {GanacheServer, configureEnvVariables} = require("@statechannels/devtools");
-configureEnvVariables();
-
-const {deploy} = require("../../deployment/deploy-test");
-
-async function globalSetup() {
-  const chain = new GanacheServer();
-  // Kill the ganache server when jest exits
-  process.on("exit", async () => {
-    await chain.close();
-  });
-  global.chain = chain;
-
-  await chain.ready();
-
-  await deploy();
-  console.log(
-    `Test contracts deployed: ${JSON.stringify(
-      require("../../deployment/network-map.json"),
-      null,
-      2
-    )}`
-  );
-}
+async function globalSetup() { }
 
 module.exports = globalSetup;
--- packages/wallet/config/jest/jest.contracts.config.js
+++ packages/wallet/config/jest/jest.contracts.config.js
@@ -3,6 +3,5 @@ let config = require("./jest.config");
 module.exports = {
   ...config,
   globalSetup: "<rootDir>/config/jest/global-setup-jest-contracts.js",
-  globalTeardown: "<rootDir>/config/jest/global-teardown-jest-contracts.js",
   testMatch: ["<rootDir>/src/**/contract-tests/?(*.)test.ts?(x)"]
 };
