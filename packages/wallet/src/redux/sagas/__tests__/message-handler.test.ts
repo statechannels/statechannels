6f18265682d8b9c6205c2f6204ba2ed4f2107c84
Liam Horne <liam@lihorne.com>
2019-11-14 14:24:16 -0500

    Use helper functions in test of UpdateChannel

--- packages/wallet/src/redux/sagas/__tests__/message-handler.test.ts
+++ packages/wallet/src/redux/sagas/__tests__/message-handler.test.ts
@@ -6,8 +6,11 @@ import {Wallet} from "ethers";
 import {messageSender} from "../message-sender";
 import * as matchers from "redux-saga-test-plan/matchers";
 import {getAddress} from "../../selectors";
-import {asAddress, bsAddress} from "../../__tests__/state-helpers";
+import {asAddress, bsAddress, appState, asPrivateKey} from "../../__tests__/state-helpers";
+import {channelFromStates} from "../../channel-store/channel-state/__tests__";
 import {getProvider} from "../../../utils/contract-utils";
+import {setChannel} from "../../../../src/redux/channel-store";
+
 describe("message listener", () => {
   const wallet = Wallet.createRandom();
   const initialState = walletStates.initialized({
@@ -36,6 +39,7 @@ describe("message listener", () => {
         .run()
     );
   });
+
   describe("CreateChannel", () => {
     it("handles a create channel request", async () => {
       const destinationA = Wallet.createRandom().address;
@@ -240,104 +244,52 @@ describe("message listener", () => {
     });
   });
 
-  it("handles an update channel request", async () => {
-    const destinationA = Wallet.createRandom().address;
-    const signingAddressA = Wallet.createRandom().address;
-    const signingAddressB = Wallet.createRandom().address;
-    const destinationB = Wallet.createRandom().address;
+  describe("UpdateChannel", () => {
+    it("handles an update channel request", async () => {
+      // Data being submitted to UpdateChannel
+      const destinationA = Wallet.createRandom().address;
+      const destinationB = Wallet.createRandom().address;
+      const appData = "0x01010101";
+      const allocations = [
+        {
+          token: "0x0",
+          allocationItems: [
+            {destination: destinationA, amount: "12"},
+            {destination: destinationB, amount: "12"}
+          ]
+        }
+      ];
 
-    const appDefinition = Wallet.createRandom().address;
-    const appData = "0x0";
-    const participants = [
-      {
-        participantId: "user-a",
-        signingAddress: signingAddressA,
-        destination: destinationA
-      },
-      {
-        participantId: "user-b",
-        signingAddress: signingAddressB,
-        destination: destinationB
-      }
-    ];
-    const allocations = [
-      {
-        token: "0x0",
-        allocationItems: [
-          {destination: destinationA, amount: "12"},
-          {destination: destinationB, amount: "12"}
-        ]
-      }
-    ];
+      // Existing data in the store
+      const testChannel = channelFromStates([appState({turnNum: 0})], asAddress, asPrivateKey);
 
-    const testChannelStore = {
-      "0xlol": {
-        address: "0x",
-        privateKey: "0x",
-        channelId: "0xlol",
-        libraryAddress: appDefinition,
-        ourIndex: 0,
-        participants,
-        channelNonce: "0x",
-        turnNum: 0,
-        signedStates: [
-          {
-            state: {
-              turnNum: 0,
-              isFinal: false,
-              channel: {participants: [signingAddressA, signingAddressB]},
-              outcome: [
-                {
-                  assetHolderAddress: "0x0",
-                  allocation: [
-                    {destination: destinationA, amount: "12"},
-                    {destination: destinationB, amount: "12"}
-                  ]
-                }
-              ],
-              challengeDuration: 0,
-              appDefinition,
-              appData: "0x"
-            },
-            signature: {v: 0, r: "", s: ""}
-          }
-        ],
-        funded: true
-      }
-    };
+      const requestMessage = JSON.stringify({
+        jsonrpc: "2.0",
+        method: "UpdateChannel",
+        id: 1,
+        params: {
+          channelId: testChannel.channelId,
+          allocations,
+          appData
+        }
+      });
 
-    const requestMessage = JSON.stringify({
-      jsonrpc: "2.0",
-      method: "UpdateChannel",
-      id: 1,
-      params: {
-        channelId: "0xlol",
-        allocations,
-        appData
-      }
-    });
+      const {effects} = await expectSaga(messageHandler, requestMessage, "localhost")
+        .withState({...initialState, channelStore: setChannel({}, testChannel)})
+        // Mock out the fork call so we don't actually try to post the message
+        .provide([[matchers.fork.fn(messageSender), 0]])
+        .run();
 
-    const {effects} = await expectSaga(messageHandler, requestMessage, "localhost")
-      .withState({...initialState, channelStore: testChannelStore})
-      // Mock out the fork call so we don't actually try to post the message
-      .provide([[matchers.fork.fn(messageSender), 0]])
-      .run();
-
-    expect(effects.put[0].payload.action).toMatchObject({
-      type: "WALLET.APPLICATION.OWN_STATE_RECEIVED",
-      state: {
-        appData,
-        channel: {participants: [signingAddressA, signingAddressB]},
-        outcome: [
-          {
-            assetHolderAddress: "0x0",
-            allocation: [
-              {destination: destinationA, amount: "12"},
-              {destination: destinationB, amount: "12"}
-            ]
-          }
-        ]
-      }
+      expect(effects.put[0].payload.action).toMatchObject({
+        type: "WALLET.APPLICATION.OWN_STATE_RECEIVED",
+        state: {
+          appData,
+          turnNum: 1,
+          outcome: [
+            {allocation: allocations[0].allocationItems, assetHolderAddress: allocations[0].token}
+          ]
+        }
+      });
     });
   });
 });
