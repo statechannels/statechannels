5193aa1678498328b99169f60325310cbbb35086
cf19drofxots <nima@l4v.io>
2019-11-15 00:07:47 -0800

    chore: remove stale jest setup/teardown files

--- packages/wallet/config/jest/global-setup-jest-contracts.ts
+++
@@ -1,8 +0,0 @@
-import log from "loglevel";
-
-import dotEnvExtended from "dotenv-extended";
-dotEnvExtended.load();
-
-log.setDefaultLevel(log.levels.INFO);
-
-export default async function globalSetup() {}
