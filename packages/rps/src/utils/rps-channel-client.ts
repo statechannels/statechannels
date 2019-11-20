45db2258d33f234598efd4be5faee35161738b42
Tom Close <tom.close@cantab.net>
2019-11-20 10:00:36 +0000

    Make a start on an RPSChannelClient

---
+++ packages/rps/src/utils/rps-channel-client.ts
@@ -0,0 +1,63 @@
+import { AppData, ChannelState, encodeAppData } from '../core';
+import { BigNumber, bigNumberify } from 'ethers/utils';
+import { ChannelClient } from './channel-client';
+import { RPS_ADDRESS } from '../constants';
+
+// This class wraps the channel client converting the request/response formats to those used in the app
+
+export class RPSChannelClient {
+  channelClient: ChannelClient;
+
+  constructor() {
+    // might want to pass this in later
+    this.channelClient = new ChannelClient();
+  }
+
+  async createChannel(
+    aAddress: string,
+    bAddress: string,
+    aBal: BigNumber,
+    bBal: BigNumber,
+    appAttrs: AppData
+  ): Promise<ChannelState> {
+    const participants = [
+      { participantId: aAddress, signingAddress: aAddress, destination: aAddress },
+      { participantId: bAddress, signingAddress: bAddress, destination: bAddress },
+    ];
+
+    const allocations = [
+      {
+        token: '0x0',
+        allocationItems: [
+          { destination: aAddress, amount: aBal },
+          { destination: bAddress, amount: bBal },
+        ],
+      },
+    ];
+
+    const appDefinition = RPS_ADDRESS;
+
+    const appData = encodeAppData(appAttrs);
+
+    // ignore return val for now and stub out response
+    await this.channelClient.createChannel({ participants, allocations, appDefinition, appData });
+
+    return await {
+      turnNum: bigNumberify(0),
+      status: 'open',
+      aUserId: aAddress,
+      bUserId: bAddress,
+      aDestination: aAddress,
+      bDestination: bAddress,
+      aBal,
+      bBal,
+      appData: appAttrs,
+    };
+  }
+
+  async joinChannel() {}
+
+  async updateChannel(channelId, aBal, bBal, appData: AppData) {}
+
+  async pushMessage() {}
+}
