e3d011bbd2f4b0135a6ffb262cd0af8ed866c6a2
George Knee <georgeknee@googlemail.com>
2019-11-20 13:04:27 +0000

    Use RPSChannelClient not ChannelClient

--- packages/rps/src/redux/message-service/channel-updated-listener.ts
+++ packages/rps/src/redux/message-service/channel-updated-listener.ts
@@ -1,30 +1,29 @@
-import { take, put } from 'redux-saga/effects';
-import { buffers, eventChannel } from 'redux-saga';
-import {
-  ChannelClient,
-  JsonRPCNotification,
-  Message,
-  NotificationName,
-} from '../../utils/channelClient';
-import { updateChannelState } from '../game/actions';
+import {take, put} from 'redux-saga/effects';
+import {buffers, eventChannel} from 'redux-saga';
+import {JsonRPCNotification, Message, NotificationName} from '../../utils/channel-client';
+import {RPSChannelClient} from '../../utils/rps-channel-client';
+import {updateChannelState} from '../game/actions';
 
 // TODO this is copied from message-queued-listener.ts so can be factored into a helper
-function createSubscribeFunction(channelClient: ChannelClient, notificationName: NotificationName) {
+function createSubscribeFunction(
+  rpsChannelClient: RPSChannelClient,
+  notificationName: NotificationName
+) {
   const subscribe = emit => {
-    channelClient.onMessageReceived(notificationName, event => {
+    rpsChannelClient.onMessageReceived(notificationName, event => {
       emit(event);
     });
 
     return () => {
-      channelClient.unSubscribe(notificationName); // TODO
+      rpsChannelClient.unSubscribe(notificationName); // TODO
     };
   };
   return subscribe;
 }
 
 export function* channelUpdatedListener() {
-  const channelClient = new ChannelClient();
-  const subscribe = createSubscribeFunction(channelClient, 'ChannelUpdated');
+  const rpsChannelClient = new RPSChannelClient();
+  const subscribe = createSubscribeFunction(rpsChannelClient, 'ChannelUpdated');
   const channel = eventChannel(subscribe, buffers.fixed(10));
 
   while (true) {
