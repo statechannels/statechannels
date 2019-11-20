a9a027fa99bafe0e718e7504a73fdb87da7d7362
George Knee <georgeknee@googlemail.com>
2019-11-20 13:04:27 +0000

    Use RPSChannelClient not ChannelClient

--- packages/rps/src/redux/message-service/message-queued-listener.ts
+++ packages/rps/src/redux/message-service/message-queued-listener.ts
@@ -1,29 +1,28 @@
-import { take, fork } from 'redux-saga/effects';
-import { buffers, eventChannel } from 'redux-saga';
-import { reduxSagaFirebase } from '../../gateways/firebase';
-import {
-  ChannelClient,
-  JsonRPCNotification,
-  Message,
-  NotificationName,
-} from '../../utils/channelClient';
+import {take, fork} from 'redux-saga/effects';
+import {buffers, eventChannel} from 'redux-saga';
+import {reduxSagaFirebase} from '../../gateways/firebase';
+import {JsonRPCNotification, Message, NotificationName} from '../../utils/channel-client';
+import {RPSChannelClient} from '../../utils/rps-channel-client';
 
-function createSubscribeFunction(channelClient: ChannelClient, notificationName: NotificationName) {
+function createSubscribeFunction(
+  rpsChannelClient: RPSChannelClient,
+  notificationName: NotificationName
+) {
   const subscribe = emit => {
-    channelClient.onMessageReceived(notificationName, event => {
+    rpsChannelClient.onMessageReceived(notificationName, event => {
       emit(event);
     });
 
     return () => {
-      channelClient.unSubscribe(notificationName); // TODO
+      rpsChannelClient.unSubscribe(notificationName); // TODO
     };
   };
   return subscribe;
 }
 
 export function* messageQueuedListener() {
-  const channelClient = new ChannelClient();
-  const subscribe = createSubscribeFunction(channelClient, 'MessageQueued');
+  const rpsChannelClient = new RPSChannelClient();
+  const subscribe = createSubscribeFunction(rpsChannelClient, 'MessageQueued');
   const channel = eventChannel(subscribe, buffers.fixed(10));
 
   while (true) {
