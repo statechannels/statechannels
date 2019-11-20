27a616576629a6e6361c492cb1d5b82017833b5c
George Knee <georgeknee@googlemail.com>
2019-11-20 15:51:36 +0000

    Add onChannelUpdated()
    add function to rps wrapper
    remove subscribe factory from saga

--- packages/rps/src/redux/message-service/channel-updated-listener.ts
+++ packages/rps/src/redux/message-service/channel-updated-listener.ts
@@ -1,29 +1,22 @@
 import {take, put} from 'redux-saga/effects';
 import {buffers, eventChannel} from 'redux-saga';
-import {JsonRPCNotification, Message, NotificationName} from '../../utils/channel-client';
+import {JsonRPCNotification, Message} from '../../utils/channel-client';
 import {RPSChannelClient} from '../../utils/rps-channel-client';
 import {updateChannelState} from '../game/actions';
 
-// TODO this is copied from message-queued-listener.ts so can be factored into a helper
-function createSubscribeFunction(
-  rpsChannelClient: RPSChannelClient,
-  notificationName: NotificationName
-) {
+export function* channelUpdatedListener() {
+  const rpsChannelClient = new RPSChannelClient();
+
   const subscribe = emit => {
-    rpsChannelClient.onMessageReceived(notificationName, event => {
+    rpsChannelClient.onChannelUpdated(event => {
       emit(event);
     });
 
     return () => {
-      rpsChannelClient.unSubscribe(notificationName); // TODO
+      rpsChannelClient.unSubscribe('ChannelUpdated');
     };
   };
-  return subscribe;
-}
 
-export function* channelUpdatedListener() {
-  const rpsChannelClient = new RPSChannelClient();
-  const subscribe = createSubscribeFunction(rpsChannelClient, 'ChannelUpdated');
   const channel = eventChannel(subscribe, buffers.fixed(10));
 
   while (true) {
