12e6483739d4963fb87c33eb064eaee5730a6908
George Knee <georgeknee@googlemail.com>
2019-11-20 18:50:12 +0000

    Remove deployment scripts from rps

--- packages/rps/deployment/deploy.ts
+++
@@ -1,9 +0,0 @@
-import { GanacheServer } from '@statechannels/devtools';
-import log from 'loglevel';
-
-import rpsArtifact from '../build/contracts/RockPaperScissors.json';
-
-export async function deployContracts(chain: GanacheServer): Promise<object> {
-  log.info(`Deploying built contracts to chain at: ${chain.provider.connection.url}`);
-  return chain.deployContracts([rpsArtifact]);
-}
