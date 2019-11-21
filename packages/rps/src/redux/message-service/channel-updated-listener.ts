3e531cb3076699103f1d380fd4d1b65d68299c77
George Knee <georgeknee@googlemail.com>
2019-11-21 07:54:42 +0000

    Use a callback that emits a channelState

--- packages/rps/src/redux/message-service/channel-updated-listener.ts
+++ packages/rps/src/redux/message-service/channel-updated-listener.ts
@@ -1,15 +1,15 @@
 import {take, put} from 'redux-saga/effects';
 import {buffers, eventChannel} from 'redux-saga';
-import {JsonRPCNotification, Message} from '../../utils/channel-client';
 import {RPSChannelClient} from '../../utils/rps-channel-client';
-import {updateChannelState} from '../game/actions';
+import {updateChannelState} from '../game-v2/actions';
+import {ChannelState} from '../../core';
 
 export function* channelUpdatedListener() {
   const rpsChannelClient = new RPSChannelClient();
 
   const subscribe = emit => {
-    rpsChannelClient.onChannelUpdated(event => {
-      emit(event);
+    rpsChannelClient.onChannelUpdated(channelState => {
+      emit(channelState);
     });
 
     return () => {
@@ -20,7 +20,7 @@ export function* channelUpdatedListener() {
   const channel = eventChannel(subscribe, buffers.fixed(10));
 
   while (true) {
-    const message: JsonRPCNotification<Message> = yield take(channel);
-    yield put(updateChannelState(message.params.data));
+    const channelState: ChannelState = yield take(channel);
+    yield put(updateChannelState(channelState));
   }
 }
