d0f6d0159f0721dccd35fb25d3041ec2334cf23f
Tom Close <tom.close@cantab.net>
2019-11-21 10:18:30 +0000

    Insufficient funds flow for player A

--- packages/rps/src/redux/game-v2/saga.ts
+++ packages/rps/src/redux/game-v2/saga.ts
@@ -1,11 +1,17 @@
-import {take, select, call, put} from 'redux-saga/effects';
+import { take, select, call, put } from 'redux-saga/effects';
 // import { take } from 'redux-saga/effects';
-import {RPSChannelClient} from '../../utils/rps-channel-client';
-import {AppData, hashPreCommit, calculateResult, updateAllocation, Player} from '../../core';
-import {updateChannelState, chooseSalt, resultArrived, restart} from './actions';
-import {GameState} from './state';
-import {randomHex} from '../../utils/randomHex';
-import {bigNumberify} from 'ethers/utils';
+import { RPSChannelClient } from '../../utils/rps-channel-client';
+import { AppData, hashPreCommit, calculateResult, updateAllocation, Player } from '../../core';
+import {
+  updateChannelState,
+  chooseSalt,
+  resultArrived,
+  restart,
+  FundingSituation,
+} from './actions';
+import { GameState } from './state';
+import { randomHex } from '../../utils/randomHex';
+import { bigNumberify, BigNumber } from 'ethers/utils';
 
 const getGameState = (state: any): GameState => state.game;
 
@@ -14,12 +20,12 @@ export function* gameSaga(channelClient: RPSChannelClient) {
   while (true) {
     yield take('*'); // run after every action
 
-    const {localState, channelState}: GameState = yield select(getGameState);
+    const { localState, channelState }: GameState = yield select(getGameState);
 
     if ('player' in localState && localState.player === 'A') {
       if (localState.type === 'GameChosen' && !channelState) {
         const openingBalance = localState.roundBuyIn.mul(5);
-        const startState: AppData = {type: 'start'};
+        const startState: AppData = { type: 'start' };
         const newChannelState = yield call(
           channelClient.createChannel,
           localState.address,
@@ -34,12 +40,12 @@ export function* gameSaga(channelClient: RPSChannelClient) {
         const salt = yield call(randomHex, 64);
         yield put(chooseSalt(salt)); // transitions us to WeaponAndSaltChosen
 
-        const {myWeapon, roundBuyIn: stake} = localState;
-        const {channelId, aBal, bBal} = channelState;
+        const { myWeapon, roundBuyIn: stake } = localState;
+        const { channelId, aBal, bBal } = channelState;
 
         const preCommit = hashPreCommit(myWeapon, salt);
 
-        const roundProposed: AppData = {type: 'roundProposed', preCommit, stake};
+        const roundProposed: AppData = { type: 'roundProposed', preCommit, stake };
 
         const updatedChannelState = yield call(
           channelClient.updateChannel,
@@ -54,9 +60,9 @@ export function* gameSaga(channelClient: RPSChannelClient) {
         channelState &&
         channelState.appData.type === 'roundAccepted'
       ) {
-        const {myWeapon, salt} = localState;
-        const {aBal, bBal, channelId} = channelState;
-        const {playerBWeapon: theirWeapon, stake} = channelState.appData;
+        const { myWeapon, salt } = localState;
+        const { aBal, bBal, channelId } = channelState;
+        const { playerBWeapon: theirWeapon, stake } = channelState.appData;
         const result = calculateResult(myWeapon, theirWeapon);
         const [aBal2, bBal2] = updateAllocation(
           result,
@@ -65,6 +71,7 @@ export function* gameSaga(channelClient: RPSChannelClient) {
           bigNumberify(aBal),
           bigNumberify(bBal)
         );
+        const fundingSituation = calculateFundingSituation(Player.PlayerA, aBal2, bBal2, stake);
 
         const reveal: AppData = {
           type: 'reveal',
@@ -81,7 +88,7 @@ export function* gameSaga(channelClient: RPSChannelClient) {
           reveal
         );
         yield put(updateChannelState(updatedChannelState));
-        yield put(resultArrived(theirWeapon, result));
+        yield put(resultArrived(theirWeapon, result, fundingSituation));
       } else if (
         localState.type === 'WaitForRestart' &&
         channelState &&
@@ -121,3 +128,20 @@ export function* gameSaga(channelClient: RPSChannelClient) {
   // if channelProposed, and has confirmed
   // call joinChannel
 }
+
+const calculateFundingSituation = (
+  player: Player,
+  aBal: BigNumber,
+  bBal: BigNumber,
+  stake: BigNumber
+): FundingSituation => {
+  const [myBal, theirBal] = player === Player.PlayerA ? [aBal, bBal] : [bBal, aBal];
+
+  if (myBal.lt(stake)) {
+    return 'MyFundsTooLow';
+  } else if (theirBal.lt(stake)) {
+    return 'OpponentsFundsTooLow';
+  } else {
+    return 'Ok';
+  }
+};
