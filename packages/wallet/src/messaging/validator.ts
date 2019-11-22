20e23b32d4d39a680e3ca20ad71ddf6a7c44e34f
Alex Gap <alex.gap@gmail.com>
2019-11-21 20:13:07 -0800

    Add notification schema

--- packages/wallet/src/messaging/validator.ts
+++ packages/wallet/src/messaging/validator.ts
@@ -1,10 +1,12 @@
 import Ajv, {ErrorObject} from "ajv";
 import * as requestSchema from "./schema/request.json";
+import * as responseSchema from "./schema/response.json";
 import * as createChannelSchema from "./schema/create-channel.json";
 import * as getAddressSchema from "./schema/get-address.json";
 import * as joinChannelSchema from "./schema/join-channel.json";
 import * as updateChannelSchema from "./schema/update-channel.json";
 import * as definitionsSchema from "./schema/definitions.json";
+import * as notifSchema from "./schema/notification.json";
 
 export interface ValidationResult {
   isValid: boolean;
@@ -21,6 +23,34 @@ export async function validateRequest(jsonRpcRequest: object): Promise<Validatio
     .addSchema(updateChannelSchema)
     .compile(requestSchema);
   const isValid = await validate(jsonRpcRequest);
+
+  return {isValid, errors: validate.errors ? validate.errors : []};
+}
+
+export async function validateResponse(jsonRpcResponse: object): Promise<ValidationResult> {
+  const ajv = new Ajv();
+  const validate = ajv
+    .addSchema(definitionsSchema)
+    .addSchema(createChannelSchema)
+    .addSchema(getAddressSchema)
+    .addSchema(joinChannelSchema)
+    .addSchema(updateChannelSchema)
+    .compile(responseSchema);
+  const isValid = await validate(jsonRpcResponse);
+
+  return {isValid, errors: validate.errors ? validate.errors : []};
+}
+
+export async function validateNotification(jsonRpcNotification: object): Promise<ValidationResult> {
+  const ajv = new Ajv();
+  const validate = ajv
+    .addSchema(definitionsSchema)
+    .addSchema(createChannelSchema)
+    .addSchema(getAddressSchema)
+    .addSchema(joinChannelSchema)
+    .addSchema(updateChannelSchema)
+    .compile(notifSchema);
+  const isValid = await validate(jsonRpcNotification);
 
   return {isValid, errors: validate.errors ? validate.errors : []};
 }
