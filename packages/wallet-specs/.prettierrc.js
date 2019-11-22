30b5d4e31957d9ba04bd3802448aa5831f6d28a4
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-11-21 18:24:35 -0700

    Set up repo

---
+++ packages/wallet-specs/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'es5',
+  overrides: [
+    {
+      files: '*.sol',
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
+};
