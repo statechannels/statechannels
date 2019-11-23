37c79e49a108f37efed05a5b95f1a138bdd8a8b3
Alex Gap <alex.gap@gmail.com>
2019-11-23 15:59:40 -0800

    Disable test setup for contract tests

--- packages/wallet/config/jest/jest.contracts.config.js
+++ packages/wallet/config/jest/jest.contracts.config.js
@@ -3,4 +3,5 @@ let config = require("./jest.config");
 module.exports = {
   ...config,
   testMatch: ["<rootDir>/src/**/contract-tests/?(*.)test.ts?(x)"],
+  setupFilesAfterEnv: [] // We don't want to run a test setup that mocks out contract related utils
 };
