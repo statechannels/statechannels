8d3178e0a9a0756813ac99187cce1c32dc7c2e84
Alon Bukai <alonzorz@gmail.com>
2019-11-23 20:18:12 +0200

    chore: ESLint write devtools

--- packages/devtools/src/utils/ganache-server.ts
+++ packages/devtools/src/utils/ganache-server.ts
@@ -1,13 +1,13 @@
-import {spawn} from "child_process";
-import {EtherlimeGanacheDeployer} from "etherlime-lib";
-import {ethers} from "ethers";
-import {JsonRpcProvider} from "ethers/providers";
-import log from "loglevel";
-import {waitUntilFree, waitUntilUsed} from "tcp-port-used";
-import kill from "tree-kill";
+import {spawn} from 'child_process';
+import {EtherlimeGanacheDeployer} from 'etherlime-lib';
+import {ethers} from 'ethers';
+import {JsonRpcProvider} from 'ethers/providers';
+import log from 'loglevel';
+import {waitUntilFree, waitUntilUsed} from 'tcp-port-used';
+import kill from 'tree-kill';
 
-import {ETHERLIME_ACCOUNTS} from "../constants";
-import {Account, DeployedArtifacts, Deployment} from "../types";
+import {ETHERLIME_ACCOUNTS} from '../constants';
+import {Account, DeployedArtifacts, Deployment} from '../types';
 
 log.setDefaultLevel(log.levels.INFO);
 
@@ -15,6 +15,40 @@ export class GanacheServer {
   provider: JsonRpcProvider;
   fundedPrivateKey: string;
   server: any;
+
+  constructor(
+    public readonly port: number = 8545,
+    accounts: Account[] = ETHERLIME_ACCOUNTS,
+    public readonly timeout: number = 5000,
+    gasLimit = 1000000000,
+    gasPrice = '0x01'
+  ) {
+    log.info(
+      `Starting ganache on port ${this.port} with network ID ${process.env.GANACHE_NETWORK_ID}`
+    );
+
+    this.fundedPrivateKey = accounts[0].privateKey;
+
+    const oneMillion = ethers.utils.parseEther('1000000');
+
+    const concat = (a, b) => a.concat(b);
+    const opts = [
+      [`--networkId ${process.env.GANACHE_NETWORK_ID}`, `--port ${this.port}`],
+      accounts.map(a => `--account ${a.privateKey},${a.amount || oneMillion}`),
+      [`--gasLimit ${gasLimit}`, `--gasPrice ${gasPrice}`]
+    ]
+      .reduce(concat)
+      .join(' ');
+
+    const cmd = `ganache-cli ${opts}`;
+
+    this.server = spawn('npx', ['-c', cmd], {stdio: 'pipe'});
+    this.server.stderr.on('data', data => {
+      log.error(`Server threw error ${data}`);
+      throw new Error('Ganache server failed to start');
+    });
+    this.provider = new JsonRpcProvider(`http://localhost:${this.port}`);
+  }
 
   static async connect(port: number): Promise<GanacheServer> {
     const provider = new JsonRpcProvider(`http://localhost:${port}`);
@@ -26,58 +60,20 @@ export class GanacheServer {
     }
   }
 
-  constructor(
-    public readonly port: number = 8545,
-    accounts: Account[] = ETHERLIME_ACCOUNTS,
-    public readonly timeout: number = 5000,
-    gasLimit: number = 1000000000,
-    gasPrice: string = "0x01"
-  ) {
-    log.info(
-      `Starting ganache on port ${this.port} with network ID ${process.env.GANACHE_NETWORK_ID}`
-    );
-
-    this.fundedPrivateKey = accounts[0].privateKey;
-
-    const oneMillion = ethers.utils.parseEther("1000000");
-
-    const concat = (a, b) => a.concat(b);
-    const opts = [
-      [`--networkId ${process.env.GANACHE_NETWORK_ID}`, `--port ${this.port}`],
-      accounts.map(a => `--account ${a.privateKey},${a.amount || oneMillion}`),
-      [`--gasLimit ${gasLimit}`, `--gasPrice ${gasPrice}`]
-    ]
-      .reduce(concat)
-      .join(" ");
-
-    const cmd = `ganache-cli ${opts}`;
-
-    this.server = spawn("npx", ["-c", cmd], {stdio: "pipe"});
-    this.server.stderr.on("data", data => {
-      log.error(`Server threw error ${data}`);
-      throw new Error("Ganache server failed to start");
-    });
-    this.provider = new JsonRpcProvider(`http://localhost:${this.port}`);
-  }
-
   async ready() {
     await waitUntilUsed(this.port, 500, this.timeout);
   }
 
   async close() {
-    try {
-      kill(this.server.pid);
-      await waitUntilFree(this.port, 500, this.timeout);
-    } catch (err) {
-      throw err;
-    }
+    kill(this.server.pid);
+    await waitUntilFree(this.port, 500, this.timeout);
   }
 
   onClose(listener: () => void) {
-    this.server.on("close", listener);
+    this.server.on('close', listener);
   }
 
-  async deployContracts(deployments: Array<Deployment | any>): Promise<DeployedArtifacts> {
+  async deployContracts(deployments: (Deployment | any)[]): Promise<DeployedArtifacts> {
     const deployer = new EtherlimeGanacheDeployer(undefined, Number(process.env.GANACHE_PORT));
 
     const deployedArtifacts: DeployedArtifacts = {};
