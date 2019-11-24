3e96473bb0b060f1aeb021ec57d6b647ad301400
Alex Gap <alex.gap@gmail.com>
2019-11-23 16:17:14 -0800

    Separarte contract test setup

--- packages/wallet/config/jest/jest.contracts.config.js
+++ packages/wallet/config/jest/jest.contracts.config.js
@@ -3,5 +3,5 @@ let config = require("./jest.config");
 module.exports = {
   ...config,
   testMatch: ["<rootDir>/src/**/contract-tests/?(*.)test.ts?(x)"],
-  setupFilesAfterEnv: [] // We don't want to run a test setup that mocks out contract related utils
+  setupFilesAfterEnv: ["<rootDir>/src/setupContractTests.ts"]
 };
