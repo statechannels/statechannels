648c704678d6af4f600b9d7d44a97309552534b8
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-11-24 12:26:50 -0700

    Make config generation automatic

--- packages/wallet-specs/src/utils.ts
+++ packages/wallet-specs/src/utils.ts
@@ -3,20 +3,17 @@ import serialize from 'serialize-javascript';
 
 export function saveConfig(
   config: any,
+  dirName = '/null', // TODO: make this required
   { guards, actions }: { guards?: any; actions?: any }
 ) {
-  const path = process.cwd();
-  const dirName = path.substring(path.lastIndexOf('/') + 1);
-  const filename = `../../../src/protocols/${dirName}/protocol.config.js`;
-
   fs.writeFile(
-    filename,
+    `${dirName}/protocol.config.js`,
     `
 const config = ${serialize(config)}
 const guards = ${serialize(guards || {})}
 const customActions = ${serialize(actions || {})}
 const machine = Machine(config, {guards, actions: customActions})
     `,
-    (err: any) => ({})
+    console.error
   );
 }
