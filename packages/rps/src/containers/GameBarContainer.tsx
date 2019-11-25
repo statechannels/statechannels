637b5efbe4efeb1e1d4699f9ece7fd83e77dd346
George Knee <georgeknee@googlemail.com>
2019-11-25 16:05:23 +0000

    GameBarContainer has default aBal, bBal

--- packages/rps/src/containers/GameBarContainer.tsx
+++ packages/rps/src/containers/GameBarContainer.tsx
@@ -3,13 +3,13 @@ import { connect } from 'react-redux';
 import GameBar from '../components/GameBar';
 import { SiteState } from '../redux/reducer';
 import { PlayingState } from 'src/redux/game-v2/state';
-import { ChannelState } from 'src/core';
 
 function mapStateToProps(state: SiteState) {
   const { localState, channelState } = state.game;
 
   const { name, opponentName, roundBuyIn, player } = localState as PlayingState;
-  const { aBal, bBal } = channelState as ChannelState; // TODO do not use type assertions
+  const aBal = channelState ? channelState.aBal : '';
+  const bBal = channelState ? channelState.aBal : '';
 
   const myBalance = player === 'A' ? aBal : bBal;
   const opponentBalance = player === 'B' ? aBal : bBal;
