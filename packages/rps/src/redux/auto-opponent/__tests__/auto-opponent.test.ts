cd332ec64a384398628ee0c98b5f2156ea7b5067
George Knee <georgeknee@googlemail.com>
2019-11-29 13:15:00 +0000

    Add comments to auto-opponent test and tweak timeout

--- packages/rps/src/redux/auto-opponent/__tests__/auto-opponent.test.ts
+++ packages/rps/src/redux/auto-opponent/__tests__/auto-opponent.test.ts
@@ -9,30 +9,34 @@ import { openGamesReducer } from '../../open-games/reducer';
 import { channelUpdatedListener } from '../../message-service/channel-updated-listener';
 import { autoPlayer, autoOpponent } from '../';
 
+const SUFFICIENT_TIME_TO_GET_TO_TURNUM_14 = 7000; // test will take at least this long to run
 const reducer = combineReducers({
   game: gameReducer,
   openGames: openGamesReducer,
 });
 
-it('runs to GameOver', async () => {
-  const client = new RPSChannelClient();
-  function* saga() {
-    yield fork(gameSaga, client);
-    yield fork(autoPlayer, 'A');
-    yield fork(autoOpponent, 'B', client);
-    yield fork(channelUpdatedListener, client);
-  }
-  const initialState = {
-    game: {
-      localState: lobby({ name: 'Player A', address: 'blah' }),
-      channelState: null,
-    },
-    openGames: [],
-  };
+it(
+  'runs to GameOver',
+  async () => {
+    const client = new RPSChannelClient();
+    function* saga() {
+      yield fork(gameSaga, client);
+      yield fork(autoPlayer, 'A');
+      yield fork(autoOpponent, 'B', client);
+      yield fork(channelUpdatedListener, client);
+    }
+    const initialState = {
+      game: {
+        localState: lobby({ name: 'Player A', address: 'blah' }),
+        channelState: null,
+      },
+      openGames: [],
+    };
 
-  const { storeState } = await expectSaga(saga)
-    .withReducer(reducer, initialState)
-    .silentRun(5000);
-
-  expect(storeState.game.localState.type).toEqual('GameOver');
-}, 6000);
+    const { storeState } = await expectSaga(saga)
+      .withReducer(reducer, initialState)
+      .silentRun(SUFFICIENT_TIME_TO_GET_TO_TURNUM_14); // Kill the saga after (hopefully) enough time. Brittle.
+    expect(storeState.game.localState.type).toEqual('GameOver');
+  },
+  SUFFICIENT_TIME_TO_GET_TO_TURNUM_14 + 1000
+); // We don't want jest to timeout
