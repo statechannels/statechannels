2be5e67523f610a741cd7839a52e0cf62b28a977
George Knee <georgeknee@googlemail.com>
2019-11-29 13:12:59 +0000

    Remove buttons from ConfirmGamePage
    Calls to and response from wallet client will progress game state

--- packages/rps/src/components/ConfirmGamePage.tsx
+++ packages/rps/src/components/ConfirmGamePage.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
-import { Button } from 'reactstrap';
-
 import { ApplicationLayout } from './ApplicationLayout';
 import * as web3Utils from 'web3-utils';
 interface Props {
-  confirmGame: () => void;
-  cancelGame: () => void;
   stake: string;
   opponentName: string;
 }
 
 export default class ConfirmGamePage extends React.PureComponent<Props> {
   render() {
-    const { confirmGame, cancelGame, stake, opponentName } = this.props;
+    const { stake, opponentName } = this.props;
     return (
       <ApplicationLayout>
         <div className="w-100 text-center mb-5">
@@ -23,16 +19,7 @@ export default class ConfirmGamePage extends React.PureComponent<Props> {
               {opponentName} has accepted your challenge with a {web3Utils.fromWei(stake, 'ether')}{' '}
               ETH buy in.
             </p>
-            <p>Do you want to play?</p>
-
-            <div>
-              <Button className="confirm-button" outline={true} onClick={confirmGame}>
-                Play
-              </Button>
-              <Button className="confirm-button" outline={true} onClick={cancelGame}>
-                Cancel
-              </Button>
-            </div>
+            <p>Waiting for your wallet...</p>
           </div>
         </div>
       </ApplicationLayout>
