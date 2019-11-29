1716e8601460bf60beedc73465a22ede6bbe1012
Tom Close <tom.close@cantab.net>
2019-11-29 17:00:15 +0000

    Handle the case where network-context.json doesn't exist and remove .bak process

--- packages/ganache-deployer/src/index.ts
+++ packages/ganache-deployer/src/index.ts
@@ -7,7 +7,6 @@ import {deployContracts} from './deployer';
 
 const GANACHE_CONTRACTS_FILE = 'ganache-network-context.json';
 const GANACHE_CONTRACTS_PATH = path.join(__dirname, '../', GANACHE_CONTRACTS_FILE);
-const GANACHE_CONTRACTS_BACKUP_PATH = path.join(__dirname, '../', `${GANACHE_CONTRACTS_FILE}.bak`);
 
 export function getNetworkContext() {
   if (!fs.existsSync(GANACHE_CONTRACTS_PATH)) {
@@ -18,10 +17,6 @@ export function getNetworkContext() {
 }
 
 export async function startGanacheAndDeploy(): Promise<GanacheServer> {
-  // TODO: This should probably merge the existing file instead of replacing it
-  log.info(`Moving existing network context file to : ${GANACHE_CONTRACTS_BACKUP_PATH}\n`);
-  fs.renameSync(GANACHE_CONTRACTS_PATH, GANACHE_CONTRACTS_BACKUP_PATH);
-  log.info(`Writing network context into file: ${GANACHE_CONTRACTS_PATH}\n`);
   try {
     const chain = new GanacheServer(Number(process.env.GANACHE_PORT));
     chain.onClose(exitHandler);
@@ -34,21 +29,14 @@ export async function startGanacheAndDeploy(): Promise<GanacheServer> {
     log.info(`Network context written to ${GANACHE_CONTRACTS_FILE}`);
     return chain;
   } catch (e) {
-    if (!fs.existsSync(GANACHE_CONTRACTS_PATH)) {
-      fs.renameSync(GANACHE_CONTRACTS_BACKUP_PATH, GANACHE_CONTRACTS_PATH);
-    }
     throw Error(e);
   }
 }
 
 async function exitHandler() {
   try {
-    fs.unlink(GANACHE_CONTRACTS_PATH, () => {
-      log.info(`Deleted locally deployed network context: ${GANACHE_CONTRACTS_FILE}`);
-
-      fs.renameSync(GANACHE_CONTRACTS_BACKUP_PATH, GANACHE_CONTRACTS_PATH);
-      log.info(`Restored original file from ${GANACHE_CONTRACTS_BACKUP_PATH}`);
-    });
+    fs.unlinkSync(GANACHE_CONTRACTS_PATH);
+    log.info(`Deleted locally deployed network context: ${GANACHE_CONTRACTS_FILE}`);
   } catch (e) {
     log.warn(`Failed to properly clean up`);
     log.error(e);
