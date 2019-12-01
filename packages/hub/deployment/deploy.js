d2f1560e9343c5b028ae780f3d3a60e671c945bc
Tom Close <tom.close@cantab.net>
2019-12-01 21:47:38 +0000

    Convert hub to new approach

---
+++ packages/hub/deployment/deploy.js
@@ -0,0 +1,20 @@
+const consensusAppArtifact = require("../build/contracts/ConsensusApp.json");
+const ethAssetHolderArtifact = require("../build/contracts/ETHAssetHolder.json");
+const nitroAdjudicatorArtifact = require("../build/contracts/NitroAdjudicator.json");
+const trivialAppArtifact = require("../build/contracts/TrivialApp.json");
+
+const {GanacheNCacheDeployer} = require("@statechannels/ganache-deployer");
+
+const deploy = async (_network, _secret, _etherscanApiKey) => {
+  const deployer = new GanacheNCacheDeployer(8547);
+
+  await deployer.deploy(consensusAppArtifact);
+  await deployer.deploy(trivialAppArtifact);
+
+  const nitroAdjudicatorAddress = await deployer.deploy(nitroAdjudicatorArtifact);
+  await deployer.deploy(ethAssetHolderArtifact, {}, nitroAdjudicatorAddress);
+};
+
+module.exports = {
+  deploy
+};
