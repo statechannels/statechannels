9daa474e27a95675e11dd4498f1670e80cc61dbf
Tom Close <tom.close@cantab.net>
2019-12-03 22:03:01 +0000

    Add the shared ganache to rps

--- packages/rps/deployment/deploy.js
+++ packages/rps/deployment/deploy.js
@@ -1,10 +1,6 @@
 const rpsArtifact = require('../build/contracts/RockPaperScissors');
 
-const { GanacheDeployer } = require('@statechannels/devtools');
-
-const deploy = async () => {
-  const deployer = new GanacheDeployer(Number(process.env.GANACHE_PORT));
-
+const deploy = async deployer => {
   const RPS_CONTRACT_ADDRESS = await deployer.deploy(rpsArtifact);
   return { RPS_CONTRACT_ADDRESS };
 };
