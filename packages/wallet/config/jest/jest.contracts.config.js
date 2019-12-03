5dc02bd1659494ece460ce2658f321f3642ca7f1
Liam Horne <liam@lihorne.com>
2019-12-03 16:41:00 -0500

    Remove redundant jest CLI args (#628)

--- packages/wallet/config/jest/jest.contracts.config.js
+++ packages/wallet/config/jest/jest.contracts.config.js
@@ -2,6 +2,7 @@ let config = require("./jest.config");
 
 module.exports = {
   ...config,
+  bail: true,
   testMatch: ["<rootDir>/src/**/contract-tests/?(*.)test.ts?(x)"],
   globalSetup: "<rootDir>/jest/contract-test-setup.ts",
   globalTeardown: "<rootDir>/jest/contract-test-teardown.ts",
