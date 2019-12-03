3efd4274d0c8b47fcae14ec4ffcc0e6bd339cdff
Alex Gap <alex@magmo.com>
2019-12-03 13:42:30 -0800

    [wallet] Update puppeteer script (#629)
    * Update puppeteer script
    
    * Update helper method to use env
    
    * Switch to CLI arg
    
    * Remove console.log

--- packages/wallet/puppeteer/helpers.ts
+++ packages/wallet/puppeteer/helpers.ts
@@ -46,9 +46,10 @@ export async function loadWallet(page: puppeteer.Page, messageListener: (message
   // TODO: This is kinda ugly but it works
   // We need to instantiate a web3 for the wallet so we import the web 3 script
   // and then assign it on the window
+  const port = process.env.GANACHE_PORT || 8560;
   const web3JsFile = fs.readFileSync(path.resolve(__dirname, "web3/web3.min.js"), "utf8");
   await page.evaluateOnNewDocument(web3JsFile);
-  await page.evaluateOnNewDocument('window.web3 = new Web3("http://localhost:8547")');
+  await page.evaluateOnNewDocument(`window.web3 = new Web3("http://localhost:${port}")`);
   await page.goto("http://localhost:3055/");
 
   await page.waitFor(500); // Delay lets things load
