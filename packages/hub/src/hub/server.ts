aba68a4b40fa8e08b3c9305b5b60a7cdd05ceb37
Mike Kerzhner <mikerzhner@gmail.com>
2019-12-04 18:16:05 -0800

    Hub: use new ganache deployer

--- packages/hub/src/hub/server.ts
+++ packages/hub/src/hub/server.ts
@@ -1,35 +1,48 @@
+import '../../config/env';
+
 import {fork} from 'child_process';
 import {Model} from 'objection';
 import {RelayableAction} from '../communication';
 import knex from '../wallet/db/connection';
-import {assetHolderListen} from '../wallet/services/asset-holder-watcher';
+import {assetHolderListen, RemoveListeners} from '../wallet/services/asset-holder-watcher';
 import {handleWalletMessage} from './handlers/handle-wallet-message';
 
-Model.knex(knex);
+export async function startServer(): Promise<RemoveListeners> {
+  Model.knex(knex);
 
-// A forked process inherits execArgv from the parent
-// --inspect-brk is present when the process is launched via vs code debug
-// The debug port cannot be used for both the parent process and child processes.
-const forkExecArgv = process.execArgv.filter(arg => !arg.includes('--inspect-brk'));
+  // A forked process inherits execArgv from the parent
+  // --inspect-brk is present when the process is launched via vs code debug
+  // The debug port cannot be used for both the parent process and child processes.
+  const forkExecArgv = process.execArgv.filter(arg => !arg.includes('--inspect-brk'));
 
-const firebaseRelay = fork(`${__dirname}/../message/firebase-relay`, [], {
-  execArgv: forkExecArgv
-});
-firebaseRelay.on('message', (message: RelayableAction) => {
-  console.log(
-    `Parent process received message from firebase": ${JSON.stringify(message, null, 1)}`
-  );
+  const firebaseRelay = fork(`${__dirname}/../message/firebase-relay`, [], {
+    execArgv: forkExecArgv,
+    env: process.env
+  });
+  firebaseRelay.on('message', (message: RelayableAction) => {
+    console.log(
+      `Parent process received message from firebase": ${JSON.stringify(message, null, 1)}`
+    );
 
-  handleWalletMessage(message)
-    .then(outgoingMessages => {
-      for (const outgoingMessage of outgoingMessages) {
-        console.log(
-          `Parent process sending message to firebase: ${JSON.stringify(outgoingMessage, null, 1)}`
-        );
-        firebaseRelay.send(outgoingMessage);
-      }
-    })
-    .catch(reason => console.error(reason));
-});
-console.log('Firebase relay sub-process started');
-assetHolderListen();
+    handleWalletMessage(message)
+      .then(outgoingMessages => {
+        for (const outgoingMessage of outgoingMessages) {
+          console.log(
+            `Parent process sending message to firebase: ${JSON.stringify(
+              outgoingMessage,
+              null,
+              1
+            )}`
+          );
+          firebaseRelay.send(outgoingMessage);
+        }
+      })
+      .catch(reason => console.error(reason));
+  });
+  console.log('Firebase relay sub-process started');
+  return await assetHolderListen();
+}
+
+if (require.main === module) {
+  startServer();
+}
