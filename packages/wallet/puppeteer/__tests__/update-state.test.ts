bbe4ee29b479798bd43e70bc2219bb6d472bb141
Alex Gap <alex.gap@gmail.com>
2019-12-05 15:32:17 -0800

    Update state test

--- packages/wallet/puppeteer/__tests__/update-state.test.ts
+++ packages/wallet/puppeteer/__tests__/update-state.test.ts
@@ -20,6 +20,8 @@ describe("State Updating", () => {
   let walletMessages: Emittery.Typed<MessageEventTypes>;
   let messageQueueFromA;
   let messageQueueFromB;
+  let notificationQueueFromA;
+  let notificationQueueFromB;
   beforeAll(async () => {
     browserA = await setUpBrowser(true);
     browserB = await setUpBrowser(true);
@@ -30,7 +32,8 @@ describe("State Updating", () => {
     walletMessages = new Emittery.Typed<MessageEventTypes>();
     messageQueueFromA = walletMessages.events(MessageType.PlayerAMessage);
     messageQueueFromB = walletMessages.events(MessageType.PlayerBMessage);
-
+    notificationQueueFromA = walletMessages.events(MessageType.PlayerANotification);
+    notificationQueueFromB = walletMessages.events(MessageType.PlayerBNotification);
     await loadWallet(walletA, createMessageHandler(walletMessages, "A"));
     await loadWallet(walletB, createMessageHandler(walletMessages, "B"));
     const fundingResult = await completeFunding(walletA, walletB, walletMessages);
@@ -50,16 +53,36 @@ describe("State Updating", () => {
       channelId
     });
   });
+  it("sends a channel updated message to player B", async () => {
+    const channelUpdatedMessage = (await messageQueueFromA.next()).value;
+    expect(channelUpdatedMessage.params.data.type).toEqual("Channel.Updated");
+  });
+
+  it("sends a channel updated event", async () => {
+    const channelUpdatedEvent = (await notificationQueueFromB.next()).value;
+    expect(channelUpdatedEvent.method).toEqual("ChannelUpdated");
+  });
+
   it("updates the state for player B", async () => {
     const updateStatePromise: Promise<any> = walletMessages.once(MessageType.PlayerBResult);
     await sendUpdateState(walletB, channelId, playerAAddress, playerBAddress);
     const response = await updateStatePromise;
 
     expect(response).toMatchObject({
-      turnNum: 4,
+      turnNum: 5,
       status: "Running",
       channelId
     });
+  });
+
+  it("sends a channel updated message to player A", async () => {
+    const channelUpdatedMessage = (await messageQueueFromB.next()).value;
+    expect(channelUpdatedMessage.params.data.type).toEqual("Channel.Updated");
+  });
+
+  it("sends a channel updated event", async () => {
+    const channelUpdatedEvent = (await notificationQueueFromA.next()).value;
+    expect(channelUpdatedEvent.method).toEqual("ChannelUpdated");
   });
 
   afterAll(async () => {
