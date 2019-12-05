9ac5684cca71f439acc9a10d7a5a1f8aeb510337
George Knee <georgeknee@googlemail.com>
2019-12-05 09:26:44 +0000

    Game reducer switches on state not action

--- packages/rps/src/redux/game/reducer.ts
+++ packages/rps/src/redux/game/reducer.ts
@@ -1,44 +1,24 @@
 import {
-  WeaponChosen,
   weaponChosen,
   weaponAndSaltChosen,
   resultPlayAgain,
   LocalState,
   lobby,
-  waitForRestart,
   chooseWeapon,
   resigned,
   gameChosen,
-  waitingRoom,
-  opponentJoined,
   gameOver,
   GameState,
   creatingOpenGame,
   needAddress,
   insufficientFunds,
+  waitForRestart,
+  waitingRoom,
+  opponentJoined,
 } from './state';
 import { Reducer, combineReducers } from 'redux';
-import {
-  GameAction,
-  JoinOpenGame,
-  ChooseWeapon,
-  ChooseSalt,
-  ResultArrived,
-  PlayAgain,
-  Resign,
-  UpdateChannelState,
-  CreateGame,
-  GameJoined,
-  StartRound,
-  GameOver,
-  UpdateProfile,
-  CancelGame,
-  NewOpenGame,
-  GotAddressFromWallet,
-  ExitToLobby,
-} from './actions';
+import { GameAction, UpdateChannelState } from './actions';
 import { ChannelState } from '../../core';
-import { unreachable } from '../../utils/unreachable';
 
 const emptyLocalState: LocalState = { type: 'Empty' };
 
@@ -57,203 +37,92 @@ const localReducer: Reducer<LocalState> = (
   state: LocalState = emptyLocalState,
   action: GameAction
 ) => {
-  switch (action.type) {
-    case 'GotAddressFromWallet':
-      return handleGotAddressFromWallet(state, action);
-    case 'UpdateProfile':
-      return handleUpdateProfile(state, action);
-    case 'NewOpenGame':
-      return handleNewOpenGame(state, action);
-    case 'JoinOpenGame':
-      return handleJoinOpenGame(state, action);
-    case 'CreateGame':
-      return handleCreateGame(state, action);
-    case 'CancelGame':
-      return handleCancelGame(state, action);
-    case 'GameJoined':
-      return handleGameJoined(state, action);
-    case 'ChooseWeapon':
-      return handleChooseWeapon(state, action);
-    case 'ChooseSalt':
-      return handleChooseSalt(state, action);
-    case 'ResultArrived':
-      return handleResultArrived(state, action);
-    case 'PlayAgain':
-      return handlePlayAgain(state, action);
-    case 'StartRound':
-      return handleStartRound(state, action);
-    case 'Resign':
-      return handleResign(state, action);
-    case 'GameOver':
-      return handleGameOver(state, action);
-    case 'ExitToLobby':
-      return handleExitToLobby(state, action);
-    default:
-      return unreachable(action, state);
+  let newState = state;
+  if (
+    action.type === 'Resign' &&
+    state.type !== 'Empty' &&
+    state.type !== 'NeedAddress' &&
+    state.type !== 'Lobby' &&
+    state.type !== 'WaitingRoom' &&
+    state.type !== 'CreatingOpenGame'
+  ) {
+    newState = resigned(state, action.iResigned);
   }
+  switch (state.type) {
+    case 'Empty':
+      if (action.type === 'UpdateProfile') {
+        newState = needAddress({ ...state, ...action });
+      }
+      break;
+    case 'NeedAddress':
+      if (action.type === 'GotAddressFromWallet') {
+        newState = lobby({
+          ...state,
+          ...action,
+        });
+      }
+      break;
+    case 'Lobby':
+      if (action.type === 'NewOpenGame') {
+        newState = creatingOpenGame(state);
+      }
+      if (action.type === 'JoinOpenGame') {
+        const { opponentName, opponentAddress, roundBuyIn } = action;
+        const { name, address } = state;
+        newState = gameChosen({ name, address, opponentName, roundBuyIn }, opponentAddress);
+      }
+      break;
+    case 'CreatingOpenGame':
+      if (action.type === 'CreateGame') {
+        newState = waitingRoom({ ...state, ...action });
+      }
+      break;
+    case 'ResultPlayAgain':
+      if (action.type === 'PlayAgain') {
+        newState = waitForRestart(state, state.theirWeapon, state.result);
+      }
+      break;
+    case 'WaitingRoom':
+      if (action.type === 'GameJoined') {
+        newState = opponentJoined({ ...state, ...action });
+      }
+      break;
+    case 'OpponentJoined':
+    case 'GameChosen':
+    case 'WaitForRestart':
+      if (action.type === 'StartRound') {
+        newState = chooseWeapon(state);
+      }
+      break;
+    case 'ChooseWeapon':
+      if (action.type === 'ChooseWeapon') {
+        newState = weaponChosen(state, action.weapon);
+      }
+      break;
+    case 'WeaponChosen':
+    case 'WeaponAndSaltChosen':
+      if (state.player === 'A' && action.type === 'ChooseSalt') {
+        newState = weaponAndSaltChosen({ ...state, player: 'A' }, action.salt);
+      }
+      if (action.type === 'ResultArrived') {
+        if (action.fundingSituation === 'Ok') {
+          newState = resultPlayAgain(state, action.theirWeapon, action.result);
+        } else {
+          newState = insufficientFunds(state, action.theirWeapon, action.result);
+        }
+      }
+      break;
+    case 'Resigned':
+    case 'InsufficientFunds':
+      if (action.type === 'GameOver') {
+        newState = gameOver(state);
+      }
+      break;
+  }
+  return newState;
 };
 
 export const gameReducer: Reducer<GameState> = combineReducers({
   localState: localReducer,
   channelState: channelReducer,
 });
-
-const handleUpdateProfile = (state: LocalState, action: UpdateProfile): LocalState => {
-  if (state.type !== 'Empty') {
-    return state;
-  }
-  return needAddress({
-    ...state,
-    ...action,
-  });
-};
-
-const handleGotAddressFromWallet = (
-  state: LocalState,
-  action: GotAddressFromWallet
-): LocalState => {
-  if (state.type !== 'NeedAddress') {
-    return state;
-  }
-  return lobby({
-    ...state,
-    ...action,
-  });
-};
-
-const handleNewOpenGame = (state: LocalState, action: NewOpenGame): LocalState => {
-  if (state.type !== 'Lobby') {
-    return state;
-  }
-
-  return creatingOpenGame(state);
-};
-
-const handleJoinOpenGame = (state: LocalState, action: JoinOpenGame): LocalState => {
-  if (state.type === 'Empty' || state.type === 'NeedAddress') {
-    return state;
-  }
-
-  const { opponentName, opponentAddress, roundBuyIn } = action;
-  const { name, address } = state;
-
-  return gameChosen({ name, address, opponentName, roundBuyIn }, opponentAddress);
-};
-
-const handleGameJoined = (state: LocalState, action: GameJoined): LocalState => {
-  if (state.type !== 'WaitingRoom') {
-    return state;
-  }
-
-  const { opponentName, opponentAddress } = action;
-  const { name, address, roundBuyIn } = state;
-
-  return opponentJoined({ name, address, opponentName, roundBuyIn, opponentAddress });
-};
-
-const handleCreateGame = (state: LocalState, action: CreateGame): LocalState => {
-  if (state.type === 'Empty' || state.type === 'NeedAddress') {
-    return state;
-  }
-
-  const { roundBuyIn } = action;
-  const { name, address } = state;
-
-  return waitingRoom({ name, address, roundBuyIn });
-};
-
-const handleCancelGame = (state: LocalState, action: CancelGame): LocalState => {
-  if (state.type !== 'WaitingRoom') {
-    return state;
-  }
-  return lobby(state);
-};
-
-const handleChooseWeapon = (state: LocalState, action: ChooseWeapon): LocalState => {
-  if (state.type !== 'ChooseWeapon') {
-    return state;
-  }
-
-  const { weapon } = action;
-  return weaponChosen(state, weapon);
-};
-
-const handleChooseSalt = (state: LocalState, action: ChooseSalt): LocalState => {
-  if (state.type !== 'WeaponChosen' || state.player !== 'A') {
-    return state;
-  }
-  // not sure why typsecript can't see that player === 'A' here ...
-  const oldLocalState = state as WeaponChosen & { player: 'A' };
-
-  const { salt } = action;
-  return weaponAndSaltChosen(oldLocalState, salt);
-};
-
-const handleResultArrived = (state: LocalState, action: ResultArrived): LocalState => {
-  if (state.type !== 'WeaponChosen' && state.type !== 'WeaponAndSaltChosen') {
-    return state;
-  }
-  const { theirWeapon, result, fundingSituation } = action;
-  switch (fundingSituation) {
-    case 'Ok':
-      return resultPlayAgain(state, theirWeapon, result);
-    case 'MyFundsTooLow':
-    case 'OpponentsFundsTooLow':
-      return insufficientFunds(state, theirWeapon, result);
-    default:
-      return state;
-  }
-};
-
-const handlePlayAgain = (state: LocalState, action: PlayAgain): LocalState => {
-  if (state.type !== 'ResultPlayAgain') {
-    return state;
-  }
-  return waitForRestart(state, state.theirWeapon, state.result);
-};
-
-const handleStartRound = (state: LocalState, action: StartRound): LocalState => {
-  if (
-    state.type !== 'WaitForRestart' &&
-    state.type !== 'GameChosen' &&
-    state.type !== 'OpponentJoined'
-  ) {
-    return state;
-  }
-
-  return chooseWeapon(state);
-};
-
-const handleResign = (state: LocalState, action: Resign): LocalState => {
-  if (
-    state.type === 'Empty' ||
-    state.type === 'NeedAddress' ||
-    state.type === 'Lobby' ||
-    state.type === 'WaitingRoom' ||
-    state.type === 'CreatingOpenGame'
-  ) {
-    return state;
-  }
-
-  return resigned(state, action.iResigned);
-};
-
-const handleGameOver = (state: LocalState, action: GameOver): LocalState => {
-  if (
-    state.type === 'Empty' ||
-    state.type === 'NeedAddress' ||
-    state.type === 'Lobby' ||
-    state.type === 'WaitingRoom' ||
-    state.type === 'CreatingOpenGame'
-  ) {
-    return state;
-  }
-  return gameOver(state);
-};
-
-const handleExitToLobby = (state: LocalState, action: ExitToLobby): LocalState => {
-  if (state.type !== 'GameOver') {
-    return state;
-  }
-  return lobby(state);
-};
