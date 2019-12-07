65bcd0cf6f4be8269cb59c94ed751188b8ce0a36
Alon Bukai <alonzorz@gmail.com>
2019-12-07 20:18:33 +0200

    chore: Prettier write Devtools

--- packages/devtools/.prettierrc.js
+++ packages/devtools/.prettierrc.js
@@ -1,7 +1,7 @@
 // NOTE: This package has inconsistent use of trailing commas and bracket spacing
 
 module.exports = {
-  ...require("../../.prettierrc.js"),
+  ...require('../../.prettierrc.js'),
   printWidth: 100,
   singleQuote: true,
   bracketSpacing: false
