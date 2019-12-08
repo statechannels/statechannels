ba0cfd6c0c00a42e4d6715e4a4db78011f597dfe
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-12-08 11:38:12 -0700

    Start getting two wallets to communicate

---
+++ packages/wallet-specs/src/runtime.ts
@@ -0,0 +1,88 @@
+import { AnyEventObject, interpret, Interpreter } from 'xstate';
+import { getChannelID, pretty } from '.';
+import { messageService } from './messaging';
+import { Wallet } from './protocols';
+import { AddressableMessage } from './wire-protocol';
+
+import { CreateChannelEvent } from './protocols/wallet/protocol';
+import { Store } from './store';
+
+const store = name => {
+  const privateKeys = { [name]: name };
+  const _store = new Store({ privateKeys });
+  messageService.on('message', (m: AddressableMessage) => {
+    if (m.to === name) {
+      switch (m.type) {
+        case 'SendStates':
+          _store.receiveStates(m.signedStates);
+      }
+    }
+  });
+
+  return _store;
+};
+
+const first = 'first';
+const second = 'second';
+const stores = {
+  first: store(first),
+  second: store(second),
+};
+
+const logEvents = name => event =>
+  console.log(`${name} received ${event.type}`);
+
+const wallet = name => {
+  return interpret(Wallet.machine(stores[name]))
+    .onEvent(logEvents(name))
+    .start();
+};
+
+const wallets: Record<string, Interpreter<Wallet.Init, any, AnyEventObject>> = {
+  first: wallet(first),
+  second: wallet(second),
+};
+
+// This is sort of the "dispatcher"
+messageService.on('message', ({ to, ...event }: AddressableMessage) => {
+  switch (event.type) {
+    case 'SendStates': {
+      stores[to].receiveStates(event.signedStates);
+      const channelId = getChannelID(event.signedStates[0].state.channel);
+
+      wallets[to].send({
+        type: 'CHANNEL_UPDATED',
+        channelId,
+      });
+      break;
+    }
+    case 'OPEN_CHANNEL': {
+      wallets[to].send(event);
+      break;
+    }
+  }
+});
+
+const createChannel: CreateChannelEvent = {
+  type: 'CREATE_CHANNEL',
+  participants: [
+    {
+      participantId: first,
+      signingAddress: first,
+      destination: first,
+    },
+    {
+      participantId: second,
+      signingAddress: second,
+      destination: second,
+    },
+  ],
+  allocations: [
+    { destination: first, amount: '3' },
+    { destination: second, amount: '1' },
+  ],
+  appDefinition: '0x',
+  appData: '0x',
+};
+
+wallets[first].send(createChannel);
