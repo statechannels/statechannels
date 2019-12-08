4b109ac753e6b749a639568f3cb35edbbe2bd737
cf19drofxots <nima@l4v.io>
2019-12-07 18:05:48 -0800

    chore: catch NODE_ENV env var not being set to test for test runs

--- packages/hub/jest/test-setup.ts
+++ packages/hub/jest/test-setup.ts
@@ -3,6 +3,15 @@ import {GanacheServer} from '@statechannels/devtools';
 import {deploy} from '../deployment/deploy';
 
 export default async function setup() {
+  if (!process.env.NODE_ENV) {
+    throw Error(`The NODE_ENV env var must be set to "test"`);
+  }
+  if (process.env.NODE_ENV != 'test') {
+    throw Error(
+      `Run "yarn db:testSetup" and rerun the attempted script prefixed with "NODE_ENV=test"`
+    );
+  }
+
   const ganacheServer = new GanacheServer(
     Number(process.env.GANACHE_PORT),
     Number(process.env.CHAIN_NETWORK_ID)
