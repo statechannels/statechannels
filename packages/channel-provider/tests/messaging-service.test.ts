a54b8c7eec9bbae2d8e10507837106fff95bc124
Liam Horne <liam@lihorne.com>
2019-12-09 15:45:45 -0500

    Significantly reduce .eslintrc.js size

--- packages/channel-provider/tests/messaging-service.test.ts
+++ packages/channel-provider/tests/messaging-service.test.ts
@@ -32,35 +32,39 @@ describe('MessagingService', () => {
     target = await uiService.getTarget();
   });
 
-  it('should send a message', done => {
-    target.addEventListener('message', (event: MessageEvent) => {
-      const receivedMessage = event.data as JsonRpcRequest;
-      expect(receivedMessage).toEqual(request);
-      done();
-    });
+  it('should send a message', () => {
+    return new Promise(done => {
+      target.addEventListener('message', (event: MessageEvent) => {
+        const receivedMessage = event.data as JsonRpcRequest;
+        expect(receivedMessage).toEqual(request);
+        done();
+      });
 
-    messagingService.send(target, request, '*');
+      messagingService.send(target, request, '*');
+    });
   });
 
-  it('should retry sending a message', done => {
-    const originalMessageHandler = target.onmessage;
-    target.onmessage = () => ({});
+  it('should retry sending a message', () => {
+    return new Promise(done => {
+      const originalMessageHandler = target.onmessage;
+      target.onmessage = () => ({});
 
-    const sendSpy = jest.spyOn(messagingService, 'send');
+      const sendSpy = jest.spyOn(messagingService, 'send');
 
-    jest.useFakeTimers();
-    messagingService.send(target, request, '*');
-    jest.advanceTimersByTime(150);
+      jest.useFakeTimers();
+      messagingService.send(target, request, '*');
+      jest.advanceTimersByTime(150);
 
-    expect(sendSpy).toHaveBeenCalledTimes(4);
-    target.onmessage = originalMessageHandler;
+      expect(sendSpy).toHaveBeenCalledTimes(4);
+      target.onmessage = originalMessageHandler;
 
-    jest.useRealTimers();
+      jest.useRealTimers();
 
-    target.addEventListener('message', (event: MessageEvent) => {
-      const receivedMessage = event.data as JsonRpcRequest;
-      expect(receivedMessage).toEqual(request);
-      done();
+      target.addEventListener('message', (event: MessageEvent) => {
+        const receivedMessage = event.data as JsonRpcRequest;
+        expect(receivedMessage).toEqual(request);
+        done();
+      });
     });
   });
 
