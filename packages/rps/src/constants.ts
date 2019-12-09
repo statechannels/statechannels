b585ba15cb24805a2655314290ae5eb0d9cf96e0
Liam Horne <liam@lihorne.com>
2019-12-09 11:59:32 -0500

    Use RPC env var in rps constants

--- packages/rps/src/constants.ts
+++ packages/rps/src/constants.ts
@@ -2,4 +2,4 @@ export const WALLET_URL = process.env.WALLET_URL || 'https://wallet.magmo.com';
 export const WALLET_IFRAME_ID = 'wallet-0000';
 
 // todo: load the correct address
-export const RPS_ADDRESS = '0x5409ED021D9299bf6814279A6A1411A7e866A631';
+export const RPS_ADDRESS = process.env.RPS_CONTRACT_ADDRESS;
