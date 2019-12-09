d3bf9933e135c6fe74353699511dbb24ced4f0bf
Liam Horne <liam@lihorne.com>
2019-12-09 09:38:48 -0500

    Re-use main eslint config

--- packages/client-api-schema/.eslintrc.js
+++ packages/client-api-schema/.eslintrc.js
@@ -1,17 +1,9 @@
+const baseConfig = require('../../.eslintrc.js');
+
 module.exports = {
+  ...baseConfig,
   env: {
     node: true,
     es6: true
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
-  rules: {
-    'prettier/prettier': 'warn',
-    '@typescript-eslint/ban-ts-ignore': 'off'
   }
 };
