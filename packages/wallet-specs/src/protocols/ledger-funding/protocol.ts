d30f71eaa59df9f8fdab370118702ece0fbd86eb
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-12-09 14:24:57 -0700

    WIP: Add ledgerFunding machine

--- packages/wallet-specs/src/protocols/ledger-funding/protocol.ts
+++ packages/wallet-specs/src/protocols/ledger-funding/protocol.ts
@@ -1,17 +1,13 @@
-import { add, store } from '../..';
+import { Machine } from 'xstate';
+import { add, MachineFactory, Store, success } from '../..';
 import { Init as CreateNullChannelArgs } from '../create-null-channel/protocol';
 import { Init as DirectFundingArgs } from '../direct-funding/protocol';
 import { Init as LedgerUpdateArgs } from '../ledger-update/protocol';
 
 const PROTOCOL = 'ledger-funding';
-const success = { type: 'final' };
 
 interface Init {
   targetChannelID: string;
-}
-
-function messageID(context: Init): string {
-  return `funding-${context.targetChannelID}`;
 }
 
 /*
@@ -35,59 +31,24 @@ const lookForExistingChannel = {
   },
 };
 
-const createNullChannelArgs: (ctx: Init) => CreateNullChannelArgs = ({
-  targetChannelID,
-}: Init) => {
-  const { channel: targetChannel, outcome } = store.getLatestState(
-    targetChannelID
-  );
-  const channel = {
-    ...targetChannel,
-    channelNonce: store.getNextNonce(targetChannel.participants),
-  };
-  return { channel, outcome };
-};
-
 const createNewChannel = {
   invoke: {
     src: 'createNullChannel',
-    data: 'createNullChannelArgs',
   },
   onDone: 'fundLedger',
 };
 
 type LedgerExists = Init & { ledgerChannelID: string };
-function directFundingArgs(ctx: LedgerExists): DirectFundingArgs {
-  return {
-    channelID: ctx.ledgerChannelID,
-    minimalOutcome: store.getLatestState(ctx.targetChannelID).outcome,
-  };
-}
 const fundLedger = {
   invoke: {
     src: 'directFunding',
-    data: 'directFundingArgs',
     onDone: 'fundTarget',
   },
 };
 
-function ledgerUpdateArgs({
-  ledgerChannelID,
-  targetChannelID,
-}: LedgerExists): LedgerUpdateArgs {
-  const amount = store
-    .getLatestSupportedAllocation(targetChannelID)
-    .map(o => o.amount)
-    .reduce(add);
-  return {
-    channelID: ledgerChannelID,
-    targetOutcome: [{ destination: targetChannelID, amount }],
-  };
-}
 const fundTarget = {
   invoke: {
     src: 'ledgerUpdate',
-    data: 'ledgerUpdateArgs',
     onDone: 'success',
   },
 };
@@ -104,7 +65,57 @@ export const config = {
   },
 };
 
-const guards = {
-  suitableChannelExists: x => true,
+export type Services = {
+  findLedgerChannelId: any;
+  createNullChannel: any;
+  directFunding: any;
+  ledgerUpdate: any;
 };
-export const mockOptions = { guards };
+
+export const machine: MachineFactory<Init, any> = (
+  store: Store,
+  context: Init
+) => {
+  const createNullChannelArgs: (ctx: Init) => CreateNullChannelArgs = ({
+    targetChannelID,
+  }: Init) => {
+    const { channel: targetChannel, outcome } = store.getLatestState(
+      targetChannelID
+    );
+    const channel = {
+      ...targetChannel,
+      channelNonce: store.getNextNonce(targetChannel.participants),
+    };
+    return { channel, outcome };
+  };
+  function directFundingArgs(ctx: LedgerExists): DirectFundingArgs {
+    return {
+      channelID: ctx.ledgerChannelID,
+      minimalOutcome: store.getLatestState(ctx.targetChannelID).outcome,
+    };
+  }
+  function ledgerUpdateArgs({
+    ledgerChannelID,
+    targetChannelID,
+  }: LedgerExists): LedgerUpdateArgs {
+    const amount = store
+      .getLatestSupportedAllocation(targetChannelID)
+      .map(o => o.amount)
+      .reduce(add);
+    return {
+      channelID: ledgerChannelID,
+      targetOutcome: [{ destination: targetChannelID, amount }],
+    };
+  }
+
+  const services: Services = {
+    findLedgerChannelId: () => true,
+    ledgerUpdate: () => true,
+    directFunding: () => true,
+    createNullChannel: () => true,
+  };
+
+  const options = { services };
+
+  return Machine(config).withConfig(options, context);
+};
