3450fa50d001d85f2ab2e97ef0548683d818866a
Mike Kerzhner <mikerzhner@gmail.com>
2019-12-09 17:03:53 -0800

    Hub: respond to Channel.Open messages

--- packages/hub/src/hub/handlers/handle-ongoing-process-action.ts
+++ packages/hub/src/hub/handlers/handle-ongoing-process-action.ts
@@ -5,18 +5,23 @@ import {
   SignedStatesReceived,
   StrategyProposed,
   ProtocolLocator,
-  StrategyApproved
+  StrategyApproved,
+  ChannelOpen,
+  ChannelJoined
 } from '../../communication';
 import {HUB_ADDRESS, unreachable} from '../../constants';
 import {errors} from '../../wallet';
 import {getCurrentState} from '../../wallet/db/queries/getCurrentState';
 import {getProcess} from '../../wallet/db/queries/walletProcess';
 import {updateLedgerChannel} from '../../wallet/services';
+import {ChannelParticipant} from '@statechannels/wallet/lib/src/redux/channel-store';
 
 export async function handleOngoingProcessAction(
-  action: StrategyProposed | SignedStatesReceived
+  action: ChannelOpen | StrategyProposed | SignedStatesReceived
 ): Promise<RelayActionWithMessage[]> {
   switch (action.type) {
+    case 'Channel.Open':
+      return handleChannelOpen(action);
     case 'WALLET.COMMON.SIGNED_STATES_RECEIVED':
       return handleSignedStatesReceived(action);
     case 'WALLET.FUNDING_STRATEGY_NEGOTIATION.STRATEGY_PROPOSED':
@@ -37,58 +42,86 @@ async function handleStrategyProposed(action: StrategyProposed) {
   return [
     relayActionWithMessage({
       toParticipantId: theirAddress,
-      fromParticipantId: 'hub',
+      fromParticipantId: HUB_ADDRESS,
       actionToRelay: strategyApproved({processId, strategy})
     })
   ];
 }
 
-async function handleSignedStatesReceived(action: SignedStatesReceived) {
-  {
-    const {processId} = action;
-    const walletProcess = await getProcess(processId);
-    if (!walletProcess) {
-      throw errors.processMissing(processId);
-    }
+async function handleChannelOpen(action: ChannelOpen) {
+  const lastState = action.signedState;
 
-    const stateRound = action.signedStates;
+  const participants = lastState.state.channel.participants;
+  const ourIndex = participants.indexOf(HUB_ADDRESS);
 
-    // For the time being, just assume a two-party channel and proceed as normal.
-    const {state: lastState} = stateRound.slice(-1)[0];
+  const {state, signature} = await updateLedgerChannel([lastState]);
 
-    const participants = lastState.channel.participants;
-    const ourIndex = participants.indexOf(HUB_ADDRESS);
-
-    const currentState = await getCurrentState(lastState);
-    const {state, signature} = await updateLedgerChannel(
-      stateRound,
-      currentState && currentState.asStateObject()
-    );
-
-    return participants
-      .filter((_, idx) => idx !== ourIndex)
-      .map(p =>
-        relayActionWithMessage({
-          toParticipantId: p,
-          fromParticipantId: 'hub',
-          actionToRelay: signedStatesReceived({
-            processId,
-            signedStates: [
-              ...action.signedStates,
-              {
-                state,
-                signature
-              }
-            ],
-            protocolLocator: action.protocolLocator
-          })
+  return participants
+    .filter((_, idx) => idx !== ourIndex)
+    .map(p =>
+      relayActionWithMessage({
+        toParticipantId: p,
+        fromParticipantId: HUB_ADDRESS,
+        actionToRelay: channelJoined({
+          signedState: {
+            state,
+            signature
+          },
+          participants: action.participants
         })
-      );
+      })
+    );
+}
+
+async function handleSignedStatesReceived(action: SignedStatesReceived) {
+  const {processId} = action;
+  const walletProcess = await getProcess(processId);
+  if (!walletProcess) {
+    throw errors.processMissing(processId);
   }
+
+  const stateRound = action.signedStates;
+
+  // For the time being, just assume a two-party channel and proceed as normal.
+  const {state: lastState} = stateRound.slice(-1)[0];
+
+  const participants = lastState.channel.participants;
+  const ourIndex = participants.indexOf(HUB_ADDRESS);
+
+  const currentState = await getCurrentState(lastState);
+  const {state, signature} = await updateLedgerChannel(
+    stateRound,
+    currentState && currentState.asStateObject()
+  );
+
+  return participants
+    .filter((_, idx) => idx !== ourIndex)
+    .map(p =>
+      relayActionWithMessage({
+        toParticipantId: p,
+        fromParticipantId: HUB_ADDRESS,
+        actionToRelay: signedStatesReceived({
+          processId,
+          signedStates: [
+            ...action.signedStates,
+            {
+              state,
+              signature
+            }
+          ],
+          protocolLocator: action.protocolLocator
+        })
+      })
+    );
 }
 
 // TODO: The following three helper functions are direct copy and paste from the wallet
 // https://github.com/statechannels/monorepo/blob/1ce06fdc98456e268a341c6aec19ef8b0a86a510/packages/wallet/src/communication/actions.ts
+
+export const channelJoined = (p: {
+  signedState: SignedState;
+  participants: ChannelParticipant[];
+}): ChannelJoined => ({...p, type: 'Channel.Joined'});
 
 export const signedStatesReceived = (p: {
   protocolLocator: ProtocolLocator;
