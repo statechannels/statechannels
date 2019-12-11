d2578fadedd1be9a93bd3c7f2130db5cc47f59e3
Alon Bukai <alonzorz@gmail.com>
2019-12-11 12:46:23 +0200

    chore: Remove duplicate Prettier options Defined in Nitro Protocol

--- packages/nitro-protocol/.prettierrc.js
+++ packages/nitro-protocol/.prettierrc.js
@@ -1,12 +1,4 @@
 module.exports = {
   ...require('../../.prettierrc.js'),
   trailingComma: 'es5',
-  overrides: [
-    {
-      files: '*.sol',
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
 };
