6c30c4024cd90f762abc760975fa7309e0632a45
Alon Bukai <alonzorz@gmail.com>
2019-12-11 13:11:46 +0200

    [monorepo] Cleanup Prettier and ESLint config (#684)
    * chore: Cleanup Prettier and ESLint config
    
    * chore: Remove duplicate Prettier options
    Defined in Nitro Protocol

--- packages/nitro-protocol/.prettierrc.js
+++ packages/nitro-protocol/.prettierrc.js
@@ -1,13 +1,4 @@
 module.exports = {
   ...require('../../.prettierrc.js'),
   trailingComma: 'es5',
-  printWidth: 100,
-  overrides: [
-    {
-      files: '*.sol',
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
 };
