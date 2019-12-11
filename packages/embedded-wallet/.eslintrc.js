7432ac4a09df72a9006416b3e5d29792dfe7f69c
Alon Bukai <alonzorz@gmail.com>
2019-12-11 16:03:54 +0200

    chore: Remove TSLint usages

--- packages/embedded-wallet/.eslintrc.js
+++ packages/embedded-wallet/.eslintrc.js
@@ -172,7 +172,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint', '@typescript-eslint/tslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'eslint:recommended',
