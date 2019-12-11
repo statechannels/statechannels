8028665ed0fecbc5080d9a33d0571664a9ac9634
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-12-11 09:11:03 -0700

    Add mock messages

--- packages/wallet-specs/src/mock-messages.ts
+++ packages/wallet-specs/src/mock-messages.ts
@@ -1,7 +1,8 @@
+import { CreateChannelEvent } from './protocols/wallet/protocol';
 import { AddressableMessage } from './wire-protocol';
 
-const messages: AddressableMessage[] = [];
-messages.push({
+const messagesToSecond: AddressableMessage[] = [];
+messagesToSecond.push({
   type: 'OPEN_CHANNEL',
   signedState: {
     state: {
@@ -24,7 +25,7 @@ messages.push({
   },
   to: 'second',
 });
-messages.push({
+messagesToSecond.push({
   type: 'SendStates',
   signedStates: [
     {
@@ -49,11 +50,74 @@ messages.push({
   ],
   to: 'second',
 });
-messages.push({
+messagesToSecond.push({
   to: 'second',
   type: 'FUNDING_STRATEGY_PROPOSED',
   targetChannelId: 'first+second',
   choice: 'Indirect',
 });
 
-export { messages };
+const messagesToFirst: AddressableMessage[] = [];
+messagesToFirst.push({
+  type: 'SendStates',
+  signedStates: [
+    {
+      state: {
+        appData: '0x',
+        appDefinition: '0x',
+        isFinal: false,
+        turnNum: 1,
+        outcome: [
+          {
+            destination: 'first',
+            amount: '3',
+          },
+          {
+            destination: 'second',
+            amount: '1',
+          },
+        ],
+        channel: {
+          participants: ['first', 'second'],
+          channelNonce: '1',
+          chainId: 'mainnet?',
+        },
+        challengeDuration: 'TODO',
+      },
+      signatures: ['second'],
+    },
+  ],
+  to: 'first',
+});
+messagesToFirst.push({
+  type: 'FUNDING_STRATEGY_PROPOSED',
+  choice: 'Indirect',
+  targetChannelId: 'first+second+1',
+  to: 'first',
+});
+
+const first = 'first';
+const second = 'second';
+export const createChannel: CreateChannelEvent = {
+  type: 'CREATE_CHANNEL',
+  participants: [
+    {
+      participantId: first,
+      signingAddress: first,
+      destination: first,
+    },
+    {
+      participantId: second,
+      signingAddress: second,
+      destination: second,
+    },
+  ],
+  allocations: [
+    { destination: first, amount: '3' },
+    { destination: second, amount: '1' },
+  ],
+  appDefinition: '0x',
+  appData: '0x',
+};
+
+export { messagesToSecond, messagesToFirst };
