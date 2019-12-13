affb7330f19e4f763f643c03ecb2e3932703ca89
Liam Horne <liam@lihorne.com>
2019-12-13 18:01:39 -0500

    Wrap rps test into an integration test

--- packages/wallet/puppeteer/scripts/rps.ts
+++ packages/wallet/puppeteer/scripts/rps.ts
@@ -1,21 +1,10 @@
 import {setUpBrowser, loadRPSApp} from "../helpers";
+import {Page} from "puppeteer";
 
 // Load environment variables from .env
 require("../../config/env");
 
-(async () => {
-  // Unfortunately we need to use two separate windows
-  // as otherwise the javascript gets paused on the non-selected tab
-  // see https://github.com/puppeteer/puppeteer/issues/3339
-  const browserA = await setUpBrowser(false);
-  const browserB = await setUpBrowser(false);
-
-  const rpsTabA = await browserA.newPage();
-  const rpsTabB = await browserB.newPage();
-
-  await loadRPSApp(rpsTabA, 0);
-  await loadRPSApp(rpsTabB, 1);
-
+export async function clickThroughRPSUI(rpsTabA: Page, rpsTabB: Page) {
   await (await rpsTabA.waitForXPath('//button[contains(., "Start Playing!")]')).click();
   await (await rpsTabB.waitForXPath('//button[contains(., "Start Playing!")]')).click();
 
@@ -44,6 +33,24 @@ require("../../config/env");
   await (await rpsTabA.waitFor('img[src*="rock"]')).click();
 
   await (await rpsTabB.waitFor('img[src*="paper"]')).click();
+}
 
-  process.exit();
-})();
+if (require.main === module) {
+  (async () => {
+    // Unfortunately we need to use two separate windows
+    // as otherwise the javascript gets paused on the non-selected tab
+    // see https://github.com/puppeteer/puppeteer/issues/3339
+    const browserA = await setUpBrowser(false);
+    const browserB = await setUpBrowser(false);
+
+    const rpsTabA = await browserA.newPage();
+    const rpsTabB = await browserB.newPage();
+
+    await loadRPSApp(rpsTabA, 0);
+    await loadRPSApp(rpsTabB, 1);
+
+    await clickThroughRPSUI(rpsTabA, rpsTabB);
+
+    // process.exit();
+  })();
+}
