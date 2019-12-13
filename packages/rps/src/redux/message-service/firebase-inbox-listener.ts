30619d6a3d7ee4a5eee4fbd9f25683747fda4599
Alex Gap <alex.gap@gmail.com>
2019-12-13 11:19:13 -0800

    Use a buffer for inbox listener

--- packages/rps/src/redux/message-service/firebase-inbox-listener.ts
+++ packages/rps/src/redux/message-service/firebase-inbox-listener.ts
@@ -3,13 +3,17 @@ import {take, call} from 'redux-saga/effects';
 import {reduxSagaFirebase} from '../../gateways/firebase';
 import {RPSChannelClient} from '../../utils/rps-channel-client';
 import {Message} from '@statechannels/channel-client';
+import {buffers} from 'redux-saga';
 
 export function* firebaseInboxListener(client: RPSChannelClient) {
   const address: string = yield call([client, 'getAddress']);
+  // TODO: Redux saga firebase accepts a buffer but the type definition hasn't been updated in the latest version (0.15.0)
+  // so we just cast as any for now
   const channel = yield call(
-    reduxSagaFirebase.database.channel,
+    reduxSagaFirebase.database.channel as any,
     `/messages/${address.toLowerCase()}`,
-    'child_added'
+    'child_added',
+    buffers.fixed(100)
   );
   while (true) {
     const firebaseResponse = yield take(channel);
