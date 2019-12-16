ed063cb7cea8bfb3d6a341c5e0acfac2634823d4
Liam Horne <liam@lihorne.com>
2019-12-16 13:10:37 -0500

    Update readme and prod site URL

--- packages/rps/src/constants.ts
+++ packages/rps/src/constants.ts
@@ -1,4 +1,4 @@
-export const WALLET_URL = process.env.WALLET_URL || 'https://wallet.magmo.com';
+export const WALLET_URL = process.env.WALLET_URL || 'https://wallet.statechannels.org';
 
 // todo: load the correct address
 export const RPS_ADDRESS = process.env.RPS_CONTRACT_ADDRESS;
