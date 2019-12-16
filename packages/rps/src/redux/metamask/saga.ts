389dc69be72becf0096c04b7cad00db00c0b4c3a
George Knee <georgeknee@googlemail.com>
2019-12-16 12:35:00 +0000

    Remove reliance on injected web3
    and refactor metamask saga a bit

--- packages/rps/src/redux/metamask/saga.ts
+++ packages/rps/src/redux/metamask/saga.ts
@@ -3,15 +3,6 @@ import * as metamaskActions from './actions';
 import {MetamaskErrorType} from './actions';
 
 export default function* checkMetamask() {
-  if (typeof window.web3 !== 'object' || window.web3 === null) {
-    yield put(
-      metamaskActions.metamaskErrorOccurred({
-        errorType: MetamaskErrorType.NoWeb3,
-      })
-    );
-    return false;
-  }
-
   const networks = {
     development: {
       networkId: '*', // match any network
@@ -30,16 +21,29 @@ export default function* checkMetamask() {
     },
   };
 
+  if (typeof window.ethereum !== 'undefined') {
+    try {
+      yield window.ethereum.enable();
+      yield put(metamaskActions.metamaskSuccess());
+      return true;
+    } catch (error) {
+      yield put(
+        metamaskActions.metamaskErrorOccurred({
+          errorType: MetamaskErrorType.MetamaskLocked,
+        })
+      );
+    }
+  } else {
+    delay(500);
+  }
+
   try {
     const targetNetworkName = process.env.TARGET_NETWORK;
-
+    const selectedNetworkId = parseInt(yield cps(window.ethereum.networkVersion), 10);
     // Find the network name that matches the currently selected network id
-    const selectedNetworkId = parseInt(yield cps(window.web3.version.getNetwork), 10);
-    // Find the network name that matches the currently selected network id
-    const selectedNetworkName =
-      Object.keys(networks).find(
-        networkName => networks[networkName].network_id === selectedNetworkId
-      ) || 'development';
+    const selectedNetworkName = Object.keys(networks).find(
+      networkName => networks[networkName].network_id === selectedNetworkId
+    );
 
     if (targetNetworkName !== selectedNetworkName) {
       yield put(
@@ -49,31 +53,6 @@ export default function* checkMetamask() {
         })
       );
       return false;
-    }
-
-    if (window.ethereum) {
-      try {
-        yield window.ethereum.enable();
-        yield put(metamaskActions.metamaskSuccess());
-        return true;
-      } catch (error) {
-        yield put(
-          metamaskActions.metamaskErrorOccurred({
-            errorType: MetamaskErrorType.MetamaskLocked,
-          })
-        );
-      }
-    } else {
-      let accountUnlocked = false;
-      while (!accountUnlocked) {
-        const accounts = yield cps(window.web3.eth.getAccounts);
-        accountUnlocked = accounts && accounts.length > 0;
-        if (!accountUnlocked) {
-          yield put(metamaskActions.metamaskSuccess());
-        } else {
-          delay(1000);
-        }
-      }
     }
   } catch (e) {
     yield put(metamaskActions.metamaskErrorOccurred({errorType: MetamaskErrorType.UnknownError}));
