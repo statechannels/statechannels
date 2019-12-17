536907074c8a61daac051d59a8bafc22ea7fabf1
George Knee <georgeknee@googlemail.com>
2019-12-17 15:00:17 +0000

    Organize test suite some more
    - add getAddress() test

--- packages/rps/src/utils/__tests__/rps-channel-client.test.ts
+++ packages/rps/src/utils/__tests__/rps-channel-client.test.ts
@@ -5,10 +5,12 @@ import {
   ChannelResult,
   PushMessageResult,
 } from '@statechannels/channel-client';
-import {encodeAppData} from '../../core';
+import {encodeAppData, ChannelState} from '../../core';
 import {bigNumberify} from 'ethers/utils';
 import {RPS_ADDRESS} from '../../constants';
 
+const MOCK_ADDRESS = '0xAddress';
+const MOCK_CHANNEL_ID = '0xChannelId';
 class MockChannelClient implements ChannelClientInterface {
   onMessageQueued = jest.fn(function(callback) {
     return () => {
@@ -31,7 +33,7 @@ class MockChannelClient implements ChannelClientInterface {
       allocations,
       appData,
       appDefinition,
-      channelId: '0x123',
+      channelId: MOCK_CHANNEL_ID,
       status: 'opening',
       turnNum: '0',
     };
@@ -57,38 +59,73 @@ class MockChannelClient implements ChannelClientInterface {
       /* */
     });
   }
-  getAddress() {
-    return new Promise<string>(() => {
-      /* */
-    });
-  }
+  getAddress = jest.fn(async function() {
+    return await MOCK_ADDRESS;
+  });
 }
 
-it('calls channelClient.createChannel with appropriately encoded date', async () => {
-  const mockChannelClient = new MockChannelClient();
-  const client = new RPSChannelClient(mockChannelClient);
+let mockChannelClient;
+let client;
 
-  await client.createChannel(aAddress, bAddress, aBal, bBal, appData.start);
+beforeAll(() => {
+  mockChannelClient = new MockChannelClient();
+  client = new RPSChannelClient(mockChannelClient);
+});
 
-  const participants = [
-    {participantId: aAddress, signingAddress: aAddress, destination: aAddress},
-    {participantId: bAddress, signingAddress: bAddress, destination: bAddress},
-  ];
-  const allocations = [
-    {
-      token: '0x0',
-      allocationItems: [
-        {destination: aAddress, amount: bigNumberify(aBal).toHexString()},
-        {destination: bAddress, amount: bigNumberify(bBal).toHexString()},
-      ],
-    },
-  ];
-  const appDefinition = RPS_ADDRESS;
+describe('when getAddress() is called', () => {
+  let result;
+  beforeAll(async () => {
+    result = await client.getAddress();
+  });
+  it('calls channelClient.getAddress()', async () => {
+    expect(mockChannelClient.getAddress).toHaveBeenCalled();
+  });
+  it('and returns the result', async () => {
+    expect(result).toEqual(MOCK_ADDRESS);
+  });
+});
 
-  expect(mockChannelClient.createChannel).toHaveBeenCalledWith(
-    participants,
-    allocations,
-    appDefinition,
-    encodeAppData(appData.start)
-  );
+describe('when createChannel() is called', () => {
+  let result;
+  beforeAll(async () => {
+    result = await client.createChannel(aAddress, bAddress, aBal, bBal, appData.start);
+  });
+  it('calls channelClient.createChannel() with appropriately encoded data', async () => {
+    const participants = [
+      {participantId: aAddress, signingAddress: aAddress, destination: aAddress},
+      {participantId: bAddress, signingAddress: bAddress, destination: bAddress},
+    ];
+    const allocations = [
+      {
+        token: '0x0',
+        allocationItems: [
+          {destination: aAddress, amount: bigNumberify(aBal).toHexString()},
+          {destination: bAddress, amount: bigNumberify(bBal).toHexString()},
+        ],
+      },
+    ];
+    const appDefinition = RPS_ADDRESS;
+
+    expect(mockChannelClient.createChannel).toHaveBeenCalledWith(
+      participants,
+      allocations,
+      appDefinition,
+      encodeAppData(appData.start)
+    );
+  });
+  it('decodes and returns the result', async () => {
+    const channelState: ChannelState = {
+      channelId: MOCK_CHANNEL_ID,
+      turnNum: '0',
+      status: 'opening',
+      aUserId: aAddress,
+      bUserId: bAddress,
+      aAddress: aAddress,
+      bAddress: bAddress,
+      aBal: aBal,
+      bBal: bBal,
+      appData: appData.start,
+    };
+    expect(result).toEqual(channelState);
+  });
 });
