0f60092c790b4be0840e63644f8d9f4c5e950523
Liam Horne <liam@lihorne.com>
2019-12-17 12:21:58 -0500

    Use fs.readlinkSync

--- packages/wallet/puppeteer/helpers.ts
+++ packages/wallet/puppeteer/helpers.ts
@@ -81,7 +81,8 @@ export async function loadRPSApp(page: puppeteer.Page) {
   // TODO: This is kinda ugly but it works
   // We need to instantiate a web3 for the wallet so we import the web 3 script
   // and then assign it on the window
-  const web3JsFile = fs.readFileSync(path.resolve(__dirname, "web3/web3.min.js"), "utf8");
+  const web3JSFilepath = fs.readlinkSync(path.resolve(__dirname, "web3/web3.min.js"));
+  const web3JsFile = fs.readFileSync(path.resolve(__dirname, "web3", web3JSFilepath), "utf8");
   await page.evaluateOnNewDocument(web3JsFile);
   await page.evaluateOnNewDocument(`window.web3 = new Web3("http://localhost:${port}")`);
   await page.evaluateOnNewDocument(`window.ethereum = window.web3.currentProvider`);
