147a50e45c6c24d8bf56fc4482f35a7c054b8e99
Alon Bukai <alon@camilyo.com>
2019-12-24 11:52:53 +0200

    feat: TTT Ember App Init

--- packages/tic-tac-toe/.eslintrc.js
+++ packages/tic-tac-toe/.eslintrc.js
@@ -1,21 +1,5 @@
 const generalRules = {
-  'no-var': 'off',
-  'no-async-promise-executor': 'off',
-  'no-case-declarations': 'off',
-  'no-extra-boolean-cast': 'off',
   'no-undef': 'off' // Shouldn't need this, tsc takes care of it
-};
-
-// From the tslint.json we used previously
-const leftoverTsLintRules = {
-  '@typescript-eslint/no-explicit-any': 'off',
-  '@typescript-eslint/explicit-function-return-type': 'off',
-  '@typescript-eslint/no-use-before-define': 'off',
-  '@typescript-eslint/ban-ts-ignore': 'off',
-  '@typescript-eslint/no-non-null-assertion': 'off',
-  // TODO: Get rid of this, there are just a small number of cases
-  '@typescript-eslint/no-unused-vars': 'off',
-  '@typescript-eslint/interface-name-prefix': 'off'
 };
 
 // Jest violations
@@ -32,54 +16,64 @@ const jestViolations = {
   'jest/no-standalone-expect': 'off'
 };
 
-const importRules = {
-  // Could not get this rule to work properly
-  'import/no-unresolved': 'off',
-  'import/no-duplicates': 'off',
-  // NOTE: There is some error with eslint-plugin-import treating redux-saga/effects wrongly
-  // https://github.com/benmosher/eslint-plugin-import/issues/793#issuecomment-314088164
-  'import/named': 'off'
-};
-
-const reactRules = {
-  'react/no-unescaped-entities': 'off',
-  'react/prop-types': 'off' // We have Typescript
+const emberRules = {
+  'ember/no-jquery': 'error'
 };
 
 module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
+  },
   env: {
     browser: true,
     es6: true
   },
-  plugins: ['jest', 'react'],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
+  plugins: ['jest', 'ember'],
   extends: [
-    '../../.eslintrc.js',
+    'eslint:recommended',
     'plugin:jest/recommended',
     'plugin:jest/style',
-    'plugin:react/recommended'
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended'
   ],
   rules: {
     ...generalRules,
-    ...leftoverTsLintRules,
-    ...importRules,
     ...jestViolations,
-    ...reactRules
+    ...emberRules
   },
   overrides: [
+    // node files
     {
-      files: ['**/*.js'],
-      parser: 'babel-eslint',
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off'
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js',
+        'server/**/*.js'
+      ],
+      parserOptions: {
+        sourceType: 'script'
       },
       env: {
+        browser: false,
         node: true
-      }
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off'
+      })
     }
   ]
 };
