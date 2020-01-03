be088b1bf8dadd180e66ae5a193c6f83ad11495c
cf19drofxots <nima@l4v.io>
2020-01-02 22:25:19 -0800

    chore: update channel ID calculation

--- packages/channel-client/src/utils.ts
+++ packages/channel-client/src/utils.ts
@@ -2,24 +2,14 @@ import {hashMessage} from 'ethers/utils';
 
 import {AllocationItem, Allocation, Participant} from './types';
 
-export function calculateChannelId(
-  participants: Participant[],
-  allocations: Allocation[],
-  appDefinition: string,
-  appData: string
-): string {
-  // This `message` construction revolves around `hashMessage` taking in a
-  // string but since the concat of the arguments are unique to each channel
-  // the channel ID should be different as a result.
+/*
+ This channel ID calculation revolves around `hashMessage` taking in a
+ string but since the concat of the arguments are unique to each channel
+ the channel ID should be different as a result.
+*/
+export function calculateChannelId(participants: Participant[], appDefinition: string): string {
   let message = '';
   participants.forEach(p => (message += p.participantId));
-  allocations.forEach((a: Allocation) => {
-    message += a.token;
-    a.allocationItems.forEach((aI: AllocationItem) => {
-      message += aI.amount + aI.destination;
-    });
-  });
-
-  message += appDefinition + appData;
+  message += appDefinition;
   return hashMessage(message);
 }
