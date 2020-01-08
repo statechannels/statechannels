6f4cab068827cadde17150579e6330d06909de5b
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-01-07 20:55:20 -0800

    Improve checkThat type checking on guards

--- packages/wallet-specs/src/store.ts
+++ packages/wallet-specs/src/store.ts
@@ -301,10 +301,12 @@ export function isAllocation(outcome: Outcome): outcome is Allocation {
 const throwError = (fn: (t1: any) => boolean, t) => {
   throw new Error(`not valid, ${fn.name} failed on ${t}`);
 };
-type TypeGuard<T> = (t1: any) => t1 is T;
-export function checkThat<T>(t, isTypeT: TypeGuard<T>): T {
+type TypeGuard<T, S> = (t1: T | S) => t1 is T;
+export function checkThat<T, S>(t: T | S, isTypeT: TypeGuard<T, S>): T {
   if (!isTypeT(t)) {
     throwError(isTypeT, t);
+    // Typescrypt doesn't know that throwError throws an error.
+    throw 'Unreachable';
   }
   return t;
 }
