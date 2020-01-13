b8b1640f528516a78f84c29457790fb69444aafc
George Knee <georgeknee@googlemail.com>
2020-01-13 13:53:36 +0000

    WIP add switches to navbar

--- packages/rps/src/components/NavigationBar.tsx
+++ packages/rps/src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Switch from 'react-switch';
 import {Button, Navbar} from 'reactstrap';
 import {Commitment} from 'fmg-core';
 import {RulesModal} from './RulesModal';
@@ -20,15 +20,48 @@ export default class NavigationBar extends React.PureComponent<Props, Commitment
   render() {
     return (
       <Navbar className="navbar">
-        <Button color="link" className="navbar-button mr-auto" onClick={this.props.rulesRequest}>
-          Rules
-        </Button>
-        <div className="circle">
-          <div className="navbar-user">{getInitials(this.props.loginDisplayName)}</div>
+        <div className="align-self-start">
+          <span className="navbar-button" color="link">
+            AutoPlayer A:&nbsp;
+          </span>
+          <Switch
+            className="react-switch ml-auto"
+            onChange={() => {
+              /* */
+            }}
+            checked={false}
+            disabled={false}
+            height={15}
+            width={28}
+          />
+          <br />
+          <span className="navbar-button" color="link">
+            AutoPlayer B:&nbsp;
+          </span>
+          <Switch
+            className="react-switch ml-auto"
+            onChange={() => {
+              /* */
+            }}
+            checked={false}
+            disabled={false}
+            height={15}
+            width={28}
+          />
         </div>
-        <Button color="link" className="navbar-button ml-auto" onClick={this.props.logoutRequest}>
-          Sign Out
-        </Button>
+        <div className="align-self-center">
+          <div className="circle">
+            <div className="navbar-user">{getInitials(this.props.loginDisplayName)}</div>
+          </div>
+        </div>
+        <div className="align-self-end">
+          <Button color="link" className="navbar-button" onClick={this.props.rulesRequest}>
+            Rules
+          </Button>
+          <Button color="link" className="navbar-button" onClick={this.props.logoutRequest}>
+            Sign Out
+          </Button>
+        </div>
         <RulesModal visible={this.props.showRules} rulesRequest={this.props.rulesRequest} />
       </Navbar>
     );
