b5f1bbcaf93bdefa2f71b390f674c6a24bee63a9
Alex Gap <alex.gap@gmail.com>
2020-01-13 15:26:30 -0800

    Remove styled components

--- packages/xstate-wallet/src/ui/Wallet.tsx
+++ packages/xstate-wallet/src/ui/Wallet.tsx
@@ -1,18 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 import {Events, Init} from '@statechannels/wallet-protocols/lib/src/protocols/wallet/protocol';
 import {State} from 'xstate';
-import styled from 'styled-components';
 
 interface Props {
   currentProcess: string;
   currentState: State<Init, Events, any, any> | undefined;
 }
-const Wrapper = styled.section`
-  padding: 4em;
-  background: white;
-  border-radius: 3px;
-  height: 90%;
-`;
 
 export const Wallet = (props: Props) => {
   let currentState = 'no process';
@@ -20,12 +13,19 @@ export const Wallet = (props: Props) => {
     currentState = props.currentState.value.toString();
   }
   return (
-    <Wrapper>
+    <div
+      style={{
+        padding: '4em',
+        backgroundColor: 'white',
+        borderRadius: 4,
+        height: '90%'
+      }}
+    >
       <div>
         <div>{props.currentProcess}</div>
         <div>{currentState}</div>
       </div>
-    </Wrapper>
+    </div>
   );
 };
 
