af631aad6bc02e48351a057a5e3a09d85405659a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-01-13 13:56:44 -0700

    [e2e-tests] run yarn lint:write

--- packages/e2e-tests/puppeteer/helpers.ts
+++ packages/e2e-tests/puppeteer/helpers.ts
@@ -1,13 +1,13 @@
-import {Browser, Page, launch} from "puppeteer";
+import {Browser, Page, launch} from 'puppeteer';
 
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from 'fs';
+import * as path from 'path';
 
 export async function loadRPSApp(page: Page, ganacheAccountIndex: number) {
   // TODO: This is kinda ugly but it works
   // We need to instantiate a web3 for the wallet so we import the web 3 script
   // and then assign it on the window
-  const web3JsFile = fs.readFileSync(path.resolve(__dirname, "web3/web3.min.js"), "utf8");
+  const web3JsFile = fs.readFileSync(path.resolve(__dirname, 'web3/web3.min.js'), 'utf8');
   await page.evaluateOnNewDocument(web3JsFile);
   await page.evaluateOnNewDocument(`window.web3 = new Web3("http://localhost:8547")`);
   await page.evaluateOnNewDocument(`window.ethereum = window.web3.currentProvider`);
@@ -18,12 +18,12 @@ export async function loadRPSApp(page: Page, ganacheAccountIndex: number) {
   );
   await page.evaluateOnNewDocument(`window.ethereum.networkVersion = 9001`);
   await page.evaluateOnNewDocument(`window.ethereum.on = () => {}`);
-  await page.goto("http://localhost:3000/", {waitUntil: "networkidle0"});
-  page.on("pageerror", error => {
+  await page.goto('http://localhost:3000/', {waitUntil: 'networkidle0'});
+  page.on('pageerror', error => {
     throw error;
   });
-  page.on("console", msg => {
-    if (msg.type() === "error") {
+  page.on('console', msg => {
+    if (msg.type() === 'error') {
       throw new Error(`Error was logged into the console ${msg.text()}`);
     }
   });
@@ -36,9 +36,9 @@ export async function setUpBrowser(headless: boolean): Promise<Browser> {
     devtools: !headless,
     // Needed to allow both windows to execute JS at the same time
     ignoreDefaultArgs: [
-      "--disable-background-timer-throttling",
-      "--disable-backgrounding-occluded-windows",
-      "--disable-renderer-backgrounding"
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-renderer-backgrounding'
     ]
   });
 
