540e888343bb855856bbc4d12eb0ee84fd6540fa
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-01-14 12:25:15 -0700

    subtract throws on unsafe subtraction

--- packages/wallet-specs/src/index.ts
+++ packages/wallet-specs/src/index.ts
@@ -79,9 +79,21 @@ export { chain } from './chain';
 
 // This stuff should be replaced with some big number logic
 type numberish = string | number | undefined;
-export const add = (a: numberish, b: numberish) => (Number(a || 0) + Number(b || 0)).toString();
-export const subtract = (a: numberish, b: numberish) => (Number(a) - Number(b)).toString();
-export const max = (a: numberish, b: numberish) => Math.max(Number(a), Number(b)).toString();
+type MathOp = (a: numberish, b: numberish) => string;
+export const add: MathOp = (a: numberish, b: numberish) =>
+  (Number(a || 0) + Number(b || 0)).toString();
+export const subtract: MathOp = (a: numberish, b: numberish) => {
+  const numA = Number(a);
+  const numB = Number(b);
+
+  if (numB > numA) {
+    throw new Error('Unsafe subtraction');
+  }
+  return (numA - numB).toString();
+};
+
+export const max: MathOp = (a: numberish, b: numberish) =>
+  Math.max(Number(a), Number(b)).toString();
 export const gt = (a: numberish, b: numberish) => Number(a) > Number(b);
 
 export const success: { type: 'final' } = { type: 'final' };
