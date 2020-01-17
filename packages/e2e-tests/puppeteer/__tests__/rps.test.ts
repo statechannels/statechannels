3cf7567d875618c5c4923a02a6cbb98b1c8bf299
George Knee <georgeknee@googlemail.com>
2020-01-17 18:37:23 +0000

    Refactor e2e test
    setup is its own function
    playing a game is tested twice, either side of close+withdraw
    dealing with different buttons on ledger vs virtual funding via promise
    race
    no need for new tabs

--- packages/e2e-tests/puppeteer/__tests__/rps.test.ts
+++ packages/e2e-tests/puppeteer/__tests__/rps.test.ts
@@ -1,5 +1,5 @@
 import {setUpBrowser, loadRPSApp} from '../helpers';
-import {clickThroughRPSUI, clickThroughResignationUI} from '../scripts/rps';
+import {clickThroughRPSUI, clickThroughResignationUI, setupRPS} from '../scripts/rps';
 import {Page, Browser} from 'puppeteer';
 
 jest.setTimeout(60000);
@@ -14,11 +14,12 @@ describe('Playing a game of RPS', () => {
     browserA = await setUpBrowser(true);
     browserB = await setUpBrowser(true);
 
-    rpsTabA = await browserA.newPage();
-    rpsTabB = await browserB.newPage();
+    rpsTabA = (await browserA.pages())[0];
+    rpsTabB = (await browserB.pages())[0];
 
     await loadRPSApp(rpsTabA, 0);
     await loadRPSApp(rpsTabB, 1);
+    await setupRPS(rpsTabA, rpsTabB);
   });
 
   afterAll(async () => {
@@ -43,7 +44,18 @@ describe('Playing a game of RPS', () => {
   it('can then withdraw funds', async () => {
     await clickThroughResignationUI(rpsTabA, rpsTabB);
 
-    // Should be on the homepage
+    // Should be in the lobby
     expect(await rpsTabB.waitForXPath('//button[contains(., "Create a game")]')).toBeDefined();
+    expect(await rpsTabA.waitForXPath('//button[contains(., "Create a game")]')).toBeDefined();
+  });
+
+  it('can play a second game', async () => {
+    await clickThroughRPSUI(rpsTabA, rpsTabB);
+    expect(await (await rpsTabA.waitFor('h1.mb-5')).evaluate(el => el.textContent)).toMatch(
+      'You lost'
+    );
+    expect(await (await rpsTabB.waitFor('h1.mb-5')).evaluate(el => el.textContent)).toMatch(
+      'You won!'
+    );
   });
 });
--- packages/e2e-tests/puppeteer/scripts/rps.ts
+++ packages/e2e-tests/puppeteer/scripts/rps.ts
@@ -1,7 +1,7 @@
 import {setUpBrowser, loadRPSApp} from '../helpers';
 import {Page} from 'puppeteer';
 
-export async function clickThroughRPSUI(rpsTabA: Page, rpsTabB: Page): Promise<void> {
+export async function setupRPS(rpsTabA: Page, rpsTabB: Page): Promise<void> {
   await (await rpsTabA.waitForXPath('//button[contains(., "Start Playing!")]')).click();
   await (await rpsTabB.waitForXPath('//button[contains(., "Start Playing!")]')).click();
 
@@ -10,53 +10,60 @@ export async function clickThroughRPSUI(rpsTabA: Page, rpsTabB: Page): Promise<v
 
   await (await rpsTabB.waitFor('#name')).type('playerB');
   (await rpsTabB.waitForXPath('//button[contains(., "Connect with MetaMask")]')).click();
+}
 
+export async function clickThroughRPSUI(rpsTabA: Page, rpsTabB: Page): Promise<void> {
   // NOTE: There is some weird scrolling issue. .click() scrolls and somehow React re-renders this
   // button and so we get a "Node is detached from document error". Using .evaluate() fixes it.
   // https://github.com/puppeteer/puppeteer/issues/3496
   await (await rpsTabA.waitForXPath('//button[contains(., "Create a game")]')).evaluate(
     'document.querySelector("button.lobby-new-game").click()'
-  );
-  await (await rpsTabA.waitForXPath('//button[contains(., "Create Game")]')).click();
+  ); // TODO this is actually Player B. Consider permuting A and B throughout this script.
 
+  await (await rpsTabA.waitForXPath('//button[contains(., "Create Game")]')).click();
   await (await rpsTabB.waitForXPath('//button[contains(., "Join")]')).click();
 
   const walletIFrameA = rpsTabA.frames()[1];
   const walletIFrameB = rpsTabB.frames()[1];
 
   await (await walletIFrameB.waitForXPath('//button[contains(., "Fund Channel")]')).click();
-
   await (await walletIFrameA.waitForXPath('//button[contains(., "Fund Channel")]')).click();
-
   await (await walletIFrameB.waitForXPath('//button[contains(., "Ok!")]')).click();
-
   await (await walletIFrameA.waitForXPath('//button[contains(., "Ok!")]')).click();
-
   await (await rpsTabA.waitFor('img[src*="rock"]')).click();
-
   await (await rpsTabB.waitFor('img[src*="paper"]')).click();
 }
 
 export async function clickThroughResignationUI(rpsTabA: Page, rpsTabB: Page): Promise<void> {
-  (await rpsTabA.waitForXPath('//button[contains(., "Resign")]')).click();
+  async function tabA(): Promise<void> {
+    const walletIFrameA = rpsTabA.frames()[1];
 
-  const walletIFrameA = rpsTabA.frames()[1];
-  const walletIFrameB = rpsTabB.frames()[1];
+    await (await rpsTabA.waitForXPath('//button[contains(., "Resign")]')).click();
+    await (await walletIFrameA.waitForXPath('//button[contains(., "Close Channel")]')).click();
+    const button = await Promise.race([
+      walletIFrameA.waitForXPath('//button[contains(., "Approve")]'), // virtual funding
+      walletIFrameA.waitForXPath('//button[contains(., "Ok")]') // ledger funding NOTE CASE SENSITIVE Ok not OK
+    ]);
+    await button.click();
+    await (await rpsTabA.waitForXPath('//button[contains(., "OK")]')).click();
+    await (await rpsTabA.waitForXPath('//button[contains(., "Exit")]')).click();
+    return;
+  }
 
-  await (await walletIFrameB.waitForXPath('//button[contains(., "Close Channel")]')).click();
+  async function tabB(): Promise<void> {
+    const walletIFrameB = rpsTabB.frames()[1];
 
-  await (await walletIFrameA.waitForXPath('//button[contains(., "Close Channel")]')).click();
+    await (await walletIFrameB.waitForXPath('//button[contains(., "Close Channel")]')).click();
+    await (await walletIFrameB.waitForXPath('//button[contains(., "Approve")]')).click();
+    await (await walletIFrameB.waitForXPath('//button[contains(., "Ok")]')).click();
+    await (await rpsTabB.waitForXPath('//button[contains(., "OK")]')).click();
+    await (await rpsTabB.waitForXPath('//button[contains(., "Exit")]')).click();
+    return;
+  }
 
-  await (await walletIFrameB.waitForXPath('//button[contains(., "Approve")]')).click();
-
-  await (await walletIFrameB.waitForXPath('//button[contains(., "Ok")]')).click();
-
-  (await rpsTabB.waitForXPath('//button[contains(., "OK")]')).click();
-
-  (await rpsTabB.waitForXPath('//button[contains(., "Exit")]')).click();
-
-  // TODO: Implement some logic so this can be called in both the ledger or virtual channel case
-  //       (in the virtual case, both UIs show "Approve" but in the ledger case, only one does)
+  const tabAcomplete = tabA();
+  const tabBcomplete = tabB();
+  await Promise.all([tabAcomplete, tabBcomplete]);
 }
 
 if (require.main === module) {
