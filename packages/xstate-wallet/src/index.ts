ff37e1c2247b9a268e821f1bb2f026fad69c9872
Alex Gap <alex.gap@gmail.com>
2020-01-23 11:58:39 -0800

    Clean up

--- packages/xstate-wallet/src/index.ts
+++ packages/xstate-wallet/src/index.ts
@@ -5,9 +5,6 @@ import {ethers} from 'ethers';
 import {Store} from './storage/store';
 import {ChainWatcher} from './chain';
 import {WorkflowManager} from './workflow-manager';
-import ReactDOM from 'react-dom';
-import React from 'react';
-import WalletUi from './ui/wallet';
 
 const ourWallet = ethers.Wallet.createRandom();
 
@@ -26,6 +23,3 @@ const workflowManager = new WorkflowManager(store);
 window.addEventListener('message', async event => {
   await handleMessage(event, workflowManager, store, ourWallet);
 });
-
-(window as any).React2 = React;
-console.log((window as any).React1 === (window as any).React2);
