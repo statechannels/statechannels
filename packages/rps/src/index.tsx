fc8b261fbc28e416ed7299037113c369a5b0839e
Alon Bukai <alonzorz@gmail.com>
2020-01-25 18:00:45 +0200

    feat: Add Sentry to RPS

--- packages/rps/src/index.tsx
+++ packages/rps/src/index.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
+import * as Sentry from '@sentry/browser';
 
 // Not adding in currently because it breaks most of our existing components
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import store from './redux/store';
 import SiteContainer from './containers/SiteContainer';
+
+Sentry.init({dsn: 'https://db92051c360145b4b3537d33881c1bc3@sentry.io/1913622'});
 
 render(
   <Provider store={store}>
