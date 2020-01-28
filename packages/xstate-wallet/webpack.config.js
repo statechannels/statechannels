fa9f6196e98b25cad2b4c717c6c64f84a45e17b1
George Knee <georgeknee@googlemail.com>
2020-01-28 16:16:00 +0000

    Add some basic styling

--- packages/xstate-wallet/webpack.config.js
+++ packages/xstate-wallet/webpack.config.js
@@ -35,6 +35,17 @@ module.exports = {
           // Compiles Sass to CSS
           'sass-loader'
         ]
+      },
+      {
+        loader: require.resolve('file-loader'),
+        // Exclude `js` files to keep "css" loader working as it injects
+        // its runtime that would otherwise be processed through "file" loader.
+        // Also exclude `html` and `json` extensions so they get processed
+        // by webpacks internal loaders.
+        exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.s[ac]ss$/i],
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
       }
     ]
   },
