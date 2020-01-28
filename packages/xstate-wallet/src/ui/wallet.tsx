94b428af70bf60c72490c33f6a9d61cdd6095609
George Knee <georgeknee@googlemail.com>
2020-01-28 16:45:13 +0000

    Use Rimble Image component

--- packages/xstate-wallet/src/ui/wallet.tsx
+++ packages/xstate-wallet/src/ui/wallet.tsx
@@ -3,7 +3,7 @@ import {Interpreter} from 'xstate';
 import {useService} from '@xstate/react';
 import './wallet.scss';
 import logo from '../images/logo.svg';
-import {Modal, Card, Flex, Blockie} from 'rimble-ui';
+import {Modal, Card, Flex, Blockie, Image} from 'rimble-ui';
 
 interface Props {
   workflow: Interpreter<any, any, any>;
@@ -12,9 +12,9 @@ export const Wallet = (props: Props) => {
   const [current] = useService(props.workflow);
   return (
     <Modal isOpen={true}>
-      <Card width={'320px'} height={'450px'} p={2} boxShadow={4}>
+      <Card width={'320px'} height={'450px'}>
         <Flex px={[3, 3, 4]} height={3} borderBottom={1} borderColor={'#E8E8E8'} mt={'0.8'}>
-          <img className="p-3" width="150" src={logo} />
+          <Image alt="State Channels" borderRadius={8} height="auto" src={logo} />
         </Flex>
         <div
           style={{
