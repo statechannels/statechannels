587560dde1c2f61be09573bd6b0a49fdf033129f
Alex Gap <alex.gap@gmail.com>
2020-02-03 16:30:47 -0800

    Initial confirm create channel implementation

---
+++ packages/xstate-wallet/src/workflows/confirm-create-channel.ts
@@ -0,0 +1,94 @@
+import {
+  MachineConfig,
+  Action,
+  DefaultGuardType,
+  GuardPredicate,
+  ConditionPredicate,
+  StateSchema
+} from 'xstate';
+import {Allocations, Participant} from '@statechannels/client-api-schema';
+
+interface WorkflowActions {
+  hideUi: Action<WorkflowContext, any>;
+  displayUi: Action<WorkflowContext, any>;
+}
+interface WorkflowGuards {
+  noBudget:
+    | GuardPredicate<WorkflowContext, WorkflowEvent>
+    | ConditionPredicate<WorkflowContext, WorkflowEvent>;
+}
+
+// While this context info may not be used by the workflow
+// it may be used when displaying a UI
+interface WorkflowContext {
+  participants: Participant[];
+  allocations: Allocations;
+  appDefinition: string;
+  appData: string;
+  chainId: string;
+  challengeDuration: number;
+}
+
+interface WorkflowStateSchema extends StateSchema<WorkflowContext> {
+  states: {
+    needUserConfirmation: {};
+    waitForUserConfirmation: {};
+    // TODO: Is it possible to type these as type:'final' ?
+    done: {};
+    failure: {};
+  };
+}
+interface UserApproves {
+  type: 'USER_APPROVES';
+}
+interface UserRejects {
+  type: 'USER_REJECTS';
+}
+type WorkflowEvent = UserApproves | UserRejects;
+const generateConfig = (
+  actions: WorkflowActions,
+  guards: WorkflowGuards
+): MachineConfig<WorkflowContext, WorkflowStateSchema, WorkflowEvent> => ({
+  initial: 'needUserConfirmation',
+  states: {
+    needUserConfirmation: {
+      on: {
+        '': [
+          {
+            target: 'waitForUserConfirmation',
+            cond: guards.noBudget,
+            actions: [actions.displayUi]
+          },
+          {
+            target: 'done'
+          }
+        ]
+      }
+    },
+    waitForUserConfirmation: {
+      on: {
+        USER_APPROVES: {target: 'done', actions: [actions.hideUi]},
+        USER_REJECTS: {target: 'failure', actions: [actions.hideUi]}
+      }
+    },
+    done: {type: 'final'},
+    failure: {type: 'final'}
+  }
+});
+
+const mockActions: WorkflowActions = {
+  hideUi: 'hideUi',
+  displayUi: 'displayUi'
+};
+const mockGuards = {
+  noBudget: {
+    // TODO: Using a guard predicate type allows for the name to be displayed
+    // on the visualizer for the condition
+    // We should probably find a better way of doing this or not bother typing guards
+    type: 'xstate.guard' as DefaultGuardType,
+    name: 'noBudget',
+    predicate: (context, event) => true
+  }
+};
+export const mockOptions = {actions: mockActions, guards: mockGuards};
+export const config = generateConfig(mockActions, mockGuards);
