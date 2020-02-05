e752bda2bd4d50acb895ad22fc6317d1bb2a6c7e
Tom Close <tom.close@cantab.net>
2020-02-05 09:37:46 +0000

    Make a start on the new store

---
+++ packages/xstate-wallet/src/store/memory-store.test.ts
@@ -0,0 +1,47 @@
+import {MemoryStore, Protocol, State} from './memory-store';
+
+describe('stateReceived', () => {
+  test('it fires when a state with the correct channel id is received', () => {
+    const channelId = 'abc123';
+    const state = {channelId};
+    const signature = {r: 'r', s: 's'};
+    const signedState = {state, signatures: [signature]};
+    const store = new MemoryStore();
+
+    const outputs: State[] = [];
+    store.statesReceived(channelId).subscribe(x => outputs.push(x));
+    store.pushMessage({signedStates: [signedState]});
+
+    expect(outputs).toEqual([state]);
+  });
+
+  test("it doesn't fire if the channelId doesn't match", () => {
+    const channelId = 'abc123';
+    const state = {channelId};
+    const signature = {r: 'r', s: 's'};
+    const signedState = {state, signatures: [signature]};
+    const store = new MemoryStore();
+
+    const outputs: State[] = [];
+    store.statesReceived('a-different-channel-id').subscribe(x => outputs.push(x));
+    store.pushMessage({signedStates: [signedState]});
+
+    expect(outputs).toEqual([]);
+  });
+});
+
+test('new-protocols', () => {
+  const protocol: Protocol = {name: 'CreateAndDirectFund', participants: []};
+
+  const store = new MemoryStore();
+
+  const outputs: Protocol[] = [];
+  store.newProtocols().subscribe(x => outputs.push(x));
+
+  store.pushMessage({protocols: [protocol]});
+  expect(outputs).toEqual([protocol]);
+
+  // doing it twice doesn't change anything
+  store.pushMessage({protocols: [protocol]});
+  expect(outputs).toEqual([protocol]);
+});
