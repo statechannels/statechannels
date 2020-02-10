8b8e68947158ebe72ebdc2e3b198d3b3703a8722
Liam Horne <liam@lihorne.com>
2020-02-10 14:30:54 -0800

    Fix siteConfig

--- packages/nitro-protocol/website/siteConfig.js
+++ packages/nitro-protocol/website/siteConfig.js
@@ -25,7 +25,7 @@ const siteConfig = {
   title: 'Nitro protocol', // Title for your website.
   tagline: 'A smart-contract protocol for state channel networks',
   url: 'https://protocol.statechannels.org', // Your website URL
-  baseUrl: '', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
