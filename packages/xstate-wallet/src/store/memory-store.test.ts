88108cf762d817befb4778e23af70b9d2c5fbd39
tomclose <tom.close@cantab.net>
2020-02-12 09:17:11 +0000

    Revert "Store states when they arrive"

--- packages/xstate-wallet/src/store/memory-store.test.ts
+++ packages/xstate-wallet/src/store/memory-store.test.ts
@@ -3,15 +3,11 @@ import {Objective} from './wire-protocol';
 import {SimpleEthAllocation, State} from './types';
 import {bigNumberify, BigNumber} from 'ethers/utils';
 import {Wallet} from 'ethers';
-import {calculateChannelId, signState} from './state-utils';
+import {calculateChannelId} from './state-utils';
 
-const {address: aAddress, privateKey: aPrivateKey} = new Wallet(
-  '0x95942b296854c97024ca3145abef8930bf329501b718c0f66d57dba596ff1318'
-); // 0x11115FAf6f1BF263e81956F0Cc68aEc8426607cf
-
-const {address: bAddress} = new Wallet(
-  '0xb3ab7b031311fe1764b657a6ae7133f19bac97acd1d7edca9409daa35892e727'
-); // 0x2222E21c8019b14dA16235319D34b5Dd83E644A9
+const {address: aAddress, privateKey: aPrivateKey} = Wallet.createRandom();
+// const {address: bAddress, privateKey: bPrivateKey} = Wallet.createRandom();
+const {address: bAddress} = Wallet.createRandom();
 const [aDestination, bDestination] = [aAddress, bAddress]; // for convenience
 
 const outcome: SimpleEthAllocation = {
@@ -27,7 +23,7 @@ const isFinal = false;
 const chainId = '1';
 const participants = [
   {participantId: 'a', destination: aDestination, signingAddress: aAddress},
-  {participantId: 'b', destination: bDestination, signingAddress: bAddress}
+  {participantId: 'b', destination: aDestination, signingAddress: bAddress}
 ];
 const stateVars = {outcome, turnNum, appData, isFinal};
 const channelNonce = bigNumberify(0);
@@ -35,10 +31,8 @@ const appDefinition = '0x5409ED021D9299bf6814279A6A1411A7e866A631';
 const challengeDuration = bigNumberify(60);
 const channelConstants = {chainId, participants, channelNonce, appDefinition, challengeDuration};
 const state: State = {...stateVars, ...channelConstants};
-const channelId = calculateChannelId(channelConstants);
-const signature = signState(state, aPrivateKey);
+const signature = '0x123';
 const signedState = {...state, signature};
-const signedStates = [signedState];
 
 describe('getAddress', () => {
   it('returns an address', () => {
@@ -48,91 +42,79 @@ describe('getAddress', () => {
     expect(address).toEqual(aAddress);
   });
 });
-const aStore = () => new MemoryStore([aPrivateKey]);
 
 describe('stateReceivedFeed', () => {
-  test('it fires when a state with the correct channel id is received', async () => {
-    const store = aStore();
+  test('it fires when a state with the correct channel id is received', () => {
+    const store = new MemoryStore();
     const outputs: State[] = [];
-    store.stateReceivedFeed(channelId).subscribe(x => outputs.push(x));
-    await store.pushMessage({signedStates});
+    store.stateReceivedFeed(calculateChannelId(signedState)).subscribe(x => outputs.push(x));
+    store.pushMessage({signedStates: [signedState]});
 
     expect(outputs).toEqual([state]);
   });
 
-  test("it doesn't fire if the channelId doesn't match", async () => {
-    const store = aStore();
+  test("it doesn't fire if the channelId doesn't match", () => {
+    const store = new MemoryStore();
 
     const outputs: State[] = [];
     store.stateReceivedFeed('a-different-channel-id').subscribe(x => outputs.push(x));
-    await store.pushMessage({signedStates});
+    store.pushMessage({signedStates: [signedState]});
 
     expect(outputs).toEqual([]);
   });
 });
 
-test('newObjectiveFeed', async () => {
+test('newObjectiveFeed', () => {
   const objective: Objective = {
     name: 'OpenChannel',
     participants: [],
     data: {targetChannelId: 'foo'}
   };
 
-  const store = aStore();
+  const store = new MemoryStore();
 
   const outputs: Objective[] = [];
   store.newObjectiveFeed.subscribe(x => outputs.push(x));
 
-  await store.pushMessage({objectives: [objective]});
+  store.pushMessage({objectives: [objective]});
   expect(outputs).toEqual([objective]);
 
   // doing it twice doesn't change anything
-  await store.pushMessage({objectives: [objective]});
+  store.pushMessage({objectives: [objective]});
   expect(outputs).toEqual([objective]);
 });
 
 describe('createChannel', () => {
-  it('returns a ChannelStoreEntry', async () => {
-    const store = aStore();
+  it('returns a channelId', async () => {
+    const store = new MemoryStore([aPrivateKey]);
 
-    const firstEntry = await store.createChannel(participants, challengeDuration, appDefinition);
+    const channelId = await store.createChannel(
+      participants,
+      challengeDuration,
+      stateVars,
+      appDefinition
+    );
 
-    expect(firstEntry.channelId).toMatch(/0x/);
-    expect(firstEntry.latestSupportedByMe).toBeUndefined();
+    expect(channelId).toMatch(/0x/);
 
-    const secondEntry = await store.createChannel(participants, challengeDuration, appDefinition);
+    const channelId2 = await store.createChannel(
+      participants,
+      challengeDuration,
+      stateVars,
+      appDefinition
+    );
 
-    expect(firstEntry.channelId).not.toEqual(secondEntry.channelId);
+    expect(channelId2).toMatch(/0x/);
+    expect(channelId2).not.toEqual(channelId);
   });
 
   it("fails if the wallet doesn't hold the private key for any participant", async () => {
     const store = new MemoryStore();
 
     await expect(
-      store.createChannel(participants, challengeDuration, appDefinition)
+      store.createChannel(participants, challengeDuration, stateVars, appDefinition)
     ).rejects.toMatchObject({
       message: "Couldn't find the signing key for any participant in wallet."
     });
-  });
-});
-
-describe('pushMessage', () => {
-  it('stores states', async () => {
-    const store = new MemoryStore([aPrivateKey]);
-    await store.createChannel(
-      signedState.participants,
-      signedState.challengeDuration,
-      signedState.appDefinition
-    );
-
-    expect((await store.getEntry(channelId)).latest).toBeUndefined();
-    await store.pushMessage({signedStates});
-    expect((await store.getEntry(channelId)).latest).toMatchObject(signedState);
-  });
-
-  it('creates a channel if it receives states for a new channel', async () => {
-    const store = new MemoryStore([aPrivateKey]);
-    await store.pushMessage({signedStates});
-    expect(await store.getEntry(channelId)).not.toBeUndefined();
   });
 });
