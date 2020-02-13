b483ba0506391f5446c2261ba55c791eca14224f
George Knee <georgeknee@googlemail.com>
2020-02-13 08:50:51 +0000

    Add test for subscribeDepositEvent
    - Fix contract event string 'DEPOSITED' -> 'Deposited'
    - port jest config over from redux wallet
    - add Adjudicator and slots for more Asset Holders to ChainWatcher class

--- packages/xstate-wallet/config/jest/jest.config.js
+++ packages/xstate-wallet/config/jest/jest.config.js
@@ -1,5 +1,9 @@
+const {resolve} = require('path');
+const root = resolve(__dirname, '../../');
 module.exports = {
+  rootDir: root,
   testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testPathIgnorePatterns: ['<rootDir>/src/tests-with-chain'],
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
   transform: {
