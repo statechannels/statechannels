7fd9e47f446c12c1d4f7f3a225bb36e2534c4f60
Liam Horne <liam@lihorne.com>
2020-02-20 12:52:55 -0800

    Remove Upload button on Web3Torrent UI

--- packages/web3torrent/src/components/layout/layout-header/LayoutHeader.tsx
+++ packages/web3torrent/src/components/layout/layout-header/LayoutHeader.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {FormButton} from '../../form';
 
 import {Link, RouteComponentProps} from 'react-router-dom';
 import {RoutePath} from '../../../routes';
@@ -13,11 +12,6 @@ const LayoutHeader: React.FC<RouteComponentProps> = props => {
         <Link className="header-logo" to={RoutePath.Root}>
           <span className="header-logo-hidden">Web3Torrent Logo - Go to Home</span>
         </Link>
-        <div className="actions-container">
-          <FormButton name="upload" onClick={() => props.history.push(RoutePath.Upload)}>
-            Upload
-          </FormButton>
-        </div>
       </nav>
     </header>
   );
