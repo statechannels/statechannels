131f3f0d651139bb78b86f584558810ec2ebde33
George Knee <georgeknee@googlemail.com>
2020-02-20 15:38:46 -0800

    Connection Banner updates dynamically
    (without refresh)
    
    Convert component to class, update state on metamask event
    
    TODO roll out to other components that need this information

--- packages/web3torrent/src/App.tsx
+++ packages/web3torrent/src/App.tsx
@@ -10,25 +10,35 @@ import Upload from './pages/upload/Upload';
 import {RoutePath} from './routes';
 import ConnectionBanner from '@rimble/connection-banner';
 
-const App: React.FC = () => {
-  const history = createBrowserHistory();
-  return (
-    <Router history={history}>
-      <main>
-        <ConnectionBanner
-          currentNetwork={parseInt(window.ethereum.chainId, 16)}
-          requiredNetwork={process.env.REACT_APP_CHAIN_NETWORK_ID}
-          onWeb3Fallback={false}
-        />
-        <Route path={RoutePath.Root} component={LayoutHeader} />
-        <Switch>
-          <Route exact path={RoutePath.Root} component={Welcome} />
-          <Route exact path={RoutePath.File} component={File} />
-          <Route exact path={RoutePath.Upload} component={Upload} />
-        </Switch>
-      </main>
-      <Route path={RoutePath.Root} component={LayoutFooter} />
-    </Router>
-  );
-};
+class App extends React.Component {
+  state = {currentNetwork: parseInt(window.ethereum.chainId, 16)};
+
+  componentDidMount() {
+    window.ethereum.on('networkChanged', chainId => {
+      this.setState({currentNetwork: parseInt(chainId, 16)});
+    });
+  }
+
+  render() {
+    const history = createBrowserHistory();
+    return (
+      <Router history={history}>
+        <main>
+          <ConnectionBanner
+            currentNetwork={this.state.currentNetwork}
+            requiredNetwork={Number(process.env.REACT_APP_CHAIN_NETWORK_ID)}
+            onWeb3Fallback={false}
+          />
+          <Route path={RoutePath.Root} component={LayoutHeader} />
+          <Switch>
+            <Route exact path={RoutePath.Root} component={Welcome} />
+            <Route exact path={RoutePath.File} component={File} />
+            <Route exact path={RoutePath.Upload} component={Upload} />
+          </Switch>
+        </main>
+        <Route path={RoutePath.Root} component={LayoutFooter} />
+      </Router>
+    );
+  }
+}
 export default App;
