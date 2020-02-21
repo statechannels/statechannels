3e3d147710fc90b32da1990f890f621afffc5266
George Knee <georgeknee@googlemail.com>
2020-02-21 11:08:34 -0800

    Mock out window.ethereum.on()

--- packages/web3torrent/src/App.test.tsx
+++ packages/web3torrent/src/App.test.tsx
@@ -4,6 +4,20 @@ import App from './App';
 import {web3torrent, WebTorrentContext} from './clients/web3torrent-client';
 
 it('renders app without crashing', () => {
+  async function on() {
+    return new Promise(r => r());
+  }
+
+  const ethereum = {
+    on
+  };
+
+  // mock out window.ethereum.enable
+  Object.defineProperty(window, 'ethereum', {
+    enumerable: true,
+    value: ethereum
+  });
+
   const div = document.createElement('div');
   ReactDOM.render(
     <WebTorrentContext.Provider value={web3torrent}>
