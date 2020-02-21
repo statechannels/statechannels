d782f768ed4c6139f2a996c880814b786f4d2b7d
George Knee <georgeknee@googlemail.com>
2020-02-21 15:16:44 -0800

    Add Web3TorrentChannelClient

---
+++ packages/web3torrent/src/clients/web3t-channel-client.ts
@@ -0,0 +1,200 @@
+import {ChannelResult, Message, ChannelClientInterface} from '@statechannels/channel-client';
+import {bigNumberify} from 'ethers/utils';
+
+interface ChannelState {
+  channelId: string;
+  turnNum: string;
+  challengeExpirationTime;
+  aUserId: string;
+  bUserId: string;
+  aAddress: string;
+  bAddress: string;
+  aOutcomeAddress: string;
+  bOutcomeAddress: string;
+  aBal: string;
+  bBal: string;
+}
+
+// This class wraps the channel client converting the request/response formats to those used in the app
+
+export class Web3TorrentChannelClient {
+  constructor(private readonly channelClient: ChannelClientInterface) {}
+
+  mySigningAddress?: string;
+  myEthereumSelectedAddress?: string; // this state can be inspected to infer whether we need to get the user to "Connect With MetaMask" or not.
+
+  async enable() {
+    /* empty */
+  }
+
+  async createChannel(
+    aAddress: string,
+    bAddress: string,
+    aBal: string,
+    bBal: string,
+    aOutcomeAddress: string = aAddress,
+    bOutcomeAddress: string = bAddress
+  ): Promise<ChannelState> {
+    const participants = formatParticipants(aAddress, bAddress, aOutcomeAddress, bOutcomeAddress);
+    const allocations = formatAllocations(aOutcomeAddress, bOutcomeAddress, aBal, bBal);
+    const appDefinition = '0x0'; // TODO SingleAssetPayments address
+
+    const channelResult = await this.channelClient.createChannel(
+      participants,
+      allocations,
+      appDefinition,
+      'appData unused'
+    );
+
+    return convertToChannelState(channelResult);
+  }
+
+  async getAddress() {
+    this.mySigningAddress = await this.channelClient.getAddress();
+    return this.mySigningAddress;
+  }
+
+  async getEthereumSelectedAddress() {
+    this.myEthereumSelectedAddress = await this.channelClient.getEthereumSelectedAddress();
+    return this.myEthereumSelectedAddress;
+  }
+
+  onMessageQueued(callback: (message: Message) => void) {
+    return this.channelClient.onMessageQueued(callback);
+  }
+
+  // Accepts an rps-friendly callback, performs the necessary encoding, and subscribes to the channelClient with an appropriate, API-compliant callback
+  onChannelUpdated(rpsCallback: (channelState: ChannelState) => any) {
+    function callback(channelResult: ChannelResult): any {
+      rpsCallback(convertToChannelState(channelResult));
+    }
+    // These are two distinct events from the channel client
+    // but for our purposes we can treat them the same
+    // and rely on the channel status
+    const unsubChannelUpdated = this.channelClient.onChannelUpdated(callback);
+    const unsubChannelProposed = this.channelClient.onChannelProposed(callback);
+
+    return () => {
+      unsubChannelUpdated();
+      unsubChannelProposed();
+    };
+  }
+
+  async joinChannel(channelId: string) {
+    const channelResult = await this.channelClient.joinChannel(channelId);
+    return convertToChannelState(channelResult);
+  }
+
+  async closeChannel(channelId: string): Promise<ChannelState> {
+    const channelResult = await this.channelClient.closeChannel(channelId);
+    return convertToChannelState(channelResult);
+  }
+
+  async challengeChannel(channelId: string): Promise<ChannelState> {
+    const channelResult = await this.channelClient.challengeChannel(channelId);
+    return convertToChannelState(channelResult);
+  }
+
+  async updateChannel(
+    channelId: string,
+    aAddress: string,
+    bAddress: string,
+    aBal: string,
+    bBal: string,
+    aOutcomeAddress: string = aAddress,
+    bOutcomeAddress: string = bAddress
+  ) {
+    const allocations = formatAllocations(aOutcomeAddress, bOutcomeAddress, aBal, bBal);
+    const participants = formatParticipants(aAddress, bAddress, aOutcomeAddress, bOutcomeAddress);
+
+    // ignore return val for now and stub out response
+    const channelResult = await this.channelClient.updateChannel(
+      channelId,
+      participants,
+      allocations,
+      'appData unused'
+    );
+
+    return convertToChannelState(channelResult);
+  }
+
+  // seeder may use this method to accept payments
+  async counterSign(channelState: ChannelState) {
+    const {
+      channelId,
+      aAddress,
+      bAddress,
+      aBal,
+      bBal,
+      aOutcomeAddress,
+      bOutcomeAddress
+    } = channelState;
+    this.updateChannel(channelId, aAddress, bAddress, aBal, bBal, aOutcomeAddress, bOutcomeAddress);
+  }
+
+  async pushMessage(message: Message) {
+    await this.channelClient.pushMessage(message);
+  }
+
+  async approveBudgetAndFund(
+    playerAmount: string,
+    hubAmount: string,
+    playerDestinationAddress: string,
+    hubAddress: string,
+    hubDestinationAddress: string
+  ) {
+    await this.channelClient.approveBudgetAndFund(
+      playerAmount,
+      hubAmount,
+      playerDestinationAddress,
+      hubAddress,
+      hubDestinationAddress
+    );
+  }
+}
+
+const convertToChannelState = (channelResult: ChannelResult): ChannelState => {
+  const {
+    turnNum,
+    channelId,
+    status,
+    participants,
+    allocations,
+    challengeExpirationTime
+  } = channelResult;
+  return {
+    channelId,
+    turnNum: turnNum.toString(), // TODO: turnNum should be switched to a number (or be a string everywhere),
+    challengeExpirationTime,
+    aUserId: participants[0].participantId,
+    bUserId: participants[1].participantId,
+    aAddress: participants[0].destination,
+    bAddress: participants[1].destination,
+    aOutcomeAddress: participants[0].destination,
+    bOutcomeAddress: participants[1].destination,
+    aBal: bigNumberify(allocations[0].allocationItems[0].amount).toString(),
+    bBal: bigNumberify(allocations[0].allocationItems[1].amount).toString()
+  };
+};
+
+const formatParticipants = (
+  aAddress: string,
+  bAddress: string,
+  aOutcomeAddress: string = aAddress,
+  bOutcomeAddress: string = bAddress
+) => [
+  {participantId: aAddress, signingAddress: aAddress, destination: aOutcomeAddress},
+  {participantId: bAddress, signingAddress: bAddress, destination: bOutcomeAddress}
+];
+
+const formatAllocations = (aAddress: string, bAddress: string, aBal: string, bBal: string) => {
+  return [
+    {
+      token: '0x0',
+      allocationItems: [
+        {destination: aAddress, amount: bigNumberify(aBal).toHexString()},
+        {destination: bAddress, amount: bigNumberify(bBal).toHexString()}
+      ]
+    }
+  ];
+};
