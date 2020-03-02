d3517391a82679b3e0a4d3099ddbb5cbcf6078c4
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-03-02 12:19:04 -0800

    Add a virtual-funding test with a dumb hub

---
+++ src/workflows/tests/dumb-hub.ts
@@ -0,0 +1,34 @@
+import {Message} from '../../store/types';
+import {Observable, fromEvent} from 'rxjs';
+import {EventEmitter} from 'eventemitter3';
+import {signState} from '../../store/state-utils';
+import {ethers} from 'ethers';
+
+export class DumbHub {
+  constructor(
+    private readonly privateKey: string,
+    private readonly _eventEmitter = new EventEmitter()
+  ) {}
+
+  public get outboxFeed(): Observable<Message> {
+    return fromEvent(this._eventEmitter, 'addToOutbox');
+  }
+
+  public async pushMessage({signedStates}: Message) {
+    signedStates?.map(signedState => {
+      const signature = signState(signedState, this.privateKey);
+      const {signatures, participants} = signedState;
+      const hubIdx = participants.findIndex(p => p.signingAddress === this.getAddress());
+      signatures[hubIdx] = signature;
+
+      this._eventEmitter.emit('addToOutbox', {
+        signedStates: [{...signedState, signatures}],
+        from: 'hub'
+      });
+    });
+  }
+
+  public getAddress() {
+    return new ethers.Wallet(this.privateKey).address;
+  }
+}
