4838176813c8ebd6e5286cd701ad949fd66aeca2
George Knee <georgeknee@googlemail.com>
2020-03-02 11:21:41 +0000

    Move ConnectionBanner to top of window
    And scrollTo it so it can be seen

--- packages/web3torrent/src/components/layout/layout-header/LayoutHeader.tsx
+++ packages/web3torrent/src/components/layout/layout-header/LayoutHeader.tsx
@@ -12,26 +12,31 @@ interface Props {
   requiredNetwork: number;
 }
 
-const LayoutHeader: React.FC<RouteComponentProps & Props> = props => {
-  return (
-    <header className="header">
-      <nav className="header-content">
-        <Link className="header-logo" to={RoutePath.Root}>
-          <span className="header-logo-hidden">Web3Torrent Logo - Go to Home</span>
-        </Link>
-        <div className="actions-container">
-          <FormButton name="upload" onClick={() => props.history.push(RoutePath.Upload)}>
-            Upload
-          </FormButton>
-        </div>
-      </nav>
-      <ConnectionBanner
-        currentNetwork={props.currentNetwork}
-        requiredNetwork={props.requiredNetwork}
-        onWeb3Fallback={!('ethereum' in window)}
-      />
-    </header>
-  );
-};
+class LayoutHeader extends React.Component<RouteComponentProps & Props> {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+  render() {
+    return (
+      <header className="header">
+        <ConnectionBanner
+          currentNetwork={this.props.currentNetwork}
+          requiredNetwork={this.props.requiredNetwork}
+          onWeb3Fallback={!('ethereum' in window)}
+        />
+        <nav className="header-content">
+          <Link className="header-logo" to={RoutePath.Root}>
+            <span className="header-logo-hidden">Web3Torrent Logo - Go to Home</span>
+          </Link>
+          <div className="actions-container">
+            <FormButton name="upload" onClick={() => this.props.history.push(RoutePath.Upload)}>
+              Upload
+            </FormButton>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
 
 export {LayoutHeader};
