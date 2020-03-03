b92b99241974236f3bcd7e66e8d690108871bbed
George Knee <georgeknee@googlemail.com>
2020-03-03 00:53:03 +0000

    Rename fields in ChannelList component

--- packages/web3torrent/src/components/channel-list/ChannelList.tsx
+++ packages/web3torrent/src/components/channel-list/ChannelList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ChannelContext} from '../../clients/web3t-channel-client';
+import {ChannelContext, ChannelState} from '../../clients/web3t-channel-client';
 import './ChannelList.scss';
 import {formatUnits} from 'ethers/utils';
 
@@ -8,42 +8,40 @@ class ChannelList extends React.Component {
   render() {
     return (
       <div className="channel-list">
-        <h2> Receiving Channels</h2>
-        <table className="channel-list-table">
-          <tbody>
-            <tr>
-              <td className={'channel-header'}>Channel ID</td>
-              <td className={'channel-header'}>Seeder</td>
-              <td className={'channel-header'}>Balance</td>
-            </tr>
-            {this.context.openChannels
-              .filter(channel => channel.leecher === this.context.myAddress)
-              .map(channel => (
-                <tr className={'channel'} key={channel.channelId}>
-                  <td className="channel-address">{channel.channelId}</td>
-                  <td className="channel-address">{channel.seeder}</td>
-                  <td className="amount-cell">{formatUnits(channel.seederBalance, 'ether')} ETH</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-        <h2> Outlaying Channels</h2>
+        <h2> Seeding Channels</h2>
         <table className="channel-list-table">
           <tbody>
             <tr>
               <td className={'channel-header'}>Channel ID</td>
               <td className={'channel-header'}>Leecher</td>
-              <td className={'channel-header'}>Balance</td>
+              <td className={'channel-header'}>Earnt</td>
             </tr>
-            {this.context.openChannels
-              .filter(channel => channel.seeder === this.context.myAddress)
-              .map(channel => (
+            {Object.values(this.context.openChannels)
+              .filter((channel: ChannelState) => channel.seeder === this.context.mySigningAddress)
+              .map((channel: ChannelState) => (
                 <tr className={'channel'} key={channel.channelId}>
                   <td className="channel-address">{channel.channelId}</td>
                   <td className="channel-address">{channel.leecher}</td>
-                  <td className="amount-cell">
-                    {formatUnits(channel.leecherBalance, 'ether')} ETH
-                  </td>
+                  <td className="amount-cell">{formatUnits(channel.seederBalance, 'wei')} wei</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <h2> Leeching Channels</h2>
+        <table className="channel-list-table">
+          <tbody>
+            <tr>
+              <td className={'channel-header'}>Channel ID</td>
+              <td className={'channel-header'}>Seeder</td>
+              <td className={'channel-header'}>Left to spend</td>
+            </tr>
+            {Object.values(this.context.openChannels)
+              .filter((channel: ChannelState) => channel.leecher === this.context.mySigningAddress)
+              .map((channel: ChannelState) => (
+                <tr className={'channel'} key={channel.channelId}>
+                  <td className="channel-address">{channel.channelId}</td>
+                  <td className="channel-address">{channel.seeder}</td>
+                  <td className="amount-cell">{formatUnits(channel.leecherBalance, 'wei')} wei</td>
                 </tr>
               ))}
           </tbody>
