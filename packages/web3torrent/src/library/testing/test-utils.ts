046134598fc7483cfcfbdd0ec6eb626afb04941d
George Knee <georgeknee@googlemail.com>
2020-03-03 18:15:39 +0000

    Add mockMetamask test util

--- packages/web3torrent/src/library/testing/test-utils.ts
+++ packages/web3torrent/src/library/testing/test-utils.ts
@@ -1,5 +1,6 @@
 import MemoryChunkStore from 'memory-chunk-store';
 import fixtures from 'webtorrent-fixtures';
+import {ChannelState} from '../../clients/web3t-channel-client';
 
 export const defaultFile = new Blob([fixtures.leaves.content]);
 export const defaultTorrentHash = fixtures.leaves.parsedTorrent.infoHash;
@@ -15,4 +16,31 @@ export const defaultSeedingOptions = (randomizeName = true) => ({
 export const defaultLeechingOptions = {
   name: 'Leaves of Grass by Walt Whitman.epub',
   store: MemoryChunkStore
+};
+
+export function mockMetamask() {
+  async function enable() {
+    return new Promise(r => r());
+  }
+  const ethereum = {
+    enable
+  };
+  // mock out window.ethereum.enable
+  Object.defineProperty(window, 'ethereum', {
+    enumerable: true,
+    value: ethereum
+  });
+}
+
+export const mockChannelState: ChannelState = {
+  channelId: '0x0',
+  turnNum: '0x0',
+  status: 'running',
+  challengeExpirationTime: '0x0',
+  seeder: '0x0',
+  leecher: '0x0',
+  seederOutcomeAddress: '0x0',
+  leecherOutcomeAddress: '0x0',
+  seederBalance: '0x0',
+  leecherBalance: '0x0'
 };
