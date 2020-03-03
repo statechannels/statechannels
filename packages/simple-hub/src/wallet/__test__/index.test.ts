e051984939a929fb06ebdcd62790613dace176f9
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-03 08:05:03 -0800

    Require json files instead of importing

--- packages/simple-hub/src/wallet/__test__/index.test.ts
+++ packages/simple-hub/src/wallet/__test__/index.test.ts
@@ -1,7 +1,10 @@
-import ledgerMessage from './ledger-message.json';
-import ledgerResponse from './ledger-response.json';
 import {respondToMessage} from '../';
 import {Message} from '@statechannels/wire-format';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ledgerMessage = require('./ledger-message.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ledgerResponse = require('./ledger-response.json');
 
 it('Echo message with signature', () => {
   const response = respondToMessage(ledgerMessage);
