04f7fa7341444a940018533da92c34c373c87ba2
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-03 09:16:25 -0800

    Remove chain tests

--- packages/simple-hub/jest/jest.chain.config.js
+++
@@ -1,5 +0,0 @@
-const config = require('./jest.config');
-config.testMatch = ['<rootDir>/src/**/__chain-test__/?(*.)test.ts?(x)'];
-config.globalSetup = '<rootDir>/jest/test-setup.ts';
-config.globalTeardown = '<rootDir>/jest/test-teardown.ts';
-module.exports = config;
