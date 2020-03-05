2131679ce40474baf63beab2ef8cf2889c044875
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-03-05 15:17:03 -0700

    Add HUB_DESTINATION constant

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,7 @@ export const NITRO_ADJUDICATOR_ADDRESS =
 
 // TODO: Move top ENV variable
 export const HUB_ADDRESS = process.env.HUB_ADDRESS || '0x100063c326b27f78b2cBb7cd036B8ddE4d4FCa7C';
+export const HUB_DESTINATION = HUB_ADDRESS;
 export const CHALLENGE_DURATION = bigNumberify(0x12c); // 5 minutes
 export const NETWORK_ID = process.env.CHAIN_NETWORK_ID || '0';
 
