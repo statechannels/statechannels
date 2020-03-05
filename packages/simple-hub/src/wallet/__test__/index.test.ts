d1983217abd7b74cce1e642311f63eaf126c353b
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-04 16:54:29 -0800

    Add 3 participant echo test

--- packages/simple-hub/src/wallet/__test__/index.test.ts
+++ packages/simple-hub/src/wallet/__test__/index.test.ts
@@ -1,19 +1,49 @@
 import {respondToMessage} from '../';
 import {Message} from '@statechannels/wire-format';
 import {serializeMessage} from '@statechannels/xstate-wallet/lib/src/serde/wire-format/serialize';
-import {ledgerState1, participants, ledgerState2} from '../test-helpers';
+import {
+  ledgerStateMessage,
+  participants,
+  ledgerStateResponse,
+  ledgerStateMessage3,
+  ledgerStateResponse3
+} from '../test-helpers';
 
-it('Echo message with signature', () => {
-  const ledgerMessage = serializeMessage(
-    {signedStates: [ledgerState1]},
-    participants[1].participantId,
-    participants[0].participantId
-  );
-  const response = respondToMessage(ledgerMessage);
-  const expectedResponse = serializeMessage(
-    {signedStates: [ledgerState2]},
-    participants[0].participantId,
-    participants[1].participantId
-  );
-  expect(response).toMatchObject<Message[]>([expectedResponse]);
+describe('ledger state', () => {
+  it('Echo message with signature', () => {
+    const ledgerMessage = serializeMessage(
+      {signedStates: [ledgerStateMessage]},
+      participants[1].participantId,
+      participants[0].participantId
+    );
+    const response = respondToMessage(ledgerMessage);
+    const expectedResponse = serializeMessage(
+      {signedStates: [ledgerStateResponse]},
+      participants[0].participantId,
+      participants[1].participantId
+    );
+    expect(response).toMatchObject<Message[]>([expectedResponse]);
+  });
+
+  it('Echo message with signature with 3 participants', () => {
+    const ledgerMessage = serializeMessage(
+      {signedStates: [ledgerStateMessage3]},
+      participants[1].participantId,
+      participants[0].participantId
+    );
+    const response = respondToMessage(ledgerMessage);
+    const expectedResponses = [
+      serializeMessage(
+        {signedStates: [ledgerStateResponse3]},
+        participants[0].participantId,
+        participants[1].participantId
+      ),
+      serializeMessage(
+        {signedStates: [ledgerStateResponse3]},
+        participants[2].participantId,
+        participants[1].participantId
+      )
+    ];
+    expect(response).toMatchObject<Message[]>(expectedResponses);
+  });
 });
