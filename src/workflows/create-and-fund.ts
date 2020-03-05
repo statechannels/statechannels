daf91320e29be65995399a2f8aff397b2bf8285c
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-03-05 09:08:30 -0700

    Refactor create-and-fund a bit more to get some type safety

--- src/workflows/create-and-fund.ts
+++ src/workflows/create-and-fund.ts
@@ -3,13 +3,13 @@ import {Machine, MachineConfig} from 'xstate';
 import {SimpleAllocation} from '../store/types';
 import * as AdvanceChannel from './advance-channel';
 
-import {MachineFactory, getDataAndInvoke} from '../utils/workflow-utils';
+import {MachineFactory} from '../utils/workflow-utils';
 import {Store} from '../store';
 import {bigNumberify} from 'ethers/utils';
 import * as Depositing from './depositing';
 import {add} from '../utils/math-utils';
 import {isSimpleEthAllocation} from '../utils/outcome';
-import {checkThat} from '../utils';
+import {checkThat, getDataAndInvoke} from '../utils';
 const PROTOCOL = 'create-and-fund';
 
 export enum Indices {
@@ -36,7 +36,11 @@ const preFundSetup = {
   }
 };
 
-const depositing = getDataAndInvoke('getDepositingInfo', 'invokeDepositing', 'postFundSetup');
+const depositing = getDataAndInvoke<Context, Service>(
+  {src: 'getDepositingInfo'},
+  {src: 'depositing'},
+  'postFundSetup'
+);
 
 const postFundSetup = {
   invoke: {
@@ -59,31 +63,33 @@ export const config: MachineConfig<Context, any, any> = {
   }
 };
 
+const services = (store: Store) => ({
+  depositing: Depositing.machine(store),
+  advanceChannel: AdvanceChannel.machine(store),
+  getDepositingInfo: getDepositingInfo(store)
+});
+type Service = keyof ReturnType<typeof services>;
+
+const options = (store: Store) => ({services: services(store)});
+
 export const machine: MachineFactory<Init, any> = (store: Store, init: Init) => {
-  async function getDepositingInfo({allocation, channelId}: Init): Promise<Depositing.Init> {
-    const {supported, myIndex} = await store.getEntry(channelId);
-    const outcome = checkThat(supported?.outcome, isSimpleEthAllocation);
+  return Machine(config).withConfig(options(store), init);
+};
 
-    const fundedAt = outcome.allocationItems.map(a => a.amount).reduce(add);
-    let depositAt = bigNumberify(0);
-    for (let i = 0; i < allocation.allocationItems.length; i++) {
-      const {amount} = allocation.allocationItems[i];
-      if (i !== myIndex) depositAt = depositAt.add(amount);
-      else {
-        const totalAfterDeposit = depositAt.add(amount);
-        return {channelId, depositAt, totalAfterDeposit, fundedAt};
-      }
+const getDepositingInfo = (store: Store) => async ({channelId}: Init): Promise<Depositing.Init> => {
+  const {supported, myIndex} = await store.getEntry(channelId);
+  const {allocationItems} = checkThat(supported?.outcome, isSimpleEthAllocation);
+
+  const fundedAt = allocationItems.map(a => a.amount).reduce(add);
+  let depositAt = bigNumberify(0);
+  for (let i = 0; i < allocationItems.length; i++) {
+    const {amount} = allocationItems[i];
+    if (i !== myIndex) depositAt = depositAt.add(amount);
+    else {
+      const totalAfterDeposit = depositAt.add(amount);
+      return {channelId, depositAt, totalAfterDeposit, fundedAt};
     }
-
-    throw Error(`Could not find an allocation for participant id ${myIndex}`);
   }
 
-  const services = {
-    invokeDepositing: Depositing.machine(store),
-    advanceChannel: AdvanceChannel.machine(store),
-    getDepositingInfo
-  };
-
-  const options = {services};
-  return Machine(config).withConfig(options, init);
+  throw Error(`Could not find an allocation for participant id ${myIndex}`);
 };
