f51d333bbe7d9af625cdc6b47ede6e51f4433629
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-06 14:06:09 -0800

    simple-hub: use observables to listen to firebase

--- packages/simple-hub/src/server.ts
+++ packages/simple-hub/src/server.ts
@@ -7,7 +7,7 @@ if (process.env.RUNTIME_ENV) {
   });
 }
 import {logger} from './logger';
-import {fbListen, fbSend} from './message/firebase-relay';
+import {fbListen, fbSend, Snapshot} from './message/firebase-relay';
 import {Message} from '@statechannels/wire-format';
 import {respondToMessage} from './wallet';
 import {assetHolderListen} from './blockchain/asset-holder-watcher';
@@ -15,22 +15,24 @@ import {onDepositEvent} from './wallet/deposit';
 
 const log = logger();
 
-export async function startServer(): Promise<any> {
-  const fbMessageCallback = async (incomingMessage: Message) => {
-    log.info({incomingMessage}, 'Received message from firebase');
+async function fbOnSnapshot(snapshot: Snapshot) {
+  const incomingMessage: Message = snapshot.val();
+  log.info({incomingMessage}, 'Received message from firebase');
 
-    const outgoingMessage = respondToMessage(incomingMessage);
-    try {
-      // log.info({message: outgoingMessages}, 'Sending message to firebase');
-      await Promise.all(outgoingMessage.map(fbSend));
-    } catch (reason) {
-      log.error(reason);
-      throw reason;
-    }
-  };
+  const outgoingMessage = respondToMessage(incomingMessage);
+  try {
+    // log.info({message: outgoingMessages}, 'Sending message to firebase');
+    await Promise.all(outgoingMessage.map(fbSend));
+  } catch (reason) {
+    log.error(reason);
+    throw reason;
+  }
+  return snapshot;
+}
 
-  fbListen(fbMessageCallback);
-  return assetHolderListen(onDepositEvent);
+export async function startServer() {
+  await assetHolderListen(onDepositEvent);
+  fbListen(fbOnSnapshot);
 }
 
 if (require.main === module) {
