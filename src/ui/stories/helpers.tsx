c4a836876e42b8401405916728267caa27280806
Alex Gap <alex.gap@consensys.net>
2020-03-08 22:06:44 -0700

    WIP

--- src/ui/stories/helpers.tsx
+++ src/ui/stories/helpers.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import {Wallet} from '../wallet';
-import {Image} from 'rimble-ui';
-import fakeApp from '../../images/fake-app.png';
 
-export function renderWalletInFrontOfApp(machine) {
+import fakeApp from '../../images/fake-app.png';
+import {Modal, Card, Flex, Image} from 'rimble-ui';
+
+export function renderComponentInFrontOfApp(component) {
   function renderFunction() {
     return (
       <div>
         <Image src={fakeApp} />
-        <Wallet workflow={machine} />
+        <Modal isOpen={true}>
+          <Card width={'320px'} height={'450px'}>
+            <Flex px={[3, 3, 4]} height={3} borderBottom={1} borderColor={'#E8E8E8'} mt={'0.8'}>
+              <Image alt="State Channels" borderRadius={8} height="auto" src={logo} />
+            </Flex>
+            {component}
+          </Card>
+        </Modal>
       </div>
     );
   }
