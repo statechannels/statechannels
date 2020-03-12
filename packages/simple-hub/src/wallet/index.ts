f5a37688709d00b7ef8c8961ffbd88e475ec7b06
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-12 08:13:34 -0700

    simple-hub: add hub participantId

--- packages/simple-hub/src/wallet/index.ts
+++ packages/simple-hub/src/wallet/index.ts
@@ -1,6 +1,6 @@
 import {Message as WireMessage} from '@statechannels/wire-format';
 import * as R from 'ramda';
-import {cHubParticipantPK, cHubParticipantAddress} from '../constants';
+import {cHubChannelPK, cHubChannelSigningAddress} from '../constants';
 import {
   deserializeMessage,
   serializeMessage,
@@ -11,7 +11,7 @@ import {
 } from './xstate-wallet-internals';
 
 function containsHub(participant: Participant): boolean {
-  return participant.signingAddress === cHubParticipantAddress;
+  return participant.signingAddress === cHubChannelSigningAddress;
 }
 const notContainsHub = R.compose(R.not, containsHub);
 
@@ -28,7 +28,7 @@ export function respondToMessage(wireMessage: WireMessage): WireMessage[] {
     state => state.participants.filter(containsHub).length
   );
   const signedStates = statesWithHub.map(state => {
-    const ourSignature = signState(state, cHubParticipantPK);
+    const ourSignature = signState(state, cHubChannelPK);
     const signatures = R.append(ourSignature, state.signatures);
     return {...state, signatures};
   });
