6c789a9fe3d075aed6fc519596159f7884e49181
Junji Zhi <jzhi316@gmail.com>
2020-03-13 11:15:52 -0400

    Fix CI and rename test variables

--- packages/web3torrent/src/library/client-seeding.test.ts
+++ packages/web3torrent/src/library/client-seeding.test.ts
@@ -25,7 +25,8 @@ async function defaultClient(): Promise<WebTorrentPaidStreamingClient> {
 
 describe('Seeding and Leeching', () => {
   let seeder: WebTorrentPaidStreamingClient;
-  let leecher: WebTorrentPaidStreamingClient;
+  let leecherA: WebTorrentPaidStreamingClient;
+  let leecherB: WebTorrentPaidStreamingClient;
 
   beforeAll(() => {
     mockMetamask();
@@ -33,7 +34,8 @@ describe('Seeding and Leeching', () => {
 
   beforeEach(async () => {
     seeder = await defaultClient();
-    leecher = await defaultClient();
+    leecherA = await defaultClient();
+    leecherB = await defaultClient();
   });
 
   it('should throw when the client is not enabled', async done => {
@@ -56,15 +58,15 @@ describe('Seeding and Leeching', () => {
 
   it('should perform the extended handshake between seeder and leecher', done => {
     seeder.seed(defaultFile as File, defaultSeedingOptions(), seededTorrent => {
-      leecher.add(seededTorrent.magnetURI, {store: MemoryChunkStore}, () => {
-        expect(leecher.torrents.length).toEqual(1);
+      leecherA.add(seededTorrent.magnetURI, {store: MemoryChunkStore}, () => {
+        expect(leecherA.torrents.length).toEqual(1);
         expect(seeder.torrents[0].wires.length).toEqual(1);
-        expect(leecher.torrents[0].wires.length).toEqual(1);
+        expect(leecherA.torrents[0].wires.length).toEqual(1);
         expect(
-          leecher.torrents[0].wires.some(
+          leecherA.torrents[0].wires.some(
             wire =>
               wire.paidStreamingExtension.peerAccount === seeder.pseAccount &&
-              wire.paidStreamingExtension.pseAccount === leecher.pseAccount
+              wire.paidStreamingExtension.pseAccount === leecherA.pseAccount
           )
         ).toBe(true);
         done();
@@ -75,10 +77,10 @@ describe('Seeding and Leeching', () => {
   it('should reach a ready-for-leeching, choked state', done => {
     seeder.seed(defaultFile as File, defaultSeedingOptions(), seededTorrent => {
       seeder.once(ClientEvents.PEER_STATUS_CHANGED, ({torrentPeers}) => {
-        expect(torrentPeers[`${leecher.pseAccount}`].allowed).toEqual(false);
+        expect(torrentPeers[`${leecherA.pseAccount}`].allowed).toEqual(false);
         done();
       });
-      leecher.add(seededTorrent.magnetURI, {store: MemoryChunkStore});
+      leecherA.add(seededTorrent.magnetURI, {store: MemoryChunkStore});
     });
   }, 10000);
 
@@ -86,11 +88,11 @@ describe('Seeding and Leeching', () => {
     seeder.seed(defaultFile as File, defaultSeedingOptions(), seededTorrent => {
       seeder.once(ClientEvents.PEER_STATUS_CHANGED, ({peerAccount}) => {
         seeder.once(ClientEvents.PEER_STATUS_CHANGED, ({torrentPeers}) => {
-          expect(torrentPeers[`${leecher.pseAccount}`].allowed).toEqual(true);
+          expect(torrentPeers[`${leecherA.pseAccount}`].allowed).toEqual(true);
         });
         seeder.togglePeer(seededTorrent.infoHash, peerAccount);
 
-        leecher.once(ClientEvents.TORRENT_DONE, ({torrent: leechedTorrent}) => {
+        leecherA.once(ClientEvents.TORRENT_DONE, ({torrent: leechedTorrent}) => {
           expect(seededTorrent.files[0].done).toEqual(leechedTorrent.files[0].done);
           expect(seededTorrent.files[0].length).toEqual(leechedTorrent.files[0].length);
           expect(seededTorrent.files[0].name).toEqual(leechedTorrent.files[0].name);
@@ -98,23 +100,26 @@ describe('Seeding and Leeching', () => {
         });
       });
 
-      leecher.add(seededTorrent.magnetURI, {store: MemoryChunkStore});
+      leecherA.add(seededTorrent.magnetURI, {store: MemoryChunkStore});
     });
   }, 10000);
 
   it('should support multiple leechers finishing their downloads', async done => {
-    const leecherA = await defaultClient();
-    const leecherB = await defaultClient();
-    let finishCount = 0;
+    const knownPeers = new Set();
 
     seeder.seed(defaultFile as File, defaultSeedingOptions(), seededTorrent => {
-      seeder.once(ClientEvents.PEER_STATUS_CHANGED, ({peerAccount}) => {
-        seeder.togglePeer(seededTorrent.infoHash, peerAccount);
+      seeder.on(ClientEvents.PEER_STATUS_CHANGED, ({peerAccount}) => {
+        // Make sure we toggle peers only twice
+        if (!knownPeers.has(peerAccount)) {
+          knownPeers.add(peerAccount);
+          seeder.togglePeer(seededTorrent.infoHash, peerAccount);
+        }
       });
       leecherA.add(seededTorrent.magnetURI, {store: MemoryChunkStore});
       leecherB.add(seededTorrent.magnetURI, {store: MemoryChunkStore});
     });
 
+    let finishCount = 0;
     async function downloadFinished() {
       finishCount += 1;
       if (finishCount == 2) {
@@ -128,6 +133,7 @@ describe('Seeding and Leeching', () => {
 
   afterEach(() => {
     seeder.destroy();
-    leecher.destroy();
+    leecherA.destroy();
+    leecherB.destroy();
   });
 });
