8c33d063fcac113f5ac9943d0cd8bebb95b03b68
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-13 12:11:45 -0700

    simple-hub-deposit: Refactor chain test to avoid callback

--- packages/simple-hub/src/blockchain/__chain-test__/deposit.test.ts
+++ packages/simple-hub/src/blockchain/__chain-test__/deposit.test.ts
@@ -1,5 +1,5 @@
 import {bigNumberify} from 'ethers/utils';
-import {assetHolderObservable, AssetHolderEvent} from '../asset-holder-watcher';
+import {assetHolderObservable} from '../asset-holder-watcher';
 import {Blockchain} from '../transaction';
 import {first} from 'rxjs/operators';
 
@@ -9,17 +9,11 @@ const five = bigNumberify(5);
 
 const channelId = '0x1823994d6d3b53b82f499c1aca2095b94108ba3ff59f55c6e765da1e24874ab2';
 
-// eslint-disable-next-line jest/no-test-callback
-test('handles deposit event', async done => {
-  const eventSubscriber = (message: AssetHolderEvent) => {
-    expect(message.channelId).toEqual(channelId);
-    expect(message.amountDeposited).toEqual(five);
-    expect(message.destinationHoldings).toEqual(five);
-    done();
-  };
-
-  (await assetHolderObservable()).pipe(first()).subscribe(eventSubscriber, e => {
-    throw e;
-  });
+test('handles deposit event', async () => {
+  const messagePromise = (await assetHolderObservable()).pipe(first()).toPromise();
   await Blockchain.fund(channelId, zero, five);
+  const message = await messagePromise;
+  expect(message.channelId).toEqual(channelId);
+  expect(message.amountDeposited).toEqual(five);
+  expect(message.destinationHoldings).toEqual(five);
 });
