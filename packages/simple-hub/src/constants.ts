f0912841d649cf384838032199ea760f1a1755c5
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-16 16:12:10 -0700

    simple-hub: add production mimicking local testing

--- packages/simple-hub/src/constants.ts
+++ packages/simple-hub/src/constants.ts
@@ -12,4 +12,4 @@ export const cHubChainPK =
   process.env.HUB_CHAIN_PK || '0x7ab741b57e8d94dd7e1a29055646bafde7010f38a900f55bbd7647880faa6ee8';
 export const cHubChainAddress = new ethers.Wallet(cHubChainPK).address;
 
-export const cHubParticipantId = process.env.HUB_PARTICIPANT_ID || 'firebase.simple-hub';
+export const cHubParticipantId = process.env.HUB_PARTICIPANT_ID || 'firebase:simple-hub';
