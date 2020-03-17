a00be464cfcb737c0d95745146801a0c5beb781f
Alon Bukai <alon@camilyo.com>
2020-03-17 19:18:49 +0200

    feat: Create Firebasestore Adapter and Serializer

---
+++ packages/tic-tac-toe/app/serializers/application.ts
@@ -0,0 +1,27 @@
+import DS from 'ember-data';
+import FirestoreSerializer from 'emberfire/serializers/firestore';
+import {singularize} from 'ember-inflector';
+
+export default class ApplicationSerializer extends FirestoreSerializer {
+  normalizeCreateRecordResponse(
+    _store: DS.Store,
+    _primaryModelClass: DS.Model,
+    payload: any,
+    id: string
+  ): {data: {id: string; attributes: object; type: string}} {
+    return {
+      data: {
+        id: id || payload.doc.id,
+        attributes: payload.data,
+        type: singularize(payload.doc.parent.id)
+      }
+    };
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your serializers.
+declare module 'ember-data/types/registries/serializer' {
+  export default interface SerializerRegistry {
+    application: ApplicationSerializer;
+  }
+}
