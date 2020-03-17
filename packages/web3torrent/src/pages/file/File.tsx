c774624b35fb07b1cc70706a88983af24700825f
George Knee <georgeknee@googlemail.com>
2020-03-17 14:05:23 +0000

    Remove unused import

--- packages/web3torrent/src/pages/file/File.tsx
+++ packages/web3torrent/src/pages/file/File.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useEffect, useState} from 'react';
 import {RouteComponentProps, useLocation} from 'react-router-dom';
 
 import {download, getTorrentPeers, WebTorrentContext} from '../../clients/web3torrent-client';
