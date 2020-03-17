570e51cf0c9ea67ed82880a5618d8e9e6d8f1dd4
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-17 14:51:07 -0700

    simple-hub: respond-to-message format

--- packages/simple-hub/src/wallet/respond-to-message.ts
+++ packages/simple-hub/src/wallet/respond-to-message.ts
@@ -4,9 +4,7 @@ import {Message, signState} from './xstate-wallet-internals';
 import {containsHub} from '../utils';
 
 export function respondToMessage(message: Message): Message {
-  const statesWithHub = message.signedStates.filter(
-    state => state.participants.some(containsHub)
-  );
+  const statesWithHub = message.signedStates.filter(state => state.participants.some(containsHub));
   const signedStates = statesWithHub.map(state => {
     const ourSignature = signState(state, cHubChannelPK);
     const signatures = R.append(ourSignature, state.signatures);
