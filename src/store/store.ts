42bc0112cea2c9b24f637fe23bc33602cd740ef0
Alex Gap <alex.gap@consensys.net>
2020-03-18 11:23:41 -0700

    Fix findIndex check

--- src/store/store.ts
+++ src/store/store.ts
@@ -156,7 +156,9 @@ export class XstateStore implements Store {
   private async initializeChannel(state: State): Promise<MemoryChannelStoreEntry> {
     const addresses = state.participants.map(x => x.signingAddress);
     const privateKeys = await this.backend.privateKeys();
-    const myIndex = addresses.findIndex(async address => !!privateKeys[address]);
+    const myIndex = addresses.findIndex(address => {
+      return !!privateKeys[address];
+    });
     if (myIndex === -1) {
       throw new Error("Couldn't find the signing key for any participant in wallet.");
     }
