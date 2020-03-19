6b316a8a43fb3784bb564f4855034f8f3f06e346
George Knee <georgeknee@googlemail.com>
2020-03-19 20:29:41 +0000

    Tweak comment spacing

--- packages/client-api-schema/src/types.ts
+++ packages/client-api-schema/src/types.ts
@@ -184,15 +184,14 @@ export type CloseAndWithdrawParams = {site: string; player: Participant; hub: Pa
 export type CloseAndWithdrawRequest = JsonRpcRequest<'CloseAndWithdraw', CloseAndWithdrawParams>;
 export type CloseAndWithdrawResponse = JsonRpcResponse<{success: boolean}>;
 // Notifications
+// these notifications come *from* the wallet, which is not strictly how JSON-RPC should work
+// (since we treat the wallet as the 'server')
 export type ChannelProposedNotification = JsonRpcNotification<'ChannelProposed', ChannelResult>;
 export type ChannelUpdatedNotification = JsonRpcNotification<'ChannelUpdated', ChannelResult>;
 export type ChannelClosingNotification = JsonRpcNotification<'ChannelClosed', ChannelResult>;
 export type MessageQueuedNotification = JsonRpcNotification<'MessageQueued', Message>;
 export type BudgetUpdatedNotification = JsonRpcNotification<'BudgetUpdated', SiteBudget>;
-
 export type UiNotification = JsonRpcNotification<'UIUpdate', {showWallet: boolean}>;
-// these notifications come *from* the wallet, which is not strictly how JSON-RPC should work
-// (since we treat the wallet as the 'server')
 
 export type Notification =
   | ChannelProposedNotification
