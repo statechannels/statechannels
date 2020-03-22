966e6106158118b4d4ddf1cb470106f8c60b1880
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-03-21 20:18:20 -0700

    Remove ledger defunding from conclude workflow.
    Since we are not using ledger funding to fund channels, we don't need
    this .

--- src/workflows/conclude-channel.ts
+++ src/workflows/conclude-channel.ts
@@ -1,12 +1,9 @@
 import {Machine} from 'xstate';
-import * as SupportState from './support-state';
 import {getDataAndInvoke, MachineFactory} from '../utils/workflow-utils';
 import {Store} from '../store';
 import {outcomesEqual} from '../store/state-utils';
-import {isIndirectFunding} from '../store/memory-store';
-import {add} from '../utils/math-utils';
-import {checkThat} from '../utils';
-import {isSimpleEthAllocation, simpleEthAllocation} from '../utils/outcome';
+import {SupportState} from '.';
+
 const WORKFLOW = 'conclude-channel';
 
 export interface Init {
@@ -19,7 +16,6 @@ const determineFundingType = {on: {'': 'success'}};
 const concludeTarget = {
   ...getDataAndInvoke('getFinalState', 'supportState', 'determineFundingType')
 };
-const ledgerDefunding = getDataAndInvoke('getDefundedLedgerState', 'supportState', 'success');
 
 const virtualDefunding = {
   initial: 'start',
@@ -55,7 +51,6 @@ export const config = {
   states: {
     concludeTarget,
     virtualDefunding,
-    ledgerDefunding,
     determineFundingType,
     success: {type: 'final' as 'final'}
   }
@@ -64,7 +59,6 @@ export const config = {
 export const mockOptions = {
   guards: {
     virtuallyFunded: _ => true,
-    indirectlyFunded: _ => true,
     directlyFunded: _ => true
   }
 };
@@ -91,42 +85,8 @@ export const machine: MachineFactory<Init, any> = (store: Store, ctx: Init) => {
     };
   }
 
-  async function getDefundedLedgerState({channelId}: Init): Promise<SupportState.Init> {
-    const funding = checkThat((await store.getEntry(channelId)).funding, isIndirectFunding);
-    const {supported: targetChannelState} = await store.getEntry(channelId);
-    const {outcome: concludedOutcome} = targetChannelState;
-    if (!targetChannelState.isFinal) throw 'Target channel not finalized';
-
-    const {supported: ledgerState, channelConstants} = await store.getEntry(funding.ledgerId);
-    if (!isSimpleEthAllocation(ledgerState.outcome) || !isSimpleEthAllocation(concludedOutcome)) {
-      throw new Error('Only SimpleEthAllocations are currently supported');
-    }
-    const allocation = ledgerState.outcome.allocationItems;
-    const idx = allocation.findIndex(({destination}) => destination === channelId);
-
-    if (
-      ledgerState.outcome.allocationItems[idx]?.amount !==
-      concludedOutcome.allocationItems.map(a => a.amount).reduce(add)
-    ) {
-      // TODO: What should we do here?
-      throw 'Target channel underfunded';
-    }
-
-    allocation.splice(idx, 1).push(...concludedOutcome.allocationItems);
-
-    return {
-      state: {
-        ...channelConstants,
-        ...ledgerState,
-        turnNum: ledgerState.turnNum.add(1),
-        outcome: simpleEthAllocation(allocation)
-      }
-    };
-  }
-
   const services = {
     getFinalState,
-    getDefundedLedgerState,
     supportState: SupportState.machine(store)
   };
 
