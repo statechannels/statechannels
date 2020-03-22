d360ef2ad283cf186c9550007a36e9742632a13e
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-03-21 18:12:34 -0700

    Allocate to hub first when creating ledger channel

--- src/workflows/index.ts
+++ src/workflows/index.ts
@@ -4,6 +4,7 @@ import * as LedgerFunding from './ledger-funding';
 import * as VirtualFundingAsLeaf from './virtual-funding-as-leaf';
 import * as VirtualFundingAsHub from './virtual-funding-as-hub';
 import * as CreateAndFund from './create-and-fund';
+import * as CreateAndFundLedger from './create-and-fund-ledger';
 import * as VirtualDefundingAsLeaf from './virtual-defunding-as-leaf';
 import * as VirtualDefundingAsHub from './virtual-defunding-as-hub';
 
@@ -15,5 +16,6 @@ export {
   VirtualFundingAsLeaf,
   VirtualFundingAsHub,
   VirtualDefundingAsLeaf,
-  VirtualDefundingAsHub
+  VirtualDefundingAsHub,
+  CreateAndFundLedger
 };
