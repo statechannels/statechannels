985bfa524a732e6ccbe7f64d964ab5d057f36739
Tom Close <tom.close@cantab.net>
2020-03-24 17:02:21 +0000

    First screen for close-ledger-and-fund

--- packages/xstate-wallet/src/ui/stories/helpers.tsx
+++ packages/xstate-wallet/src/ui/stories/helpers.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 
 import fakeApp from '../../images/fake-app.png';
-import {Modal, Card, Flex, Image} from 'rimble-ui';
-import logo from '../../images/logo.svg';
+import {Image} from 'rimble-ui';
+import {Layout} from '../layout';
 
 export function renderComponentInFrontOfApp(component) {
   function renderFunction() {
     return (
       <div>
         <Image src={fakeApp} />
-        <Modal isOpen={true}>
-          <Card width={'320px'} height={'450px'}>
-            <Flex px={[3, 3, 4]} height={3} borderBottom={1} borderColor={'#E8E8E8'} mt={'0.8'}>
-              <Image alt="State Channels" borderRadius={8} height="auto" src={logo} />
-            </Flex>
-            {component}
-          </Card>
-        </Modal>
+        <Layout>{component}</Layout>
       </div>
     );
   }
