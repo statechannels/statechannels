3913c69a38350d20b2b88c0eed2e7ee0e89a52e7
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-03-31 07:45:23 -0600

    Remove unused code from ConfirmCreateChannel workflow.

--- src/workflows/confirm-create-channel.ts
+++ src/workflows/confirm-create-channel.ts
@@ -1,34 +1,11 @@
-import {MachineConfig, Action, StateSchema, Machine, Condition, StateMachine, State} from 'xstate';
-import {Participant} from '@statechannels/client-api-schema';
-import {createMockGuard} from '../utils';
-import {Store} from '../store';
-import {SimpleAllocation} from '../store/types';
-import {BigNumber} from 'ethers/utils';
-import {MessagingServiceInterface} from '../messaging';
+import {MachineConfig, StateSchema, Machine, StateMachine, State} from 'xstate';
 
-interface WorkflowActions {
-  hideUi: Action<WorkflowContext, any>;
-  displayUi: Action<WorkflowContext, any>;
-}
-interface WorkflowGuards {
-  noBudget: Condition<WorkflowContext, WorkflowEvent>;
-}
-// While this context info may not be used by the workflow
-// it may be used when displaying a UI
-export interface WorkflowContext {
-  participants: Participant[];
-  outcome: SimpleAllocation;
-  appDefinition: string;
-  appData: string;
-  chainId: string;
-  challengeDuration: BigNumber;
-}
+export type WorkflowContext = {};
 
 interface WorkflowStateSchema extends StateSchema<WorkflowContext> {
+  initial: 'waitForUserConfirmation';
   states: {
-    needUserConfirmation: {};
     waitForUserConfirmation: {};
-    // TODO: Is it possible to type these as type:'final' ?
     done: {};
     failure: {};
   };
@@ -46,66 +23,15 @@ interface UserRejects {
 }
 type WorkflowEvent = UserApproves | UserRejects;
 
-const generateConfig = (
-  actions: WorkflowActions,
-  guards: WorkflowGuards
-): MachineConfig<WorkflowContext, WorkflowStateSchema, WorkflowEvent> => ({
+export const config: MachineConfig<WorkflowContext, WorkflowStateSchema, WorkflowEvent> = {
   id: 'confirm-create-channel',
-  initial: 'needUserConfirmation',
+  initial: 'waitForUserConfirmation',
   states: {
-    needUserConfirmation: {
-      on: {
-        '': [
-          {
-            target: 'waitForUserConfirmation',
-            cond: guards.noBudget
-          },
-          {
-            target: 'done'
-          }
-        ]
-      }
-    },
-    waitForUserConfirmation: {
-      on: {
-        USER_APPROVES: {target: 'done'},
-        USER_REJECTS: {target: 'failure', actions: [actions.hideUi]}
-      }
-    },
+    waitForUserConfirmation: {on: {USER_APPROVES: 'done', USER_REJECTS: 'failure'}},
     done: {type: 'final', data: context => context},
-    failure: {type: 'final'}
+    failure: {}
   }
-});
-
-const mockActions: WorkflowActions = {
-  hideUi: 'hideUi',
-  displayUi: 'displayUi'
 };
-const mockGuards = {
-  noBudget: createMockGuard('noBudget')
-};
-export const mockOptions = {actions: mockActions, guards: mockGuards};
-export const mockConfig = generateConfig(mockActions, mockGuards);
-const guards = {noBudget: () => true};
-export const config = generateConfig(mockActions, guards);
 
-export const confirmChannelCreationWorkflow = (
-  _store: Store,
-  messagingService: MessagingServiceInterface,
-  context: WorkflowContext
-): WorkflowMachine => {
-  const actions = {
-    // TODO: We should probably set up some standard actions for all workflows
-    displayUi: () => {
-      messagingService.sendDisplayMessage('Show');
-    },
-    hideUi: () => {
-      messagingService.sendDisplayMessage('Hide');
-    }
-  };
-  return Machine(config).withConfig({actions}, context) as WorkflowMachine;
-};
-// TODO: Once budgets are a thing this should check for a budget
-// TODO: We shouldn't need to cast this but some xstate typing is not lining up around stateSchema
-
-export type WorkflowMachine = StateMachine<WorkflowContext, StateSchema, WorkflowEvent, any>;
+export const workflow = (ctx: WorkflowContext): WorkflowMachine => Machine(config).withContext(ctx);
+export type WorkflowMachine = StateMachine<WorkflowContext, any, WorkflowEvent, any>;
