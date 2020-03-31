b0a7d4889aedb97ef4be99838919250da3291835
Mike Kerzhner <mikerzhner@gmail.com>
2020-03-31 13:37:19 -0700

    xstate-wallet: do not generate source map when building production

--- webpack-build.config.js
+++ webpack-build.config.js
@@ -2,5 +2,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  devtool: ''
 });
