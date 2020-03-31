9b822b70b01b0f1a2deea10cf9fba5e7e3b0ce72
George Knee <georgeknee@googlemail.com>
2020-03-31 14:22:01 +0100

    Use betterAjvErrors

--- packages/client-api-schema/src/parse-request.ts
+++ packages/client-api-schema/src/parse-request.ts
@@ -2,40 +2,24 @@
 // otherwise we force all consumers of the package to set esModuleInterop to true
 import Ajv = require('ajv');
 
+// You need to pass `jsonPointers: true`
+const ajv = new Ajv({jsonPointers: true});
+import betterAjvErrors from 'better-ajv-errors';
+
 // eslint-disable-next-line
 const apiSchema = require('./generated-schema.json'); // because https://github.com/TypeStrong/ts-loader/issues/905
 import {Request} from './types.js';
 
-const ajv = new Ajv();
 ajv.addSchema(apiSchema, 'api.json');
-
-function prettyPrintError(e: Ajv.ErrorObject): string {
-  switch (e.keyword) {
-    case 'additionalProperties': {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const unexpected = (e.params as any).additionalProperty;
-      return `Unexpected property '${unexpected}' found at root${e.dataPath} `;
-    }
-    case 'required': {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const missing = (e.params as any).missingProperty;
-      return `Missing required property '${missing}' at root${e.dataPath}`;
-    }
-    case 'type':
-    case 'pattern': {
-      return `Property at root${e.dataPath} ${e.message}`;
-    }
-  }
-  return JSON.stringify(e);
-}
 
 export const validateRequest = ajv.compile({$ref: 'api.json#/definitions/Request'});
 
 export function parseRequest(jsonBlob: object): Request {
   const valid = validateRequest(jsonBlob);
   if (!valid) {
-    const errorMessages = validateRequest.errors?.map(e => prettyPrintError(e)).join(`;\n`);
-    throw new Error(`Validation Error: ${errorMessages}`);
+    const output = betterAjvErrors(apiSchema, jsonBlob, validateRequest.errors);
+    console.error(output);
+    throw new Error(`Validation Error`);
   }
   return jsonBlob as Request;
 }
