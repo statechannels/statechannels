34de2514a85c1a7a3683feebd76a2e3b196448dd
Junji Zhi <jzhi316@gmail.com>
2020-03-31 20:06:37 -0400

    Implement an end-to-end test for web3torrent with Puppeteer
    The test covers one uploader and one downloader, covering file upload, opening
    and closing channels.

--- packages/e2e-tests/puppeteer/__tests__/web3torrent.test.ts
+++ packages/e2e-tests/puppeteer/__tests__/web3torrent.test.ts
@@ -2,9 +2,9 @@
 import {Page, Browser} from 'puppeteer';
 import {configureEnvVariables, getEnvBool} from '@statechannels/devtools';
 
-import {setUpBrowser, loadWeb3App} from '../helpers';
+import {setUpBrowser, loadWeb3App, waitAndOpenChannel, waitForClosingChannel} from '../helpers';
 
-import {seederUploadsAFile, startDownload, cancelDownload} from '../scripts/web3torrent';
+import {uploadFile, startDownload, cancelDownload} from '../scripts/web3torrent';
 
 jest.setTimeout(200_000);
 
@@ -13,25 +13,20 @@ const HEADLESS = getEnvBool('HEADLESS');
 
 let browserA: Browser;
 let browserB: Browser;
-let browserC: Browser;
 let web3tTabA: Page;
 let web3tTabB: Page;
-let web3tTabC: Page;
 
-describe('Multiple downloaders', () => {
+describe('Supports torrenting among peers with channels', () => {
   beforeAll(async () => {
     // 100ms sloMo avoids some undiagnosed race conditions
     browserA = await setUpBrowser(HEADLESS, 100);
     browserB = await setUpBrowser(HEADLESS, 100);
-    browserC = await setUpBrowser(HEADLESS, 100);
 
     web3tTabA = (await browserA.pages())[0];
     web3tTabB = (await browserB.pages())[0];
-    web3tTabC = (await browserC.pages())[0];
 
-    await loadWeb3App(web3tTabA, 0);
-    await loadWeb3App(web3tTabB, 0);
-    await loadWeb3App(web3tTabC, 0);
+    await loadWeb3App(web3tTabA, 0, 'file/new', true);
+    await loadWeb3App(web3tTabB, 0, '', true);
   });
 
   afterAll(async () => {
@@ -41,23 +36,23 @@ describe('Multiple downloaders', () => {
     if (browserB) {
       await browserB.close();
     }
-    if (browserC) {
-      await browserC.close();
-    }
   });
 
-  it('allows multiple peers to start torrenting', async () => {
+  it('allows peers to start torrenting', async () => {
     console.log('A uploads a file...');
-    const url = await seederUploadsAFile(web3tTabA);
+    const url = await uploadFile(web3tTabA);
 
-    console.log('B and C start downloading...');
+    console.log('B starts downloading...');
     await startDownload(web3tTabB, url);
-    await startDownload(web3tTabC, url);
 
-    console.log('B and C cancel download...');
+    await waitAndOpenChannel(web3tTabA);
+    await waitAndOpenChannel(web3tTabB);
+
+    await web3tTabB.waitFor(3000);
+    console.log('B cancels download...');
     await cancelDownload(web3tTabB);
-    await cancelDownload(web3tTabC);
 
-    // Assert uploader earned equal to sum of spent by the downloaders
+    await waitForClosingChannel(web3tTabB);
+    await waitForClosingChannel(web3tTabA);
   });
 });
--- packages/e2e-tests/puppeteer/scripts/web3torrent.ts
+++ packages/e2e-tests/puppeteer/scripts/web3torrent.ts
@@ -1,12 +1,37 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import {Page} from 'puppeteer';
-export async function seederUploadsAFile(page: Page): Promise<string> {
-  return page.url();
+
+export async function uploadFile(page: Page): Promise<string> {
+  await page.waitForSelector('input[type=file]');
+  const fileToUpload = 'file.pdf';
+
+  // https://pub.dev/documentation/puppeteer/latest/puppeteer/FileChooser-class.html
+  // Not clear why puppeteer FileChooser won't work out of box. We are doing it manually for now.
+  const inputUploadHandle = await page.$('input[type=file]');
+  await inputUploadHandle!.uploadFile(fileToUpload);
+  await inputUploadHandle!.evaluate(upload => {
+    // eslint-disable-next-line no-undef
+    upload.dispatchEvent(new Event('change', {bubbles: true}));
+  });
+
+  await page.waitFor(3000);
+
+  const downloadLink = await page.$eval(
+    '#root > main > section > section.torrentInfo > div:nth-child(2) > a',
+    a => a.getAttribute('href')
+  );
+
+  return downloadLink ? downloadLink : '';
 }
 
-export async function startDownload(page: Page): Promise<void> {
-  page;
+export async function startDownload(page: Page, url: string): Promise<void> {
+  await page.goto(url);
+  const downloadButton = '#root > main > section > button';
+  await page.waitForSelector(downloadButton);
+  await page.waitFor(2000);
+  await page.click(downloadButton);
 }
 
 export async function cancelDownload(page: Page): Promise<void> {
-  page;
+  await page.click('#root > main > section > section.downloadingInfo > button');
 }
