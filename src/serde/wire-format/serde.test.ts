ac075850f53903d4f7278f368831cfa85f103831
Alex Gap <alex.gap@consensys.net>
2020-04-02 16:13:25 -0700

    Update example and re-enable tests

--- src/serde/wire-format/serde.test.ts
+++ src/serde/wire-format/serde.test.ts
@@ -9,15 +9,12 @@ import {serializeState, serializeMessage} from './serialize';
 import {deserializeState, deserializeMessage} from './deserialize';
 import {validateState} from '@statechannels/wire-format';
 
-// TODO: Get this working
-// It looks like the signature data captured is no longer correct?
-// I'd verify this but we didn't store the private key :(
-it.skip('works for states', () => {
+it('works for states', () => {
   expect(serializeState(internalStateFormat)).toEqual(wireStateFormat);
   expect(deserializeState(wireStateFormat)).toEqual(internalStateFormat);
 });
 
-it.skip('works for a message', () => {
+it('works for a message', () => {
   const {recipient, sender} = wireMessageFormat;
   expect(serializeMessage(internalMessageFormat, recipient, sender)).toEqual(wireMessageFormat);
   expect(deserializeMessage(wireMessageFormat)).toEqual(internalMessageFormat);
