5c6da3e5d906f47875911626a53e503ac3f36dcb
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-06 11:23:42 -0700

    Refactor (trying to resolve flickering test)

--- packages/e2e-tests/puppeteer/__tests__/web3torrent.test.ts
+++ packages/e2e-tests/puppeteer/__tests__/web3torrent.test.ts
@@ -15,6 +15,7 @@ let browserA: Browser;
 let browserB: Browser;
 let web3tTabA: Page;
 let web3tTabB: Page;
+let tabs: [Page, Page];
 
 describe('Supports torrenting among peers with channels', () => {
   beforeAll(async () => {
@@ -24,6 +25,7 @@ describe('Supports torrenting among peers with channels', () => {
 
     web3tTabA = (await browserA.pages())[0];
     web3tTabB = (await browserB.pages())[0];
+    tabs = [web3tTabA, web3tTabB];
 
     await loadDapp(web3tTabA, 0, true);
     await loadDapp(web3tTabB, 0, true);
@@ -47,20 +49,21 @@ describe('Supports torrenting among peers with channels', () => {
     console.log('B starts downloading...');
     await startDownload(web3tTabB, url);
 
-    await waitAndOpenChannel(web3tTabA);
-    await waitAndOpenChannel(web3tTabB);
+    console.log('Waiting for open channels');
+    await Promise.all(tabs.map(waitAndOpenChannel));
 
     // Let the download cointinue for some time
-    await web3tTabB.waitFor(3000);
+    console.log('Downloading');
+    await web3tTabB.waitFor(1000);
+
     console.log('B cancels download');
     await cancelDownload(web3tTabB);
 
-    await waitForClosingChannel(web3tTabB);
-    await waitForClosingChannel(web3tTabA);
+    console.log('Waiting for channels to close');
+    await Promise.all(tabs.map(waitForClosingChannel));
 
     // Inject some delays. Otherwise puppeteer may read the stale amounts and fails.
-    await web3tTabA.waitFor(1500);
-    await web3tTabB.waitFor(1500);
+    await Promise.all([web3tTabA, web3tTabB].map(tab => tab.waitFor(1500)));
 
     console.log('Checking exchanged amount between downloader and uploader...');
     const earnedColumn = await web3tTabA.$('td.earned');
