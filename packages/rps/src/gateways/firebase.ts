4512e670db65254473410d46de268a3a82f5403a
Liam Horne <liam@lihorne.com>
2020-04-08 13:59:27 -0400

    Remove unnecessary Firebase config options

--- packages/rps/src/gateways/firebase.ts
+++ packages/rps/src/gateways/firebase.ts
@@ -6,11 +6,7 @@ import ReduxSagaFirebase from 'redux-saga-firebase';
 
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: `${process.env.FIREBASE_PROJECT}.firebaseapp.com`,
   databaseURL: `https://${process.env.FIREBASE_PROJECT}.firebaseio.com`,
-  projectId: process.env.FIREBASE_PROJECT,
-  storageBucket: '',
-  messagingSenderId: '913007764573',
 };
 
 const fire = firebase.initializeApp(config);
