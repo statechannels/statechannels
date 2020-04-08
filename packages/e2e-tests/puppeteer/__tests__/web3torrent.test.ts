2400ffe51a5a960b6246ae360823be41cd85e639
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-08 10:04:53 -0700

    :pig: Don't wait for the confirm-create-channel button when virtually funding
    TODO: A more robust way for waiting on the channels to be ready.

--- packages/e2e-tests/puppeteer/__tests__/web3torrent.test.ts
+++ packages/e2e-tests/puppeteer/__tests__/web3torrent.test.ts
@@ -10,7 +10,7 @@ jest.setTimeout(200_000);
 
 configureEnvVariables();
 const HEADLESS = getEnvBool('HEADLESS');
-const HANDLE_BUDGET_PROMPT = process.env.REACT_APP_FUNDING_STRATEGY === 'Virtual';
+const USES_VIRTUAL_FUNDING = process.env.REACT_APP_FUNDING_STRATEGY === 'Virtual';
 
 let browserA: Browser;
 let browserB: Browser;
@@ -49,13 +49,13 @@ describe('Supports torrenting among peers with channels', () => {
 
   it('allows peers to start torrenting', async () => {
     console.log('A uploads a file');
-    const url = await uploadFile(web3tTabA, HANDLE_BUDGET_PROMPT);
+    const url = await uploadFile(web3tTabA, USES_VIRTUAL_FUNDING);
 
     console.log('B starts downloading...');
-    await startDownload(web3tTabB, url, HANDLE_BUDGET_PROMPT);
+    await startDownload(web3tTabB, url, USES_VIRTUAL_FUNDING);
 
     console.log('Waiting for open channels');
-    await Promise.all(tabs.map(waitAndOpenChannel));
+    await Promise.all(tabs.map(waitAndOpenChannel(USES_VIRTUAL_FUNDING)));
 
     // Let the download cointinue for some time
     console.log('Downloading');
