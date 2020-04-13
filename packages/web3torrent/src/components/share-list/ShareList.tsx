a2a779cc15bca0870f5cdb54fb79b5f28f844a25
Mike Kerzhner <mikerzhner@gmail.com>
2020-04-13 10:24:00 -0700

    web3torrent: move click logic to ShareFile component

--- packages/web3torrent/src/components/share-list/ShareList.tsx
+++ packages/web3torrent/src/components/share-list/ShareList.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import {RoutePath} from '../../routes';
 import {Torrent} from '../../types';
 import {ShareFile} from './share-file/ShareFile';
 import './ShareList.scss';
-import {useHistory} from 'react-router-dom';
 
 export type ShareListProps = {torrents: Array<Partial<Torrent>>};
 
-const ShareList: React.FC<ShareListProps> = ({torrents}) => {
-  const history = useHistory();
+export const ShareList: React.FC<ShareListProps> = ({torrents}) => {
   return (
     <table className="share-list">
       <tbody>
-        {torrents.length
-          ? torrents.map(file => (
-              <ShareFile
-                file={file}
-                key={file.name}
-                goTo={hash => history.push(`${RoutePath.File}#${hash}`)}
-              />
-            ))
-          : false}
+        {torrents.map(file => (
+          <ShareFile file={file} key={file.name} />
+        ))}
       </tbody>
     </table>
   );
 };
-
-export {ShareList};
