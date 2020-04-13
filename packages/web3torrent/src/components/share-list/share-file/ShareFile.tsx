dac7699b31fde33a0912bcf7c83f8aab73ae9b55
Mike Kerzhner <mikerzhner@gmail.com>
2020-04-13 10:24:00 -0700

    web3torrent: move click logic to ShareFile component

--- packages/web3torrent/src/components/share-list/share-file/ShareFile.tsx
+++ packages/web3torrent/src/components/share-list/share-file/ShareFile.tsx
@@ -4,17 +4,23 @@ import {FormButton} from '../../form';
 import './ShareFile.scss';
 import {prettyPrintWei, calculateWei} from '../../../utils/calculateWei';
 import prettier from 'prettier-bytes';
+import {RoutePath} from '../../../routes';
+import {useHistory} from 'react-router-dom';
 
-export type ShareFileProps = {file: Partial<Torrent>; goTo: (name: string) => void};
+export type ShareFileProps = {file: Partial<Torrent>};
 
-const ShareFile: React.FC<ShareFileProps> = ({file, goTo}: ShareFileProps) => {
+const ShareFile: React.FC<ShareFileProps> = ({file}: ShareFileProps) => {
+  const history = useHistory();
   return (
     <tr className={'share-file'}>
       <td className="name-cell">{file.name}</td>
       <td className="other-cell">{prettier(file.length)}</td>
       <td className="other-cell">{prettyPrintWei(calculateWei(file.length))}</td>
       <td className="button-cell">
-        <FormButton name="download" onClick={() => goTo(file.magnetURI || file.name || '')}>
+        <FormButton
+          name="download"
+          onClick={() => history.push(`${RoutePath.File}#${file.magnetURI || file.name || ''}`)}
+        >
           Download
         </FormButton>
       </td>
