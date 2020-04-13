f2e50347e867ea42f847036d28ddc78f2d78bcf3
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-13 16:10:10 -0700

    One filel, two seeders, one leecher

--- packages/e2e-tests/puppeteer/__tests__/web3torrent.stress.test.ts
+++ packages/e2e-tests/puppeteer/__tests__/web3torrent.stress.test.ts
@@ -15,27 +15,32 @@ jest.setTimeout(HEADLESS ? 200_000 : 1_000_000);
 
 const USES_VIRTUAL_FUNDING = true;
 
-const enum Label {
-  A = 'A',
-  B = 'B'
-}
+describe('One file, two seeders, one leecher', () => {
+  const enum Label {
+    A = 'A',
+    B = 'B',
+    C = 'C',
+    D = 'D'
+  }
 
-const labels: Label[] = [Label.A, Label.B];
-type Data<T> = Record<Label, T>;
+  const labels: Label[] = [Label.A, Label.B, Label.C];
+  type Data<T> = Record<Label, T>;
 
-const assignEachLabel = <T>(data: Data<T>, cb: (label: Label) => any) =>
-  Promise.all(labels.map(async label => (data[label] = await cb(label))));
+  const assignEachLabel = <T>(data: Data<T>, cb: (label: Label) => any) =>
+    Promise.all(labels.map(async label => (data[label] = await cb(label))));
 
-const forEach = async <T>(data: Data<T>, cb: (obj: T, label: Label) => any) =>
-  await Promise.all(labels.map(async label => await cb(data[label], label)));
+  const forEach = async <T>(data: Data<T>, cb: (obj: T, label: Label) => any) =>
+    await Promise.all(labels.map(async label => await cb(data[label], label)));
 
-describe('Supports torrenting among peers with channels', () => {
-  const browsers: Data<Browser> = {} as Data<Browser>;
+  const browsers: Data<{browser: Browser; metamask: Dappeteer}> = {} as Data<{
+    browser: Browser;
+    metamask: Dappeteer;
+  }>;
   const tabs: Data<Page> = {} as Data<Page>;
   const metamasks: Data<Dappeteer> = {} as Data<Dappeteer>;
   afterAll(async () => {
     if (HEADLESS) {
-      await forEach(browsers, async browser => browser && (await browser.close()));
+      await forEach(browsers, async ({browser}) => browser && (await browser.close()));
     }
   });
 
@@ -46,7 +51,7 @@ describe('Supports torrenting among peers with channels', () => {
     await assignEachLabel(browsers, async () => await setUpBrowser(HEADLESS, 100));
 
     console.log('Waiting on pages');
-    await assignEachLabel(tabs, async label => (await browsers[label].pages())[0]);
+    await assignEachLabel(tabs, async label => (await browsers[label].browser.pages())[0]);
 
     const logPageOutput = (role: Label) => (msg: any) =>
       // use console.error so we can redirect STDERR to a file
@@ -55,21 +60,26 @@ describe('Supports torrenting among peers with channels', () => {
 
     await tabs.A.goto('http://localhost:3000/file/new', {waitUntil: 'load'});
 
-    console.log('A uploads a file');
-    const url = await uploadFile(tabs.A, USES_VIRTUAL_FUNDING, metamasks.A);
+    console.log('A, B upload the same file');
+    const [file] = [Label.A, Label.B].map(async label => {
+      const tab = tabs[label];
+      const {metamask} = browsers[label];
+      await tab.goto('http://localhost:3000/file/new', {waitUntil: 'load'});
+      return await uploadFile(tab, USES_VIRTUAL_FUNDING, metamask);
+    });
 
     console.log('B starts downloading...');
-    await startDownload(tabs.B, url, USES_VIRTUAL_FUNDING, metamasks.B);
+    await startDownload(tabs.B, await file, USES_VIRTUAL_FUNDING, metamasks.B);
 
     console.log('Waiting for open channels');
     await forEach(tabs, waitAndOpenChannel(USES_VIRTUAL_FUNDING));
 
     // Let the download cointinue for some time
     console.log('Downloading');
-    await tabs.B.waitFor(1000);
+    await tabs.C.waitFor(1000);
 
-    console.log('B cancels download');
-    await cancelDownload(tabs.B);
+    console.log('C cancels download');
+    await cancelDownload(tabs.C);
 
     console.log('Waiting for channels to close');
     await forEach(tabs, waitForClosingChannel);
