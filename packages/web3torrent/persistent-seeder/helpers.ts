52eb86ff54e8755a3622260425faf13c2bb2f216
George Knee <georgeknee@googlemail.com>
2020-04-14 15:53:51 +0100

    WIP Initial sketch of persistent-seeder
     (mashup of e2e and tracker)

---
+++ packages/web3torrent/persistent-seeder/helpers.ts
@@ -0,0 +1,139 @@
+import {Browser, Page, Frame, launch} from 'puppeteer';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+export async function loadDapp(
+  page: Page,
+  ganacheAccountIndex: number,
+  ignoreConsoleError?: boolean
+): Promise<void> {
+  page.on('pageerror', error => {
+    throw error;
+  });
+
+  page.on('console', msg => {
+    if (msg.type() === 'error' && !ignoreConsoleError) {
+      throw new Error(`Error was logged into the console ${msg.text()}`);
+    }
+    console.log('Page console log: ', msg.text());
+  });
+}
+
+// waiting for a css selector, and then clicking that selector is more robust than waiting for
+// an XPath and then calling .click() on the resolved handle. We do not use the return value from the
+// waitForSelector promise, so we avoid any errors where that return value loses its meaning
+// https://github.com/puppeteer/puppeteer/issues/3496
+// https://github.com/puppeteer/puppeteer/issues/2977
+export async function waitForAndClickButton(
+  page: Page,
+  frame: Frame,
+  selector: string
+): Promise<void> {
+  try {
+    await frame.waitForSelector(selector);
+  } catch (error) {
+    console.error(
+      'frame.waitForSelector(' + selector + ') failed on frame ' + (await frame.title())
+    );
+    await page.screenshot({path: 'e2e-wait.error.png'});
+    throw error;
+  }
+  try {
+    return await frame.click(selector);
+  } catch (error) {
+    console.error('frame.click(' + selector + ') failed on frame ' + (await frame.title()));
+    await page.screenshot({path: 'e2e-click.error.png'});
+    throw error;
+  }
+}
+
+export async function setUpBrowser(headless: boolean, slowMo?: number): Promise<Browser> {
+  const browser = await launch({
+    headless,
+    slowMo,
+    devtools: !headless,
+    // Keep code here for convenience... if you want to use redux-dev-tools
+    // then download and unzip the release from Github and specify the location.
+    // Github URL: https://github.com/zalmoxisus/redux-devtools-extension/releases
+    // args: [
+    //   '--disable-extensions-except=/Users/liam/Downloads/redux-dev-tools',
+    //   '--load-extension=/Users/liam/Downloads/redux-dev-tools'
+    // ],
+    //, Needed to allow both windows to execute JS at the same time
+    ignoreDefaultArgs: [
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-renderer-backgrounding'
+    ],
+    args: [
+      // Needed to inject web3.js code into wallet iframe
+      // https://github.com/puppeteer/puppeteer/issues/2548#issuecomment-390077713
+      '--disable-features=site-per-process'
+    ]
+  });
+
+  return browser;
+}
+
+export async function waitForBudgetEntry(page: Page): Promise<void> {
+  await page.waitForSelector('.site-budget-table > tbody > tr');
+}
+
+export async function waitForEmptyBudget(page: Page): Promise<void> {
+  // eslint-disable-next-line no-undef
+  await page.waitForFunction(() => !document.querySelector('.site-budget-table'));
+}
+
+export async function withdrawAndWait(page: Page): Promise<void> {
+  console.log('Withdrawing funds');
+  const walletIFrame = page.frames()[1];
+  const web3TorrentIFrame = page.frames()[0];
+  await waitForAndClickButton(page, web3TorrentIFrame, '#budget-withdraw');
+  await waitForAndClickButton(page, walletIFrame, '#approve-withdraw');
+}
+
+export async function waitAndApproveBudget(page: Page): Promise<void> {
+  console.log('Approving budget');
+
+  const approveBudgetButton = '.approve-budget-button';
+
+  const walletIFrame = page.frames()[1];
+  await waitForAndClickButton(page, walletIFrame, approveBudgetButton);
+}
+
+interface Window {
+  channelProvider: import('@statechannels/channel-provider').ChannelProviderInterface;
+  channelRunning(): void;
+}
+declare let window: Window;
+
+export const waitAndOpenChannel = (usingVirtualFunding: boolean) => async (
+  page: Page
+): Promise<void> => {
+  if (!usingVirtualFunding) {
+    console.log('Waiting for create channel button');
+
+    const createChannelButton = 'div.application-workflow-prompt > div > button';
+
+    const walletIFrame = page.frames()[1];
+    await waitForAndClickButton(page, walletIFrame, createChannelButton);
+  } else {
+    return new Promise(resolve =>
+      page.exposeFunction('channelRunning', resolve).then(() =>
+        page.evaluate(() => {
+          window.channelProvider.on('ChannelUpdated', () => {
+            window.channelRunning();
+            window.channelProvider.off('ChannelUpdated');
+          });
+        })
+      )
+    );
+  }
+};
+
+export async function waitForClosingChannel(page: Page): Promise<void> {
+  const closingText = 'div.application-workflow-prompt > h1';
+  const closingIframeB = page.frames()[1];
+  await closingIframeB.waitForSelector(closingText);
+}
---
+++ packages/web3torrent/persistent-seeder/index.ts
@@ -0,0 +1,84 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import {Page} from 'puppeteer';
+import fs from 'fs';
+
+import {waitAndApproveBudget, setUpBrowser, loadDapp} from './helpers';
+
+function prepareUploadFile(path: string): void {
+  const content = 'web3torrent\n'.repeat(100000);
+  const buf = Buffer.from(content);
+  fs.writeFile(path, buf, err => {
+    if (err) {
+      console.log(err);
+      throw new Error('Failed to prepare the upload file');
+    }
+  });
+}
+
+export async function uploadFile(page: Page, handleBudgetPrompt: boolean): Promise<string> {
+  await page.waitForSelector('input[type=file]');
+
+  // Generate a /tmp file with deterministic data for upload testing
+  const fileToUpload = '/tmp/web3torrent-tests-stub';
+  prepareUploadFile(fileToUpload);
+
+  // https://pub.dev/documentation/puppeteer/latest/puppeteer/FileChooser-class.html
+  // Not clear why puppeteer FileChooser won't work out of box. We are doing it manually for now.
+  const inputUploadHandle = await page.$('input[type=file]');
+  await inputUploadHandle!.uploadFile(fileToUpload);
+  await inputUploadHandle!.evaluate(upload => {
+    // eslint-disable-next-line no-undef
+    upload.dispatchEvent(new Event('change', {bubbles: true}));
+  });
+
+  if (handleBudgetPrompt) {
+    await waitAndApproveBudget(page);
+  }
+
+  const downloadLinkSelector = '#download-link';
+  await page.waitForSelector(downloadLinkSelector);
+  const downloadLink = await page.$eval(downloadLinkSelector, a => a.getAttribute('href'));
+
+  return downloadLink ? downloadLink : '';
+}
+
+export async function startDownload(
+  page: Page,
+  url: string,
+  handleBudgetPrompt: boolean
+): Promise<void> {
+  await page.goto(url);
+  const downloadButton = '#download-button:not([disabled])';
+  await page.waitForSelector(downloadButton);
+  await page.click(downloadButton);
+
+  if (handleBudgetPrompt) {
+    await waitAndApproveBudget(page);
+  }
+}
+
+export async function cancelDownload(page: Page): Promise<void> {
+  await page.click('#cancel-download-button');
+}
+
+const HEADLESS = false; // TODO set true
+
+async function script() {
+  // 100ms sloMo avoids some undiagnosed race conditions
+  console.log('Opening browsers');
+
+  const browser = await setUpBrowser(HEADLESS, 100);
+
+  console.log('Waiting on pages');
+  const web3tTab = (await browser.pages())[0];
+
+  console.log('Loading dapp');
+  await loadDapp(web3tTab, 0, true);
+
+  await web3tTab.goto('http://localhost:3000/file/new', {waitUntil: 'load'}); // TODO replace with deployed app
+
+  console.log('A uploads a file');
+  const url = await uploadFile(web3tTab, false); // TODO setup budget manually so that metamask is not required
+}
+
+script();
