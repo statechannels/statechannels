9a4ef351f1f30a8d7d53bc53dde3225ebce44e5d
Alex Gap <alex.gap@consensys.net>
2020-04-15 16:19:19 -0700

    undo hack

--- packages/web3torrent/src/clients/payment-channel-client.ts
+++ packages/web3torrent/src/clients/payment-channel-client.ts
@@ -167,15 +167,12 @@ export class PaymentChannelClient {
   }
 
   insertIntoChannelCache(channelState: ChannelState) {
-    // TODO: The joinChannel promise never resolves, meaning this function is not called
-    // by the leecher.
-    this.channelCache[channelState.channelId] = channelState;
+    this.channelCache = {...this.channelCache, [channelState.channelId]: channelState};
   }
 
   updateChannelCache(channelState: ChannelState) {
-    // TODO: This implementation is identical to insertIntoChannelCache because of joinChannel
-    // never resolving
-    this.channelCache[channelState.channelId] = channelState;
+    this.channelCache[channelState.channelId] && // only update an existing key
+      (this.channelCache[channelState.channelId] = channelState);
   }
 
   // Accepts an payment-channel-friendly callback, performs the necessary encoding, and subscribes to the channelClient with an appropriate, API-compliant callback
