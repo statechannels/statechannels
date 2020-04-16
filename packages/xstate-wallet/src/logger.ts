e724a05e2d0b1c646ceaa34401eae419c4661ea1
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-16 12:48:37 -0700

    Install pino-pretty

--- packages/xstate-wallet/src/logger.ts
+++ packages/xstate-wallet/src/logger.ts
@@ -1,12 +1,3 @@
 import pino from 'pino';
 
-const _logger = pino();
-
-const log = {
-  debug: _logger.debug.bind(_logger),
-  info: _logger.info.bind(_logger),
-  error: _logger.error.bind(_logger),
-  fatal: _logger.fatal.bind(_logger)
-};
-
-export {log};
+export const logger = pino({prettyPrint: true});
