13d95f77bcfacbc2eacb2d4e1a8c14ec56d82910
George Knee <georgeknee@googlemail.com>
2020-04-17 12:04:02 +0100

    Point rps at local firebase and reenable budget

--- packages/rps/src/gateways/firebase.ts
+++ packages/rps/src/gateways/firebase.ts
@@ -4,10 +4,13 @@ import 'firebase/database';
 
 import ReduxSagaFirebase from 'redux-saga-firebase';
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  databaseURL: `https://${process.env.FIREBASE_PROJECT}.firebaseio.com`,
-};
+export const config =
+  process.env.NODE_ENV === 'test'
+    ? undefined
+    : {
+        apiKey: process.env.FIREBASE_API_KEY,
+        databaseURL: process.env.FIREBASE_URL,
+      };
 
 const fire = firebase.initializeApp(config);
 
