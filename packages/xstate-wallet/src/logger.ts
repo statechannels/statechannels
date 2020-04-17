53a45619eed9a57e654c8917fb64aacf2536bac9
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-16 19:59:25 -0700

    Clean up pino config

--- packages/xstate-wallet/src/logger.ts
+++ packages/xstate-wallet/src/logger.ts
@@ -1,20 +1,20 @@
 import pino from 'pino';
 import {LOG_FILE} from './constants';
 
-const destination =
-  LOG_FILE && process.env.NODE_ENV === 'test' ? pino.destination(LOG_FILE) : undefined;
-const prettyPrint = destination ? false : {translateTime: true};
+// TODO: Is there a better way to determine if we're in a browser context?
+const isBrowserContext = process.env.NODE_ENV === 'test';
+const destination = LOG_FILE && isBrowserContext ? pino.destination(LOG_FILE) : undefined;
+
+const prettyPrint = LOG_FILE ? false : {translateTime: true};
 const name = 'xstate-wallet';
 
-// If we are in a browser, pino.destination is not available.
-// But, if we've specified a LOG_FILE, we assume that the log statements are meant to be stored as JSON objects
-const browser = {
-  write: o => console.log(JSON.stringify({...o, name}))
-};
+// If we are in a browser, and we've specified a LOG_FILE, we assume that the
+// log statements are meant to be stored as JSON objects
+// So, we log serialized objects, appending the name (which the pino browser-api appears to remove?)
+const browser =
+  LOG_FILE && isBrowserContext
+    ? {write: o => console.log(JSON.stringify({...o, name}))}
+    : undefined;
 
-let logger: ReturnType<typeof pino>;
-if (destination) logger = pino({name}, destination);
-else if (LOG_FILE) logger = pino({name, browser});
-else logger = pino({name, prettyPrint});
-
-export {logger};
+const opts = {name, prettyPrint, browser};
+export const logger = destination ? pino(opts, destination) : pino(opts);
