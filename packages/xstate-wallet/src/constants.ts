467735b1fc73ac6491feb0ff8263f110766b03cd
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-16 23:07:39 -0700

    clean up constants

--- packages/xstate-wallet/src/constants.ts
+++ packages/xstate-wallet/src/constants.ts
@@ -9,8 +9,10 @@ export const NITRO_ADJUDICATOR_ADDRESS =
   process.env.NITRO_ADJUDICATOR_ADDRESS || '0x0000000000000000000000000000000000000000';
 
 // TODO: Move top ENV variable
-// Corresponds to '0x8624ebe7364bb776f891ca339f0aaa820cc64cc9fca6a28eec71e6d8fc950f29' from test data
-export const HUB_ADDRESS = process.env.HUB_ADDRESS || '0xaaaa84838319627Fa056fC3FC29ab94d479B8502';
+export const HUB_ADDRESS =
+  process.env.HUB_ADDRESS ||
+  // Corresponds to '0x8624ebe7364bb776f891ca339f0aaa820cc64cc9fca6a28eec71e6d8fc950f29' from test data
+  '0xaaaa84838319627Fa056fC3FC29ab94d479B8502';
 export const HUB_DESTINATION = hexZeroPad(HUB_ADDRESS, 32) as any;
 export const HUB = {
   destination: HUB_DESTINATION,
@@ -23,26 +25,19 @@ export const NETWORK_ID = process.env.CHAIN_NETWORK_ID || '0';
 export const ETH_TOKEN = '0x0000000000000000000000000000000000000000';
 
 export function assetHolderAddress(tokenAddress: string): string | undefined {
-  if (bigNumberify(tokenAddress).isZero()) {
-    return ETH_ASSET_HOLDER_ADDRESS;
-  }
-  // TODO: store mapping, implement lookup
-  return tokenAddress;
+  if (bigNumberify(tokenAddress).isZero()) return ETH_ASSET_HOLDER_ADDRESS;
+
+  throw 'Tokens not supported';
 }
 
 export function tokenAddress(assetHolderAddress: string): string | undefined {
-  if (assetHolderAddress === ETH_ASSET_HOLDER_ADDRESS) {
-    return ETH_TOKEN;
-  }
-  // TODO: store mapping, implement lookup
-  return assetHolderAddress;
-}
-// TODO: Use getEnvBool from devtools once working
-export function getBool(val: string | undefined, throwIfMissing = true): boolean {
-  if (throwIfMissing && val === undefined) {
-    throw Error(`Environment variable ${name} is not set.`);
-  }
+  if (assetHolderAddress === ETH_ASSET_HOLDER_ADDRESS) return ETH_TOKEN;
 
+  throw 'Tokens not supported';
+}
+
+// TODO: Use getEnvBool from devtools once working
+export function getBool(val: string | undefined): boolean {
   switch (val) {
     case undefined:
     case null:
@@ -55,8 +50,8 @@ export function getBool(val: string | undefined, throwIfMissing = true): boolean
       return true;
   }
 }
-export const CLEAR_STORAGE_ON_START = getBool(process.env.CLEAR_STORAGE_ON_START, false);
-export const USE_INDEXED_DB = getBool(process.env.USE_INDEXED_DB, false);
+export const CLEAR_STORAGE_ON_START = getBool(process.env.CLEAR_STORAGE_ON_START);
+export const USE_INDEXED_DB = getBool(process.env.USE_INDEXED_DB);
 
 export const LOG_FILE = process.env.LOG_FILE;
-export const ADD_LOGS = LOG_FILE ? true : getBool(process.env.ADD_LOGS || 'false');
+export const ADD_LOGS = !!LOG_FILE || getBool(process.env.ADD_LOGS);
