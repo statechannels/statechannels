ffa35f270c55cfde7775acaec658a8de3184fedf
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-17 10:54:35 -0700

    [web3torrent] configure pino-logger file/console behaviour

--- packages/web3torrent/src/logger.ts
+++ packages/web3torrent/src/logger.ts
@@ -1,6 +1,27 @@
 import pino from 'pino';
+import {LOG_DESTINATION, ADD_LOGS} from './constants';
 
-const logger = pino();
+// TODO: Is there a better way to determine if we're in a browser context?
+const IS_BROWSER_CONTEXT = process.env.NODE_ENV !== 'test';
+const LOG_TO_CONSOLE = LOG_DESTINATION === 'console';
+const LOG_TO_FILE = ADD_LOGS && !LOG_TO_CONSOLE;
 
-// Temporarily export default with type similar to debug
+const name = 'web3torrent';
+
+const destination =
+  LOG_TO_FILE && !IS_BROWSER_CONTEXT ? pino.destination(LOG_DESTINATION) : undefined;
+
+// If we are in a browser, but we want to LOG_TO_FILE, we assume that the
+// log statements are meant to be stored as JSON objects
+// So, we log serialized objects, appending the name (which the pino browser-api appears to remove?)
+const browser =
+  LOG_TO_FILE && IS_BROWSER_CONTEXT
+    ? {write: o => console.log(JSON.stringify({...o, name}))}
+    : undefined;
+
+const prettyPrint = LOG_TO_CONSOLE ? {translateTime: true} : false;
+
+const opts = {name, prettyPrint, browser};
+export const logger = destination ? pino(opts, destination) : pino(opts);
+
 export default prefix => (...x) => logger.info(prefix, ...x);
