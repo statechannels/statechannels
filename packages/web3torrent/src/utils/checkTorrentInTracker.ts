4a1f5da55135570de0ebc6fc1e4a7e7a641785ab
Santiago Persico <spersico@users.noreply.github.com>
2020-04-17 19:42:09 -0300

    fix tracker check, add logs (#1476)
    * fix tracker check, add logs
    
    * Update packages/web3torrent/src/utils/checkTorrentInTracker.ts
    
    Co-Authored-By: Mike Kerzhner <1062379+kerzhner@users.noreply.github.com>
    
    Co-authored-by: Mike Kerzhner <1062379+kerzhner@users.noreply.github.com>

--- packages/web3torrent/src/utils/checkTorrentInTracker.ts
+++ packages/web3torrent/src/utils/checkTorrentInTracker.ts
@@ -1,33 +1,33 @@
 import {Client} from 'bittorrent-tracker';
 import {defaultTrackerOpts} from '../constants';
 import {TorrentTestResult} from '../library/types';
+import debug from 'debug';
+const log = debug('web3torrent:tracker-check');
 
 export async function checkTorrentInTracker(infoHash: string) {
+  log(`Scraping tracker for torrent ${infoHash}`);
   const trackerClient: Client = new Client({
     ...defaultTrackerOpts,
     infoHash: [infoHash]
   });
-  let completePeers;
-  const gotAWire: Promise<boolean> = new Promise(resolve => {
-    const updateIfSeederFound = data => {
-      completePeers = data.complete;
-    };
-    trackerClient.once('peer', function() {
-      resolve(true);
-    });
-    trackerClient.once('update', updateIfSeederFound);
-    trackerClient.start();
+  const trackerScrape: Promise<number> = new Promise(resolve => {
+    trackerClient.once('scrape', data => resolve(data.complete));
+    setTimeout(() => trackerClient.scrape(), 2500); // waits ~2 seconds as that's the tracker update tick
   });
   const timer: Promise<undefined> = new Promise((resolve, _) => setTimeout(resolve, 5000));
-  const raceResult = await Promise.race([gotAWire, timer]);
-  trackerClient.stop();
-  trackerClient.destroy();
-  if (raceResult) {
-    return TorrentTestResult.SEEDERS_FOUND; // Could connect to a peer
+
+  const raceResult = await Promise.race([trackerScrape, timer]);
+  trackerClient.stop(); // cleanup
+  trackerClient.destroy(); // cleanup
+
+  if (Number.isInteger(raceResult)) {
+    if (raceResult > 0) {
+      log(`Test Result: SEEDERS_FOUND (${raceResult})`);
+      return TorrentTestResult.SEEDERS_FOUND; // Found seeders (peers with the complete torrent)
+    }
+    log('Test Result: NO_SEEDERS_FOUND');
+    return TorrentTestResult.NO_SEEDERS_FOUND; // was able to connect, but no seeders found
   }
-  if (Number.isInteger(completePeers)) {
-    // was able to connect to the tracker, but couldn't connect to a peer
-    return TorrentTestResult.NO_SEEDERS_FOUND;
-  }
+  log('Test Result: NO_CONNECTION');
   return TorrentTestResult.NO_CONNECTION; // wasn't able to get a response from the tracker
 }
