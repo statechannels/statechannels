9bd73aba7c53abb201404b8c621f5220067a604a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-16 17:57:59 -0700

    [xstate-wallet] pino works in browser when LOG_FILE is defined

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,20 @@
 import pino from 'pino';
 import {LOG_FILE} from './constants';
 
-const destination = LOG_FILE && pino.destination(LOG_FILE);
-const prettyPrint = destination ? false : {translateTime: true, ignore: 'pid,hostname'};
+const destination =
+  LOG_FILE && process.env.NODE_ENV === 'test' ? pino.destination(LOG_FILE) : undefined;
+const prettyPrint = destination ? false : {translateTime: true};
 const name = 'xstate-wallet';
+
+// If we are in a browser, pino.destination is not available.
+// But, if we've specified a LOG_FILE, we assume that the log statements are meant to be stored as JSON objects
+const browser = {
+  write: o => console.log(JSON.stringify({...o, name}))
+};
 
 let logger: ReturnType<typeof pino>;
 if (destination) logger = pino({name}, destination);
+else if (LOG_FILE) logger = pino({name, browser});
 else logger = pino({name, prettyPrint});
 
 export {logger};
