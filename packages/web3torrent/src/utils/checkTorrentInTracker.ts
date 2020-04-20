4f67ce75e60507039387b096202a2172659d095f
Santiago Persico <santiagopersico@gmail.com>
2020-04-20 18:56:37 -0300

    trackerClient rename

--- packages/web3torrent/src/utils/checkTorrentInTracker.ts
+++ packages/web3torrent/src/utils/checkTorrentInTracker.ts
@@ -7,18 +7,15 @@ const log = logger.child({module: 'tracker-check'});
 export async function checkTorrentInTracker(infoHash: string) {
   log.info(`Scraping tracker for torrent ${infoHash}`);
 
-  const trackerClient: Client = new Client({
-    ...defaultTrackerOpts,
-    infoHash: [infoHash]
-  });
+  const client = new Client({...defaultTrackerOpts, infoHash: [infoHash]});
   const trackerScrape: Promise<number> = new Promise(resolve => {
-    trackerClient.once('scrape', data => resolve(data.complete));
-    setTimeout(() => trackerClient.scrape(), 2500); // waits ~2 seconds as that's the tracker update tick
+    client.once('scrape', data => resolve(data.complete));
+    setTimeout(() => client.scrape(), 2500); // waits ~2 seconds as that's the tracker update tick
     setTimeout(() => resolve(-1), 5000); // returns -1 if there was no answer from the tracker(s) for ~2.5 seconds
   });
   const scrapeResult = await trackerScrape;
-  trackerClient.stop();
-  trackerClient.destroy();
+  client.stop();
+  client.destroy();
 
   if (scrapeResult > 0) {
     log.info(`Test Result: SEEDERS_FOUND (${scrapeResult})`);
