56e0c95cde96acd4900c851cd7ffc147e393853c
Santiago Persico <spersico@users.noreply.github.com>
2020-04-21 15:03:40 -0300

    W3T Start-storybook fix (#1495)
    * add missing property in mock context
    
    * uncomments and fixes ShareFile story
    
    * Update packages/web3torrent/.storybook/web3torrent-client.mock.js
    
    Co-Authored-By: Mike Kerzhner <1062379+kerzhner@users.noreply.github.com>
    
    * lint mock web3torrent client
    
    Co-authored-by: Mike Kerzhner <1062379+kerzhner@users.noreply.github.com>

--- packages/web3torrent/.storybook/web3torrent-client.mock.js
+++ packages/web3torrent/.storybook/web3torrent-client.mock.js
@@ -1,22 +1,19 @@
 const React = require('react');
 const utils = require('./../src/utils/test-utils');
 
-const mockWeb3TorrentClient = () => {
+function mockWeb3TorrentClient() {
   const _peers = utils.createMockTorrentPeers();
-  const torrent = utils.createMockTorrent({
+  const torrent = utils.createMockTorrentUI({
     numPeers: Object.keys(_peers).length,
     _peers,
-    wires: Array.from(_peers, ({ wire }) => wire)
+    wires: Array.from(_peers, ({wire}) => wire)
   });
 
-  return { torrents: [torrent] };
-};
+  return {torrents: [torrent], paymentChannelClient: {challengeChannel: _ => {}}};
+}
 
 const web3Torrent = mockWeb3TorrentClient();
 
 const Web3TorrentContext = React.createContext(web3Torrent);
 
-module.exports = {
-  Web3TorrentContext,
-  web3Torrent
-};
+module.exports = {Web3TorrentContext, web3Torrent};
--- packages/web3torrent/src/components/share-list/share-file/ShareFile.stories.tsx
+++ packages/web3torrent/src/components/share-list/share-file/ShareFile.stories.tsx
@@ -1,29 +1,24 @@
 import {withActions} from '@storybook/addon-actions';
-import {number, text, withKnobs} from '@storybook/addon-knobs';
+import {withKnobs} from '@storybook/addon-knobs';
 import {storiesOf} from '@storybook/react';
 import React from 'react';
 import '../../../App.scss';
 import '../ShareList.scss';
 import {ShareFile} from './ShareFile';
 import './ShareFile.scss';
+import {createMockTorrentUI} from '../../../utils/test-utils';
+import {MemoryRouter} from 'react-router-dom';
+import {RoutePath} from '../../../routes';
 
 // todo: fix after type refactor
 storiesOf('Web3Torrent', module)
   .addDecorator(withKnobs())
   .addDecorator(withActions('click'))
+  .addDecorator(story => <MemoryRouter initialEntries={[RoutePath.Root]}>{story()}</MemoryRouter>)
   .add('ShareFile', () => (
     <table className="share-list">
       <tbody>
-        {/*         <ShareFile
-          key="1"
-          file={{
-            name: text('File name', 'Once-Upon-A-Time.zip', 'File data'),
-            length: number('File size (in Mb)', 172, {step: 0.01}, 'File data'),
-            numPeers: number('Number of peers', 17, {step: 1}, 'File data'),
-            magnetURI: text('Magnet link', '', 'File data')
-          }}
-        ></ShareFile>
- */}
+        <ShareFile key="1" file={createMockTorrentUI()}></ShareFile>
       </tbody>
     </table>
   ));
