ec42b802de60cc2bc0a1498269eef356db2a4b88
George Knee <georgeknee@googlemail.com>
2020-04-22 17:40:01 +0100

    Incorporate dappeteer into e2e helpers
    change default ganache port to 8545

--- packages/e2e-tests/puppeteer/constants.ts
+++ packages/e2e-tests/puppeteer/constants.ts
@@ -1,5 +1,6 @@
 import {getEnvBool, configureEnvVariables} from '@statechannels/devtools';
 configureEnvVariables();
 export const HEADLESS = getEnvBool('HEADLESS');
+export const USE_DAPPETEER = getEnvBool('USE_DAPPETEER');
 export const USES_VIRTUAL_FUNDING = process.env.REACT_APP_FUNDING_STRATEGY === 'Virtual';
 export const JEST_TIMEOUT = HEADLESS ? 200_000 : 1_000_000;
