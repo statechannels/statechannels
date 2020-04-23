7a9a5a22880e1f6e1c1b490bee803552df90da21
Alex Gap <alex.gap@consensys.net>
2020-04-23 00:19:02 -0700

    Clean up index

--- packages/web3torrent/src/index.tsx
+++ packages/web3torrent/src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {web3torrent, Web3TorrentContext} from './clients/web3torrent-client';
+
 import {OmniProvider} from './contexts/omni-provider';
 
 ReactDOM.render(
   <OmniProvider>
-    <Web3TorrentContext.Provider value={web3torrent}>
-      <App />
-    </Web3TorrentContext.Provider>
+    <App />
   </OmniProvider>,
   document.getElementById('root')
 );
