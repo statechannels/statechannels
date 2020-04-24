52e690b8a6275d16dbd7561e5485f26116406e11
Alex Gap <alex.gap@consensys.net>
2020-04-24 14:31:37 -0700

    Fix budget fetching

--- packages/web3torrent/src/context/budget-context.ts
+++ packages/web3torrent/src/context/budget-context.ts
@@ -11,14 +11,18 @@ interface Props {
 }
 export function useBudgetContext({w3tClient}: Props) {
   const [loading, setLoading] = useState(true);
-
+  const [doInitialFetch, setDoInitialFetch] = useState(true);
+  const [budget, setBudget] = useState(undefined);
   useEffect(() => {
     const getAndSetBudget = async () => {
       const {paymentChannelClient} = w3tClient;
+
       await paymentChannelClient.getBudget();
+      setBudget(paymentChannelClient.budgetCache);
       setLoading(false);
+      setDoInitialFetch(false);
     };
-    if (w3tClient) {
+    if (doInitialFetch) {
       getAndSetBudget();
     }
   });
@@ -27,15 +31,15 @@ export function useBudgetContext({w3tClient}: Props) {
   const createBudget = async () => {
     setLoading(true);
     await paymentChannelClient.createBudget(INITIAL_BUDGET_AMOUNT);
-
+    setBudget(paymentChannelClient.budgetCache);
     setLoading(false);
   };
   const closeBudget = async () => {
     setLoading(true);
     await paymentChannelClient.closeAndWithdraw();
-
+    setBudget(paymentChannelClient.budgetCache);
     setLoading(false);
   };
 
-  return {budget: paymentChannelClient.budgetCache, loading, createBudget, closeBudget};
+  return {budget, loading, createBudget, closeBudget};
 }
