cea943502974e2f21bc2b4b5d4acdc39c4c1f963
George Knee <georgeknee@googlemail.com>
2020-04-27 14:59:30 +0100

    Kill devServer on exit

--- packages/web3torrent/scripts/start.js
+++ packages/web3torrent/scripts/start.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 
-// TODO connect to *existing* ganache. Here we start a new instance to aid development.
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
-let devServer; // TODO annotate type
-let trackerServer; // TODO annotate type
+let devServer;
+let trackerServer;
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -39,6 +37,14 @@ void (async () => {
     }
   });
   process.on('SIGTERM', () => {
+    if (devServer) {
+      devServer.kill();
+    }
+    if (trackerServer) {
+      trackerServer.kill();
+    }
+  });
+  process.on('exit', () => {
     if (devServer) {
       devServer.kill();
     }
