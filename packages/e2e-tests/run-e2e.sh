da655be4a97932710300aec0aa030e9dd41c9e7e
George Knee <georgeknee@googlemail.com>
2020-04-27 15:00:11 +0100

    Use more robust cleanup command
    This kills all processes with the PGID of this process

--- packages/e2e-tests/run-e2e.sh
+++ packages/e2e-tests/run-e2e.sh
@@ -1,11 +1,10 @@
 cd ../devtools
 yarn start:shared-ganache & 
-cd ../$1 && yarn run wait-on ../../.ganache-deployments
+cd ../$1 && yarn run wait-on ../../.ganache-deployments/ganache-deployments-8545.json
 yarn start &
 cd ../xstate-wallet
-yarn run wait-on ../../.ganache-deployments 
 yarn start &
 cd ../e2e-tests
 yarn test $1
 rm -rf ../../.ganache-deployments
-pkill -P $$
+kill -- -$$
