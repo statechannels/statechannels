bcc3b8067d34d9314402f91f4546f5d08c050336
George Knee <georgeknee@googlemail.com>
2020-04-27 14:59:10 +0100

    Remove unused variable and check

--- packages/rps/scripts/start.js
+++ packages/rps/scripts/start.js
@@ -1,18 +1,12 @@
-// TODO connect to *existing* ganache. Here we start a new instance to aid development.
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
-let ganacheServer;
 let devServer;
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
-  if (ganacheServer) {
-    ganacheServer.close();
-  }
   if (devServer) {
     devServer.close();
   }
