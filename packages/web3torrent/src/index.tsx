fdf069338352c00e56130a9a2baf740d9104ff78
Liam Horne <liam@lihorne.com>
2020-04-27 12:49:58 -0400

    Add drift chatbox on web3torrent

--- packages/web3torrent/src/index.tsx
+++ packages/web3torrent/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Drift from 'react-driftjs';
+
 import App from './App';
 import {web3torrent, Web3TorrentContext} from './clients/web3torrent-client';
 
 ReactDOM.render(
   <Web3TorrentContext.Provider value={web3torrent}>
     <App />
+    {process.env.REACT_APP_INCLUDE_DRIFT_CHATBOX && <Drift appId="kp3gf29uk9c3" />}
   </Web3TorrentContext.Provider>,
   document.getElementById('root')
 );
