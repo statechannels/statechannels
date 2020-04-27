c59260c2a93c84132731a2d96703ad034c8f3eb8
Liam Horne <liam@lihorne.com>
2020-04-27 10:48:12 -0400

    Add build scripts to capture monorepo env file (#1561)
    * Add build script for web3torrent to capture env file
    
    * Add asame script to xstate-wallet
    
    * Format files
    
    * Add SKIP_PREFLIGHT_CHECK=true to .env.test
    
    * Apply suggestions from code review
    
    Co-Authored-By: George C. Knee <georgeknee@googlemail.com>
    
    * Add SKIP_PREFLIGHT_CHECK=true to .env.development
    
    Co-authored-by: George C. Knee <georgeknee@googlemail.com>

---
+++ packages/web3torrent/scripts/build.js
@@ -0,0 +1,12 @@
+const {spawn} = require('child_process');
+const {configureEnvVariables} = require('@statechannels/devtools');
+
+/**
+ * The purpose of this file is simply to run configureEnvVariables
+ * before executing the react-scripts build command.
+ */
+void (() =>
+  configureEnvVariables() &&
+  spawn('yarn', ['run', 'react-scripts', 'build'], {
+    stdio: 'inherit'
+  }).on('close', process.exit))();
