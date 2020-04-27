2d641d02e33a05d8c8c94fe34c2d71d0e3b4469c
George Knee <georgeknee@googlemail.com>
2020-04-27 11:58:21 +0100

    Cleanup folder when killing ganache
    we wait on the folder

--- packages/e2e-tests/run-e2e.sh
+++ packages/e2e-tests/run-e2e.sh
@@ -3,3 +3,4 @@ cd ../$1 && yarn run wait-on ../../.ganache-deployments && yarn start &
 cd ../xstate-wallet && yarn run wait-on ../../.ganache-deployments && yarn start &
 cd ../e2e-tests && yarn test $1
 pkill -P $$
+rm -rf ../../.ganache-deployments
