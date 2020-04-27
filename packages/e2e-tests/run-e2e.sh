0a88b85c2f0bdd4c0f2442bb664eb99a7aa215d5
George Knee <georgeknee@googlemail.com>
2020-04-27 13:05:06 +0100

    Flatten out processes
    easier to kill them when done

--- packages/e2e-tests/run-e2e.sh
+++ packages/e2e-tests/run-e2e.sh
@@ -1,6 +1,11 @@
-cd ../devtools && yarn start:shared-ganache & 
-cd ../$1 && yarn run wait-on ../../.ganache-deployments && yarn start &
-cd ../xstate-wallet && yarn run wait-on ../../.ganache-deployments && yarn start &
-cd ../e2e-tests && yarn test $1
-pkill -P $$
+cd ../devtools
+yarn start:shared-ganache & 
+cd ../$1 && yarn run wait-on ../../.ganache-deployments
+yarn start &
+cd ../xstate-wallet
+yarn run wait-on ../../.ganache-deployments 
+yarn start &
+cd ../e2e-tests
+yarn test $1
 rm -rf ../../.ganache-deployments
+pkill -P $$
