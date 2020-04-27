a2d4f7b2d76ad6e7d8c9fc251a159e0bebfd7379
George Knee <georgeknee@googlemail.com>
2020-04-27 11:54:24 +0100

    Better cleanup of background processes

--- packages/e2e-tests/run-e2e.sh
+++ packages/e2e-tests/run-e2e.sh
@@ -1,8 +1,5 @@
 cd ../devtools && yarn start:shared-ganache & 
-job1=$!
 cd ../$1 && yarn run wait-on ../../.ganache-deployments && yarn start &
-job2=$!
 cd ../xstate-wallet && yarn run wait-on ../../.ganache-deployments && yarn start &
-job3=$!
 cd ../e2e-tests && yarn test $1
-kill $job1 $job2 $job3
+pkill -P $$
