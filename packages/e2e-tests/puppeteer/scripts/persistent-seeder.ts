fc4ffed520faca4040f8263af1406777c3df0eab
George Knee <georgeknee@googlemail.com>
2020-04-28 16:04:21 +0100

    Add persistent-seeder script + cmd
    upgrade tsnode
    read in new env vars to helpers.ts

--- packages/e2e-tests/puppeteer/scripts/persistent-seeder.ts
+++ packages/e2e-tests/puppeteer/scripts/persistent-seeder.ts
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import {setUpBrowser, setupLogging} from '../helpers';
+import {uploadFile} from './web3torrent';
 
 (async (): Promise<void> => {
   if (require.main === module) {
     console.log('Opening browser');
-    const {browser} = await setUpBrowser(false, 0);
+    const {browser, metamask} = await setUpBrowser(false, 0);
 
     console.log('Waiting on pages');
     const web3tTabA = (await browser.pages())[0];
@@ -12,8 +12,10 @@ import {setUpBrowser, setupLogging} from '../helpers';
     console.log('Setting up logging...');
     await setupLogging(web3tTabA, 0, 'minimal-dapp', true);
 
-    await web3tTabA.goto('http://bbc.com', {waitUntil: 'load'});
+    await web3tTabA.goto('https://web3torrent.statechannels.org/upload', {waitUntil: 'load'});
     await web3tTabA.bringToFront();
-    console.log('completed!');
+
+    const url = await uploadFile(web3tTabA, true, metamask);
+    console.log(`seeding: go to ${url}`);
   }
 })();
