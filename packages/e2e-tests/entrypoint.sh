ea3b4638aee9c8c8960be0e63366cd85668bdb64
Santiago Persico <santiagopersico@gmail.com>
2020-04-28 05:38:11 -0300

    update entrypoint to allow interactive console

--- packages/e2e-tests/entrypoint.sh
+++ packages/e2e-tests/entrypoint.sh
@@ -7,15 +7,8 @@ Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 export DISPLAY=:99.0
 export PUPPETEER_EXEC_PATH="google-chrome-stable"
 
-# Run commands
-cmd=$@
-echo "Running '$cmd'!"
-if $cmd; then
-    # no op
-    echo "Successfully ran '$cmd'"
-else
-    exit_code=$?
-    echo "Failure running '$cmd', exited with $exit_code"
-    echo ''
-    exit $exit_code
-fi
+#!/bin/sh
+cd packages/e2e-tests
+
+set -e
+exec "$@"
