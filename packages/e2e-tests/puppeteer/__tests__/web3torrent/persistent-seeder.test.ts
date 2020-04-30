e2b12b4d5a4be4167c8e02bedef8c6b371aaa070
George Knee <georgeknee@googlemail.com>
2020-04-30 09:59:57 +0100

    Remove persistent-seeder.test.ts
    This was mainly for debugging purposes

--- packages/e2e-tests/puppeteer/__tests__/web3torrent/persistent-seeder.test.ts
+++
@@ -1,9 +0,0 @@
-import {persistentSeeder} from '../../scripts/persistent-seeder';
-jest.setTimeout(120000);
-describe('Persistent Seeder', () => {
-  it('uploads a file, approves a budget, deposits with the hub, and echoes url', async () => {
-    await persistentSeeder();
-    expect(true).toBe(true);
-    // TODO cleanup broswers?
-  });
-});
