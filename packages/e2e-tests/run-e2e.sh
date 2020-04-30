9ec66bc7c9eb9e8cccff486cd7f81814ce570e36
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-29 17:48:23 -0700

    More variables

--- packages/e2e-tests/run-e2e.sh
+++ packages/e2e-tests/run-e2e.sh
@@ -7,6 +7,10 @@ E2E_ROOT=$(pwd)
 PACKAGES=$E2E_ROOT/..
 MONOREPO_ROOT=$E2E_ROOT/../..
 
+WAIT_ON_TIMEOUT=60000
+WAIT_ON_INTERVAL=5000
+WAIT_ON_DELAY=5000
+
 cleanup() {
   if test -f "$MONOREPO_ROOT/.ganache-deployments/ganache-deployments-8545.json"; then
     rm $MONOREPO_ROOT/.ganache-deployments/ganache-deployments-8545.json
@@ -21,17 +25,18 @@ trap 'trap - SIGTERM && cleanup' SIGINT SIGTERM EXIT
 cd $PACKAGES/devtools
 yarn start:shared-ganache | tee $E2E_ROOT/shared-ganache.log & 
 
-cd $PACKAGES/$APP && yarn run wait-on ../../.ganache-deployments/ganache-deployments-8545.json
+yarn run wait-on -t $WAIT_ON_TIMEOUT -i $WAIT_ON_INTERVAL $MONOREPO_ROOT/.ganache-deployments/ganache-deployments-8545.json
+cd $PACKAGES/$APP 
 yarn start | tee $E2E_ROOT/app.log &
 
-cd ../xstate-wallet
+cd $PACKAGES/xstate-wallet
 yarn start | tee $E2E_ROOT/xstate-wallet.log &
 
-cd ../simple-hub
+cd $PACKAGES/simple-hub
 yarn hub:watch | tee $E2E_ROOT/hub.log &
 
-cd ../e2e-tests
-yarn run wait-on -d 5000 http://localhost:3000 http://localhost:3055
+cd $PACKAGES/e2e-tests
+yarn run wait-on -d $WAIT_ON_DELAY -t $WAIT_ON_TIMEOUT -i $WAIT_ON_INTERVAL http://localhost:3000 http://localhost:3055
 yarn test $APP
 code=$?
 
