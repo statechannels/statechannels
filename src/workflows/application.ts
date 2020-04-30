497d8ed134f3649c49db3a3aba3e54fe26448ce4
Alex Gap <alex.gap@consensys.net>
2020-04-30 14:16:58 -0700

    updateStoreAndSendResponse in one action

--- src/workflows/application.ts
+++ src/workflows/application.ts
@@ -63,7 +63,6 @@ type WorkflowGuards = Guards<
 >;
 
 export interface WorkflowActions {
-  sendUpdateChannelResponse: Action<any, PlayerStateUpdate>;
   sendChallengeChannelResponse: Action<RequestIdExists & ChannelIdExists, any>;
   sendCloseChannelResponse: Action<ChannelIdExists, any>;
   sendCreateChannelResponse: Action<RequestIdExists & ChannelIdExists, any>;
@@ -74,7 +73,7 @@ export interface WorkflowActions {
   hideUi: Action<WorkflowContext, any>;
   sendChannelUpdatedNotification: Action<WorkflowContext, any>;
   spawnObservers: AssignAction<ChannelIdExists, any>;
-  updateStoreWithPlayerState: Action<WorkflowContext, PlayerStateUpdate>;
+  updateStoreAndSendResponse: Action<WorkflowContext, PlayerStateUpdate>;
 }
 
 export type WorkflowEvent =
@@ -183,7 +182,7 @@ const generateConfig = (
         SPAWN_OBSERVERS: {actions: actions.spawnObservers},
         PLAYER_STATE_UPDATE: {
           target: 'running',
-          actions: [actions.updateStoreWithPlayerState, actions.sendUpdateChannelResponse]
+          actions: [actions.updateStoreAndSendResponse]
         },
         CHANNEL_UPDATED: [
           {target: 'closing', cond: guards.channelClosing},
@@ -246,11 +245,6 @@ export const workflow = (
       .pipe(map(storeEntry => ({type: 'CHANNEL_UPDATED', storeEntry})));
 
   const actions: WorkflowActions = {
-    sendUpdateChannelResponse: async (context: any, event: PlayerStateUpdate) => {
-      const entry = await store.getEntry(context.channelId);
-      messagingService.sendResponse(event.requestId, await serializeChannelEntry(entry));
-    },
-
     sendCloseChannelResponse: async (context: ChannelIdExists) => {
       const entry = await store.getEntry(context.channelId);
       if (context.requestId) {
@@ -311,7 +305,9 @@ export const workflow = (
     assignRequestId: assign((context, event: JoinChannelEvent | PlayerRequestConclude) => ({
       requestId: event.requestId
     })),
-    updateStoreWithPlayerState: async (context: ChannelIdExists, event: PlayerStateUpdate) => {
+    updateStoreAndSendResponse: async (context: ChannelIdExists, event: PlayerStateUpdate) => {
+      // TODO: This should probably be done in a service and we should handle the failure cases
+      // For now we update the store and then send the response in one action so the response has the latest state
       if (context.channelId === event.channelId) {
         const existingState = (await store.getEntry(event.channelId)).latest;
         const newState = {
@@ -320,7 +316,8 @@ export const workflow = (
           appData: event.appData,
           outcome: event.outcome
         };
-        await store.signAndAddState(event.channelId, newState);
+        const entry = await store.signAndAddState(event.channelId, newState);
+        await messagingService.sendResponse(event.requestId, await serializeChannelEntry(entry));
       }
     }
   };
@@ -412,11 +409,10 @@ const mockActions: Record<keyof WorkflowActions, string> = {
   sendChallengeChannelResponse: 'sendChallengeChannelResponse',
   sendCreateChannelResponse: 'sendCreateChannelResponse',
   sendJoinChannelResponse: 'sendJoinChannelResponse',
-  sendUpdateChannelResponse: 'sendUpdateChannelResponse',
   hideUi: 'hideUi',
   displayUi: 'displayUi',
   spawnObservers: 'spawnObservers',
-  updateStoreWithPlayerState: 'updateStoreWithPlayerState',
+  updateStoreAndSendResponse: 'updateStoreAndSendResponse',
   assignRequestId: 'assignRequestId'
 };
 
