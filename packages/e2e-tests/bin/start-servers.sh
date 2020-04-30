dd00dc1faa05754f9726a92b35176d10ace540aa
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-30 08:53:17 -0700

    Add start-servers script to start app, wallet & simple-hub
    This can then be used as a standalone script, for example if I want to run
    integration tests multiple times (or just run the app in the browser).

---
+++ packages/e2e-tests/bin/start-servers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+set -u
+
+APP=$1
+MONOREPO_ROOT=$(cd ../../ && pwd)
+PACKAGES=$MONOREPO_ROOT/packages
+E2E_ROOT=$PACKAGES/e2e-tests
+PORT=8545
+GANACHE_DEPLOYMENTS=$MONOREPO_ROOT/.ganache-deployments/ganache-deployments-$PORT.json
+
+WAIT_ON_TIMEOUT=60000
+WAIT_ON_INTERVAL=5000
+WAIT_ON_DELAY=5000
+
+cleanup() {
+  if test -f $GANACHE_DEPLOYMENTS; then
+    rm $GANACHE_DEPLOYMENTS
+  fi
+}
+
+# On exit signals, kill self & child processes 
+# For some reason, the `node scripts/start.js` command in xstate-wallet
+# does not immediately terminate, even if I send the KILL signal (kill 0 -9)
+# It seems to wait for compilation to succeed, and then terminates, which can
+# take ~30s on my machine.
+trap "trap - SIGTERM && cleanup && kill 0" SIGINT SIGTERM EXIT
+
+if test -f $GANACHE_DEPLOYMENTS; then
+  echo $GANACHE_DEPLOYMENTS exists. Perhaps Ganache is running on port $PORT
+  exit 1
+fi
+
+cd $PACKAGES/devtools
+yarn start:shared-ganache | tee $E2E_ROOT/shared-ganache.log & 
+
+yarn run wait-on -t $WAIT_ON_TIMEOUT -i $WAIT_ON_INTERVAL $MONOREPO_ROOT/.ganache-deployments/ganache-deployments-8545.json
+
+cd $PACKAGES/$APP 
+yarn start | tee $E2E_ROOT/app.log &
+
+cd $PACKAGES/xstate-wallet
+yarn start | tee $E2E_ROOT/xstate-wallet.log &
+
+cd $PACKAGES/simple-hub
+yarn hub:watch | tee $E2E_ROOT/hub.log &
+
+wait
