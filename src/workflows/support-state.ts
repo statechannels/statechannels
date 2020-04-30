1a4d1ce747b9f1cd642b4690cc45eaabf7e098fb
Alex Gap <alex.gap@consensys.net>
2020-04-30 13:29:09 -0700

    Update support state to only sign if needed

--- src/workflows/support-state.ts
+++ src/workflows/support-state.ts
@@ -1,9 +1,10 @@
 import {AnyEventObject, AssignAction, MachineConfig, assign, spawn, Machine, Actor} from 'xstate';
 import {filter, map} from 'rxjs/operators';
 import {Store} from '../store';
-import {statesEqual, outcomesEqual, calculateChannelId} from '../store/state-utils';
+import {statesEqual, calculateChannelId} from '../store/state-utils';
 import {State} from '../store/types';
-
+import {logger} from '../logger';
+const warn = logger.warn.bind(logger);
 const WORKFLOW = 'support-state';
 
 export type Init = {state: State; observer?: Actor<any, any>};
@@ -16,57 +17,50 @@ Do we abort? Or do we try to reach consensus on a later state?
 */
 export const config: MachineConfig<HasChannelId, any, AnyEventObject> = {
   key: WORKFLOW,
-  initial: 'sendState',
+  initial: 'signState',
   states: {
-    sendState: {
+    signState: {
       entry: [
         assign<HasChannelId>({channelId: ({state}) => calculateChannelId(state)}),
         'spawnObserver'
       ],
-      invoke: {src: 'sendState'},
+      invoke: {src: 'signState'},
       on: {SUPPORTED: 'success'}
     },
     success: {type: 'final'}
   }
 };
 
-type Services = {sendState(ctx: HasChannelId): any};
+type Services = {signState(ctx: HasChannelId, event): any};
 
 type Options = {
   services: Services;
   actions: {spawnObserver: AssignAction<HasChannelId, any>};
 };
 
-const sendState = (store: Store) => async ({state, channelId}: HasChannelId) => {
+const signState = (store: Store) => async ({state, channelId}: HasChannelId) => {
   const entry = await store.getEntry(channelId);
-  const {isSupportedByMe, isSupported} = entry;
-  // TODO: Should these safety checks be performed in the store?
-  if (
-    // If we've haven't already signed a state, there's no harm in supporting one.
-    !isSupportedByMe ||
-    // If we've already supported this state, we might as well re-send it.
-    (isSupportedByMe && statesEqual(entry.latestSignedByMe, state)) ||
-    // Otherwise, we only send it if we haven't signed any new states.
-    (isSupported && entry.supported.turnNum.lt(state.turnNum)) ||
-    // We always support a final state if it matches the outcome that we have signed
-    (state.isFinal && outcomesEqual(state.outcome, entry.latestSignedByMe.outcome))
-  ) {
+  const {isSupportedByMe} = entry;
+  // We only sign the state if we haven't signed it already
+  if (!isSupportedByMe || !statesEqual(entry.latestSignedByMe, state)) {
     await store.signAndAddState(channelId, state);
-  } else {
-    throw 'Not safe to send';
+  } else if (statesEqual(entry.latestSignedByMe, state)) {
+    // The support state machine was started with a state that we already support
+    // That's fine but we output a warning in case that's unexpected
+    warn({state}, 'The state is already supported');
   }
 };
 
 const notifyWhenSupported = (store: Store, {state, channelId}: HasChannelId) =>
   store.channelUpdatedFeed(channelId).pipe(
     filter(({isSupported}) => isSupported),
-    filter(({supported}) => statesEqual(state, supported)),
+    filter(entry => statesEqual(state, entry.supported)),
     map(() => 'SUPPORTED')
   );
 
 const options = (store: Store): Options => ({
   services: {
-    sendState: sendState(store)
+    signState: signState(store)
   },
   actions: {
     spawnObserver: assign<HasChannelId>((ctx: HasChannelId) => ({
