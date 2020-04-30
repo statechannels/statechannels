e2f197d4507f282605aa2b345288cba507a3cdfb
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-29 17:48:03 -0700

    Always remove deployments file

--- packages/e2e-tests/run-e2e.sh
+++ packages/e2e-tests/run-e2e.sh
@@ -5,33 +5,34 @@ set -u
 APP=$1
 E2E_ROOT=$(pwd)
 PACKAGES=$E2E_ROOT/..
+MONOREPO_ROOT=$E2E_ROOT/../..
+
+cleanup() {
+  if test -f "$MONOREPO_ROOT/.ganache-deployments/ganache-deployments-8545.json"; then
+    rm $MONOREPO_ROOT/.ganache-deployments/ganache-deployments-8545.json
+  fi
+  kill 0
+}
 
 # Kill child processes if I receive SIGINT, SIGTERM or EXIT
-trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
+trap 'trap - SIGTERM && cleanup' SIGINT SIGTERM EXIT
 
 
 cd $PACKAGES/devtools
 yarn start:shared-ganache | tee $E2E_ROOT/shared-ganache.log & 
-ganache=$!
 
 cd $PACKAGES/$APP && yarn run wait-on ../../.ganache-deployments/ganache-deployments-8545.json
 yarn start | tee $E2E_ROOT/app.log &
-app=$!
 
 cd ../xstate-wallet
 yarn start | tee $E2E_ROOT/xstate-wallet.log &
-wallet=$!
 
 cd ../simple-hub
 yarn hub:watch | tee $E2E_ROOT/hub.log &
-hub=$!
 
 cd ../e2e-tests
 yarn run wait-on -d 5000 http://localhost:3000 http://localhost:3055
 yarn test $APP
 code=$?
 
-if test -f "../../.ganache-deployments/ganache-deployments-8545.json"; then
-  rm ../../.ganache-deployments/ganache-deployments-8545.json
-fi
 exit $code
