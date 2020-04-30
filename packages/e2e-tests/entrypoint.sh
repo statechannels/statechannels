14c4663d5b94b67a7942825b52271ce24896237e
George Knee <georgeknee@googlemail.com>
2020-04-30 12:25:47 +0100

    Tidy up comments

--- packages/e2e-tests/entrypoint.sh
+++ packages/e2e-tests/entrypoint.sh
@@ -13,14 +13,11 @@ else
   yarn test:e2e:w3t
 fi
 
-# I FOUND IT.
-# using the set -e and exec "$@" changes the context of the shell, making that the Xvfb command
-# to stop being valid.
+# using set -e and exec "$@" changes the context of the shell, invalidating the Xvfb command
 
-# PROOF: 
 # - running "docker run -it dapp" will make the command in line 13 run, 
-#   without changing the context... and the seeder test PASSES!
+#   without changing the context
 # - running "docker run -it dapp bash" and then running "yarn test:e2e:w3t" will fail
 # - running "docker run -it dapp bash" and then running 
 #   Xvfb -ac :99 -screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR > /dev/null 2>&1 &
-#   and THEN "yarn test:e2e:w3t" will WORK!
+#   and THEN "yarn test:e2e:w3t" will work
