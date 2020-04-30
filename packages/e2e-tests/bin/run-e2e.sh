4829437b0c78f24c9c2a4382566df71c21d77d88
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-04-30 11:45:39 -0700

    Don't trap EXIT, and cleanup  when tests pass
    This lets us exit with the same status as jest, when tests pass

--- packages/e2e-tests/bin/run-e2e.sh
+++ packages/e2e-tests/bin/run-e2e.sh
@@ -9,11 +9,13 @@ WAIT_ON_INTERVAL=5000
 WAIT_ON_DELAY=5000
 
 cleanup() {
-  kill 0
+  echo killing $services
+  kill -9 $services
+  wait
 }
 
-# Kill child processes if I receive SIGINT, SIGTERM or EXIT
-trap 'trap - SIGTERM && cleanup' SIGINT SIGTERM EXIT
+# Kill child processes if I receive SIGINT, SIGTERM
+trap 'cleanup' SIGINT SIGTERM
 
 yarn start-servers $APP &
 services=$!
@@ -21,4 +23,8 @@ echo Services running at PID $services
 
 yarn run wait-on -d $WAIT_ON_DELAY -t $WAIT_ON_TIMEOUT -i $WAIT_ON_INTERVAL http://localhost:3000 http://localhost:3055
 
-yarn test $APP
+yarn test $APP --runInBand 
+exit_status=$?
+
+cleanup
+exit $exit_status
