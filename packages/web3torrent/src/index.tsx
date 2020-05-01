f8788a2e87122a98d686b5ebbc5e89dcb1ad42dc
George Knee <georgeknee@googlemail.com>
2020-05-01 10:31:29 +0100

    Introduce REACT_APP_DRIFT_CHATBOX_APP_ID env var

--- packages/web3torrent/src/index.tsx
+++ packages/web3torrent/src/index.tsx
@@ -8,7 +8,9 @@ import {web3torrent, Web3TorrentContext} from './clients/web3torrent-client';
 ReactDOM.render(
   <Web3TorrentContext.Provider value={web3torrent}>
     <App />
-    {process.env.REACT_APP_INCLUDE_DRIFT_CHATBOX && <Drift appId="kp3gf29uk9c3" />}
+    {process.env.REACT_APP_DRIFT_CHATBOX_APP_ID && (
+      <Drift appId={process.env.REACT_APP_DRIFT_CHATBOX_APP_ID} />
+    )}
   </Web3TorrentContext.Provider>,
   document.getElementById('root')
 );
