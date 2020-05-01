dea82e8f5622fcb717e815692944acf6f9f49154
Junji Zhi <jzhi316@gmail.com>
2020-05-01 10:51:07 -0400

    Add test assert for conclude channels
    The test fails because we didn't implement withdraw for direct funding case.

--- packages/xstate-wallet/src/workflows/tests/conclude-channel.test.ts
+++ packages/xstate-wallet/src/workflows/tests/conclude-channel.test.ts
@@ -1,7 +1,7 @@
 import {interpret} from 'xstate';
 import waitForExpect from 'wait-for-expect';
 
-import {Init, machine} from '../create-and-fund';
+import {Init, machine as createMachine} from '../create-and-fund';
 
 import {Store} from '../../store';
 import {bigNumberify} from 'ethers/utils';
@@ -9,7 +9,7 @@ import {bigNumberify} from 'ethers/utils';
 import {firstState, calculateChannelId, createSignatureEntry} from '../../store/state-utils';
 import {ChannelConstants, Outcome, State} from '../../store/types';
 import {AddressZero} from 'ethers/constants';
-import {machine as concludeChannelMachine} from '../conclude-channel';
+import {machine as concludeMachine} from '../conclude-channel';
 
 import {wallet1, wallet2, participants, wallet3, TEST_SITE} from './data';
 import {subscribeToMessages} from './message-service';
@@ -83,7 +83,8 @@ beforeEach(async () => {
 });
 
 it('is idempoent when concluding twice', async () => {
-  const connectToStore = (store: Store) => interpret(machine(store).withContext(context)).start();
+  // Let A and B create and fund channel
+  const connectToStore = (store: Store) => interpret(createMachine(store).withContext(context)).start();
   const [aService, bService] = [aStore, bStore].map(connectToStore);
 
   await waitForExpect(async () => {
@@ -91,10 +92,17 @@ it('is idempoent when concluding twice', async () => {
     expect(aService.state.value).toEqual('success');
   }, EXPECT_TIMEOUT);
 
+  // Both conclude the channel
+  const connectToStore2 = (store: Store) => interpret(concludeMachine(store).withContext(context)).start();
+  const [aService2, bService2] = [aStore, bStore].map(connectToStore2);
+
+  await waitForExpect(async () => {
+    expect(bService2.state.value).toEqual('success');
+    expect(aService2.state.value).toEqual('success');
+  }, EXPECT_TIMEOUT);
+
   // Inject service.stop() when A concludes and  A restarts concluding
 
-  interpret(concludeChannelMachine(aStore).withContext(context)).start();
-  interpret(concludeChannelMachine(bStore).withContext(context)).start();
-
   // Assert expected states
+  expect((await aStore.chain.getChainInfo(targetChannelId)).amount).toMatchObject(bigNumberify(0));
 });
