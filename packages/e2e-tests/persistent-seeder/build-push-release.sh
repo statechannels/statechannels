6a72d629c50a4866def891f76abe88f755dcdad0
Santiago Persico <spersico@users.noreply.github.com>
2020-05-04 15:40:18 -0300

    Update packages/e2e-tests/persistent-seeder/build-push-release.sh
    Co-authored-by: Mike Kerzhner <1062379+kerzhner@users.noreply.github.com>

--- packages/e2e-tests/persistent-seeder/build-push-release.sh
+++ packages/e2e-tests/persistent-seeder/build-push-release.sh
@@ -4,6 +4,6 @@ set -euf -o pipefail
 BASEDIR=$(dirname "$0")
 
 # MAIN SCRIPT
-docker build -t registry.heroku.com/persistent-seeder-production/persistent-seeder -f persistent-seeder.dockerfile "$BASEDIR"/../../.. || restoreDockerIgnore
+docker build -t registry.heroku.com/persistent-seeder-production/persistent-seeder -f persistent-seeder.dockerfile "$BASEDIR"/../../..
 docker push registry.heroku.com/persistent-seeder-production/persistent-seeder
 heroku container:release -a persistent-seeder-staging persistent-seeder
