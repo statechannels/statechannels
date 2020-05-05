30d1610a8873559f41746977e60cd57a673876ac
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-05-05 14:25:21 -0700

    [e2e-tests] Wait for servers before running tests

---
+++ packages/e2e-tests/jest.setup.ts
@@ -0,0 +1,14 @@
+import waitOn from 'wait-on';
+
+export default async () => {
+  const resources = ['http://localhost:3000', 'http://localhost:3055'];
+  const opts = {resources, delay: 5000, interval: 2000, timeout: 120000};
+  try {
+    console.log('Waiting for servers');
+    await waitOn(opts);
+    console.log('Servers started');
+    // once here, all resources are available
+  } catch (err) {
+    console.error(err);
+  }
+};
