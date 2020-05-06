53975f591fe23ebfe4d741a3d5c418f7d11ec0af
George Knee <georgeknee@googlemail.com>
2020-05-06 10:28:31 +0100

    F&R simple-hub-staging -> simple-hub-production

--- packages/simple-hub/docker/build-push-release.sh
+++ packages/simple-hub/docker/build-push-release.sh
@@ -2,6 +2,6 @@
 set -euf -o pipefail
 BASEDIR=$(dirname "$0")
 
-docker build -t registry.heroku.com/simple-hub-staging/simple-hub -f "$BASEDIR"/simple-hub.dockerfile "$BASEDIR"/../../..
-docker push registry.heroku.com/simple-hub-staging/simple-hub
-heroku container:release -a simple-hub-staging simple-hub
+docker build -t registry.heroku.com/simple-hub-production/simple-hub -f "$BASEDIR"/simple-hub.dockerfile "$BASEDIR"/../../..
+docker push registry.heroku.com/simple-hub-production/simple-hub
+heroku container:release -a simple-hub-production simple-hub
