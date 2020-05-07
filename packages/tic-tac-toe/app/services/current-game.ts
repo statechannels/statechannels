e0dccd6868a6c650290cfe06bd4bc5fa6c34f7d7
Alon Bukai <alonzorz@gmail.com>
2020-05-07 10:13:47 +0300

    feat: Save ChannelState in CurrentGameService

--- packages/tic-tac-toe/app/services/current-game.ts
+++ packages/tic-tac-toe/app/services/current-game.ts
@@ -1,6 +1,8 @@
 import Service from '@ember/service';
 import ChallengeModel from '../models/challenge';
 import {tracked} from '@glimmer/tracking';
+import {AppData} from '../core/app-data';
+import {ChannelState} from '../core/channel-state';
 
 export enum Player {
   A,
@@ -10,6 +12,7 @@ export enum Player {
 export default class CurrentGameService extends Service {
   @tracked private game!: ChallengeModel;
   @tracked private player!: Player;
+  @tracked private channelState!: ChannelState<AppData>;
 
   public getGame(): ChallengeModel {
     return this.game;
@@ -25,6 +28,14 @@ export default class CurrentGameService extends Service {
 
   public setPlayer(player: Player): void {
     this.player = player;
+  }
+
+  public getChannelState(): ChannelState<AppData> {
+    return this.channelState;
+  }
+
+  public setChannelState(channelState: ChannelState<AppData>): void {
+    this.channelState = channelState;
   }
 }
 
