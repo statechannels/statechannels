fd1d498ce982e73e73956718ca2ff65377536e5a
Santiago Persico <santiagopersico@gmail.com>
2020-05-08 18:38:58 -0300

    refactor showTImeRemaining to be clearer

--- packages/web3torrent/src/components/torrent-info/peer-network-stats/PeerNetworkStats.tsx
+++ packages/web3torrent/src/components/torrent-info/peer-network-stats/PeerNetworkStats.tsx
@@ -5,15 +5,18 @@ import {TorrentUI, IdleStatuses} from '../../../types';
 
 export type PeerNetworkStatsProps = {torrent: TorrentUI};
 
-const PeerNetworkStats: React.FC<PeerNetworkStatsProps> = ({torrent}: PeerNetworkStatsProps) =>
-  !IdleStatuses.includes(torrent.status) && (
-    <section className="PeerNetworkStats">
-      {!torrent.originalSeed && torrent.parsedTimeRemaining && torrent.parsedTimeRemaining + '. '}
-      {prettier(torrent.done || !torrent.downloadSpeed ? 0 : torrent.downloadSpeed)}
-      /s down, {prettier(!torrent.uploadSpeed ? 0 : torrent.uploadSpeed)}/s up
-      <br />
-      <strong data-test-selector="numPeers">{torrent.numPeers}</strong> Peers connected
-    </section>
+const PeerNetworkStats: React.FC<PeerNetworkStatsProps> = ({torrent}: PeerNetworkStatsProps) => {
+  const showTimeRemaining = !torrent.originalSeed && torrent.parsedTimeRemaining;
+  return (
+    !IdleStatuses.includes(torrent.status) && (
+      <section className="PeerNetworkStats">
+        {showTimeRemaining && `${torrent.parsedTimeRemaining}. `}
+        {prettier(torrent.done || !torrent.downloadSpeed ? 0 : torrent.downloadSpeed)}
+        /s down, {prettier(!torrent.uploadSpeed ? 0 : torrent.uploadSpeed)}/s up
+        <br />
+        <strong data-test-selector="numPeers">{torrent.numPeers}</strong> Peers connected
+      </section>
+    )
   );
-
+};
 export {PeerNetworkStats};
