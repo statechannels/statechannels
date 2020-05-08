b59d6b975c78a6e84a45cdc972324bdd46df6187
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-05-08 14:37:51 -0700

    Fix lint warning

--- packages/e2e-tests/puppeteer/helpers.ts
+++ packages/e2e-tests/puppeteer/helpers.ts
@@ -5,7 +5,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import {USE_DAPPETEER, TARGET_NETWORK, TX_WAIT_TIMEOUT, SCREENSHOT_DIR} from './constants';
 import {ETHERLIME_ACCOUNTS} from '@statechannels/devtools';
-import {dir} from 'console';
 
 const logDistinguisherCache: Record<string, true | undefined> = {};
 
