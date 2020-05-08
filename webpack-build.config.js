b7da1a9fb318cda13dfb55b9d5cd72f1f44355f4
Liam Horne <liam@lihorne.com>
2020-05-08 12:40:17 -0400

    Keep function names but still minify webpack build (#1743)

--- webpack-build.config.js
+++ webpack-build.config.js
@@ -1,6 +1,20 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
 
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = merge(common, {
-  mode: 'development'
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          // We keep function names because various workflows use Function.name to refer
+          // to actions that an xstate machine will trigger under certain conditions
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          keep_fnames: true
+        }
+      })
+    ]
+  }
 });
