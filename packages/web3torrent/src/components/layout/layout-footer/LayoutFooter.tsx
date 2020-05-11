25a2c59c01612eaef982492b832dca6abd413ab6
Liam Horne <liam@lihorne.com>
2020-05-11 15:02:45 -0400

    Update packages/web3torrent/src/components/layout/layout-footer/LayoutFooter.tsx
    Co-authored-by: Mike Kerzhner <1062379+kerzhner@users.noreply.github.com>

--- packages/web3torrent/src/components/layout/layout-footer/LayoutFooter.tsx
+++ packages/web3torrent/src/components/layout/layout-footer/LayoutFooter.tsx
@@ -8,7 +8,7 @@ const LayoutFooter: React.FC<RouteComponentProps> = () => (
   <footer>
     <img className="footer-logo" src="/assets/logo.svg" alt="Web3Torrent logo" />
     <span className="footer-text">
-      Works in Chrome, Firefox, and Opera. Source code Available on GitHub. © 2020 State Channels.
+      Works in Chrome, Firefox, and Opera. Source code available on GitHub. © 2020 State Channels.
       Version:{' '}
       <a
         href={`https://github.com/statechannels/monorepo/tree/${COMMIT_HASH}/packages/web3torrent`}
