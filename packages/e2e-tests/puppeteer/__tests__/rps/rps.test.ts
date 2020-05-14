527e8410912988939a4e5cd489234d22d2293e6a
George Knee <georgeknee@googlemail.com>
2020-05-14 10:03:06 +0100

    Unskip rps test

--- packages/e2e-tests/puppeteer/__tests__/rps/rps.test.ts
+++ packages/e2e-tests/puppeteer/__tests__/rps/rps.test.ts
@@ -9,7 +9,7 @@ import {
   setupFakeWeb3,
   takeScreenshot
 } from '../../helpers';
-import {login, startFundAndPlaySingleMove} from '../../scripts/rps';
+import {login, startFundAndPlaySingleMove, bResigns} from '../../scripts/rps';
 import {Dappeteer} from 'dappeteer';
 import {USE_DAPPETEER, CLOSE_BROWSERS} from '../../constants';
 
@@ -70,16 +70,16 @@ describe('completes game 1 (challenge by A, challenge by B, resign by B) and beg
     console.log('logging in..');
     await login(rpsTabA, rpsTabB);
     console.log('starting first game...');
-    return await startFundAndPlaySingleMove(rpsTabA, metamaskA, rpsTabB, metamaskB);
+    await startFundAndPlaySingleMove(rpsTabA, metamaskA, rpsTabB, metamaskB);
     // xstate wallet does not fully support challenging yet
     // console.log('A challenging...');
     // await aChallenges(rpsTabA, rpsTabB);
     // console.log('B challenging...');
     // await bChallenges(rpsTabA, rpsTabB);
-    // console.log('B resigning...');
-    // await bResigns(rpsTabA, rpsTabB);
-    // console.log('starting second game...');
-    // return await startFundAndPlaySingleMove(rpsTabA, metamaskA, rpsTabB, metamaskB);
+    console.log('B resigning...');
+    await bResigns(rpsTabA, metamaskA, rpsTabB, metamaskB);
+    console.log('starting second game...');
+    return await startFundAndPlaySingleMove(rpsTabA, metamaskA, rpsTabB, metamaskB);
     // (ultimate and intermediate) test success implied by promises resolving
     // therefore no assertions needed in this test
   });
