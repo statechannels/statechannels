7e5b1ef07510e42520b4aff49dcbd9c25fc3a09f
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-05-13 18:32:27 -0700

    [xstate-wallet, web3torrent, channel-provider] Improve logs
    I was using the `transmit` option in
    https://github.com/statechannels/monorepo/pull/1829
    and
    https://github.com/statechannels/monorepo/pull/1836
    
    While investigating around
    https://github.com/statechannels/monorepo/pull/1839,
    I realized that I could use the `write` option in any case.
    
    This means
    - I don't have to transform the logEvent thata `transmit` gives you: http://getpino.io/#/docs/browser?id=transmit-object
    - I can keep the same console formatting (yellow for warn, red for error, etc) by passing an object for `write`: http://getpino.io/#/docs/browser?id=write-function-object

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,7 @@
 import pino from 'pino';
 
 import {LOG_DESTINATION, ADD_LOGS, NODE_ENV} from './config';
+import _ from 'lodash';
 
 // TODO: Is there a better way to determine if we're in a browser context?
 const IS_BROWSER_CONTEXT = NODE_ENV !== 'test';
@@ -13,27 +14,30 @@ const name = 'xstate-wallet';
 const destination =
   LOG_TO_FILE && !IS_BROWSER_CONTEXT ? pino.destination(LOG_DESTINATION) : undefined;
 
-const serializeLogEvent = o => JSON.stringify({...o, name});
+const postMessageAndCallConsoleFn = (consoleFn: {
+  (message?: any, ...optionalParams: any[]): void;
+}) => (o: any) => {
+  const withName = JSON.stringify({...o, name});
 
-let browser: any = IS_BROWSER_CONTEXT
+  // The simplest way to give users/developers easy access to the logs in a single place is to
+  // make the application aware of all the pino logs via postMessage
+  // Then, the application can package up all the logs into a single file
+  window.parent.postMessage({type: 'PINO_LOG', logEvent: JSON.parse(withName)}, '*');
+  if (LOG_TO_FILE) consoleFn(withName);
+  else consoleFn(o.msg, _.omit(o, 'msg'));
+};
+
+const browser: any = IS_BROWSER_CONTEXT
   ? {
-      transmit: {
-        send: (_, logEvent) =>
-          // The simplest way to give users/developers easy access to the logs in a single place is to
-          // make the application aware of all the pino logs via postMessage
-          // Then, the application can package up all the logs into a single file
-          window.parent.postMessage(
-            {type: 'PINO_LOG', logEvent: {...JSON.parse(JSON.stringify(logEvent)), name}},
-            '*'
-          )
+      write: {
+        error: postMessageAndCallConsoleFn(console.error),
+        warn: postMessageAndCallConsoleFn(console.warn),
+        info: postMessageAndCallConsoleFn(console.info),
+        debug: postMessageAndCallConsoleFn(console.debug),
+        trace: postMessageAndCallConsoleFn(console.trace)
       }
     }
   : undefined;
-
-if (browser && LOG_TO_FILE) {
-  // TODO: Use the logBlob instead of writing to the browser logs
-  browser = {...browser, write: o => console.log(serializeLogEvent(o))};
-}
 
 const prettyPrint = LOG_TO_CONSOLE ? {translateTime: true} : false;
 
