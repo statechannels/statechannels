a6912ebe47cf5fd7535eef1e92c3cb2f545c12aa
George Knee <georgeknee@googlemail.com>
2020-05-14 13:57:49 +0100

    Unskip both players resigning

--- packages/e2e-tests/puppeteer/__tests__/rps/rps.test.ts
+++ packages/e2e-tests/puppeteer/__tests__/rps/rps.test.ts
@@ -9,7 +9,7 @@ import {
   setupFakeWeb3,
   takeScreenshot
 } from '../../helpers';
-import {login, startFundAndPlaySingleMove, aResigns} from '../../scripts/rps';
+import {login, startFundAndPlaySingleMove, aResigns, bResigns} from '../../scripts/rps';
 import {Dappeteer} from 'dappeteer';
 import {USE_DAPPETEER, CLOSE_BROWSERS} from '../../constants';
 
@@ -77,10 +77,10 @@ describe('completes game 1 (challenge by A, challenge by B, resign by B) and beg
     // console.log('B challenging...');
     // await bChallenges(rpsTabA, rpsTabB);
     console.log('B resigning...');
+    await bResigns(rpsTabA, metamaskA, rpsTabB, metamaskB);
+    console.log('starting second game...');
+    await startFundAndPlaySingleMove(rpsTabA, metamaskA, rpsTabB, metamaskB);
+    console.log('A resigning...');
     await aResigns(rpsTabA, metamaskA, rpsTabB, metamaskB);
-    // console.log('starting second game...'); // Starting a second game does not yet work
-    // return await startFundAndPlaySingleMove(rpsTabA, metamaskA, rpsTabB, metamaskB);
-    // (ultimate and intermediate) test success implied by promises resolving
-    // therefore no assertions needed in this test
   });
 });
