9da0ab184b478e310e5538150806f2344876918a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-05-15 12:29:11 -0700

    :pig: Give the simple-hub a head start
    https://github.com/statechannels/monorepo/issues/1650#issuecomment-629437976

--- bin/start-servers.sh
+++ bin/start-servers.sh
@@ -36,13 +36,15 @@ yarn start:shared-ganache | tee $E2E_ROOT/shared-ganache.log &
 
 yarn run wait-on -t $WAIT_ON_TIMEOUT -i $WAIT_ON_INTERVAL $MONOREPO_ROOT/.ganache-deployments/ganache-deployments-8545.json
 
+cd $PACKAGES/simple-hub
+yarn hub:start:dev | tee $E2E_ROOT/hub.log &
+
+sleep 5000
+
 cd $PACKAGES/$APP 
 yarn start | tee $E2E_ROOT/$APP.log &
 
 cd $PACKAGES/xstate-wallet
 yarn start | tee $E2E_ROOT/xstate-wallet.log &
-
-cd $PACKAGES/simple-hub
-yarn hub:start:dev | tee $E2E_ROOT/hub.log &
 
 wait
