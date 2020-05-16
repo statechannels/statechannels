fe4bb64db271837b4c43c8358935213f5c9cf607
Liam Horne <liam@lihorne.com>
2020-05-16 19:40:05 -0400

    Increase JEST_TIMEOUT if using ropsten

--- packages/e2e-tests/puppeteer/constants.ts
+++ packages/e2e-tests/puppeteer/constants.ts
@@ -3,8 +3,8 @@ configureEnvVariables();
 export const HEADLESS = getEnvBool('HEADLESS');
 export const USE_DAPPETEER = getEnvBool('USE_DAPPETEER');
 export const USES_VIRTUAL_FUNDING = process.env.FUNDING_STRATEGY === 'Virtual';
-export const JEST_TIMEOUT = HEADLESS ? 200_000 : 1_000_000;
 export const TARGET_NETWORK = process.env.TARGET_NETWORK || 'localhost';
+export const JEST_TIMEOUT = TARGET_NETWORK === 'ropsten' ? 1_000_000 : 200_000;
 export const APP_URL = process.env.WEB3TORRENT_URL
   ? process.env.WEB3TORRENT_URL
   : 'http://localhost:3000';
