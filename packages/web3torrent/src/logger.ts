14099304be33ffd9b90ba35b470bad6ae795b250
Liam Horne <liam@lihorne.com>
2020-05-18 14:53:13 -0400

    Use JEST_WORKER_ID to detect IS_BROWSER_CONTEXT (#1893)
    * Use JEST_WORKER_ID to detect IS_BROWSER_CONTEXT
    
    * Flip equality around

--- packages/web3torrent/src/logger.ts
+++ packages/web3torrent/src/logger.ts
@@ -2,8 +2,7 @@ import pino from 'pino';
 import {LOG_DESTINATION, ADD_LOGS, LOG_LEVEL, VERSION} from './constants';
 import _ from 'lodash';
 
-// TODO: Is there a better way to determine if we're in a browser context?
-const IS_BROWSER_CONTEXT = process.env.NODE_ENV !== 'test';
+const IS_BROWSER_CONTEXT = process.env.JEST_WORKER_ID === undefined;
 const LOG_TO_CONSOLE = LOG_DESTINATION === 'console';
 const LOG_TO_FILE = ADD_LOGS && !LOG_TO_CONSOLE;
 
