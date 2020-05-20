b2b76ceba61ed9118e33e4a8f798dabf4bad0be8
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-05-20 10:45:14 -0700

    Add more error codes
    See https://github.com/statechannels/monorepo/issues/1853

--- packages/client-api-schema/src/error-codes.ts
+++ packages/client-api-schema/src/error-codes.ts
@@ -6,6 +6,14 @@ export default {
     UserDeclined: 200
   },
   CloseChannel: {
-    NotYourTurn: 300
+    NotYourTurn: 300,
+    ChannelNotFound: 301
+  },
+  UpdateChannel: {
+    ChannelNotFound: 400,
+    InvalidTransition: 401,
+    InvalidAppData: 402,
+    NotYourTurn: 403,
+    ChannelClosed: 403
   }
 };
