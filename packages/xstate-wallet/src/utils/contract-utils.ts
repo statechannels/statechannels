bbb4043b3f949e17fed659adc9f0991c98f211db
Alex Gap <alex.gap@consensys.net>
2020-05-21 16:12:08 -0700

    Direct reference

--- packages/xstate-wallet/src/utils/contract-utils.ts
+++ packages/xstate-wallet/src/utils/contract-utils.ts
@@ -1,9 +1,11 @@
-import {Contract, providers} from 'ethers';
+import {Contract} from 'ethers';
 import {ContractArtifacts} from '@statechannels/nitro-protocol';
 
 import {ETH_ASSET_HOLDER_ADDRESS} from '../config';
 import {MOCK_TOKEN, MOCK_ASSET_HOLDER_ADDRESS, ETH_TOKEN} from '../constants';
 import {bigNumberify} from 'ethers/utils';
+
+import {Web3Provider, JsonRpcProvider} from '@ethersproject/providers';
 
 export function assetHolderAddress(tokenAddress: string): string | undefined {
   if (bigNumberify(tokenAddress).isZero()) return ETH_ASSET_HOLDER_ADDRESS;
@@ -19,11 +21,11 @@ export function tokenAddress(assetHolderAddress: string): string | undefined {
   throw 'TokenAddress not found';
 }
 
-export function getProvider(): providers.Web3Provider | providers.JsonRpcProvider {
+export function getProvider(): Web3Provider | JsonRpcProvider {
   if (window.ethereum) {
-    return new providers.Web3Provider(window.ethereum);
+    return new Web3Provider(window.ethereum);
   } else {
-    return new providers.JsonRpcProvider(`http://localhost:${process.env.GANACHE_PORT}`);
+    return new JsonRpcProvider(`http://localhost:${process.env.GANACHE_PORT}`);
   }
 }
 
