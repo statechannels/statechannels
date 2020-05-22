9e41f874ed9a44b1190194e1041c6d84ae31c577
Alex Gap <alex.gap@consensys.net>
2020-05-21 21:38:49 -0700

    Lint run

--- src/workflows/tests/application.test.ts
+++ src/workflows/tests/application.test.ts
@@ -1,11 +1,11 @@
 import {interpret} from 'xstate';
-import {ethers} from 'ethers';
+import {ethers, BigNumber} from 'ethers';
 import waitForExpect from 'wait-for-expect';
 import {Store} from '../../store';
 import {StateVariables, isOpenChannel} from '../../store/types';
 import {ChannelStoreEntry} from '../../store/channel-store-entry';
 import {MessagingService, MessagingServiceInterface} from '../../messaging';
-import {BigNumber} from 'ethers';
+
 import {simpleEthAllocation, exists} from '../../utils';
 import {ChannelUpdated, JoinChannelEvent} from '../../event-types';
 import {Application} from '..';
