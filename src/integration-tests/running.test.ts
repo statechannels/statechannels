f87be3dd0cec524df907eff39152092c39017f4f
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-05-25 10:45:12 -0700

    Refactor running test to block on error/response/notification messages

--- src/integration-tests/running.test.ts
+++ src/integration-tests/running.test.ts
@@ -1,11 +1,41 @@
 import {FakeChain} from '../chain';
 import {Player, hookUpMessaging, generatePlayerUpdate} from './helpers';
-import waitForExpect from 'wait-for-expect';
 import {simpleEthAllocation} from '../utils';
-import {first} from 'rxjs/operators';
 import {CHAIN_NETWORK_ID} from '../config';
 import {BigNumber, constants} from 'ethers';
+import {ErrorResponse} from '@statechannels/client-api-schema/src';
 jest.setTimeout(30000);
+
+const resolveOnError = (player: Player, errorCode: ErrorResponse['error']['code']) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => reject(`Timed out waiting for error code ${errorCode}`), 3000);
+    player.messagingService.outboxFeed.subscribe(message => {
+      if ('error' in message) {
+        if (message.error.code === errorCode) resolve(message.error);
+        else reject(`Expected code ${errorCode} but received code ${message.error.code}`);
+      }
+    });
+  });
+
+const resolveOnResponse = (player: Player) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => reject('Timed out waiting for update channel response'), 3000);
+    player.messagingService.outboxFeed.subscribe(
+      message => 'id' in message && 'result' in message && resolve(message.result)
+    );
+  });
+
+const resolveOnNotification = (player: Player) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => reject('Timed out waiting for channel updated notification'), 3000);
+    player.messagingService.outboxFeed.subscribe(
+      message =>
+        !('id' in message) &&
+        'method' in message &&
+        message.method === 'ChannelUpdated' &&
+        resolve(message.params)
+    );
+  });
 
 test('accepts states when running', async () => {
   const fakeChain = new FakeChain();
@@ -21,6 +51,13 @@ test('accepts states when running', async () => {
     fakeChain
   );
   const players = [playerA, playerB];
+  const expectTurnNumber = async (n: number) =>
+    Promise.all(
+      players.map(async player => {
+        expect(player.workflowState).toEqual('running');
+        expect((await player.store.getEntry(channelId)).latest.turnNum.toNumber()).toBe(n);
+      })
+    );
 
   const amount = BigNumber.from('0x06f05b59d3b20000');
   const outcome = simpleEthAllocation(players.map(({destination}) => ({destination, amount})));
@@ -41,41 +78,27 @@ test('accepts states when running', async () => {
     signatures: []
   });
   const {channelId} = await players.map(({store}) => store.addState(signedState))[0];
+  const applicationDomain = 'localhost';
 
-  await playerB.store
-    .channelUpdatedFeed(channelId)
-    .pipe(first())
-    .toPromise();
-
-  const context: any = {channelId, applicationDomain: 'localhost', fundingStrategy: 'Direct'};
+  const context: any = {channelId, applicationDomain, fundingStrategy: 'Direct'};
   playerA.startAppWorkflow('running', context);
   playerB.startAppWorkflow('running', context);
   playerA.workflowMachine?.send('SPAWN_OBSERVERS');
   playerB.workflowMachine?.send('SPAWN_OBSERVERS');
-  await playerA.messagingService.receiveRequest(
-    generatePlayerUpdate(channelId, playerA.participant, playerB.participant),
-    'localhost'
-  );
 
-  await waitForExpect(async () => {
-    expect(playerA.workflowState).toEqual('running');
-    expect(playerB.workflowState).toEqual('running');
-    const playerATurnNum = (await playerA.store.getEntry(channelId)).latest.turnNum.toNumber();
-    expect(playerATurnNum).toBe(turnNum.add(1).toNumber());
-    const playerBTurnNum = (await playerB.store.getEntry(channelId)).latest.turnNum.toNumber();
-    expect(playerBTurnNum).toBe(turnNum.add(1).toNumber());
-  }, 3000);
+  const update = generatePlayerUpdate(channelId, playerA.participant, playerB.participant);
+  playerB.messagingService.receiveRequest(update, applicationDomain);
+  await resolveOnError(playerB, 400);
 
-  await playerB.messagingService.receiveRequest(
-    generatePlayerUpdate(channelId, playerA.participant, playerB.participant),
-    'localhost'
-  );
-  await waitForExpect(async () => {
-    expect(playerA.workflowState).toEqual('running');
-    expect(playerB.workflowState).toEqual('running');
-    const playerATurnNum = (await playerA.store.getEntry(channelId)).latest.turnNum.toNumber();
-    expect(playerATurnNum).toBe(turnNum.add(2).toNumber());
-    const playerBTurnNum = (await playerB.store.getEntry(channelId)).latest.turnNum.toNumber();
-    expect(playerBTurnNum).toBe(turnNum.add(2).toNumber());
-  }, 3000);
+  playerA.messagingService.receiveRequest(update, applicationDomain);
+  await resolveOnResponse(playerA);
+  await resolveOnNotification(playerB);
+
+  await expectTurnNumber(turnNum.add(1).toNumber());
+
+  await playerB.messagingService.receiveRequest(update, applicationDomain);
+  await resolveOnResponse(playerB);
+  await resolveOnNotification(playerA);
+
+  await expectTurnNumber(turnNum.add(2).toNumber());
 });
