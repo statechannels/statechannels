1d6993661c463b197a5c38f6198f4b691560b762
Liam Horne <liam@lihorne.com>
2020-05-26 14:47:00 -0400

    Make persistent-seeder headless

--- packages/persistent-seeder/persistent-seeder.ts
+++ packages/persistent-seeder/persistent-seeder.ts
@@ -1,13 +1,15 @@
-import {setUpBrowser} from '@statechannels/e2e-tests/puppeteer/helpers';
+import {setUpBrowser, setupFakeWeb3} from '@statechannels/e2e-tests/puppeteer/helpers';
 import {uploadFile} from '@statechannels/e2e-tests/puppeteer/scripts/web3torrent';
 import {APP_URL as WEB3TORRENT_URL} from '@statechannels/e2e-tests/puppeteer/constants';
 
 export async function persistentSeeder(): Promise<void> {
   console.log('Opening browser');
-  const {browser, metamask} = await setUpBrowser(false, 0);
+  const {browser, metamask} = await setUpBrowser(true);
 
   console.log('Waiting on pages');
   const web3tTabA = (await browser.pages())[0];
+
+  await setupFakeWeb3(web3tTabA, -1);
 
   await web3tTabA.goto(WEB3TORRENT_URL + '/upload', {waitUntil: 'load'});
   await web3tTabA.bringToFront();
