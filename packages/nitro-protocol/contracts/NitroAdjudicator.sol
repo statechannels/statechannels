17e5b768b4c6fd8d8cd6eabfee41cfb8c69396f5
George C. Knee <georgeknee@googlemail.com>
2020-05-27 15:47:41 +0100

    [nitro-protocol] Tweaks before publishing package (#1971)
    * Expose getFixedPart
    
    * Add public validTransition wrapper
    
    * Fix return declaration

--- packages/nitro-protocol/contracts/NitroAdjudicator.sol
+++ packages/nitro-protocol/contracts/NitroAdjudicator.sol
@@ -184,6 +184,29 @@ contract NitroAdjudicator is Adjudicator, ForceMove {
 
         }
     }
+
+    /**
+    * @notice Check that the submitted pair of states form a valid transition (public wrapper for internal function _requireValidTransition)
+    * @dev Check that the submitted pair of states form a valid transition (public wrapper for internal function _requireValidTransition)
+    * @param nParticipants Number of participants in the channel.
+    transition
+    * @param isFinalAB Pair of booleans denoting whether the first and second state (resp.) are final.
+    * @param ab Variable parts of each of the pair of states
+    * @param turnNumB turnNum of the later state of the pair.
+    * @param appDefinition Address of deployed contract containing application-specific validTransition function.
+    * @return true if the later state is a validTransition from its predecessor, reverts otherwise.
+    */
+    function validTransition(
+        uint256 nParticipants,
+        bool[2] memory isFinalAB, // [a.isFinal, b.isFinal]
+        ForceMoveApp.VariablePart[2] memory ab, // [a,b]
+        uint256 turnNumB,
+        address appDefinition
+        ) public pure returns (bool) {
+
+        return _requireValidTransition(nParticipants, isFinalAB, ab, turnNumB, appDefinition);
+
+    }
 }
 
 
