3634866cb98840b79272ae4c0f4cbafa01756b01
Liam Horne <liam@lihorne.com>
2020-05-28 18:29:13 -0400

    Kill channels after two keep-alive pings with zero progress (#2010)
    * Kill channels after two keep-alive pings with zero progress
    
    * Use internal PaidStreamingExtension variable

--- packages/web3torrent/src/library/web3torrent-lib.ts
+++ packages/web3torrent/src/library/web3torrent-lib.ts
@@ -230,6 +230,15 @@ export default class WebTorrentPaidStreamingClient extends WebTorrent {
       if (!torrent.done && wire.amChoking) {
         wire._clearTimeout();
       }
+
+      if (wire.paidStreamingExtension.leechingChannelId) {
+        if (wire.downloaded === wire.paidStreamingExtension._keepAliveIncrementalDownloaded) {
+          this.closeChannel(wire, wire.paidStreamingExtension.leechingChannelId);
+        } else {
+          wire.paidStreamingExtension._keepAliveIncrementalDownloaded = wire.downloaded;
+        }
+      }
+
       this.emitTorrentUpdated(torrent.infoHash);
     });
 
