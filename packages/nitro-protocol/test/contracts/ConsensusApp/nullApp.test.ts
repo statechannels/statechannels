37658ad54c0e3234a7c2c76b2a6fa2dade8f6bad
George Knee <georgeknee@googlemail.com>
2020-06-03 13:42:25 +0100

    Add nullAp.test.ts

---
+++ packages/nitro-protocol/test/contracts/ConsensusApp/nullApp.test.ts
@@ -0,0 +1,48 @@
+import {expectRevert} from '@statechannels/devtools';
+import {Contract, Wallet} from 'ethers';
+import {AddressZero} from 'ethers/constants';
+import {getTestProvider, setupContracts} from '../../test-helpers';
+import NitroAdjudicatorArtifact from '../../../build/contracts/TESTNitroAdjudicator.json';
+import {getVariablePart, State, Channel} from '../../../src';
+
+const provider = getTestProvider();
+
+let NitroAdjudicator: Contract;
+beforeAll(async () => {
+  NitroAdjudicator = await setupContracts(
+    provider,
+    NitroAdjudicatorArtifact,
+    process.env.TEST_NITRO_ADJUDICATOR_ADDRESS
+  );
+});
+
+describe('null app', () => {
+  // eslint-disable-next-line jest/expect-expect
+  it('should revert when validTransition is called', async () => {
+    const channel: Channel = {
+      participants: [Wallet.createRandom().address, Wallet.createRandom().address],
+      chainId: '0x1',
+      channelNonce: '0x1',
+    };
+    const fromState: State = {
+      channel,
+      outcome: [],
+      turnNum: 1,
+      isFinal: false,
+      challengeDuration: 0x0,
+      appDefinition: AddressZero,
+      appData: '0x0',
+    };
+    const toState: State = {...fromState, turnNum: 2};
+
+    expectRevert(async () => {
+      await NitroAdjudicator.validTransition(
+        1,
+        [false, false],
+        [getVariablePart(fromState), getVariablePart(toState)],
+        5,
+        AddressZero
+      );
+    }, 'VM Exception while processing transaction: revert');
+  });
+});
