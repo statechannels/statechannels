8f02b40b423591e51afc580e76b766e1597a39a4
George Knee <georgeknee@googlemail.com>
2020-06-05 16:46:46 +0100

    Tweak Token.sol
    (breaking change in @openzeppelin/contracts)

--- packages/nitro-protocol/contracts/Token.sol
+++ packages/nitro-protocol/contracts/Token.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.6.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-/**
-  * @dev This contract extends an ERC20 implementation, and mints 10,000 tokens to the deploying account. Used for testing purposes.
-*/
-contract Token is ERC20 {
 
+/**
+ * @dev This contract extends an ERC20 implementation, and mints 10,000 tokens to the deploying account. Used for testing purposes.
+ */
+contract Token is ERC20 {
     /**
-    * @dev Constructor function minting 10,000 tokens to the msg.sender (deploying account).
-    */
-    constructor() public {
+     * @dev Constructor function minting 10,000 tokens to the msg.sender (deploying account).
+     */
+    constructor(uint256) public ERC20('TestToken', 'TEST') {
         _mint(msg.sender, 10000);
     }
 }
