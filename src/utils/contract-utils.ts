e9084629efc4d6aabf8f2079bf989d158407b3ce
Mike Kerzhner <mikerzhner@gmail.com>
2020-06-05 08:47:49 -0700

    xstate-wallet: remove bespoke reload logic

--- src/utils/contract-utils.ts
+++ src/utils/contract-utils.ts
@@ -6,7 +6,6 @@ import {MOCK_TOKEN, MOCK_ASSET_HOLDER_ADDRESS, ETH_TOKEN} from '../constants';
 import {BigNumber, providers} from 'ethers';
 
 let provider: providers.Web3Provider | providers.JsonRpcProvider;
-let network: string;
 
 export function assetHolderAddress(tokenAddress: string): string | undefined {
   if (BigNumber.from(tokenAddress).isZero()) return ETH_ASSET_HOLDER_ADDRESS;
@@ -31,29 +30,13 @@ export function getProvider(): providers.Web3Provider | providers.JsonRpcProvide
     } else {
       // https://github.com/ethers-io/ethers.js/issues/861#issuecomment-638031278
       provider = new providers.Web3Provider(window.ethereum, 'any');
-      provider.on('network', (newNetwork, oldNetwork) => {
+      provider.on('network', (_, oldNetwork) => {
         // When a Provider makes its initial connection, it emits a "network"
         // event with a null oldNetwork along with the newNetwork. So, if the
         // oldNetwork exists, it represents a changing network
         if (oldNetwork) {
-          window.location.reload();
+          //window.location.reload();
         }
-      });
-      // The code below is reloads the page on network change. This is needed due to:
-      // - Metamask no longer reloads the page on chain change: https://medium.com/metamask/no-longer-reloading-pages-on-network-change-fbf041942b44
-      // - ethers does not have an elegant way to update the provider on network change: https://github.com/MetaMask/metamask-extension/issues/8077#issuecomment-637338683
-
-      // window.ethereum.networkVersion seems to be initialized asynchronously and can be undefined.
-      // https://ethereum.stackexchange.com/questions/82994/window-ethereum-networkversion-undefined#comment103120_82995
-      network = window.ethereum.networkVersion;
-      // The correct event to use is chainChanged: https://docs.metamask.io/guide/ethereum-provider.html#methods-new-api
-      // As of Metmask version 7.7.9, this event does not seem to be working.
-      // Note about the callback below:
-      // - If window.ethereum.networkVersion is undefined, this callback will fire when the networkVersion is initialized.
-      // - If window.ethereum.networkVersion is defined, this callback will only fire when the network is changed in Metamask.
-      window.ethereum.on('networkChanged', (newNetwork: string) => {
-        if (!network) network = newNetwork;
-        if (network !== newNetwork) window.location.reload();
       });
     }
   } else {
