fec71f54d275d84fc9ced54fa4cef7e329b1a66e
Mike Kerzhner <mikerzhner@gmail.com>
2020-06-09 16:57:44 -0700

    web3torrent: update unsubscribe comment

--- packages/web3torrent/src/pages/file/File.tsx
+++ packages/web3torrent/src/pages/file/File.tsx
@@ -81,7 +81,9 @@ const File: React.FC<Props> = props => {
       .pipe(throttleTime(1_000 / MAX_FPS, undefined, {trailing: false, leading: true}))
       .subscribe(onTorrentUpdate);
 
-    // It is not clear why optional chaining is needed.
+    // It is not clear why the following error is sometimes thrown.
+    // TypeError: Cannot read property 'closed' of null
+
     return () => {
       try {
         subscription.unsubscribe();
