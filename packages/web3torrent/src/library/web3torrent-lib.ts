c982c459e93b53fbeb4cec4d4944e0761fe0ad9a
Mike Kerzhner <mikerzhner@gmail.com>
2020-06-09 12:08:29 -0700

    web3torrent: simplify trust calculation

--- packages/web3torrent/src/library/web3torrent-lib.ts
+++ packages/web3torrent/src/library/web3torrent-lib.ts
@@ -511,7 +511,7 @@ export default class WebTorrentPaidStreamingClient extends WebTorrent {
 
     const trust = wire.trust ?? INITIAL_PEER_TRUST;
     let numBlocksToPayFor = requests.length > trust ? trust : requests.length;
-    wire.trust = trust * 2 >= MAX_PEER_TRUST ? MAX_PEER_TRUST : trust * 2;
+    wire.trust = trust * 2 > MAX_PEER_TRUST ? MAX_PEER_TRUST : trust * 2;
     let tailBytes = 0;
 
     // On each wire, the algorithm tries to download the uneven piece (which is always the last piece)
