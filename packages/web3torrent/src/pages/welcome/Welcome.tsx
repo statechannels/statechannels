e5b6f2d30e5106bcc2cc40cc08eceecdf6ec2eb0
Liam Horne <liam@lihorne.com>
2020-06-09 10:14:08 -0400

    Update the subtitle slightly again on landing page

--- packages/web3torrent/src/pages/welcome/Welcome.tsx
+++ packages/web3torrent/src/pages/welcome/Welcome.tsx
@@ -21,8 +21,8 @@ const Welcome: React.FC<Props> = props => {
       </div>
       <div className="subtitle">
         <p>
-          Web3Torrent brings crypto payments to the torrenting world. Under the hood it uses a
-          paid-streaming extension to the{' '}
+          Web3Torrent brings crypto payments to the torrenting world. <br />
+          Under the hood it uses a paid-streaming extension to the{' '}
           <a href="https://webtorrent.io/" target="_blank" rel="noopener noreferrer">
             WebTorrent
           </a>{' '}
@@ -31,8 +31,8 @@ const Welcome: React.FC<Props> = props => {
             State Channels
           </a>{' '}
           technology to achieve seamless value transfer on the ethereum blockchain. Peers can share
-          files in a torrent swarm: and downloaders make micropayments to uploaders, for the data
-          they provide.
+          files in a torrent swarm and downloaders make micropayments to uploaders for the data they
+          provide.
         </p>
       </div>
       <h2>Download a sample file</h2>
