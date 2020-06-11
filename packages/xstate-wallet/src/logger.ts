90cd4b121bf924cf8664965e7542f593c6873088
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-06-11 13:29:02 -0600

    Remove unnecessary log

--- packages/xstate-wallet/src/logger.ts
+++ packages/xstate-wallet/src/logger.ts
@@ -45,9 +45,6 @@ const prettyPrint = LOG_TO_CONSOLE ? {translateTime: true} : false;
 const level = window.localStorage.LOG_LEVEL ?? LOG_LEVEL;
 const opts = {name, prettyPrint, browser, level};
 const logger = destination ? pino(opts, destination) : pino(opts);
-logger.on('level-change', (lvl, val, prevLvl, prevVal) => {
-  console.log('wallet: %s (%d) was changed to %s (%d)', lvl, val, prevLvl, prevVal);
-});
 
 window.addEventListener('message', event => {
   const key = 'LOG_LEVEL';
