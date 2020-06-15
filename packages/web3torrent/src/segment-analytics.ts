d6ddabbabb2658793b8e85a10109f357d67426e1
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-06-15 12:46:52 -0700

    [web3torrent] Add documentation around analytics

--- packages/web3torrent/src/segment-analytics.ts
+++ packages/web3torrent/src/segment-analytics.ts
@@ -1,3 +1,10 @@
 import _ from 'lodash';
+
+/**
+ * Segment analytics are injected in index.html
+ * This exposes them for ease of use, without having to refer to the window object,
+ * or worry about undefined functions
+ */
+
 export const track = window.analytics?.track ?? _.noop;
 export const identify = window.analytics?.identify ?? _.noop;
