1009da4e010a21f2dd527bc26bf79e728b80cb75
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-06-16 13:54:16 -0700

    Export track, identify segment callbacks

---
+++ src/segment-analytics.ts
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+
+/**
+ * Segment analytics are injected in index.html
+ * This exposes them for ease of use, without having to refer to the window object,
+ * or worry about undefined functions
+ */
+
+// TODO: Why are SegmentAnalytics types not usable?
+const analytics: {
+  track(
+    event: string,
+    properties?: Record<string, any>,
+    options?: any,
+    callback?: () => void
+  ): void;
+  identify(userId: string, callback?: () => void): void;
+} = (window as any).analytics;
+
+export const track = analytics.track ?? _.noop;
+export const identify = analytics?.identify ?? _.noop;
