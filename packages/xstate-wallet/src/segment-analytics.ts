f0785423e53769520988d7445859e8a2d7c13113
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-06-18 17:00:32 -0700

    [wallet, web3torrent] Fix segment analytics again ...

--- packages/xstate-wallet/src/segment-analytics.ts
+++ packages/xstate-wallet/src/segment-analytics.ts
@@ -1,10 +1,22 @@
 import _ from 'lodash';
 
-/**
- * Segment analytics are injected in index.html
- * This exposes them for ease of use, without having to refer to the window object,
- * or worry about undefined functions
- */
+/*
+Segment analytics are injected in index.html
+This exposes them for ease of use, without having to refer to the window object,
+or worry about undefined functions
+Since segment re-defines window.analytics after it's been loaded, at runtime,
+we need to make sure we're referring to the fresh ones.
+So, we intercept calls to track/identify, and fetch the latest window.analytics value
+*/
 
-export const track = window.analytics?.track ?? _.noop;
-export const identify = window.analytics?.identify ?? _.noop;
+type Action = 'track' | 'identify';
+const segmentFunction = (action: 'track' | 'identify') =>
+  window.analytics ? window.analytics[action] : _.noop;
+
+export const analytics: Pick<SegmentAnalytics.AnalyticsJS, Action> = {
+  track: (event, ...args) => segmentFunction('track')(event, ...args),
+  identify: (event, ...args) => segmentFunction('identify')(event, ...args)
+};
+
+const {track, identify} = analytics;
+export {track, identify};
