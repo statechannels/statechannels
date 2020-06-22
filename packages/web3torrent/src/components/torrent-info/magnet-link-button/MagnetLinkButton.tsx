6781d7a4cf2167f6e1583b165e75e3d83d4e2835
Liam Horne <liam@lihorne.com>
2020-06-22 08:22:52 -0400

    Add analytics for clicking the share button

--- packages/web3torrent/src/components/torrent-info/magnet-link-button/MagnetLinkButton.tsx
+++ packages/web3torrent/src/components/torrent-info/magnet-link-button/MagnetLinkButton.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {clipboardCopy} from '../../../utils/copy-to-clipboard';
+import {track} from '../../../segment-analytics';
 
 export interface MagnetLinkProps {
   linkText?: string;
@@ -16,6 +17,9 @@ export const MagnetLinkButton: React.FC<MagnetLinkProps> = props => {
       type="button"
       onClick={event => {
         event.preventDefault();
+        track('Magnet Link Copied', {
+          href: window.location.href
+        });
         clipboardCopy(window.location.href);
         setCopied(true);
         setTimeout(() => setCopied(false), 3000);
