c98333ab1f8fc5eb15e77870859f34248c11b3ea
George Knee <georgeknee@googlemail.com>
2020-06-23 14:56:21 +0100

    Add nitro signState

--- wallet-benchmarking/plot-benchmarks.py
+++ wallet-benchmarking/plot-benchmarks.py
@@ -11,13 +11,13 @@ labels = []
 values_list = []
 with open('./times.json') as json_file:
     data = json.load(json_file)
-    print(data)
     for key in data:
         labels.append(key)
         values_list.append(map(lambda x: float(x)*1e-6, data[key]))
 
+ax.set_ylim([-1, 20])
 plt.boxplot(values_list)
-plt.xticks([1, 2], labels)
+plt.xticks(np.arange(1, len(labels)+1), labels)
 ax.set_ylabel('ms')
 fig.tight_layout()
 plt.savefig("./benchmark.svg")
--- wallet-benchmarking/signatures.js
+++ wallet-benchmarking/signatures.js
@@ -1,11 +1,14 @@
 const {ethers} = require('ethers');
 const fs = require('fs');
+const {signState} = require('@statechannels/nitro-protocol');
+const {HashZero, AddressZero} = ethers.constants;
 
+const SAMPLES = 1000;
 const wallet = ethers.Wallet.createRandom();
 const results = {};
 let times;
 times = [];
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < SAMPLES; i++) {
   const before = process.hrtime()[1]; // in ns
   wallet.signMessage('test message');
   const after = process.hrtime()[1];
@@ -14,7 +17,7 @@ for (let i = 0; i < 3; i++) {
 results['ethers.signMessage'] = [...times];
 
 times = [];
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < SAMPLES; i++) {
   const before = process.hrtime()[1]; // in ns
   wallet.signMessage(
     'a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message '
@@ -23,6 +26,33 @@ for (let i = 0; i < 3; i++) {
   times.push(after - before);
 }
 results['ethers.signMessage2'] = [...times];
+
+times = [];
+for (let i = 0; i < SAMPLES; i++) {
+  const state = {
+    isFinal: false,
+    channel: {
+      chainId: '0x0',
+      channelNonce: '0x0',
+      participants: [wallet.address]
+    },
+    outcome: [
+      {
+        assetHolderAddress: AddressZero,
+        allocationItems: []
+      }
+    ],
+    appDefinition: AddressZero,
+    appData: HashZero,
+    challengeDuration: 1,
+    turnNum: 1
+  };
+  const before = process.hrtime()[1]; // in ns
+  signState(state, wallet.privateKey);
+  const after = process.hrtime()[1];
+  times.push(after - before);
+}
+results['nitro.signState'] = [...times];
 
 fs.writeFile('times.json', JSON.stringify(results), err => {
   if (err) throw err;
