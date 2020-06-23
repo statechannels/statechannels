388b494c253dbcfc86f93b4e7691b28c46f0ca02
George Knee <georgeknee@googlemail.com>
2020-06-23 14:42:36 +0100

    Support multiple datasets

--- wallet-benchmarking/signatures.js
+++ wallet-benchmarking/signatures.js
@@ -1,15 +1,29 @@
 const {ethers} = require('ethers');
-const ObjectsToCsv = require('objects-to-csv');
+const fs = require('fs');
+
 const wallet = ethers.Wallet.createRandom();
-
-const times = [];
-
-for (let i = 0; i < 1000; i++) {
-  const before = process.hrtime()[1];
+const results = {};
+let times;
+times = [];
+for (let i = 0; i < 3; i++) {
+  const before = process.hrtime()[1]; // in ns
   wallet.signMessage('test message');
   const after = process.hrtime()[1];
   times.push(after - before);
 }
+results['ethers.signMessage'] = [...times];
 
-const csv = new ObjectsToCsv([{'ethers.signMessage': times}], {append: true});
-csv.toDisk('times.csv');
+times = [];
+for (let i = 0; i < 3; i++) {
+  const before = process.hrtime()[1]; // in ns
+  wallet.signMessage(
+    'a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message a much longer message '
+  );
+  const after = process.hrtime()[1];
+  times.push(after - before);
+}
+results['ethers.signMessage2'] = [...times];
+
+fs.writeFile('times.json', JSON.stringify(results), err => {
+  if (err) throw err;
+});
