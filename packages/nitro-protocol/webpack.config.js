ce94e090dbd7a005c916d41acd437d7e15770666
George Knee <georgeknee@googlemail.com>
2020-06-30 11:08:31 +0100

    add webpack config

---
+++ packages/nitro-protocol/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'test'),
+          path.resolve(__dirname, 'build/contracts/*.json'),
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+    mainFields: ['unpkg', 'browser', 'module', 'main'],
+  },
+  target: 'web',
+  mode: 'production',
+  output: {
+    filename: 'nitro-protocol.min.js',
+    libraryTarget: 'window',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    fs: 'empty',
+    child_process: 'empty',
+  },
+};
+
+module.exports = [config];
