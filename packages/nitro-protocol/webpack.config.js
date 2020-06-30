75bf2df3fe1efcfb57719cbe0c339e389331ec40
George Knee <georgeknee@googlemail.com>
2020-06-30 13:03:20 +0100

    Webpack uses tsc output

--- packages/nitro-protocol/webpack.config.js
+++ packages/nitro-protocol/webpack.config.js
@@ -3,24 +3,8 @@
 const path = require('path');
 
 const config = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.(ts|json)$/,
-        use: 'ts-loader',
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'test'),
-          path.resolve(__dirname, 'build/contracts/*.json'),
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts'],
-    mainFields: ['unpkg', 'browser', 'module', 'main'],
-  },
+  entry: './lib/src/index.js',
+  module: {},
   target: 'web',
   mode: 'production',
   output: {
