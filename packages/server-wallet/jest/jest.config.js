d0722267e54546bb1ded070c9f0ad0ff4b9f0da5
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-08 12:12:23 -0700

    Simplify jest config

--- packages/server-wallet/jest/jest.config.js
+++ packages/server-wallet/jest/jest.config.js
@@ -3,32 +3,14 @@ const root = resolve(__dirname, '../');
 
 module.exports = {
   rootDir: root,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
   setupFiles: ['./env.js'],
   setupFilesAfterEnv: ['./jest/knex-setup-teardown.ts'],
   testMatch: ['<rootDir>/**/__test__/**/?(*.)(spec|test).ts'],
   testEnvironment: 'node',
   testURL: 'http://localhost',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$'],
-  moduleFileExtensions: [
-    'web.ts',
-    'ts',
-    'web.tsx',
-    'tsx',
-    'web.js',
-    'js',
-    'web.jsx',
-    'jsx',
-    'json',
-    'node',
-    'mjs'
-  ],
-  globals: {
-    'ts-jest': {
-      tsConfig: './tsconfig.json'
-    }
-  }
+  transform: {'^.+\\.ts$': 'ts-jest'},
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|ts)$'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  globals: {'ts-jest': {tsConfig: './tsconfig.json'}}
 };
