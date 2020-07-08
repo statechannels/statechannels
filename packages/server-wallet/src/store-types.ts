426d6ed5983d487f1f885427ea33f78176db7aab
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-07 17:15:26 -0700

    Change store types

--- packages/server-wallet/src/store-types.ts
+++ packages/server-wallet/src/store-types.ts
@@ -1,52 +1,45 @@
-import {BigNumber} from 'ethers';
-import {Funding} from './store';
 import {FundingStrategy} from '@statechannels/client-api-schema/src';
-import {SignatureEntry, ChannelStoreEntry} from './channel-store-entry';
 
-export interface DomainBudget {
-  domain: string;
-  hubAddress: string;
-  forAsset: Record<string, AssetBudget | undefined>;
-}
+type Brand<T, K extends string> = T & {[key in K]: void};
+// TODO: These should each have validators
+type Address = Brand<string, '_isAddress'>;
+type Bytes = Brand<string, '_isBytes'>;
+type Signature = Brand<string, '_isSignature'>;
+type Bytes32 = Brand<string, '_isBytes32'>;
+type Uint48 = Brand<number, '_isUint48'>;
+type Uint256 = Brand<string, '_isUint256'>;
+type Destination = Brand<string, '_isDestination'>;
 
-interface ChannelBudgetEntry {
-  amount: BigNumber;
-}
-export interface AssetBudget {
-  assetHolderAddress: string;
-  availableReceiveCapacity: BigNumber;
-  availableSendCapacity: BigNumber;
-  channels: Record<string, ChannelBudgetEntry>;
-}
+export type SignatureEntry = {signature: Signature; signer: Address};
+
 export interface Participant {
   participantId: string;
-  signingAddress: string;
+  signingAddress: Address;
   destination: Destination;
 }
 // signers
 
 export interface StateVariables {
   outcome: Outcome;
-  turnNum: number;
-  appData: string;
+  turnNum: Uint48;
+  appData: Bytes;
   isFinal: boolean;
 }
 export type StateVariablesWithHash = StateVariables & Hashed;
-export type Destination = string & {_isDestination: void};
 export interface AllocationItem {
   destination: Destination;
-  amount: BigNumber;
+  amount: Uint256;
 }
 export interface SimpleAllocation {
   type: 'SimpleAllocation';
-  assetHolderAddress: string;
+  assetHolderAddress: Address;
   allocationItems: AllocationItem[];
 }
 export interface SimpleGuarantee {
   type: 'SimpleGuarantee';
-  targetChannelId: string;
-  assetHolderAddress: string;
-  destinations: string[];
+  targetChannelId: Bytes32;
+  assetHolderAddress: Address;
+  destinations: Destination[];
 }
 export interface MixedAllocation {
   type: 'MixedAllocation';
@@ -62,21 +55,21 @@ export function isAllocation(outcome: Outcome): outcome is Allocation {
 }
 
 export interface ChannelConstants {
-  chainId: string;
+  chainId: Bytes32;
   participants: Participant[];
-  channelNonce: number;
-  appDefinition: string;
-  challengeDuration: number;
+  channelNonce: Uint48;
+  appDefinition: Address;
+  challengeDuration: Uint48;
 }
 
-export interface State extends ChannelConstants, StateVariables {}
+export type State = {
+  consts: ChannelConstants;
+  vars: StateVariables;
+};
 
-interface Signed {
-  signatures: SignatureEntry[];
-}
-interface Hashed {
-  stateHash: string;
-}
+type Signed = {signatures: SignatureEntry[]};
+type Hashed = {stateHash: Bytes32};
+
 export type SignedState = State & Signed;
 export type SignedStateWithHash = SignedState & Hashed;
 
@@ -88,40 +81,21 @@ type _Objective<Name, Data> = {
   type: Name;
   data: Data;
 };
+
 export type OpenChannel = _Objective<
   'OpenChannel',
-  {
-    targetChannelId: string;
-    fundingStrategy: FundingStrategy;
-  }
+  {targetChannelId: Bytes32; fundingStrategy: FundingStrategy}
 >;
 export type VirtuallyFund = _Objective<
   'VirtuallyFund',
-  {
-    targetChannelId: string;
-    jointChannelId: string;
-  }
+  {targetChannelId: Bytes32; jointChannelId: Bytes32}
 >;
 export type FundGuarantor = _Objective<
   'FundGuarantor',
-  {
-    jointChannelId: string;
-    ledgerId: string;
-    guarantorId: string;
-  }
+  {jointChannelId: Bytes32; ledgerId: Bytes32; guarantorId: Bytes32}
 >;
-export type FundLedger = _Objective<
-  'FundLedger',
-  {
-    ledgerId: string;
-  }
->;
-export type CloseLedger = _Objective<
-  'CloseLedger',
-  {
-    ledgerId: string;
-  }
->;
+export type FundLedger = _Objective<'FundLedger', {ledgerId: Bytes32}>;
+export type CloseLedger = _Objective<'CloseLedger', {ledgerId: Bytes32}>;
 export type Objective = OpenChannel | VirtuallyFund | FundGuarantor | FundLedger | CloseLedger;
 
 const guard = <T extends Objective>(name: Objective['type']) => (o: Objective): o is T =>
@@ -139,95 +113,6 @@ export interface Message {
 
 export type ChannelStoredData = {
   stateVariables: Array<SignedStateWithHash>;
-  channelConstants: Omit<ChannelConstants, 'challengeDuration' | 'channelNonce'> & {
-    challengeDuration: number;
-    channelNonce: number;
-  };
-  funding: Funding | undefined;
-  applicationDomain: string | undefined;
+  channelConstants: ChannelConstants;
   myIndex: number;
 };
-export interface DBBackend {
-  initialize(cleanSlate: boolean, name: string): Promise<any>;
-
-  // TODO: Perhaps the backend API should look more like this?
-  // privateKeys(): Promise<Array<{signingAddress: string; privateKey: string}>>;
-  privateKeys(): Promise<Record<string, string | undefined>>;
-  ledgers(): Promise<Record<string, string | undefined>>;
-  nonces(): Promise<Record<string, number | undefined>>;
-  objectives(): Promise<Objective[]>;
-  channels(): Promise<Record<string, ChannelStoreEntry | undefined>>;
-
-  setDestinationAddress(destinationAddress: string): Promise<string>;
-  getDestinationAddress(): Promise<string | undefined>;
-
-  setPrivateKey(key: string, value: string): Promise<string>;
-  getPrivateKey(key: string): Promise<string | undefined>;
-
-  setChannel(key: string, value: ChannelStoredData): Promise<ChannelStoredData>;
-  getChannel(key: string): Promise<ChannelStoreEntry | undefined>;
-
-  getBudget(key: string): Promise<DomainBudget | undefined>;
-  setBudget(key: string, budget: DomainBudget): Promise<DomainBudget>;
-  deleteBudget(key: string): Promise<void>;
-
-  setLedger(key: string, value: string): Promise<string>;
-  getLedger(key: string): Promise<string | undefined>;
-
-  setNonce(key: string, value: number): Promise<number>;
-  getNonce(key: string): Promise<number | undefined>;
-
-  setObjective(key: number, value: Objective): Promise<Objective>;
-  getObjective(key: number): Promise<Objective | undefined>;
-
-  /**
-   * Starts an async database transaction.
-   *
-   * When mode is 'readwrite', acquires a lock on each store listed in stores param.
-   *
-   * dexie backend rejects with 'NotFoundError: TableX not part of transaction', if cb
-   * attempts to use table not listed by stores param.
-   *
-   * Rejects if tx.abort() is called.
-   *
-   * @param mode
-   * @param stores array of ObjectStore names usd in cb
-   * @param cb callback to execute within transaction scope
-   * @returns promise resolving to return value of cb
-   */
-  transaction<T, S extends ObjectStores>(
-    mode: TXMode,
-    stores: S[],
-    cb: (tx: Transaction) => Promise<T>
-  ): Promise<T>;
-  transactionOngoing: boolean;
-}
-
-export type Transaction = {
-  abort(): void;
-  // TODO: We could expose a store function on the transaction and
-  // potentially do away with the individual getters on the backend interface
-  // EG:
-  // store<S extends Stores>(s: S): ObjectStore<S>;
-  // Or, we could just expose direct properties, like
-  // channels(): Table<ChannelRecord>
-};
-
-export type TXMode = 'readonly' | 'readwrite';
-
-export const enum ObjectStores {
-  channels = 'channels',
-  objectives = 'objectives',
-  nonces = 'nonces',
-  privateKeys = 'privateKeys',
-  destinationAddress = 'destinationAddress',
-  ledgers = 'ledgers',
-  budgets = 'budgets'
-}
-
-declare global {
-  interface Window {
-    channelProvider: import('@statechannels/channel-provider').ChannelProviderInterface;
-    ethereum: any;
-  }
-}
