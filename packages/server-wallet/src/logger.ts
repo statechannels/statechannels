16b379fc8bc852feba17eb1ec053aefbe2695de6
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-08 12:20:03 -0700

    Simplify logger

--- packages/server-wallet/src/logger.ts
+++ packages/server-wallet/src/logger.ts
@@ -1,10 +1,3 @@
 import pino from 'pino';
 
-let _logger: pino.Logger;
-
-export function logger() {
-  if (!_logger) {
-    _logger = pino();
-  }
-  return _logger;
-}
+export const logger = pino();
