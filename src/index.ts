d6fbd180f42d3cb1642147156791f5184ecf9900
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-08 17:27:48 -0700

    Remove services
    I don't think this code is particularly useful anymore

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import { Bytes } from './types';
 import { queries } from './db/queries/channels';
-import { formResponse, nextState, validSignature } from './services/channelManager';
 
 import errors from './errors';
-import { updateLedgerChannel } from './services/ledgerChannelManager';
-import { State } from './store-types';
 export { errors };
 
 export default class Wallet {
   sanitize: (appAttrs: any) => Bytes;
-  validSignature = validSignature;
   updateChannel = queries.updateChannel;
-  updateLedgerChannel = updateLedgerChannel;
-  nextState = nextState;
 
   constructor(sanitizeAppAttrs) {
     this.sanitize = sanitizeAppAttrs;
   }
-
-  formResponse = (state: State) => formResponse(state);
 }
