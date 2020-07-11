f7f4275b782c991ac201a721dec511c46061e681
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-10 19:54:41 -0700

    Channel seeds uses fixtures

--- src/db/seeds/1_channel_seeds.ts
+++ src/db/seeds/1_channel_seeds.ts
@@ -1,61 +1,12 @@
-import { Model } from 'objection';
-import {
-  allocationOutcome2,
-  DUMMY_RULES_ADDRESS,
-} from '../../test/test-constants';
-import {
-  BEGINNING_APP_CHANNEL_ID,
-  beginningAppPhaseChannel,
-  ONGOING_APP_CHANNEL_ID,
-  ongoingAppPhaseChannel,
-} from '../../test/test-data';
-import Channel, { ChannelColumns } from '../../models/channel';
+import { Channel } from '../../models/channel';
+import { channel } from '../../wallet/__test__/fixtures/channel';
 import knex from '../../db-admin/db-admin-connection';
-import { SignedStateVarsWithHash } from '@statechannels/wallet-core';
-import { extractVariables } from '../../state-utils';
-
-Model.knex(knex);
-
-const base = {
-  appDefinition: DUMMY_RULES_ADDRESS,
-  challengeDuration: 1000,
-  chainId: '0x01',
-  channelNonce: 1,
-  participants: [],
-  isFinal: false,
-  appData: '',
-  signatures: [],
-  stateHash: '0x123',
-};
-
-// ***************
-// Ledger channels
-// ***************
-
-function postfundSetupState(turnNum: number): SignedStateVarsWithHash {
-  return { ...base, turnNum, outcome: allocationOutcome2 };
-}
-
-function appState(turnNum: number): SignedStateVarsWithHash {
-  return extractVariables({ ...base, turnNum, outcome: allocationOutcome2 });
-}
-
-const beginningAppPhaseChannelWithStates: ChannelColumns = {
-  ...beginningAppPhaseChannel,
-  channelId: BEGINNING_APP_CHANNEL_ID,
-  vars: [postfundSetupState(2), postfundSetupState(3)],
-};
-
-const ongoingAppPhaseChannelWithStates: ChannelColumns = {
-  ...ongoingAppPhaseChannel,
-  channelId: ONGOING_APP_CHANNEL_ID,
-  vars: [appState(4), appState(5)],
-};
+import { stateWithSignaturesAndHash } from '../../wallet/__test__/fixtures/states';
 
 const seeds = [
-  beginningAppPhaseChannelWithStates,
-  ongoingAppPhaseChannelWithStates,
-];
+  channel(),
+  channel({ channelNonce: 1234, vars: [stateWithSignaturesAndHash()] }),
+].map(c => c.toColumns());
 
 // *******
 // Exports
@@ -66,7 +17,3 @@ export function seed() {
     .del()
     .then(() => Channel.query().insert(seeds.map(Channel.prepareJsonBColumns)));
 }
-
-export const stateConstructors = {
-  postfundSetupState,
-};
