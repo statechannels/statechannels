ac42e6b14c5336f1194d682bcebda626669bb3f8
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-12 21:58:10 -0700

    Remove unused file

--- src/utilities/hex-utils.ts
+++
@@ -1,8 +0,0 @@
-import { BigNumber } from 'ethers';
-
-const bigNumberify = (n: number | string) => BigNumber.from(n);
-export function addHex(a: string, b: string): string {
-  return bigNumberify(a)
-    .add(bigNumberify(b))
-    .toHexString();
-}
