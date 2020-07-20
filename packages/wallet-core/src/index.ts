0f730c1c437b6f57b9f7832c8eb5985b5532949a
Liam Horne <liam@lihorne.com>
2020-07-20 20:37:12 +0300

    Update wallet-core distribution

--- packages/wallet-core/src/index.ts
+++ packages/wallet-core/src/index.ts
@@ -2,3 +2,8 @@ export * from './types';
 export * from './state-utils';
 export * from './utils';
 export * from './bignumber';
+
+export * from './serde/app-messages/deserialize';
+export * from './serde/app-messages/serialize';
+export * from './serde/wire-format/deserialize';
+export * from './serde/wire-format/serialize';
