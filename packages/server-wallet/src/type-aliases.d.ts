f27d0ba2cbe59191d177c49b68d351363b1d2615
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-21 09:03:30 -0600

    Remove unused file

--- packages/server-wallet/src/type-aliases.d.ts
+++
@@ -1,6 +0,0 @@
-declare type ParticipantId = string;
-declare type Address = string;
-declare type Uint256 = string;
-declare type Uint48 = number;
-declare type Bytes32 = string;
-declare type Bytes = string;
