94bf57958b5e59037105a11beba05f4c718cc024
Liam Horne <liam@lihorne.com>
2020-07-23 17:31:48 +0300

    Cleaner implementation

--- packages/server-wallet/src/http/app.ts
+++ packages/server-wallet/src/http/app.ts
@@ -1,46 +1,68 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import * as jayson from 'jayson';
+import {Server, Method} from 'jayson';
 import {configureEnvVariables} from '@statechannels/devtools';
 
 import '../db/connection'; // This is how the DB is "connected"
 
-import {Wallet, AddressedMessage} from '../wallet';
+import {Wallet, WalletInterface} from '../wallet';
 
 configureEnvVariables();
 
 const app = express();
 
-// Should we use this?
-type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
-
 // TODO: Request and response types are not as defined in client-api-schema
 // TODO: Type error responses in a way that doesn't wrap them inside JsonRpc boilerplate
 
-const jsonRpcServer = new jayson.Server({
-  example: (args: any, callback: any): void => {
-    callback(null, 'hello');
-  },
+const jsonRpcServer = new Server(
+  {},
+  {
+    router: function(
+      // TODO: Type method type
+      method: any
+    ): Method {
+      const wrapAsyncWalletMethod = (wallet: Wallet, method: keyof WalletInterface): Method =>
+        new Method(async function(args: any, done: any) {
+          let err = null;
+          let ret = undefined;
+          try {
+            ret = await wallet[method](args);
+          } catch (e) {
+            err = e;
+          } finally {
+            done(err, ret);
+          }
+        });
 
-  pushMessage: async (
-    args: AddressedMessage,
-    callback: (err: any, ret?: ThenArg<ReturnType<typeof myWallet.pushMessage>>) => void
-  ): Promise<void> => {
-    try {
-      callback(null, await myWallet.pushMessage(args));
-    } catch (e) {
-      callback(e.toString());
-    }
-  },
+      switch (method) {
+        // TODO: Remove
+        case 'example':
+          return new Method(function(args: any, done: any) {
+            done(null, 'hello');
+          });
 
-  // TODO: Implement more methods
-  // createChannel: (): void => { return; },
-  // joinChannel: (): void => { return; },
-  // updateChannel: (): void => { return; },
-  // closeChannel: (): void => { return; },
-  // getChannels: (): void => { return; },
-  // onNotification: (): void => { return; },
-});
+        case 'CreateChannel':
+          return wrapAsyncWalletMethod(myWallet, 'createChannel');
+
+        case 'UpdateChannel':
+          return wrapAsyncWalletMethod(myWallet, 'updateChannel');
+
+        case 'CloseChannel':
+          return wrapAsyncWalletMethod(myWallet, 'closeChannel');
+
+        case 'PushMessage':
+          return wrapAsyncWalletMethod(myWallet, 'pushMessage');
+
+        // TODO: Add the other methods here
+
+        default:
+          return new Method((args: any, done: any) =>
+            done({code: 404, message: `Wallet does not implement ${method}`})
+          );
+      }
+    },
+  }
+);
 
 app.use(bodyParser.json());
 app.use(jsonRpcServer.middleware());
