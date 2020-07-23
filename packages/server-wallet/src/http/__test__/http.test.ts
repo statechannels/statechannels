5777615a99719899469b954da991aaec8b4ca156
Liam Horne <liam@lihorne.com>
2020-07-23 17:40:00 +0300

    add resp to basic test

--- packages/server-wallet/src/http/__test__/http.test.ts
+++ packages/server-wallet/src/http/__test__/http.test.ts
@@ -3,12 +3,12 @@ import request from 'supertest';
 import app from '../app';
 
 describe('http server', () => {
-  it('can receive jsonrpc encoded calls over http post', async () => {
-    return request(app)
+  it('can receive jsonrpc encoded calls over http post and respond', async () =>
+    request(app)
       .post('/')
-      .send(JSON.stringify({method: 'example', id: 1, params: {}, jsonrpc: '2.0'}))
+      .send({method: 'example', id: 1, params: {}, jsonrpc: '2.0'})
       .set('Content-Type', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200);
-  });
+      .expect({jsonrpc: '2.0', id: 1, result: 'hello'})
+      .expect(200));
 });
