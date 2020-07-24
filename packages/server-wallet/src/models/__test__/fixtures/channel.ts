f6fbfa810602e8ce0451f38ffb9ab47b80ef76db
Alex Gap <alex.gap@consensys.net>
2020-07-24 10:57:31 -0700

    Rename FixtureProps

--- packages/server-wallet/src/models/__test__/fixtures/channel.ts
+++ packages/server-wallet/src/models/__test__/fixtures/channel.ts
@@ -2,12 +2,12 @@ import {calculateChannelId} from '@statechannels/wallet-core';
 import _ from 'lodash';
 
 import {Channel, RequiredColumns} from '../../../models/channel';
-import {Fixture, fixture, FixtureProps} from '../../../wallet/__test__/fixtures/utils';
+import {Fixture, fixture, DeepPartial} from '../../../wallet/__test__/fixtures/utils';
 import {addHash} from '../../../state-utils';
 import {alice} from '../../../wallet/__test__/fixtures/signingWallets';
 import {createState, stateSignedBy} from '../../../wallet/__test__/fixtures/states';
 
-export const channel: Fixture<Channel> = (props?: FixtureProps<RequiredColumns>) => {
+export const channel: Fixture<Channel> = (props?: DeepPartial<RequiredColumns>) => {
   const {appDefinition, chainId, challengeDuration, channelNonce, participants} = createState();
 
   const defaults: RequiredColumns = {
--- packages/server-wallet/src/wallet/__test__/fixtures/utils.ts
+++ packages/server-wallet/src/wallet/__test__/fixtures/utils.ts
@@ -1,9 +1,9 @@
 import _ from 'lodash';
-export type FixtureProps<T> = {
-  [P in keyof T]?: FixtureProps<T[P]>;
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
 };
-export type Fixture<T> = (props?: FixtureProps<T>) => T;
+export type Fixture<T> = (props?: DeepPartial<T>) => T;
 
 export const fixture = function<T>(defaults: T, modifier: Function = _.identity): Fixture<T> {
-  return (props?: FixtureProps<T>): T => modifier(_.merge(_.cloneDeep(defaults), props));
+  return (props?: DeepPartial<T>): T => modifier(_.merge(_.cloneDeep(defaults), props));
 };
