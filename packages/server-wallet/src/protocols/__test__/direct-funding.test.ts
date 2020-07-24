3aa67ae9ace2505cf9ae56f77a48fbb03cb00fcb
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-24 10:01:36 -0600

    Tidy up test data

--- packages/server-wallet/src/protocols/__test__/direct-funding.test.ts
+++ packages/server-wallet/src/protocols/__test__/direct-funding.test.ts
@@ -1,10 +1,11 @@
-import {simpleEthAllocation, makeDestination, StateVariables, BN} from '@statechannels/wallet-core';
+import {simpleEthAllocation, makeDestination, BN} from '@statechannels/wallet-core';
 import matchers from '@pacote/jest-either';
 import {some, none} from 'fp-ts/lib/Option';
 
 import {protocol} from '../direct-funding';
 import {alice} from '../../wallet/__test__/fixtures/signingWallets';
 import {Address, Uint256} from '../../type-aliases';
+import {stateVars} from '../../wallet/__test__/fixtures/state-vars';
 
 import {
   directFundingProtocolState,
@@ -13,41 +14,25 @@ import {
 
 expect.extend(matchers);
 
-it('generates an action to sign the post fund setup', async () => {
-  const outcome = simpleEthAllocation([
-    {amount: BN.from('0x5'), destination: makeDestination(alice().address)},
-  ]);
-  const prefundState: Partial<StateVariables> = {
-    turnNum: 0,
-    outcome,
-  };
+const outcome = simpleEthAllocation([
+  {amount: BN.from('0x5'), destination: makeDestination(alice().address)},
+]);
+const prefundState = stateVars({outcome, turnNum: 0});
+const postFundState = stateVars({outcome, turnNum: 3});
 
+it('generates an action to sign the post fund setup', async () => {
   const funding: Record<Address, Uint256> = {[outcome.assetHolderAddress]: '0x5'};
   const protocolState = directFundingProtocolState(
     withSupportedState(prefundState)({app: {funding}})
   );
 
-  expect(await protocol(protocolState)).toMatchRight(
-    some({
-      type: 'SignState',
-      ...prefundState,
-      turnNum: 3,
-    })
-  );
+  expect(await protocol(protocolState)).toMatchRight(some({type: 'SignState', ...postFundState}));
 });
 
 it('generates no actions if the post fund setup is signed', async () => {
-  const outcome = simpleEthAllocation([
-    {amount: BN.from('0x5'), destination: makeDestination(alice().address)},
-  ]);
-  const postfundSetup: Partial<StateVariables> = {
-    turnNum: 3,
-    outcome,
-  };
-
   const funding: Record<Address, Uint256> = {[outcome.assetHolderAddress]: '0x5'};
   const protocolState = directFundingProtocolState(
-    withSupportedState(postfundSetup)({app: {funding}})
+    withSupportedState(postFundState)({app: {funding}})
   );
 
   expect(await protocol(protocolState)).toMatchRight(none);
