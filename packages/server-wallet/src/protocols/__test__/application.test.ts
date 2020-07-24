bc40aff27c2f72a9ed735f89f505f3bff026fa81
Alex Gap <alex.gap@consensys.net>
2020-07-24 10:52:32 -0700

    Use wallet core uint256

--- packages/server-wallet/src/protocols/__test__/application.test.ts
+++ packages/server-wallet/src/protocols/__test__/application.test.ts
@@ -1,10 +1,9 @@
-import {simpleEthAllocation, makeDestination, BN} from '@statechannels/wallet-core';
+import {simpleEthAllocation, makeDestination, BN, Uint256} from '@statechannels/wallet-core';
 import matchers from '@pacote/jest-either';
 import {some, none} from 'fp-ts/lib/Option';
 
 import {protocol} from '../application';
 import {alice} from '../../wallet/__test__/fixtures/signingWallets';
-import {Uint256} from '../../type-aliases';
 
 import {
   directFundingProtocolState,
@@ -14,13 +13,13 @@ import {
 expect.extend(matchers);
 
 const outcome = simpleEthAllocation([
-  {amount: BN.from('0x5'), destination: makeDestination(alice().address)},
+  {amount: BN.from(5), destination: makeDestination(alice().address)},
 ]);
 const prefundState = {outcome, turnNum: 0};
 const postFundState = {outcome, turnNum: 3};
 
 it('generates an action to sign the post fund setup', async () => {
-  const funding = (): Uint256 => '0x5';
+  const funding = (): Uint256 => BN.from(5);
   const protocolState = directFundingProtocolState(
     withSupportedState(prefundState)({app: {funding}})
   );
@@ -29,7 +28,7 @@ it('generates an action to sign the post fund setup', async () => {
 });
 
 it('generates no actions if the post fund setup is signed', async () => {
-  const funding = (): Uint256 => '0x5';
+  const funding = (): Uint256 => BN.from(5);
   const protocolState = directFundingProtocolState(
     withSupportedState(postFundState)({app: {funding}})
   );
