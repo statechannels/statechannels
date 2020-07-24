5bfef93ab6fcc1f235fe39cfb47ffaa15cc3c068
Alex Gap <alex.gap@consensys.net>
2020-07-24 10:48:40 -0700

    Rename to application

--- packages/server-wallet/src/protocols/__test__/fixtures/application-protocol-state.ts
+++ packages/server-wallet/src/protocols/__test__/fixtures/application-protocol-state.ts
@@ -2,7 +2,7 @@ import {StateVariables} from '@statechannels/wallet-core';
 
 import {fixture, Fixture} from '../../../wallet/__test__/fixtures/utils';
 import {channel} from '../../../models/__test__/fixtures/channel';
-import {ProtocolState} from '../../direct-funding';
+import {ProtocolState} from '../../application';
 
 export const directFundingProtocolState = fixture({app: channel().protocolState});
 export const withSupportedState = (stateVars: Partial<StateVariables>): Fixture<ProtocolState> =>
