dcbfac54eaeeb6b83c46c4de72976ff8c385e0f5
Mike Kerzhner <mikerzhner@gmail.com>
2020-07-27 10:04:43 -0700

    Factor out helper of updateChannel

--- packages/server-wallet/src/handlers/updateChannel.ts
+++ packages/server-wallet/src/handlers/updateChannel.ts
@@ -2,6 +2,7 @@ import {Either, left, right, chain, map} from 'fp-ts/lib/Either';
 import {SignedStateWithHash, StateVariables, Outcome} from '@statechannels/wallet-core';
 import {pipe} from 'fp-ts/lib/function';
 import {ChannelId} from '@statechannels/client-api-schema';
+import {curry} from 'lodash';
 
 import {ProtocolAction, SignState} from '../protocols/actions';
 import {ChannelState} from '../protocols/state';
@@ -33,6 +34,30 @@ class UpdateChannelError extends Error {
 
 const hasSupportedState = (cs: ChannelState): cs is ChannelStateWithSupported => !!cs.supported;
 
+// The helper functions should be factored out, tested, and reusable
+function isMyTurn(ss: ChannelStateWithSupported): Either<Error, ChannelStateWithSupported> {
+  if (ss.supported.turnNum + (1 % ss.supported.participants.length) === ss.myIndex)
+    return right(ss);
+  return left(new UpdateChannelError(Errors.notMyTurn));
+}
+
+function hasRunningTurnNumber(
+  cs: ChannelStateWithSupported
+): Either<Error, ChannelStateWithSupported> {
+  if (cs.supported.turnNum < 3) return left(new UpdateChannelError(Errors.notInRunningStage));
+  return right(cs);
+}
+
+const incrementTurnNumber = (
+  args: UpdateChannelHandlerParams,
+  cs: ChannelStateWithSupported
+): StateVariables => ({
+  ...args,
+  turnNum: cs.supported.turnNum + 1,
+  isFinal: false,
+});
+// END helper functions
+
 export function updateChannel(
   args: UpdateChannelHandlerParams,
   channelState: ChannelState
@@ -42,20 +67,9 @@ export function updateChannel(
     cs: ChannelState
   ): Either<UpdateChannelError, ChannelStateWithSupported> =>
     hasSupportedState(cs) ? right(cs) : left(new UpdateChannelError(Errors.invalidLatestState));
-  const hasRunningTurnNumber: ValidateState = cs =>
-    cs.supported.turnNum < 3 ? left(new UpdateChannelError(Errors.notInRunningStage)) : right(cs);
-  const isMyTurn: ValidateState = ss =>
-    ss.supported.turnNum + (1 % ss.supported.participants.length) === ss.myIndex
-      ? right(ss)
-      : left(new UpdateChannelError(Errors.notMyTurn));
-  const newState = (cs: ChannelStateWithSupported): StateVariables => ({
-    ...args,
-    turnNum: cs.supported.turnNum + 1,
-    isFinal: false,
-  });
 
   // todo: use Action creator from another branch
-  const finalAction = (sv: StateVariables): SignState => ({
+  const signStateAction = (sv: StateVariables): SignState => ({
     type: 'SignState',
     channelId: args.channelId,
     ...sv,
@@ -66,7 +80,7 @@ export function updateChannel(
     ensureSupportedStateExists,
     chain(hasRunningTurnNumber),
     chain(isMyTurn),
-    map(newState),
-    map(finalAction)
+    map(curry(incrementTurnNumber)(args)),
+    map(signStateAction)
   );
 }
