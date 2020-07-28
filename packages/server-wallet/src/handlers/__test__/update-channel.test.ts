9ae59c851a920db4f5a161534f2a8655e276802c
Mike Kerzhner <mikerzhner@gmail.com>
2020-07-28 16:52:16 -0700

    Add no supported state test

--- packages/server-wallet/src/handlers/__test__/update-channel.test.ts
+++ packages/server-wallet/src/handlers/__test__/update-channel.test.ts
@@ -4,6 +4,7 @@ import {left} from 'fp-ts/lib/Either';
 import {updateChannel, UpdateChannelError} from '../update-channel';
 import {updateChannelFixture, signStateFixture} from '../fixtures/update-channel';
 import {channelStateFixture} from '../../protocols/__test__/fixtures/channel-state';
+import {ChannelState} from '../../protocols/state';
 
 expect.extend(matchers);
 
@@ -12,11 +13,14 @@ test('validUpdate', () => {
   expect(result).toEqualRight(signStateFixture());
 });
 
+const invalidLatest = new UpdateChannelError(UpdateChannelError.reasons.invalidLatestState);
 const notMyTurn = new UpdateChannelError(UpdateChannelError.reasons.notMyTurn);
 
+const noSupportedState: ChannelState = {...channelStateFixture(), supported: undefined};
 test.each`
   updateChannelArgs         | channelState                                      | result
+  ${updateChannelFixture()} | ${noSupportedState}                               | ${invalidLatest}
   ${updateChannelFixture()} | ${channelStateFixture({supported: {turnNum: 4}})} | ${notMyTurn}
-`('error cases', ({updateChannelArgs, channelState, result}) => {
+`('error cases $result', ({updateChannelArgs, channelState, result}) => {
   expect(updateChannel(updateChannelArgs, channelState)).toMatchObject(left(result));
 });
