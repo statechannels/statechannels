f73a9c8d6b6f2ce9fa2bf427e58b288f2c2cca3a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-07-31 00:19:05 -0600

    Add test case showcasing that signState does not block concurrent updates

--- packages/server-wallet/src/wallet/__test__/lock-app.test.ts
+++ packages/server-wallet/src/wallet/__test__/lock-app.test.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import {ChannelResult} from '@statechannels/client-api-schema';
+import {StateVariables} from '@statechannels/wallet-core';
 
 import {Channel} from '../../models/channel';
 import {withSupportedState} from '../../models/__test__/fixtures/channel';
@@ -22,42 +23,77 @@ it('works', async () => {
   ).resolves.toMatchObject({channelResult: {turnNum: 6}});
 });
 
-it('works when run concurrently', async () => {
-  await seedAlicesSigningWallet(knex);
-  const c = withSupportedState(stateVars({turnNum: 5}))();
-  await Channel.query().insert(c);
+describe('concurrency', () => {
+  let channelId: string;
+  let numResolved: number;
+  let numRejected: number;
+  let numSettled: number;
+  let next: StateVariables;
 
-  const {channelId, latest} = c;
-  const next = {...latest, turnNum: latest.turnNum + 1};
-
+  let countResolvedPromise: any;
+  let countRejectedPromise: any;
+  let countSettledPromise: any;
   const numAttempts = 10;
-  let numResolved = 0;
-  let numRejected = 0;
-  let numSettled = 0;
-  const countResolvedPromise = ({channelResult}: {channelResult: ChannelResult}): any => {
-    expect(channelResult).toMatchObject({turnNum: 6});
-    numResolved += 1;
-  };
-  const countRejectedPromise = (error: Error): any => {
-    expect(error).toMatchObject(new Error('Stale state'));
-    numRejected += 1;
-  };
-  const countSettledPromise = (): any => (numSettled += 1);
 
-  await Promise.all(
-    _.range(numAttempts).map(() =>
-      Store.lockApp(channelId, async tx => Store.signState(channelId, next, tx))
-        .then(countResolvedPromise)
-        .catch(countRejectedPromise)
-        .finally(countSettledPromise)
-    )
-  );
+  beforeEach(async () => {
+    await seedAlicesSigningWallet(knex);
+    const c = withSupportedState(stateVars({turnNum: 5}))();
+    await Channel.query().insert(c);
+    channelId = c.channelId;
 
-  expect(numResolved).toEqual(1);
-  expect(numRejected).toEqual(numAttempts - 1);
-  expect(numSettled).toEqual(numAttempts);
+    next = {...c.latest, turnNum: c.latest.turnNum + 1};
 
-  await expect(Store.getChannel(channelId, undefined)).resolves.toMatchObject({
-    latest: {turnNum: 6},
+    numResolved = 0;
+    numRejected = 0;
+    numSettled = 0;
+    countResolvedPromise = ({channelResult}: {channelResult: ChannelResult}): any => {
+      expect(channelResult).toMatchObject({turnNum: 6});
+      numResolved += 1;
+    };
+    countRejectedPromise = (error: Error): any => {
+      expect(error).toMatchObject(new Error('Stale state'));
+      numRejected += 1;
+    };
+    countSettledPromise = (): any => (numSettled += 1);
+  });
+
+  it('works when run concurrently', async () => {
+    await Promise.all(
+      _.range(numAttempts).map(() =>
+        Store.lockApp(channelId, async tx => Store.signState(channelId, next, tx))
+          .then(countResolvedPromise)
+          .catch(countRejectedPromise)
+          .finally(countSettledPromise)
+      )
+    );
+
+    expect([numResolved, numRejected, numSettled]).toMatchObject([1, 9, 10]);
+
+    expect(numResolved).toEqual(1);
+    expect(numRejected).toEqual(numAttempts - 1);
+    expect(numSettled).toEqual(numAttempts);
+
+    await expect(Store.getChannel(channelId, undefined)).resolves.toMatchObject({
+      latest: {turnNum: 6},
+    });
+  });
+
+  test('sign state does not block concurrent updates', async () => {
+    await Promise.all(
+      _.range(numAttempts).map(() =>
+        Store.signState(channelId, next, undefined as any)
+          .then(countResolvedPromise)
+          .catch(countRejectedPromise)
+          .finally(countSettledPromise)
+      )
+    );
+
+    expect(numResolved).toEqual(10);
+    expect(numRejected).toEqual(0);
+    expect(numSettled).toEqual(numAttempts);
+
+    await expect(Store.getChannel(channelId, undefined)).resolves.toMatchObject({
+      latest: {turnNum: 6},
+    });
   });
 });
