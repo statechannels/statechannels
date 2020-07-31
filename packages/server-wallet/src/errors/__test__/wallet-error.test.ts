6a1d4e7030df99fdae4d1613214b9571ee5ce9e9
Mike Kerzhner <mikerzhner@gmail.com>
2020-07-30 19:07:32 -0700

    Add isWalletError unit tests

---
+++ packages/server-wallet/src/errors/__test__/wallet-error.test.ts
@@ -0,0 +1,12 @@
+import {UpdateChannelError} from '../../handlers/update-channel';
+import {isWalletError} from '../wallet-error';
+
+test('passes typeguard', () => {
+  const updateChannelError = new UpdateChannelError(UpdateChannelError.reasons.channelNotFound);
+  expect(isWalletError(updateChannelError)).toBeTruthy();
+});
+
+test('fails typeguard', () => {
+  const fakeError = {type: 'bogusError'};
+  expect(isWalletError(fakeError)).toBeFalsy();
+});
