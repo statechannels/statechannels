fddcb4a1cf9da1303ef0e5b3492da64c25af7163
geoknee <georgeknee@googlemail.com>
2020-08-04 15:43:57 +0100

    Cleanup types
    - JsonRpc header types remain generic and ignorant of our application-specific types, and now live in schema package exclusively (not in channel provider anymore)
    - Our application-specific types are all prepended with StateChannels, e.g. StateChannelsRequest
    - parseNotification implemented and used in iframe-channel-provider
    - request ids made mandatory at the JSONRPC level, optional at the channelprovider.request level
    - Listeners callbakcs narrowed to StateChannelsResponse type

--- src/channel-wallet.ts
+++ src/channel-wallet.ts
@@ -2,7 +2,11 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import {interpret, Interpreter, State} from 'xstate';
 import {Guid} from 'guid-typescript';
-import {Notification, Response, ErrorResponse} from '@statechannels/client-api-schema';
+import {
+  StateChannelsNotification,
+  StateChannelsResponse,
+  StateChannelsErrorResponse
+} from '@statechannels/client-api-schema';
 import {filter, take} from 'rxjs/operators';
 import {
   Message,
@@ -183,13 +187,15 @@ export class ChannelWallet {
     }
   }
 
-  public async pushMessage(jsonRpcMessage, fromDomain) {
+  public async pushMessage(jsonRpcMessage: object, fromDomain: string) {
     // Update any workflows waiting on an observable
     await this.messagingService.receiveRequest(jsonRpcMessage, fromDomain);
   }
 
   public onSendMessage(
-    callback: (jsonRpcMessage: Notification | Response | ErrorResponse) => void
+    callback: (
+      jsonRpcMessage: StateChannelsNotification | StateChannelsResponse | StateChannelsErrorResponse
+    ) => void
   ) {
     this.messagingService.outboxFeed.subscribe(m => callback(m));
   }
