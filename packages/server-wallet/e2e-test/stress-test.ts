b09ff44b31dec41606027039bac39d4b1dbb5097
Alex Gap <alex.gap@consensys.net>
2020-08-04 14:06:56 -0700

    Add v1 stress test

--- packages/server-wallet/e2e-test/stress-test.ts
+++ packages/server-wallet/e2e-test/stress-test.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 import {Participant} from '@statechannels/client-api-schema';
-
+import yargs from 'yargs';
+import Table from 'cli-table3';
+import {Timer} from 'unitimer';
+import createTimer from 'unitimer';
 import {alice, bob} from '../src/wallet/__test__/fixtures/signing-wallets';
 import {Channel} from '../src/models/channel';
 import {withSupportedState} from '../src/models/__test__/fixtures/channel';
@@ -16,7 +19,6 @@ import {
   PongServer,
 } from '../e2e-test/e2e-utils';
 import PingClient from '../e2e-test/ping/client';
-
 async function seedTestChannels(
   ping: Participant,
   pingPrivateKey: string,
@@ -24,7 +26,7 @@ async function seedTestChannels(
   pongPrivateKey: string,
   numOfChannels: number
 ): Promise<string[]> {
-  const channelIds = [];
+  const channelIds: string[] = [];
   for (let i = 0; i < numOfChannels; i++) {
     const seed = withSupportedState(
       {turnNum: 3},
@@ -49,6 +51,25 @@ async function seedTestChannels(
 }
 
 (async function(): Promise<void> {
+ const { numChannels, numPings} = yargs
+  .command('stress-test', 'Ping many channels concurrently')
+    .example(
+      'stress-test --numChannels 20 --numPings 5',
+      'Runs the stress test using 20 channels calling ping 5 times on each channel'
+    )
+  .options({
+    numChannels: {type: 'number'},
+    numPings: {type: 'number'},
+  })
+    .default({numPings: 1, numChannels: 100}).argv;
+
+  process.on('exit', async () => {
+    if (pongServer) {
+      // TODO: Determine why killServer doesn't work node script
+      pongServer.server.kill();
+      await killServer(pongServer);
+    }
+  });
   let pongServer: PongServer | undefined = undefined;
   try {
     pongServer = startPongServer();
@@ -73,26 +94,52 @@ async function seedTestChannels(
       alice().privateKey,
       await pingClient.getPongsParticipantInfo(),
       bob().privateKey,
-      100
+      numChannels
     );
 
-    console.log('Starting test');
-    console.time('one ping one pong 100 channels');
-    const pingPromises = channelIds.map(c => pingClient.ping(c));
-    await Promise.all(pingPromises);
-    console.timeEnd('one ping one pong 100 channels');
-    // TODO: Determine why killServer doesn't work node script
-    pongServer.server.kill();
-    await killServer(pongServer);
+    const fullTimer = createTimer('full');
+    const channelTimers: Record<string, Timer> = channelIds.reduce((result, c) => {
+      result[c] = createTimer(c);
 
+      return result;
+    }, {} as Record<string, Timer>);
+
+    fullTimer.start();
+
+    console.log('Starting test');
+
+    const pingPromises = channelIds.map(async c => {
+      for (let i = 0; i < numPings; i++) {
+        channelTimers[c].start();
+        await pingClient.ping(c);
+
+        channelTimers[c].stop();
+      }
+    });
+    await Promise.all(pingPromises);
+
+    fullTimer.stop();
+    const maxUpdate = Math.max(...Object.keys(channelTimers).map(k => channelTimers[k].max()));
+    const minUpdate = Math.min(...Object.keys(channelTimers).map(k => channelTimers[k].min()));
+    const arrayOfMeans = Object.keys(channelTimers).map(k => channelTimers[k].mean());
+    const meanUpdate = arrayOfMeans.reduce((a, b) => a + b, 0) / arrayOfMeans.length;
+    const arrayOfTotals = Object.keys(channelTimers).map(k => channelTimers[k].stats().total);
+    const maxPerChannel = Math.max(...arrayOfTotals);
+    const minPerChannel = Math.min(...arrayOfTotals);
+    const meanPerChannel = arrayOfTotals.reduce((a, b) => a + b, 0) / arrayOfTotals.length;
+
+    console.log(`Total test run ${fullTimer.took()} MS`);
+    const table = new Table({head: ['Action', 'Min (MS)', 'Max (MS)', 'Avg (MS)']});
+
+    table.push(
+      ['Individual Ping call', minUpdate, maxUpdate, meanUpdate],
+      [`${numPings} consecutive calls of Ping`, minPerChannel, maxPerChannel, meanPerChannel]
+    );
+    console.log(table.toString());
     process.exit(0);
   } catch (error) {
     console.error(error);
-    if (pongServer) {
-      // TODO: Determine why killServer doesn't work node script
-      pongServer.server.kill();
-      await killServer(pongServer);
-    }
+
     process.exit(1);
   }
 })();
