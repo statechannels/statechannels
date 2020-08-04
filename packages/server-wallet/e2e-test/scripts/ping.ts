372718a535ef94a016bdb54b5312b3f044b68a76
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-08-04 11:46:34 -0700

    Add a ping script which pings many channels in parallel

---
+++ packages/server-wallet/e2e-test/scripts/ping.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs';
+import {Model} from 'objection';
+import Knex from 'knex';
+import _ from 'lodash';
+
+import {dbCofig} from '../../src/db-config';
+import PingClient from '../ping/client';
+import {alice} from '../../src/wallet/__test__/fixtures/signing-wallets';
+
+const {database, channels} = yargs
+  .command('ping', 'Ping many channels concurrently')
+  .example('ping --database ping --channels 0xf00 0x123 0xabc', 'Pings three channels')
+  .options({
+    database: {type: 'string', demandOption: true},
+    channels: {type: 'array', demandOption: true},
+  })
+  .coerce('channels', (channels: number[]): string[] =>
+    channels.map(channel => channel.toString(16))
+  ).argv;
+
+const knex = Knex(_.merge(dbCofig, {connection: {database}}));
+Model.knex(knex);
+
+(async (): Promise<any> => {
+  const pingClient = new PingClient(alice().privateKey, `http://127.0.0.1:65535`);
+  await Promise.all((channels || []).map(async channelId => pingClient.ping(channelId)));
+
+  console.log('DONE');
+})();
