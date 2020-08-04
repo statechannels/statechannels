1f4fd3e7b3996382fe4a8b7b0f3a0bc5df35989c
geoknee <georgeknee@googlemail.com>
2020-08-04 10:19:47 +0100

    Add Notification validation

--- packages/client-api-schema/src/validation.ts
+++ packages/client-api-schema/src/validation.ts
@@ -1,7 +1,7 @@
 // need to use this syntax, because ajv uses export= style exports
 // otherwise we force all consumers of the package to set esModuleInterop to true
 import Ajv = require('ajv');
-import {Request, Response} from './types';
+import {Request, Response, Notification} from './types';
 
 // You need to pass `jsonPointers: true`
 const ajv = new Ajv({jsonPointers: true, verbose: true});
@@ -13,6 +13,8 @@ ajv.addSchema(apiSchema, 'api.json');
 
 export const validateRequest = ajv.compile({$ref: 'api.json#/definitions/Request'});
 export const validateResponse = ajv.compile({$ref: 'api.json#/definitions/Response'});
+export const validateNotification = ajv.compile({$ref: 'api.json#/definitions/Notification'});
+
 function prettyPrintError(e: Ajv.ErrorObject): string {
   switch (e.keyword) {
     case 'additionalProperties': {
@@ -67,4 +69,24 @@ export function parseResponse(jsonBlob: object): Response {
     );
   }
   return jsonBlob as Response;
+}
+
+/**
+ * Validates a notification against the API schema & returns the input cast to the correctly narrowed type.
+ *
+ * @param jsonBlob - A javascript object that might be a valid {@link Notification}
+ * @returns The input, but with the correct type, if it is valid.
+ */
+export function parseNotification(jsonBlob: object): Notification {
+  const valid = validateNotification(jsonBlob);
+  if (!valid) {
+    throw new Error(
+      `
+      Validation Error:
+        input: ${JSON.stringify(jsonBlob)};\n
+        ${validateNotification.errors?.map(e => prettyPrintError(e)).join(`;\n`)}
+      `
+    );
+  }
+  return jsonBlob as Notification;
 }
