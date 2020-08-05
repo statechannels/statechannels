44af32e0529940b7c3108dca7ca3d430c62992e7
Liam Horne <liam@lihorne.com>
2020-08-05 11:01:44 -0400

    Clean e2e-test files a bit (#2425)
    * Move db-config into db folder
    
    * Clean up start script and folder organization for payer a bit
    
    * Update packages/server-wallet/e2e-test/payer/start.ts
    
    Co-authored-by: andrewgordstewart <andrew.gord.stewart@gmail.com>
    
    Co-authored-by: andrewgordstewart <andrew.gord.stewart@gmail.com>

--- packages/server-wallet/src/db/connection.ts
+++ packages/server-wallet/src/db/connection.ts
@@ -1,7 +1,7 @@
 import {Model} from 'objection';
 import Knex from 'knex';
 
-import {dbConfig} from '../db-config';
+import {dbConfig} from '../db/config';
 
 const knex = Knex(dbConfig);
 Model.knex(knex);
