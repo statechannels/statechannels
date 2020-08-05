01fc1b7a89cec596eb237774396174a9fe73731b
geoknee <georgeknee@googlemail.com>
2020-08-05 14:12:05 +0100

    Fill out all notifications

--- packages/client-api-schema/src/__tests__/parsing.test.ts
+++ packages/client-api-schema/src/__tests__/parsing.test.ts
@@ -9,27 +9,31 @@ import {goodRequests, goodNotifications} from './good_sample_messages';
 import {badRequests, badNotifications} from './bad_sample_messages';
 
 describe('requests', () => {
-  it('validates and parses', () => {
-    for (const request of goodRequests) {
-      expect(validateRequest(request)).toBe(true);
+  for (const request of goodRequests) {
+    it(`validates and parses ${request.method}`, () => {
       expect(parseRequest(request)).toEqual(request);
-    }
-    for (const request of badRequests) {
-      expect(validateRequest(request)).toBe(false);
+      expect(validateRequest(request)).toBe(true);
+    });
+  }
+  for (const request of badRequests) {
+    it(`detects a dodgy      ${request.method} and throws`, () => {
       expect(() => parseRequest(request)).toThrow();
-    }
-  });
+      expect(validateRequest(request)).toBe(false);
+    });
+  }
 });
 
 describe('notifications', () => {
-  it('validates and parses', () => {
-    for (const notification of goodNotifications) {
-      expect(validateNotification(notification)).toBe(true);
+  for (const notification of goodNotifications) {
+    it(`validates and parses ${notification.method}`, () => {
       expect(parseNotification(notification)).toEqual(notification);
-    }
-    for (const notification of badNotifications) {
-      expect(validateNotification(notification)).toBe(false);
+      expect(validateNotification(notification)).toBe(true);
+    });
+  }
+  for (const notification of badNotifications) {
+    it(`detects a dodgy      ${notification.method} and throws`, () => {
       expect(() => parseNotification(notification)).toThrow();
-    }
-  });
+      expect(validateNotification(notification)).toBe(false);
+    });
+  }
 });
