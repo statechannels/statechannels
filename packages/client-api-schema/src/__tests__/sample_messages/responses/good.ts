64f2a9dade1a2e9ae145dc9f340444e30fcabadb
geoknee <georgeknee@googlemail.com>
2020-08-05 14:55:29 +0100

    Complete coverage for responses

--- packages/client-api-schema/src/__tests__/sample_messages/responses/good.ts
+++ packages/client-api-schema/src/__tests__/sample_messages/responses/good.ts
@@ -1,4 +1,4 @@
-import {StateChannelsResponse, ChannelResult} from '../../../../lib/src';
+import {StateChannelsResponse, ChannelResult, DomainBudget} from '../../../../lib/src';
 
 const channelResult: ChannelResult = {
   channelId: '0x697ecf681033a2514ed19c90299a67ae8677f3c78b5877fe4550c4f0960e87b7',
@@ -35,11 +35,45 @@ const channelResult: ChannelResult = {
   appData:
     '0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000016345785d8a00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004444444444444444444444444444444444444444444444444444444444444444'
 };
+const budget: DomainBudget = {
+  domain: 'www.somewhere.com',
+  hubAddress: '0x0',
+  budgets: []
+};
 
 export const goodResponses: StateChannelsResponse[] = [
   {
     jsonrpc: '2.0',
     id: 1581594378830,
     result: channelResult
+  },
+  {
+    jsonrpc: '2.0',
+    id: 2581594378830,
+    result: [channelResult, channelResult]
+  },
+  {
+    jsonrpc: '2.0',
+    id: 1581594378831,
+    result: budget
+  },
+  {
+    jsonrpc: '2.0',
+    id: 1581594378832,
+    result: {success: true}
+  },
+  {
+    jsonrpc: '2.0',
+    id: 1581594378833,
+    result: {
+      signingAddress: '0xAE363d29fc0f6A9bbBbEcC87751e518Cd9CA83C0',
+      destinationAddress: '0xAE363d29fc0f6A9bbBbEcC87751e518Cd9CA83C0',
+      walletVersion: 'five'
+    }
+  },
+  {
+    jsonrpc: '2.0',
+    id: 1581594378833,
+    result: {}
   }
 ];
