2253933f6fae71c47d9c205dc281fae4464e0ae9
geoknee <georgeknee@googlemail.com>
2020-08-05 11:52:46 +0100

    Add ChannelProposed good and bad

--- packages/client-api-schema/src/__tests__/parsing.test.ts
+++ packages/client-api-schema/src/__tests__/parsing.test.ts
@@ -1,7 +1,12 @@
-import {validateRequest, parseRequest} from '../validation';
+import {
+  validateRequest,
+  parseRequest,
+  validateNotification,
+  parseNotification
+} from '../validation';
 
-import {goodRequests} from './good_sample_messages';
-import {badRequests} from './bad_sample_messages';
+import {goodRequests, goodNotifications} from './good_sample_messages';
+import {badRequests, badNotifications} from './bad_sample_messages';
 
 describe('requests', () => {
   it('validates and parses', () => {
@@ -12,6 +17,19 @@ describe('requests', () => {
     for (const request of badRequests) {
       expect(validateRequest(request)).toBe(false);
       expect(() => parseRequest(request)).toThrow();
+    }
+  });
+});
+
+describe('notifications', () => {
+  it('validates and parses', () => {
+    for (const notification of goodNotifications) {
+      expect(validateNotification(notification)).toBe(true);
+      expect(parseNotification(notification)).toEqual(notification);
+    }
+    for (const notification of badNotifications) {
+      expect(validateNotification(notification)).toBe(false);
+      expect(() => parseNotification(notification)).toThrow();
     }
   });
 });
