4c6b2b615f7a4826966498f9500a5f29870caa44
geoknee <georgeknee@googlemail.com>
2020-08-06 13:04:27 +0100

    Fix test a bit

--- packages/channel-client/tests/client-provider-wallet.test.ts
+++ packages/channel-client/tests/client-provider-wallet.test.ts
@@ -1,32 +1,29 @@
+/* eslint-disable no-undef */
 /**
  * @jest-environment jsdom
  */
 
-jest.setTimeout(30000);
-
 import {IFrameChannelProviderInterface} from '@statechannels/iframe-channel-provider';
+jest.setTimeout(3000);
+require('@statechannels/iframe-channel-provider');
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ifp = require('@statechannels/iframe-channel-provider');
+let channelProvider: IFrameChannelProviderInterface;
 
-let cp = (window as any).channelProvider as IFrameChannelProviderInterface;
+beforeAll(() => {
+  window.addEventListener('message', event => console.log(event));
+  channelProvider = (window as any).channelProvider;
+});
 
 describe('Client-Provider-Wallet', () => {
-  beforeAll(() => {
-    cp = ifp;
-  });
-
-  it('Attaches the channel provider to the window object', () => {
-    expect((window as any).channelProvider).toBeDefined();
-  });
-
   it('Mounts the iframe pointed at the hosted wallet, and enables', async () => {
-    await ((window as any).channelProvider as IFrameChannelProviderInterface).mountWalletComponent(
+    console.log(channelProvider);
+    const mountPromise = channelProvider.mountWalletComponent(
       'https://xstate-wallet.statechannels.org/'
     );
-    await ((window as any).channelProvider as IFrameChannelProviderInterface).enable();
-    expect(
-      ((window as any).channelProvider as IFrameChannelProviderInterface).walletVersion
-    ).toBeDefined();
+
+    window.dispatchEvent(new MessageEvent('message', {data: 'Wallet Ready'}));
+    await mountPromise;
+    await channelProvider.enable();
+    expect(channelProvider.walletVersion).toBeDefined();
   });
 });
