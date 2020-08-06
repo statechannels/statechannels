792885b8d733c510770b40e97cd0ada93937b3ee
geoknee <georgeknee@googlemail.com>
2020-08-06 13:39:37 +0100

    manually dispatch message

--- packages/channel-client/tests/client-provider-wallet.test.ts
+++ packages/channel-client/tests/client-provider-wallet.test.ts
@@ -4,24 +4,36 @@
  */
 
 import {IFrameChannelProviderInterface} from '@statechannels/iframe-channel-provider';
+import {WalletReady} from '@statechannels/client-api-schema';
 jest.setTimeout(3000);
 require('@statechannels/iframe-channel-provider');
 
 let channelProvider: IFrameChannelProviderInterface;
 
 beforeAll(() => {
-  window.addEventListener('message', event => console.log(event));
+  window.addEventListener('message', event => console.log(event.data));
   channelProvider = (window as any).channelProvider;
 });
 
 describe('Client-Provider-Wallet', () => {
   it('Mounts the iframe pointed at the hosted wallet, and enables', async () => {
-    console.log(channelProvider);
     const mountPromise = channelProvider.mountWalletComponent(
       'https://xstate-wallet.statechannels.org/'
     );
 
-    window.dispatchEvent(new MessageEvent('message', {data: 'Wallet Ready'}));
+    const walletReadyMessage: WalletReady = {
+      jsonrpc: '2.0',
+      method: 'WalletReady',
+      params: {}
+    };
+
+    window.dispatchEvent(
+      new MessageEvent('message', {
+        origin: 'https://xstate-wallet.statechannels.org/',
+        data: walletReadyMessage
+      })
+    );
+    console.log(channelProvider);
     await mountPromise;
     await channelProvider.enable();
     expect(channelProvider.walletVersion).toBeDefined();
