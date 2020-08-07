30dc412b49e431a4bc8e1be968cecb9041c58ca9
geoknee <georgeknee@googlemail.com>
2020-08-07 15:54:03 +0100

    Add a create-channel test

--- packages/channel-client/tests/integration-tests/mount-and-enable.test.ts
+++ packages/channel-client/tests/integration-tests/mount-and-enable.test.ts
@@ -1,15 +1,11 @@
 /* eslint-disable no-undef */
 
 import {IFrameChannelProviderInterface} from '@statechannels/iframe-channel-provider';
+import {sleep} from './helpers';
 
-jest.setTimeout(30000);
 require('@statechannels/iframe-channel-provider');
 
 let channelProvider: IFrameChannelProviderInterface;
-
-function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 beforeAll(() => {
   channelProvider = (window as any).channelProvider;
@@ -20,7 +16,7 @@ describe('Client-Provider-Wallet', () => {
     await channelProvider.mountWalletComponent('http://localhost:3055');
     const iframe = document.getElementById('channelProviderUi') as HTMLIFrameElement;
     const enablePromise = channelProvider.enable();
-    await sleep(1000); // wait for UI
+    await sleep(100); // wait for UI
     iframe.contentWindow?.document.getElementById('connect-with-metamask-button')?.click();
     await enablePromise;
     expect(channelProvider.signingAddress).toBeDefined();
