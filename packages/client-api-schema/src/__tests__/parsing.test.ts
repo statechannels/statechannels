745b00483a3241667057ff7870df07a24d9345f0
geoknee <georgeknee@googlemail.com>
2020-08-07 09:16:36 +0100

    Convert for loop to map

--- packages/client-api-schema/src/__tests__/parsing.test.ts
+++ packages/client-api-schema/src/__tests__/parsing.test.ts
@@ -19,61 +19,61 @@ import {goodErrorResponses} from './sample_messages/error-responses/good';
 import {badErrorResponses} from './sample_messages/error-responses/bad';
 
 describe('requests', () => {
-  for (const request of goodRequests) {
+  goodRequests.map(request => {
     it(`validates and parses ${request.method}`, () => {
       expect(parseRequest(request)).toEqual(request);
       expect(validateRequest(request)).toBe(true);
     });
-  }
-  for (const request of badRequests) {
+  });
+  badRequests.map(request => {
     it(`detects a dodgy      ${request.method} and throws`, () => {
       expect(() => parseRequest(request)).toThrow();
       expect(validateRequest(request)).toBe(false);
     });
-  }
+  });
 });
 
 describe('notifications', () => {
-  for (const notification of goodNotifications) {
+  goodNotifications.map(notification => {
     it(`validates and parses ${notification.method}`, () => {
       expect(parseNotification(notification)).toEqual(notification);
       expect(validateNotification(notification)).toBe(true);
     });
-  }
-  for (const notification of badNotifications) {
+  });
+  badNotifications.map(notification => {
     it(`detects a dodgy      ${notification.method} and throws`, () => {
       expect(() => parseNotification(notification)).toThrow();
       expect(validateNotification(notification)).toBe(false);
     });
-  }
+  });
 });
 
 describe('responses', () => {
-  for (const response of goodResponses) {
+  goodResponses.map(response => {
     it(`validates and parses ${response.id}`, () => {
       expect(parseResponse(response)).toEqual(response);
       expect(validateResponse(response)).toBe(true);
     });
-  }
-  for (const response of badResponses) {
+  });
+  badResponses.map(response => {
     it(`detects a dodgy      response and throws`, () => {
       expect(() => parseResponse(response)).toThrow();
       expect(validateResponse(response)).toBe(false);
     });
-  }
+  });
 });
 
 describe('error responses', () => {
-  for (const response of goodErrorResponses) {
+  goodErrorResponses.map(response => {
     it(`validates and parses ${response.error.code}: ${response.error.message}`, () => {
       expect(parseErrorResponse(response)).toEqual(response);
       expect(validateErrorResponse(response)).toBe(true);
     });
-  }
-  for (const response of badErrorResponses) {
+  });
+  badErrorResponses.map(response => {
     it(`detects a dodgy      ${response.error.code} error response and throws`, () => {
       expect(() => parseErrorResponse(response)).toThrow();
       expect(validateErrorResponse(response)).toBe(false);
     });
-  }
+  });
 });
