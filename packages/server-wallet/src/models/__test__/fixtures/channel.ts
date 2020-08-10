26f940afaf200feee2b755c4e5ea0758c7f4224e
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-08-10 07:35:49 -0700

    Add channelId to withSupportedState fixture

--- packages/server-wallet/src/models/__test__/fixtures/channel.ts
+++ packages/server-wallet/src/models/__test__/fixtures/channel.ts
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 import {Channel, RequiredColumns} from '../../../models/channel';
 import {Fixture, fixture, DeepPartial} from '../../../wallet/__test__/fixtures/utils';
-import {addHash, dropNonVariables} from '../../../state-utils';
+import {addHash, dropNonVariables, addChannelId} from '../../../state-utils';
 import {alice, bob} from '../../../wallet/__test__/fixtures/signing-wallets';
 import {createState, stateSignedBy} from '../../../wallet/__test__/fixtures/states';
 import {SigningWallet} from '../../signing-wallet';
@@ -45,5 +45,6 @@ export const withSupportedState = (
         ),
       ],
       ...channelProps,
-    })
+    }),
+    addChannelId
   );
--- packages/server-wallet/src/state-utils.ts
+++ packages/server-wallet/src/state-utils.ts
@@ -4,8 +4,11 @@ import {
   SignedStateVarsWithHash,
   State,
   hashState,
+  calculateChannelId,
 } from '@statechannels/wallet-core';
 import _ from 'lodash';
+
+import {Bytes32} from './type-aliases';
 
 export const dropNonVariables = (s: SignedStateVarsWithHash): SignedStateVarsWithHash =>
   _.pick(s, 'appData', 'outcome', 'isFinal', 'turnNum', 'stateHash', 'signatures');
@@ -17,3 +20,7 @@ export const addHash = <T extends State = State>(s: T): T & Hashed => ({
   ...s,
   stateHash: hashState(s),
 });
+
+export const addChannelId = <T extends ChannelConstants = ChannelConstants>(
+  c: T
+): T & {channelId: Bytes32} => ({...c, channelId: calculateChannelId(c)});
