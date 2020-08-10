3dd21ff425f4c8720016e32e4b4dcee1b1e6c740
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-08-10 12:57:55 -0600

    Fix application-protocol-state fixture

--- packages/server-wallet/src/protocols/__test__/fixtures/application-protocol-state.ts
+++ packages/server-wallet/src/protocols/__test__/fixtures/application-protocol-state.ts
@@ -6,11 +6,12 @@ import {
   withSupportedState as channelWithSupportedState,
 } from '../../../models/__test__/fixtures/channel';
 import {ProtocolState} from '../../application';
+import {stateVars} from '../../../wallet/__test__/fixtures/state-vars';
 
 export const applicationProtocolState = fixture({app: channel().protocolState});
-export const withSupportedState = (stateVars: Partial<StateVariables>): Fixture<ProtocolState> =>
+export const withSupportedState = (vars: Partial<StateVariables>): Fixture<ProtocolState> =>
   fixture(
     applicationProtocolState({
-      app: channelWithSupportedState()({vars: [stateVars]}).protocolState,
+      app: channelWithSupportedState()({vars: [stateVars(vars)]}).protocolState,
     })
   );
