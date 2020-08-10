b09d3d7d1c213240b07fa4ee14163b85f0f870a6
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-08-10 11:07:05 -0600

    Add third signing wallet/participant
    Useful for testing other cases

--- packages/server-wallet/src/wallet/__test__/fixtures/signing-wallets.ts
+++ packages/server-wallet/src/wallet/__test__/fixtures/signing-wallets.ts
@@ -11,3 +11,9 @@ export const bob = (): SigningWallet =>
     // 0x2222E21c8019b14dA16235319D34b5Dd83E644A9
     privateKey: '0xb3ab7b031311fe1764b657a6ae7133f19bac97acd1d7edca9409daa35892e727',
   });
+
+export const charlie = (): SigningWallet =>
+  SigningWallet.fromJson({
+    // 0x33335846dd121B14B4C313Cb6b766F09e75890dF
+    privateKey: '0xc9a5f30ceaf2a0ccbb30d50aa9de3f273aa6e76f89e26090c42775e9647f5b6a',
+  });
