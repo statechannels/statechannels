43570ce5c5edd275d9979baabd38c9f2d5a12ed5
Alex Gap <alex.gap@consensys.net>
2020-08-13 12:16:54 -0700

    Add simple signature test

---
+++ packages/server-wallet/src/wallet/__test__/add-signed-state.test.ts
@@ -0,0 +1,28 @@
+import Objection from 'objection';
+
+import {Store} from '../store';
+import {Channel} from '../../models/channel';
+
+import {createState} from './fixtures/states';
+import {alice} from './fixtures/signing-wallets';
+
+describe('addSignedState', () => {
+  let tx: Objection.Transaction;
+
+  afterEach(async () => tx.rollback());
+
+  beforeEach(async () => {
+    tx = await Channel.startTransaction();
+  });
+
+  const BOB_SIGNATURE =
+    '0x36a5fd36a1c9a85afdeee3f9471579656eefceb08bc0ff53d194a67d6433c6385cc8c9aa049306fc7cce901f7b3345bccde311cceadc74a40a89a9d74d86d9b91b';
+  it('throws on an invalid signature', async () => {
+    const signedState = {
+      ...createState(),
+      signatures: [{signer: alice().address, signature: BOB_SIGNATURE}],
+    };
+
+    await expect(Store.addSignedState(signedState, tx)).rejects.toThrow('Invalid signature');
+  });
+});
