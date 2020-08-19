3b69b239e0c0b2bd37d9c4105a63d310086d429a
Mike Kerzhner <mikerzhner@gmail.com>
2020-08-19 12:27:17 -0700

    Add fastSignState

--- packages/nitro-protocol/test/src/signatures.test.ts
+++ packages/nitro-protocol/test/src/signatures.test.ts
@@ -1,13 +1,37 @@
-import {Wallet} from 'ethers';
+import {Wallet, utils} from 'ethers';
 import {AddressZero} from 'ethers/constants';
 import {arrayify, splitSignature, verifyMessage} from 'ethers/utils';
 
 import {hashChallengeMessage} from '../../src/contract/challenge';
 import {hashState, State} from '../../src/contract/state';
-import {getStateSignerAddress, signChallengeMessage, signState} from '../../src/signatures';
+import {
+  getStateSignerAddress,
+  signChallengeMessage,
+  signState,
+  fastSignState,
+} from '../../src/signatures';
 
 describe('signatures', () => {
   describe('signState', () => {
+    it.only('sign vs fastSign', async () => {
+      const wallet = Wallet.createRandom();
+      const state: State = {
+        channel: {chainId: '0x1', channelNonce: 0x01, participants: [wallet.address]},
+        outcome: [],
+        turnNum: 1,
+        isFinal: false,
+        appData: '0x0',
+        appDefinition: AddressZero,
+        challengeDuration: 0x5,
+      };
+
+      const signedState = signState(state, wallet.privateKey);
+      const fastSignedState = fastSignState(state, wallet.privateKey);
+      console.log(utils.joinSignature(signedState.signature));
+      console.log(`The private key is ${wallet.privateKey}`);
+      expect(utils.joinSignature(signedState.signature)).toEqual((await fastSignedState).signature);
+    });
+
     it('signs a state', async () => {
       const wallet = Wallet.createRandom();
       const state: State = {
@@ -32,6 +56,7 @@ describe('signatures', () => {
 
       expect(getStateSignerAddress(signedState)).toEqual(wallet.address);
     });
+
     it('throws an exception if signing with non-participant private key', () => {
       const wallet = Wallet.createRandom();
       const state: State = {
