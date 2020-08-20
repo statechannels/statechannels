69b47c1484e4ad9c4699a14169ea91f245905def
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-08-20 10:49:17 -0600

    Log out the wallet & state on error
    So that if this test ever fails, we can reproduce it.

--- packages/server-wallet/src/utilities/__test__/signatures.test.ts
+++ packages/server-wallet/src/utilities/__test__/signatures.test.ts
@@ -1,25 +1,34 @@
 import {Wallet} from 'ethers';
 import {AddressZero} from '@ethersproject/constants';
 import {State, simpleEthAllocation, signState} from '@statechannels/wallet-core';
+import _ from 'lodash';
 
 import {participant} from '../../wallet/__test__/fixtures/participants';
 import {fastSignState} from '../signatures';
+import {logger} from '../../logger';
 
 it('sign vs fastSign', async () => {
-  const wallet = Wallet.createRandom();
-  const state: State = {
-    chainId: '0x1',
-    channelNonce: 0x01,
-    participants: [participant({signingAddress: wallet.address})],
-    outcome: simpleEthAllocation([]),
-    turnNum: 1,
-    isFinal: false,
-    appData: '0x0',
-    appDefinition: AddressZero,
-    challengeDuration: 0x5,
-  };
+  _.range(5).map(async channelNonce => {
+    const {address, privateKey} = Wallet.createRandom();
+    const state: State = {
+      chainId: '0x1',
+      channelNonce,
+      participants: [participant({signingAddress: address})],
+      outcome: simpleEthAllocation([]),
+      turnNum: 1,
+      isFinal: false,
+      appData: '0x0',
+      appDefinition: AddressZero,
+      challengeDuration: 0x5,
+    };
 
-  const signedState = signState(state, wallet.privateKey);
-  const fastSignedState = fastSignState(state, wallet.privateKey);
-  expect(signedState).toEqual((await fastSignedState).signature);
+    const signedState = signState(state, privateKey);
+    const fastSignedState = fastSignState(state, privateKey);
+    try {
+      expect(signedState).toEqual((await fastSignedState).signature);
+    } catch (error) {
+      logger.info({error, state, privateKey});
+      throw error;
+    }
+  });
 });
