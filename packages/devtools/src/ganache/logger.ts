7e3ebf2f990815c7d1b738a53cdb2175a01b3657
Alex Gap <alex.gap@consensys.net>
2020-08-25 22:23:32 -0700

    really disable it

--- packages/devtools/src/ganache/logger.ts
+++ packages/devtools/src/ganache/logger.ts
@@ -1,15 +1,15 @@
-import pino from 'pino';
+// import pino from 'pino';
 
-import {LOG_DESTINATION, LOG_LEVEL} from './config';
+// import {LOG_DESTINATION,} from './config';
 
-const LOG_TO_CONSOLE = LOG_DESTINATION === 'console';
+// const LOG_TO_CONSOLE = LOG_DESTINATION === 'console';
 // const LOG_TO_FILE = ADD_LOGS && !LOG_TO_CONSOLE;
 
-const name = 'ganache-cli';
+// const name = 'ganache-cli';
 
 // const destination = LOG_TO_FILE ? pino.destination(LOG_DESTINATION) : undefined;
 
-const prettyPrint = LOG_TO_CONSOLE ? {translateTime: true} : false;
+// const prettyPrint = LOG_TO_CONSOLE ? {translateTime: true} : false;
 
-const opts = {name, prettyPrint, level: LOG_LEVEL};
-export const logger = pino(opts);
+// const opts = {name, prettyPrint, level: LOG_LEVEL};
+export const logger = {} as any;
