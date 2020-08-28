72b7b3826dd728ea8248cefdf87c6c482aa866a7
Alex Gap <alex.gap@consensys.net>
2020-08-28 08:37:09 -0700

    set node env to test

--- packages/server-wallet/e2e-test/scripts/profile.sh
+++ packages/server-wallet/e2e-test/scripts/profile.sh
@@ -10,9 +10,9 @@ if [ -d ./.clinic ]; then
 fi
 
 echo "Generating flamegraph"
-SERVER_DB_NAME=payer NODE_ENV=development npx ts-node ./e2e-test/scripts/generate-profile-data.ts flamegraph
+SERVER_DB_NAME=payer NODE_ENV=test npx ts-node ./e2e-test/scripts/generate-profile-data.ts flamegraph
 npx clinic flame --visualize-only .clinic/*.clinic-flame
 
 echo "Generating bubbleprof"
-SERVER_DB_NAME=payer NODE_ENV=development npx ts-node ./e2e-test/scripts/generate-profile-data.ts bubbleprof
+SERVER_DB_NAME=payer NODE_ENV=test npx ts-node ./e2e-test/scripts/generate-profile-data.ts bubbleprof
 npx clinic bubbleprof --visualize-only .clinic/*.clinic-bubbleprof
