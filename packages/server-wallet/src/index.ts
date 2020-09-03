07a216058afc7aac20058ba9508c8327e57612c1
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-02 17:31:25 -0700

    server-wallet: export adminKnex

--- packages/server-wallet/src/index.ts
+++ packages/server-wallet/src/index.ts
@@ -2,5 +2,6 @@ import {Message} from '@statechannels/wallet-core';
 
 import {Wallet} from './wallet';
 import {Outgoing} from './protocols/actions';
+import adminKnex from './db-admin/db-admin-connection';
 
-export {Wallet, Message, Outgoing};
+export {Wallet, Message, Outgoing, adminKnex as WalletKnex};
