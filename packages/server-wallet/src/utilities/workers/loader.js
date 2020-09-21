98dc1ff57290b4f5abca9695ae673d01b19ac6f1
Alex Gap <alex.gap@mesh.xyz>
2020-09-21 21:40:11 +0000

    clean up loader.js

--- packages/server-wallet/src/utilities/workers/loader.js
+++ packages/server-wallet/src/utilities/workers/loader.js
@@ -1,4 +1,13 @@
 const path = require('path');
+const fs = require('fs');
 
-require('ts-node').register({typeCheck: false});
-require(path.resolve(__dirname, './worker.ts'));
+const TS_WORKER_PATH = path.resolve(__dirname, './worker.ts');
+const JS_WORKER_PATH = path.resolve(__dirname, './worker.js');
+if (fs.existsSync(TS_WORKER_PATH)) {
+  require('ts-node').register({typeCheck: false});
+  require(TS_WORKER_PATH);
+} else if (fs.existsSync(JS_WORKER_PATH)) {
+  require(JS_WORKER_PATH);
+} else {
+  throw new Error('Could not find worker.ts or worker.js');
+}
