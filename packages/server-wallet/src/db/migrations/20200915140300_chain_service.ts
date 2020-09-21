8e917646eed3d2bdda05b0011058c0b682228803
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-21 15:30:39 -0600

    feat(server-wallet): validate chain_service_requests database constraint
    Address https://github.com/statechannels/statechannels/pull/2561#discussion_r491507854

--- packages/server-wallet/src/db/migrations/20200915140300_chain_service.ts
+++ packages/server-wallet/src/db/migrations/20200915140300_chain_service.ts
@@ -14,6 +14,14 @@ export async function up(knex: Knex): Promise<any> {
       .notNullable()
       .defaultTo(defaultFundingStrategy);
   });
+
+  await knex.raw(`\
+    ALTER TABLE ${channels}
+    ADD CONSTRAINT valid_chain_service_requests
+    CHECK (
+      ${chainServiceRequests} <@ ARRAY['fund']
+    )
+  `);
 }
 
 export async function down(knex: Knex): Promise<any> {
