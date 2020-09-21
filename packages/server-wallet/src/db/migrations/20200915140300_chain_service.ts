29380531f20c385583bd2fa86bf2122bb968ec38
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-21 13:44:48 -0600

    feat(server-wallet): default to Unknown funding strategy in channels table

--- packages/server-wallet/src/db/migrations/20200915140300_chain_service.ts
+++ packages/server-wallet/src/db/migrations/20200915140300_chain_service.ts
@@ -1,13 +1,18 @@
 import * as Knex from 'knex';
+import {FundingStrategy} from '@statechannels/client-api-schema';
 
 const channels = 'channels';
 const chainServiceRequests = 'chain_service_requests';
 const fundingStrategy = 'funding_strategy';
+const defaultFundingStrategy: FundingStrategy = 'Unknown';
 
 export async function up(knex: Knex): Promise<any> {
   await knex.schema.alterTable(channels, table => {
     table.specificType(chainServiceRequests, 'text[]').notNullable();
-    table.string(fundingStrategy).notNullable();
+    table
+      .string(fundingStrategy)
+      .notNullable()
+      .defaultTo(defaultFundingStrategy);
   });
 }
 
