cc2c68436d4008a87e8c1ecb959cd41b79ba670d
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-22 12:23:59 -0600

    feat(server-wallet): respond to setFunding call from ChainService

--- packages/server-wallet/src/mock-chain-service/index.ts
+++ packages/server-wallet/src/mock-chain-service/index.ts
@@ -9,6 +9,15 @@ type FundChannelArg = {
   expectedHeld: Uint256;
   amount: Uint256;
 };
+export type SetFundingArg = {
+  channelId: Bytes32;
+  assetHolderAddress: Address;
+  amount: Uint256;
+};
+
+export interface ChainEventListener {
+  setFunding(arg: SetFundingArg): void;
+}
 export class OnchainService {
   static fundChannel(_arg: FundChannelArg): Promise<SubmissionStatus> {
     const submissionStatus: SubmissionStatus = 'Success';
