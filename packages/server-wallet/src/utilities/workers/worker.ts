a97012f48774ec3f662901e04f17be6252e0a692
Alex Gap <alex.gap@mesh.xyz>
2020-09-22 00:04:24 +0000

    Add proper error handling

--- packages/server-wallet/src/utilities/workers/worker.ts
+++ packages/server-wallet/src/utilities/workers/worker.ts
@@ -1,11 +1,11 @@
 import {parentPort, isMainThread, workerData} from 'worker_threads';
 
 import {hashState} from '@statechannels/wallet-core';
+import {left, right} from 'fp-ts/lib/Either';
 
 import {fastRecoverAddress, fastSignState} from '../signatures';
 import {Wallet} from '../..';
 import {ServerWalletConfig} from '../../config';
-import {logger} from '../../logger';
 
 import {isStateChannelWorkerData} from './worker-data';
 
@@ -13,26 +13,33 @@ const wallet = new Wallet(workerData as ServerWalletConfig);
 
 parentPort?.on('message', async (message: any) => {
   if (isMainThread) {
-    throw new Error('Attempting to execute worker thread script on the main thread');
+    parentPort?.postMessage(
+      left(new Error('Attempting to execute worker thread script on the main thread'))
+    );
   }
 
   if (!isStateChannelWorkerData(message)) {
-    logger.error({message}, 'Incorrect worker data');
-    throw new Error('Incorrect worker data');
+    parentPort?.postMessage(left(new Error('Incorrect worker data')));
   }
-  switch (message.operation) {
-    case 'HashState':
-      return parentPort?.postMessage(await hashState(message.state));
+  try {
+    switch (message.operation) {
+      case 'HashState':
+        return parentPort?.postMessage(right(await hashState(message.state)));
 
-    case 'RecoverAddress':
-      return parentPort?.postMessage(
-        await fastRecoverAddress(message.state, message.signature, message.state.stateHash)
-      );
-    case 'SignState':
-      return parentPort?.postMessage(await fastSignState(message.state, message.privateKey));
-    case 'UpdateChannel':
-      return parentPort?.postMessage(await wallet._updateChannel(message.args));
-    case 'PushMessage':
-      return parentPort?.postMessage(await wallet._pushMessage(message.args));
+      case 'RecoverAddress':
+        return parentPort?.postMessage(
+          await fastRecoverAddress(message.state, message.signature, message.state.stateHash)
+        );
+      case 'SignState':
+        return parentPort?.postMessage(
+          right(await fastSignState(message.state, message.privateKey))
+        );
+      case 'UpdateChannel':
+        return parentPort?.postMessage(right(await wallet._updateChannel(message.args)));
+      case 'PushMessage':
+        return parentPort?.postMessage(right(await wallet._pushMessage(message.args)));
+    }
+  } catch (error) {
+    return parentPort?.postMessage(left(error));
   }
 });
