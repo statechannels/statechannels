02da2805db041d8e888655d7dc512cbdd9b2aa3f
geoknee <georgeknee@googlemail.com>
2020-09-23 18:16:56 +0100

    swap in some wasmUtils

--- packages/server-wallet/src/utilities/signatures.ts
+++ packages/server-wallet/src/utilities/signatures.ts
@@ -1,15 +1,12 @@
-import {instantiateSecp256k1, Secp256k1, RecoveryId} from '@bitauth/libauth';
-import {utils, Wallet} from 'ethers';
+import {Wallet} from 'ethers';
 import {State, StateWithHash} from '@statechannels/wallet-core';
-
-let secp256k1: Secp256k1;
-export const initialized: Promise<any> = instantiateSecp256k1().then(m => (secp256k1 = m));
+import * as wasmUtils from '@statechannels/wasm-utils';
 
 const knownWallets: Record<string, string> = {};
 const cachedAddress = (privateKey: string): string =>
   knownWallets[privateKey] || (knownWallets[privateKey] = new Wallet(privateKey).address);
 
-export async function fastSignState(
+export async function signState(
   state: StateWithHash,
   privateKey: string
 ): Promise<{state: State; signature: string}> {
@@ -20,40 +17,6 @@ export async function fastSignState(
 
   const {stateHash} = state;
 
-  const signature = await fastSignData(stateHash, privateKey);
+  const signature = await wasmUtils.signState(stateHash, privateKey);
   return {state, signature};
-}
-
-export async function fastRecoverAddress(signature: string, stateHash: string): Promise<string> {
-  await initialized;
-  const recover = Number.parseInt('0x' + signature.slice(-2)) - 27;
-
-  const digest = Buffer.from(hashMessage(stateHash).substr(2), 'hex');
-  const recoveredAddress = utils.computeAddress(
-    secp256k1.recoverPublicKeyCompressed(
-      Buffer.from(signature.slice(2, -2), 'hex'),
-      recover as RecoveryId,
-      digest
-    )
-  );
-
-  return recoveredAddress;
-}
-
-export async function fastSignData(hashedData: string, privateKey: string): Promise<string> {
-  await initialized;
-
-  const hash = hashMessage(hashedData);
-  const digest = Buffer.from(hash.substr(2), 'hex');
-  const signature = secp256k1.signMessageHashRecoverableCompact(
-    Buffer.from(privateKey.substr(2), 'hex'),
-    digest
-  );
-
-  const v = 27 + signature.recoveryId;
-  return '0x' + Buffer.from(signature.signature).toString('hex') + v.toString(16);
-}
-
-function hashMessage(hashedData: string): string {
-  return utils.hashMessage(utils.arrayify(hashedData));
 }
