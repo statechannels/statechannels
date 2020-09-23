3407ea1e5c6ccacc097187b0d1d8d83a86a7a573
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-23 07:34:48 -0600

    feat(server-wallet): wire up wallet for funding events

--- packages/server-wallet/src/mock-chain-service/index.ts
+++ packages/server-wallet/src/mock-chain-service/index.ts
@@ -1,6 +1,14 @@
 import {Address} from '@statechannels/client-api-schema';
+import {Evt} from 'evt';
 
 import {Bytes32, Uint256} from '../type-aliases';
+import {
+  OnchainServiceInterface,
+  ChainEventNames,
+  ChannelEventRecordMap,
+  FundingEvent,
+} from '../onchain-service';
+import {Wallet} from '../wallet';
 
 type SubmissionStatus = 'Success' | 'Fail';
 type FundChannelArg = {
@@ -18,9 +26,33 @@ export type SetFundingArg = {
 export interface ChainEventListener {
   setFunding(arg: SetFundingArg): void;
 }
-export class OnchainService {
+export class OnchainService implements OnchainServiceInterface {
   static fundChannel(_arg: FundChannelArg): Promise<SubmissionStatus> {
     const submissionStatus: SubmissionStatus = 'Success';
     return Promise.resolve(submissionStatus);
+  }
+
+  registerChannel(_channelId: Bytes32, _assetHolders: Address[]): Promise<void> {
+    return Promise.resolve();
+  }
+
+  // TODO: remove in v1
+  attachChannelWallet(_wallet: Wallet): void {
+    return;
+  }
+
+  attachHandler<T extends ChainEventNames>(
+    _assetHolderAddr: Address,
+    _event: T,
+    _callback: (event: ChannelEventRecordMap[T]) => void | Promise<void>,
+    _filter?: (event: ChannelEventRecordMap[T]) => boolean,
+    _timeout?: number
+  ): Evt<ChannelEventRecordMap[T]> | Promise<ChannelEventRecordMap[T]> {
+    const mockEvt = new Evt<FundingEvent>();
+    return mockEvt;
+  }
+
+  detachAllHandlers(_assetHolderAddr: Address, _event?: ChainEventNames): void {
+    return;
   }
 }
