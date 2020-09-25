2e03ffdf43a8784d5aad6381dab5574e940b7e0b
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-25 08:23:27 -0600

    test: properly capture fundChannel rejection

--- packages/server-wallet/src/chain-service/__chain-test__/chain-service.test.ts
+++ packages/server-wallet/src/chain-service/__chain-test__/chain-service.test.ts
@@ -1,6 +1,5 @@
 import {randomChannelId} from '@statechannels/nitro-protocol';
 import {BN} from '@statechannels/wallet-core';
-import {Wallet} from 'ethers';
 
 import {defaultConfig} from '../../config';
 import {ChainService} from '../chain-service';
@@ -14,14 +13,33 @@ describe('fundChannel', () => {
   it('Successfully funds channel', async () => {
     const chainService = new ChainService(rpcEndpoint, defaultConfig.serverPrivateKey);
     const channelId = randomChannelId();
-    const assetHolderAddress = Wallet.createRandom().address;
+    /* eslint-disable-next-line no-process-env, @typescript-eslint/no-non-null-assertion */
+    const assetHolderAddress = process.env.ETH_ASSET_HOLDER_ADDRESS!;
     const request = await chainService.fundChannel({
       channelId,
       assetHolderAddress,
       expectedHeld: BN.from(0),
-      amount: BN.from(0),
+      amount: BN.from(5),
     });
     const receipt = request.wait();
     expect((await receipt).status).toBe(1);
+
+    const request2 = await chainService.fundChannel({
+      channelId,
+      assetHolderAddress,
+      expectedHeld: BN.from(5),
+      amount: BN.from(5),
+    });
+    const receipt2 = request2.wait();
+    expect((await receipt2).status).toBe(1);
+
+    const fundChannelPromise = chainService.fundChannel({
+      channelId,
+      assetHolderAddress,
+      expectedHeld: BN.from(5),
+      amount: BN.from(5),
+    });
+    // todo: is there a good way to validate that the error thrown is one we expect?
+    await expect(fundChannelPromise).rejects.toThrow();
   });
 });
