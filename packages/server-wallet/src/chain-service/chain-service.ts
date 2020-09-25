2c844da919319d63d4b5d2bde78e216d381743c9
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-25 16:16:15 -0600

    feat: add ChainObserver registerChannel

--- packages/server-wallet/src/chain-service/chain-service.ts
+++ packages/server-wallet/src/chain-service/chain-service.ts
@@ -1,12 +1,18 @@
-import {createETHDepositTransaction} from '@statechannels/nitro-protocol';
-import {providers, Wallet} from 'ethers';
+import {
+  ContractArtifacts,
+  createETHDepositTransaction,
+  DepositedEvent,
+} from '@statechannels/nitro-protocol';
+import {BigNumber, Contract, providers, Wallet} from 'ethers';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 import {Address, Bytes32, Uint256} from '../type-aliases';
 
 export type SetFundingArg = {
   channelId: Bytes32;
   assetHolderAddress: Address;
-  amount: Uint256;
+  amount: BigNumber;
 };
 
 type FundChannelArg = {
@@ -16,23 +22,38 @@ type FundChannelArg = {
   amount: Uint256;
 };
 
-export interface ChainEventListener {
+export interface ChainEventSubscriber {
   setFunding(arg: SetFundingArg): void;
 }
 
 interface ChainEventEmitterInterface {
-  registerChannel(channelId: Bytes32, assetHolders: Address[]): Promise<void>;
-  attachChannelWallet(listenter: ChainEventListener): void;
+  registerChannel(
+    channelId: Bytes32,
+    assetHolders: Address[],
+    listener: ChainEventSubscriber
+  ): Promise<void>;
 }
 
 interface ChainMofifierInterface {
   fundChannel(arg: FundChannelArg): Promise<providers.TransactionResponse>;
 }
 
-//todo: implement ChainEventEmitter
-export class ChainService implements ChainMofifierInterface {
+interface FundingEvent {
+  transactionHash: string;
+  type: 'Deposited';
+  blockNumber: number;
+  final: boolean;
+  channelId: Bytes32;
+  amount: string;
+  destinationHoldings: string;
+}
+
+export class ChainService implements ChainMofifierInterface, ChainEventEmitterInterface {
   private readonly ethWallet: Wallet;
+  private provider: providers.JsonRpcProvider;
+  private addressToObservables: Map<string, Observable<SetFundingArg>> = new Map();
   constructor(provider: string, pk: string) {
+    this.provider = new providers.JsonRpcProvider(provider);
     this.ethWallet = new Wallet(pk, new providers.JsonRpcProvider(provider));
   }
 
@@ -47,6 +68,34 @@ export class ChainService implements ChainMofifierInterface {
     };
     return this.ethWallet.sendTransaction({
       ...transactionRequest,
+    });
+  }
+
+  //todo: add channelId filtering
+  async registerChannel(
+    channelId: Bytes32,
+    assetHolders: Address[],
+    subscriber: ChainEventSubscriber
+  ): Promise<void> {
+    assetHolders.map(assetHolder => {
+      let obs = this.addressToObservables.get(assetHolder);
+      if (!obs) {
+        const contract: Contract = new Contract(
+          assetHolder,
+          ContractArtifacts.EthAssetHolderArtifact.abi
+        ).connect(this.provider);
+        obs = new Observable<DepositedEvent>(subscriber => {
+          contract.on('Deposited', subscriber.next);
+        }).pipe(
+          map(event => ({
+            channelId: event.destination,
+            assetHolderAddress: assetHolder,
+            amount: event.destinationHoldings,
+          }))
+        );
+      }
+      obs.subscribe({next: subscriber.setFunding});
+      this.addressToObservables.set(assetHolder, obs);
     });
   }
 }
