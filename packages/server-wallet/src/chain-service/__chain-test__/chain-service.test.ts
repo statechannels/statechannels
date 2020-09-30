a9c177a5811f54870d1705f7b8a7e53fdd515a25
Mike Kerzhner <mikerzhner@gmail.com>
2020-09-30 15:54:58 -0700

    test: factor out helpers for chain-service test

--- packages/server-wallet/src/chain-service/__chain-test__/chain-service.test.ts
+++ packages/server-wallet/src/chain-service/__chain-test__/chain-service.test.ts
@@ -1,5 +1,6 @@
 import {randomChannelId} from '@statechannels/nitro-protocol';
 import {BN} from '@statechannels/wallet-core';
+import {providers} from 'ethers';
 
 import {defaultConfig} from '../../config';
 import {ChainService} from '../chain-service';
@@ -20,40 +21,46 @@ beforeEach(() => {
 
 afterEach(() => chainService.destructor());
 
+function fundChannel(
+  expectedHeld: number,
+  amount: number,
+  channelId: string = randomChannelId()
+): {
+  channelId: string;
+  request: Promise<providers.TransactionResponse>;
+} {
+  const request = chainService.fundChannel({
+    channelId,
+    assetHolderAddress: ethAssetHolderAddress,
+    expectedHeld: BN.from(expectedHeld),
+    amount: BN.from(amount),
+  });
+  return {channelId, request};
+}
+
+async function waitForChannelFunding(
+  expectedHeld: number,
+  amount: number,
+  channelId: string = randomChannelId()
+): Promise<string> {
+  const request = await fundChannel(expectedHeld, amount, channelId).request;
+  await request.wait();
+  return channelId;
+}
+
 describe('fundChannel', () => {
   it('Successfully funds channel with 2 participants, rejects invalid 3rd', async () => {
-    const channelId = randomChannelId();
-    const request = await chainService.fundChannel({
-      channelId,
-      assetHolderAddress: ethAssetHolderAddress,
-      expectedHeld: BN.from(0),
-      amount: BN.from(5),
-    });
-    const receipt = request.wait();
-    expect((await receipt).status).toBe(1);
+    const channelId = await waitForChannelFunding(0, 5);
+    await waitForChannelFunding(5, 5, channelId);
 
-    const request2 = await chainService.fundChannel({
-      channelId,
-      assetHolderAddress: ethAssetHolderAddress,
-      expectedHeld: BN.from(5),
-      amount: BN.from(5),
-    });
-    const receipt2 = request2.wait();
-    expect((await receipt2).status).toBe(1);
-
-    const fundChannelPromise = chainService.fundChannel({
-      channelId,
-      assetHolderAddress: ethAssetHolderAddress,
-      expectedHeld: BN.from(5),
-      amount: BN.from(5),
-    });
+    const {request: fundChannelPromise} = fundChannel(5, 5, channelId);
     // todo: is there a good way to validate that the error thrown is one we expect?
     await expect(fundChannelPromise).rejects.toThrow();
   });
 });
 
 describe('registerChannel', () => {
-  it('Successfully registers channel and receives funding event', async () => {
+  it('Successfully registers channel and receives follow on funding event', async () => {
     const channelId = randomChannelId();
     const wrongChannelId = randomChannelId();
     let counter = 0;
@@ -69,18 +76,8 @@ describe('registerChannel', () => {
                 amount: BN.from(0),
               });
               counter++;
-              chainService.fundChannel({
-                channelId: wrongChannelId,
-                assetHolderAddress: ethAssetHolderAddress,
-                expectedHeld: BN.from(0),
-                amount: BN.from(5),
-              });
-              chainService.fundChannel({
-                channelId,
-                assetHolderAddress: ethAssetHolderAddress,
-                expectedHeld: BN.from(0),
-                amount: BN.from(5),
-              });
+              fundChannel(0, 5, wrongChannelId);
+              fundChannel(0, 5, channelId);
               break;
             case 1:
               expect(arg).toMatchObject({
