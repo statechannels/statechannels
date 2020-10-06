cdc7087e82e5ce6930c3f98ac390a841abe20d07
George C. Knee <georgeknee@googlemail.com>
2020-10-06 11:18:13 +0100

    Update packages/devtools/.eslintrc.js

--- packages/devtools/.eslintrc.js
+++ packages/devtools/.eslintrc.js
@@ -8,6 +8,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off'
-    // TODO remove these ^
+    // TODO remove these and fix resulting issues ^
   }
 };
