87070fd1c6778dad6533199a148b689def43590a
Mike Kerzhner <mikerzhner@gmail.com>
2020-10-05 17:50:47 -0700

    test: add chain service deposit stress test

---
+++ packages/server-wallet/src/chain-service/__chain-test__/stress.test.ts
@@ -0,0 +1,51 @@
+import {ContractArtifacts, randomChannelId} from '@statechannels/nitro-protocol';
+import {BN} from '@statechannels/wallet-core';
+import {Contract, providers, Wallet} from 'ethers';
+import _ from 'lodash';
+
+import {defaultConfig} from '../../config';
+import {ChainService} from '../chain-service';
+
+/* eslint-disable no-process-env, @typescript-eslint/no-non-null-assertion */
+const erc20AssetHolderAddress = process.env.ERC20_ASSET_HOLDER_ADDRESS!;
+const erc20Address = process.env.ERC20_ADDRESS!;
+/* eslint-enable no-process-env, @typescript-eslint/no-non-null-assertion */
+
+if (!defaultConfig.rpcEndpoint) throw new Error('rpc endpoint must be defined');
+const rpcEndpoint = defaultConfig.rpcEndpoint;
+const provider: providers.JsonRpcProvider = new providers.JsonRpcProvider(rpcEndpoint);
+const ethWallet = new Wallet(defaultConfig.serverPrivateKey, provider);
+
+let chainService: ChainService;
+
+beforeEach(() => {
+  chainService = new ChainService(rpcEndpoint, defaultConfig.serverPrivateKey, 50);
+});
+
+afterEach(() => chainService.destructor());
+
+// Do not run by default as it takes several minutes
+describe('fundChannel', () => {
+  it.skip('100 simultaneous deposits', async () => {
+    const depositAmount = 5;
+    const iterations = 100;
+    const contract: Contract = new Contract(
+      erc20Address,
+      ContractArtifacts.TokenArtifact.abi,
+      ethWallet
+    );
+    await (await contract.increaseAllowance(erc20AssetHolderAddress, BN.from(500))).wait();
+
+    const promises = _.range(0, iterations).map(() =>
+      chainService.fundChannel({
+        channelId: randomChannelId(),
+        assetHolderAddress: erc20AssetHolderAddress,
+        expectedHeld: BN.from(0),
+        amount: BN.from(depositAmount),
+        allowanceAlreadyIncreased: true,
+      })
+    );
+
+    Promise.all(promises);
+  }, 200_000);
+});
