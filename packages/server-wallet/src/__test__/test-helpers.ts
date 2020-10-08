c8bc5a381af2bb73b54aa6581b0b73ce693fa9b3
geoknee <georgeknee@googlemail.com>
2020-10-08 13:47:37 +0100

    test: define getPayloadFor(),getChannelResultFor()

---
+++ packages/server-wallet/src/__test__/test-helpers.ts
@@ -0,0 +1,22 @@
+import {ChannelResult} from '@statechannels/client-api-schema';
+
+import {Outgoing} from '..';
+
+export function getPayloadFor(participantId: string, outbox: Outgoing[]): unknown {
+  const filteredOutbox = outbox.filter(outboxItem => outboxItem.params.recipient === participantId);
+  if (filteredOutbox.length != 1)
+    throw Error(`Expected exactly one message in outbox: found ${filteredOutbox.length}`);
+  return filteredOutbox[0].params.data;
+}
+
+export function getChannelResultFor(
+  channelId: string,
+  channelResults: ChannelResult[]
+): ChannelResult {
+  const filteredChannelResults = channelResults.filter(
+    channelResult => channelResult.channelId === channelId
+  );
+  if (filteredChannelResults.length != 1)
+    throw Error(`Expected exactly one message in outbox: found ${filteredChannelResults.length}`);
+  return filteredChannelResults[0];
+}
