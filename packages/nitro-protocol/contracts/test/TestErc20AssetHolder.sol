f89dd760320c3aa7b87eb98782792793ce0d5f6b
geoknee <georgeknee@googlemail.com>
2020-10-19 14:13:11 +0100

    test: enhance TestErc20AssetHolder contract

--- packages/nitro-protocol/contracts/test/TestErc20AssetHolder.sol
+++ packages/nitro-protocol/contracts/test/TestErc20AssetHolder.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 import '../ERC20AssetHolder.sol';
+import './TESTAssetHolder.sol';
 
 /**
  * @dev This contract is a clone of the ERC20AssetHolder contract. It will be initialized to point to the TestNitroAdjudicator.
@@ -10,4 +11,26 @@ contract TestErc20AssetHolder is ERC20AssetHolder {
         public
         ERC20AssetHolder(_AdjudicatorAddress, _TokenAddress)
     {}
+
+    /**
+     * @dev Manually set the holdings mapping to a given amount for a given channelId.  Shortcuts the deposit workflow (ONLY USE IN A TESTING ENVIRONMENT)
+     * @param channelId Unique identifier for a state channel.
+     * @param amount The number of assets that should now be "escrowed: against channelId
+     */
+    function setHoldings(bytes32 channelId, uint256 amount) external {
+        holdings[channelId] = amount;
+    }
+
+    /**
+     * @dev Sets the given assetOutcomeHash for the given channelId in the assetOutcomeHashes storage mapping, but circumvents the AdjudicatorOnly modifier (thereby allowing externally owned accounts to call the method).
+     * @param channelId Unique identifier for a state channel.
+     * @param assetOutcomeHash The keccak256 of the abi.encode of the Outcome.
+     */
+    function setAssetOutcomeHashPermissionless(bytes32 channelId, bytes32 assetOutcomeHash)
+        external
+        returns (bool success)
+    {
+        _setAssetOutcomeHash(channelId, assetOutcomeHash);
+        return true;
+    }
 }
