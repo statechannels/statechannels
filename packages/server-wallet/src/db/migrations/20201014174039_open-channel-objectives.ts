fc11fdb4abbf4a1e7e10dbff61c24546c03928ef
geoknee <georgeknee@googlemail.com>
2020-10-19 18:41:21 +0100

    refactor: add open-,close-channel-objective tables

---
+++ packages/server-wallet/src/db/migrations/20201014174039_open-channel-objectives.ts
@@ -0,0 +1,17 @@
+import * as Knex from 'knex';
+
+const tableName = 'open-channel-objectives';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable(tableName, function(table) {
+    table.integer('objective_id'); // TODO make primary key (we would violate this constraint now)
+    table.string('status');
+    table.string('type');
+    table.string('target_channel_id');
+    table.string('funding_strategy');
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  await knex.schema.dropTable(tableName);
+}
