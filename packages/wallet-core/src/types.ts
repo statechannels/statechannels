2783e044c9786cd8ac6e05ad3a3f33996cc72f1b
geoknee <georgeknee@googlemail.com>
2020-10-20 07:48:52 +0100

    refactor: introduce objectiveId() fn

--- packages/wallet-core/src/types.ts
+++ packages/wallet-core/src/types.ts
@@ -150,6 +150,20 @@ export const isFundGuarantor = guard<FundGuarantor>('FundGuarantor');
 export const isFundLedger = guard<FundLedger>('FundLedger');
 export const isCloseLedger = guard<CloseLedger>('CloseLedger');
 
+export function objectiveId(objective: Objective): string {
+  switch (objective.type) {
+    case 'OpenChannel':
+    case 'CloseChannel':
+    case 'VirtuallyFund':
+      return [objective.type, objective.data.targetChannelId].join('-');
+    case 'FundGuarantor':
+      return [objective.type, objective.data.guarantorId].join('-');
+    case 'FundLedger':
+    case 'CloseLedger':
+      return [objective.type, objective.data.ledgerId].join('-');
+  }
+}
+
 type GetChannel = {type: 'GetChannel'; channelId: string};
 export type ChannelRequest = GetChannel;
 
