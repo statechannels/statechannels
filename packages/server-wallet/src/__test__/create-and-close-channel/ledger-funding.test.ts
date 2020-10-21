9e0499f4dc2d354745e9f371bf21e6dbab623609
Liam Horne <liam@lihorne.com>
2020-10-21 09:41:14 -0400

    test: add higher level wallet testing strategy

--- packages/server-wallet/src/__test__/create-and-close-channel/ledger-funding.test.ts
+++ packages/server-wallet/src/__test__/create-and-close-channel/ledger-funding.test.ts
@@ -1,8 +1,17 @@
-import {CreateChannelParams, Participant, Allocation} from '@statechannels/client-api-schema';
-import {BN, makeDestination} from '@statechannels/wallet-core';
+import {CreateChannelParams, Allocation} from '@statechannels/client-api-schema';
+import {
+  AllocationItem,
+  areAllocationItemsEqual,
+  BN,
+  makeDestination,
+  Participant,
+} from '@statechannels/wallet-core';
 import {ethers} from 'ethers';
+import {hexZeroPad} from 'ethers/lib/utils';
 
+import {Outgoing} from '../..';
 import {defaultTestConfig} from '../../config';
+import {Bytes32} from '../../type-aliases';
 import {Wallet} from '../../wallet';
 import {getChannelResultFor, getPayloadFor, getSignedStateFor} from '../test-helpers';
 
@@ -103,6 +112,23 @@ const createLedgerChannel = async (): Promise<string> => {
 afterEach(() => {
   a.store.ledgers = {};
   b.store.ledgers = {};
+});
+
+expect.extend({
+  toContainAllocationItem(received: AllocationItem[], argument: AllocationItem) {
+    const pass = received.some(areAllocationItemsEqual.bind(null, argument));
+    if (pass) {
+      return {
+        pass: true,
+        message: () => `expected ${received} not to be divisible by ${argument}`,
+      };
+    } else {
+      return {
+        pass: false,
+        message: () => `expected ${received} to be divisible by ${argument}`,
+      };
+    }
+  },
 });
 
 describe('Funding a single channel', () => {
@@ -586,5 +612,256 @@ describe('Funding multiple channels concurrently (two sides)', () => {
     );
 
     expect(resultA5.outbox).toMatchObject([]);
+  });
+
+  const testCreateChannelParams = (): CreateChannelParams => ({
+    participants: [participantA, participantB],
+    allocations: [
+      {
+        allocationItems: [
+          {
+            destination: participantA.destination,
+            amount: BN.from(1),
+          },
+          {
+            destination: participantB.destination,
+            amount: BN.from(1),
+          },
+        ],
+        token: ETH_ASSET_HOLDER_ADDRESS, // must be even length
+      },
+    ],
+    appDefinition: ethers.constants.AddressZero,
+    appData: '0x00', // must be even length
+    fundingStrategy: 'Ledger',
+  });
+
+  async function exchangeMessagesBetweenAandB(bToA: Outgoing[][], aToB: Outgoing[][]) {
+    while (aToB.length + bToA.length > 0) {
+      const nextMessageFromB = bToA.shift();
+      const nextMessageFromA = aToB.shift();
+
+      const newFromA =
+        nextMessageFromB &&
+        (await a.pushMessage(getPayloadFor(participantA.participantId, nextMessageFromB)));
+
+      const newFromB =
+        nextMessageFromA &&
+        (await b.pushMessage(getPayloadFor(participantB.participantId, nextMessageFromA)));
+
+      newFromB?.outbox.length && bToA.push(newFromB.outbox);
+      newFromA?.outbox.length && aToB.push(newFromA.outbox);
+    }
+  }
+
+  async function proposeMultipleChannelsToEachother(
+    createChannelParams: CreateChannelParams,
+    N = 2 // Total number of channels = 2 * N
+  ): Promise<{
+    aToJoin: Bytes32[];
+    bToJoin: Bytes32[];
+  }> {
+    const aToJoin = [];
+    const bToJoin = [];
+    for (let i = 0; i < N; i++) {
+      const createA1 = await a.createChannel(createChannelParams);
+      await b.pushMessage(getPayloadFor(participantB.participantId, createA1.outbox));
+      const createB2 = await b.createChannel(createChannelParams);
+      await a.pushMessage(getPayloadFor(participantA.participantId, createB2.outbox));
+      aToJoin.push(createB2.channelResults[0].channelId);
+      bToJoin.push(createA1.channelResults[0].channelId);
+    }
+    return {aToJoin, bToJoin};
+  }
+
+  it('can fund 4 channels by ledger, 2 created concurrently at a time', async () => {
+    const ledgerChannelId = await createLedgerChannel();
+
+    const createA1 = await a.createChannel(testCreateChannelParams());
+    const createA2 = await a.createChannel(testCreateChannelParams());
+    const channelId1 = createA1.channelResults[0].channelId;
+    const channelId2 = createA2.channelResults[0].channelId;
+
+    await b.pushMessage(getPayloadFor(participantB.participantId, createA1.outbox));
+    await b.pushMessage(getPayloadFor(participantB.participantId, createA2.outbox));
+
+    const createB3 = await b.createChannel(testCreateChannelParams());
+    const createB4 = await b.createChannel(testCreateChannelParams());
+    const channelId3 = createB3.channelResults[0].channelId;
+    const channelId4 = createB4.channelResults[0].channelId;
+
+    await a.pushMessage(getPayloadFor(participantA.participantId, createB3.outbox));
+    await a.pushMessage(getPayloadFor(participantA.participantId, createB4.outbox));
+
+    const joinB1 = await b.joinChannel({channelId: channelId1});
+    const joinB2 = await b.joinChannel({channelId: channelId2});
+    const joinA3 = await a.joinChannel({channelId: channelId3});
+    const joinA4 = await a.joinChannel({channelId: channelId4});
+
+    const messagesAwillSendToB = [joinA3.outbox, joinA4.outbox];
+    const messagesBwillSendToA = [joinB1.outbox, joinB2.outbox];
+
+    await exchangeMessagesBetweenAandB(messagesBwillSendToA, messagesAwillSendToB);
+
+    const {channelResults: channelResultsA} = await a.getChannels();
+    const {channelResults: channelResultsB} = await b.getChannels();
+
+    for (const channelId of [channelId1, channelId2, channelId3, channelId4]) {
+      expect(getChannelResultFor(channelId, channelResultsA)).toMatchObject({
+        turnNum: 3,
+        status: 'running',
+      });
+      expect(getChannelResultFor(channelId, channelResultsB)).toMatchObject({
+        turnNum: 3,
+        status: 'running',
+      });
+    }
+
+    const expectedLedgerAllocations = [
+      {
+        allocationItems: [
+          {destination: participantA.destination, amount: hexZeroPad(BN.from(6), 32)},
+          {destination: participantB.destination, amount: hexZeroPad(BN.from(6), 32)},
+          {destination: channelId1, amount: hexZeroPad(BN.from(2), 32)},
+          {destination: channelId2, amount: hexZeroPad(BN.from(2), 32)},
+          {destination: channelId3, amount: hexZeroPad(BN.from(2), 32)},
+          {destination: channelId4, amount: hexZeroPad(BN.from(2), 32)},
+        ],
+      },
+    ];
+
+    expect(getChannelResultFor(ledgerChannelId, channelResultsA)).toMatchObject({
+      status: 'running',
+      allocations: expectedLedgerAllocations,
+    });
+
+    expect(getChannelResultFor(ledgerChannelId, channelResultsB)).toMatchObject({
+      status: 'running',
+      allocations: expectedLedgerAllocations,
+    });
+  });
+
+  it('can fund 4 channels by ledger, 1 created at a time, using joinChannel', async () => {
+    const ledgerChannelId = await createLedgerChannel();
+
+    const {aToJoin, bToJoin} = await proposeMultipleChannelsToEachother(testCreateChannelParams());
+
+    const joinB1 = await b.joinChannel({channelId: bToJoin[0]});
+    const joinA2 = await a.joinChannel({channelId: aToJoin[0]});
+    const joinB3 = await b.joinChannel({channelId: bToJoin[1]});
+    const joinA4 = await a.joinChannel({channelId: aToJoin[1]});
+
+    const messagesAwillSendToB = [joinA2.outbox, joinA4.outbox];
+    const messagesBwillSendToA = [joinB1.outbox, joinB3.outbox];
+
+    await exchangeMessagesBetweenAandB(messagesBwillSendToA, messagesAwillSendToB);
+
+    const {channelResults: channelResultsA} = await a.getChannels();
+    const {channelResults: channelResultsB} = await b.getChannels();
+
+    const {
+      allocations: [{allocationItems: ledgerAllocationsA}],
+    } = getChannelResultFor(ledgerChannelId, channelResultsA);
+
+    const {
+      allocations: [{allocationItems: ledgerAllocationsB}],
+    } = getChannelResultFor(ledgerChannelId, channelResultsB);
+
+    const channelIds = [...bToJoin, ...aToJoin];
+
+    for (const channelId of channelIds) {
+      const running = {turnNum: 3, status: 'running'};
+      expect(getChannelResultFor(channelId, channelResultsA)).toMatchObject(running);
+      expect(getChannelResultFor(channelId, channelResultsB)).toMatchObject(running);
+      expect(ledgerAllocationsA).toContainAllocationItem({
+        destination: makeDestination(channelId),
+        amount: BN.from(2),
+      });
+      expect(ledgerAllocationsB).toContainAllocationItem({
+        destination: makeDestination(channelId),
+        amount: BN.from(2),
+      });
+    }
+
+    expect(ledgerAllocationsA).toContainAllocationItem({
+      destination: participantA.destination,
+      amount: BN.from(6),
+    });
+
+    expect(ledgerAllocationsA).toContainAllocationItem({
+      destination: participantB.destination,
+      amount: BN.from(6),
+    });
+
+    expect(ledgerAllocationsB).toContainAllocationItem({
+      destination: participantA.destination,
+      amount: BN.from(6),
+    });
+
+    expect(ledgerAllocationsB).toContainAllocationItem({
+      destination: participantB.destination,
+      amount: BN.from(6),
+    });
+  });
+
+  it('can fund 4 channels by ledger, 1 created at a time, using joinChannels', async () => {
+    const ledgerChannelId = await createLedgerChannel();
+
+    const {aToJoin, bToJoin} = await proposeMultipleChannelsToEachother(testCreateChannelParams());
+
+    const joinB = await b.joinChannels(bToJoin);
+    const joinA = await a.joinChannels(aToJoin);
+
+    const messagesAwillSendToB = [joinA.outbox];
+    const messagesBwillSendToA = [joinB.outbox];
+
+    await exchangeMessagesBetweenAandB(messagesBwillSendToA, messagesAwillSendToB);
+
+    const {channelResults: channelResultsA} = await a.getChannels();
+    const {channelResults: channelResultsB} = await b.getChannels();
+
+    const {
+      allocations: [{allocationItems: ledgerAllocationsA}],
+    } = getChannelResultFor(ledgerChannelId, channelResultsA);
+
+    const {
+      allocations: [{allocationItems: ledgerAllocationsB}],
+    } = getChannelResultFor(ledgerChannelId, channelResultsB);
+
+    const channelIds = [...bToJoin, ...aToJoin];
+
+    for (const channelId of channelIds) {
+      const running = {turnNum: 3, status: 'running'};
+      expect(getChannelResultFor(channelId, channelResultsA)).toMatchObject(running);
+      expect(getChannelResultFor(channelId, channelResultsB)).toMatchObject(running);
+      expect(ledgerAllocationsA).toContainAllocationItem({
+        destination: makeDestination(channelId),
+        amount: BN.from(2),
+      });
+      expect(ledgerAllocationsB).toContainAllocationItem({
+        destination: makeDestination(channelId),
+        amount: BN.from(2),
+      });
+    }
+
+    expect(ledgerAllocationsA).toContainAllocationItem({
+      destination: participantA.destination,
+      amount: BN.from(6),
+    });
+
+    expect(ledgerAllocationsA).toContainAllocationItem({
+      destination: participantB.destination,
+      amount: BN.from(6),
+    });
+
+    expect(ledgerAllocationsB).toContainAllocationItem({
+      destination: participantA.destination,
+      amount: BN.from(6),
+    });
+
+    expect(ledgerAllocationsB).toContainAllocationItem({
+      destination: participantB.destination,
+      amount: BN.from(6),
+    });
   });
 });
