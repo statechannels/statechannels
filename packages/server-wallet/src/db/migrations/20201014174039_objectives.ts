1409722ef18e9d56176c1c6bdccd99174926f0c1
geoknee <georgeknee@googlemail.com>
2020-10-21 13:40:07 +0100

    refactor: objectives table has a jsonb 'data' col

--- packages/server-wallet/src/db/migrations/20201014174039_objectives.ts
+++ packages/server-wallet/src/db/migrations/20201014174039_objectives.ts
@@ -7,16 +7,7 @@ export async function up(knex: Knex): Promise<any> {
     table.string('objective_id').primary();
     table.string('status').notNullable();
     table.string('type').notNullable();
-    table.string('target_channel_id');
-    table.string('funding_strategy');
-    table.string('ledger_id');
-    table.string('joint_channel_id');
-    table.string('guarantor_id');
-
-    table.foreign('target_channel_id').references('channels.channel_id');
-    table.foreign('ledger_id').references('channels.channel_id');
-    table.foreign('joint_channel_id').references('channels.channel_id');
-    table.foreign('guarantor_id').references('channels.channel_id');
+    table.jsonb('data');
   });
 }
 
