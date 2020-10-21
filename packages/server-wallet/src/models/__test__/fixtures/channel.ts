9d71e17b9d15c8c9f15b3821fd6277cd3055cc4d
Liam Horne <liam@lihorne.com>
2020-10-20 20:59:14 -0400

    test: remove hard coded channel fixture argument for Direct

--- packages/server-wallet/src/models/__test__/fixtures/channel.ts
+++ packages/server-wallet/src/models/__test__/fixtures/channel.ts
@@ -28,7 +28,7 @@ export const channel: Fixture<Channel> = (props?: DeepPartial<RequiredColumns>) 
 
   columns.vars.map(s => (s = dropNonVariables(addHash({...columns, ...s}))));
   (columns as any).channelId = calculateChannelId(columns);
-  const channel = Channel.fromJson({...columns, fundingStrategy: 'Direct'});
+  const channel = Channel.fromJson({...columns});
 
   return channel;
 };
