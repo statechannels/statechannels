f42a0681f932d835f65372a6d3d84543c313ae84
geoknee <georgeknee@googlemail.com>
2020-10-23 14:53:38 +0100

    feat: add ledgers table and ledger model

---
+++ packages/server-wallet/src/db/migrations/20201014174041_ledgers.ts
@@ -0,0 +1,19 @@
+import * as Knex from 'knex';
+
+const tableName = 'ledgers';
+
+export async function up(knex: Knex): Promise<any> {
+  await knex.schema.createTable(tableName, function(table) {
+    table
+      .string('ledger_channel_id')
+      .primary()
+      .references('channel_id')
+      .inTable('channels');
+    table.string('asset_holder_address').notNullable();
+    table.string('status').notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  await knex.schema.dropTable(tableName);
+}
