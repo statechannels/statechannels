d915427cda940583eb58a5fb15ed9210215884dc
Tom Close <tom.close@cantab.net>
2020-10-23 16:10:11 +0100

    fix: disable validTransition check for ledger channels

--- packages/server-wallet/src/wallet/store.ts
+++ packages/server-wallet/src/wallet/store.ts
@@ -190,6 +190,7 @@ export class Store {
       supported &&
       // If the state is the same as the support state then its not a transition just adding signatures
       !statesEqual(supported, state) &&
+      !(await this.isLedger(channelId)) &&
       !(await timer('validating transition', async () =>
         this.validateTransition(supported, signedState, tx)
       ))
@@ -540,7 +541,8 @@ export class Store {
     if (
       channel.supported &&
       // If the state is the same as the support state then its not a transition just adding signatures
-      !statesEqual(channel.supported, incomingState)
+      !statesEqual(channel.supported, incomingState) &&
+      !(await this.isLedger(channelId))
     ) {
       const {supported} = channel;
 
@@ -549,7 +551,7 @@ export class Store {
           this.validateTransition(supported, incomingState, tx)
         ))
       ) {
-        logger.error('Invalid state transition — state is stored but not supported', {
+        throw new StoreError('Invalid state transition', {
           from: channel.supported,
           to: wireSignedState,
         });
