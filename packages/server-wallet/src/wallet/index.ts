ade15bfea8d1a033028e938944484477bb443335
Mike Kerzhner <1062379+kerzhner@users.noreply.github.com>
2020-10-27 16:05:07 -0700

    fix: Add non-null assertion for app.supported
    Co-authored-by: andrewgordstewart <andrew.gord.stewart@gmail.com>

--- packages/server-wallet/src/wallet/index.ts
+++ packages/server-wallet/src/wallet/index.ts
@@ -566,7 +566,7 @@ export class Wallet extends EventEmitter<WalletEvent>
               await this.store.addChainServiceRequest(action.channelId, 'withdraw', tx);
               // Note, this returns a promise. We are NOT waiting on the promise
               // app.supported is defined (if the wallet is functioning correctly), but the compiler is not aware of that
-              this.chainService.concludeAndWithdraw(app.supported ? [app.supported] : []);
+              this.chainService.concludeAndWithdraw([app.supported!]);
               return;
             default:
               throw 'Unimplemented';
