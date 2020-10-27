2fd4a0d075bbcc807cd482187aaca2e3e0d4bbc7
Mike Kerzhner <mikerzhner@gmail.com>
2020-10-27 13:31:22 -0700

    test: create multiple accounts for ganache

--- packages/server-wallet/src/config.ts
+++ packages/server-wallet/src/config.ts
@@ -42,10 +42,10 @@ export const defaultConfig: ServerWalletConfig = {
   postgresDBPassword: process.env.SERVER_DB_PASSWORD,
   serverSignerPrivateKey:
     process.env.SERVER_SIGNER_PRIVATE_KEY ||
-    '0x7ab741b57e8d94dd7e1a29055646bafde7010f38a900f55bbd7647880faa6ee8',
+    '0x1b427b7ab88e2e10674b5aa92bb63c0ca26aa0b5a858e1d17295db6ad91c049b',
   serverPrivateKey:
     process.env.SERVER_PRIVATE_KEY ||
-    '0x1b427b7ab88e2e10674b5aa92bb63c0ca26aa0b5a858e1d17295db6ad91c049b',
+    '0x7ab741b57e8d94dd7e1a29055646bafde7010f38a900f55bbd7647880faa6ee8',
   rpcEndpoint: process.env.RPC_ENDPOINT,
   chainNetworkID: process.env.CHAIN_NETWORK_ID || '0x00',
   ethAssetHolderAddress: process.env.ETH_ASSET_HOLDER_ADDRESS,
