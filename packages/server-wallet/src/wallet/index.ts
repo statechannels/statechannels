5cd6ec98850905712906628bc4a1cb9d7dbf6fff
Liam Horne <liam@lihorne.com>
2020-10-29 15:02:53 -0400

    chore: nit - amountOfChannels -> numberOfChannels

--- packages/server-wallet/src/wallet/index.ts
+++ packages/server-wallet/src/wallet/index.ts
@@ -94,7 +94,7 @@ export type WalletInterface = {
   // App channel management
   createChannels(
     args: CreateChannelParams,
-    amountOfChannels: number
+    numberOfChannels: number
   ): Promise<MultipleChannelOutput>;
 
   joinChannels(channelIds: ChannelId[]): Promise<MultipleChannelOutput>;
@@ -298,7 +298,7 @@ export class Wallet extends EventEmitter<WalletEvent>
   }
   async createChannels(
     args: CreateChannelParams,
-    amountOfChannels: number
+    numberOfChannels: number
   ): Promise<MultipleChannelOutput> {
     const {
       participants,
@@ -310,7 +310,7 @@ export class Wallet extends EventEmitter<WalletEvent>
     } = args;
     const outcome: Outcome = deserializeAllocations(allocations);
     const results = await Promise.all(
-      _.range(amountOfChannels).map(async () => {
+      _.range(numberOfChannels).map(async () => {
         const channelNonce = await this.store.nextNonce(participants.map(p => p.signingAddress));
         const constants: ChannelConstants = {
           channelNonce,
