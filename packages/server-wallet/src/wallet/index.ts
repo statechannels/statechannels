b2e902e386786a896db3df4bd2bf369cb1bd3db0
Tom Close <tom.close@cantab.net>
2020-10-30 11:55:16 +0000

    refactor: tidy up objective manager initialization

--- packages/server-wallet/src/wallet/index.ts
+++ packages/server-wallet/src/wallet/index.ts
@@ -172,7 +172,7 @@ export class Wallet extends EventEmitter<WalletEvent>
       store: this.store,
       chainService: this.chainService,
       logger,
-      timingMetrics: !!walletConfig?.timingMetrics,
+      timingMetrics: this.walletConfig.timingMetrics,
     });
   }
 
