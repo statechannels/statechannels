5c891412353abb9433de2367bc5146f555d5dfc3
Tom Close <tom.close@cantab.net>
2020-10-30 14:25:38 +0000

    refactor: move ExecutionResult into a separate file

---
+++ packages/server-wallet/src/objectives/types.ts
@@ -0,0 +1,20 @@
+import {Logger} from 'pino';
+import {ChannelResult} from '@statechannels/client-api-schema';
+
+import {Store} from '../wallet/store';
+import {ChainServiceInterface} from '../chain-service';
+import {Outgoing} from '../protocols/actions';
+
+export interface ObjectiveManagerParams {
+  store: Store;
+  logger: Logger;
+  chainService: ChainServiceInterface;
+  timingMetrics: boolean;
+}
+
+// todo: currently duplicated in wallet/index.ts
+export type ExecutionResult = {
+  outbox: Outgoing[];
+  channelResults: ChannelResult[];
+  error?: any;
+};
