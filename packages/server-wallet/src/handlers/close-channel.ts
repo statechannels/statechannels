d25f93b7e2ccfb91cced04777e41ac5b2307c2b5
Liam Horne <liam@lihorne.com>
2020-10-31 18:04:00 -0400

    refactor: remove unused close-channel handler code

--- packages/server-wallet/src/handlers/close-channel.ts
+++ packages/server-wallet/src/handlers/close-channel.ts
@@ -1,16 +1,6 @@
-import {Either, left, right, chain, map} from 'fp-ts/lib/Either';
-import {StateVariables} from '@statechannels/wallet-core';
-import {pipe} from 'fp-ts/lib/function';
 import {ChannelId} from '@statechannels/client-api-schema';
 
-import {SignState, signState} from '../protocols/actions';
-import {ChannelState, ChannelStateWithSupported} from '../protocols/state';
 import {WalletError, Values} from '../errors/wallet-error';
-
-import {hasSupportedState, isMyTurn, supported} from './helpers';
-
-type HandlerResult = Either<CloseChannelError, SignState>;
-type StepResult = Either<CloseChannelError, ChannelStateWithSupported>;
 
 export interface CloseChannelHandlerParams {
   channelId: ChannelId;
@@ -31,30 +21,4 @@ export class CloseChannelError extends WalletError {
   ) {
     super(reason);
   }
-}
-
-const ensureSupportedStateExists = (
-  cs: ChannelState
-): Either<CloseChannelError, ChannelStateWithSupported> =>
-  hasSupportedState(cs)
-    ? right(cs)
-    : left(new CloseChannelError(CloseChannelError.reasons.noSupportedState));
-
-function ensureItIsMyTurn(cs: ChannelStateWithSupported): StepResult {
-  if (isMyTurn(cs)) return right(cs);
-  return left(new CloseChannelError(CloseChannelError.reasons.notMyTurn));
-}
-
-export function closeChannel(channelState: ChannelState): HandlerResult {
-  const {channelId} = channelState;
-  const signStateAction = (sv: StateVariables): SignState =>
-    signState({...sv, channelId, isFinal: true, turnNum: sv.turnNum + 1});
-
-  return pipe(
-    channelState,
-    ensureSupportedStateExists,
-    chain(ensureItIsMyTurn),
-    map(supported),
-    map(signStateAction)
-  );
 }
