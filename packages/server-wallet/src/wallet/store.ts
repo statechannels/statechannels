3564a6f4fd3db850b477de4a5164b44aa14d75d7
Liam Horne <liam@lihorne.com>
2020-11-01 18:39:00 -0500

    fix: remove subtely introduced additional JS ecrecover call (via deserialization fn)

--- packages/server-wallet/src/wallet/store.ts
+++ packages/server-wallet/src/wallet/store.ts
@@ -23,7 +23,6 @@ import {
   convertToParticipant,
   SignedState,
   objectiveId,
-  deserializeState,
   isSimpleAllocation,
   checkThat,
 } from '@statechannels/wallet-core';
@@ -573,8 +572,6 @@ export class Store {
         tx
       ));
 
-    const incomingState = deserializeState(wireSignedState);
-
     const sswh: SignedStateWithHash = addHash({
       chainId: wireSignedState.chainId,
       channelNonce: wireSignedState.channelNonce,
@@ -594,7 +591,7 @@ export class Store {
 
       if (
         !(await timer('validating transition', async () =>
-          this.validateTransition(supported, incomingState, tx)
+          this.validateTransition(supported, sswh, tx)
         ))
       ) {
         throw new StoreError('Invalid state transition', {
