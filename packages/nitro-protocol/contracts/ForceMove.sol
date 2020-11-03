064466f8e769186c6304e9a7bbe08ba67c66b5b7
geoknee <georgeknee@googlemail.com>
2020-11-03 15:30:33 +0000

    refactor: copypaste fn instead of importing

--- packages/nitro-protocol/contracts/ForceMove.sol
+++ packages/nitro-protocol/contracts/ForceMove.sol
@@ -3,7 +3,6 @@ pragma experimental ABIEncoderV2;
 
 import './interfaces/IForceMove.sol';
 import './interfaces/ForceMoveApp.sol';
-import "solidity-bytes-utils/contracts/BytesLib.sol";
 
 /**
  * @dev An implementation of ForceMove protocol, which allows state channels to be adjudicated and finalized.
@@ -502,7 +501,7 @@ contract ForceMove is IForceMove {
         // and that the b.turnNum = a.turnNum + 1
         if (isFinalAB[1]) {
             require(
-                BytesLib.equal(ab[1].outcome, ab[0].outcome),
+                _bytesEqual(ab[1].outcome, ab[0].outcome),
                 'InvalidTransitionError: Cannot move to a final state with a different default outcome'
             );
         } else {
@@ -512,11 +511,11 @@ contract ForceMove is IForceMove {
             );
             if (turnNumB < 2 * nParticipants) {
                 require(
-                    BytesLib.equal(ab[1].outcome, ab[0].outcome),
+                    _bytesEqual(ab[1].outcome, ab[0].outcome),
                     'InvalidTransitionError: Cannot change the default outcome during setup phase'
                 );
                 require(
-                    BytesLib.equal(ab[1].appData, ab[0].appData),
+                    _bytesEqual(ab[1].appData, ab[0].appData),
                     'InvalidTransitionError: Cannot change the appData during setup phase'
                 );
             } else {
@@ -532,6 +531,61 @@ contract ForceMove is IForceMove {
             }
         }
         return true;
+    }
+
+    /**
+     * @notice Check for equality of two byte strings
+     * @dev Check for equality of two byte strings
+     * @param _preBytes One bytes string
+     * @param _postBytes The other bytes string
+     * @return true if the bytes are identical, false otherwise.
+     */
+    function _bytesEqual(bytes memory _preBytes, bytes memory _postBytes)
+        internal
+        pure
+        returns (bool)
+    {
+        // copied from https://www.npmjs.com/package/solidity-bytes-utils/v/0.1.1
+        bool success = true;
+
+        assembly {
+            let length := mload(_preBytes)
+
+            // if lengths don't match the arrays are not equal
+            switch eq(length, mload(_postBytes))
+                case 1 {
+                    // cb is a circuit breaker in the for loop since there's
+                    //  no said feature for inline assembly loops
+                    // cb = 1 - don't breaker
+                    // cb = 0 - break
+                    let cb := 1
+
+                    let mc := add(_preBytes, 0x20)
+                    let end := add(mc, length)
+
+                    for {
+                        let cc := add(_postBytes, 0x20)
+                        // the next line is the loop condition:
+                        // while(uint256(mc < end) + cb == 2)
+                    } eq(add(lt(mc, end), cb), 2) {
+                        mc := add(mc, 0x20)
+                        cc := add(cc, 0x20)
+                    } {
+                        // if any of these checks fails then arrays are not equal
+                        if iszero(eq(mload(mc), mload(cc))) {
+                            // unsuccess:
+                            success := 0
+                            cb := 0
+                        }
+                    }
+                }
+                default {
+                    // unsuccess:
+                    success := 0
+                }
+        }
+
+        return success;
     }
 
     /**
