f88f04b32293435e910bcee00166611c94f11256
Mike Kerzhner <mikerzhner@gmail.com>
2020-11-05 13:01:33 -0800

    refactor: rename stale address and bytes checks

--- packages/server-wallet/src/db/utils.ts
+++ packages/server-wallet/src/db/utils.ts
@@ -1,9 +1,22 @@
 import * as Knex from 'knex';
 
-export function addAddressCheck(knex: Knex, table: string, column: string): Knex.Raw {
+export function addAddressCheckOld(knex: Knex, table: string, column: string): Knex.Raw {
   return knex.raw(`\
     ALTER TABLE ${table}\
     ADD CONSTRAINT ${column}_is_address CHECK (${column} ~ '^0x[0-9a-fA-F]{40}$')
+  `);
+}
+
+export function addBytesCheckOld(knex: Knex, table: string, column: string): Knex.Raw {
+  return knex.raw(`\
+    ALTER TABLE ${table}\
+    ADD CONSTRAINT ${column}_is_bytes CHECK (${column} ~ '^0x[0-9a-fA-F]*$')
+  `);
+}
+export function addBytes32CheckOld(knex: Knex, table: string, column: string): Knex.Raw {
+  return knex.raw(`\
+    ALTER TABLE ${table}\
+    ADD CONSTRAINT ${column}_is_bytes32 CHECK (${column} ~ '^0x[0-9a-fA-F]{64}$')
   `);
 }
 
@@ -14,15 +27,22 @@ export function addUint48Check(knex: Knex, table: string, column: string): Knex.
   `);
 }
 
+export function addAddressCheck(knex: Knex, table: string, column: string): Knex.Raw {
+  return knex.raw(`\
+    ALTER TABLE ${table}\
+    ADD CONSTRAINT ${column}_is_address CHECK (${column} ~ '^0x[0-9a-f]{40}$')
+  `);
+}
+
 export function addBytesCheck(knex: Knex, table: string, column: string): Knex.Raw {
   return knex.raw(`\
     ALTER TABLE ${table}\
-    ADD CONSTRAINT ${column}_is_bytes CHECK (${column} ~ '^0x[0-9a-fA-F]*$')
+    ADD CONSTRAINT ${column}_is_bytes CHECK (${column} ~ '^0x[0-9a-f]*$')
   `);
 }
 export function addBytes32Check(knex: Knex, table: string, column: string): Knex.Raw {
   return knex.raw(`\
     ALTER TABLE ${table}\
-    ADD CONSTRAINT ${column}_is_bytes32 CHECK (${column} ~ '^0x[0-9a-fA-F]{64}$')
+    ADD CONSTRAINT ${column}_is_bytes32 CHECK (${column} ~ '^0x[0-9a-f]{64}$')
   `);
 }
