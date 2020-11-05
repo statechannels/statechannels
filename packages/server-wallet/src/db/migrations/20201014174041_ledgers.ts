4cbe5f9b7d6f4dd6f38864b750861b028c57ff51
Liam Horne <liam@lihorne.com>
2020-11-05 15:08:20 -0500

    doc: remove unnecessary eslint-disable-next-line usages

--- packages/server-wallet/src/db/migrations/20201014174041_ledgers.ts
+++ packages/server-wallet/src/db/migrations/20201014174041_ledgers.ts
@@ -7,5 +7,6 @@ export async function up(knex: Knex): Promise<any> {
   });
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export async function down(_knex: Knex): Promise<any> {}
+export async function down(_knex: Knex): Promise<any> {
+  return;
+}
