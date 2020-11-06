370a818bf461e414922c516c537aae16d241a0dd
Mike Kerzhner <mikerzhner@gmail.com>
2020-11-06 13:27:10 -0800

    refactor: remove Address type alias

--- packages/server-wallet/src/type-aliases.ts
+++ packages/server-wallet/src/type-aliases.ts
@@ -1,5 +1,4 @@
 export declare type ParticipantId = string;
-export declare type Address = string;
 export declare type Uint256 = string;
 export declare type Uint48 = number;
 export declare type Bytes32 = string;
