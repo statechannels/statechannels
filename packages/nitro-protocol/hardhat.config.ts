f7b3a4aed54490fbd8a8e2bb498a78aa438dbe33
Mike Kerzhner <mikerzhner@gmail.com>
2020-11-13 17:13:01 -0800

    fix: hardcode the address used for rinkeby deployments

--- packages/nitro-protocol/hardhat.config.ts
+++ packages/nitro-protocol/hardhat.config.ts
@@ -19,7 +19,11 @@ module.exports = {
   solidity: '0.6.12',
   namedAccounts: {
     deployer: {
-      rinkeby: 0,
+      // Address used for the initial deploy of the contracts.
+      // Ask @kerzhner for the private key if you would like to deploy from the same address
+      // Otherwise uncomment the line below
+      //rinkeby: 0,
+      rinkeby: '0x87612aAD373586A38062c29F833A2AbC72038591',
     },
   },
   networks: {
