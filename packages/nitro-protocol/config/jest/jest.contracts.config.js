122cab76e403c5328d95592e197c7fe5b30cba7f
geoknee <georgeknee@googlemail.com>
2020-11-16 18:50:23 +0000

    test: do not use jest-gas-reporter
    It doesn't work anyway.
     I tried passing in the new contract artifact directory.

--- packages/nitro-protocol/config/jest/jest.contracts.config.js
+++ packages/nitro-protocol/config/jest/jest.contracts.config.js
@@ -1,6 +1,6 @@
 var config = require('./jest.config');
 config.testMatch = ['<rootDir>/test/contracts/**/*.test.ts'];
-config.reporters = ['default', '@statechannels/jest-gas-reporter'];
+config.reporters = ['default'];
 config.globalSetup = '<rootDir>/jest/contract-test-setup.ts';
 config.globalTeardown = '<rootDir>/jest/contract-test-teardown.ts';
 module.exports = config;
