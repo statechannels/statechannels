e056a11a3a70d020912515e5c5852fae5963f960
Mike Kerzhner <mikerzhner@gmail.com>
2020-11-16 16:17:04 -0800

    test: allow chain configuration to be defined in environment files
    This is useful for testing with networks other than Ganache

--- packages/server-wallet/jest/chain-setup.ts
+++ packages/server-wallet/jest/chain-setup.ts
@@ -1,32 +1,38 @@
 /* eslint-disable no-process-env */
 import {ETHERLIME_ACCOUNTS, GanacheServer} from '@statechannels/devtools';
+import {CHAIN_NETWORK_ID} from '@statechannels/wallet-core/lib/src/config';
 import {utils} from 'ethers';
 
 import {deploy} from '../deployment/deploy';
 
 export default async function setup(): Promise<void> {
-  process.env['CHAIN_NETWORK_ID'] = '0x01';
-  process.env['GANACHE_HOST'] = '0.0.0.0';
-  process.env['GANACHE_PORT'] = '8545';
-  process.env[
-    'RPC_ENDPOINT'
-  ] = `http://${process.env['GANACHE_HOST']}:${process.env['GANACHE_PORT']}`;
+  if (process.env.CHAIN_NETWORK_ID) {
+    console.log(
+      `CHAIN_NETWORK_ID defined as ${CHAIN_NETWORK_ID}. Assuming chain env vars are set by caller`
+    );
+  } else {
+    process.env['CHAIN_NETWORK_ID'] = '0x01';
+    process.env['GANACHE_HOST'] = '0.0.0.0';
+    process.env['GANACHE_PORT'] = '8545';
+    process.env[
+      'RPC_ENDPOINT'
+    ] = `http://${process.env['GANACHE_HOST']}:${process.env['GANACHE_PORT']}`;
 
-  const accounts = ETHERLIME_ACCOUNTS.map(account => ({
-    ...account,
-    amount: utils.parseEther('100').toString(),
-  }));
+    const accounts = ETHERLIME_ACCOUNTS.map(account => ({
+      ...account,
+      amount: utils.parseEther('100').toString(),
+    }));
 
-  if (!process.env.GANACHE_PORT) {
-    throw new Error('process.env.GANACHE_PORT must be defined');
+    if (!process.env.GANACHE_PORT) {
+      throw new Error('process.env.GANACHE_PORT must be defined');
+    }
+    const ganacheServer = new GanacheServer(parseInt(process.env.GANACHE_PORT), 1337, accounts);
+    await ganacheServer.ready();
+
+    const deployedArtifacts = await deploy();
+
+    process.env = {...process.env, ...deployedArtifacts};
+
+    (global as any).__GANACHE_SERVER__ = ganacheServer;
   }
-  const ganacheServer = new GanacheServer(parseInt(process.env.GANACHE_PORT), 1337, accounts);
-  await ganacheServer.ready();
-
-  const deployedArtifacts = await deploy();
-
-  process.env = {...process.env, ...deployedArtifacts};
-
-  (global as any).__ARTIFACTS__ = deployedArtifacts;
-  (global as any).__GANACHE_SERVER__ = ganacheServer;
 }
