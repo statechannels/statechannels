39116a1b9b10cda416542d095d01f223dd9947d6
geoknee <georgeknee@googlemail.com>
2020-11-17 10:23:24 +0000

    chore: use two separate hardhat configs
    This means we only have to worry about specifying
     certain config fields (which are populated from env vars)
     when we actually do a deploy.

--- packages/nitro-protocol/hardhat.config.ts
+++ packages/nitro-protocol/hardhat.config.ts
@@ -1,11 +1,4 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-import 'hardhat-deploy';
-import 'hardhat-deploy-ethers';
-
-const config = {
+export default {
   solidity: {
     version: '0.6.12',
     settings: {
@@ -23,31 +16,9 @@ const config = {
       },
     },
   },
-  namedAccounts: {
-    deployer: {},
-  },
-  networks: {},
   paths: {
     sources: 'contracts',
     deploy: 'hardhat-deploy',
     deployments: 'hardhat-deployments',
   },
 };
-
-const infuraToken = process.env.INFURA_TOKEN;
-const rinkebyDeployerPK = process.env.RINKEBY_DEPLOYER_PK;
-if (infuraToken && rinkebyDeployerPK) {
-  config.networks['rinkeby'] = {
-    url: 'https://rinkeby.infura.io/v3/' + infuraToken,
-    accounts: [rinkebyDeployerPK],
-  };
-  config.namedAccounts.deployer = {
-    rinkeby: '0x87612aAD373586A38062c29F833A2AbC72038591',
-    // Address used for the initial deploy of the contracts.
-    // Ask @kerzhner for the private key if you would like to deploy from the same address
-    // Otherwise uncomment the line below
-    //rinkeby: 0,
-  };
-}
-
-module.exports = config;
