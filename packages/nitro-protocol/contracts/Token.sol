99065de732a37d1e49691f350d013a74ba1ef8bb
Liam Horne <liam@lihorne.com>
2020-11-18 08:24:05 -0500

    test: increase number of tokens minted in Token.sol contract

--- packages/nitro-protocol/contracts/Token.sol
+++ packages/nitro-protocol/contracts/Token.sol
@@ -3,13 +3,13 @@ pragma solidity 0.7.4;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 /**
- * @dev This contract extends an ERC20 implementation, and mints 10,000 tokens to the deploying account. Used for testing purposes.
+ * @dev This contract extends an ERC20 implementation, and mints 10,000,000,000 tokens to the deploying account. Used for testing purposes.
  */
 contract Token is ERC20 {
     /**
-     * @dev Constructor function minting 10,000 tokens to the msg.sender (deploying account).
+     * @dev Constructor function minting 10,000,000,000 tokens to the msg.sender (deploying account).
      */
     constructor(uint256) ERC20('TestToken', 'TEST') {
-        _mint(msg.sender, 10000);
+        _mint(msg.sender, 10000000000);
     }
 }
