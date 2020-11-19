1a806e159bcf02c47021574e90eb83493187a9ab
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-11-19 09:26:04 -0700

    test: fix validator test

--- packages/wire-format/src/__tests__/validator.test.ts
+++ packages/wire-format/src/__tests__/validator.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-try-expect */
 import {messageIsValid, validateMessage} from '../validator';
 
 import * as good from './good_sample_messages';
@@ -19,21 +20,82 @@ describe('validate message', () => {
     expect(validateMessage(good.goodMessage)).toEqual(good.goodMessage);
   });
 
-  it('returns helpful error messages', () => {
-    expect(() => validateMessage(bad.dataMissing)).toThrow(
-      `Validation Error: Missing required property 'data' at root`
+  expect.extend({
+    toThrowValidationError(
+      errThrower,
+      message,
+      jsonBlob,
+      validationError
+    ): jest.CustomMatcherResult {
+      try {
+        errThrower();
+      } catch (err) {
+        if (err.constructor.name !== 'WireFormatValidationError')
+          return {
+            pass: false,
+            message: () =>
+              `expected function to throw WireFormatValidationError, threw ${err.constructor.name}`
+          };
+
+        if (err.message !== message) {
+          return {
+            pass: false,
+            message: () => `expected message ${message}, received ${err.message}`
+          };
+        }
+        if (err.jsonBlob !== jsonBlob) {
+          return {
+            pass: false,
+            message: () => `incorrect json blob`
+          };
+        }
+
+        if (!err.errorMessages.includes(validationError)) {
+          return {
+            pass: false,
+            message: () =>
+              `expected validation error ${validationError}, received array ${JSON.stringify(
+                err.errorMessages
+              )}`
+          };
+        }
+
+        return {pass: true, message: () => 'passed'};
+      }
+
+      return {pass: false, message: () => 'expected to throw error'};
+    }
+  });
+
+  it('test custom matcher', () => {
+    // TODO: I don't know how to get rid of this type error
+    expect(() => validateMessage(bad.dataMissing)).toThrowValidationError(
+      'Invalid message',
+      bad.dataMissing,
+      "Missing required property 'data' at root"
     );
-    expect(() => validateMessage(bad.extraProperty)).toThrow(
-      `Validation Error: Unexpected property 'iShouldntBeHere' found at root`
+    expect(() => validateMessage(bad.extraProperty)).toThrowValidationError(
+      'Invalid message',
+      bad.extraProperty,
+      "Unexpected property 'iShouldntBeHere' found at root "
     );
-    expect(() => validateMessage(bad.emptyState)).toThrow(
-      `Validation Error: Missing required property 'appData' at root.data.signedStates[0]`
+
+    expect(() => validateMessage(bad.emptyState)).toThrowValidationError(
+      'Invalid message',
+      bad.emptyState,
+      "Missing required property 'appData' at root.data.signedStates[0]"
     );
-    expect(() => validateMessage(bad.emptyStringObjectives)).toThrow(
-      `Validation Error: Property at root.data.objectives should be array`
+
+    expect(() => validateMessage(bad.emptyStringObjectives)).toThrowValidationError(
+      'Invalid message',
+      bad.emptyStringObjectives,
+      'Property at root.data.objectives should be array'
     );
-    expect(() => validateMessage(bad.nullObjectives)).toThrow(
-      `Validation Error: Property at root.data.objectives should be array`
+
+    expect(() => validateMessage(bad.nullObjectives)).toThrowValidationError(
+      'Invalid message',
+      bad.nullObjectives,
+      'Property at root.data.objectives should be array'
     );
   });
 });
