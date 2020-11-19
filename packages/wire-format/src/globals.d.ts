1f09d982d88522bb569931b8d90b12d590773b8f
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-11-19 12:05:54 -0700

    refactor: lint warning

--- packages/wire-format/src/globals.d.ts
+++ packages/wire-format/src/globals.d.ts
@@ -2,6 +2,7 @@ export {};
 declare global {
   namespace jest {
     interface Matchers</* eslint-disable-line */ R> {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       toThrowValidationError<R>(message: string, jsonBlob: any, validationError: string): R;
     }
   }
