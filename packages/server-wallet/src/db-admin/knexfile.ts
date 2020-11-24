0a963c4b5df1cce8537dc16c2362c60afe36d799
Alex Gap <alex.gap@mesh.xyz>
2020-11-23 18:01:26 -0800

    chore: remove outdated reference

--- packages/server-wallet/src/db-admin/knexfile.ts
+++ packages/server-wallet/src/db-admin/knexfile.ts
@@ -38,7 +38,6 @@ export function createKnexConfig(walletConfig: ServerWalletConfig): Config<any> 
       loadExtensions: extensions,
     },
     /**
-     * SEE: db-admin-connection.ts
      * To safely run migrations, we cannot use knexSnakeCaseMappers in the knex config
      * https://github.com/Vincit/objection.js/issues/1144
      * So, in our admin knex config, which is used for running migrations, we
