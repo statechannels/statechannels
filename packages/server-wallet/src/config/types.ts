a3f20ebad5fb8c937b7e5b0b459d1b7aebc7e822
Alex Gap <alex.gap@mesh.xyz>
2020-11-24 11:14:24 -0800

    chore: documentation

--- packages/server-wallet/src/config/types.ts
+++ packages/server-wallet/src/config/types.ts
@@ -1,38 +1,65 @@
 import {Level} from 'pino';
 
+/**
+ * Either a connection string or a config object with the dbName specified
+ */
 export type DatabaseConnectionConfiguration = RequiredConnectionConfiguration &
   Partial<OptionalConnectionConfiguration>;
 
+/**
+ * Either a database connection string or a config object specifying the database name
+ */
 export type RequiredConnectionConfiguration = {dbName: string} | string;
+
+/**
+ * Optional database config properties.
+ */
 export type OptionalConnectionConfiguration =
   | {host: string; port: number; user?: string; password?: string}
   | string;
 
+/**
+ * Database pool size options
+ */
 export type DatabasePoolConfiguration = {max?: number; min?: number};
 
+/**
+ * The minimum required database configuration that must be provided
+ */
 export type RequiredDatabaseConfiguration = {connection: RequiredConnectionConfiguration};
 export type OptionalDatabaseConfiguration = {
   pool?: DatabasePoolConfiguration;
   debug?: boolean;
   connection: OptionalConnectionConfiguration;
 };
+
+/**
+ * The fully defined database configuration
+ */
 export type DatabaseConfiguration = RequiredDatabaseConfiguration & OptionalDatabaseConfiguration;
 
-export type LoggingConfiguration = {logLevel: Level; logDestination: string};
-export type MetricConfiguration = {timingMetrics: boolean; metricsOutputFile?: string};
 /**
- * The required configuration to use the server wallet
+ * Logging configuration options
+ */
+export type LoggingConfiguration = {logLevel: Level; logDestination: string};
+
+/**
+ * Metrics configuration options
+ */
+export type MetricConfiguration = {timingMetrics: boolean; metricsOutputFile?: string};
+
+/**
+ * The minimum required configuration to use the server wallet.
  */
 export type RequiredServerWalletConfig = {
   databaseConfiguration: RequiredDatabaseConfiguration;
 };
 
 /**
- * Additional configuration options for the server wallet
+ * Additional configuration options for the server wallet that are not required.
  */
 export interface OptionalServerWalletConfig {
   databaseConfiguration: OptionalDatabaseConfiguration;
-
   ethereumPrivateKey: string;
   networkConfiguration: NetworkConfiguration;
   skipEvmValidation: boolean;
@@ -41,10 +68,21 @@ export interface OptionalServerWalletConfig {
   metricConfiguration: MetricConfiguration;
 }
 
+/**
+ * This is a fully filled out config. All Required and Optional fields are defined.
+ */
 export type ServerWalletConfig = RequiredServerWalletConfig & OptionalServerWalletConfig;
+
+/**
+ * This is the config accepted by the wallet create method.
+ * It is the required config properties plus additional optional properties
+ */
 export type IncomingServerWalletConfig = RequiredServerWalletConfig &
   Partial<OptionalServerWalletConfig>;
 
+/**
+ * Various network configuration options
+ */
 export type NetworkConfiguration = {
   erc20Address?: string;
   ethAssetHolderAddress?: string;
