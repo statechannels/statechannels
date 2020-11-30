84cc061e6eec85865b91fd2fb660f1b4abd9da3a
geoknee <georgeknee@googlemail.com>
2020-11-30 13:50:10 +0000

    perf: enable solidity compiler optimization

--- packages/nitro-protocol/hardhat.config.ts
+++ packages/nitro-protocol/hardhat.config.ts
@@ -2,6 +2,10 @@ export default {
   solidity: {
     version: '0.7.4',
     settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
       outputSelection: {
         '*': {
           '*': [
