4ed1b195a3f2a32cf470c4c73bd7c67fd40cbf00
Liam Horne <liam@lihorne.com>
2020-11-30 05:18:09 -0500

    fix: use hexZeroPad to normalize string as bytes32 versus BigNumber.from (#2982)
    * fix: use hexZeroPad to normalize string as bytes32 versus BigNumber.from
    
    * test: add tests for convertBytes32ToAddress

---
+++ packages/nitro-protocol/test/src/contract/asset-holder.test.ts
@@ -0,0 +1,14 @@
+import {convertBytes32ToAddress} from '../../../src/contract/asset-holder';
+
+describe('convertBytes32ToAddress', () => {
+  it.each`
+    bytes32                                                                 | address
+    ${'0x0000000000000000000000000000000000000000000000000000000000000000'} | ${'0x0000000000000000000000000000000000000000'}
+    ${'0x000000000000000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'} | ${'0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa'}
+    ${'0x000000000000000000000000aAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa'} | ${'0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa'}
+    ${'0x000000000000000000000000000000000000000000000000000000000000000a'} | ${'0x000000000000000000000000000000000000000A'}
+    ${'0x000000000000000000000000000000000000000000000000000000000000000A'} | ${'0x000000000000000000000000000000000000000A'}
+  `(`$bytes32 -- $address`, ({bytes32, address}) => {
+    expect(convertBytes32ToAddress(bytes32)).toBe(address);
+  });
+});
