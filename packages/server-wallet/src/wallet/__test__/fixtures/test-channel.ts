472d812695a957b9eee3a5b7dabc3ce0e7ee1af5
Tom Close <tom.close@cantab.net>
2020-12-02 12:24:39 +0000

    refactor: update TestChannel to support final states and changing balances

--- packages/server-wallet/src/wallet/__test__/fixtures/test-channel.ts
+++ packages/server-wallet/src/wallet/__test__/fixtures/test-channel.ts
@@ -24,6 +24,12 @@ import {alice, bob} from './participants';
 import {alice as aliceWallet, bob as bobWallet} from './signing-wallets';
 import {stateWithHashSignedBy} from './states';
 
+interface TestChannelArgs {
+  aBal?: number;
+  bBal?: number;
+  channelNonce?: number;
+}
+
 /** A two-party channel between Alice and Bob, with state history. For testing purposes. */
 export class TestChannel {
   public participantA: Participant = alice();
@@ -32,7 +38,7 @@ export class TestChannel {
   public signingWalletA: SigningWallet = aliceWallet();
   public signingWalletB: SigningWallet = bobWallet();
   public startBals: [number, number];
-  public channelNonce = 5;
+  public channelNonce: number;
 
   public get participants(): Participant[] {
     return [this.participantA, this.participantB];
@@ -41,12 +47,14 @@ export class TestChannel {
     return [this.signingWalletA, this.signingWalletB];
   }
 
-  public static create(aBal: number, bBal: number): TestChannel {
-    return new TestChannel(aBal, bBal);
+  public static create(args: TestChannelArgs): TestChannel {
+    return new TestChannel(args);
   }
 
-  private constructor(aBal: number, bBal: number) {
+  protected constructor(args: TestChannelArgs) {
+    const {aBal, bBal, channelNonce} = {aBal: 5, bBal: 5, channelNonce: 5, ...args};
     this.startBals = [aBal, bBal];
+    this.channelNonce = channelNonce;
   }
 
   /**
@@ -57,38 +65,42 @@ export class TestChannel {
    * Note - in cases where participants double-sign the same states, n might _not_
    * be the turnNum
    */
-  public state(n: number): State {
+  public state(n: number, bals?: [number, number], final = ''): State {
     return {
       ...this.channelConstants,
       appData: '0x',
-      isFinal: false,
+      isFinal: final === 'final',
       turnNum: n,
-      outcome: this.startOutcome,
+      outcome: bals ? this.toOutcome(bals) : this.startOutcome,
     };
   }
 
-  public signedStateWithHash(n: number): SignedStateWithHash {
-    return stateWithHashSignedBy([this.signingWallets[n % 2]])(this.state(n));
+  public signedStateWithHash(n: number, bals?: [number, number], final = ''): SignedStateWithHash {
+    return stateWithHashSignedBy([this.signingWallets[n % 2]])(this.state(n, bals, final));
   }
 
   /**
    * Gives the nth state in the history, signed by the correct participant
    */
-  public wireState(n: number): WireState {
-    return serializeState(this.signedStateWithHash(n));
+  public wireState(n: number, bals?: [number, number], final = ''): WireState {
+    return serializeState(this.signedStateWithHash(n, bals, final));
   }
 
-  public wirePayload(n: number): Payload {
+  public wirePayload(n: number, bals?: [number, number], final = ''): Payload {
     return {
       walletVersion: WALLET_VERSION,
-      signedStates: [this.wireState(n)],
+      signedStates: [this.wireState(n, bals, final)],
     };
   }
 
   public get startOutcome(): Outcome {
+    return this.toOutcome(this.startBals);
+  }
+
+  public toOutcome([aBal, bBal]: [number, number]): Outcome {
     return simpleEthAllocation([
-      {destination: this.participantA.destination, amount: BN.from(this.startBals[0])},
-      {destination: this.participantB.destination, amount: BN.from(this.startBals[1])},
+      {destination: this.participantA.destination, amount: BN.from(aBal)},
+      {destination: this.participantB.destination, amount: BN.from(bBal)},
     ]);
   }
 
@@ -129,7 +141,44 @@ export class TestChannel {
     };
   }
 
+  public get openChannelPayload(): Payload {
+    return {
+      walletVersion: WALLET_VERSION,
+      signedStates: [this.wireState(0)],
+      objectives: [this.openChannelObjective],
+    };
+  }
+
   public get assetHolderAddress(): Address {
     return ETH_ASSET_HOLDER_ADDRESS;
+  }
+
+  public get getChannelRequest(): Payload {
+    return {
+      walletVersion: WALLET_VERSION,
+      requests: [{channelId: this.channelId, type: 'GetChannel'}],
+    };
+  }
+
+  static mergePayloads(payload1: Payload, payload2: Payload): Payload {
+    return {
+      walletVersion: payload1.walletVersion,
+      signedStates: combineArrays(payload1.signedStates, payload2.signedStates),
+      requests: combineArrays(payload1.requests, payload2.requests),
+      objectives: combineArrays(payload1.objectives, payload2.objectives),
+    };
+  }
+
+  static get emptyPayload(): Payload {
+    return {walletVersion: WALLET_VERSION};
+  }
+}
+
+function combineArrays<T>(a1: T[] | undefined, a2: T[] | undefined): T[] | undefined {
+  const result = [...(a1 || []), ...(a2 || [])];
+  if (result.length > 0) {
+    return result;
+  } else {
+    return undefined;
   }
 }
