3391f1fd482e511e16f38b9a79da71faadc14a1c
geoknee <georgeknee@googlemail.com>
2020-12-04 08:55:57 +0000

    docs: add comment about gas

--- packages/nitro-protocol/test/contracts/AssetHolder/transfer.test.ts
+++ packages/nitro-protocol/test/contracts/AssetHolder/transfer.test.ts
@@ -122,6 +122,7 @@ describe('transfer', () => {
           }
         });
         const {events: eventsFromTx, gasUsed} = await (await tx).wait();
+        // NOTE: _transferAsset is a NOOP in TESTAssetHolder, so gas costs will be much lower than for a real Asset Holder
         await writeGasConsumption('transfer.gas.md', name, gasUsed);
         expect(eventsFromTx).toMatchObject(expectedEvents);
         // Check new holdings
