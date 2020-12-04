f142680dc905197828b1e2e026e1630740274d8e
Liam Horne <liam@lihorne.com>
2020-12-04 11:53:54 -0500

    doc: note what the default hardhat network chainid is

--- packages/nitro-protocol/hardhat-deploy/02-token.ts
+++ packages/nitro-protocol/hardhat-deploy/02-token.ts
@@ -10,6 +10,7 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
 
   const chainId = await getChainId();
 
+  // This is the default hardhat network chainid
   if (chainId !== '31337') {
     console.info(`Skipping deployment of Token for chain with ID: ${chainId}`);
     return;
