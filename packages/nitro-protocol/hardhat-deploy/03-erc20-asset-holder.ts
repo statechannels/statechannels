87c31e7eb46e547a82cfacf5e40968ffe120773a
Liam Horne <liam@lihorne.com>
2020-12-04 11:53:54 -0500

    doc: note what the default hardhat network chainid is

--- packages/nitro-protocol/hardhat-deploy/03-erc20-asset-holder.ts
+++ packages/nitro-protocol/hardhat-deploy/03-erc20-asset-holder.ts
@@ -7,6 +7,7 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
 
   const chainId = await getChainId();
 
+  // This is the default hardhat network chainid
   if (chainId !== '31337') {
     console.info(`Skipping deployment of ERC20AssetHolder for chain with ID: ${chainId}`);
     return;
