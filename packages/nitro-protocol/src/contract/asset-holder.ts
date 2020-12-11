2901e98cc257c0efc88b21230d8701ae5fc9787b
geoknee <georgeknee@googlemail.com>
2020-12-11 09:17:20 +0000

    feat: add computeNewAllocation helper

--- packages/nitro-protocol/src/contract/asset-holder.ts
+++ packages/nitro-protocol/src/contract/asset-holder.ts
@@ -2,6 +2,8 @@ import {utils, BigNumber} from 'ethers';
 
 import {parseEventResult} from '../ethers-utils';
 
+import {AllocationItem} from './outcome';
+
 export interface DepositedEvent {
   destination: string;
   amountDeposited: BigNumber;
@@ -48,4 +50,55 @@ export function convertAddressToBytes32(address: string): string {
 
   // We pad to 66 = (32*2) + 2('0x')
   return utils.hexZeroPad(normalizedAddress, 32);
+}
+
+/**
+ *
+ * Emulates solidity code. TODO replace with PureEVM implementation?
+ * @param initialHoldings
+ * @param allocation
+ * @param indices
+ */
+export function computeNewAllocation(
+  initialHoldings: string,
+  allocation: AllocationItem[], // we must index this with a JS number that is less than 2**32 - 1
+  indices: number[]
+): {newAllocation: AllocationItem[]; deleted: boolean; payouts: string[]; totalPayouts: string} {
+  const payouts = Array(indices.length).fill(0);
+  let totalPayouts = BigNumber.from(0);
+  const newAllocation: AllocationItem[] = [];
+  let safeToDelete = true;
+  let surplus = BigNumber.from(initialHoldings);
+  let k = 0;
+
+  for (let i = 0; i < allocation.length; i++) {
+    newAllocation.push({
+      destination: allocation[i].destination,
+      amount: BigNumber.from(0).toHexString(),
+    });
+    const affordsForDestination = min(BigNumber.from(allocation[i].amount), surplus);
+    if (indices.length == 0 || (k < indices.length && indices[k] === i)) {
+      newAllocation[i].amount = BigNumber.from(allocation[i].amount)
+        .sub(affordsForDestination)
+        .toHexString();
+      payouts[k] = affordsForDestination.toHexString();
+      totalPayouts = totalPayouts.add(affordsForDestination);
+      ++k;
+    } else {
+      newAllocation[i].amount = allocation[i].amount;
+    }
+    if (!BigNumber.from(newAllocation[i].amount).isZero()) safeToDelete = false;
+    surplus = surplus.sub(affordsForDestination);
+  }
+
+  return {
+    newAllocation,
+    deleted: safeToDelete,
+    payouts,
+    totalPayouts: totalPayouts.toHexString(),
+  };
+}
+
+function min(a: BigNumber, b: BigNumber) {
+  return a.gt(b) ? b : a;
 }
