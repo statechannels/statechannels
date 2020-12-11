608ed0a3a940fa4f64596a29f8bfeaff827f7937
geoknee <georgeknee@googlemail.com>
2020-12-11 18:52:55 +0000

    chore: use tsconfig instead of tsConfig in config
    - quiets a warning

--- packages/nitro-protocol/config/jest/jest.config.js
+++ packages/nitro-protocol/config/jest/jest.config.js
@@ -6,7 +6,7 @@ const root = resolve(__dirname, '../../');
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfig: './tsconfig.json',
+      tsconfig: './tsconfig.json',
     },
   },
   rootDir: root,
