013ea5e397a611887c13e50251ce8f190d37c5ec
geoknee <georgeknee@googlemail.com>
2020-12-15 09:53:21 +0000

    build: add dummy variable to TestETHAssetHolder

--- packages/nitro-protocol/contracts/test/TestEthAssetHolder.sol
+++ packages/nitro-protocol/contracts/test/TestEthAssetHolder.sol
@@ -7,5 +7,6 @@ import '../ETHAssetHolder.sol';
  * @dev This contract is a clone of the ETHAssetHolder contract. It will be initialized to point to the TestNitroAdjudicator.
  */
 contract TestEthAssetHolder is ETHAssetHolder {
+    uint256 public dummy; // this is simply to make the contract have distinct bytecode from the ETHAssetHolder (otherwise bytecode verification can fail)
     constructor(address _AdjudicatorAddress) ETHAssetHolder(_AdjudicatorAddress) {} // solhint-disable-line no-empty-blocks
 }
