8c67bc32a2072945fbc913be223217584cad39a0
Tom Close <tom.close@cantab.net>
2020-12-18 15:34:48 +0000

    refactor: rewrite and fix DirectFunder

--- packages/server-wallet/src/protocols/direct-funder.ts
+++ packages/server-wallet/src/protocols/direct-funder.ts
@@ -1,4 +1,4 @@
-import {BN, checkThat, isSimpleAllocation} from '@statechannels/wallet-core';
+import {Address, BN, checkThat, isSimpleAllocation, Uint256} from '@statechannels/wallet-core';
 import _ from 'lodash';
 import {Transaction} from 'objection';
 import {Logger} from 'pino';
@@ -38,69 +38,83 @@ export class DirectFunder {
     response: WalletResponse,
     tx: Transaction
   ): Promise<boolean> {
-    // don't check channel state - that should already have happened
-    // check funding state
+    const assetHolderAddress = this.assetHolder(channel);
+    const [targetBefore, targetAfter, targetTotal] = this.fundingMilestones(channel);
 
-    if (channel.protocolState.directFundingStatus === 'Funded') {
-      return true;
-    }
+    const currentFunding = await this.store.getFunding(channel.channelId, assetHolderAddress, tx);
+    const currentAmount = currentFunding?.amount || 0;
 
-    // request fund channel if my turn
-    await this.requestFundChannelIfMyTurn(channel, tx);
+    // if we're fully funded, we're done
+    if (BN.gte(currentAmount, targetTotal)) return true;
+
+    // if it isn't my turn yet, take no action
+    if (BN.lt(currentAmount, targetBefore)) return false;
+
+    // if my deposit is already on chain, take no action
+    if (BN.gte(currentAmount, targetAfter)) return false;
+
+    // if there's an outstanding chain request, take no action
+    if (await this.store.fundingRequestExists(channel.channelId, tx)) return false;
+
+    // otherwise, deposit
+    const amountToDeposit = BN.sub(targetAfter, currentAmount); // previous checks imply this is >0
+    await ChainServiceRequest.insertOrUpdate(channel.channelId, 'fund', tx);
+    await this.chainService.fundChannel({
+      channelId: channel.channelId,
+      assetHolderAddress: assetHolderAddress,
+      expectedHeld: BN.from(currentAmount),
+      amount: BN.from(amountToDeposit),
+    });
 
     return false;
   }
 
-  private async requestFundChannelIfMyTurn(channel: Channel, tx: Transaction): Promise<void> {
-    const app = channel.protocolState;
+  private assetHolder(channel: Channel): Address {
+    const supported = channel.supported;
+    if (!supported) {
+      throw new Error(`Channel passed to DriectFunded has no supported state`);
+    }
+
+    const {assetHolderAddress} = checkThat(supported?.outcome, isSimpleAllocation);
+
+    return assetHolderAddress;
+  }
+
+  /**
+   * Returns a triple of balance [targetBefore, targetAfter, targetTotal], where
+   *  - targetBefore is the balance where I should start depositing
+   *  - targetAfter should be the balance where I stop depositing
+   *  - targetTotal is the total amount that should be in the channel
+   *
+   * @param channel
+   */
+  private fundingMilestones(channel: Channel): [Uint256, Uint256, Uint256] {
     /**
      * The below logic assumes:
      *  1. Each destination occurs at most once.
      *  2. We only care about a single destination.
      * One reason to drop (2), for instance, is to support ledger top-ups with as few state updates as possible.
      */
-
-    if (!app.supported) return;
-
-    const existingRequest = app.chainServiceRequests.find(csr => csr.request === 'fund');
-    if (existingRequest?.isValid()) return;
-
-    if (app.directFundingStatus !== 'ReadyToFund') return;
-
-    const myDestination = app.participants[app.myIndex].destination;
-    const {allocationItems, assetHolderAddress} = checkThat(
-      app.supported?.outcome,
-      isSimpleAllocation
-    );
-
-    const currentFunding = app.funding(assetHolderAddress)?.amount;
-    if (!currentFunding) {
-      // this is a developer error, so throw
-      throw new Error(
-        `Funding not loaded from db. Make sure you use .withGraphJoined('funding') when fetching the channel.`
-      );
+    const supported = channel.supported;
+    if (!supported) {
+      throw new Error(`Channel passed to DriectFunded has no supported state`);
     }
 
-    const allocationsBeforeMe = _.takeWhile(allocationItems, a => a.destination !== myDestination);
-    const targetBeforeMyDeposit = allocationsBeforeMe.map(a => a.amount).reduce(BN.add, BN.from(0));
-    if (BN.lt(currentFunding, targetBeforeMyDeposit)) return;
+    const myDestination = channel.participants[channel.myIndex].destination;
+    const {allocationItems} = checkThat(supported.outcome, isSimpleAllocation);
 
     const myAllocationItem = _.find(allocationItems, ai => ai.destination === myDestination);
     if (!myAllocationItem) {
       throw new Error(`My destination ${myDestination} is not in allocations ${allocationItems}`);
     }
-    if (BN.eq(myAllocationItem.amount, 0)) return;
-    const targetAfterMyDeposit = BN.add(targetBeforeMyDeposit, myAllocationItem.amount);
-    if (BN.gte(currentFunding, targetAfterMyDeposit)) return;
-    const amountToDeposit = BN.sub(targetAfterMyDeposit, currentFunding); // previous line implies this is >0
 
-    // actions
-    await ChainServiceRequest.insertOrUpdate(app.channelId, 'fund', tx);
-    await this.chainService.fundChannel({
-      channelId: app.channelId,
-      assetHolderAddress: assetHolderAddress,
-      expectedHeld: BN.from(currentFunding),
-      amount: BN.from(amountToDeposit),
-    });
+    const allocationsBefore = _.takeWhile(allocationItems, a => a.destination !== myDestination);
+    const targetBefore = allocationsBefore.map(a => a.amount).reduce(BN.add, BN.from(0));
+
+    const targetAfter = BN.add(targetBefore, myAllocationItem.amount);
+
+    const total = allocationItems.map(a => a.amount).reduce(BN.add, BN.from(0));
+
+    return [targetBefore, targetAfter, total];
   }
 }
