c0ee2212a6f6ba356709f55706e45dba68d84018
geoknee <georgeknee@googlemail.com>
2021-01-11 14:30:44 +0000

    refactor: reuse validateTransition

--- packages/server-wallet/src/models/channel.ts
+++ packages/server-wallet/src/models/channel.ts
@@ -25,6 +25,7 @@ import {
 } from '../protocols/state';
 import {WalletError, Values} from '../errors/wallet-error';
 import {dropNonVariables} from '../state-utils';
+import {validateTransition} from '../utilities/validate-transition';
 
 import {SigningWallet} from './signing-wallet';
 import {Funding} from './funding';
@@ -436,6 +437,11 @@ export class Channel extends Model implements RequiredColumns {
    * The support proof for the latest supported state, if one exists, [] otherwise
    */
   private get _latestSupportProof(): Array<SignedStateWithHash> {
+    // TODO: activate these fields for proper application checks (may be resource hungry)
+    const logger = undefined;
+    const byteCode = undefined;
+    const skipAppTransition = true;
+
     let supportProof: Array<SignedStateWithHash> = [];
 
     let participantsWhoHaveNotSigned = new Set(this.participants.map(p => p.signingAddress));
@@ -444,7 +450,10 @@ export class Channel extends Model implements RequiredColumns {
     for (const signedState of this.sortedStates) {
       // If there is not a valid transition we know there cannot be a valid support proof
       // so we clear out what we have and start at the current signed state
-      if (previousState && !this.validChain(signedState, previousState)) {
+      if (
+        previousState &&
+        !validateTransition(signedState, previousState, logger, byteCode, skipAppTransition)
+      ) {
         supportProof = [];
         participantsWhoHaveNotSigned = new Set(this.participants.map(p => p.signingAddress));
       }
@@ -467,27 +476,6 @@ export class Channel extends Model implements RequiredColumns {
       previousState = signedState;
     }
     return [];
-  }
-  // This is a simple check based on _requireValidTransition from NitroProtocol
-  // We will eventually want to perform a proper validTransition check
-  // but we will have to be careful where we do that to prevent eating up a ton of cpu
-  private validChain(firstState: SignedState, secondState: SignedState): boolean {
-    if (firstState.turnNum + 1 !== secondState.turnNum) {
-      return false;
-    }
-    if (secondState.isFinal) {
-      return outcomesEqual(firstState.outcome, secondState.outcome);
-    }
-    if (secondState.turnNum < 2 * this.nParticipants) {
-      return (
-        outcomesEqual(firstState.outcome, secondState.outcome) &&
-        firstState.appData === secondState.appData
-      );
-    }
-    if (secondState.appDefinition === '0x00') {
-      return false; // running apps with 0x00 need all signatures, no valid chain
-    }
-    return true;
   }
 
   public get otherParticipants(): Participant[] {
