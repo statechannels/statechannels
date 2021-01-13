f2a4f3326a0233e2efa1175c2620f10b48052ade
Alex Gap <alex.gap@gmail.com>
2021-01-13 10:00:51 -0800

    fix: switch column to jsonb[]

--- packages/server-wallet/src/db/migrations/20210112170805_rename_challenge_status.ts
+++ packages/server-wallet/src/db/migrations/20210112170805_rename_challenge_status.ts
@@ -6,13 +6,16 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(`ALTER TABLE ${old_table_name} RENAME TO ${new_table_name}`);
 
   await knex.schema.alterTable(new_table_name, table => {
-    table.renameColumn('challenge_state', 'states');
+    table.dropColumn('challenge_state');
+    table.specificType('states', 'jsonb[]');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.raw(`ALTER TABLE ${new_table_name} RENAME TO ${old_table_name}`);
+
   await knex.schema.alterTable(old_table_name, table => {
-    table.renameColumn('states', 'challenge_state');
+    table.dropColumn('states');
+    table.jsonb('challenge_state');
   });
 }
