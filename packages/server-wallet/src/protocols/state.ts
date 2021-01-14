1449403c2a91c7a715aa385bb4fd140bf1068e14
Liam Horne <liam@lihorne.com>
2021-01-14 08:24:33 -0800

    refactor: simplify logic further

--- packages/server-wallet/src/protocols/state.ts
+++ packages/server-wallet/src/protocols/state.ts
@@ -64,18 +64,19 @@ export const status = (channelState: ChannelState): ChannelStatus => {
   const {supported, latest, latestSignedByMe, support} = channelState;
   const {participants} = supported ?? latest;
 
-  if (supported?.isFinal) {
-    if (support?.every(s => s.isFinal)) return 'closed'; // the entire support chain isFinal
-    return 'closing'; // the latest state, and possibly other states, are isFinal
+  if (!supported) {
+    if (!latestSignedByMe) return 'proposed';
+    return 'opening';
   }
 
-  if (latest.turnNum < 2 * participants.length) {
-    if (supported?.turnNum === participants.length - 1) return 'running'; // 3
-    if (latestSignedByMe) return 'opening'; // 0 or 1 signed by me
-    return 'proposed'; // 0 or 1 signed, but not by me
+  if (supported.turnNum < 2 * participants.length - 1) return 'opening';
+
+  if (supported.isFinal) {
+    if (_.every(support, 'isFinal')) return 'closed';
+    return 'closing';
   }
 
-  return 'running'; // unambiguously running e.g., 4, 5, 6, ...
+  return 'running';
 };
 
 export const toChannelResult = (channelState: ChannelState): ChannelResult => {
