3cd1bd93719b218e5ec8d03d63ff4414f0f18e61
Andrew Stewart <andrew.gord.stewart@gmail.com>
2021-02-02 13:59:40 -0700

    chore: e2e test uses custom matchers

--- packages/server-wallet/jest/jest.e2e.config.js
+++ packages/server-wallet/jest/jest.e2e.config.js
@@ -1,7 +1,7 @@
 const config = require('./jest.config');
 config.testMatch = ['<rootDir>/e2e-test/*.test.ts?(x)'];
 // We don't want to use the default knex setup as we're using two DBs for the e2e test
-config.setupFilesAfterEnv = [];
+config.setupFilesAfterEnv = ['<rootDir>/jest/custom-matchers.ts'];
 config.globalSetup = '<rootDir>/jest/chain-setup.ts';
 config.globalTeardown = '<rootDir>/jest/test-teardown.ts';
 module.exports = config;
