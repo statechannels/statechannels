006f2b2261449dc66b4fa5c451234732a190d918
geoknee <georgeknee@googlemail.com>
2021-02-08 22:18:43 +0000

    remove many .catch

--- packages/server-wallet/src/wallet/store.ts
+++ packages/server-wallet/src/wallet/store.ts
@@ -421,11 +421,7 @@ export class Store {
 
     const objectiveToBeStored = {...objective, status: 'pending' as const};
 
-    return ObjectiveModel.insert(objectiveToBeStored, tx).catch(e => {
-      // If the objective exists our job is done
-      if (!(e instanceof UniqueViolationError)) throw e;
-      return ObjectiveModel.forId(objectiveId(objectiveToBeStored), tx);
-    }) as Promise<DBDefundChannelObjective>;
+    return ObjectiveModel.insert(objectiveToBeStored, tx) as Promise<DBDefundChannelObjective>;
   }
 
   async ensureSubmitChallengeObjective(
@@ -445,11 +441,7 @@ export class Store {
       status: 'pending' as const,
     };
 
-    return ObjectiveModel.insert(objectiveToBeStored, tx).catch(e => {
-      // If the objective exists our job is done
-      if (!(e instanceof UniqueViolationError)) throw e;
-      return ObjectiveModel.forId(objectiveId(objectiveToBeStored), tx);
-    }) as Promise<DBSubmitChallengeObjective>;
+    return ObjectiveModel.insert(objectiveToBeStored, tx) as Promise<DBSubmitChallengeObjective>;
   }
 
   async ensureOpenChannelObjective(
@@ -494,11 +486,7 @@ export class Store {
       .returning('*')
       .first();
 
-    return ObjectiveModel.insert(objectiveToBeStored, tx).catch(e => {
-      // If the objective exists our job is done
-      if (!(e instanceof UniqueViolationError)) throw e;
-      return ObjectiveModel.forId(objectiveId(objectiveToBeStored), tx);
-    }) as Promise<DBOpenChannelObjective>;
+    return ObjectiveModel.insert(objectiveToBeStored, tx) as Promise<DBOpenChannelObjective>;
   }
 
   async ensureCloseChannelObjective(
@@ -526,11 +514,7 @@ export class Store {
       },
     };
 
-    return ObjectiveModel.insert(objectiveToBeStored, tx).catch(e => {
-      // If the objective exists our job is done
-      if (!(e instanceof UniqueViolationError)) throw e;
-      return ObjectiveModel.forId(objectiveId(objectiveToBeStored), tx);
-    }) as Promise<DBCloseChannelObjective>;
+    return ObjectiveModel.insert(objectiveToBeStored, tx) as Promise<DBCloseChannelObjective>;
   }
 
   async isLedger(channelId: Bytes32, tx?: Transaction): Promise<boolean> {
