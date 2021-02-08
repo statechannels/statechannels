4d7662f8b842f72708bbc4581725245390b9f2b9
geoknee <georgeknee@googlemail.com>
2021-02-08 09:48:36 +0000

    chore: Channel implements ComputedColumns

--- packages/server-wallet/src/models/channel.ts
+++ packages/server-wallet/src/models/channel.ts
@@ -67,7 +67,9 @@ export type ComputedColumns = {
   readonly channelId: Bytes32;
 };
 
-export class Channel extends Model implements RequiredColumns {
+type ChannelColumns = RequiredColumns & ComputedColumns;
+
+export class Channel extends Model implements ChannelColumns {
   readonly id!: number;
 
   channelId!: Bytes32;
