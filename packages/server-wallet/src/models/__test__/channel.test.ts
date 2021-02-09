20119a4839fe28f6c27e841881911617ea5e0e83
Andrew Stewart <andrew.gord.stewart@gmail.com>
2021-02-08 22:26:00 -0800

    test: remove unnecessary test case
    the test fails when the default connection pool size is at most 1
    
    the point of this test case was to explain to devs how transactions work.
    I hope that devs know by now!

--- packages/server-wallet/src/models/__test__/channel.test.ts
+++ packages/server-wallet/src/models/__test__/channel.test.ts
@@ -38,30 +38,6 @@ it('does not store extraneous fields in the variables property', async () => {
   });
 });
 
-it('can insert multiple channels instances within a transaction', async () => {
-  const c1 = channel({vars: [stateWithHashSignedBy()()]});
-  const c2 = channel({
-    channelNonce: 1234,
-    vars: [stateWithHashSignedBy()({channelNonce: 1234})],
-  });
-
-  await Channel.transaction(knex, async tx => {
-    await Channel.query(tx).insert(c1);
-
-    expect(await Channel.query(tx).select()).toHaveLength(1);
-
-    await Channel.query(tx).insert(c2);
-    expect(await Channel.query(tx).select()).toHaveLength(2);
-
-    // You can query the DB outside of this transaction,
-    // where the channels have not yet been committed
-    expect(await Channel.query(knex).select()).toHaveLength(0);
-  });
-
-  // The transaction has been committed. Two channels were stored.
-  expect(await Channel.query(knex).select()).toHaveLength(2);
-});
-
 describe('validation', () => {
   it('throws when inserting a model where the channelId is inconsistent', () =>
     expect(
