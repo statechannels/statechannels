10bcb219c2f0246ab3cedd37e5c86213403a04a5
Andrew Stewart <andrew.gord.stewart@gmail.com>
2021-02-09 08:16:40 -0800

    test: export constructKnex
    this eases the construction of a new knex instance with a non-default
    configuration

--- packages/server-wallet/jest/knex-setup-teardown.ts
+++ packages/server-wallet/jest/knex-setup-teardown.ts
@@ -1,17 +1,36 @@
 import {configureEnvVariables} from '@statechannels/devtools';
 import Knex from 'knex';
 import _ from 'lodash';
-import {DBAdmin} from '../src/db-admin/db-admin'
+import {DBAdmin} from '../src/db-admin/db-admin';
 
 configureEnvVariables();
 
-import {extractDBConfigFromServerWalletConfig, defaultTestConfig} from '../src/config';
+import {
+  extractDBConfigFromServerWalletConfig,
+  defaultTestConfig,
+  DatabaseConfiguration,
+} from '../src/config';
 
+const constructedKnexs: Knex[] = []
+
+/**
+ * 
+ * @param databaseConfiguration 
+ * 
+ * returns a knex instance which is automatically destroyed after all jest tests have run
+ */
+export let constructKnex = (databaseConfiguration: Partial<DatabaseConfiguration>): Knex =>
+  {
+    const knex = Knex(extractDBConfigFromServerWalletConfig(defaultTestConfig({databaseConfiguration})));
+    constructedKnexs.push(knex)
+
+    return knex
+  }
 
 export let testKnex: Knex;
 
 beforeAll(async () => {
-  testKnex = Knex(extractDBConfigFromServerWalletConfig(defaultTestConfig()));
+  testKnex = constructKnex({});
   await DBAdmin.truncateDataBaseFromKnex(testKnex);
 });
 
@@ -19,5 +38,5 @@ afterAll(async () => {
   // We need to close the db connection after the test suite has run.
   // Otherwise, jest will not exit within the required one second after the test
   // suite has finished
-  await testKnex.destroy();
+  await Promise.all(constructedKnexs.map(async knex => knex.destroy()))
 });
