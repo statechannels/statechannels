5fdf1b9c0b0d534c61b38cc2914f6b76336dd4a6
Andrew Stewart <andrew.gord.stewart@gmail.com>
2021-02-10 15:03:21 -0800

    fix: multi-threaded wallet re-emits events that worker-wallet emitted

--- packages/server-wallet/src/wallet/multi-threaded-wallet/index.ts
+++ packages/server-wallet/src/wallet/multi-threaded-wallet/index.ts
@@ -1,10 +1,22 @@
+import {Worker} from 'worker_threads';
+
 import {UpdateChannelParams} from '@statechannels/client-api-schema';
 
 import {IncomingServerWalletConfig} from '../../config';
-import {MultipleChannelOutput, SingleChannelOutput} from '../types';
+import {MultipleChannelOutput, SingleChannelOutput, WalletEvent} from '../types';
 import {SingleThreadedWallet} from '../wallet';
 
 import {WorkerManager} from './manager';
+
+export type WalletEventEmitted<E extends WalletEvent = WalletEvent> = {
+  type: 'WalletEventEmitted';
+  name: E['type'];
+  value: E['value'];
+};
+
+function isEventEmitted<E extends WalletEvent>(msg: any): msg is WalletEventEmitted<E> {
+  return 'type' in msg && msg.type === 'WalletEventEmitted';
+}
 
 /**
  * A multi-threaded Nitro wallet
@@ -20,7 +32,11 @@ export class MultiThreadedWallet extends SingleThreadedWallet {
 
   protected constructor(walletConfig: IncomingServerWalletConfig) {
     super(walletConfig);
-    this.workerManager = new WorkerManager(this.walletConfig);
+    this.workerManager = new WorkerManager(this.walletConfig, (worker: Worker) =>
+      worker.on('message', (msg: any) => {
+        if (isEventEmitted(msg)) this.emit(msg.name, msg.value);
+      })
+    );
   }
 
   async updateChannel(args: UpdateChannelParams): Promise<SingleChannelOutput> {
