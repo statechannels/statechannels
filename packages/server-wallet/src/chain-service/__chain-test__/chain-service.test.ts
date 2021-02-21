34718ded3b7416ae7fbed75e2c3f874ba2155d67
isoosiss7 <jyuan08@gmail.com>
2021-02-21 09:37:23 -0800

    enh: fix comments in test case

--- packages/server-wallet/src/chain-service/__chain-test__/chain-service.test.ts
+++ packages/server-wallet/src/chain-service/__chain-test__/chain-service.test.ts
@@ -297,7 +297,7 @@ describe('registerChannel', () => {
     const channelId = randomChannelId();
     await waitForChannelFunding(0, 5, channelId);
     const blockDeposited = await provider.getBlockNumber();
-    mineBlocks(); // async
+    mineBlocks();
 
     await new Promise(resolve =>
       chainService.registerChannel(channelId, [ethAssetHolderAddress], {
@@ -313,10 +313,8 @@ describe('registerChannel', () => {
       })
     );
 
+    // by the time holdingUpdated is called, it should be 5 blocks away
     const blockReported = await provider.getBlockNumber();
-
-    // chainService.getInitialHoldings will wait for the deposit event
-    // to confirm, before triggering holdingUpdated
     expect(blockReported).toBeGreaterThanOrEqual(blockDeposited + 5);
   });
 
