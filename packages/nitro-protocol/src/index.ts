177d527cdc6d71ae7689fb42a15fa89d54de1f4a
geoknee <georgeknee@googlemail.com>
2021-03-08 17:25:00 +0000

    feat: add HashLockArtifact

--- packages/nitro-protocol/src/index.ts
+++ packages/nitro-protocol/src/index.ts
@@ -7,6 +7,7 @@ import FULLEthAssetHolderArtifact from '../artifacts/contracts/ETHAssetHolder.so
 import FULLTestNitroAdjudicatorArtifact from '../artifacts/contracts/test/TESTNitroAdjudicator.sol/TESTNitroAdjudicator.json';
 import FULLTestAssetHolderArtifact from '../artifacts/contracts/test/TESTAssetHolder.sol/TESTAssetHolder.json';
 import FULLCountingAppArtifact from '../artifacts/contracts/CountingApp.sol/CountingApp.json';
+import FULLHashLockArtifact from '../artifacts/contracts/examples/HashLock.sol/HashLock.json';
 
 // https://hardhat.org/guides/compile-contracts.html#artifacts
 const fields = [
@@ -34,6 +35,7 @@ export const ContractArtifacts = {
   NitroAdjudicatorArtifact: minimize(FULLNitroAdjudicatorArtifact),
   Erc20AssetHolderArtifact: minimize(FULLErc20AssetHolderArtifact),
   EthAssetHolderArtifact: minimize(FULLEthAssetHolderArtifact),
+  HashLock: minimize(FULLHashLockArtifact),
 };
 
 /*
