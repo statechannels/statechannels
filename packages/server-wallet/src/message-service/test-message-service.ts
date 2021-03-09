b4521ea16c46cf1ad44ea676487c274848762bfb
Alex Gap <alex.gap@gmail.com>
2021-03-09 15:52:36 -0800

    feat: introduce new message service
    Introduces a new message service that can be used to send messages
    between participants.

---
+++ packages/server-wallet/src/message-service/test-message-service.ts
@@ -0,0 +1,53 @@
+import {Message} from '@statechannels/client-api-schema';
+
+import {Wallet} from '../wallet';
+
+import {MessageHandler, MessagingServiceInterface} from './types';
+
+export class TestMessageService implements MessagingServiceInterface {
+  protected constructor(private _receive: MessageHandler) {}
+
+  static async createTestMessagingService(
+    messageHandler: MessageHandler
+  ): Promise<MessagingServiceInterface> {
+    const service = new TestMessageService(messageHandler);
+    return service;
+  }
+
+  async send(messages: Message[]): Promise<void> {
+    for (const message of messages) {
+      await this._receive(message.recipient, message.data, this);
+    }
+  }
+}
+
+/**
+ * This is a helper method that sets up a message service for a collection of wallets.
+ * Whenever handleMessages or send are called they are pushed into the appropriate wallet.
+ * Any response to the pushMessage is then sent to the other participants
+ * @param wallets The collection of wallets that will be communicating. A participantId must be provided for each wallet.
+ * @returns A messaging service that can be used to send messages.
+ * @example
+ * const ms = setupTestMessagingService(...bla);
+ * const result = wallet.createChannel(..bla);
+ *
+ * // This will send all the messages from the result of the create channel call
+ * // and will handle any responses to those messages and so on...
+ * await ms.handleMessages(result.outbox);
+ */
+export function setupTestMessagingService(
+  wallets: {participantId: string; wallet: Wallet}[]
+): Promise<MessagingServiceInterface> {
+  const messageHandler: MessageHandler = async (to, message, me) => {
+    // TODO: This assumes only 1 matching wallet.
+    const matching = wallets.find(w => w.participantId === to);
+
+    if (!matching) {
+      throw new Error(`Invalid to value ${to}`);
+    }
+    const result = await matching.wallet.pushMessage(message);
+
+    await me.send(result.outbox.map(o => o.params));
+  };
+  return TestMessageService.createTestMessagingService(messageHandler);
+}
---
+++ packages/server-wallet/src/message-service/types.ts
@@ -0,0 +1,30 @@
+import {Message} from '@statechannels/client-api-schema';
+
+/**
+ * This is the handler that any messaging service implementation should call when receiving a message
+ * The handler is responsible for pushing the message into the appropriate wallet
+ */
+export type MessageHandler = (
+  // Including the to makes it easier for a messaging service to handle multiple wallets if it chooses to do so
+  to: string,
+  message: unknown,
+  messageService: MessagingServiceInterface
+) => Promise<void>;
+
+/**
+ * A MessageServiceFactory is responsible for generating a MessageService.
+ * Eventually the wallet will require a MessageServiceFactory.
+ * The wallet will use it to construct the messagingService that it can use to send messages.
+ * messageHandler should be triggered whenever the messaging service receives a message.
+ */
+export type MessageServiceFactory = (
+  messageHandler: MessageHandler
+) => Promise<MessagingServiceInterface>;
+
+export interface MessagingServiceInterface {
+  /**
+   * Sends out the messages to the specified by participants.
+   * @param messages The collection of messages to send
+   */
+  send(message: Message[]): Promise<void>;
+}
