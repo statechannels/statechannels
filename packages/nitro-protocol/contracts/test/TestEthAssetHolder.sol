dfc7c06ca2aa5bade4a659c4c82abfc632927e48
geoknee <georgeknee@googlemail.com>
2021-03-15 21:04:02 +0000

    remove unecessary import

--- packages/nitro-protocol/contracts/test/TestEthAssetHolder.sol
+++ packages/nitro-protocol/contracts/test/TestEthAssetHolder.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.7.4;
 pragma experimental ABIEncoderV2;
 import '../ETHAssetHolder.sol';
-import './TESTAssetHolder.sol';
 
 /**
  * @dev This contract is a clone of the ETHAssetHolder contract. It will be initialized to point to the TestNitroAdjudicator.
