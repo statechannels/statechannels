d55c58a93c4529e0b8a6db67d5cd3c13d7047595
geoknee <georgeknee@googlemail.com>
2021-03-15 18:31:29 +0000

    update usd_per_eth and gwei_per_gas

--- packages/nitro-protocol/notes/blog-posts/gas-optimizations/gas-usage-chart.py
+++ packages/nitro-protocol/notes/blog-posts/gas-optimizations/gas-usage-chart.py
@@ -6,9 +6,9 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-usd_per_eth = 142
+usd_per_eth = 1783
 eth_per_gwei = 1e-9
-gwei_per_gas = 2
+gwei_per_gas = 138
 
 usd_per_1M_gas = usd_per_eth * eth_per_gwei * gwei_per_gas * 1e6
 print(usd_per_1M_gas)
