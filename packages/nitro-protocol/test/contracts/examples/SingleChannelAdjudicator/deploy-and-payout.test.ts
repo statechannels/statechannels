dc333c6f8dfb84962ee9464da0bace486c0c0c19
geoknee <georgeknee@googlemail.com>
2021-03-24 13:56:54 +0000

    add a test for deploying and paying out

---
+++ packages/nitro-protocol/test/contracts/examples/SingleChannelAdjudicator/deploy-and-payout.test.ts
@@ -0,0 +1,260 @@
+import {expectRevert} from '@statechannels/devtools';
+import {Contract, Wallet, ethers, BigNumber, utils, constants, ContractFactory} from 'ethers';
+
+import SingleChannelAdjudicatorArtifact from '../../../../artifacts/contracts/examples/SingleChannelAdjudicator.sol/SingleChannelAdjudicator.json';
+import TokenArtifact from '../../../../artifacts/contracts/Token.sol/Token.json';
+import {Channel, getChannelId} from '../../../../src/contract/channel';
+import {channelDataToStatus} from '../../../../src/contract/channel-storage';
+import {AllocationAssetOutcome} from '../../../../src/contract/outcome';
+import {State} from '../../../../src/contract/state';
+import {concludePushOutcomeAndTransferAllArgs} from '../../../../src/contract/transaction-creators/nitro-adjudicator';
+import {
+  checkMultipleAssetOutcomeHashes,
+  checkMultipleHoldings,
+  compileEventsFromLogs,
+  computeOutcome,
+  getPlaceHolderContractAddress,
+  getRandomNonce,
+  getTestProvider,
+  OutcomeShortHand,
+  randomChannelId,
+  randomExternalDestination,
+  replaceAddressesAndBigNumberify,
+  setupContracts,
+  writeGasConsumption,
+} from '../../../test-helpers';
+import {signStates} from '../../../../src';
+import {NITRO_MAX_GAS} from '../../../../src/transactions';
+
+const provider = getTestProvider();
+let SingleChannelAdjudicator: Contract;
+let Token: Contract;
+const chainId = process.env.CHAIN_NETWORK_ID;
+const participants = ['', '', ''];
+const wallets = new Array(3);
+const challengeDuration = 0x1000;
+
+let appDefinition;
+
+const addresses = {
+  // Channels
+  c: undefined,
+  C: randomChannelId(),
+  X: randomChannelId(),
+  // Externals
+  A: randomExternalDestination(),
+  B: randomExternalDestination(),
+  // // Externals preloaded with TOK (cheaper to pay to)
+  At: randomExternalDestination(),
+  Bt: randomExternalDestination(),
+  // Asset Holders
+  ETH: undefined,
+  ETH2: undefined,
+  ERC20: undefined,
+};
+
+const tenPayouts = {ERC20: {}};
+const fiftyPayouts = {ERC20: {}};
+const oneHundredPayouts = {ERC20: {}};
+
+// for (let i = 0; i < 100; i++) {
+//   const destination = randomExternalDestination();
+//   addresses[i.toString()] = destination;
+//   if (i < 10) tenPayouts.ERC20[i.toString()] = 1;
+//   if (i < 50) fiftyPayouts.ERC20[i.toString()] = 1;
+//   if (i < 100) oneHundredPayouts.ERC20[i.toString()] = 1;
+// }
+
+// Populate wallets and participants array
+for (let i = 0; i < 3; i++) {
+  wallets[i] = Wallet.createRandom();
+  participants[i] = wallets[i].address;
+}
+beforeAll(async () => {
+  appDefinition = getPlaceHolderContractAddress();
+  Token = await setupContracts(provider, TokenArtifact, process.env.TEST_TOKEN_ADDRESS);
+  // Preload At and Bt with TOK
+  await (await Token.transfer('0x' + addresses.At.slice(26), BigNumber.from(1))).wait();
+  await (await Token.transfer('0x' + addresses.Bt.slice(26), BigNumber.from(1))).wait();
+});
+
+const accepts1 = '{ETH: {A: 1}}';
+const accepts2 = '{ETH: {A: 1}, ETH2: {A: 2}}';
+const accepts3 = '{ETH2: {A: 1, B: 1}}';
+const accepts4 = '{ERC20: {A: 1, B: 1}}';
+const accepts4a = '{ERC20: {A: 1}}';
+const accepts5 = '{ERC20: {At: 1, Bt: 1}} (At and Bt already have some TOK)';
+const accepts6 = '10 TOK payouts';
+const accepts7 = '50 TOK payouts';
+const accepts8 = '100 TOK payouts';
+
+const oneState = {
+  whoSignedWhat: [0, 0, 0],
+  appData: [ethers.constants.HashZero],
+};
+const turnNumRecord = 5;
+let channelNonce = getRandomNonce('concludePushOutcomeAndTransferAll');
+describe('concludePushOutcomeAndTransferAll', () => {
+  beforeEach(() => (channelNonce += 1));
+  it.each`
+    description | outcomeShortHand         | heldBefore         | heldAfter          | newOutcome | payouts                  | reasonString
+    ${accepts4} | ${{ERC20: {A: 1, B: 1}}} | ${{ERC20: {c: 2}}} | ${{ERC20: {c: 0}}} | ${{}}      | ${{ERC20: {A: 1, B: 1}}} | ${undefined}
+  `(
+    '$description', // For the purposes of this test, chainId and participants are fixed, making channelId 1-1 with channelNonce
+    async ({
+      description,
+      outcomeShortHand,
+      heldBefore,
+      heldAfter,
+      newOutcome,
+      payouts,
+      reasonString,
+    }: {
+      description: string;
+      outcomeShortHand: OutcomeShortHand;
+      initialFingerprint;
+      heldBefore: OutcomeShortHand;
+      heldAfter: OutcomeShortHand;
+      newOutcome: OutcomeShortHand;
+      payouts: OutcomeShortHand;
+      reasonString;
+    }) => {
+      const deployer = await provider.getSigner(1);
+      const channel: Channel = {chainId, participants, channelNonce};
+      const channelId = getChannelId(channel);
+
+      const SingleChannelAdjudicatorFactory = await ContractFactory.fromSolidity(
+        SingleChannelAdjudicatorArtifact,
+        deployer
+      );
+      console.log(SingleChannelAdjudicatorFactory);
+
+      const SingleChannelAdjudicator = await SingleChannelAdjudicatorFactory.deploy(channelId);
+
+      console.log(SingleChannelAdjudicatorFactory);
+
+      addresses.c = channelId;
+      addresses.ERC20 = Token.address; // this matches the changes made to the contract
+      const support = oneState;
+      const {appData, whoSignedWhat} = support;
+      const numStates = appData.length;
+      const largestTurnNum = turnNumRecord + 1;
+      const initialFingerprint = ethers.constants.HashZero;
+
+      // Transfer some tokens into the relevant AssetHolder
+      // Do this step before transforming input data (easier)
+      if ('ERC20' in heldBefore) {
+        await (await Token.transfer(SingleChannelAdjudicator.address, heldBefore.ERC20.c)).wait();
+      }
+      //   if ('ETH' in heldBefore) {
+      //     await (
+      //       await EthAssetHolder1.deposit(channelId, '0x00', heldBefore.ETH.c, {
+      //         value: heldBefore.ETH.c,
+      //       })
+      //     ).wait();
+      //   }
+      //   if ('ETH2' in heldBefore) {
+      //     await (
+      //       await EthAssetHolder2.deposit(channelId, '0x00', heldBefore.ETH2.c, {
+      //         value: heldBefore.ETH2.c,
+      //       })
+      //     ).wait();
+      //   }
+
+      // Transform input data (unpack addresses and BigNumberify amounts)
+      [heldBefore, outcomeShortHand, newOutcome, heldAfter, payouts] = [
+        heldBefore,
+        outcomeShortHand,
+        newOutcome,
+        heldAfter,
+        payouts,
+      ].map(object => replaceAddressesAndBigNumberify(object, addresses) as OutcomeShortHand);
+
+      // Compute the outcome.
+      const outcome: AllocationAssetOutcome[] = computeOutcome(outcomeShortHand);
+
+      // Construct states
+      const states: State[] = [];
+      for (let i = 1; i <= numStates; i++) {
+        states.push({
+          isFinal: true,
+          channel,
+          outcome,
+          appDefinition,
+          appData: appData[i - 1],
+          challengeDuration,
+          turnNum: largestTurnNum + i - numStates,
+        });
+      }
+
+      // Call public wrapper to set state (only works on test contract)
+      //   await (await NitroAdjudicator.setStatus(channelId, initialFingerprint)).wait();
+      //   expect(await NitroAdjudicator.statusOf(channelId)).toEqual(initialFingerprint);
+
+      // Sign the states
+      const sigs = await signStates(states, wallets, whoSignedWhat);
+
+      // Form transaction
+      const tx = SingleChannelAdjudicator.concludePushOutcomeAndTransferAll(
+        ...concludePushOutcomeAndTransferAllArgs(states, sigs, whoSignedWhat),
+        {gasLimit: 3000000}
+      );
+
+      // Switch on overall test expectation
+      if (reasonString) {
+        await expectRevert(() => tx, reasonString);
+      } else {
+        const receipt = await (await tx).wait();
+
+        expect(BigNumber.from(receipt.gasUsed).lt(BigNumber.from(NITRO_MAX_GAS))).toBe(true);
+
+        await writeGasConsumption(
+          './concludePushOutcomeAndTransferAll.gas.md',
+          description,
+          receipt.gasUsed
+        );
+
+        // Compute expected ChannelDataHash
+        const blockTimestamp = (await provider.getBlock(receipt.blockNumber)).timestamp;
+        const expectedFingerprint = channelDataToStatus({
+          turnNumRecord: 0,
+          finalizesAt: blockTimestamp,
+          outcome,
+        });
+
+        // Check fingerprint against the expected value
+        expect(await SingleChannelAdjudicator.status()).toEqual(expectedFingerprint);
+
+        // Extract logs
+        const {logs} = await (await tx).wait();
+
+        // Compile events from logs
+        const events = compileEventsFromLogs(logs, [SingleChannelAdjudicator]);
+
+        // Compile event expectations
+
+        const expectedEvents = [];
+
+        // Add Conclude event to expectations
+        expectedEvents.push({
+          contract: SingleChannelAdjudicator.address,
+          name: 'Concluded',
+          args: {channelId},
+        });
+
+        // Check that each expectedEvent is contained as a subset of the properies of each *corresponding* event: i.e. the order matters!
+        expect(events).toMatchObject(expectedEvents);
+
+        // Check new holdings on each AssetHolder
+        // checkMultipleHoldings(heldAfter, [EthAssetHolder1, EthAssetHolder2, ERC20AssetHolder]);
+
+        // Check new assetOutcomeHash on each AssetHolder
+        //     checkMultipleAssetOutcomeHashes(channelId, newOutcome, [
+        //       EthAssetHolder1,
+        //       EthAssetHolder2,
+        //       ERC20AssetHolder,
+        //     ]);
+      }
+    }
+  );
+});
