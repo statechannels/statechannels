941e27d63418e4243121c24a326a4d561f2c9baa
Alex Gap <alex.gap@gmail.com>
2021-04-08 16:16:19 -0700

    test: add crash and restart test

--- packages/server-wallet/src/__test-with-peers__/wallet/jumpstart.test.ts
+++ packages/server-wallet/src/__test-with-peers__/wallet/jumpstart.test.ts
@@ -1,5 +1,6 @@
 import {Wallet} from '../../wallet';
 import {
+  crashAndRestart,
   getPeersSetup,
   messageService,
   peerEngines,
@@ -34,6 +35,37 @@ describe('jumpstartObjectives', () => {
     expect(jumpstartResults).toHaveLength(0);
   });
 
+  it('can jumpstart objectives successfully after a restart', async () => {
+    let wallet = await Wallet.create(peerEngines.a, messageService, {numberOfAttempts: 1});
+
+    // This ensures that the channel will be joined so the objective can progress
+    peerEngines.b.on('objectiveStarted', async (o: WalletObjective) => {
+      await peerEngines.b.joinChannels([o.data.targetChannelId]);
+    });
+
+    const numberOfChannels = 5;
+
+    messageService.setLatencyOptions({dropRate: 1});
+    const createResponse = await wallet.createChannels(
+      Array(numberOfChannels).fill(getWithPeersCreateChannelsArgs())
+    );
+    const createResults = await Promise.all(createResponse.map(r => r.done));
+
+    expect(createResults).toHaveLength(numberOfChannels);
+    // No messages were sent through so we expect them all to fail
+    expect(createResults.every(d => d.type === 'EnsureObjectiveFailed')).toBe(true);
+
+    await crashAndRestart('A');
+
+    wallet = await Wallet.create(peerEngines.a, messageService, {numberOfAttempts: 1});
+
+    const jumpstartResponse = await wallet.jumpStartObjectives();
+    const jumpstartResults = await Promise.all(jumpstartResponse.map(r => r.done));
+
+    expect(jumpstartResults).toHaveLength(numberOfChannels);
+    // Jumpstart should of completed the objectives
+    expect(jumpstartResults.every(d => d.type === 'Success')).toBe(true);
+  });
   it('can jumpstart objectives successfully', async () => {
     const wallet = await Wallet.create(peerEngines.a, messageService, {numberOfAttempts: 1});
 
