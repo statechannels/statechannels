6f05259a779d81025d6e25ecb90600e841b03df1
Alex Gap <alex.gap@gmail.com>
2021-04-09 10:01:51 -0700

    test: remove redundant test

--- packages/server-wallet/src/__test-with-peers__/wallet/jumpstart.test.ts
+++ packages/server-wallet/src/__test-with-peers__/wallet/jumpstart.test.ts
@@ -13,6 +13,7 @@ beforeAll(getPeersSetup());
 afterAll(peersTeardown);
 
 jest.setTimeout(60_000);
+
 describe('jumpstartObjectives', () => {
   it('returns an empty array when there are no objectives', async () => {
     const wallet = await Wallet.create(peerEngines.a, messageService, {numberOfAttempts: 1});
@@ -58,35 +59,6 @@ describe('jumpstartObjectives', () => {
     await crashAndRestart('A');
 
     wallet = await Wallet.create(peerEngines.a, messageService, {numberOfAttempts: 1});
-
-    const jumpstartResponse = await wallet.jumpStartObjectives();
-    const jumpstartResults = await Promise.all(jumpstartResponse.map(r => r.done));
-
-    expect(jumpstartResults).toHaveLength(numberOfChannels);
-    // Jumpstart should of completed the objectives
-    expect(jumpstartResults.every(d => d.type === 'Success')).toBe(true);
-  });
-  it('can jumpstart objectives successfully', async () => {
-    const wallet = await Wallet.create(peerEngines.a, messageService, {numberOfAttempts: 1});
-
-    // This ensures that the channel will be joined so the objective can progress
-    peerEngines.b.on('objectiveStarted', async (o: WalletObjective) => {
-      await peerEngines.b.joinChannels([o.data.targetChannelId]);
-    });
-
-    const numberOfChannels = 5;
-    messageService.setLatencyOptions({dropRate: 1});
-    const createResponse = await wallet.createChannels(
-      Array(numberOfChannels).fill(getWithPeersCreateChannelsArgs())
-    );
-    const createResults = await Promise.all(createResponse.map(r => r.done));
-
-    expect(createResults).toHaveLength(numberOfChannels);
-    // No messages were sent through so we expect them all to fail
-    expect(createResults.every(d => d.type === 'EnsureObjectiveFailed')).toBe(true);
-
-    // Allow all messages through
-    messageService.setLatencyOptions({dropRate: 0});
 
     const jumpstartResponse = await wallet.jumpStartObjectives();
     const jumpstartResults = await Promise.all(jumpstartResponse.map(r => r.done));
