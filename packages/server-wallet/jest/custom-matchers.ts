f314436a14c4ddb88cf8533142a5915fb8df869b
Alex Gap <alex.gap@gmail.com>
2021-04-12 10:17:21 -0700

    test: add custom matcher for convenience

--- packages/server-wallet/jest/custom-matchers.ts
+++ packages/server-wallet/jest/custom-matchers.ts
@@ -1,4 +1,5 @@
 import { AllocationItem, areAllocationItemsEqual } from "@statechannels/wallet-core";
+import { ObjectiveDoneResult, ObjectiveResult } from "../src/wallet";
 
 expect.extend({
     toContainAllocationItem(received: AllocationItem[], argument: AllocationItem) {
@@ -51,3 +52,43 @@ expect.extend({
     }
   },
 });
+
+expect.extend({
+  toBeObjectiveDoneType: async (
+    received: ObjectiveResult[],
+    expected: ObjectiveDoneResult['type']
+  ) => {
+    const pass = received.length > 0 && received.every(async o => (await o.done).type === expected);
+    if (pass) {
+      const matchingObjectives = received.filter(async o => (await o.done).type === expected);
+      return {
+        pass,
+        message: () =>
+          `expected every objective to not be ${expected}. Failing objectives: ${printObjectives(
+            matchingObjectives
+          )}`,
+      };
+    } else {
+      if (received.length === 0) {
+        return {message: () => 'expected at least one ObjectiveResult', pass};
+      } else {
+        const notMatchingObjectives = received.filter(async o => (await o.done).type === expected);
+        return {
+          pass,
+          message: () =>
+            `expected every objective to be ${expected}. Failing objectives: ${printObjectives(
+              notMatchingObjectives
+            )}`,
+        };
+      }
+    }
+  },
+});
+
+async function printObjectives(results: ObjectiveResult[]): Promise<string> {
+  return JSON.stringify(
+    results.map(async r => ({type: (await r.done).type, objectiveId: r.objectiveId})),
+    null,
+    1
+  );
+}
--- packages/server-wallet/src/__test__/globals.d.ts
+++ packages/server-wallet/src/__test__/globals.d.ts
@@ -1,9 +1,12 @@
+import {ObjectiveDoneResult} from '../wallet';
+
 export {};
 declare global {
   namespace jest {
     interface Matchers</* eslint-disable-line */ R> {
       toContainAllocationItem<R>(received: {amount: string; destination: string}): R;
       toContainObject<R>(argument: R): R;
+      toBeObjectiveDoneType<R>(expectedType: ObjectiveDoneResult['type']): R;
     }
   }
 }
