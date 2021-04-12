5cffd8e23011c446f61b33b7fd79001d44d23742
Alex Gap <alex.gap@gmail.com>
2021-04-12 14:25:48 -0700

    test: multiple jumpstarts

--- packages/server-wallet/src/__test-with-peers__/wallet/jumpstart.test.ts
+++ packages/server-wallet/src/__test-with-peers__/wallet/jumpstart.test.ts
@@ -61,4 +61,35 @@ describe('jumpstartObjectives', () => {
 
     await expect(jumpstartResponse).toBeObjectiveDoneType('Success');
   });
+
+  it('can jumpstart multiple times', async () => {
+    const wallet = await Wallet.create(peerEngines.a, messageService, {
+      numberOfAttempts: 99999, // We want the wallet to keep trying
+      initialDelay: 100,
+      multiple: 1,
+    });
+
+    // This ensures that the channel will be joined so the objective can progress
+    peerEngines.b.on('objectiveStarted', async (o: WalletObjective) => {
+      await peerEngines.b.joinChannels([o.data.targetChannelId]);
+    });
+
+    const numberOfChannels = 5;
+
+    // No messages get through so none of the promises should resolve
+    messageService.setLatencyOptions({dropRate: 1});
+
+    const createResponse = await wallet.createChannels(
+      Array(numberOfChannels).fill(getWithPeersCreateChannelsArgs())
+    );
+    const jumpstartResponse1 = await wallet.jumpStartObjectives();
+    const jumpstartResponse2 = await wallet.jumpStartObjectives();
+
+    // Allow the messages through. The next retry from jumpstart or create will get a message through.
+    messageService.setLatencyOptions({dropRate: 0});
+
+    await expect(createResponse).toBeObjectiveDoneType('Success');
+    await expect(jumpstartResponse1).toBeObjectiveDoneType('Success');
+    await expect(jumpstartResponse2).toBeObjectiveDoneType('Success');
+  });
 });
