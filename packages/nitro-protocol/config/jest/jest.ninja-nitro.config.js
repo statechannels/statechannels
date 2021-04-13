9d267e4bcff74c6cb82b811a8a634196c7fbc772
geoknee <georgeknee@googlemail.com>
2021-04-13 15:21:15 +0100

    separate ninja-nitro test setup

--- packages/nitro-protocol/config/jest/jest.ninja-nitro.config.js
+++ packages/nitro-protocol/config/jest/jest.ninja-nitro.config.js
@@ -1,6 +1,6 @@
 var config = require('./jest.config');
 config.testMatch = ['<rootDir>/test/**/ninja-nitro/**/*.test.ts'];
 config.reporters = ['default'];
-config.globalSetup = '<rootDir>/jest/contract-test-setup.ts';
+config.globalSetup = '<rootDir>/jest/ninja-nitro-contract-test-setup.ts';
 config.globalTeardown = '<rootDir>/jest/contract-test-teardown.ts';
 module.exports = config;
