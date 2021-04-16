0158e3ab62704bb92d3d0a6e87cd68d9ce5d415a
Mike Kerzhner <mikerzhner@gmail.com>
2021-04-16 10:54:30 -0700

    refactor: start to add rich objective UI

---
+++ packages/browser-wallet/src/ui/objective/confirm-create-channel-workflow.tsx
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import '../wallet.scss';
+import {Button, Flex, Text as RimbleText} from 'rimble-ui';
+
+import {ObjectiveContext} from './objective-context';
+
+export const ConfirmCreateChannel = () => {
+  const onObjectiveEvent = useContext(ObjectiveContext);
+
+  return (
+    <Flex alignItems="left" flexDirection="column">
+      <RimbleText fontSize={2} pb={2}>
+        Do you wish to create a channel?
+      </RimbleText>
+      <Button id="yes" onClick={onObjectiveEvent({type: 'Approval'})}>
+        Yes
+      </Button>
+      <Button.Text onClick={onObjectiveEvent({type: 'Approval'})}>No</Button.Text>
+    </Flex>
+  );
+};
--- packages/browser-wallet/src/ui/wallet.tsx
+++ packages/browser-wallet/src/ui/wallet.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Interpreter} from 'xstate';
 import {useService} from '@xstate/react';
+import {DirectFunder} from '@statechannels/wallet-core';
 
 import {WindowContext} from './window-context';
 import './wallet.scss';
@@ -9,27 +10,45 @@ import {EnableEthereum} from './workflows/enable-ethereum-workflow';
 import {Layout} from './layout';
 import {ApproveBudgetAndFund} from './workflows/approve-budget-and-fund-workflow';
 import {CloseLedgerAndWithdraw} from './workflows/close-ledger-and-withdraw';
+import {Objective} from './objective/objective';
+import {ObjectiveContext} from './objective/objective-context';
 
 interface Props {
-  workflow: Interpreter<any, any, any>;
+  onObjectiveEvent(event: DirectFunder.OpenChannelEvent): void;
+  workflow?: Interpreter<any, any, any>;
+  // TODO: generalize to all rich objectives
+  objective?: DirectFunder.OpenChannelObjective;
 }
 
 export const Wallet = (props: Props) => {
-  const [current, send] = useService(props.workflow);
-  return (
-    <WindowContext.Provider value={window}>
+  if (props.workflow) {
+    const [current, send] = useService(props.workflow);
+    return (
+      <WindowContext.Provider value={window}>
+        <Layout>
+          {props.workflow.id === 'application-workflow' && (
+            <ApplicationWorkflow current={current} />
+          )}
+          {props.workflow.id === 'enable-ethereum' && (
+            <EnableEthereum current={current} send={send} />
+          )}
+          {props.workflow.id === 'approve-budget-and-fund' && (
+            <ApproveBudgetAndFund service={props.workflow} />
+          )}
+          {props.workflow.id === 'close-and-withdraw' && (
+            <CloseLedgerAndWithdraw service={props.workflow} />
+          )}
+        </Layout>
+      </WindowContext.Provider>
+    );
+  }
+  if (props.objective) {
+    <ObjectiveContext.Provider value={props.onObjectiveEvent}>
       <Layout>
-        {props.workflow.id === 'application-workflow' && <ApplicationWorkflow current={current} />}
-        {props.workflow.id === 'enable-ethereum' && (
-          <EnableEthereum current={current} send={send} />
-        )}
-        {props.workflow.id === 'approve-budget-and-fund' && (
-          <ApproveBudgetAndFund service={props.workflow} />
-        )}
-        {props.workflow.id === 'close-and-withdraw' && (
-          <CloseLedgerAndWithdraw service={props.workflow} />
-        )}
+        <Objective objective={props.objective} />
       </Layout>
-    </WindowContext.Provider>
-  );
+      ;
+    </ObjectiveContext.Provider>;
+  }
+  throw new Error('A wallet renders an objective or a workflow. Neither has been supplied.');
 };
