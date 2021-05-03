d872f2327698ecd16a44b10395eccb5c693ff129
Alex Gap <alex.gap@gmail.com>
2021-05-03 09:29:37 -0700

    chore: some method docs

--- packages/server-wallet/src/wallet/objective-monitor.ts
+++ packages/server-wallet/src/wallet/objective-monitor.ts
@@ -24,6 +24,7 @@ export class ObjectiveMonitor extends EventEmitter<ObjectiveMonitorEvents> {
   public constructor(engineEmitter: EngineEmitter, logger?: Logger) {
     super();
 
+    // Listen for new objectives and make sure we're monitoring them
     engineEmitter.on('objectiveStarted', async (o: WalletObjective) => {
       const {objectiveId} = o;
       if (!this.isMonitored(objectiveId)) {
@@ -33,6 +34,7 @@ export class ObjectiveMonitor extends EventEmitter<ObjectiveMonitorEvents> {
       }
     });
 
+    // Listen for objective completed and mark objectives as complete
     engineEmitter.on('objectiveSucceeded', o => {
       const {objectiveId} = o;
       if (this.isMonitored(objectiveId)) {
@@ -43,15 +45,31 @@ export class ObjectiveMonitor extends EventEmitter<ObjectiveMonitorEvents> {
       }
     });
   }
+
+  /**
+   * Returns whether an objective has been completed or not
+   * @param objectiveId The id of the objective to check
+   * @returns True if the objective status is failed or succeeded, false otherwise
+   */
   public isComplete(objectiveId: string): boolean {
     const result = this._objectives.get(objectiveId);
     return result === 'succeeded' || result === 'failed';
   }
 
+  /**
+   * Returns whether the ObjectiveMonitor is monitoring a specified objective
+   * @param objectiveId The id of the objective to check
+   * @returns true if the objective is monitored by the Objective Manager, false otherwise
+   */
   public isMonitored(objectiveId: string): boolean {
     return this._objectives.has(objectiveId);
   }
 
+  /**
+   * Mark an objective as complete
+   * @param objectiveId The id of the objective to update.
+   * @param status Either a succeeded or failed status. Defaults to succeeded.
+   */
   public markAsComplete(objectiveId: string, status: 'succeeded' | 'failed' = 'succeeded'): void {
     if (!this._objectives.has(objectiveId)) {
       throw new Error('Cannot update the handling status if the objective is not being monitored');
@@ -60,6 +78,10 @@ export class ObjectiveMonitor extends EventEmitter<ObjectiveMonitorEvents> {
     }
   }
 
+  /**
+   * Lets the Objective Monitor know that we should watch the objectives for any change of status
+   * @param objectives A collection of objectives
+   */
   public monitorObjectives(objectives: WalletObjective[]): void {
     for (const objective of objectives) {
       this._objectives.set(objective.objectiveId, objective.status);
