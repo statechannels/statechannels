90ecce645d8445f1c75355d977de72167e0d928c
Mike Kerzhner <mikerzhner@gmail.com>
2021-05-12 09:25:53 -0600

    fix: set ssl for the hub
    Heroku now requires ssl
    https://devcenter.heroku.com/changelog-items/2035

--- packages/hub/src/db-config.ts
+++ packages/hub/src/db-config.ts
@@ -4,10 +4,13 @@ import '../env';
 
 export const dbCofig: Config = {
   client: 'pg',
-  connection: process.env.DATABASE_URL || {
+  connection: process.env.DATABASE_URL + '?ssl=true&sslmode=require' || {
     host: process.env.HUB_DB_HOST,
     port: Number(process.env.HUB_DB_PORT),
     database: process.env.HUB_DB_NAME,
-    user: process.env.HUB_DB_USER
-  }
+    user: process.env.HUB_DB_USER,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
