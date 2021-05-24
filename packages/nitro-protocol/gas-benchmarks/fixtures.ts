2f5c9c139db22aff42f2bff5fbd812b5e1973f7a
geoknee <georgeknee@googlemail.com>
2021-05-24 13:26:30 +0100

    add some test fixtures
    TODO make Alice, Nigel and Bob distinct from each other

---
+++ packages/nitro-protocol/gas-benchmarks/fixtures.ts
@@ -0,0 +1,19 @@
+import {Wallet} from '@ethersproject/wallet';
+
+import {Channel, getChannelId} from '../src';
+
+export const chainId = '0x1';
+export const channelNonce = 2;
+
+export const Nigel = new Wallet(
+  '0x277fb9e0ad81dc836c60294e385b10dfcc0a9586eeb0b1d31da92e384a0d2efa'
+); // Pays gas for infastructure deployments.
+
+export const Alice = new Wallet(
+  '0x277fb9e0ad81dc836c60294e385b10dfcc0a9586eeb0b1d31da92e384a0d2efa'
+);
+export const Bob = new Wallet('0x277fb9e0ad81dc836c60294e385b10dfcc0a9586eeb0b1d31da92e384a0d2efa');
+export const participants = [Alice.address, Bob.address];
+
+const channel: Channel = {chainId, channelNonce, participants};
+export const channelId = getChannelId(channel);
