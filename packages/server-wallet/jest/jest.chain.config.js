cd34903513e6ec52ebb7ea4e5c2d0acb460442fe
Alex Gap <alex.gap@gmail.com>
2021-06-01 15:32:45 -0700

    allow custom matchers for chain tests This allows us to use the toBeObjectiveDoneType extension which is helpful dealing with the wallet in chain tests.

--- packages/server-wallet/jest/jest.chain.config.js
+++ packages/server-wallet/jest/jest.chain.config.js
@@ -1,6 +1,6 @@
 const config = require('./jest.config');
 config.testMatch = ['<rootDir>/src/**/__chain-test__/?(*.)test.ts?(x)'];
-config.setupFilesAfterEnv = [];
+config.setupFilesAfterEnv = ['./jest/custom-matchers.ts'];
 config.globalSetup = '<rootDir>/jest/chain-setup.ts';
 config.globalTeardown = '<rootDir>/jest/test-teardown.ts';
 module.exports = config;
