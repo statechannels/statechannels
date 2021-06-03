db48bfe932bc351de8d0326685ffb9cd61ceb143
geoknee <georgeknee@googlemail.com>
2021-06-03 14:15:01 +0100

    extend test coverage

--- packages/nitro-protocol/test/contracts/examples/XinJ/validTransition.test.ts
+++ packages/nitro-protocol/test/contracts/examples/XinJ/validTransition.test.ts
@@ -33,29 +33,41 @@ import {
   setupContract,
 } from '../../../test-helpers';
 
-const revertReasons = [
+type RevertReason =
   // each reason represents a distinct code path that we should check in this test
-  'destinations may not change',
-  'p2.amt constant',
-  'incorrect move from ABC',
-  'inferior support proof',
-  'incorrect move from A',
-  'incorrect move from B',
-  'move from ABC,A,B only',
-  'X / J outcome mismatch',
-  'appDefinition changed',
-  'challengeDuration changed',
-  '1 or 2 states required',
-  'whoSignedWhat.length must be 2',
-  'sig0 !by participant0',
-  'sig1 !by participant1',
-  'sig0 on state0 !by participant0',
-  'sig0 on state1 !by participant1',
-  'sig0 on state1 !by participant0',
-  'sig0 on state0 !by participant1',
-  'invalid whoSignedWhat',
-  'invalid transition in X',
-];
+  | 'destinations may not change'
+  | 'p2.amt constant'
+  | 'incorrect move from ABC'
+  | 'inferior support proof'
+  | 'incorrect move from A'
+  | 'incorrect move from B'
+  | 'move from ABC,A,B only'
+  | 'X / J outcome mismatch'
+  | 'appDefinition changed'
+  | 'challengeDuration changed'
+  | '1 or 2 states required'
+  | 'whoSignedWhat.length must be 2'
+  | 'sig0 !by participant0'
+  | 'sig1 !by participant1'
+  | 'sig0 on state0 !by participant0'
+  | 'sig0 on state1 !by participant1'
+  | 'sig0 on state1 !by participant0'
+  | 'sig0 on state0 !by participant1'
+  | 'invalid whoSignedWhat'
+  | 'invalid transition in X';
+
+// We also want to check each transition:
+//    AB
+//    ^^
+//   /  \
+// A      B
+// ^      ^
+//  \    /
+//    ABC
+
+// AND we want to check various ways to support a state in X
+// i.e. whoSignedWhat = [0,1], [0,0] and [1,0]
+// TODO we will need to have something more interesting than the null app in order to test [0,1] and [1,0] cases
 
 // Utilities
 // TODO: move to a src file
@@ -221,19 +233,60 @@ beforeAll(async () => {
 });
 
 describe('XinJ', () => {
-  it('returns true for a correct ABC=>A transition', async () => {
-    const turnNumTo = 0; // TODO this is unused, but presumably it _should_ be used
-    const nParticipants = 0; // TODO this is unused
-    const signedByFrom = 0b00; // TODO this is unused
-    const signedByTo = 0b01; // just by Alice
+  const turnNumTo = 0; // TODO this is unused, but presumably it _should_ be used
+  const nParticipants = 0; // TODO this is unused
+  const signedByFrom = 0b00; // TODO this is unused
+  it('returns true / false for a correct / incorrect ABC => A transition', async () => {
     const result = await xInJ.validTransition(
       fromVariablePartForJ,
       toVariablePartForJ,
       turnNumTo,
       nParticipants,
       signedByFrom,
-      signedByTo
+      0b01 // signedByTo = just Alice
     );
     expect(result).toBe(true);
+    const reason: RevertReason = 'incorrect move from ABC';
+    await expectRevert(
+      () =>
+        xInJ.validTransition(
+          fromVariablePartForJ,
+          toVariablePartForJ,
+          turnNumTo,
+          nParticipants,
+          signedByFrom,
+          0b10 // signedByTo = just Bob
+        ),
+      reason
+    );
+  });
+  it('returns true for a correct ABC=>B transition', async () => {
+    toVariablePartForJ.appData = encodeXinJData({
+      alreadyMoved: AlreadyMoved.B,
+      channelIdForX: getChannelId(stateForX.channel),
+      supportProofForX: supportProofForX(stateForX),
+    });
+    const result = await xInJ.validTransition(
+      fromVariablePartForJ,
+      toVariablePartForJ,
+      turnNumTo,
+      nParticipants,
+      signedByFrom,
+      0b10 // signedByTo = just Bob
+    );
+    expect(result).toBe(true);
+    const reason: RevertReason = 'incorrect move from ABC';
+    await expectRevert(
+      () =>
+        xInJ.validTransition(
+          fromVariablePartForJ,
+          toVariablePartForJ,
+          turnNumTo,
+          nParticipants,
+          signedByFrom,
+          0b01 // signedByTo = just Alice
+        ),
+      reason
+    );
   });
 });
