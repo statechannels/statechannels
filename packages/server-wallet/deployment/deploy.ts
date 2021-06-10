f14642f29a01a17b4c075ef197461716d698e9ba
geoknee <georgeknee@googlemail.com>
2021-06-10 17:58:29 +0100

    rip out GanacheDeployer from server wallet

--- packages/server-wallet/deployment/deploy.ts
+++ packages/server-wallet/deployment/deploy.ts
@@ -1,7 +1,14 @@
 import {Address} from '@statechannels/client-api-schema';
 import {ContractArtifacts, TestContractArtifacts} from '@statechannels/nitro-protocol';
-import {TEST_ACCOUNTS, GanacheDeployer} from '@statechannels/devtools';
-import {Wallet} from 'ethers';
+import {TEST_ACCOUNTS} from '@statechannels/devtools';
+import {ContractFactory, providers, Wallet} from 'ethers';
+
+// eslint-disable-next-line no-process-env
+const rpcEndPoint = 'http://localhost:' + process.env.GANACHE_PORT;
+const provider = new providers.JsonRpcProvider(rpcEndPoint);
+
+const {EthAssetHolderArtifact, Erc20AssetHolderArtifact} = ContractArtifacts;
+const {TestNitroAdjudicatorArtifact, TokenArtifact} = TestContractArtifacts;
 
 // NOTE: deploying contracts like this allows the onchain service package to
 // be easily extracted
@@ -13,33 +20,32 @@ export type TestNetworkContext = {
   NITRO_ADJUDICATOR_ADDRESS: Address;
 };
 
+const [
+  ethAssetHolderFactory,
+  erc20AssetHolderFactory,
+  testNitroAdjudicatorFactory,
+  tokenFactory
+] = [
+  EthAssetHolderArtifact,
+  Erc20AssetHolderArtifact,
+  TestNitroAdjudicatorArtifact,
+  TokenArtifact
+].map(artifact =>
+  new ContractFactory(artifact.abi, artifact.bytecode).connect(provider.getSigner(0))
+);
+
 export async function deploy(): Promise<TestNetworkContext> {
-  const ethereumPrivateKey = TEST_ACCOUNTS[0].privateKey;
-  const ganachePort = Number(process.env.GANACHE_PORT);
 
-  // TODO: best way to configure this?
-  const deployer = new GanacheDeployer(ganachePort, ethereumPrivateKey);
-  const {EthAssetHolderArtifact, Erc20AssetHolderArtifact} = ContractArtifacts;
+  const NITRO_ADJUDICATOR_ADDRESS = (await testNitroAdjudicatorFactory.deploy()).address;
+  const ETH_ASSET_HOLDER_ADDRESS = (await ethAssetHolderFactory.deploy(NITRO_ADJUDICATOR_ADDRESS))
+    .address;
 
-  const NITRO_ADJUDICATOR_ADDRESS = await deployer.deploy(
-    TestContractArtifacts.TestNitroAdjudicatorArtifact as any
-  );
-  const ERC20_ADDRESS = await deployer.deploy(
-    TestContractArtifacts.TokenArtifact as any,
-    {},
-    new Wallet(ethereumPrivateKey).address
-  );
-  const ERC20_ASSET_HOLDER_ADDRESS = await deployer.deploy(
-    Erc20AssetHolderArtifact as any,
-    {},
-    NITRO_ADJUDICATOR_ADDRESS,
-    ERC20_ADDRESS
-  );
-  const ETH_ASSET_HOLDER_ADDRESS = await deployer.deploy(
-    EthAssetHolderArtifact as any,
-    {},
-    NITRO_ADJUDICATOR_ADDRESS
-  );
+  const ERC20_ADDRESS = (
+    await tokenFactory.deploy(new Wallet(TEST_ACCOUNTS[0].privateKey).address)
+  ).address;
+  const ERC20_ASSET_HOLDER_ADDRESS = (
+    await erc20AssetHolderFactory.deploy(NITRO_ADJUDICATOR_ADDRESS, ERC20_ADDRESS)
+  ).address;
 
   return {
     NITRO_ADJUDICATOR_ADDRESS,
