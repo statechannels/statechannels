1aa367d7888abaacef6449a7ae990bde95491a16
geoknee <georgeknee@googlemail.com>
2021-06-17 09:04:41 +0100

    add fixtures for G and J

--- packages/nitro-protocol/gas-benchmarks/fixtures.ts
+++ packages/nitro-protocol/gas-benchmarks/fixtures.ts
@@ -3,12 +3,14 @@ import {Wallet} from '@ethersproject/wallet';
 import {constants, ContractReceipt} from 'ethers';
 
 import {
+  Allocation,
   Bytes32,
   Channel,
   convertAddressToBytes32,
   encodeOutcome,
   getChannelId,
   getFixedPart,
+  Guarantee,
   hashAppPart,
   signChallengeMessage,
   SignedState,
@@ -27,6 +29,9 @@ export const Alice = new Wallet(
   '0x277fb9e0ad81dc836c60294e385b10dfcc0a9586eeb0b1d31da92e384a0d2efa'
 );
 export const Bob = new Wallet('0xc8774aa98410b3e3281ff1ec40ea2637d2b9280328c4d1ff00d06cd95dd42cbd');
+export const Ingrid = new Wallet(
+  '0x558789345da13a7ac1d6d6ac9275ba66836eb4a088efc1920db0f5d092d6ee71'
+);
 export const participants = [Alice.address, Bob.address];
 
 export const channel: Channel = {chainId, channelNonce, participants};
@@ -34,6 +39,7 @@ export const channelId = getChannelId(channel);
 
 export const someOtherChannelId = getChannelId({...channel, channelNonce: 1337});
 
+// X
 export function someState(assetHolderAddress: string): State {
   return {
     challengeDuration: 600,
@@ -54,7 +60,10 @@ export function someState(assetHolderAddress: string): State {
   };
 }
 
-export function someLedgerState(assetHolderAddress: string): State {
+// L
+export const ledgerChannel: Channel = {chainId, channelNonce: channelNonce + 10000, participants};
+export const ledgerChannelId = getChannelId(ledgerChannel);
+export function someLedgerStateFundingX(assetHolderAddress: string): State {
   return {
     challengeDuration: 600,
     appDefinition: constants.AddressZero,
@@ -70,6 +79,86 @@ export function someLedgerState(assetHolderAddress: string): State {
     appData: '0x',
   };
 }
+export function someLedgerStateFundingG(assetHolderAddress: string): State {
+  return {
+    challengeDuration: 600,
+    appDefinition: constants.AddressZero,
+    channel: ledgerChannel,
+    turnNum: 6,
+    isFinal: false,
+    outcome: [
+      {
+        assetHolderAddress,
+        allocationItems: [{destination: guarantorChannelId, amount: '0xa'}],
+      },
+    ],
+    appData: '0x',
+  };
+}
+
+// J
+export const jointChannel: Channel = {
+  chainId,
+  channelNonce: channelNonce, // note different participant set so nonce re-use is OK
+  participants: [...participants, Ingrid.address],
+};
+export const jointChannelId = getChannelId(jointChannel);
+export const jointChannelAllocation: Allocation = [
+  {destination: channelId, amount: '0xa'},
+  {destination: convertAddressToBytes32(Ingrid.address), amount: '0xa'},
+];
+
+export function someJointChannelState(assetHolderAddress: string): State {
+  return {
+    challengeDuration: 600,
+    appDefinition: constants.AddressZero,
+    channel: jointChannel,
+    turnNum: 6,
+    isFinal: false,
+    outcome: [
+      {
+        assetHolderAddress,
+        allocationItems: jointChannelAllocation,
+      },
+    ],
+    appData: '0x',
+  };
+}
+
+// G
+export const guarantee: Guarantee = {
+  targetChannelId: jointChannelId,
+  destinations: [
+    convertAddressToBytes32(Alice.address),
+    convertAddressToBytes32(Ingrid.address),
+    channelId,
+  ],
+};
+
+export const guarantorChannel: Channel = {
+  chainId,
+  channelNonce: channelNonce + 10001,
+  participants,
+};
+export const guarantorChannelId = getChannelId(guarantorChannel);
+export function someGuarantorState(assetHolderAddress: string): State {
+  return {
+    challengeDuration: 600,
+    appDefinition: constants.AddressZero,
+    channel: guarantorChannel,
+    turnNum: 6,
+    isFinal: false,
+    outcome: [
+      {
+        assetHolderAddress,
+        guarantee,
+      },
+    ],
+    appData: '0x',
+  };
+}
+
+// Utils
 
 export function finalState(assetHolderAddress: string): State {
   return {
@@ -85,8 +174,8 @@ export function counterSignedSupportProof( // for challenging and outcome pushin
   fixedPart: FixedPart;
   variableParts: VariablePart[];
   isFinalCount: number;
-  whoSignedWhat: [0, 0];
-  signatures: [Signature, Signature];
+  whoSignedWhat: [0, 0] | [0, 0, 0];
+  signatures: [Signature, Signature] | [Signature, Signature, Signature];
   challengeSignature: Signature;
   outcomeBytes: string;
   stateHash: string;
@@ -97,11 +186,15 @@ export function counterSignedSupportProof( // for challenging and outcome pushin
     fixedPart: getFixedPart(state),
     variableParts: [getVariablePart(state)],
     isFinalCount: 0,
-    whoSignedWhat: [0, 0],
-    signatures: [
-      signState(state, Alice.privateKey).signature,
-      signState(state, Bob.privateKey).signature,
-    ],
+    whoSignedWhat: state.channel.participants.length == 2 ? [0, 0] : [0, 0, 0],
+    signatures:
+      state.channel.participants.length == 2
+        ? [signState(state, Alice.privateKey).signature, signState(state, Bob.privateKey).signature]
+        : [
+            signState(state, Alice.privateKey).signature,
+            signState(state, Bob.privateKey).signature,
+            signState(state, Ingrid.privateKey).signature,
+          ],
     challengeSignature: signChallengeMessage([{state} as SignedState], Alice.privateKey),
     outcomeBytes: encodeOutcome(state.outcome),
     stateHash: hashState(state),
@@ -117,8 +210,8 @@ export function finalizationProof( // for concluding
   appPartHash: Bytes32;
   outcomeBytes: Bytes;
   numStates: 1;
-  whoSignedWhat: [0, 0];
-  sigs: [Signature, Signature];
+  whoSignedWhat: [0, 0] | [0, 0, 0];
+  sigs: [Signature, Signature] | [Signature, Signature, Signature];
 } {
   return {
     largestTurnNum: state.turnNum,
@@ -126,11 +219,15 @@ export function finalizationProof( // for concluding
     appPartHash: hashAppPart(state),
     outcomeBytes: encodeOutcome(state.outcome),
     numStates: 1,
-    whoSignedWhat: [0, 0],
-    sigs: [
-      signState(state, Alice.privateKey).signature,
-      signState(state, Bob.privateKey).signature,
-    ],
+    whoSignedWhat: state.channel.participants.length == 2 ? [0, 0] : [0, 0, 0],
+    sigs:
+      state.channel.participants.length == 2
+        ? [signState(state, Alice.privateKey).signature, signState(state, Bob.privateKey).signature]
+        : [
+            signState(state, Alice.privateKey).signature,
+            signState(state, Bob.privateKey).signature,
+            signState(state, Ingrid.privateKey).signature,
+          ],
   };
 }
 
@@ -144,6 +241,3 @@ export async function waitForChallengesToTimeOut(finalizesAtArray: number[]): Pr
   await provider.send('evm_setNextBlockTimestamp', [finalizesAt + 1]);
   await provider.send('evm_mine', []);
 }
-
-export const ledgerChannel: Channel = {chainId, channelNonce: channelNonce + 10000, participants};
-export const ledgerChannelId = getChannelId(ledgerChannel);
