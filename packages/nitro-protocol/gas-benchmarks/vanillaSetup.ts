2702f4730500196da51684a9d53d3ec461814ee0
geoknee <georgeknee@googlemail.com>
2021-06-24 16:19:20 +0100

    remove deceased contracts from setup file

--- packages/nitro-protocol/gas-benchmarks/vanillaSetup.ts
+++ packages/nitro-protocol/gas-benchmarks/vanillaSetup.ts
@@ -8,8 +8,6 @@ import kill from 'tree-kill';
 import {BigNumber} from '@ethersproject/bignumber';
 
 import nitroAdjudicatorArtifact from '../artifacts/contracts/NitroAdjudicator.sol/NitroAdjudicator.json';
-import ethAssetHolderArtifact from '../artifacts/contracts/ETHAssetHolder.sol/ETHAssetHolder.json';
-import erc20AssetHolderArtifact from '../artifacts/contracts/ERC20AssetHolder.sol/ERC20AssetHolder.json';
 import tokenArtifact from '../artifacts/contracts/Token.sol/Token.json';
 import {NitroAdjudicator} from '../typechain/NitroAdjudicator';
 import {ETHAssetHolder} from '../typechain/ETHAssetHolder';
@@ -44,19 +42,9 @@ export const provider = new providers.JsonRpcProvider(hardHatNetworkEndpoint);
 
 let snapshotId = 0;
 
-const ethAssetHolderFactory = new ContractFactory(
-  ethAssetHolderArtifact.abi,
-  ethAssetHolderArtifact.bytecode
-).connect(provider.getSigner(0));
-
 const tokenFactory = new ContractFactory(tokenArtifact.abi, tokenArtifact.bytecode).connect(
   provider.getSigner(0)
 );
-
-const erc20AssetHolderFactory = new ContractFactory(
-  erc20AssetHolderArtifact.abi,
-  erc20AssetHolderArtifact.bytecode
-).connect(provider.getSigner(0));
 
 const nitroAdjudicatorFactory = new ContractFactory(
   nitroAdjudicatorArtifact.abi,
@@ -66,14 +54,7 @@ const nitroAdjudicatorFactory = new ContractFactory(
 beforeAll(async () => {
   await waitOn({resources: [hardHatNetworkEndpoint]});
   nitroAdjudicator = (await nitroAdjudicatorFactory.deploy()) as NitroAdjudicator & Contract;
-  ethAssetHolder = (await ethAssetHolderFactory.deploy(
-    nitroAdjudicator.address
-  )) as ETHAssetHolder & Contract;
   token = (await tokenFactory.deploy(provider.getSigner(0).getAddress())) as Token & Contract;
-  erc20AssetHolder = (await erc20AssetHolderFactory.deploy(
-    nitroAdjudicator.address,
-    token.address
-  )) as ERC20AssetHolder & Contract;
   snapshotId = await provider.send('evm_snapshot', []);
 });
 
