8f8c19d953aad213354c3a44b17cb78cf0f9ea3d
geoknee <georgeknee@googlemail.com>
2021-06-25 12:08:12 +0100

    update transfer.test given new architecture
    * points at testMultiAssetHolder
    * zero allocations left in place (for now)
    * use deposit API to prepare test

--- packages/nitro-protocol/test/contracts/MultiAssetHolder/transfer.test.ts
+++ packages/nitro-protocol/test/contracts/MultiAssetHolder/transfer.test.ts
@@ -1,17 +1,26 @@
 import {expectRevert} from '@statechannels/devtools';
-import {Contract, BigNumber} from 'ethers';
+import {constants, Contract} from 'ethers';
 
 import {
-  allocationToParams,
   getRandomNonce,
   getTestProvider,
+  MAGIC_ADDRESS_INDICATING_ETH,
   randomChannelId,
   randomExternalDestination,
   replaceAddressesAndBigNumberify,
+  setupContract,
 } from '../../test-helpers';
-import {encodeAllocation} from '../../../src/contract/outcome';
+import {encodeOutcome, hashOutcome, Outcome} from '../../../src/contract/outcome';
+import {TESTMultiAssetHolder} from '../../../typechain/TESTMultiAssetHolder';
+// eslint-disable-next-line import/order
+import TESTMultiAssetHolderArtifact from '../../../artifacts/contracts/test/TESTMultiAssetHolder.sol/TESTMultiAssetHolder.json';
+import {channelDataToStatus} from '../../../src';
 
-let AssetHolder: Contract;
+const testMultiAssetHolder = (setupContract(
+  getTestProvider(),
+  TESTMultiAssetHolderArtifact,
+  process.env.TEST_MULTI_ASSET_HOLDER_ADDRESS
+) as unknown) as TESTMultiAssetHolder & Contract;
 
 const addresses = {
   // Channels
@@ -23,29 +32,31 @@ const addresses = {
   B: randomExternalDestination(),
 };
 
-const reason0 = 'h(allocation)!=assetOutcomeHash';
+const reason0 = 'Channel not finalized';
 const reason1 = 'Indices must be sorted';
+const reason2 = 'incorrect fingerprint';
 
 // c is the channel we are transferring from.
 describe('transfer', () => {
   it.each`
-    name                               | heldBefore | setOutcome            | indices      | newOutcome      | heldAfter       | payouts         | reason
-    ${' 0. outcome not set         '}  | ${{c: 1}}  | ${{}}                 | ${[0]}       | ${{}}           | ${{}}           | ${{A: 1}}       | ${reason0}
-    ${' 1. funded          -> 1 EOA'}  | ${{c: 1}}  | ${{A: 1}}             | ${[0]}       | ${{}}           | ${{}}           | ${{A: 1}}       | ${undefined}
-    ${' 2. overfunded      -> 1 EOA'}  | ${{c: 2}}  | ${{A: 1}}             | ${[0]}       | ${{}}           | ${{c: 1}}       | ${{A: 1}}       | ${undefined}
-    ${' 3. underfunded     -> 1 EOA'}  | ${{c: 1}}  | ${{A: 2}}             | ${[0]}       | ${{A: 1}}       | ${{}}           | ${{A: 1}}       | ${undefined}
-    ${' 4. funded      -> 1 channel'}  | ${{c: 1}}  | ${{C: 1}}             | ${[0]}       | ${{}}           | ${{c: 0, C: 1}} | ${{}}           | ${undefined}
-    ${' 5. overfunded  -> 1 channel'}  | ${{c: 2}}  | ${{C: 1}}             | ${[0]}       | ${{}}           | ${{c: 1, C: 1}} | ${{}}           | ${undefined}
-    ${' 6. underfunded -> 1 channel'}  | ${{c: 1}}  | ${{C: 2}}             | ${[0]}       | ${{C: 1}}       | ${{c: 0, C: 1}} | ${{}}           | ${undefined}
-    ${' 7. -> 2 EOA         1 index'}  | ${{c: 2}}  | ${{A: 1, B: 1}}       | ${[0]}       | ${{A: 0, B: 1}} | ${{c: 1}}       | ${{A: 1}}       | ${undefined}
-    ${' 8. -> 2 EOA         1 index'}  | ${{c: 1}}  | ${{A: 1, B: 1}}       | ${[0]}       | ${{A: 0, B: 1}} | ${{c: 0}}       | ${{A: 1}}       | ${undefined}
-    ${' 9. -> 2 EOA         partial'}  | ${{c: 3}}  | ${{A: 2, B: 2}}       | ${[1]}       | ${{A: 2, B: 1}} | ${{c: 2}}       | ${{B: 1}}       | ${undefined}
-    ${'10. -> 2 chan             no'}  | ${{c: 1}}  | ${{C: 1, X: 1}}       | ${[1]}       | ${{C: 1, X: 1}} | ${{c: 1}}       | ${{}}           | ${undefined}
-    ${'11. -> 2 chan           full'}  | ${{c: 1}}  | ${{C: 1, X: 1}}       | ${[0]}       | ${{C: 0, X: 1}} | ${{c: 0, C: 1}} | ${{}}           | ${undefined}
-    ${'12. -> 2 chan        partial'}  | ${{c: 3}}  | ${{C: 2, X: 2}}       | ${[1]}       | ${{C: 2, X: 1}} | ${{c: 2, X: 1}} | ${{}}           | ${undefined}
-    ${'13. -> 2 indices'}              | ${{c: 3}}  | ${{C: 2, X: 2}}       | ${[0, 1]}    | ${{C: 0, X: 1}} | ${{c: 0, X: 1}} | ${{C: 2}}       | ${undefined}
-    ${'14. -> 3 indices'}              | ${{c: 5}}  | ${{A: 1, C: 2, X: 2}} | ${[0, 1, 2]} | ${{}}           | ${{c: 0, X: 2}} | ${{A: 1, C: 2}} | ${undefined}
-    ${'15. -> reverse order (see 13)'} | ${{c: 3}}  | ${{C: 2, X: 2}}       | ${[1, 0]}    | ${{C: 2, X: 1}} | ${{c: 2, X: 1}} | ${{}}           | ${reason1}
+    name                                   | heldBefore | setOutcome            | indices      | newOutcome            | heldAfter       | payouts         | reason
+    ${' 0. channel not finalized        '} | ${{c: 1}}  | ${{}}                 | ${[0]}       | ${{}}                 | ${{}}           | ${{A: 1}}       | ${reason0}
+    ${' 1. funded          -> 1 EOA'}      | ${{c: 1}}  | ${{A: 1}}             | ${[0]}       | ${{A: 0}}             | ${{}}           | ${{A: 1}}       | ${undefined}
+    ${' 2. overfunded      -> 1 EOA'}      | ${{c: 2}}  | ${{A: 1}}             | ${[0]}       | ${{A: 0}}             | ${{c: 1}}       | ${{A: 1}}       | ${undefined}
+    ${' 3. underfunded     -> 1 EOA'}      | ${{c: 1}}  | ${{A: 2}}             | ${[0]}       | ${{A: 1}}             | ${{}}           | ${{A: 1}}       | ${undefined}
+    ${' 4. funded      -> 1 channel'}      | ${{c: 1}}  | ${{C: 1}}             | ${[0]}       | ${{C: 0}}             | ${{c: 0, C: 1}} | ${{}}           | ${undefined}
+    ${' 5. overfunded  -> 1 channel'}      | ${{c: 2}}  | ${{C: 1}}             | ${[0]}       | ${{C: 0}}             | ${{c: 1, C: 1}} | ${{}}           | ${undefined}
+    ${' 6. underfunded -> 1 channel'}      | ${{c: 1}}  | ${{C: 2}}             | ${[0]}       | ${{C: 1}}             | ${{c: 0, C: 1}} | ${{}}           | ${undefined}
+    ${' 7. -> 2 EOA         1 index'}      | ${{c: 2}}  | ${{A: 1, B: 1}}       | ${[0]}       | ${{A: 0, B: 1}}       | ${{c: 1}}       | ${{A: 1}}       | ${undefined}
+    ${' 8. -> 2 EOA         1 index'}      | ${{c: 1}}  | ${{A: 1, B: 1}}       | ${[0]}       | ${{A: 0, B: 1}}       | ${{c: 0}}       | ${{A: 1}}       | ${undefined}
+    ${' 9. -> 2 EOA         partial'}      | ${{c: 3}}  | ${{A: 2, B: 2}}       | ${[1]}       | ${{A: 2, B: 1}}       | ${{c: 2}}       | ${{B: 1}}       | ${undefined}
+    ${'10. -> 2 chan             no'}      | ${{c: 1}}  | ${{C: 1, X: 1}}       | ${[1]}       | ${{C: 1, X: 1}}       | ${{c: 1}}       | ${{}}           | ${undefined}
+    ${'11. -> 2 chan           full'}      | ${{c: 1}}  | ${{C: 1, X: 1}}       | ${[0]}       | ${{C: 0, X: 1}}       | ${{c: 0, C: 1}} | ${{}}           | ${undefined}
+    ${'12. -> 2 chan        partial'}      | ${{c: 3}}  | ${{C: 2, X: 2}}       | ${[1]}       | ${{C: 2, X: 1}}       | ${{c: 2, X: 1}} | ${{}}           | ${undefined}
+    ${'13. -> 2 indices'}                  | ${{c: 3}}  | ${{C: 2, X: 2}}       | ${[0, 1]}    | ${{C: 0, X: 1}}       | ${{c: 0, X: 1}} | ${{C: 2}}       | ${undefined}
+    ${'14. -> 3 indices'}                  | ${{c: 5}}  | ${{A: 1, C: 2, X: 2}} | ${[0, 1, 2]} | ${{A: 0, C: 0, X: 0}} | ${{c: 0, X: 2}} | ${{A: 1, C: 2}} | ${undefined}
+    ${'15. -> reverse order (see 13)'}     | ${{c: 3}}  | ${{C: 2, X: 2}}       | ${[1, 0]}    | ${{C: 2, X: 1}}       | ${{c: 2, X: 1}} | ${{}}           | ${reason1}
+    ${'16. incorrect fingerprint        '} | ${{c: 1}}  | ${{}}                 | ${[0]}       | ${{}}                 | ${{}}           | ${{A: 1}}       | ${reason2}
   `(
     `$name: heldBefore: $heldBefore, setOutcome: $setOutcome, newOutcome: $newOutcome, heldAfter: $heldAfter, payouts: $payouts`,
     async ({name, heldBefore, setOutcome, indices, newOutcome, heldAfter, reason}) => {
@@ -60,55 +71,107 @@ describe('transfer', () => {
       newOutcome = replaceAddressesAndBigNumberify(newOutcome, addresses);
       heldAfter = replaceAddressesAndBigNumberify(heldAfter, addresses);
 
-      // Reset the holdings (only works on test contract)
-      new Set([...Object.keys(heldAfter), ...Object.keys(heldBefore)]).forEach(async key => {
-        // Key must be either in heldBefore or heldAfter or both
-        const amount = heldBefore[key] ? heldBefore[key] : BigNumber.from(0);
-        await (await AssetHolder.setHoldings(key, amount)).wait();
-        expect((await AssetHolder.holdings(key)).eq(amount)).toBe(true);
-      });
+      // pay some ETH to the contract
+
+      // Deposit into channels
+
+      await Promise.all(
+        Object.keys(heldBefore).map(async key => {
+          // Key must be either in heldBefore or heldAfter or both
+          const amount = heldBefore[key];
+          await (
+            await testMultiAssetHolder.deposit(MAGIC_ADDRESS_INDICATING_ETH, key, 0, amount, {
+              value: amount,
+            })
+          ).wait();
+          expect(
+            (await testMultiAssetHolder.holdings(MAGIC_ADDRESS_INDICATING_ETH, key)).eq(amount)
+          ).toBe(true);
+        })
+      );
 
       // Compute an appropriate allocation.
       const allocation = [];
       Object.keys(setOutcome).forEach(key =>
         allocation.push({destination: key, amount: setOutcome[key]})
       );
-      const [, assetOutcomeHash] = allocationToParams(allocation);
+      const outcomeHash = hashOutcome([
+        {assetHolderAddress: MAGIC_ADDRESS_INDICATING_ETH, allocationItems: allocation},
+      ]);
+      const outcomeBytes = encodeOutcome([
+        {assetHolderAddress: MAGIC_ADDRESS_INDICATING_ETH, allocationItems: allocation},
+      ]);
 
-      // Set assetOutcomeHash
-      await (
-        await AssetHolder.setAssetOutcomeHashPermissionless(channelId, assetOutcomeHash)
-      ).wait();
-      expect(await AssetHolder.assetOutcomeHashes(channelId)).toBe(assetOutcomeHash);
+      // Set adjudicator status
+      const stateHash = constants.HashZero; // not realistic, but OK for purpose of this test
+      const challengerAddress = constants.AddressZero; // not realistic, but OK for purpose of this test
+      const finalizesAt = 42;
+      const turnNumRecord = 7;
 
-      const tx = AssetHolder.transfer(channelId, encodeAllocation(allocation), indices);
+      if (reason != 'Channel not finalized') {
+        await (
+          await testMultiAssetHolder.setStatusFromChannelData(channelId, {
+            turnNumRecord,
+            finalizesAt,
+            stateHash: constants.HashZero,
+            challengerAddress: constants.AddressZero,
+            outcomeHash,
+          })
+        ).wait();
+      }
+
+      const tx = testMultiAssetHolder.transfer(
+        MAGIC_ADDRESS_INDICATING_ETH,
+        channelId,
+        reason == 'incorrect fingerprint' ? '0xdeadbeef' : outcomeBytes,
+        stateHash,
+        challengerAddress,
+        indices
+      );
 
       // Call method in a slightly different way if expecting a revert
       if (reason) {
         await expectRevert(() => tx, reason);
       } else {
-        const expectedEvents = [
-          {
-            event: 'AllocationUpdated',
-            args: {channelId, initialHoldings: heldBefore[channelId]},
-          },
-        ];
-
         const {events: eventsFromTx} = await (await tx).wait();
-        // NOTE: _transferAsset is a NOOP in TESTAssetHolder, so gas costs will be much lower than for a real Asset Holder
-        expect(eventsFromTx).toMatchObject(expectedEvents);
         // Check new holdings
-        Object.keys(heldAfter).forEach(async key =>
-          expect(await AssetHolder.holdings(key)).toEqual(heldAfter[key])
+        await Promise.all(
+          Object.keys(heldAfter).map(async key =>
+            expect(await testMultiAssetHolder.holdings(MAGIC_ADDRESS_INDICATING_ETH, key)).toEqual(
+              heldAfter[key]
+            )
+          )
         );
 
-        // Check new assetOutcomeHash
+        // Check new status
         const allocationAfter = [];
         Object.keys(newOutcome).forEach(key => {
           allocationAfter.push({destination: key, amount: newOutcome[key]});
         });
-        const [, expectedNewOutcomeHash] = allocationToParams(allocationAfter);
-        expect(await AssetHolder.assetOutcomeHashes(channelId)).toEqual(expectedNewOutcomeHash);
+        const outcomeAfter: Outcome = [
+          {assetHolderAddress: MAGIC_ADDRESS_INDICATING_ETH, allocationItems: allocationAfter},
+        ];
+        const expectedStatusAfter = channelDataToStatus({
+          turnNumRecord,
+          finalizesAt,
+          // stateHash will be set to HashZero by this helper fn
+          // if state property of this object is undefined
+          challengerAddress,
+          outcome: outcomeAfter,
+        });
+        expect(await testMultiAssetHolder.statusOf(channelId)).toEqual(expectedStatusAfter);
+
+        const expectedEvents = [
+          {
+            event: 'FingerprintUpdated',
+            args: {
+              channelId,
+              outcomeBytes: encodeOutcome(outcomeAfter),
+            },
+          },
+        ];
+
+        expect(eventsFromTx).toMatchObject(expectedEvents);
       }
     }
   );
