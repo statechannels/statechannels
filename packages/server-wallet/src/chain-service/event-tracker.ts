0b218ffc1b0d2f721a13fda4731ed5167ea2576a
geoknee <georgeknee@googlemail.com>
2021-07-07 18:41:52 +0100

    reinstate assetMap to EventTracker
    assets are still treated independently,
    even though we don't have separate asset holders anymore

--- packages/server-wallet/src/chain-service/event-tracker.ts
+++ packages/server-wallet/src/chain-service/event-tracker.ts
@@ -1,3 +1,4 @@
+import {Address} from '@statechannels/nitro-protocol/src/contract/types';
 import {Logger} from 'pino';
 
 import {
@@ -18,36 +19,38 @@ import {
  */
 export class EventTracker {
   private logger: Logger;
-  private blockNumber = 0;
-  private logIndex = 0;
+  private assetMap: Map<Address, {blockNumber: number; logIndex: number}>;
   private managedSubscriber: ChainEventSubscriberInterface;
 
   constructor(managedSubscriber: ChainEventSubscriberInterface, logger: Logger) {
     this.logger = logger;
     this.managedSubscriber = managedSubscriber;
     this.logger = logger;
+    this.assetMap = new Map<string, {blockNumber: number; logIndex: number}>();
   }
 
-  private isNewEvent(blockNumber: number, logIndex: number): boolean {
+  private isNewEvent(asset: Address, blockNumber: number, logIndex: number): boolean {
+    const eventRecord = this.assetMap.get(asset);
     this.logger.debug(
-      `EventTracker.isNewEvent: ${blockNumber}, ${logIndex}, ${this.blockNumber}, ${this.logIndex}`
+      `EventTracker.isNewEvent: ${asset}, ${blockNumber}, ${logIndex}, ${JSON.stringify(
+        eventRecord
+      )}`
     );
 
     let isNew = false;
-    if (this.blockNumber == 0 && this.logIndex == 0) {
+    if (!eventRecord) {
       isNew = true;
-    } else if (blockNumber > this.blockNumber) {
+    } else if (blockNumber > eventRecord.blockNumber) {
       isNew = true;
-    } else if (blockNumber === this.blockNumber) {
-      if (logIndex > this.logIndex) {
+    } else if (blockNumber === eventRecord.blockNumber) {
+      if (logIndex > eventRecord.logIndex) {
         isNew = true;
       }
     }
 
     if (isNew) {
       // Save latest event ever seen
-      this.blockNumber = blockNumber;
-      this.logIndex = logIndex;
+      this.assetMap.set(asset, {blockNumber: blockNumber, logIndex: logIndex});
       this.logger.debug('Event is new');
       return true;
     } else {
@@ -63,14 +66,14 @@ export class EventTracker {
     logIndex: number = Number.MAX_SAFE_INTEGER // Why default to max: getInitialHoldings can call this without logIndex,
     // in which case should be considered the latest balance in block
   ): void {
-    if (this.isNewEvent(blockNumber, logIndex)) {
+    if (this.isNewEvent(arg.asset, blockNumber, logIndex)) {
       this.managedSubscriber.holdingUpdated(arg);
     }
   }
 
   // Pass event to managed subscriber only if new
   assetOutcomeUpdated(arg: AssetOutcomeUpdatedArg, blockNumber: number, logIndex: number): void {
-    if (this.isNewEvent(blockNumber, logIndex)) {
+    if (this.isNewEvent(arg.assetHolderAddress, blockNumber, logIndex)) {
       this.managedSubscriber.assetOutcomeUpdated(arg);
     }
   }
