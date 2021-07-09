2d7f592f6432ec26bc63e3761c3b49eb6664e82c
geoknee <georgeknee@googlemail.com>
2021-07-09 15:19:27 +0100

    fix typos

--- packages/nitro-protocol/test/contracts/MultiAssetHolder/deposit.test.ts
+++ packages/nitro-protocol/test/contracts/MultiAssetHolder/deposit.test.ts
@@ -67,79 +67,72 @@ describe('deposit', () => {
     ${description6} | ${ETH}   | ${0} | ${1}         | ${2}   | ${0}      | ${'holdings < expectedHeld'}
     ${description7} | ${ETH}   | ${3} | ${1}         | ${1}   | ${3}      | ${'holdings already sufficient'}
     ${description8} | ${ETH}   | ${3} | ${2}         | ${2}   | ${4}      | ${undefined}
-  `(
-    '$description',
-    async ({description, asset, held, expectedHeld, amount, reasonString, heldAfter}) => {
-      held = BigNumber.from(held);
-      expectedHeld = BigNumber.from(expectedHeld);
-      amount = BigNumber.from(amount);
-      heldAfter = BigNumber.from(heldAfter);
+  `('$description', async ({asset, held, expectedHeld, amount, reasonString, heldAfter}) => {
+    held = BigNumber.from(held);
+    expectedHeld = BigNumber.from(expectedHeld);
+    amount = BigNumber.from(amount);
+    heldAfter = BigNumber.from(heldAfter);
 
-      const destinationChannel: Channel = {chainId, channelNonce, participants};
-      const destination = getChannelId(destinationChannel);
+    const destinationChannel: Channel = {chainId, channelNonce, participants};
+    const destination = getChannelId(destinationChannel);
 
+    if (asset === ERC20) {
+      // Check msg.sender has enough tokens
+      const balance = await token.balanceOf(signer0Address);
+      await expect(balance.gte(held.add(amount))).toBe(true);
+
+      // Increase allowance
+      await (await token.increaseAllowance(testNitroAdjudicator.address, held.add(amount))).wait(); // Approve enough for setup and main test
+
+      // Check allowance updated
+      const allowance = BigNumber.from(
+        await token.allowance(signer0Address, testNitroAdjudicator.address)
+      );
+      expect(allowance.sub(amount).sub(held).gte(0)).toBe(true);
+    }
+
+    if (held > 0) {
+      // Set holdings by depositing in the 'safest' way
+      const {events} = await (
+        await testNitroAdjudicator.deposit(asset, destination, 0, held, {
+          value: asset === ETH ? held : 0,
+        })
+      ).wait();
+      expect(await testNitroAdjudicator.holdings(asset, destination)).toEqual(held);
       if (asset === ERC20) {
-        // Check msg.sender has enough tokens
-        const balance = await token.balanceOf(signer0Address);
-        await expect(balance.gte(held.add(amount))).toBe(true);
-
-        // Increase allowance
-        await (
-          await token.increaseAllowance(testNitroAdjudicator.address, held.add(amount))
-        ).wait(); // Approve enough for setup and main test
-
-        // Check allowance updated
-        const allowance = BigNumber.from(
-          await token.allowance(signer0Address, testNitroAdjudicator.address)
-        );
-        expect(allowance.sub(amount).sub(held).gte(0)).toBe(true);
-      }
-
-      if (held > 0) {
-        // Set holdings by depositing in the 'safest' way
-        const {events} = await (
-          await testNitroAdjudicator.deposit(asset, destination, 0, held, {
-            value: asset === ETH ? amount : 0,
-          })
-        ).wait();
-        expect(await testNitroAdjudicator.holdings(asset, destination)).toEqual(held);
-        if (asset === ERC20) {
-          const {data: amountTransferred} = getTransferEvent(events);
-          expect(held.eq(amountTransferred)).toBe(true);
-        }
-      }
-
-      const balanceBefore = await getBalance(asset, signer0Address);
-
-      const tx = testNitroAdjudicator.deposit(token.address, destination, expectedHeld, amount, {
-        value: asset === ETH ? amount : 0,
-      });
-
-      if (reasonString) {
-        await expectRevert(() => tx, reasonString);
-      } else {
-        const {events} = await (await tx).wait();
-        if (description == description5) console.log(await tx);
-
-        const depositedEvent = getDepositedEvent(events);
-        expect(depositedEvent).toMatchObject({
-          destination,
-          amountDeposited: heldAfter.sub(held),
-          destinationHoldings: heldAfter,
-        });
-
-        if (asset == ERC20) {
-          const amountTransferred = BigNumber.from(getTransferEvent(events).data);
-          expect(heldAfter.sub(held).eq(amountTransferred)).toBe(true);
-          const balanceAfter = await getBalance(asset, signer0Address);
-          expect(balanceAfter.eq(balanceBefore.sub(heldAfter.sub(held)))).toBe(true);
-        }
-
-        const allocatedAmount = await testNitroAdjudicator.holdings(token.address, destination);
-        await expect(allocatedAmount).toEqual(heldAfter);
+        const {data: amountTransferred} = getTransferEvent(events);
+        expect(held.eq(amountTransferred)).toBe(true);
       }
     }
-  );
+
+    const balanceBefore = await getBalance(asset, signer0Address);
+
+    const tx = testNitroAdjudicator.deposit(token.address, destination, expectedHeld, amount, {
+      value: asset === ETH ? amount : 0,
+    });
+
+    if (reasonString) {
+      await expectRevert(() => tx, reasonString);
+    } else {
+      const {events} = await (await tx).wait();
+      const depositedEvent = getDepositedEvent(events);
+      expect(depositedEvent).toMatchObject({
+        destination,
+        amountDeposited: heldAfter.sub(held),
+        destinationHoldings: heldAfter,
+      });
+
+      if (asset == ERC20) {
+        const amountTransferred = BigNumber.from(getTransferEvent(events).data);
+        expect(heldAfter.sub(held).eq(amountTransferred)).toBe(true);
+        const balanceAfter = await getBalance(asset, signer0Address);
+        expect(balanceAfter.eq(balanceBefore.sub(heldAfter.sub(held)))).toBe(true);
+      }
+
+      const allocatedAmount = await testNitroAdjudicator.holdings(token.address, destination);
+      await expect(allocatedAmount).toEqual(heldAfter);
+    }
+  });
 });
 
 const getDepositedEvent = events => events.find(({event}) => event === 'Deposited').args;
