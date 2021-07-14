8e73497c2d686f6a1d7da3dd4a29034808204937
Alex Gap <alex.gap@gmail.com>
2021-07-14 12:12:42 -0700

    Add utilities to start ganache and a load node

---
+++ packages/server-wallet/src/e2e-testing/scripts/start-ganache.ts
@@ -0,0 +1,96 @@
+import yargs from 'yargs/yargs';
+import {hideBin} from 'yargs/helpers';
+import {TEST_ACCOUNTS} from '@statechannels/devtools';
+import {writeFile} from 'jsonfile';
+import ganache from 'ganache-core';
+import {ethers} from 'ethers';
+import {waitUntilUsed} from 'tcp-port-used';
+import chalk from 'chalk';
+import _ from 'lodash';
+import ms from 'ms';
+
+import {deploy} from '../../../deployment/deploy';
+import {setupUnhandledErrorListeners} from '../utils';
+
+setupUnhandledErrorListeners();
+setupGanache();
+
+async function setupGanache() {
+  const commandArguments = await yargs(hideBin(process.argv))
+    .option('port', {
+      alias: 'p',
+      default: 8545,
+      describe: 'port for the ganache server to run on',
+    })
+    .option('miningInterval', {
+      alias: 'mi',
+      description: 'The duration(in ms) for how often a block should be mined.',
+      default: ms('2s'),
+    })
+    .option('displayGanacheOutput', {
+      alias: 'd',
+      description: 'Whether or not ganache output is displayed',
+      choices: ['verbose', 'normal', 'off'],
+      coerce: (arg: string) => arg.toLocaleLowerCase(),
+      default: 'normal',
+    })
+    .option('chainId', {alias: 'c', description: 'The chain id to use', default: 9001})
+    .option('artifactFile', {
+      alias: 'af',
+      description: 'The file to write the artifacts to',
+      default: 'temp/contract_artifacts.json',
+    }).argv;
+
+  // ganache core exports a very permissive object[] type for accounts
+  // it should be {balance: HexString, secretKey: string}[]
+  const serverOptions: ganache.IServerOptions = {
+    network_id: commandArguments.chainId,
+    networkId: commandArguments.chainId,
+    logger: {
+      log: (msg: string) =>
+        commandArguments.displayGanacheOutput === 'off' ? _.noop() : console.log(chalk.gray(msg)),
+    },
+    port: commandArguments.port,
+
+    accounts: TEST_ACCOUNTS.map(a => ({
+      balance: ethers.utils.parseEther('1000000').toHexString(),
+      secretKey: a.privateKey,
+    })),
+    gasLimit: 10_000_000,
+    gasPrice: '0x1',
+    verbose: commandArguments.displayGanacheOutput === 'verbose',
+  };
+  // These seem to be needed to get ganache to use the correct chain id
+  const workaroundOptions = {
+    _chainId: commandArguments.chainId,
+    _chainIdRpc: commandArguments.chainId,
+  };
+
+  const server = ganache.server({...serverOptions, ...workaroundOptions});
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  server.listen(commandArguments.port, () => {});
+
+  await waitUntilUsed(commandArguments.port, 500, ms('10s'));
+
+  // While ganache supports time based mining it disables auto mining after contract calls
+  // By sending the mine instructions ourselves we get the best of both worlds
+  setInterval(() => {
+    const payload = {id: Date.now(), jsonrpc: '2.0', method: 'evm_mine', params: []};
+
+    server.provider.send(payload, (err, _result) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }, commandArguments.miningInterval);
+
+  console.log(chalk.green(`Ganche started on port ${commandArguments.port}`));
+
+  const endpoint = `http://localhost:${commandArguments.port}`;
+
+  // Deploy the contracts and output to the artifact file
+  const deployResults = await deploy(endpoint);
+  await writeFile(commandArguments.artifactFile, deployResults);
+
+  console.log(chalk.green(`Contract artifacts written to ${commandArguments.artifactFile}`));
+}
---
+++ packages/server-wallet/src/e2e-testing/scripts/start-load-node.ts
@@ -0,0 +1,128 @@
+import path from 'path';
+import util from 'util';
+
+import * as jsonfile from 'jsonfile';
+import yargs from 'yargs/yargs';
+import {hideBin} from 'yargs/helpers';
+import _ from 'lodash';
+import chalk from 'chalk';
+import ms from 'ms';
+
+import {
+  defaultTestWalletConfig,
+  overwriteConfigWithDatabaseConnection,
+  WalletConfig,
+} from '../../config';
+import {DBAdmin} from '../..';
+import {ARTIFACTS_DIR} from '../../../jest/chain-setup';
+import {WalletLoadNode} from '../wallet-load-node';
+import {createArtifactDirectory, getRoleInfo, setupUnhandledErrorListeners} from '../utils';
+
+setupUnhandledErrorListeners();
+
+setupNode().then(serverNode => {
+  console.log(chalk.green(`Server Wallet node listening on port ${serverNode.loadPort}`));
+  serverNode.listen();
+});
+
+async function setupNode(): Promise<WalletLoadNode> {
+  createArtifactDirectory();
+
+  const {role: roleId, roleFile, dbPoolSizeMax, migrateDB, clearDB} = await yargs(
+    hideBin(process.argv)
+  )
+    .option('role', {
+      alias: 'r',
+      describe: 'The id of the role for this node',
+      demandOption: true,
+      type: 'string',
+    })
+    .option('roleFile', {
+      alias: 'f',
+      describe: 'The path to a file containing the role information',
+      default: './src/e2e-testing/test-data/roles.json',
+    })
+    .option('dbPoolSizeMax', {
+      alias: 'db',
+      default: 10,
+      min: 1,
+      describe: 'The maxmimum amount of concurrent db connections',
+    })
+    .option('migrateDB', {
+      alias: 'm',
+      default: false,
+      describe: `Whether the db should be migrated (and created if it doesn't exist`,
+    })
+    .option('clearDB', {
+      alias: 'c',
+      default: true,
+      describe: 'Whether the db is truncated before the node is started',
+    }).argv;
+
+  const {peers, roleConfig} = await getRoleInfo(roleFile, roleId);
+
+  console.log(chalk.cyanBright(`Starting server wallet node as role ${roleId}`));
+  console.log(chalk.yellow(`Configuration: ${util.inspect(roleConfig)}`));
+
+  const rpcEndPoint = `http://localhost:${roleConfig.ganachePort}`;
+
+  const walletConfig: WalletConfig = overwriteConfigWithDatabaseConnection(
+    defaultTestWalletConfig({
+      databaseConfiguration: {pool: {min: 0, max: dbPoolSizeMax}},
+      networkConfiguration: {
+        chainNetworkID: roleConfig.chainId,
+      },
+      loggingConfiguration: {
+        logDestination: path.join(ARTIFACTS_DIR, 'e2e.log'),
+        logLevel: 'trace',
+      },
+      chainServiceConfiguration: {
+        attachChainService: true,
+        provider: rpcEndPoint,
+        pk: roleConfig.chainPrivateKey,
+        allowanceMode: 'MaxUint',
+        pollingInterval: ms('1s'),
+      },
+      // Nodes are running locally so we expect communication to be quick
+      syncConfiguration: {
+        pollInterval: ms('1s'),
+        timeOutThreshold: ms('5 minutes'),
+        staleThreshold: ms('1 minute'),
+      },
+
+      privateKey: roleConfig.privateKey,
+    }),
+    {database: roleConfig.databaseName}
+  );
+
+  if (migrateDB) {
+    // If the DB already exist createDatabase does nothing
+    await DBAdmin.createDatabase(walletConfig);
+
+    await DBAdmin.migrateDatabase(walletConfig);
+  }
+  if (clearDB) {
+    // Clean out any existing test data
+    await DBAdmin.truncateDatabase(walletConfig);
+  }
+
+  // Set up the contract artifact env vars
+  // as they are still used by the wallet
+  const contractArtifacts = await jsonfile.readFile(roleConfig.artifactFile);
+  // eslint-disable-next-line no-process-env
+  process.env = {...process.env, ...contractArtifacts};
+
+  const serverConfig = {
+    serverId: roleId,
+    loadServerPort: roleConfig.loadServerPort,
+    messagePort: roleConfig.messagePort,
+  };
+
+  const serverNode = await WalletLoadNode.create(walletConfig, serverConfig, peers);
+
+  for (const {messagePort} of peers) {
+    await serverNode.registerMessagePeer(messagePort);
+  }
+
+  return serverNode;
+}
