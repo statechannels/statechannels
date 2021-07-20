7ca125f7321fa04789eadb14aa5140d4d0e20657
geoknee <georgeknee@googlemail.com>
2021-07-20 14:29:55 +0100

    increase timeout to 30s

--- packages/nitro-protocol/config/jest/jest.contracts.config.js
+++ packages/nitro-protocol/config/jest/jest.contracts.config.js
@@ -3,5 +3,5 @@ config.testMatch = ['<rootDir>/test/contracts/**/*.test.ts'];
 config.reporters = ['default'];
 config.globalSetup = '<rootDir>/jest/contract-test-setup.ts';
 config.globalTeardown = '<rootDir>/jest/contract-test-teardown.ts';
-config.testTimeout = 15_000;
+config.testTimeout = 30_000;
 module.exports = config;
