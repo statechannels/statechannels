fe7cf69b3d76f6efa32cb7af0391b904cc917dbd
Colin Kennedy <colin.kennedy@mesh.xyz>
2021-07-28 10:57:42 -0300

    refactor: move ChannelData/ChMode  definitions

--- packages/nitro-protocol/contracts/interfaces/IStatusManager.sol
+++ packages/nitro-protocol/contracts/interfaces/IStatusManager.sol
@@ -1,3 +1,13 @@
 pragma solidity 0.7.4;
 
-interface IStatusManager {}
+interface IStatusManager {
+    enum ChannelMode {Open, Challenge, Finalized}
+
+    struct ChannelData {
+        uint48 turnNumRecord;
+        uint48 finalizesAt;
+        bytes32 stateHash; // keccak256(abi.encode(State))
+        address challengerAddress;
+        bytes32 outcomeHash;
+    }
+}
