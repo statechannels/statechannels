2127fd55748b65d5d6328e23ce3e382d0adad2e7
Colin Kennedy <colin.kennedy@mesh.xyz>
2021-07-28 11:26:38 -0300

    refactor: adjust inheritance on NitroAdjudicator

--- packages/nitro-protocol/contracts/NitroAdjudicator.sol
+++ packages/nitro-protocol/contracts/NitroAdjudicator.sol
@@ -9,7 +9,7 @@ import './MultiAssetHolder.sol';
 /**
  * @dev The NitroAdjudicator contract extends MultiAssetHolder and hence inherits all MultiAssetHolder methods.
  */
-contract NitroAdjudicator is MultiAssetHolder {
+contract NitroAdjudicator is ForceMove, MultiAssetHolder {
     /**
      * @notice Finalizes a channel by providing a finalization proof, and liquidates all assets for the channel.
      * @dev Finalizes a channel by providing a finalization proof, and liquidates all assets for the channel.
