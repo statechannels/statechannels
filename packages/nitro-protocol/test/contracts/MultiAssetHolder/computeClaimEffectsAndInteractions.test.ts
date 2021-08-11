63a3789a3dfdbce5fd02dd3804dd80b8efc49558
geoknee <georgeknee@googlemail.com>
2021-08-11 14:06:52 +0100

    rewrite test completely
    - it is now deterministic
    - both on and off chain methods tested
    - against the same literal inputs and  expectations
    - easy to add more test cases

--- packages/nitro-protocol/test/contracts/MultiAssetHolder/computeClaimEffectsAndInteractions.test.ts
+++ packages/nitro-protocol/test/contracts/MultiAssetHolder/computeClaimEffectsAndInteractions.test.ts
@@ -1,12 +1,7 @@
-import {BigNumber} from 'ethers';
-import shuffle from 'lodash.shuffle';
+import {constants} from 'ethers';
+import {Allocation, AllocationType} from '@statechannels/exit-format';
 
-import {
-  getTestProvider,
-  setupContract,
-  randomExternalDestination,
-  randomChannelId,
-} from '../../test-helpers';
+import {getTestProvider, setupContract} from '../../test-helpers';
 import {TESTNitroAdjudicator} from '../../../typechain/TESTNitroAdjudicator';
 // eslint-disable-next-line import/order
 import TESTNitroAdjudicatorArtifact from '../../../artifacts/contracts/test/TESTNitroAdjudicator.sol/TESTNitroAdjudicator.json';
@@ -17,65 +12,140 @@ const testNitroAdjudicator = (setupContract(
   process.env.TEST_NITRO_ADJUDICATOR_ADDRESS
 ) as unknown) as TESTNitroAdjudicator;
 
-import {AllocationItem, Guarantee} from '../../../src';
 import {computeClaimEffectsAndInteractions} from '../../../src/contract/multi-asset-holder';
+import {encodeGuaranteeData} from '../../../src/contract/outcome';
 
-const randomAllocation = (numAllocationItems: number): AllocationItem[] => {
-  return numAllocationItems > 0
-    ? [...Array(numAllocationItems)].map(() => ({
-        destination: randomExternalDestination(),
-        amount: BigNumber.from(Math.ceil(Math.random() * 10)).toHexString(),
-      }))
-    : [];
+const Alice = '0x000000000000000000000000000000000000000000000000000000000000000a';
+const Bob = '0x000000000000000000000000000000000000000000000000000000000000000b';
+
+interface TestCaseInputs {
+  initialHoldings: string;
+  sourceAllocations: Allocation[];
+  targetAllocations: Allocation[];
+  indexOfTargetInSource: number;
+  targetAllocationIndicesToPayout: number[];
+}
+
+interface TestCaseOutputs {
+  newSourceAllocations: Allocation[];
+  newTargetAllocations: Allocation[];
+  exitAllocations: Allocation[];
+  totalPayouts: string;
+}
+interface TestCase {
+  inputs: TestCaseInputs;
+  outputs: TestCaseOutputs;
+}
+
+const testcase1: TestCase = {
+  inputs: {
+    initialHoldings: '0x0f', // surplus initially 16
+    indexOfTargetInSource: 1,
+    targetAllocationIndicesToPayout: [], // meaning 'all'
+    sourceAllocations: [
+      {
+        destination: constants.HashZero,
+        amount: '0x08', // decreased to 8
+        allocationType: AllocationType.guarantee,
+        metadata: encodeGuaranteeData([Alice, Bob]),
+      },
+      {
+        destination: constants.HashZero,
+        amount: '0x07', // decreased to 7
+        allocationType: AllocationType.guarantee,
+        metadata: encodeGuaranteeData([Bob, Alice]),
+      },
+    ],
+    targetAllocations: [
+      {
+        destination: Alice, // Alice has a low priority. Nothing for her.
+        amount: '0x08',
+        allocationType: AllocationType.simple,
+        metadata: '0x',
+      },
+      {
+        destination: Bob, // Bob has highest priority
+        amount: '0x08', // surplus still 7, so 7 afforded for Bob
+        allocationType: AllocationType.simple,
+        metadata: '0x',
+      },
+    ],
+  },
+  outputs: {
+    newSourceAllocations: [
+      {
+        destination: constants.HashZero,
+        amount: '0x08',
+        allocationType: AllocationType.guarantee,
+        metadata: encodeGuaranteeData([Alice, Bob]),
+      },
+      {
+        destination: constants.HashZero,
+        amount: '0x00',
+        allocationType: AllocationType.guarantee,
+        metadata: encodeGuaranteeData([Bob, Alice]),
+      },
+    ],
+    newTargetAllocations: [
+      {
+        destination: Alice,
+        amount: '0x08',
+        allocationType: AllocationType.simple,
+        metadata: '0x',
+      },
+      {
+        destination: Bob,
+        amount: '0x01',
+        allocationType: AllocationType.simple,
+        metadata: '0x',
+      },
+    ],
+    exitAllocations: [
+      {
+        destination: Bob,
+        amount: '0x07',
+        allocationType: AllocationType.simple,
+        metadata: '0x',
+      },
+    ],
+    totalPayouts: '0x07',
+  },
 };
 
-const randomGuarantee = (allocation: AllocationItem[]): Guarantee => {
-  return {
-    targetChannelId: randomChannelId(),
-    destinations: shuffle(allocation.map(a => a.destination)),
-  };
-};
+const testCases: TestCase[][] = [[testcase1]];
 
-const heldBefore = BigNumber.from(100).toHexString();
-const allocation = randomAllocation(Math.ceil(Math.random() * 20));
-const indices = shuffle([...Array(allocation.length).keys()]); // [0, 1, 2, 3,...] but shuffled
-const guarantee = randomGuarantee(allocation);
-
-describe('MultiAssetHolder._computeNewAllocationWithGuarantee', () => {
-  it(`matches on chain method for input \n heldBefore: ${heldBefore}, \n allocation: ${JSON.stringify(
-    allocation,
-    null,
-    2
-  )}, \n indices: ${indices}, \n guarantee: ${JSON.stringify(guarantee, null, 2)}`, async () => {
-    // check local function works as expected
-    const locallyComputedNewAllocation = computeClaimEffectsAndInteractions(
-      heldBefore,
-      allocation,
-      indices,
-      guarantee
+describe('computeClaimEffectsAndInteractions', () => {
+  it.each(testCases)('off chain method matches expectation', (testCase: TestCase) => {
+    const offChainResult = computeClaimEffectsAndInteractions(
+      testCase.inputs.initialHoldings,
+      testCase.inputs.sourceAllocations,
+      testCase.inputs.targetAllocations,
+      testCase.inputs.indexOfTargetInSource,
+      testCase.inputs.targetAllocationIndicesToPayout
     );
 
-    const result = await testNitroAdjudicator.compute_claim_effects_and_interactions({});
+    expect(offChainResult.newSourceAllocations).toMatchObject(
+      testCase.outputs.newSourceAllocations
+    );
+    expect(offChainResult.newTargetAllocations).toMatchObject(
+      testCase.outputs.newTargetAllocations
+    );
+    expect(offChainResult.exitAllocations).toMatchObject(testCase.outputs.exitAllocations);
+    expect(offChainResult.totalPayouts).toEqual(testCase.outputs.totalPayouts);
+  });
 
-    //   BigNumber.from(heldBefore),
-    //   allocation,
-    //   indices,
-    //   guarantee
-    // );
+  it.each(testCases)('on chain method matches expectation', async (testCase: TestCase) => {
+    const onChainResult = await testNitroAdjudicator.compute_claim_effects_and_interactions(
+      testCase.inputs.initialHoldings,
+      testCase.inputs.sourceAllocations,
+      testCase.inputs.targetAllocations,
+      testCase.inputs.indexOfTargetInSource,
+      testCase.inputs.targetAllocationIndicesToPayout
+    );
 
-    expect(result).toBeDefined();
-    expect(result.newAllocation).toMatchObject(
-      locallyComputedNewAllocation.newAllocation.map(a => ({
-        ...a,
-        amount: BigNumber.from(a.amount),
-      }))
-    );
-    expect((result as any).allocatesOnlyZeros).toEqual(
-      locallyComputedNewAllocation.allocatesOnlyZeros
-    );
-    expect(result.totalPayouts).toEqual(BigNumber.from(locallyComputedNewAllocation.totalPayouts));
-    expect(result.payouts).toMatchObject(
-      locallyComputedNewAllocation.payouts.map(p => BigNumber.from(p))
-    );
+    expect(onChainResult.newSourceAllocations).toMatchObject(testCase.outputs.newSourceAllocations);
+    expect(onChainResult.newTargetAllocations).toMatchObject(testCase.outputs.newTargetAllocations);
+    expect(onChainResult.exitAllocations).toMatchObject(testCase.outputs.exitAllocations);
+    expect(onChainResult.totalPayouts).toEqual(testCase.outputs.totalPayouts);
   });
 });
