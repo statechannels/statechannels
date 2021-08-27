1a157b5b8d8e2c70bf620f1ce47797e79f741d48
geoknee <georgeknee@googlemail.com>
2021-08-27 09:33:04 +0100

    update MultiAssetHolder.sol to use ExitFormat
    completely rewrite transfer and claim
    (they now follow the checks, effect, interactions pattern, with each component in its own function)

--- packages/nitro-protocol/contracts/interfaces/IMultiAssetHolder.sol
+++ packages/nitro-protocol/contracts/interfaces/IMultiAssetHolder.sol
@@ -2,50 +2,12 @@
 pragma solidity 0.7.4;
 pragma experimental ABIEncoderV2;
 
-import '../Outcome.sol';
+import {ExitFormat as Outcome} from '@statechannels/exit-format/contracts/ExitFormat.sol';
 
 /**
  * @dev The IMultiAssetHolder interface calls for functions that allow assets to be transferred from one channel to other channel and/or external destinations, as well as for guarantees to be claimed.
  */
 interface IMultiAssetHolder {
-    /**
-     * @notice Transfers as many funds escrowed against `channelId` as can be afforded for a specific destination. Assumes no repeated entries.
-     * @dev Transfers as many funds escrowed against `channelId` as can be afforded for a specific destination. Assumes no repeated entries.
-     * @param assetIndex The index of the asset to be paid out (in the outcome)
-     * @param fromChannelId Unique identifier for state channel to transfer funds *from*.
-     * @param outcomeBytes The abi.encode of AssetOutcome.Allocation
-     * @param stateHash the hash of the state stored on chain
-     * @param indices Array with each entry denoting the index of a destination to transfer funds to.
-     */
-    function transfer(
-        uint256 assetIndex,
-        bytes32 fromChannelId,
-        bytes calldata outcomeBytes,
-        bytes32 stateHash,
-        uint256[] memory indices
-    ) external;
-
-    /**
-     * @notice Transfers as many funds escrowed against `guarantorChannelId` as can be afforded for a specific destination in the beneficiaries of the __target__ of that channel. Checks against the storage in this contract.
-     * @dev Transfers as many funds escrowed against `guarantorChannelId` as can be afforded for a specific destination in the beneficiaries of the __target__ of that channel. Checks against the storage in this contract.
-     * @param assetIndex The index of the asset to be paid out (in the outcome)
-     * @param guarantorChannelId Unique identifier for a guarantor state channel.
-     * @param guarantorOutcomeBytes The abi.encode of the guarantor outcome.
-     * @param guarantorStateHash the hash of the state stored on chain for the guarantor
-     * @param targetOutcomeBytes The abi.encode of the guarantor outcome.
-     * @param targetStateHash the hash of the state stored on chain for the guarantor
-     * @param indices Array with each entry denoting the index of a destination (in the target channel) to transfer funds to. Should be in increasing order. An empty array indicates "all".
-     */
-    function claim(
-        uint256 assetIndex,
-        bytes32 guarantorChannelId,
-        bytes memory guarantorOutcomeBytes,
-        bytes32 guarantorStateHash,
-        bytes memory targetOutcomeBytes,
-        bytes32 targetStateHash,
-        uint256[] memory indices
-    ) external;
-
     /**
      * @notice Deposit ETH against a given destination.
      * @dev Deposit ETH against a given destination.
@@ -60,6 +22,53 @@ interface IMultiAssetHolder {
         uint256 expectedHeld,
         uint256 amount
     ) external payable;
+
+    /**
+     * @notice Transfers as many funds escrowed against `channelId` as can be afforded for a specific destination. Assumes no repeated entries.
+     * @dev Transfers as many funds escrowed against `channelId` as can be afforded for a specific destination. Assumes no repeated entries.
+     * @param assetIndex Will be used to slice the outcome into a single asset outcome.
+     * @param fromChannelId Unique identifier for state channel to transfer funds *from*.
+     * @param outcomeBytes The encoded Outcome of this state channel
+     * @param stateHash The hash of the state stored when the channel finalized.
+     * @param indices Array with each entry denoting the index of a destination to transfer funds to. An empty array indicates "all".
+     */
+    function transfer(
+        uint256 assetIndex, // TODO consider a uint48?
+        bytes32 fromChannelId,
+        bytes memory outcomeBytes,
+        bytes32 stateHash,
+        uint256[] memory indices
+    ) external;
+
+    /**
+     * @param sourceChannelId Unique identifier for a guarantor state channel.
+     * @param sourceStateHash Hash of the state stored when the guarantor channel finalized.
+     * @param sourceOutcomeBytes The abi.encode of guarantor channel outcome
+     * @param sourceAssetIndex the index of the targetted asset in the source outcome.
+     * @param indexOfTargetInSource The index of the guarantee allocation to the target channel in the source outcome.
+     * @param targetStateHash Hash of the state stored when the target channel finalized.
+     * @param targetOutcomeBytes The abi.encode of target channel outcome
+     * @param targetAssetIndex the index of the targetted asset in the target outcome.
+     * @param targetAllocationIndicesToPayout Array with each entry denoting the index of a destination (in the target channel) to transfer funds to. Should be in increasing order. An empty array indicates "all"
+     */
+    struct ClaimArgs {
+        bytes32 sourceChannelId;
+        bytes32 sourceStateHash;
+        bytes sourceOutcomeBytes;
+        uint256 sourceAssetIndex;
+        uint256 indexOfTargetInSource;
+        bytes32 targetStateHash;
+        bytes targetOutcomeBytes;
+        uint256 targetAssetIndex;
+        uint256[] targetAllocationIndicesToPayout;
+    }
+
+    /**
+     * @notice Transfers as many funds escrowed against `sourceChannelId` as can be afforded for the destinations specified by indices in the beneficiaries of the __target__ of the channel at indexOfTargetInSource.
+     * @dev Transfers as many funds escrowed against `sourceChannelId` as can be afforded for the destinations specified by indices in the beneficiaries of the __target__ of the channel at indexOfTargetInSource.
+     * @param claimArgs arguments used in the claim function. Used to avoid stack too deep error.
+     */
+    function claim(ClaimArgs memory claimArgs) external;
 
     /**
      * @dev Indicates that `amountDeposited` has been deposited into `destination`.
